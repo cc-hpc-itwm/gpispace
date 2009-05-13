72f45e8fbea5ecaf8c72dec7f768ae1cef07b3fb
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-05-13 13:08:05 +0000

    * added logging support * added protocol buffers starting version
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@23 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/sdpa/trunk/sdpa/messages/SDPAEvent.hpp
@@ -0,0 +1,27 @@
+#ifndef SDPA_EVENT_HPP
+#define SDPA_EVENT_HPP 1
+
+#include <string>
+
+#include <seda/IEvent.hpp>
+#include <sdpa/memory.hpp>
+
+namespace sdpa {
+    class SDPAEvent : public seda::IEvent {
+    public:
+        typedef sdpa::shared_ptr<SDPAEvent> Ptr;
+
+        typedef std::string address_t;
+
+        SDPAEvent(const address_t &from, const address_t &to);
+        ~SDPAEvent() {}
+
+        const address_t & from() const { return from_; }
+        const address_t & to() const { return to_; }
+    private:
+        address_t from_;
+        address_t to_;
+    };
+}
+
+#endif // SDPA_EVENT_HPP
