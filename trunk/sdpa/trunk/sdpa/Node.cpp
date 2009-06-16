61daa9f9b004d078fba1830a92339120cdc5806e
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-06-16 15:01:57 +0000

    Added a class representing node related data TODO: implement a job-queue (currently it's just a typedef on std::list<Job::ptr_t>
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@95 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/sdpa/trunk/sdpa/Node.cpp
@@ -0,0 +1,14 @@
+#include "Node.hpp"
+
+using namespace sdpa;
+
+Node::Node(const std::string &name, const std::string &location)
+  : name_(name), location_(location), tstamp_(sdpa::util::now()) {}
+
+void Node::update(const sdpa::events::SDPAEvent &event) {
+  tstamp_ = sdpa::util::now();
+}
+
+void Node::dispatch(Job::ptr_t &job) {
+  queue_.push_back(job);
+}
