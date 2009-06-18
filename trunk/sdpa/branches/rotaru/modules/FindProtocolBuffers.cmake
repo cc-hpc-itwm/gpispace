284a233e67a3af9c364978b5a748863f93afb3d0
rotaru <rotaru@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-06-18 14:26:58 +0000

    updated JobFSM & OrchFSM
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@104 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/branches/rotaru/modules/FindProtocolBuffers.cmake
+++ trunk/sdpa/branches/rotaru/modules/FindProtocolBuffers.cmake
@@ -6,67 +6,76 @@
 # portable) 
 # * PB_PROTOC_CMD the protoc executable
 
-IF( NOT PB_ROOT )
-  SET(PB_ROOT "")
-ENDIF(NOT PB_ROOT)
+if(PB_HOME MATCHES "")
+  if("" MATCHES "$ENV{PB_HOME}")
+    message(STATUS "PB_HOME env is not set, setting it to /usr/local")
+    set (PB_HOME "/usr/local")
+  else("" MATCHES "$ENV{PB_HOME}")
+    set (PB_HOME "$ENV{PB_HOME}")
+  endif("" MATCHES "$ENV{PB_HOME}")
+else(PB_HOME MATCHES "")
+  message(STATUS "PB_HOME is not empty: \"${PB_HOME}\"")
+  set (PB_HOME "${PB_HOME}")
+endif(PB_HOME MATCHES "")
 
 IF(WIN32)
-FIND_FILE(PB_PROTOC_CMD
-  NAMES
+  FIND_FILE(PB_PROTOC_CMD
+    NAMES
     protoc.exe
-  PATHS
-    "[HKEY_CURRENT_USER\\smc\\bin]"
+    PATHS
+    ${PB_HOME}/bin
+    ${CMAKE_BINARY_PATH}
+    "[HKEY_CURRENT_USER\\protobuf\\bin]"
     /usr/local/bin
     /usr/bin
-    $ENV{PROTOC_HOME}/bin
-    ${PB_ROOT}/bin
     PATH_SUFFIXES
     bin
-  DOC "Location of the protoc file"
-)
+    DOC "Location of the protoc file"
+    )
 ELSE(WIN32)
-FIND_FILE(PB_PROTOC_CMD
-  NAMES
+  FIND_FILE(PB_PROTOC_CMD
+    NAMES
     protoc
-  PATHS
-    "[HKEY_CURRENT_USER\\smc\\bin]"
+    PATHS
+    ${PB_HOME}/bin
+    ${CMAKE_BINARY_PATH}
     /usr/local/bin
     /usr/bin
-    $ENV{PROTOC_HOME}/bin
     PATH_SUFFIXES
     bin
-  DOC "Location of the protoc file"
-)
+    DOC "Location of the protoc file"
+    )
 ENDIF(WIN32)
 
 FIND_PATH(PB_INCLUDE_DIR google/protobuf/message.h
+  ${PB_HOME}/include
   ${CMAKE_INCLUDE_PATH}
-  $ENV{PB_HOME}/include
-  ${PB_ROOT}/include
   /usr/local/include
   /usr/include
-)
+  )
 
 IF(WIN32)
   SET(PB_LIBRARY_NAMES ${PB_LIBRARY_NAMES} libprotobuf.lib)
 ELSE(WIN32)
-  SET(PB_LIBRARY_NAMES ${PB_LIBRARY_NAMES} libprotobuf.a)
+  SET(PB_LIBRARY_NAMES ${PB_LIBRARY_NAMES} libprotobuf.a libprotobuf.so)
 ENDIF(WIN32)
 
 FIND_LIBRARY(PB_LIBRARY
   NAMES ${PB_LIBRARY_NAMES}
-  PATHS ${CMAKE_LIBRARY_PATH} $ENV{PROTOC_HOME}/lib ${PB_ROOT}/lib /usr/lib /usr/local/lib
-)
-
+  PATHS ${PB_HOME}/lib ${CMAKE_LIBRARY_PATH} /usr/lib /usr/local/lib
+  )
 
 # if the include and the program are found then we have it
 IF(PB_PROTOC_CMD AND PB_LIBRARY) 
   SET(PB_FOUND "YES")
+else(PB_PROTOC_CMD AND PB_LIBRARY) 
+  message(STATUS "ProtocolBuffers could not be found, try setting PB_HOME (value=\"${PB_HOME}\").")
 ENDIF(PB_PROTOC_CMD AND PB_LIBRARY)
 
 MARK_AS_ADVANCED(
+  PB_HOME
   PB_PROTOC_CMD
   PB_LIBRARY
   PB_INCLUDE_DIR
-)
+  )
 
