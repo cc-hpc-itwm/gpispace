889182124ff1b1e7b533d5593181df99f910b51f
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-06-19 11:51:48 +0000

    renamed daemons to apps and moved cmdline client into that directory too
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@120 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/sdpa/trunk/apps/sdpac/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <sdpa/sdpa-config.hpp>
+
+int main (int argc, char **argv) {
+  std::cerr << "SDPA - Seismic Data Processing Architecture" << std::endl;
+  std::cerr << "===========================================" << std::endl;
+  std::cerr << std::endl;
+  std::cerr << "Version: " << SDPA_VERSION << std::endl;
+  std::cerr << "Copyright 2009 - Fraunhofer ITWM" << std::endl;
+}
