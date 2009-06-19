f9e4d6d437de132a556173cb2be803ea90d0671d
SVN_andreas_hohe <SVN_andreas_hohe@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-06-19 14:30:57 +0000

    included gwes main program for executing workflows on the command line
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@125 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/gwes/trunk/gwes_cpp/src/main.cpp
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2009 Fraunhofer Gesellschaft, Munich, Germany,
+ * for its Fraunhofer Institute for Computer Architecture and Software
+ * Technology (FIRST), Berlin, Germany 
+ * All rights reserved. 
+ */
+//gwes
+#include "GWES.h"
+#include "WorkflowObserver.h"
+#include "Channel.h"
+
+using namespace std;
+using namespace gwdl;
+using namespace gwes;
+
+void usage();
+string getUserName();
+
+int main(int argc, char* argv[]) {
+
+	int c;
+	int command=-1;
+	string workflowfn;
+
+	//	while ((c = getopt(argc, argv, "s:")) != -1) {
+	//		switch (c) {
+	//		case 'i':
+	//			command = COMMAND_INITIATE;
+	//			workflowfn = optarg;
+	//			break;
+	//		case '?':
+	//			if (optopt == 'i')
+	//				fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+	//			else if (isprint(optopt))
+	//				fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+	//			else
+	//				fprintf(stderr,
+	//				"Unknown option character `\\x%x'.\n",
+	//				optopt);
+	//			return 1;
+	//		default:
+	//			abort();
+	//		}
+	//	}
+
+	if (argc < 2) {
+		cerr << "ERROR: workflow not specified!" << endl;
+		usage();
+		exit(1);
+	} else {
+		workflowfn = argv[1];
+	}
+	
+	GWES gwes;
+	cout << "### BEGIN EXECUTION " << workflowfn << endl;
+	Workflow* wfP = new Workflow(workflowfn);
+
+	// initiate workflow
+	cout << "initiating workflow ..." << endl;
+	string workflowId = gwes.initiate(*wfP, getUserName());
+
+	// register channel with source observer
+	WorkflowObserver* observerP = new WorkflowObserver();
+	Channel* channelP = new Channel(observerP);
+	gwes.connect(channelP, workflowId);
+
+	// start workflow
+	gwes.start(workflowId);
+
+	// wait for workflow to end
+	WorkflowHandler* wfhP = gwes.getWorkflowHandlerTable().get(workflowId);
+	wfhP->waitForStatusChangeToCompletedOrTerminated();
+
+	// print workflow
+	cout << *wfP << endl;
+	cout << "### END EXECUTION " << workflowfn << endl;
+
+	//	//ToDo: return 0, 1, ...
+}
+
+void usage() {
+	cout << "---------------------------------------------------" << endl;
+	cout << "Usage: gwes <GWorkflowDL>" << endl;
+	cout << "<GWorkflowDL>: Filename of workflow to invoke." << endl;
+	cout << "---------------------------------------------------" << endl;
+}
+
+string getUserName() {
+	char* userNameP = getenv("USER");
+	if (userNameP != NULL) {
+		string userName(userNameP);
+		if ( userName.size() > 0) {
+			return userName; 
+		}
+	}
+	return "NN";
+}
+
