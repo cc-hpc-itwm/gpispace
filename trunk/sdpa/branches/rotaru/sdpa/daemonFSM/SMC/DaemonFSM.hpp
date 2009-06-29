13cbc22c7811c674422f3fa5b54f23b4d44fe273
rotaru <rotaru@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-06-29 14:36:30 +0000

    updated tests
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@198 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/branches/rotaru/sdpa/daemonFSM/SMC/DaemonFSM.hpp
+++ trunk/sdpa/branches/rotaru/sdpa/daemonFSM/SMC/DaemonFSM.hpp
@@ -5,29 +5,31 @@
 #include <sdpa/daemonFSM/SMC/DaemonFSM_sm.h>
 #include <sdpa/logging.hpp>
 
-namespace sdpa { namespace fsm { namespace smc {
-		class DaemonFSM : public sdpa::daemon::GenericDaemon {
-			public:
-				typedef std::tr1::shared_ptr<DaemonFSM> Ptr;
+namespace sdpa {
+	namespace fsm {
+		namespace smc {
+			class DaemonFSM : public sdpa::daemon::GenericDaemon {
+				public:
+					typedef std::tr1::shared_ptr<DaemonFSM> Ptr;
 
-				DaemonFSM() : SDPA_INIT_LOGGER("sdpa.fsm.DaemonFSM"), GenericDaemon("", ""), m_fsmContext(*this) {
-					SDPA_LOG_DEBUG("State machine created");
-				}
-
-				DaemonFSM(const std::string& name, const std::string& outputStage) :
-					SDPA_INIT_LOGGER("sdpa.fsm.DaemonFSM"), GenericDaemon(name, outputStage), m_fsmContext(*this) {
+					DaemonFSM() : SDPA_INIT_LOGGER("sdpa.fsm.DaemonFSM"), GenericDaemon("", ""), m_fsmContext(*this) {
 						SDPA_LOG_DEBUG("State machine created");
 					}
 
-				virtual ~DaemonFSM() {
-					SDPA_LOG_DEBUG("State machine destroyed");
-				}
+					DaemonFSM(const std::string& name, const std::string& outputStage) :
+						SDPA_INIT_LOGGER("sdpa.fsm.DaemonFSM"), GenericDaemon(name, outputStage), m_fsmContext(*this) {
+							SDPA_LOG_DEBUG("State machine created");
+						}
 
-				DaemonFSMContext& GetContext() { return m_fsmContext; }
-			private:
-				SDPA_DECLARE_LOGGER();
-				DaemonFSMContext m_fsmContext;
-		};
+					virtual ~DaemonFSM() {
+						SDPA_LOG_DEBUG("State machine destroyed");
+					}
+
+					DaemonFSMContext& GetContext() { return m_fsmContext; }
+				private:
+					SDPA_DECLARE_LOGGER();
+					DaemonFSMContext m_fsmContext;
+			};
 }}}
 
 #endif
