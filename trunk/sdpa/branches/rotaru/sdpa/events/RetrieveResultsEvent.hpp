1bd406f1eabaa597bdf57fe2a499d8f5f8dcf803
rotaru <rotaru@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-07-01 13:12:46 +0000

    changed RetrieveResultsEvent
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@216 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/sdpa/branches/rotaru/sdpa/events/RetrieveResultsEvent.hpp
@@ -0,0 +1,27 @@
+#ifndef SDPA_RETRIEVERESULTSEVENT_HPP
+#define SDPA_RETRIEVERESULTSEVENT_HPP
+
+#include <iostream>
+#include <boost/statechart/event.hpp>
+#include <sdpa/events/JobEvent.hpp>
+namespace sc = boost::statechart;
+
+namespace sdpa {
+namespace events {
+	class RetrieveResultsEvent : public sdpa::events::JobEvent, public sc::event<sdpa::events::RetrieveResultsEvent> {
+	public:
+		typedef sdpa::shared_ptr<RetrieveResultsEvent> Ptr;
+
+		RetrieveResultsEvent(const address_t& from, const address_t& to, const sdpa::job_id_t& job_id = sdpa::job_id_t())
+          :  sdpa::events::JobEvent(from, to, job_id) {
+			std::cout << "Create event 'RetrieveResultsEvent'"<< std::endl; }
+
+		virtual ~RetrieveResultsEvent() {
+			std::cout << "Delete event 'RetrieveResultsEvent'"<< std::endl; }
+
+		std::string str() const { std::cout<<from()<<" - RetrieveResultsEvent -> "<<to()<<std::endl; }
+	};
+}}
+
+
+#endif
