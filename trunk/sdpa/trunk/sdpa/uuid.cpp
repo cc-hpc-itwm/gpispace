8d656f42b227fbdc6aac2185b6925b11d5cfa978
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-07-01 08:43:23 +0000

    added a UUID wrapper class
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@208 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/sdpa/trunk/sdpa/uuid.cpp
@@ -0,0 +1,41 @@
+#include "uuid.hpp"
+#include <sstream>
+#include <cstring> // memcpy
+
+using namespace sdpa;
+
+uuid::uuid(const uuid &other) {
+  set(other.data());
+}
+
+uuid::uuid(const uuid_t &data) {
+  set(data);
+}
+
+uuid::uuid() {}
+
+uuid::~uuid() {}
+
+const uuid &uuid::operator=(const uuid &other) {
+  if (this != &other) {
+    set(other.data());
+  }
+  return *this;
+}
+
+void uuid::set(const uuid_t &data) {
+  dirty_ = true;
+  std::memcpy(uuid_, data, sizeof(uuid_t));
+}
+
+const std::string &uuid::str() const {
+  if (dirty_) {
+    std::stringstream sstr;
+    sstr << std::hex;
+    for (std::size_t i(0); i < sizeof(uuid_t); ++i) {
+      sstr << uuid_[i];
+    }
+    str_tmp_ = sstr.str();
+  }
+  return str_tmp_;
+}
---
+++ trunk/sdpa/trunk/sdpa/uuid.hpp
@@ -0,0 +1,35 @@
+#ifndef SDPA_UUID_HPP
+#define SDPA_UUID_HPP 1
+
+#include <string>
+
+namespace sdpa {
+  class uuid {
+  public:
+    typedef unsigned char uuid_t[16];
+
+    uuid(const uuid &other);
+
+    explicit
+    uuid(const uuid_t &data);
+
+    uuid();
+
+    ~uuid();
+
+    const uuid &operator=(const uuid &rhs);
+
+    const uuid_t &data() const { return uuid_; }
+    uuid_t &data() { return uuid_; }
+
+    const std::string &str() const;
+  private:
+    void set(const uuid_t &data);
+
+    uuid_t uuid_;
+    mutable bool dirty_;
+    mutable std::string str_tmp_;
+  };
+}
+
+#endif
