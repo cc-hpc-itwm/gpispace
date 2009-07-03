51707d6c1845b672bdbf5aa8d3821ed2d2626b78
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-07-03 15:12:46 +0000

    made use of the client api to test its usability
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@233 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/apps/sdpac/main.cpp
+++ trunk/sdpa/trunk/apps/sdpac/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+
 #include <sdpa/sdpa-config.hpp>
+#include <sdpa/client/Client.hpp>
 
 int main (int argc, char **argv) {
   std::cerr << "SDPA - Seismic Data Processing Architecture" << std::endl;
@@ -7,4 +9,18 @@ int main (int argc, char **argv) {
   std::cerr << std::endl;
   std::cerr << "Version: " << SDPA_VERSION << std::endl;
   std::cerr << "Copyright 2009 - Fraunhofer ITWM" << std::endl;
+  std::cerr << "Contact: alexander.petry@itwm.fraunhofer.de" << std::endl;
+  std::cerr << std::endl;
+
+  sdpa::client::Client::ptr_t api(sdpa::client::Client::create());
+
+  api->start();
+
+  sdpa::job_id_t jid = api->submitJob("<xml></xml>");
+  api->queryJob(jid);
+  api->cancelJob(jid);
+  api->retrieveResults(jid);
+  api->deleteJob(jid);
+
+  api->shutdown();
 }
