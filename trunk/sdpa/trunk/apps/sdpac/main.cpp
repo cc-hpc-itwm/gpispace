f23c5f82599ca5a07bf36eef54e62dc1f345acf2
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-07-06 12:43:20 +0000

    beautified the client-header-output information
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@240 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/apps/sdpac/main.cpp
+++ trunk/sdpa/trunk/apps/sdpac/main.cpp
@@ -4,16 +4,23 @@
 #include <sdpa/client/Client.hpp>
 
 int main (int argc, char **argv) {
-  std::cerr << "SDPA - Seismic Data Processing Architecture" << std::endl;
-  std::cerr << "===========================================" << std::endl;
+  std::cerr << "           "
+            << "SDPA - Seismic Data Processing Architecture" << std::endl;
+  std::cerr << "           "
+            << "===========================================" << std::endl;
+  std::cerr << "                            "
+            << "v" << SDPA_VERSION
+            << std::endl
+            << "                 "
+            << " " << SDPA_COPYRIGHT
+            << std::endl;
+  std::cerr << "       "
+            << SDPA_CONTACT
+            << std::endl;
   std::cerr << std::endl;
-  std::cerr << "Version: " << SDPA_VERSION << std::endl;
-  std::cerr << "Copyright 2009 - Fraunhofer ITWM" << std::endl;
-  std::cerr << "Contact: alexander.petry@itwm.fraunhofer.de" << std::endl;
-  std::cerr << std::endl;
+
 
   sdpa::client::Client::ptr_t api(sdpa::client::Client::create());
-
   api->start();
 
   sdpa::job_id_t jid = api->submitJob("<xml></xml>");
