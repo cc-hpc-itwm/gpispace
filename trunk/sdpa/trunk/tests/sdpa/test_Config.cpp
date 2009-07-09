c88d2d24dc0d3caeaa44dfa7df954448d267c43b
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-07-09 09:32:05 +0000

    added a small test suite for the sdpa::config::Config class
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@281 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/sdpa/trunk/tests/sdpa/test_Config.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <sstream>
+
+#include "test_Config.hpp"
+#include <sdpa/util.hpp>
+#include <sdpa/Config.hpp>
+
+using namespace sdpa;
+using namespace sdpa::tests;
+
+CPPUNIT_TEST_SUITE_REGISTRATION( sdpa::tests::ConfigTest );
+
+void ConfigTest::setUp() {
+}
+
+void ConfigTest::tearDown() {
+}
+
+void ConfigTest::testPopulate() {
+  sdpa::config::Config::ptr_t cfg = sdpa::config::Config::create();
+  sdpa::config::CommonConfiguration()(*cfg);
+  cfg->parse_env();
+  cfg->parse_file("...");
+  cfg->parse("./program blah blah blah");
+
+  CPPUNIT_ASSERT(cfg->is_set("sdpa.logging.level"));
+  CPPUNIT_ASSERT(cfg->get("sdpa.logging.level") == "debug");
+
+  if (cfg->is_set("sdpac.server")) { // orchestrator
+
+  }
+}
