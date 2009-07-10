28f9deef6430caf924b21419da839fd7a5a9a6ba
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-07-10 13:29:07 +0000

    basic logger skeleton
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@299 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/fhglog/trunk/fhglog/Logger.hpp
+++ trunk/fhglog/trunk/fhglog/Logger.hpp
@@ -1,4 +1,66 @@
-#ifndef FHGLOG_LOGGER_HPP
-#define FHGLOG_LOGGER_HPP 1
+#ifndef FHG_LOG_LOGGER_HPP
+#define FHG_LOG_LOGGER_HPP 1
+
+#include <string>
+
+#include <fhglog/LogEvent.hpp>
+#include <fhglog/Appender.hpp>
+
+namespace fhg { namespace log {
+
+  class LogLevel {
+  public:
+    enum Level {
+      UNSET = 0,
+      TRACE,
+      DEBUG,
+      INFO,
+      WARN,
+      ERROR,
+      FATAL
+    };
+
+    LogLevel(Level lvl)
+      : lvl_(lvl) {}
+    LogLevel(const LogLevel &other)
+      : lvl_(other.lvl_) {}
+
+    LogLevel &operator=(const LogLevel &other) {
+      if (this != &other) {
+        lvl_ = other.lvl_;
+      }
+      return *this;
+    }
+    bool operator==(const LogLevel &other) {
+      return lvl_ == other.lvl_;
+    }
+    bool operator!=(const LogLevel &other) {
+      return !(*this == other);
+    }
+    bool operator<(const LogLevel &other) {
+      return lvl_ < other.lvl_;
+    }
+  private:
+    Level lvl_;
+  };
+
+  class Logger {
+    public:
+      typedef std::size_t verbosity_type;
+
+      void setLevel(const LogLevel &level);
+      bool isLevelEnabled(const LogLevel &level);
+
+      void log(const LogEvent &event);
+
+      void addAppender(Appender::ptr_t appender);
+      Appender::ptr_t getAppender(const std::string &appender_name);
+      void removeAppender(const std::string &appender_name);
+    private:
+      Logger(const std::string &name);
+      LogLevel lvl_;
+      verbosity_type verbosity_;
+  };
+}}
 
 #endif
