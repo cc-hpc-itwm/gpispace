b99525e72b2aeba991b84dda0f2b768038ec7607
SVN_andreas_hohe <SVN_andreas_hohe@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-07-30 11:41:31 +0000

    Included cmake search for libxslt library
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@309 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/gwes/trunk/modules/FindLibXslt.cmake
@@ -0,0 +1,49 @@
+# -*- mode: cmake; -*-
+# Locate libxslt include paths and libraries
+# libxslt can be found at http://xmlsoft.org/XSLT/ 
+
+# This module defines
+# LIBXSLT_INCLUDE_DIR, where to find header files, etc.
+# LIBXSLT_LIBRARIES, the libraries to link against to use libxslt.
+# LIBXSLT_FOUND, If false, don't try to use libxslt.
+
+IF (NOT LIBXSLT_FIND_QUIETLY)
+  MESSAGE(STATUS "Looking for libxslt...")
+ENDIF (NOT LIBXSLT_FIND_QUIETLY)
+
+SET( LIBXSLT_FOUND "NO" )
+
+FIND_PATH(LIBXSLT_INCLUDE_DIRS libxslt/xslt.h
+  "[HKEY_CURRENT_USER\\software\\libxslt\\src]"
+  "[HKEY_CURRENT_USER\\libxslt\\src]"
+  $ENV{LIBXSLTROOT}/src/
+  /usr/local/include
+  /usr/include
+)
+
+FIND_LIBRARY(LIBXSLT_LIBRARIES
+  NAMES 
+    xslt
+  PATHS
+    "[HKEY_CURRENT_USER\\software\\libxslt\\lib]"
+    "[HKEY_CURRENT_USER\\libxslt\\lib]"
+    $ENV{LIBXSLTROOT}/lib
+    /usr/local/lib
+    /usr/lib
+)
+
+# if the include a the library are found then we have it
+IF(LIBXSLT_INCLUDE_DIRS)
+  IF(LIBXSLT_LIBRARIES)
+    SET( LIBXSLT_FOUND "YES" )
+    IF (NOT LIBXSLT_FIND_QUIETLY)
+      MESSAGE(STATUS "Looking for libxslt... - found ${LIBXSLT_LIBRARIES}")
+    ENDIF (NOT LIBXSLT_FIND_QUIETLY)
+  ENDIF(LIBXSLT_LIBRARIES)
+ENDIF(LIBXSLT_INCLUDE_DIRS)
+
+MARK_AS_ADVANCED(
+  LIBXSLT_INCLUDE_DIRS
+  LIBXSLT_LIBRARIES
+  LIBXSLT_FOUND
+)
