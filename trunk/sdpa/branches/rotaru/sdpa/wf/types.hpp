c22a9202fac704845c1cfe3a411822744a68c051
rotaru <rotaru@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-09-01 11:30:00 +0000

    Switched to Andres Gwes interface
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@332 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/branches/rotaru/sdpa/wf/types.hpp
+++ trunk/sdpa/branches/rotaru/sdpa/wf/types.hpp
@@ -1,11 +1,17 @@
 #ifndef SDPA_WF_TYPES_HPP
 #define SDPA_WF_TYPES_HPP 1
+
+#include <list>
+using namespace std;
+
 namespace sdpa {
 	namespace wf {
 		typedef std::string workflow_t;
 		typedef std::string activity_t;
 		typedef std::string workflow_id_t;
 		typedef std::string activity_id_t;
+		typedef std::string parameter_t;
+		typedef std::list<parameter_t> parameter_list_t;
 	}
 }
 #endif
