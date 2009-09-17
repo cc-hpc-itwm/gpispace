985bb91a5b55ae5be1a00f4b61d758ca868224f3
rotaru <rotaru@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-09-17 15:13:02 +0000

    added DaemonFSM.cpp
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@447 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/sdpa/branches/rotaru/sdpa/daemon/daemonFSM/SMC/DaemonFSM.cpp
@@ -0,0 +1,36 @@
+#include "DaemonFSM.hpp"
+
+using namespace sdpa::fsm::smc;
+using namespace sdpa::events;
+
+void DaemonFSM::handleDaemonEvent(const seda::IEvent::Ptr& pEvent)
+{
+	if( StartUpEvent* ptr = dynamic_cast<StartUpEvent*>(pEvent.get()) )
+		GetContext().StartUp(*ptr);
+	else if( ConfigOkEvent* ptr = dynamic_cast<ConfigOkEvent*>(pEvent.get()) )
+		GetContext().ConfigOk(*ptr);
+	else if( ConfigNokEvent* ptr = dynamic_cast<ConfigNokEvent*>(pEvent.get()) )
+		GetContext().ConfigNok(*ptr);
+	else if( InterruptEvent* ptr = dynamic_cast<InterruptEvent*>(pEvent.get()) )
+		GetContext().Interrupt(*ptr);
+	else if( WorkerRegistrationEvent* ptr = dynamic_cast<WorkerRegistrationEvent*>(pEvent.get()) )
+		GetContext().RegisterWorker(*ptr);
+	else if( DeleteJobEvent* ptr = dynamic_cast<DeleteJobEvent*>(pEvent.get()) )
+		GetContext().DeleteJob(*ptr);
+	else if( SubmitJobEvent* ptr = dynamic_cast<SubmitJobEvent*>(pEvent.get()) )
+		GetContext().SubmitJob(*ptr);
+	else if( LifeSignEvent* ptr = dynamic_cast<LifeSignEvent*>(pEvent.get()) )
+		GetContext().LifeSign(*ptr);
+	else if( RequestJobEvent* ptr = dynamic_cast<RequestJobEvent*>(pEvent.get()) )
+		GetContext().RequestJob(*ptr);
+	else if( SubmitJobAckEvent* ptr = dynamic_cast<SubmitJobAckEvent*>(pEvent.get()) )
+		GetContext().SubmitJobAck(*ptr);
+	else if( ConfigRequestEvent* ptr = dynamic_cast<ConfigRequestEvent*>(pEvent.get()) )
+		GetContext().ConfigRequest(*ptr);
+	else if( JobFinishedEvent* ptr = dynamic_cast<JobFinishedEvent*>(pEvent.get()) )
+		GetContext().JobFinished(*ptr);
+	else if( JobFailedEvent* ptr = dynamic_cast<JobFailedEvent*>(pEvent.get()) )
+		GetContext().JobFailed(*ptr);
+	else if( CancelJobAckEvent* ptr = dynamic_cast<CancelJobAckEvent*>(pEvent.get()) )
+		GetContext().JobCanceled(*ptr);
+}
