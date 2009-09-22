30d0dd385999a89a765f5d44162ac5bfecbb9b15
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-09-22 16:01:27 +0000

    added ctest configuration
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@473 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/fhglog/trunk/CTestConfig.cmake
@@ -0,0 +1,4 @@
+ SET (CTEST_DROP_METHOD http)
+ SET (CTEST_DROP_SITE "localhost")
+ SET (CTEST_DROP_LOCATION "/cgi-bin/HTTPUploadDartFile.cgi")
+ SET (CTEST_TRIGGER_SITE "http://${DROP_SITE}/cgi-bin/Submit-CMake-TestingResults.pl")
