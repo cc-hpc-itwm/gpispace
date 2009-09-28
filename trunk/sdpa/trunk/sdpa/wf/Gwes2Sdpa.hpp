13423434c9821b53bdf8068855d056afd1112013
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-09-28 13:14:32 +0000

    renamed the interfaces between sdpa and gwes
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@497 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/sdpa/trunk/sdpa/wf/Gwes2Sdpa.hpp
@@ -0,0 +1,77 @@
+#ifndef GWES_2_SDPA_HPP
+#define GWES_2_SDPA_HPP 1
+
+#include <string>
+#include <sdpa/wf/types.hpp>
+#include <sdpa/daemon/exceptions.hpp>
+
+ //for the exceptions, should be changed -> put common exceptions into some shared folder
+
+namespace sdpa { namespace wf {
+
+/**
+ * Interface class for the communication from GWES to SDPA.
+ */
+class Gwes2Sdpa {
+
+public:
+
+	// typedefs have been moved to Types.h
+
+	/**
+	 * Virtual destructor because of virtual methods.
+	 */
+	// don't need it!
+	//virtual ~Gwes2Sdpa();
+
+	/**
+	 * Submit a sub workflow to the SDPA.
+	 * This method is to be called by the GWES in order to delegate
+	 * the execution of sub workflows.
+	 * The SDPA will use the callback handler Sdpa2Gwes in order
+	 * to notify the GWES about status transitions.
+	 */
+	virtual workflow_id_t submitWorkflow(const workflow_t &workflow) = 0;
+
+	/**
+	 * Submit an atomic activity to the SDPA.
+	 * This method is to be called by the GWES in order to delegate
+	 * the execution of activities.
+	 * The SDPA will use the callback handler Sdpa2Gwes in order
+	 * to notify the GWES about activity status transitions.
+	 */
+	virtual activity_id_t submitActivity(const activity_t &activity) = 0;
+
+	/**
+	 * Cancel a sub workflow that has previously been submitted to
+	 * the SDPA. The parent job has to cancel all children.
+	 */
+	virtual void cancelWorkflow(const workflow_id_t &workflowId) throw (sdpa::daemon::NoSuchWorkflowException) = 0;
+
+	/**
+	 * Cancel an atomic activity that has previously been submitted to
+	 * the SDPA.
+	 */
+	virtual void cancelActivity(const activity_id_t &activityId)  throw (sdpa::daemon::NoSuchActivityException) = 0;
+
+	/**
+	 * Notify the SDPA that a workflow finished (state transition
+	 * from running to finished).
+	 */
+	virtual void workflowFinished(const workflow_id_t &workflowId) throw (sdpa::daemon::NoSuchWorkflowException) = 0;
+
+	/**
+	 * Notify the SDPA that a workflow failed (state transition
+	 * from running to failed).
+	 */
+	virtual void workflowFailed(const workflow_id_t &workflowId) throw (sdpa::daemon::NoSuchWorkflowException) = 0;
+
+	/**
+	 * Notify the SDPA that a workflow has been canceled (state
+	 * transition from * to terminated.
+	 */
+	virtual void workflowCanceled(const workflow_id_t &workflowId) throw (sdpa::daemon::NoSuchWorkflowException) = 0;
+};
+
+}}
+#endif
---
+++ trunk/sdpa/trunk/sdpa/wf/Sdpa2Gwes.hpp
@@ -0,0 +1,91 @@
+#ifndef SDPA_2_GWES_HPP
+#define SDPA_2_GWES_HPP 1
+
+#include <string>
+#include <sdpa/wf/types.hpp>
+#include <sdpa/daemon/exceptions.hpp>
+#include <sdpa/common.hpp>
+
+namespace sdpa { namespace wf {
+
+class Gwes2Sdpa;
+
+/**
+ * Interface class for the communication from SDPA to GWES.
+ */
+class Sdpa2Gwes {
+
+public:
+
+	typedef sdpa::shared_ptr<Sdpa2Gwes> ptr_t;
+	/**
+	 * Virtual destructor because of virtual methods.
+	 */
+	//don't need it!
+	//virtual ~Sdpa2Gwes();
+
+	/**
+	 * Notify the GWES that an activity has been dispatched
+	 * (state transition from "pending" to "running").
+	 * This method is to be invoked by the SDPA.
+	 */
+	virtual void activityDispatched(const workflow_id_t &workflowId,
+			const activity_id_t &activityId) throw (sdpa::daemon::NoSuchWorkflowException, sdpa::daemon::NoSuchActivityException) = 0;
+
+	/**
+	 * Notify the GWES that an activity has failed
+	 * (state transition from "running" to "failed").
+	 * This method is to be invoked by the SDPA.
+	 */
+	virtual void activityFailed(const workflow_id_t &workflowId,
+			const activity_id_t &activityId,
+			const parameter_list_t &output) throw (sdpa::daemon::NoSuchWorkflowException, sdpa::daemon::NoSuchActivityException) = 0;
+
+	/**
+	 * Notify the GWES that an activity has finished
+	 * (state transition from running to finished).
+	 * This method is to be invoked by the SDPA.
+	 */
+	virtual void activityFinished(const workflow_id_t &workflowId,
+			const activity_id_t &activityId,
+			const parameter_list_t &output) throw (sdpa::daemon::NoSuchWorkflowException, sdpa::daemon::NoSuchActivityException) = 0;
+
+	/**
+	 * Notify the GWES that an activity has been canceled
+	 * (state transition from * to terminated).
+	 * This method is to be invoked by the SDPA.
+	 */
+	virtual void activityCanceled(const workflow_id_t &workflowId,
+			const activity_id_t &activityId) throw (sdpa::daemon::NoSuchWorkflowException, sdpa::daemon::NoSuchActivityException) = 0;
+
+	/**
+	 * Register a SDPA handler that implements the Gwes2Sdpa
+	 * interface. This handler is notified on each status
+	 * transitions of each workflow. This handler is also used
+	 * by the GWES to delegate the execution of activities or
+	 * sub workflows to the SDPA.
+	 * Currently you can only register ONE handler for a GWES.
+	 */
+	virtual void registerHandler(Gwes2Sdpa *sdpa) const = 0;
+
+	/**
+	 * Submit a workflow to the GWES.
+	 * This method is to be invoked by the SDPA.
+	 * The GWES will initiate and start the workflow
+	 * asynchronously and notifiy the SPDA about status transitions
+	 * using the callback methods of the Gwes2Sdpa handler.
+	 */
+	virtual workflow_id_t submitWorkflow(workflow_t &workflow) = 0; //throw (gwdl::WorkflowFormatException) = 0;
+
+	/**
+	 * Cancel a workflow asynchronously.
+	 * This method is to be invoked by the SDPA.
+	 * The GWES will notify the SPDA about the
+	 * completion of the canceling process by calling the
+	 * callback method Gwes2Sdpa::workflowCanceled.
+	 */
+	virtual void cancelWorkflow(const workflow_id_t &workflowId) throw (sdpa::daemon::NoSuchWorkflowException) = 0;
+};
+
+}}
+#endif
