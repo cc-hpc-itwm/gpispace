7b795e99a6872e392023e922393df8791a52ebb5
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-09-28 14:32:54 +0000

    removed old cout comments and the iostream include
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@504 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/sdpa/events/RequestJobEvent.hpp
+++ trunk/sdpa/trunk/sdpa/events/RequestJobEvent.hpp
@@ -1,7 +1,6 @@
 #ifndef SDPA_REQUESTJOBEVENT_HPP
 #define SDPA_REQUESTJOBEVENT_HPP 1
 
-#include <iostream>
 #include <boost/statechart/event.hpp>
 #include <sdpa/events/MgmtEvent.hpp>
 namespace sc = boost::statechart;
@@ -13,11 +12,9 @@ namespace events {
         typedef sdpa::shared_ptr<RequestJobEvent> Ptr;
 
         RequestJobEvent(const address_t& from, const address_t& to) : MgmtEvent(from, to) {
-			//std::cout << "Create event 'RequestJobEvent'"<< std::endl;
         }
 
     	virtual ~RequestJobEvent() {
-    		//std::cout << "Delete event 'RequestJobEvent'"<< std::endl;
     	}
 
     	const sdpa::job_id_t & last_job_id() const { return last_job_id_; }
