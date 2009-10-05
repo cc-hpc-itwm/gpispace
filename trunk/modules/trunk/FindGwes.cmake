e5e3b2ee54c7c1be4d204f04b932ed2e74003d7a
krueger <krueger@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-10-05 14:16:24 +0000

    FindGwes work both with -DGWES_HOME env ENV-var GWES_HOME
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@596 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/modules/trunk/FindGwes.cmake
+++ trunk/modules/trunk/FindGwes.cmake
@@ -1,3 +1,4 @@
+# -*- mode: cmake; -*-
 # locates the Gwes Workflow Engine
 # This file defines:
 # * GWES_FOUND if gwes was was found
@@ -8,23 +9,66 @@
 # * GWDL_LIBRARY The lib to link to (currently only a static unix lib) 
 # * GWDL_INCLUDE_DIR
 
-if(GWES_HOME MATCHES "")
-  if("" MATCHES "$ENV{GWES_HOME}")
-    message(STATUS "GWES_HOME env is not set, setting it to /usr/local")
-    set (GWES_HOME "/usr/local")
-  else("" MATCHES "$ENV{GWES_HOME}")
-    set (GWES_HOME "$ENV{GWES_HOME}")
-  endif("" MATCHES "$ENV{GWES_HOME}")
-else(GWES_HOME MATCHES "")
-  message(STATUS "GWES_HOME is not empty: \"${GWES_HOME}\"")
-  set (GWES_HOME "${GWES_HOME}")
-endif(GWES_HOME MATCHES "")
-
-FIND_PATH(GWES_INCLUDE_DIR gwes/GWES.h
-  ${GWES_HOME}/include
+# set defaults
+SET(_gwes_HOME "/usr/local")
+SET(_gwes_INCLUDE_SEARCH_DIRS
   ${CMAKE_INCLUDE_PATH}
   /usr/local/include
   /usr/include
+  )
+
+SET(_gwes_LIBRARIES_SEARCH_DIRS
+  ${CMAKE_LIBRARY_PATH}
+  /usr/local/lib
+  /usr/lib
+  )
+
+SET(_gwes_BINARY_SEARCH_DIRS
+  ${CMAKE_BINARY_PATH}
+  /usr/local/lib
+  /usr/lib
+  )
+
+##
+if( "${GWES_HOME}" STREQUAL "")
+  if("" MATCHES "$ENV{GWES_HOME}")
+    message(STATUS "GWES_HOME env is not set, setting it to /usr/local")
+    set (GWES_HOME ${_gwes_HOME})
+  else("" MATCHES "$ENV{GWES_HOME}")
+    set (GWES_HOME "$ENV{GWES_HOME}")
+  endif("" MATCHES "$ENV{GWES_HOME}")
+else( "${GWES_HOME}" STREQUAL "")
+  message(STATUS "GWES_HOME is not empty: \"${GWES_HOME}\"")
+endif( "${GWES_HOME}" STREQUAL "")
+##
+
+message(STATUS "Looking for GWES in ${GWES_HOME}")
+
+# fix searchpath now
+IF( NOT ${GWES_HOME} STREQUAL "" )
+    SET(_gwes_INCLUDE_SEARCH_DIRS ${GWES_HOME}/include ${_gwes_INCLUDE_SEARCH_DIRS})
+    SET(_gwes_LIBRARIES_SEARCH_DIRS ${GWES_HOME}/lib ${_gwes_LIBRARIES_SEARCH_DIRS})
+    SET(_gwes_HOME ${GWES_HOME})
+ENDIF( NOT ${GWES_HOME} STREQUAL "" )
+
+IF( NOT $ENV{GWES_INCLUDEDIR} STREQUAL "" )
+  SET(_gwes_INCLUDE_SEARCH_DIRS $ENV{GWES_INCLUDEDIR} ${_gwes_INCLUDE_SEARCH_DIRS})
+ENDIF( NOT $ENV{GWES_INCLUDEDIR} STREQUAL "" )
+
+IF( NOT $ENV{GWES_LIBRARYDIR} STREQUAL "" )
+  SET(_gwes_LIBRARIES_SEARCH_DIRS $ENV{GWES_LIBRARYDIR} ${_gwes_LIBRARIES_SEARCH_DIRS})
+ENDIF( NOT $ENV{GWES_LIBRARYDIR} STREQUAL "" )
+
+IF( GWES_HOME )
+  SET(_gwes_INCLUDE_SEARCH_DIRS ${GWES_HOME}/include ${_gwes_INCLUDE_SEARCH_DIRS})
+  SET(_gwes_LIBRARIES_SEARCH_DIRS ${GWES_HOME}/lib ${_gwes_LIBRARIES_SEARCH_DIRS})
+  SET(_gwes_HOME ${GWES_HOME})
+ENDIF( GWES_HOME )
+
+# find the include files
+FIND_PATH(GWES_INCLUDE_DIR
+  NAMES gwes/GWES.h
+  HINTS ${_gwes_INCLUDE_SEARCH_DIRS}
 )
 
 IF(WIN32)
@@ -35,7 +79,7 @@ ENDIF(WIN32)
 
 FIND_LIBRARY(GWES_LIBRARY
   NAMES ${GWES_LIBRARY_NAMES}
-  PATHS ${GWES_HOME}/lib ${CMAKE_LIBRARY_PATH} /usr/lib /usr/local/lib
+  HINTS ${_gwes_LIBRARIES_SEARCH_DIRS}
   )
 
 # if the include and the program are found then we have it
@@ -47,6 +91,7 @@ else(GWES_INCLUDE_DIR AND GWES_LIBRARY)
 ENDIF(GWES_INCLUDE_DIR AND GWES_LIBRARY)
 
 MARK_AS_ADVANCED(
+  GWES_FOUND
   GWES_HOME
   GWES_LIBRARY
   GWES_INCLUDE_DIR
@@ -58,23 +103,66 @@ MARK_AS_ADVANCED(
 ##
 ##
 
-if(GWDL_HOME MATCHES "")
-  if("" MATCHES "$ENV{GWDL_HOME}")
-    message(STATUS "GWDL_HOME env is not set, setting it to /usr/local")
-    set (GWDL_HOME "/usr/local")
-  else("" MATCHES "$ENV{GWDL_HOME}")
-    set (GWDL_HOME "$ENV{GWDL_HOME}")
-  endif("" MATCHES "$ENV{GWDL_HOME}")
-else(GWDL_HOME MATCHES "")
-  message(STATUS "GWDL_HOME is not empty: \"${GWDL_HOME}\"")
-  set (GWDL_HOME "${GWDL_HOME}")
-endif(GWDL_HOME MATCHES "")
-
-FIND_PATH(GWDL_INCLUDE_DIR gwdl/Workflow.h
-  ${GWDL_HOME}/include
+# set defaults
+SET(_gwdl_HOME "/usr/local")
+SET(_gwdl_INCLUDE_SEARCH_DIRS
   ${CMAKE_INCLUDE_PATH}
   /usr/local/include
   /usr/include
+  )
+
+SET(_gwdl_LIBRARIES_SEARCH_DIRS
+  ${CMAKE_LIBRARY_PATH}
+  /usr/local/lib
+  /usr/lib
+  )
+
+SET(_gwdl_BINARY_SEARCH_DIRS
+  ${CMAKE_BINARY_PATH}
+  /usr/local/lib
+  /usr/lib
+  )
+
+##
+if( "${GWDL_HOME}" STREQUAL "")
+  if("" MATCHES "$ENV{GWDL_HOME}")
+    message(STATUS "GWDL_HOME env is not set, setting it to /usr/local")
+    set (GWDL_HOME ${_gwdl_HOME})
+  else("" MATCHES "$ENV{GWDL_HOME}")
+    set (GWDL_HOME "$ENV{GWDL_HOME}")
+  endif("" MATCHES "$ENV{GWDL_HOME}")
+else( "${GWDL_HOME}" STREQUAL "")
+  message(STATUS "GWDL_HOME is not empty: \"${GWDL_HOME}\"")
+endif( "${GWDL_HOME}" STREQUAL "")
+##
+
+message(STATUS "Looking for GWDL in ${GWDL_HOME}")
+
+# fix searchpath now
+IF( NOT ${GWDL_HOME} STREQUAL "" )
+    SET(_gwdl_INCLUDE_SEARCH_DIRS ${GWDL_HOME}/include ${_gwdl_INCLUDE_SEARCH_DIRS})
+    SET(_gwdl_LIBRARIES_SEARCH_DIRS ${GWDL_HOME}/lib ${_gwdl_LIBRARIES_SEARCH_DIRS})
+    SET(_gwdl_HOME ${GWDL_HOME})
+ENDIF( NOT ${GWDL_HOME} STREQUAL "" )
+
+IF( NOT $ENV{GWDL_INCLUDEDIR} STREQUAL "" )
+  SET(_gwdl_INCLUDE_SEARCH_DIRS $ENV{GWDL_INCLUDEDIR} ${_gwdl_INCLUDE_SEARCH_DIRS})
+ENDIF( NOT $ENV{GWDL_INCLUDEDIR} STREQUAL "" )
+
+IF( NOT $ENV{GWDL_LIBRARYDIR} STREQUAL "" )
+  SET(_gwdl_LIBRARIES_SEARCH_DIRS $ENV{GWDL_LIBRARYDIR} ${_gwdl_LIBRARIES_SEARCH_DIRS})
+ENDIF( NOT $ENV{GWDL_LIBRARYDIR} STREQUAL "" )
+
+IF( GWDL_HOME )
+  SET(_gwdl_INCLUDE_SEARCH_DIRS ${GWDL_HOME}/include ${_gwdl_INCLUDE_SEARCH_DIRS})
+  SET(_gwdl_LIBRARIES_SEARCH_DIRS ${GWDL_HOME}/lib ${_gwdl_LIBRARIES_SEARCH_DIRS})
+  SET(_gwdl_HOME ${GWDL_HOME})
+ENDIF( GWDL_HOME )
+
+# find the include files
+FIND_PATH(GWDL_INCLUDE_DIR
+  NAMES gwdl/Workflow.h
+  HINTS ${_gwdl_INCLUDE_SEARCH_DIRS}
 )
 
 IF(WIN32)
@@ -85,7 +173,7 @@ ENDIF(WIN32)
 
 FIND_LIBRARY(GWDL_LIBRARY
   NAMES ${GWDL_LIBRARY_NAMES}
-  PATHS ${GWDL_HOME}/lib ${CMAKE_LIBRARY_PATH} /usr/lib /usr/local/lib
+  HINTS ${_gwdl_LIBRARIES_SEARCH_DIRS}
   )
 
 # if the include and the program are found then we have it
@@ -97,6 +185,7 @@ else(GWDL_INCLUDE_DIR AND GWDL_LIBRARY)
 ENDIF(GWDL_INCLUDE_DIR AND GWDL_LIBRARY)
 
 MARK_AS_ADVANCED(
+  GWDL_FOUND
   GWDL_HOME
   GWDL_LIBRARY
   GWDL_INCLUDE_DIR
