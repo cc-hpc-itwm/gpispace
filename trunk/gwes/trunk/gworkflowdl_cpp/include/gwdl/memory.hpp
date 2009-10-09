753ac1aea434f7b7cd0abbecca8cbe42d1d07744
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-10-09 09:58:09 +0000

    added an include file for easy selection of shared_ptr types
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@689 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/gwes/trunk/gworkflowdl_cpp/include/gwdl/memory.hpp
@@ -0,0 +1,21 @@
+#ifndef GWDL_MEMORY_HPP
+#define GWDL_MEMORY_HPP 1
+
+#if HAVE_CONFIG_H
+#include <gwdl/gwdl-config.hpp>
+#endif
+
+#define USE_STL_TR1 1
+
+#if USE_STL_TR1 == 1
+#include <tr1/memory>
+#else
+#include <boost/tr1/memory.hpp>
+#endif
+
+namespace gwdl
+{
+    using std::tr1::shared_ptr;
+}
+
+#endif
