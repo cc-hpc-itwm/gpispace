afb751e27fa68010159b5bdc18267eb129b43742
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-10-15 15:55:45 +0000

    added throw() clauses to the function definitions
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@817 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/tests/sdpa/ExampleModule.cpp
+++ trunk/sdpa/trunk/tests/sdpa/ExampleModule.cpp
@@ -1,5 +1,6 @@
 #include <sdpa/modules/Module.hpp>
 #include <string>
+#include <cstdlib> // malloc, free
 
 using namespace sdpa::modules;
 
@@ -23,11 +24,34 @@ void Add(sdpa::modules::Module::data_t &params)
   params["out"].token().data(a+b);
 }
 
+void Malloc(sdpa::modules::Module::data_t &p) throw (std::exception)
+{
+  const std::size_t bytes = p["size"].token().data_as<std::size_t>();
+  void *ptr = std::malloc(bytes);
+  if (0 == ptr) {
+    throw std::runtime_error("memory allocation failed");
+  }
+  p["out"].token().data(ptr);
+}
+
+void Free(sdpa::modules::Module::data_t &p) throw (std::exception)
+{
+  void *ptr = p["ptr"].token().data_as<void*>();
+  if (ptr) {
+    free(ptr);
+    p["ptr"].token().data((void*)0);
+  } else {
+    throw std::runtime_error("double free detected");
+  }
+}
+
 extern "C" {
   void sdpa_mod_init(Module *mod) {
     SDPA_REGISTER_FUN(mod, HelloWorld);
     SDPA_REGISTER_FUN(mod, DoNothing);
     SDPA_REGISTER_FUN(mod, Add);
+    SDPA_REGISTER_FUN(mod, Malloc);
+    SDPA_REGISTER_FUN(mod, Free);
   }
 }
 
