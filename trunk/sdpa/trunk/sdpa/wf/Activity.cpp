5a0bbcd067e49f8273417c1485a78d8a5f8d312f
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-10-15 08:02:46 +0000

    starting wrapping gwes::activity
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@800 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/sdpa/wf/Activity.cpp
+++ trunk/sdpa/trunk/sdpa/wf/Activity.cpp
@@ -3,11 +3,47 @@
 using namespace sdpa::wf;
 
 Activity::Activity(const std::string &a_name, const Method &a_method, const parameter_list_t & params)
-  : name_(a_name)
+  : Properties()
+  , name_(a_name)
   , method_(a_method)
   , params_(params)
 {
 }
+
+Activity::Activity(const gwes::Activity &gwes_activity)
+  : Properties()
+  , name_(gwes_activity.getID())
+  , method_("", "")
+  , params_()
+{
+  // FIXME: getters on gwes::Activity should be const
+  gwes::Activity &act = const_cast<gwes::Activity&>(gwes_activity);
+  if (act.getOperationCandidate())
+  {
+    method_ = Method(act.getOperationCandidate()->getOperationName());
+    if (method_.module().empty() || method_.name().empty())
+    {
+      throw std::runtime_error("could not parse operation name into Method description: " + act.getOperationCandidate()->getOperationName());
+    }
+  }
+
+/*
+  // build parameter list
+  for (gwes::parameter_list_t::iterator it=parameters->begin(); it!=parameters->end(); ++it) {
+    switch (it->scope) {
+      case (TokenParameter::SCOPE_READ):
+      case (TokenParameter::SCOPE_INPUT):
+      case (TokenParameter::SCOPE_WRITE):
+        continue;
+      case (TokenParameter::SCOPE_OUTPUT):
+        it->tokenP = new Token(new Data(string("<data><output xmlns=\"\">15</output></data>")));
+        LOG_INFO(logger, "generated dummy output token: \n" << *it->tokenP);
+        break;
+    }
+  }
+*/
+}
+
 
 Activity::Activity(const Activity &other)
   : name_(other.name())
@@ -53,5 +89,3 @@ std::ostream & operator<<(std::ostream & os, const sdpa::wf::Activity::Method &m
   m.writeTo(os);
   return os;
 }
-
-
