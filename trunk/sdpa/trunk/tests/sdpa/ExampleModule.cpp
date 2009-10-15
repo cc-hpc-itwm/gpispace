705ed1b4ef5ebb6e779b32394a698c366ab9df66
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-10-15 14:05:39 +0000

    added a simple Add function to the example module that adds two long longs and stores the sum
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@810 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/tests/sdpa/ExampleModule.cpp
+++ trunk/sdpa/trunk/tests/sdpa/ExampleModule.cpp
@@ -11,11 +11,23 @@ void HelloWorld(sdpa::modules::Module::data_t &params)
 
 void DoNothing(sdpa::modules::Module::data_t &)
 {
+
+}
+
+// add two integer parameters and store the result in "out"
+void Add(sdpa::modules::Module::data_t &params)
+{
+  const long long a = params["a"].token().data_as<long long>();
+  const long long b = params["b"].token().data_as<long long>();
+
+  params["out"].token().data(a+b);
 }
 
 extern "C" {
   void sdpa_mod_init(Module *mod) {
     SDPA_REGISTER_FUN(mod, HelloWorld);
     SDPA_REGISTER_FUN(mod, DoNothing);
+    SDPA_REGISTER_FUN(mod, Add);
   }
 }
+
