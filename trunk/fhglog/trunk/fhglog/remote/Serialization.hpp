b59fbe431ffc3672f824f3be4d1b8660040a4ac9
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-10-19 11:38:44 +0000

    started serialization functions
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@863 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/fhglog/trunk/fhglog/remote/Serialization.hpp
@@ -0,0 +1,44 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Serialization.hpp
+ *
+ *    Description:  serialization functions for logevents
+ *
+ *        Version:  1.0
+ *        Created:  10/19/2009 01:17:25 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Alexander Petry (petry), alexander.petry@itwm.fraunhofer.de
+ *        Company:  Fraunhofer ITWM
+ *
+ * =====================================================================================
+ */
+
+#ifndef FHG_LOG_REMOTE_SERIALIZATION_HPP
+#define FHG_LOG_REMOTE_SERIALIZATION_HPP 1
+
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
+#include <boost/serialization/base_object.hpp>
+#include <boost/serialization/utility.hpp>
+#include <boost/serialization/list.hpp>
+#include <boost/serialization/map.hpp>
+#include <boost/serialization/assume_abstract.hpp>
+
+#include <fhglog/Properties.hpp>
+#include <sdpa/wf/Token.hpp>
+#include <sdpa/wf/Parameter.hpp>
+#include <sdpa/wf/Activity.hpp>
+
+namespace boost { namespace serialization {
+  template <class Archive>
+  void serialize(Archive & ar, fhg::log::LogLevel & level, const unsigned int /* version */)
+  {
+    ar & level.lvl();
+  }
+}}
+
+#endif
