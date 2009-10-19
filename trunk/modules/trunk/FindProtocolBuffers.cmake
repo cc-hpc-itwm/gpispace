521a5c3232b6779dba48dd7d2066b418bcf5e36f
krueger <krueger@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-10-19 09:09:36 +0000

    Fix: include <package>Config.cmake only once.
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@859 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/modules/trunk/FindProtocolBuffers.cmake
+++ trunk/modules/trunk/FindProtocolBuffers.cmake
@@ -8,100 +8,31 @@
 # * PB_PROTOC_CMD the protoc executable
 
 cmake_minimum_required(VERSION "2.6.2" FATAL_ERROR)
+message(STATUS "FindProtocolBuffers check")
 
-# set defaults
-SET(_pb_HOME "/usr/local")
-SET(_pb_INCLUDE_SEARCH_DIRS
-  ${CMAKE_INCLUDE_PATH}
-  /usr/local/include
-  /usr/include
-  )
+include(FindPackageHelper)
+check_package_search_path(PB google/protobuf/message.h protobuf 2.0.3
+  PB_INCLUDE_SEARCH_DIRS
+  PB_LIBRARIES_SEARCH_DIRS
+  PB_BINARY_SEARCH_DIRS
+  PB_HOME)
 
-SET(_pb_LIBRARIES_SEARCH_DIRS
-  ${CMAKE_LIBRARY_PATH}
-  /usr/local/lib
-  /usr/lib
-  )
-
-SET(_pb_BINARY_SEARCH_DIRS
-  ${CMAKE_BINARY_PATH}
-  /usr/local/lib
-  /usr/lib
-  )
-
-##
-if( "${PB_HOME}" STREQUAL "")
-  if("" MATCHES "$ENV{PB_HOME}")
-    message(STATUS "PB_HOME env is not set, setting it to /usr/local")
-    set (PB_HOME ${_pb_HOME})
-  else("" MATCHES "$ENV{PB_HOME}")
-    set (PB_HOME "$ENV{PB_HOME}")
-  endif("" MATCHES "$ENV{PB_HOME}")
-else( "${PB_HOME}" STREQUAL "")
-  message(STATUS "PB_HOME is not empty: \"${PB_HOME}\"")
-endif( "${PB_HOME}" STREQUAL "")
-##
-
-message(STATUS "Looking for protobuf in ${PB_HOME}")
-
-IF( NOT ${PB_HOME} STREQUAL "" )
-    SET(_pb_INCLUDE_SEARCH_DIRS ${PB_HOME}/include ${_pb_INCLUDE_SEARCH_DIRS})
-    SET(_pb_LIBRARIES_SEARCH_DIRS ${PB_HOME}/lib ${_pb_LIBRARIES_SEARCH_DIRS})
-    SET(_pb_BINARY_SEARCH_DIRS ${PB_HOME}/bin ${_pb_BINARY_SEARCH_DIRS})
-    SET(_pb_HOME ${PB_HOME})
-  ENDIF( NOT ${PB_HOME} STREQUAL "" )
-
-  IF( NOT $ENV{PB_INCLUDEDIR} STREQUAL "" )
-    SET(_pb_INCLUDE_SEARCH_DIRS $ENV{PB_INCLUDEDIR} ${_pb_INCLUDE_SEARCH_DIRS})
-  ENDIF( NOT $ENV{PB_INCLUDEDIR} STREQUAL "" )
-
-  IF( NOT $ENV{PB_LIBRARYDIR} STREQUAL "" )
-    SET(_pb_LIBRARIES_SEARCH_DIRS $ENV{PB_LIBRARYDIR} ${_pb_LIBRARIES_SEARCH_DIRS})
-  ENDIF( NOT $ENV{PB_LIBRARYDIR} STREQUAL "" )
-
-IF( NOT $ENV{PB_BINARYDIR} STREQUAL "" )
-  SET(_pb_BINARY_SEARCH_DIRS $ENV{PB_BINARYDIR} ${_pb_BINARY_SEARCH_DIRS})
-ENDIF( NOT $ENV{PB_BINARYDIR} STREQUAL "" )
-
-IF( PB_HOME )
-  SET(_pb_INCLUDE_SEARCH_DIRS ${PB_HOME}/include ${_pb_INCLUDE_SEARCH_DIRS})
-  SET(_pb_LIBRARIES_SEARCH_DIRS ${PB_HOME}/lib ${_pb_LIBRARIES_SEARCH_DIRS})
-  SET(_pb_BINARY_SEARCH_DIRS ${PB_HOME}/lib ${_pb_BINARY_SEARCH_DIRS})
-  SET(_pb_HOME ${PB_HOME})
-ENDIF( PB_HOME )
-
-# find the include files
-FIND_PATH(PB_INCLUDE_DIR
-  NAMES  google/protobuf/message.h
-  HINTS  ${_pb_INCLUDE_SEARCH_DIRS}
-)
-
+check_package(PB google/protobuf/message.h protobuf 2.0.3)
 
 IF(WIN32)
   FIND_FILE(PB_PROTOC_CMD
     NAMES    protoc.exe
-    HINTS    ${_pb_BINARY_SEARCH_DIRS}
+    HINTS    ${PB_BINARY_SEARCH_DIRS}
     "[HKEY_CURRENT_USER\\protobuf\\bin]"
     DOC "Location of the protoc file"
     )
 ELSE(WIN32)
   FIND_FILE(PB_PROTOC_CMD
     NAMES    protoc
-    HINTS    ${_pb_BINARY_SEARCH_DIRS}
+    HINTS    ${PB_BINARY_SEARCH_DIRS}
     DOC "Location of the protoc file"
     )
 ENDIF(WIN32)
-
-IF(WIN32)
-  SET(PB_LIBRARY_NAMES ${PB_LIBRARY_NAMES} libprotobuf.lib)
-ELSE(WIN32)
-  SET(PB_LIBRARY_NAMES ${PB_LIBRARY_NAMES} libprotobuf.a libprotobuf.so)
-ENDIF(WIN32)
-
-FIND_LIBRARY(PB_LIBRARY
-  NAMES ${PB_LIBRARY_NAMES}
-  HINTS ${_pb_LIBRARIES_SEARCH_DIRS}
-  )
 
 # if the include and the program are found then we have it
 IF(PB_PROTOC_CMD AND PB_LIBRARY) 
@@ -112,9 +43,12 @@ else(PB_PROTOC_CMD AND PB_LIBRARY)
 ENDIF(PB_PROTOC_CMD AND PB_LIBRARY)
 
 MARK_AS_ADVANCED(
-  PB_HOME
+#  PB_HOME
+#  PB_FOUND
   PB_PROTOC_CMD
-  PB_LIBRARY
-  PB_INCLUDE_DIR
+#  PB_LIBRARY
+#  PB_LIBRARIES
+#  PB_LIBRARY_DIR
+#  PB_INCLUDE_DIR
   )
 
