33d265f2ac6b62b7ce230c951ccb52f1ffdc264e
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-10-20 13:17:06 +0000

    tagged fhglog v1.1.0
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@894 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/fhglog/tags/v1.1.0/fhglog/fhglog.hpp
@@ -0,0 +1,14 @@
+#ifndef  FHGLOG_INC
+#define  FHGLOG_INC
+
+#include	<fhglog/LogMacros.hpp>
+#include	<fhglog/LoggerApi.hpp>
+#include	<fhglog/Appender.hpp>
+#include    <fhglog/Filter.hpp>
+#include    <fhglog/LogEvent.hpp>
+
+#include	<fhglog/StreamAppender.hpp>
+#include	<fhglog/SynchronizedAppender.hpp>
+#include	<fhglog/FileAppender.hpp>
+
+#endif   /* ----- #ifndef FHGLOG_INC  ----- */
