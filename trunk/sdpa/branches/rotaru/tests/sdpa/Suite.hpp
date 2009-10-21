8320afbbb68b89423aea1f859fff52ad486d645c
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-10-21 16:49:05 +0000

    fixed include error
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@948 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/branches/rotaru/tests/sdpa/Suite.hpp
+++ trunk/sdpa/branches/rotaru/tests/sdpa/Suite.hpp
@@ -13,7 +13,7 @@
 #include <tests/sdpa/test_JobFSM_SMC.hpp>
 #include <tests/sdpa/test_DaemonDummyGwes.hpp>
 #include <tests/sdpa/test_DaemonRealGwes.hpp>
-#include <tests/sdpa/test_D2DDummyGwes.hpp>
+//#include <tests/sdpa/test_D2DDummyGwes.hpp>
 #include <tests/sdpa/test_Scheduler.hpp>
 
 namespace sdpa { namespace tests {
