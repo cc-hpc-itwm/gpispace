a28f8cb3f0fb59ba8fefb92353407ec125eb7c0c
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-10-28 12:12:07 +0000

    added default constructor for delete-job-ack
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@1000 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/sdpa/events/DeleteJobAckEvent.hpp
+++ trunk/sdpa/trunk/sdpa/events/DeleteJobAckEvent.hpp
@@ -18,9 +18,15 @@ namespace sdpa { namespace events {
     public:
         typedef sdpa::shared_ptr<DeleteJobAckEvent> Ptr;
 
-        DeleteJobAckEvent(const address_t& a_from, const address_t& a_to, const sdpa::job_id_t& a_job_id = sdpa::job_id_t(""))
-              :  sdpa::events::JobEvent( a_from, a_to, a_job_id )
-        { }
+        DeleteJobAckEvent()
+         :  JobEvent( "", "", "" )
+        {}
+
+        DeleteJobAckEvent(const address_t& a_from
+                        , const address_t& a_to
+                        , const sdpa::job_id_t& a_job_id = sdpa::job_id_t(""))
+         :  sdpa::events::JobEvent( a_from, a_to, a_job_id )
+        {}
 
     	virtual ~DeleteJobAckEvent() { }
 
