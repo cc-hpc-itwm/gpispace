246160ae8299c0d7ceda35464088a59924a69ee5
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-10-28 12:12:05 +0000

    deletejob ack is a job-event, included it
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@999 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/sdpa/events/DeleteJobAckEvent.hpp
+++ trunk/sdpa/trunk/sdpa/events/DeleteJobAckEvent.hpp
@@ -1,14 +1,20 @@
 #ifndef SDPA_DeleteJobAckEvent_HPP
 #define SDPA_DeleteJobAckEvent_HPP 1
 
-#include <boost/statechart/event.hpp>
-#include <sdpa/events/MgmtEvent.hpp>
+#include <sdpa/sdpa-config.hpp>
 
+#ifdef USE_BOOST_SC
+#   include <boost/statechart/event.hpp>
 namespace sc = boost::statechart;
+#endif
+#include <sdpa/events/JobEvent.hpp>
 
-namespace sdpa {
-namespace events {
-    class DeleteJobAckEvent : public sdpa::events::JobEvent, public sc::event<sdpa::events::DeleteJobAckEvent> {
+namespace sdpa { namespace events {
+#ifdef USE_BOOST_SC
+    class DeleteJobAckEvent : public JobEvent, public sc::event<DeleteJobAckEvent> {
+#else
+    class DeleteJobAckEvent : public JobEvent {
+#endif
     public:
         typedef sdpa::shared_ptr<DeleteJobAckEvent> Ptr;
 
