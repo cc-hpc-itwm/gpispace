48b6f76bc5d6a7ef0d90eae0af647bc40af0528c
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-10-30 14:30:13 +0000

    hotfix in seda: decorator should call callbacks on decorated strategy
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@1024 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/seda/trunk/seda/StrategyDecorator.hpp
+++ trunk/seda/trunk/seda/StrategyDecorator.hpp
@@ -9,6 +9,15 @@ namespace seda {
     StrategyDecorator(const std::string& name, const seda::Strategy::Ptr& s);
     virtual ~StrategyDecorator();
     virtual void perform(const IEvent::Ptr&);
+
+    virtual void onStageStart(const std::string &s)
+    {
+      _component->onStageStart(s);
+    }
+    virtual void onStageStop(const std::string &s)
+    {
+      _component->onStageStop(s);
+    }
   private:
     seda::Strategy::Ptr _component;
   };
