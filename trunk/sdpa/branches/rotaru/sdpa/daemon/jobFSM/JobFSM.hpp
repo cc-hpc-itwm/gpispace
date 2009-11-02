cba9a6b96aee586ffd122c80321ebbc3021e3f9d
rotaru <rotaru@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-11-02 10:36:49 +0000

    added DaemonFSM.hpp and JobFSM.hpp
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@1035 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/sdpa/branches/rotaru/sdpa/daemon/jobFSM/JobFSM.hpp
@@ -0,0 +1,10 @@
+
+#ifndef JOBFSM_HPP_
+#define JOBFSM_HPP_
+#include <sdpa/daemon/jobFSM/SMC/JobFSM.hpp>
+#include <sdpa/daemon/jobFSM/BSC/JobFSM.hpp>
+
+namespace jsm = sdpa::fsm::smc;
+using namespace jsm;
+
+#endif /* JOBFSM_HPP_ */
