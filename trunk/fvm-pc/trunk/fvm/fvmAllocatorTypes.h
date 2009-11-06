ada7fa61103e7c0244ea9cad1dcabc444633e9d8
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-11-06 17:38:25 +0000

    added a fvm-pc subproject that fits a bit nicer into our current structure   * i took the basis of Rui's code and modified it so that it does not     depend on any external library
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@1085 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/fvm-pc/trunk/fvm/fvmAllocatorTypes.h
@@ -0,0 +1,45 @@
+#ifndef _FVM_ALLOCATOR_H_
+#define _FVM_ALLOCATOR_H_
+
+/* ************* Macros **************** */
+#define IN_MB(X) (X / 1024 /1024)
+#define ACK_OK 1
+#define ACK_NOK 0
+
+#define ALLOCATOR_PORT_REMOTE "12000" 
+#define BACKLOG 10
+
+/* ************* type definitions *************** */
+typedef struct fvmMemPointer
+{
+	void *fvmAddress;
+	unsigned long long offset;
+	unsigned int size;
+}fvmMemPointer_t;
+
+typedef enum {
+  QUERYGLOBALLOC,
+  QUERYGLOBALLOCACK,
+  ABORTGLOBALLOC,
+  ABORTGLOBALLOCACK,
+  COMMITGLOBALLOC,
+  COMMITGLOBALLOCACK,
+  COMMSTOPGLOBAL,
+  COMMSTOPGLOBALACK,
+  COMMITGLOBALLOCACKGLOBALLY,
+  LOCALLOC,
+  LOCALFREE,
+  GLOBALFREE,
+  GLOBALFREEACK
+} fvmAllocType_t;
+
+typedef unsigned long fvmAllocHandle_t;
+
+typedef struct {
+  fvmAllocType_t type;
+  unsigned long size;
+  int root;
+  fvmAllocHandle_t handle;
+} fvmAllocRequest_t;
+
+#endif
---
+++ trunk/fvm-pc/trunk/fvm/fvm_common.h
@@ -0,0 +1,101 @@
+
+#ifndef _FVM_C_H_
+#define _FVM_C_H_
+
+typedef enum {
+	STARTMSG = 1,
+	REQUESTMSG,
+	ALLOCMSG,
+	ACKMSG
+} fvmMsgType_t;
+
+/* #define STARTMSG 1 */
+/* #define REQUESTMSG 2 */
+/* #define ALLOCMSG 3 */
+/* #define ACKMSG 4 */
+
+typedef unsigned long fvmOffset_t;
+typedef unsigned long fvmShmemOffset_t;
+
+
+typedef enum {
+  START,
+  FGLOBALLOC,
+  FLOCALLOC,
+  FGLOBALFREE,
+  FLOCALFREE,
+  PUTGLOBAL,
+  GETGLOBAL,
+  PUTLOCAL,
+  GETLOCAL,
+  READDMA,
+  WRITEDMA,
+  WAITCOMM,
+  LEAVE
+} fvmOperation_t;
+
+/* handle for communication */
+typedef int fvmCommHandle_t;
+
+typedef enum 
+  {
+#ifndef NBOUNDCHECK
+    COMM_HANDLE_ERROR_SHMEM_BOUNDARY = -10,
+#endif
+
+#ifdef DEBUG
+    COMM_HANDLE_ERROR_INVALID_HANDLE,
+    COMM_HANDLE_ERROR_INVALID_SCRATCH_HANDLE,
+    COMM_HANDLE_ERROR_INVALID_SIZE,
+    COMM_HANDLE_ERROR_ARENA_UNKNOWN,
+#endif
+    /* error states */
+    COMM_HANDLE_ERROR_TOO_MANY,
+    COMM_HANDLE_ERROR_HANDLE_UNKNOWN,
+    COMM_HANDLE_ERROR_ACK_FAILED,
+    COMM_HANDLE_ERROR_SCRATCH_SIZE_TOO_SMALL,
+    COMM_HANDLE_ERROR_SIZE_NOT_MATCH, 
+    COMM_HANDLE_FREE, // 0
+    COMM_HANDLE_NOT_FINISHED,
+    COMM_HANDLE_OK
+
+  } fvmCommHandleState_t;
+
+/* TODO: maybe an different struct for the put/get to make the normal struct smaller? */
+typedef struct fvmRequestArgs
+{
+  fvmOffset_t arg_fvmOffset;
+  fvmShmemOffset_t arg_shmOffset;
+  unsigned int arg_size;
+  fvmCommHandle_t arg_handle;
+  fvmAllocHandle_t arg_allochandle;
+  fvmAllocHandle_t arg_scratchhandle;
+} fvmRequestArgs_t;
+
+typedef struct fvmRequest
+{
+  fvmOperation_t op;
+  fvmRequestArgs_t args;
+} fvmRequest_t;
+
+
+typedef struct msgQueueMsg {
+  long mtype; /* always needed by msgqueue */
+  fvmRequest_t request;
+} msgQueueMsg_t;
+
+
+typedef struct msgQueueAllocMsg {
+  long mtype; /* always needed by msgqueue */
+  fvmAllocHandle_t handle;
+} msgQueueAllocMsg_t;
+
+typedef struct msgq_ack {
+  long mtype;
+  int ret;
+} msgq_ack_t;
+
+
+
+
+#endif
