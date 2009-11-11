7843b9140358371b3b2bb9167c481a2aee0213a2
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-11-11 11:07:55 +0000

    added a very small process-container test that makes a global alloc/dealloc
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@1140 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/nre-worker/trunk/mods/pc-test.cpp
@@ -0,0 +1,21 @@
+#include <sdpa/modules/Module.hpp>
+#include <iostream>
+#include <string>
+#include <cstdlib> // malloc, free
+#include <fvm-pc/pc.hpp>
+
+using namespace sdpa::modules;
+
+static void RunTest() throw (std::exception)
+{
+  std::cout << "running RunTest example" << std::endl;
+  fvmAllocHandle_t src = fvmGlobalAlloc((256*1024*1024));
+  fvmGlobalFree(src);
+}
+
+SDPA_MOD_INIT_START(pc-test)
+{
+  // run test cases
+  RunTest();
+}
+SDPA_MOD_INIT_END(pc-test)
