8d5e5486811151946563beb4f47551582a004777
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-11-12 22:29:24 +0000

    removed default parameter for job-id
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@1206 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/sdpa/events/JobFailedAckEvent.hpp
+++ trunk/sdpa/trunk/sdpa/events/JobFailedAckEvent.hpp
@@ -23,7 +23,7 @@ namespace sdpa { namespace events {
           : JobEvent("", "", "")
         {}
 
-		JobFailedAckEvent(const address_t& a_from, const address_t& a_to, const sdpa::job_id_t& a_job_id = sdpa::job_id_t(""))
+		JobFailedAckEvent(const address_t& a_from, const address_t& a_to, const sdpa::job_id_t& a_job_id)
           :  sdpa::events::JobEvent( a_from, a_to, a_job_id ) {
 		}
 
