7c4b8e9cee48da2580ab431e9dc983f362af5ea1
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-11-12 00:40:20 +0000

    added a module that uses fhglog
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@1157 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/nre-worker/trunk/mods/log-test.cpp
@@ -0,0 +1,20 @@
+#include <sdpa/modules/Module.hpp>
+#include <iostream>
+#include <string>
+#include <cstdlib> // malloc, free
+
+#include <fhglog/fhglog.hpp>
+
+using namespace sdpa::modules;
+
+static void RunTest() throw (std::exception)
+{
+  MLOG(INFO, "performing RunTest()...");
+}
+
+SDPA_MOD_INIT_START(log-test)
+{
+  // run test cases
+  RunTest();
+}
+SDPA_MOD_INIT_END(log-test)
