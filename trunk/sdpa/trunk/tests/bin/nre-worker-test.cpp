6ed4314cbd240d9f9a74f6bc1e642873e7acfd15
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-11-12 13:17:44 +0000

    executing something
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@1181 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/tests/bin/nre-worker-test.cpp
+++ trunk/sdpa/trunk/tests/bin/nre-worker-test.cpp
@@ -18,19 +18,32 @@
 
 #include <fhglog/fhglog.hpp>
 #include <sdpa/daemon/nre/NreWorkerClient.hpp>
+#include <sdpa/wf/Activity.hpp>
 
 int main(int ac, char **av)
 {
   fhg::log::Configurator::configure();
 
   std::string worker_location("127.0.0.1:8000");
+  std::string function_call("init@init");
   if (ac > 1)
   {
     worker_location = av[1];
   }
+  if (ac > 2)
+  {
+    function_call = av[2];
+  }
 
   sdpa::nre::worker::NreWorkerClient client(worker_location);
   client.start();
+
+  // try to execute an activity
+  sdpa::wf::Activity a_in("activity-1", sdpa::wf::Method(function_call));
+  sdpa::wf::Activity a_ou(client.execute(a_in));
+  LOG(INFO, "got reply: " << a_ou);
+
+  client.stop();
   return 0;
 }
 
