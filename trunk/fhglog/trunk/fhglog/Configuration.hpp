d47a22b4884bb215faff9945b325bba325817a1b
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-11-14 06:11:39 +0000

    a bit more sophisticated logging configuration
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@1241 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/fhglog/trunk/fhglog/Configuration.hpp
+++ trunk/fhglog/trunk/fhglog/Configuration.hpp
@@ -6,12 +6,21 @@
 #endif
 
 #include <fhglog/fhglog.hpp>
+#include <fhglog/util.hpp> // environment parser
 
 #include <iostream>
 
 namespace fhg { namespace log {
   class DefaultConfiguration {
     public:
+      DefaultConfiguration()
+        : level_(LogLevel::DEF_LEVEL)
+        , to_console_("")
+        , to_file_("")
+        , to_server_("")
+        , fmt_string_("")
+      {}
+
       void operator() () throw() {
 #if FHGLOG_DISABLE_LOGGING != 1
 #ifndef NDEBUG
@@ -19,8 +28,14 @@ namespace fhg { namespace log {
 #endif
         try {
           getLogger().removeAllAppenders();
-          getLogger().addAppender(Appender::ptr_t(new StreamAppender("console")))->setFormat(Formatter::Default());
-          getLogger().setLevel(LogLevel::TRACE);
+
+          parse_environment();
+          if (! check_config())
+          {
+            fallback_configuration();
+          }
+
+          configure();
         } catch (const std::exception& ex) {
           std::clog << "E: Could not configure the logging environment: " << ex.what() << std::endl;
         } catch (...) {
@@ -29,6 +44,152 @@ namespace fhg { namespace log {
         std::clog.flush();
 #endif
       }
+
+    private:
+      /*
+       * This configuration takes the following environment variables into account (case sensitive):
+       *  FHGLOG_level                      // log everything with at least this level, defaults to TRACE
+       *  FHGLOG_format=log-format          // defaults to the default format in Formatter
+       *  FHGLOG_to_console={stdout,stderr} // log to stdout or stderr
+       *  FHGLOG_to_file=path to logfile    // log to specified file
+       *  FHGLOG_to_server=ip:port          // log to the specified server
+       */
+      void parse_environment()
+      {
+        environment_t env = get_environment_variables();
+
+        for (environment_t::const_iterator entry(env.begin()); entry != env.end(); ++entry)
+        {
+          std::string key(entry->first);
+          std::string val(entry->second);
+          parse_key_value(key, val);
+        }
+      }
+
+      bool check_config()
+      {
+        if (to_console_.empty()
+         && to_server_.empty()
+         && to_file_.empty()
+        )
+        {
+          return false;
+        }
+
+        return true;
+      }
+
+      void configure()
+      {
+        Formatter::ptr_t fmt(Formatter::Default());
+
+        if (fmt_string_.size())
+        {
+#ifndef NDEBUG
+          std::clog << "D: setting format to \"" << fmt_string_ << "\"" << std::endl;
+#endif
+          if      (fmt_string_ == "full")    fmt = Formatter::Full();
+          else if (fmt_string_ == "short")   fmt = Formatter::Short(); 
+          else if (fmt_string_ == "default") fmt = Formatter::Default(); 
+          else                               fmt = Formatter::Custom(fmt_string_);
+        }
+
+        if (to_console_.size())
+        {
+#ifndef NDEBUG
+          std::clog << "D: logging to console" << std::endl;
+#endif
+          getLogger().addAppender(Appender::ptr_t(new StreamAppender("console", std::cerr)))->setFormat(fmt);
+        }
+
+        if (to_file_.size())
+        {
+          try
+          {
+            getLogger().addAppender(Appender::ptr_t(new FileAppender("log-file", to_file_)))->setFormat(fmt);
+#ifndef NDEBUG
+            std::clog << "D: logging to file: " << to_file_ << std::endl;
+#endif
+          }
+          catch (const std::exception &ex)
+          {
+            std::clog << "E: could not open log-file " << to_file_ << ": " << ex.what() << std::endl;
+          }
+        }
+
+#if defined(FHGLOG_WITH_REMOTE_LOGGING)
+        if (to_server_.size())
+        {
+          try
+          {
+            // TODO: split to_remote_ into host and port
+            getLogger().addAppender(Appender::ptr_t(new remote::RemoteAppender("log-server", to_server_)));
+#ifndef NDEBUG
+            std::clog << "D: logging to server: " << to_server_ << std::endl;
+#endif
+          }
+          catch (const std::exception &ex)
+          {
+            std::clog << "E: could not create remote logger to: " << to_server_ << ": " << ex.what() << std::endl;
+          }
+        }
+#endif
+
+#ifndef NDEBUG
+        std::clog << "D: loglevel set to " << level_ << std::endl;
+#endif
+        getLogger().setLevel(level_);
+      }
+
+      void fallback_configuration()
+      {
+        level_ = LogLevel::DEF_LEVEL;
+        to_console_ = "stderr";
+        to_file_ = "";
+        to_server_ = "";
+      }
+
+      void parse_key_value(const std::string &key, const std::string &val)
+      {
+        if (key == "FHGLOG_level")
+        {
+          level_ = LogLevel(val);
+        }
+        else if (key == "FHGLOG_format")
+        {
+          fmt_string_ = val;
+        }
+        else if (key == "FHGLOG_to_console")
+        {
+          to_console_ = val;
+        }
+        else if (key == "FHGLOG_to_file")
+        {
+          to_file_ = val;
+        }
+        else if (key == "FHGLOG_to_server")
+        {
+          to_server_ = val;
+        }
+        else if (key.substr(0, 6) == "FHGLOG")
+        {
+#ifndef NDEBUG
+        std::clog << "D: ignoring key: " << key << std::endl;
+#endif
+        }
+        else
+        {
+          // completely ignore
+        }
+      }
+
+    private:
+      // internal config variables
+      fhg::log::LogLevel level_;
+      std::string to_console_;
+      std::string to_file_;
+      std::string to_server_;
+      std::string fmt_string_;
   };
 
   class Configurator {
