546c60bbc7b0ee10f0e16b51788e885fbae4c07b
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-11-14 06:10:01 +0000

    added config.hpp inclusion and depending on that remote logger
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@1226 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/fhglog/trunk/fhglog/fhglog.hpp
+++ trunk/fhglog/trunk/fhglog/fhglog.hpp
@@ -1,6 +1,10 @@
 #ifndef  FHGLOG_INC
 #define  FHGLOG_INC
 
+#if defined(HAVE_CONFIG_H)
+#  include <fhglog/fhglog-config.hpp>
+#endif
+
 #include	<fhglog/LogMacros.hpp>
 #include	<fhglog/LoggerApi.hpp>
 #include	<fhglog/Appender.hpp>
@@ -10,6 +14,10 @@
 #include	<fhglog/StreamAppender.hpp>
 #include	<fhglog/SynchronizedAppender.hpp>
 #include	<fhglog/FileAppender.hpp>
+
+#if FHGLOG_WITH_REMOTE_LOGGING
+#   include <fhglog/remote/RemoteAppender.hpp>
+#endif
 
 #include    <fhglog/Configuration.hpp>
 
