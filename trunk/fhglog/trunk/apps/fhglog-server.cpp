da80a034ef43e25e5c6ff67841872d1cf01a90a8
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-11-14 06:12:49 +0000

    removed start/stop but added fmt specification support
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@1251 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/fhglog/trunk/apps/fhglog-server.cpp
+++ trunk/fhglog/trunk/apps/fhglog-server.cpp
@@ -20,9 +20,9 @@
 #include <cstdlib>
 #include <csignal>
 
+#include <fhglog/fhglog.hpp>
+#include <fhglog/util.hpp>
 #include <fhglog/remote/LogServer.hpp>
-#include <fhglog/NullAppender.hpp>
-#include <fhglog/Configuration.hpp>
 
 boost::asio::io_service io_service;
 
@@ -35,27 +35,25 @@ int main(int argc, char **argv)
 {
   try
   {
-    std::string mode("start");
     std::string port_string(FHGLOG_DEFAULT_PORT);
+    std::string fmt_string("full");
+
     if (argc > 1)
     {
       const std::string a1(argv[1]);
       if (a1 == "-h")
       {
-        std::cerr << "Usage: " << argv[0] << " [{start|stop} port]" << std::endl;
+        std::cerr << "Usage: " << argv[0] << "[fmt [port]]" << std::endl;
+        std::cerr << "\tfmt  - format to use {short, full, custom} (default: full)" << std::endl;
         std::cerr << "\tport - udp port to listen on (default:" << FHGLOG_DEFAULT_PORT << ")" << std::endl;
         return 1;
       }
-      else if (a1 == "stop")
+      else
       {
-        mode = a1;
-      }
-      else if (a1 == "start")
-      {
-        mode = a1;
+        fmt_string = a1;
       }
     }
-    
+
     if (argc > 2)
     {
       port_string = argv[2];
@@ -70,35 +68,25 @@ int main(int argc, char **argv)
       return 2;
     }
 
-//    fhg::log::Configurator::configure();
+    signal(SIGINT, signal_handler);
+    signal(SIGTERM, signal_handler);
 
-    if (mode == "start")
-    {
-      signal(SIGINT, signal_handler);
-      signal(SIGTERM, signal_handler);
+    // my own output goes to stderr
+    fhg::log::getLogger().addAppender(fhg::log::Appender::ptr_t(new fhg::log::StreamAppender("console", std::cerr)))->setFormat(fhg::log::Formatter::Default());
 
-      // my own output goes to stderr
-      fhg::log::getLogger().addAppender(fhg::log::Appender::ptr_t(new fhg::log::StreamAppender("console", std::cerr)))->setFormat(fhg::log::Formatter::Default());
+    // remote messages go to stdout
+    fhg::log::Appender::ptr_t appender(new fhg::log::StreamAppender("console", std::cout));
 
-      // remote messages go to stdout
-      fhg::log::Appender::ptr_t appender(new fhg::log::StreamAppender("console", std::cout));
-      appender->setFormat(fhg::log::Formatter::Full());
-      fhg::log::remote::LogServer server(appender, io_service, port);
+    if      (fmt_string == "full")  appender->setFormat(fhg::log::Formatter::Full());
+    else if (fmt_string == "short") appender->setFormat(fhg::log::Formatter::Short());
+    else                            appender->setFormat(fhg::log::Formatter::Custom(fmt_string));
 
-      io_service.run();
-      LOG(INFO, "done.");
-      return 0;
-    }
-    else if (mode == "stop")
-    {
-      std::cerr << "stopping server on port " << port << std::endl;
-      std::cerr << "\tFIXME: implement me correctly" << std::endl;
-      std::ostringstream cmd;
-      cmd << "echo -n QUIT deadbeef | nc -q 0 -u localhost " << port;
-      return system(cmd.str().c_str());
-    }
+    fhg::log::remote::LogServer server(appender, io_service, port);
+
+    io_service.run();
+    LOG(INFO, "done.");
   }
-  catch (std::exception& e)
+  catch (const std::exception& e)
   {
     std::cerr << "Exception: " << e.what() << "\n";
   }
