4a363193e12e4e6cf347299d2e8ee8688f738b51
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-11-14 06:13:15 +0000

    PingReply contains rusage information about the worker process and added operator<< for messages
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@1255 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/sdpa/daemon/nre/messages.hpp
+++ trunk/sdpa/trunk/sdpa/daemon/nre/messages.hpp
@@ -19,9 +19,14 @@
 #ifndef SDPA_APPS_NRE_WORKER_MESSAGES_HPP
 #define SDPA_APPS_NRE_WORKER_MESSAGES_HPP 1
 
+#include <ostream>
 #include <string>
 #include <fhglog/fhglog.hpp>
+#include <sdpa/wf/Activity.hpp>
 #include <sdpa/daemon/nre/ExecutionContext.hpp>
+
+#include <sys/time.h>
+#include <sys/resource.h>
 
 namespace sdpa { namespace nre { namespace worker {
   class Message
@@ -32,6 +37,7 @@ namespace sdpa { namespace nre { namespace worker {
     virtual ~Message() {}
 
     virtual Message *execute(ExecutionContext *context) = 0;
+    virtual void writeTo(std::ostream &) const = 0;
 
     // indicate a long-running execution request
     virtual bool would_block() const { return false; }
@@ -59,6 +65,8 @@ namespace sdpa { namespace nre { namespace worker {
   class PingReply : public Reply
   {
   public:
+    typedef struct rusage  usage_t;
+
     PingReply()
       : key_("")
       , pid_(0)
@@ -67,16 +75,27 @@ namespace sdpa { namespace nre { namespace worker {
     PingReply(const std::string &a_key, const pid_t &my_pid)
       : key_(a_key)
       , pid_(my_pid)
-    {}
+    {
+      getrusage(RUSAGE_SELF, &usage_);
+    }
+
+    virtual void writeTo(std::ostream &os) const
+    {
+      os << "PingReply: key=" << key() << " pid=" << pid() << " utime=" << usage().ru_utime.tv_sec;
+    }
 
     std::string &key() { return key_; }
     const std::string &key() const { return key_; }
 
     pid_t &pid() { return pid_; }
     const pid_t &pid() const { return pid_; }
+
+    const usage_t &usage() const { return usage_; }
+    usage_t &usage() { return usage_; }
   private:
     std::string key_;
     pid_t pid_;
+    usage_t usage_;
   };
 
   class PingRequest : public Request
@@ -89,6 +108,11 @@ namespace sdpa { namespace nre { namespace worker {
     explicit PingRequest(const std::string &a_key)
       : key_(a_key)
     {}
+
+    virtual void writeTo(std::ostream &os) const
+    {
+      os << "PingRequest: key="<< key();
+    }
 
     std::string &key() { return key_; }
     const std::string &key() const { return key_; }
@@ -113,6 +137,11 @@ namespace sdpa { namespace nre { namespace worker {
       : result_(execution_result)
     {}
 
+    virtual void writeTo(std::ostream &os) const
+    {
+      os << "ExecuteReply: result="<< result();
+    }
+
     result_t &result() { return result_; }
     const result_t &result() const { return result_; }
   private:
@@ -131,6 +160,11 @@ namespace sdpa { namespace nre { namespace worker {
 
     virtual bool would_block() const { return true; }
 
+    virtual void writeTo(std::ostream &os) const
+    {
+      os << "ExecuteReqst";
+    }
+
     virtual Reply *execute(ExecutionContext *ctxt)
     {
       const std::string mod_name(activity().method().module());
@@ -138,21 +172,26 @@ namespace sdpa { namespace nre { namespace worker {
 
       try
       {
+        LOG(INFO, "executing activity: " << activity());
+
         ctxt->loader().get(mod_name).call(fun_name, activity().parameters());
+
         LOG(INFO, "execution of activity finished");
         activity().state() = sdpa::wf::Activity::ACTIVITY_FINISHED;
         return new ExecuteReply(activity());
       }
       catch (const std::exception &ex)
       {
-        LOG(WARN, "execution of activity failed: " << ex.what());
+        LOG(ERROR, "execution of activity failed: " << ex.what());
         activity().state() = sdpa::wf::Activity::ACTIVITY_FAILED;
+        activity().properties().put("reason", ex.what());
         return new ExecuteReply(activity());
       }
       catch (...)
       {
-        LOG(WARN, "execution of activity failed: ");
+        LOG(ERROR, "execution of activity failed: ");
         activity().state() = sdpa::wf::Activity::ACTIVITY_FAILED;
+        activity().properties().put("reason", "unknown");
         return new ExecuteReply(activity());
       }
     }
@@ -173,6 +212,11 @@ namespace sdpa { namespace nre { namespace worker {
       : path_(path_to_module)
     {}
 
+    virtual void writeTo(std::ostream &os) const
+    {
+      os << "ModuleLoaded: path="<< path();
+    }
+
     const std::string &path() const { return path_; }
     std::string &path() { return path_; }
   private:
@@ -189,6 +233,11 @@ namespace sdpa { namespace nre { namespace worker {
       : path_(path_to_module)
       , reason_(reason_for_failure)
     {}
+
+    virtual void writeTo(std::ostream &os) const
+    {
+      os << "ModuleNotLoaded: path="<< path() << " reason=" << reason();
+    }
 
     const std::string &path() const { return path_; }
     std::string &path() { return path_; }
@@ -210,6 +259,11 @@ namespace sdpa { namespace nre { namespace worker {
       : path_(path_to_module)
     { }
 
+    virtual void writeTo(std::ostream &os) const
+    {
+      os << "LoadModule: path="<< path();
+    }
+
     virtual Reply *execute(ExecutionContext *ctxt)
     {
       try
@@ -230,5 +284,11 @@ namespace sdpa { namespace nre { namespace worker {
     std::string path_;
   };
 }}}
+
+inline std::ostream &operator<<(std::ostream &os, const sdpa::nre::worker::Message &m)
+{
+  m.writeTo(os);
+  return os;
+}
 
 #endif
