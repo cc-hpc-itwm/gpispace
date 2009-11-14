6c9aa4a02713d6339eeec0ef5ba7860459cd75ef
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-11-14 13:45:44 +0000

    more sophisticated tests can be done now (parses command line for function + parameters + datatype
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@1264 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/tests/bin/nre-worker-test.cpp
+++ trunk/sdpa/trunk/tests/bin/nre-worker-test.cpp
@@ -17,6 +17,7 @@
  */
 
 #include <fhglog/fhglog.hpp>
+#include <fhglog/util.hpp>
 #include <sdpa/daemon/nre/NreWorkerClient.hpp>
 #include <sdpa/wf/Activity.hpp>
 
@@ -35,18 +36,57 @@ int main(int ac, char **av)
     function_call = av[2];
   }
 
+  sdpa::wf::parameters_t params;
+  for (int i = 3; i < ac; ++i)
+  {
+    // param = datatype:value
+    // param = value
+    std::pair<std::string, std::string> kv = fhg::log::split_string(av[i], "=");
+
+    std::string p_name = kv.first;
+    std::string value  = kv.second;
+    std::string datatype = "unknown";
+
+    kv = fhg::log::split_string(kv.second, ":");
+    if (kv.second.empty()) value = kv.first;
+    else if (kv.first.empty()) value = kv.second;
+    else
+    {
+      datatype = kv.first;
+      value = kv.second;
+    }
+
+    // TODO: parse edge type
+    std::cout << "adding parameter (type " << datatype << ") " << p_name << "=" << value << "" << std::endl;
+
+    sdpa::wf::Parameter p(p_name, sdpa::wf::Parameter::INPUT_EDGE, sdpa::wf::Token(value));
+    p.token().properties().put("datatype", datatype);
+    params[p_name] = p;
+  }
+
   sdpa::nre::worker::NreWorkerClient client(worker_location);
   client.start();
 
   client.ping();
 
   // try to execute an activity
-//  sdpa::wf::parameters_t params;
-//  params["config_file"].token().data("/etc/passwd");
-//  sdpa::wf::Activity a_in("activity-1", sdpa::wf::Method(function_call), params);
-  sdpa::wf::Activity a_in("activity-1", sdpa::wf::Method(function_call));
-  sdpa::wf::Activity a_ou(client.execute(a_in));
-  LOG(INFO, "got reply: " << a_ou);
+  sdpa::wf::Activity req("activity-1", sdpa::wf::Method(function_call), params);
+  std::cout << "sending ";
+  req.writeTo(std::cout, false);
+  std::cout << std::endl;
+
+  sdpa::wf::Activity res(client.execute(req));
+  std::cout << "got ";
+  res.writeTo(std::cout, false);
+  std::cout << std::endl;
+
+  std::cout << std::endl;
+  std::cout << "========= " << ((res.state() == sdpa::wf::Activity::ACTIVITY_FINISHED) ? ("finished") : ("failed")) << " ========" << std::endl;
+  std::cout << res.reason() << std::endl;
+  for (sdpa::wf::parameters_t::const_iterator out(res.parameters().begin()); out != res.parameters().end(); ++out)
+  {
+    std::cout << out->first << "=" << out->second.token().properties().get("datatype") << ":" << "\"" << out->second.token().data() << "\"" << std::endl;
+  }
 
   client.stop();
   return 0;
