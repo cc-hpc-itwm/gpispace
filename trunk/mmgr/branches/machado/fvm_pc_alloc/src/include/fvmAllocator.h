1b3df6f73e1d8d2837db9bc5786a56f100cece50
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-11-15 14:33:24 +0000

    split fvmAllocator.h into fvmAllocator.h and fvmAllocatorTypes.h
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@1275 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/mmgr/branches/machado/fvm_pc_alloc/src/include/fvmAllocator.h
+++ trunk/mmgr/branches/machado/fvm_pc_alloc/src/include/fvmAllocator.h
@@ -1,50 +1,7 @@
 #ifndef _FVM_ALLOCATOR_H_
 #define _FVM_ALLOCATOR_H_
 
-/* ************* Macros **************** */
-#define IN_MB(X) (X / 1024 /1024)
-#define ACK_OK 1
-#define ACK_NOK 0
-
-#define ALLOCATOR_PORT_REMOTE "14000" 
-#define BACKLOG 10
-
-/* ************* type definitions *************** */
-/* typedef struct fvmMemPointer */
-/* { */
-/* 	void *fvmAddress; */
-/* 	unsigned long long offset; */
-/* 	unsigned int size; */
-/* }fvmMemPointer_t; */
-
-
-typedef enum {
-  QUERYGLOBALLOC,
-  QUERYGLOBALLOCACK,
-  ABORTGLOBALLOC,
-  ABORTGLOBALLOCACK,
-  COMMITGLOBALLOC,
-  COMMITGLOBALLOCACK,
-  COMMSTOPGLOBAL,
-  COMMSTOPGLOBALACK,
-  COMMITGLOBALLOCACKGLOBALLY,
-  LOCALLOC,
-  LOCALFREE,
-  GLOBALFREE,
-  GLOBALFREEACK
-} fvmAllocType_t;
-
-typedef unsigned long fvmAllocHandle_t;
-typedef unsigned long fvmSize_t;
-
-typedef struct {
-  fvmAllocType_t type;
-  fvmSize_t size;
-  int root;
-  fvmAllocHandle_t handle;
-} fvmAllocRequest_t;
-
-
+#include "fvmAllocatorTypes.h"
 
 /* ************ Function definitions ****************** */
 int fvmMMInit(void * ptr, size_t length, int rank, int nnodes,const char **hosts);
