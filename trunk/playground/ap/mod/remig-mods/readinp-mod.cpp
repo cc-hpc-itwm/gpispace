f3dab29d1b774a701ee2781f7e1c0f86588452d1
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-11-24 15:33:06 +0000

    remig modules (the real ones
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@1473 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/playground/ap/mod/remig-mods/readinp-mod.cpp
@@ -0,0 +1,31 @@
+#include <sdpa/modules/Macros.hpp>
+#include <sdpa/modules/assert.hpp>
+
+#include <fhglog/fhglog.hpp>
+
+#include <fvm-pc/pc.hpp>
+
+using namespace sdpa::modules;
+
+void readinp (data_t &params)
+{
+  const fvmAllocHandle_t memhandle_for_configuration
+    (params.at("memhandle_for_configuration").token().data_as<fvmAllocHandle_t>());
+  ASSERT_HANDLE(memhandle_for_configuration);
+
+  MLOG (DEBUG, "memhandle_for_configuration = " << memhandle_for_configuration);
+
+  // at the moment: do nothing
+
+  params["seq"].token().data("SEQ");
+}
+
+SDPA_MOD_INIT_START(readinp)
+{
+  SDPA_REGISTER_FUN_START(readinp);
+    SDPA_ADD_INP( "memhandle_for_configuration", fvmAllocHandle_t );
+
+    SDPA_ADD_OUT("seq", char *);
+  SDPA_REGISTER_FUN_END(readinp);
+}
+SDPA_MOD_INIT_END(readinp)
