060a19c23ab36f2f8018733593711783a51bf576
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-11-25 00:05:04 +0000

    added new test scripts
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@1488 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/playground/ap/test-scripts/calc.sh
+++ trunk/playground/ap/test-scripts/calc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ $# != 4 ]; then
-  echo "usage: $0 slice_and_depth number_of_depth_levels memhandle_for_config memhandle_for_temp_output_volume"
+  echo "usage: $0 memhandle_for_config number_of_depth_levels slice_and_depth memhandle_for_temp_outputvolume"
   exit 1
 fi 
 
-nre-pcc -f calc@calc -i slice_and_depth="$1" -i number_of_depthlevels="$2" -i memhandle_for_configuration="$3" -i memhandle_for_temp_output_volume_calc="$4" -o slice_and_depth_OUT
+nre-pcc -f calc@calc -i memhandle_for_configuration="$1" -i number_of_depthlevels="$2" -i slice_and_depth="$3" -i memhandle_for_temp_outputvolume="$4" -o slice_and_depth_OUT
