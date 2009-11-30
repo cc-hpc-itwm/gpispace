b65ec9ad84e1e91b077a46efdccba4c3ebe3fe92
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-11-30 22:23:21 +0000

    implemented put and get on base of memcpy
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@1558 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/fvm-pc/trunk/fvm-pc/pc_fake.cpp
+++ trunk/fvm-pc/trunk/fvm-pc/pc_fake.cpp
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <dtmmgr.h>
@@ -29,6 +30,7 @@ enum FVM_PC_API_ERRORS
   , FVM_EINVALSZ   = -2
   , FVM_EOUTOFMEM  = -3
   , FVM_ETOOLARGE  = -4
+  , FVM_ESRCH      = -5
 };
 
 static void cleanUp()
@@ -85,21 +87,25 @@ int fvmConnect(fvm_pc_config_t cfg)
 	if (! pcShm)
 	{
 	  fprintf(stderr, "fvm-pc: shared memory allocation failed!");
+	  cleanUp();
 	  return FVM_EOUTOFMEM;
 	}
+	memset(pcShm, 0, pcShmSize);
 
     pcFVMSize = cfg.fvmsize;
     fprintf(stderr, "fvm-pc: allocating %lu bytes of VM\n", pcFVMSize);
     pcFVM = malloc(pcFVMSize);
 	if (! pcFVM)
 	{
-	  fprintf(stderr, "fvm-pc: shared memory allocation failed!");
+	  fprintf(stderr, "fvm-pc: VM memory allocation failed!");
+	  cleanUp();
 	  return FVM_EOUTOFMEM;
 	}
+	memset(pcFVM, 0, pcFVMSize);
 
-    fprintf(stderr, "fvm-pc: using %lu bytes of VM memory\n", cfg.fvmsize);
+    fprintf(stderr, "fvm-pc: using %lu bytes of VM memory\n", pcFVMSize);
 
-    dtmmgr_init (&dtmmgr, cfg.fvmsize, 2);
+    dtmmgr_init (&dtmmgr, pcFVMSize, 2);
 	dtmmgr_info (dtmmgr);
 
     return 0;
@@ -115,75 +121,134 @@ int fvmLeave()
 fvmAllocHandle_t fvmGlobalAlloc(fvmSize_t size)
 {
   static fvmAllocHandle_t num_allocs = 0;
-  if (size > 0)
-  {
-    fvmAllocHandle_t ptr= (++num_allocs);
 
+  fvmAllocHandle_t ptr= (++num_allocs);
+  AllocReturn_t AllocReturn = dtmmgr_alloc( &dtmmgr, (Handle_t)ptr, (Arena_t)ARENA_GLOBAL, (Size_t)size);
+  switch (AllocReturn)
+  {
+	case ALLOC_SUCCESS:
 #ifndef NDEBUG
-    fprintf(stderr, "fvm-pc: global alloc handle %lu -> %lu bytes\n", ptr, size);
+      fprintf(stderr, "fvm-pc: global alloc handle %lu -> %lu bytes\n", ptr, size);
+	  dtmmgr_status (dtmmgr);
 #endif
-    return ptr;
+	  return ptr;
+	case ALLOC_INSUFFICIENT_CONTIGUOUS_MEMORY:
+#ifndef NDEBUG
+      fprintf(stderr, "fvm-pc: global alloc failed: not enough contigiuous memory!\n");
+	  dtmmgr_status (dtmmgr);
+#endif
+	  // FIXME: set errno!
+	  return 0;
+	default:
+#ifndef NDEBUG
+      fprintf(stderr, "fvm-pc: global alloc failed!\n");
+	  dtmmgr_status (dtmmgr);
+#endif
+	  return 0;
   }
-  else
-  {
-    fprintf(stderr, "fvm-pc: global alloc failed: size 0 is invalid\n");
-  }
-
-  return 0;
 }
 
 int fvmGlobalFree(fvmAllocHandle_t ptr)
 {
-  if (ptr > 0)
+#ifndef NDEBUG
+      fprintf(stderr, "fvm-pc: global free: handle=%lu\n", ptr);
+#endif
+
+  switch (HandleReturn_t ret = dtmmgr_free (&dtmmgr, ptr, ARENA_GLOBAL))
   {
-    return 0;
+	case RET_SUCCESS:
+	  return 0;
+	case RET_HANDLE_UNKNOWN:
+	  return FVM_ESRCH;
+	case RET_FAILURE:
+	  return FVM_EGENERAL;
+	default:
+      fprintf(stderr, "fvm-pc: global free failed: unknown return code: %d\n", ret);
+	  return FVM_EGENERAL;
   }
-  return -1;
 }
 
 fvmAllocHandle_t fvmLocalAlloc(fvmSize_t size)
 {
   static fvmAllocHandle_t num_allocs = 0;
-  if (size > 0)
-  {
-    fvmAllocHandle_t ptr= (++num_allocs);
 
+  fvmAllocHandle_t ptr= (++num_allocs);
+  AllocReturn_t AllocReturn = dtmmgr_alloc( &dtmmgr, (Handle_t)ptr, (Arena_t)ARENA_LOCAL, (Size_t)size);
+  switch (AllocReturn)
+  {
+	case ALLOC_SUCCESS:
 #ifndef NDEBUG
-    fprintf(stderr, "fvm-pc: local alloc handle %lu -> %lu bytes\n", ptr, size);
+      fprintf(stderr, "fvm-pc: local alloc handle %lu -> %lu bytes\n", ptr, size);
+	  dtmmgr_status (dtmmgr);
 #endif
-    return ptr;
+	  return ptr;
+	case ALLOC_INSUFFICIENT_CONTIGUOUS_MEMORY:
+#ifndef NDEBUG
+      fprintf(stderr, "fvm-pc: local alloc failed: not enough contigiuous memory!\n");
+	  dtmmgr_status (dtmmgr);
+#endif
+	  // FIXME: set errno!
+	  return 0;
+	default:
+#ifndef NDEBUG
+      fprintf(stderr, "fvm-pc: local alloc failed!\n");
+	  dtmmgr_status (dtmmgr);
+#endif
+	  return 0;
   }
-  else
-  {
-    fprintf(stderr, "fvm-pc: local alloc failed: size 0 is invalid\n");
-  }
-
-  return 0;
 }
 
 int fvmLocalFree(fvmAllocHandle_t ptr)
 {
-  if (ptr > 0)
+#ifndef NDEBUG
+      fprintf(stderr, "fvm-pc: local free: handle=%lu\n", ptr);
+#endif
+
+  switch (HandleReturn_t ret = dtmmgr_free (&dtmmgr, ptr, ARENA_LOCAL))
   {
-    return 0;
+	case RET_SUCCESS:
+	  return 0;
+	case RET_HANDLE_UNKNOWN:
+	  return FVM_ESRCH;
+	case RET_FAILURE:
+	  return FVM_EGENERAL;
+	default:
+	  // FIXME: set errno instead!
+      fprintf(stderr, "fvm-pc: local free failed: unknown return code: %d\n", ret);
+	  return FVM_EGENERAL;
   }
-  return -1;
 }
 
 static fvmCommHandle_t fvmCommData(const fvmAllocHandle_t handle,
 				   const fvmOffset_t fvmOffset,
 				   const fvmSize_t size,
 				   const fvmShmemOffset_t shmemOffset,
-				   const fvmAllocHandle_t scratchHandle,
+				   const fvmAllocHandle_t /* scratchHandle */,
 				   const fvmOperation_t op)
 {
-  fvmRequest_t request;
-  request.op = op;
-  request.args.arg_handle = handle;
-  request.args.arg_fvmOffset = fvmOffset;
-  request.args.arg_size = size;
-  request.args.arg_shmOffset = shmemOffset;
-  request.args.arg_scratchhandle = scratchHandle;
+  Offset_t BaseOffset = -1;
+  switch (op)
+  {
+	case GETGLOBAL:
+	  dtmmgr_offset_size (dtmmgr, handle, ARENA_GLOBAL, &BaseOffset, NULL);
+	  memcpy((char*)(pcShm) + shmemOffset, (char*)(pcFVM) + BaseOffset + fvmOffset, size);
+	  break;
+	case PUTGLOBAL:
+	  dtmmgr_offset_size (dtmmgr, handle, ARENA_GLOBAL, &BaseOffset, NULL);
+	  memcpy((char*)(pcFVM) + BaseOffset + fvmOffset, (char*)(pcShm) + shmemOffset, size);
+	  break;
+	case GETLOCAL:
+	  dtmmgr_offset_size (dtmmgr, handle, ARENA_LOCAL, &BaseOffset, NULL);
+	  memcpy((char*)(pcShm) + shmemOffset, (char*)(pcFVM) + BaseOffset + fvmOffset, size);
+	  break;
+	case PUTLOCAL:
+	  dtmmgr_offset_size (dtmmgr, handle, ARENA_LOCAL, &BaseOffset, NULL);
+	  memcpy((char*)(pcFVM) + BaseOffset + fvmOffset, (char*)(pcShm) + shmemOffset, size);
+	  break;
+	default:
+	  fprintf(stderr, "fvm-pc: fvmCommData() unsupported comm-operation:%d\n", op);
+	  return -1;
+  }
 
   return 0;
 }
@@ -241,7 +306,6 @@ fvmCommHandle_t fvmGetLocalData(const fvmAllocHandle_t handle,
 				const fvmSize_t size,
 				const fvmShmemOffset_t shmemOffset)
 {
-  
   fvmCommHandle_t commhandle = fvmCommData(handle, fvmOffset, size, shmemOffset, 0, GETLOCAL);
   
 #ifndef NDEBUGCOMM
