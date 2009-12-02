d1abc4d716f724edc29d7f0860291141d7080b89
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-12-02 12:14:11 +0000

    implemented local memory dump
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@1568 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/nre-worker/trunk/mods/debug-mod.cpp
+++ trunk/nre-worker/trunk/mods/debug-mod.cpp
@@ -9,7 +9,7 @@
 
 using namespace sdpa::modules;
 
-static void dump_memory(data_t &params) throw (std::exception)
+static void dump_local_memory(data_t &params) throw (std::exception)
 {
   const fvmAllocHandle_t memory_handle = params.at("handle").token().data_as<fvmAllocHandle_t>();
   ASSERT_HANDLE(memory_handle);
@@ -18,19 +18,44 @@ static void dump_memory(data_t &params) throw (std::exception)
   const fvmSize_t size = params.at("size").token().data_as<fvmSize_t>();
   const std::string file = params.at("file").token().data_as<std::string>();
 
-  DMLOG(INFO, "dumping memory: handle=" << memory_handle << " offset=" << offset << " size=" << size << " to file '" << file << "'");
+  DMLOG(INFO, "dumping local memory: handle=" << memory_handle << " offset=" << offset << " size=" << size << " to file '" << file << "'");
 
-  params["error_code"].token().data(0);
+  std::ofstream ofs(file.c_str());
+  if (! ofs)
+  {
+	MLOG(ERROR, "could not open output file for writing!");
+	throw std::runtime_error("could not open output file for writing!");
+  }
+
+  fvmSize_t bytes_to_transfer = size;
+  fvmSize_t bytes_transfered = 0;
+  while (bytes_to_transfer > 0)
+  {
+	const fvmSize_t transfer_size = std::min (bytes_to_transfer, fvmGetShmemSize());
+	MLOG(TRACE, "transfering " << transfer_size << " bytes of memory");
+	fvmCommHandle_t comm = fvmGetLocalData (memory_handle, bytes_transfered, transfer_size, 0);
+	fvmCommHandleState_t comm_state = waitComm(comm);
+	if (comm_state != COMM_HANDLE_OK)
+	{
+	  MLOG(ERROR, "could not fvmGetLocalData(): " << comm_state);
+	  throw std::runtime_error("could not fvmGetLocalData()");
+	}
+
+	ofs.write( (char*)fvmGetShmemPtr(), transfer_size);
+	bytes_to_transfer -= transfer_size;
+	bytes_transfered  += transfer_size;
+  }
+  DMLOG(INFO, "dumped local memory");
 }
 
 SDPA_MOD_INIT_START(debug)
 {
-  SDPA_REGISTER_FUN_START(dump_memory);
+  SDPA_REGISTER_FUN_START(dump_local_memory);
 	SDPA_ADD_INP( "handle", fvmAllocHandle_t );
 	SDPA_ADD_INP( "offset", fvmOffset_t );
 	SDPA_ADD_INP( "size", fvmSize_t );
 	SDPA_ADD_INP( "file", std::string );
     SDPA_ADD_OUT( "error_code", int );
-  SDPA_REGISTER_FUN_END(dump_memory);
+  SDPA_REGISTER_FUN_END(dump_local_memory);
 }
 SDPA_MOD_INIT_END(debug)
