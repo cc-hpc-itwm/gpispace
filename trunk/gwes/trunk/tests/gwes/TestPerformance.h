d680433998d71db9b5ebd6d802ba9e7819ed5f5a
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2009-12-08 10:48:54 +0000

    more performance tests
    From: Andreas Hoheisel <andreas.hoheisel@first.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@1623 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/gwes/trunk/tests/gwes/TestPerformance.h
+++ trunk/gwes/trunk/tests/gwes/TestPerformance.h
@@ -6,6 +6,11 @@
 #include <gwes/GWES.h>
 //fhglog
 #include <fhglog/fhglog.hpp>
+// boost
+#include <boost/timer.hpp>
+// std
+#include <time.h>
+#include <sys/resource.h>
 
 namespace gwes {
   namespace tests {
@@ -14,6 +19,8 @@ namespace gwes {
       CPPUNIT_TEST( testWorkflowControlLoop ); 
       CPPUNIT_TEST( testManySimpleWorkflows );
       CPPUNIT_TEST( testManyConcurrentWorkflows );
+      CPPUNIT_TEST( testWorkflowWithManyActivities );
+      CPPUNIT_TEST( testWorkflowWithManySdpaActivities );
       CPPUNIT_TEST_SUITE_END();
 
       public:
@@ -24,6 +31,9 @@ namespace gwes {
       void testWorkflowControlLoop();
       void testManySimpleWorkflows(); 
       void testManyConcurrentWorkflows();
+      void testWorkflowWithManyActivities(); 
+      void testWorkflowWithManySdpaActivities(); 
+
       
     private:
       gwdl::Workflow::ptr_t _testWorkflow(std::string workflowfn, gwes::GWES &gwes);
@@ -31,9 +41,19 @@ namespace gwes {
       WorkflowHandler::status_t _monitorWorkflow(WorkflowHandler::status_t oldStatus, WorkflowHandler* wfhP);
       void _loggerShutup();
       void _loggerAsBefore();
+      void _measureBefore();
+      std::string _measureAfter();
       gwes::GWES m_gwes;
       fhg::log::LogLevel _oldGwesLevel;
       fhg::log::LogLevel _oldGwdlLevel;
+      
+      // sensors
+      boost::timer _boosttimer;
+      time_t _timeBefore;
+      time_t _timeAfter;
+      rusage _usageBefore;
+      rusage _usageAfter;
+      
     };
   }
 }
