817ccbb143386516b57068698d3c752a0f08123d
rahn <rahn@c3adf61f-8968-0410-94d0-83abfdf586cf>
2010-01-06 11:17:54 +0000

    program to count copies
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@1741 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/we/trunk/m.cpp
@@ -0,0 +1,99 @@
+
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include <net.hpp>
+
+using std::cout;
+using std::endl;
+
+static unsigned long cons (0);
+static unsigned long decons (0);
+static unsigned long copy (0);
+
+class place_t
+{
+private:
+  int x;
+public:
+  void out (std::string msg) { cout << msg << ": " << x << endl; }
+
+  place_t () : x(0) { out ("place_t()"); ++cons; }
+  place_t (const place_t & p) { x = p.x + 1; out ("copy place_t()"); ++copy; }
+  ~place_t () { out ("~place_t()"); ++decons; }
+
+  const int get (void) const { return x; }
+
+  bool operator == (const place_t & other) const 
+  {
+    cout << "compare this.x == " << x
+         << " with other.x == " << other.x
+         << endl;
+
+    return (x == other.x);
+  }
+};
+
+static inline const std::size_t hash_value (const place_t & p)
+{
+  return p.get();
+}
+
+std::ostream & operator << (std::ostream & s, const place_t & p)
+{
+  return s << "place_t: copy count = " << p.get();
+}
+
+typedef std::string transition_t;
+typedef std::string edge_t;
+typedef std::string token_t;
+
+typedef net<place_t, transition_t, edge_t, token_t> pnet_t;
+
+static void print (const pnet_t & n)
+{
+  cout << "*** places" << endl;
+
+  for (pnet_t::place_const_it pit (n.places()); pit.has_more(); ++pit)
+    cout << *pit << " => " << n.place(*pit) << endl;
+}
+
+int
+main ()
+{
+  {
+    pnet_t n;
+
+    place_t p;
+
+    cout << "add_place => " << n.add_place (p) << endl;
+    cout << "add_place => " << n.add_place (p) << endl;
+
+    print (n);
+
+    try
+      {
+        cout << "del_place => " << n.delete_place (p) << endl;
+      }
+    catch (no_such)
+      {
+        cout << "NOT THERE!" << endl;
+      }
+
+    print (n);
+
+    for (pnet_t::place_const_it pit (n.places()); pit.has_more(); ++pit)
+      n.delete_place (*pit);
+
+    print (n);
+  }
+
+  cout << "#" << endl;
+  cout << "# cons = " << cons << endl;
+  cout << "# decons = " << decons << endl;
+  cout << "# copy = " << copy << endl;
+
+  return EXIT_SUCCESS;
+}
