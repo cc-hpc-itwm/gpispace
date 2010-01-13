f784b2640f9bf85f6b157e50e9872a429a21bdb1
rahn <rahn@c3adf61f-8968-0410-94d0-83abfdf586cf>
2010-01-13 16:46:49 +0000

    adjacency matrix with different types for row and col
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@1791 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/we/trunk/adjacency.hpp
+++ trunk/we/trunk/adjacency.hpp
@@ -8,6 +8,132 @@
 
 namespace adjacency
 {
+  template<typename L, typename R>
+  struct adj_const_it
+  {
+  private:
+    typedef std::pair<L,R> pair_t;
+    typedef std::vector<pair_t> vec_t;
+    typedef typename vec_t::const_iterator it_t;
+
+    it_t pos;
+    const it_t end;
+  public:
+    adj_const_it (const it_t & _pos, const it_t & _end)
+      : pos (_pos)
+      , end (_end)
+    {}
+
+    const bool has_more (void) const { return (pos != end) ? true : false; }
+    void operator ++ (void) { ++pos; }
+
+    const L & operator * (void) const { return pos->first; }
+    const R & operator () (void) const { return pos->second; }
+  };
+
+  template<typename ROW, typename COL, typename ADJ>
+  class tab
+  {
+  private:
+    const ADJ invalid;
+
+    typedef std::pair<ROW,ADJ> row_adj_t;
+    typedef std::pair<COL,ADJ> col_adj_t;
+
+    typedef std::vector<row_adj_t> row_adj_vec_t;
+    typedef std::vector<col_adj_t> col_adj_vec_t;
+    
+    typedef std::vector<col_adj_vec_t> row_tab_t;
+    typedef std::vector<row_adj_vec_t> col_tab_t;
+
+    row_tab_t row_tab;
+    col_tab_t col_tab;
+
+  public:
+    const adj_const_it<COL,ADJ> row_const_it (const ROW & r) const
+    {
+      return adj_const_it<COL,ADJ> (row_tab[r].begin(), row_tab[r].end());
+    }
+
+    const adj_const_it<ROW,ADJ> col_const_it (const COL & c) const
+    {
+      return adj_const_it<ROW,ADJ> (col_tab[c].begin(), col_tab[c].end());
+    }
+
+    tab (const ADJ & _invalid, const ROW & r = 0, const COL & c = 0)
+      : invalid (_invalid)
+      , row_tab (r)
+      , col_tab (c)
+    {}
+
+    const ADJ get_adjacent (const ROW & r, const COL & c) const
+    {
+      ADJ v (invalid);
+
+      if (r < row_tab.size())
+        for ( typename col_adj_vec_t::const_iterator it (row_tab[r].begin())
+            ; it != row_tab[r].end()
+            ; ++it
+            )
+          if (it->first == c)
+            {
+              v = it->second;
+              break;
+            }
+
+      return v;
+    }
+
+    void clear_adjacent (const ROW & r, const COL & c)
+    {
+      if (r < row_tab.size())
+        for ( typename col_adj_vec_t::const_iterator it (row_tab[r].begin())
+            ; it != row_tab[r].end()
+            ; ++it
+            )
+          if (it->first == c)
+            {
+              row_tab[r].erase (it);
+              break;
+            }
+
+      if (c < col_tab.size())
+        for ( typename row_adj_vec_t::const_iterator it (col_tab[c].begin())
+            ; it != col_tab[c].end()
+            ; ++it
+            )
+          if (it->first == r)
+            {
+              col_tab[c].erase (it);
+              break;
+            }
+    }
+
+  private:
+    template<typename T, typename MAT>
+    void adjust_size (const T & x, MAT & mat)
+    {
+      typedef typename MAT::size_type sz_t;
+
+      sz_t sz (std::max (sz_t(1), mat.size()));
+
+      while (x >= sz)
+        sz <<= 1;
+
+      mat.resize (sz);
+    }
+
+  public:
+    void set_adjacent (const ROW & r, const COL & c, const ADJ & v)
+    {
+      adjust_size<ROW> (r, row_tab);
+      adjust_size<COL> (c, col_tab);
+
+      row_tab[r].push_back (col_adj_t (c, v));
+      col_tab[c].push_back (row_adj_t (r, v));
+    }
+  };
+
   template<typename IDX, typename ADJ>
   class table
   {
