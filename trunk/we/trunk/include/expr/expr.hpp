acb2a22c7f6cce8f5ac6d90f7c8cfc24ab524c90
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2010-02-02 23:54:01 +0000

    moved expression files into corresponding include and ap directory
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@1947 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/we/trunk/include/expr/expr.hpp
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  expr.hpp
+ *
+ *    Description:  expression parser using Boost.Spirit
+ *
+ *        Version:  1.0
+ *        Created:  02/01/2010 05:04:31 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Alexander Petry (petry), alexander.petry@itwm.fraunhofer.de
+ *        Company:  Fraunhofer ITWM
+ *
+ * =====================================================================================
+ */
+
+#ifndef WE_EXPR_HPP
+#define WE_EXPR_HPP 1
+
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/support.hpp>
+
+#endif
