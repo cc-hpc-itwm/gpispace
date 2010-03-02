0d0875bf5627e2e7371bbada7b8b8b054ecb6bed
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2010-03-02 11:10:36 +0000

    added we mgmt layer start
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@1996 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/we/trunk/tests/test_layer.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <sstream>
+
+#include "layer.hpp"
+
+using namespace we::mgmt;
+
+typedef petri_net::net<detail::place_t, detail::transition_t, detail::edge_t, detail::token_t> pnet_t;
+
+static std::ostream & operator << (std::ostream & s, const pnet_t & n)
+{
+  for (pnet_t::place_const_it p (n.places()); p.has_more(); ++p)
+    {
+      s << "[" << n.get_place (*p) << ":";
+
+      for (pnet_t::token_place_it tp (n.get_token (*p)); tp.has_more(); ++tp)
+        s << " " << *tp;
+
+      s << "]";
+    }
+
+  return s;
+}
+
+int main ()
+{
+  std::cout << "running layer test..." << std::endl;
+  pnet_t net;
+  petri_net::pid_t pid_in = net.add_place(detail::place_t("in"));
+  petri_net::pid_t pid_out = net.add_place(detail::place_t("out"));
+
+  petri_net::tid_t tid_start (
+	  net.add_transition(
+		detail::transition_t("start", detail::transition_t::INTERNAL_SIMPLE)));
+  petri_net::eid_t eid_start_in (
+	  net.add_edge(detail::edge_t("in"), petri_net::connection_t (petri_net::PT, tid_start, pid_in)));
+  petri_net::eid_t eid_start_out (
+	  net.add_edge(detail::edge_t("out"), petri_net::connection_t (petri_net::TP, tid_start, pid_out)));
+
+  detail::edge_t e("foo");
+
+  std::cout << net << std::endl;
+
+  std::cout 
+	<< "in=" << pid_in << " out=" << pid_out
+	<< " trans=" << tid_start
+	<< " e_in" << eid_start_in << " e_out=" << eid_start_out
+	<< std::endl;
+  return EXIT_SUCCESS;
+}
