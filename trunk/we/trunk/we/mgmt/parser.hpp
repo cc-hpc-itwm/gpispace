42b11e5f1603c88fcb591e34cd3221ad62ec9258
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2010-03-02 11:10:44 +0000

    added a parser template function placeholder
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2000 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/we/trunk/we/mgmt/parser.hpp
+++ trunk/we/trunk/we/mgmt/parser.hpp
@@ -22,7 +22,25 @@
 #include <we/mgmt/bits/types.hpp>
 
 namespace we { namespace mgmt {
+  template <typename Net>
+  struct NetTraits
+  {
+	typedef petri_net::pid_t pid_t;
+	typedef petri_net::tid_t tid_t;
+	typedef petri_net::eid_t eid_t;
+  };
 
+  template <typename _Net, typename _NetTraits, typename _Data>
+  void parse (_Net & net, const _Data &)
+  {
+	typename _NetTraits::pid_t pid_in = net.add_place(detail::place_t("in"));
+	typename _NetTraits::pid_t pid_out = net.add_place(detail::place_t("out"));
+	typename _NetTraits::tid_t tid_start (
+	  net.add_transition(
+		detail::transition_t("start", detail::transition_t::INTERNAL_SIMPLE)));
+	net.add_edge(detail::edge_t("in"), petri_net::connection_t (petri_net::PT, tid_start, pid_in));
+	net.add_edge(detail::edge_t("out"), petri_net::connection_t (petri_net::TP, tid_start, pid_out));
+  }
 }}
 
 #endif
