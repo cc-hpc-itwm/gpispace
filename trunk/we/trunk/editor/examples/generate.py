dd4de7e9cd6ddf1dfbddcf7837c1d5a1215389ad
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2010-03-17 12:15:34 +0000

    smaller distances between places
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2107 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/we/trunk/editor/examples/generate.py
+++ trunk/we/trunk/editor/examples/generate.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python
 
-import sys
+import sys, math
 from PyQt4 import QtXml
 
 if __name__ == "__main__":
   num_places = 1000
-  places_per_row = 100
+  if len(sys.argv) > 1:
+    num_places = int(sys.argv[1])
+  places_per_row = int(math.sqrt(num_places))
 
   print "<workflow>"
-  start_x = 500
-  start_y = 500
-  pos = (start_x, start_y)
-  offset = 100
+  start_x = 50
+  start_y = 50
+  x, y = start_x, start_y
+  offset = 75
   max_x = start_x + 50 * offset
   for p in xrange(num_places):
-    x,y = pos
     print "\t<place name=\"%d\" pos=\"(%f, %f)\"/>" % (p, x, y)
-    if x + offset > max_x:
+    x += offset
+    if x > max_x:
       x = start_x
       y = y + offset
-    else:
-      x = x + offset
-    pos = (x,y)
+    if p > 0:
+      print "\t<edge from=\"%d\" to=\"%d\"/>" % (p-1, p)
   print "</workflow>"
