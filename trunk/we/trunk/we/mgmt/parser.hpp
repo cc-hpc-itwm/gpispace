3c314fe0d65532168186db9268859f832df0a435
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2010-04-07 12:44:50 +0000

    reduced amount of initial tokens and removed commented section
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2209 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/we/trunk/we/mgmt/parser.hpp
+++ trunk/we/trunk/we/mgmt/parser.hpp
@@ -51,59 +51,11 @@ namespace we { namespace mgmt {
 	typedef typename traits_type::edge_t edge_t;
 	typedef typename traits_type::token_t token_t;
 
-    // TODO parsing should return an "activity" style thing
-    //      where this activity thing contains one of:
-    //         - network
-    //         - expression
-    //         - module call
-//	static void parse (net_type & net, const data_type &)
-//	{
-//	  // map-reduce network with 3 parallel nodes
-//
-//	  const std::size_t NUM_NODES=3;
-//	  const std::size_t NUM_TOKEN=1;
-//
-//	  pid_t pid_in = net.add_place(place_t("in"));
-//	  pid_t pid_out = net.add_place(place_t("out"));
-//
-//	  tid_t tid_map = net.add_transition (transition_t("map", transition_t::MODULE_CALL));
-//	  tid_t tid_red = net.add_transition (transition_t("red", transition_t::MODULE_CALL));
-//
-//	  net.add_edge (edge_t("map"), petri_net::connection_t (petri_net::PT, tid_map, pid_in));
-//	  net.add_edge (edge_t("red"), petri_net::connection_t (petri_net::TP, tid_red, pid_out));
-//
-//	  // create places and transitions
-//	  for (std::size_t n(0); n < NUM_NODES; ++n)
-//	  {
-//		const pid_t pid_wi = net.add_place(place_t("wi_" + show(n)));
-//		const pid_t pid_wo = net.add_place(place_t("wo_" + show(n)));
-//		const tid_t tid_w = net.add_transition( transition_t("work_"+show(n), transition_t::MODULE_CALL));
-//
-//		// connect map to work input
-//		net.add_edge (edge_t("map_" + show(n)), petri_net::connection_t (petri_net::TP, tid_map, pid_wi));
-//
-//		// connect work in
-//		net.add_edge (edge_t("work_in_"+show(n)), petri_net::connection_t (petri_net::PT, tid_w, pid_wi));
-//
-//		// connect work out
-//		net.add_edge (edge_t("work_out_" + show(n)), petri_net::connection_t (petri_net::TP, tid_w, pid_wo));
-//
-//		// connect work output to red
-//		net.add_edge (edge_t("red_" + show(n)), petri_net::connection_t (petri_net::PT, tid_red, pid_wo));
-//	  }
-//
-//	  for (std::size_t t (0); t < NUM_TOKEN; ++t)
-//	  {
-//		std::string name = "token-" + show(t);
-//		net.put_token(pid_in, token_t(name));
-//	  }
-//	}
-
     template <typename Activity>
     static void parse( Activity & act, const data_type &)
     {
       const std::size_t NUM_NODES=3;
-      const std::size_t NUM_TOKEN=3;
+      const std::size_t NUM_TOKEN=1;
 
       // create the subnetwork
       net_type map_reduce_subnet ("map-reduce-subnet");
@@ -202,9 +154,6 @@ namespace we { namespace mgmt {
         map_reduce.add_edge (edge_t("i"), petri_net::connection_t (petri_net::PT, tid_sub, mr_inp));
         map_reduce.add_edge (edge_t("o"), petri_net::connection_t (petri_net::TP, tid_sub, mr_out));
 
-//        map_reduce.put_token(mr_inp, token_t("test"));
-        std::cerr << "D: " << map_reduce.enabled_transitions().size() << std::endl;
-
         // dummy transition
         transition_t map_reduce_trans ("map-reduce", map_reduce, true);
         map_reduce_trans.connect_in (pid_t(0), mr_inp);
@@ -212,17 +161,10 @@ namespace we { namespace mgmt {
 
         act.assign (map_reduce_trans);
 
-        std::cerr << "D: mr-inp=" << mr_inp << std::endl;
-        std::cerr << "D: mr-out=" << mr_out << std::endl;
-        std::cerr << "D: is-net=" << map_reduce_trans.is_net() << std::endl;
-        std::cerr << "D: enabld=" << map_reduce_trans.template as<typename transition_t::net_type>()->enabled_transitions().size() << std::endl;
-        std::cerr << "D: enabld=" << act.has_enabled() << std::endl;
-
         // put some tokens on the input
         for (std::size_t t (0); t < NUM_TOKEN; ++t)
         {
-          std::string name = "token-" + ::util::show(t);
-          act.input().push_back( std::make_pair(token_t(name), pid_t(0))); //  ) );map_reduce.put_token(mr_inp, token_t(name));
+          act.input().push_back( std::make_pair(token_t("token-" + ::util::show(t)), pid_t(0)));
         }
       }
     }
