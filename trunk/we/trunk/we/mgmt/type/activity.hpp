a94fbda54ed27fa6c237f307e165b687a991b90e
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2010-04-14 15:22:53 +0000

    Stripped down activity
      + all references to transition category and casts removed
      + serialization code
      + output code
      - functions not implemented yet
    
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2302 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/we/trunk/we/mgmt/type/activity.hpp
+++ trunk/we/trunk/we/mgmt/type/activity.hpp
@@ -16,8 +16,8 @@
  * =====================================================================================
  */
 
-#ifndef WE_MGMT_BITS_ACTIVITY_HPP
-#define WE_MGMT_BITS_ACTIVITY_HPP 1
+#ifndef WE_MGMT_TYPE_ACTIVITY_HPP
+#define WE_MGMT_TYPE_ACTIVITY_HPP 1
 
 #include <vector>
 #include <boost/unordered_set.hpp>
@@ -26,67 +26,59 @@
 #include <boost/thread/shared_mutex.hpp>
 #include <boost/random.hpp>
 
-#include <we/mgmt/bits/pid_map_t.hpp>
+#include <we/type/id.hpp>
 
-namespace we { namespace mgmt { namespace detail {
-  template <typename Transition>
+namespace we { namespace mgmt { namespace type {
+  template <typename Transition, typename Token>
   struct activity_traits
   {
     typedef petri_net::pid_t pid_t;
-    typedef petri_net::tid_t tid_t;
     typedef Transition transition_type;
+    typedef Token token_type;
 
-    typedef typename transition_type::net_type net_type;
-    typedef typename transition_type::mod_type mod_type;
-    typedef typename transition_type::expr_type expr_type;
-    typedef typename transition_type::category_t transition_cat_t;
-
-    typedef typename net_type::token_type token_type;
     typedef std::pair<token_type, pid_t> token_on_place_t;
     typedef std::vector<token_on_place_t> input_t;
     typedef std::vector<token_on_place_t> output_t;
   };
 
-  template <typename Id, typename Transition, typename Traits = activity_traits<Transition> >
-  class activity
+  template <typename Transition, typename Token, typename Id = petri_net::pid_t, typename Traits = activity_traits<Transition, Token> >
+  class activity_t
   {
-    typedef activity<Id, Transition, Traits> this_type;
+    typedef activity_t<Transition, Token, Id, Traits> this_type;
 
   public:
-    typedef Id id_type;
     typedef Transition transition_type;
+    typedef Id id_type;
     typedef Traits traits_type;
-
-    typedef typename traits_type::net_type net_type;
-    typedef typename traits_type::mod_type mod_type;
-    typedef typename traits_type::expr_type expr_type;
 
     typedef typename traits_type::token_on_place_t token_on_place_t;
     typedef typename traits_type::input_t input_t;
     typedef typename traits_type::output_t output_t;
     typedef typename traits_type::pid_t pid_t;
-    typedef typename traits_type::tid_t tid_t;
 
     typedef boost::unordered_set<id_type> children_set_t;
     typedef boost::shared_lock<this_type> shared_lock_t;
     typedef boost::unique_lock<this_type> unique_lock_t;
 
-    // handle mapping to and from transition-local places
-    typedef traits::pid_map_traits<pid_t> pid_map_traits;
-    typedef typename pid_map_traits::type pid_map_t;
+    activity_t ()
+      : id_ (petri_net::traits::id_traits<id_type>::invalid())
+      , parent_ (petri_net::traits::id_traits<id_type>::invalid())
+    { }
 
     explicit
-    activity (const id_type id)
+    activity_t (const id_type id)
       : id_ (id)
+      , parent_ (petri_net::traits::id_traits<id_type>::invalid())
     { }
 
     template <typename T>
-    activity (const id_type id, const T & transition)
+    activity_t (const id_type id, const T & transition)
       : id_ (id)
+      , parent_ (petri_net::traits::id_traits<id_type>::invalid())
       , transition_ (transition)
     { }
 
-    activity (const this_type & other)
+    activity_t (const this_type & other)
       : id_ (other.id_)
       , parent_ (other.parent_)
       , flags_ (other.flags_)
@@ -96,7 +88,7 @@ namespace we { namespace mgmt { namespace detail {
       , output_ (other.output_)
     { }
 
-    activity & operator= (const this_type & other)
+    this_type & operator= (const this_type & other)
     {
       if (this != &other)
       {
@@ -118,19 +110,31 @@ namespace we { namespace mgmt { namespace detail {
       transition_ = t;
     }
 
+    // WORK TODO serialization
     struct flags_t
     {
-      bool suspended : 1;
-      bool cancelling : 1;
-      bool cancelled : 1;
-      bool failed : 1;
+      bool suspended;
+      bool cancelling;
+      bool cancelled;
+      bool failed;
 
       flags_t ()
-        : suspended(0)
-        , cancelling(0)
-        , cancelled(0)
-        , failed(0)
+        : suspended(false)
+        , cancelling(false)
+        , cancelled(false)
+        , failed(false)
       { }
+
+    private:
+      friend class boost::serialization::access;
+      template<class Archive>
+      void serialize (Archive & ar, const unsigned int)
+      {
+        ar & BOOST_SERIALIZATION_NVP(suspended);
+        ar & BOOST_SERIALIZATION_NVP(cancelling);
+        ar & BOOST_SERIALIZATION_NVP(cancelled);
+        ar & BOOST_SERIALIZATION_NVP(failed);
+      }
     };
 
     inline
@@ -179,113 +183,46 @@ namespace we { namespace mgmt { namespace detail {
     }
 
     this_type
-    extract(id_type id)
+    extract(const id_type id)
     {
-      unique_lock_t lock(const_cast<this_type&>(*this));
-
-      typename net_type::activity_t net_activity = transition_.template as<net_type>()->extract_activity_random(engine_);
-
-      this_type act = this->create_activity_from_net_activity (net_activity, id);
-
-      return act;
+      throw std::runtime_error("extract(): not implemented");
     }
 
     // TODO: work here
     template <typename Output>
     void
-    inject_results(const Output & o)
-    {
-      unique_lock_t lock(*this);
-
-      // the passed results are in "local view", we have to map them before we
-      // can inject
-      output_t mapped_output;
-      mapped_output.reserve (o.size());
-      map_from_local (o.begin(), o.end(), std::back_inserter(mapped_output));
-
-      if (transition_.is_net())
-      {
-        transition_. template as<net_type>()->inject_activity_result (mapped_output);
-      }
-      else
-      {
-        output_ = mapped_output;
-      }
-    }
+    inject_results(const Output &)
+    { }
 
     template <typename Activity>
     void
     child_finished (const Activity & act)
     {
-      unique_lock_t lock(*this);
-
-      output_t mapped_output;
-      mapped_output.reserve (act.output().size());
-      act.map_from_local (act.output().begin(), act.output().end(), std::back_inserter(mapped_output));
-
-      transition_. template as<net_type>()->inject_activity_result (mapped_output);
-
+      // WORK TODO
       unlink_child (act);
     }
 
     void prepare_input()
     {
-      unique_lock_t lock(*this);
-      if (transition_.is_net())
-      {
-        for (typename input_t::const_iterator it = input_.begin(); it != input_.end(); ++it)
-        {
-          typename pid_map_traits::result_type map_result =
-            pid_map_traits::map_to( transition_.i_mapping, it->second );
-          // should always be safe!
-          if (map_result.second)
-          {
-            transition_.template as<net_type>()->put_token( map_result.first, it->first );
-            std::cerr << "\tmapped token " << it->first << " on outer place " << it->second << " to inner " << map_result.first << std::endl;
-          }
-        }
-      }
-      else
-      {
-        // nothing to do (TODO?)
-      }
     }
 
     template <typename Context>
     void execute ( Context & )
-    {
-      if (transition_.is_expr())
-      {
-        std::cerr << "executing expression" << std::endl;
-      }
-    }
+    { }
 
     bool
     done (void) const
     {
+      // WORK TODO
       shared_lock_t lock(const_cast<this_type&>(*this));
-      if (transition_.is_net())
-      {
-        return (this->children_.empty()) && ( ! has_enabled() );
-      }
-      else
-      {
-        return ! output_.empty();
-      }
+      return (this->children_.empty()) && ( ! has_enabled());
     }
 
     bool
     has_enabled (void) const
     {
       shared_lock_t lock(const_cast<this_type&>(*this));
-      if (transition_.is_net())
-      {
-        return ! (transition_.template as<net_type>()->enabled_transitions().empty());
-      }
-      else
-      {
-        return false;
-      }
+      return false;
     }
 
     const input_t & input() const
@@ -317,10 +254,7 @@ namespace we { namespace mgmt { namespace detail {
     num_enabled (void) const
     {
       shared_lock_t lock(const_cast<this_type&>(*this));
-      if (transition_.is_net())
-        return (transition_.template as<net_type>()->enabled_transitions().size());
-      else
-        return 0;
+      return 0;
     }
 
     // **********************************
@@ -381,59 +315,17 @@ namespace we { namespace mgmt { namespace detail {
     }
 
   private:
-    template <typename Iter, typename OutputIterator>
-    void map_from_local(Iter begin, Iter end, OutputIterator dst) const
-    {
-      for (Iter it (begin); it != end; ++it)
-      {
-        typename pid_map_traits::result_type map_result = 
-          pid_map_traits::map_from( transition_.o_mapping, it->second );
-        if (map_result.second) // mapped
-        {
-          *dst++ = token_on_place_t (it->first, map_result.first);
-        }
-      }
-    }
-
-    template <typename Iter, typename OutputIterator>
-    void map_to_local(Iter begin, Iter end, OutputIterator dst) const
-    {
-      for (Iter it (begin); it != end; ++it)
-      {
-        typename pid_map_traits::result_type map_result = 
-          pid_map_traits::map_to( transition_.i_mapping, it->second );
-        if (map_result.second) // mapped
-        {
-          *dst++ = token_on_place_t (it->first, map_result.first);
-        }
-      }
-    }
-
     template <typename NetActivity>
     this_type
     create_activity_from_net_activity (NetActivity const & net_act, const id_type id)
     {
       this_type act(id);
-      this->link_with_child(act);
-      typename net_type::transition_type trans = transition_.template as<net_type>()->get_transition (net_act.tid);
-      act.assign (trans);
-      act.input_.reserve (net_act.input.size());
-
-      for (typename net_type::input_t::const_iterator it = net_act.input.begin(); it != net_act.input.end(); ++it)
-      {
-        typename pid_map_traits::result_type map_result = 
-          pid_map_traits::map_to( trans.i_mapping, it->second.first );
-        if (map_result.second) // mapped
-        {
-          act.input_.push_back ( token_on_place_t(it->first, map_result.first) );
-        }
-      }
-
+      this->link_child(act);
       return act;
     }
 
     template <typename Activity>
-    void link_with_child(Activity & act)
+    void link_child(Activity & act)
     {
       act.parent_ = this->id_;
       this->children_.insert( act.id_ );
@@ -447,7 +339,7 @@ namespace we { namespace mgmt { namespace detail {
 
     friend class boost::serialization::access;
     template<class Archive>
-    void serialize (Archive & ar, const unsigned int version)
+    void serialize (Archive & ar, const unsigned int)
     {
       ar & BOOST_SERIALIZATION_NVP(id_);
       ar & BOOST_SERIALIZATION_NVP(parent_);
@@ -472,6 +364,27 @@ namespace we { namespace mgmt { namespace detail {
 
     boost::mt19937 engine_;
   };
+
+
+  template <typename Transition, typename Token, typename Id, typename Traits>
+  std::ostream & operator << ( std::ostream & os
+                             , const activity_t<Transition, Token, Id, Traits> & act
+                             )
+  {
+    os << "{";
+      os << "act, "
+         << act.id()
+         << ", "
+         << "flags" // TODO act.flags()
+         << ", "
+         << act.transition()
+         << ", "
+         << "input" // TODO input
+         << "output" // TODO output
+      ;
+    os << "}";
+    return os;
+  }
 }}}
 
 #endif
