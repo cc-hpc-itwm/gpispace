c84a8bcfc560027802f66402ac3c94751deee82a
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2010-04-14 15:22:55 +0000

    added activity test
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2303 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/we/trunk/tests/test_activity.cpp
@@ -0,0 +1,120 @@
+#include <sstream>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
+#include <we/net.hpp>
+#include <we/type/transition.hpp>
+#include <we/type/place.hpp>
+#include <we/type/token.hpp>
+#include <we/mgmt/type/activity.hpp>
+
+using petri_net::connection_t;
+using petri_net::PT;
+using petri_net::PT_READ;
+using petri_net::TP;
+
+typedef place::type place_t;
+typedef token::type token_t;
+typedef unsigned int edge_t;
+typedef we::type::transition_t<place_t, edge_t, token_t> transition_t;
+typedef petri_net::net<place_t, transition_t, edge_t, token_t> pnet_t;
+typedef we::mgmt::type::activity_t<transition_t, token_t> activity_t;
+
+int main (int, char **)
+{
+  // ************************************ //
+  pnet_t net("the inner net");
+
+  petri_net::pid_t pid_vid (net.add_place (place_t ("vid","long")));
+
+  signature::structured_t sig_store;
+
+  sig_store["bid"] = "long";
+  sig_store["seen"] = "bitset";
+
+  petri_net::pid_t pid_store (net.add_place (place::type("store", sig_store)));
+
+  transition_t trans_inner 
+    ( "trans_inner"
+    , transition_t::expr_type
+      ( "${store.seen} := bitset_insert (${store.seen}, ${vid}); \
+         ${store.bid}  := ${store.bid}                         ; \
+         ${pair.bid}   := ${store.bid}                         ; \
+         ${pair.vid}   := ${vid}                                 "
+      )
+    , "!bitset_is_element (${store.seen}, ${vid})"
+    , true
+    );
+
+  signature::structured_t sig_pair;
+
+  sig_pair["bid"] = "long";
+  sig_pair["vid"] = "long";
+
+  petri_net::pid_t pid_pair (net.add_place (place::type("pair", sig_pair)));
+
+  trans_inner.add_ports ()
+    ("vid","long",we::type::PORT_IN)
+    ("store",sig_store,we::type::PORT_IN_OUT)
+    ("pair",sig_pair,we::type::PORT_OUT)
+    ;
+
+  trans_inner.add_connections ()
+    (pid_vid,"vid")
+    (pid_store,"store")
+    ("pair",pid_pair)
+    ("store",pid_store)
+    ;
+
+  petri_net::tid_t tid (net.add_transition (trans_inner));
+
+  edge_t e (0);
+
+  net.add_edge (e++, connection_t (PT, tid, pid_store));
+  net.add_edge (e++, connection_t (TP, tid, pid_store));
+  net.add_edge (e++, connection_t (PT_READ, tid, pid_vid));
+  net.add_edge (e++, connection_t (TP, tid, pid_pair));
+  
+  token::put (net, pid_vid, literal::type(0L));
+
+  {
+    token::structured_t m;
+
+    m["bid"] = 0L;
+    m["seen"] = bitsetofint::type(0);
+
+    token::put (net, pid_store, m);
+  }
+  // ************************************ //
+
+  transition_t tnet ("tnet", transition_t::net_type (net));
+  tnet.add_ports()
+    ("vid", "long", we::type::PORT_IN, pid_vid)
+    ("store", sig_store, we::type::PORT_IN_OUT, pid_store)
+    ("pair", sig_pair, we::type::PORT_OUT, pid_pair)
+  ;
+
+  activity_t act ( 0, tnet );
+
+  std::cout << "act (original)" << std::endl << act << std::endl;
+  {
+    std::ostringstream oss;
+    {
+      boost::archive::text_oarchive oa (oss, boost::archive::no_header);
+      oa << BOOST_SERIALIZATION_NVP (act);
+    }
+    std::cout << "act (serialized): " << oss.str() << std::endl;
+
+    activity_t act_d;
+    {
+      std::istringstream iss (oss.str());
+      {
+        boost::archive::text_iarchive ia (iss, boost::archive::no_header);
+        ia >> BOOST_SERIALIZATION_NVP (act_d);
+      }
+    }
+    std::cout << "act (deserialized): " << std::endl << act_d << std::endl;
+  }
+
+  return EXIT_SUCCESS;
+}
