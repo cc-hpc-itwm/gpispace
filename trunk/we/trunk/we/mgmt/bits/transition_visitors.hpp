bef5f80f3f6ff5318a6a20656c9afbae39a81e61
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2010-04-15 10:22:16 +0000

    added extract visitor
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2318 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/we/trunk/we/mgmt/bits/transition_visitors.hpp
+++ trunk/we/trunk/we/mgmt/bits/transition_visitors.hpp
@@ -23,6 +23,7 @@
 #include <string>
 
 #include <boost/variant.hpp>
+#include <we/util/warnings.hpp>
 #include <we/type/transition.hpp>
 
 namespace we { namespace mgmt { namespace visitor {
@@ -42,15 +43,65 @@ namespace we { namespace mgmt { namespace visitor {
   {
   public:
     template <typename Place, typename Trans, typename Edge, typename Token>
-    bool operator () (const petri_net::net<Place, Trans, Edge, Token> & net)
+    bool operator () (const petri_net::net<Place, Trans, Edge, Token> & net) const
     {
       return ! net.enabled_transitions().empty();
     }
 
     template <typename T>
-    bool operator () (const T &)
+    bool operator () (const T &) const
     {
       throw exception::operation_not_supported ("has_enabled");
+    }
+  };
+
+  template<typename Activity, typename Engine>
+  class activity_extractor
+    : public boost::static_visitor<Activity>
+  {
+  private:
+    Engine & engine_;
+  public:
+    explicit
+    activity_extractor (Engine & engine)
+      : engine_(engine)
+    {}
+
+    template <typename Place, typename Trans, typename Edge, typename Token>
+    Activity operator () (petri_net::net < Place
+                                         , Trans
+                                         , Edge
+                                         , Token
+                                         > & net)
+    {
+      typedef petri_net::net < Place
+                             , Trans
+                             , Edge
+                             , Token
+                             > pnet_t;
+
+      typedef typename pnet_t::activity_t activity_t;
+      typedef typename pnet_t::input_t input_t;
+
+      activity_t net_act = net.extract_activity_random (engine_);
+      Activity act = Activity (net.get_transition (net_act.tid));
+
+      // TODO beautify this
+      for (typename input_t::const_iterator inp (net_act.input.begin()); inp != net_act.input.end(); ++inp)
+      {
+        act.input ().push_back ( std::make_pair ( inp->first, inp->second.first) );
+      }
+      return act;
+    }
+
+    Activity operator () (we::type::module_call_t &)
+    {
+      throw exception::operation_not_supported ("activity_extractor (module_call)");
+    }
+
+    Activity operator () (we::type::expression_t &)
+    {
+      throw exception::operation_not_supported ("activity_extractor (expression)");
     }
   };
 }}}
