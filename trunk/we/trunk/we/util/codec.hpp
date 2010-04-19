e774c1fb016ded0cc7e263cfbd85f9f67ab0e708
petry <petry@c3adf61f-8968-0410-94d0-83abfdf586cf>
2010-04-19 22:54:53 +0000

    added convenience functions to serialize/deserialize
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2344 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/we/trunk/we/util/codec.hpp
@@ -0,0 +1,83 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  codec.hpp
+ *
+ *    Description:  encode and decode functions
+ *
+ *        Version:  1.0
+ *        Created:  04/19/2010 09:58:05 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Alexander Petry (petry), alexander.petry@itwm.fraunhofer.de
+ *        Company:  Fraunhofer ITWM
+ *
+ * =====================================================================================
+ */
+
+#ifndef WE_UTIL_CODEC_HPP
+#define WE_UTIL_CODEC_HPP 1
+
+#include <sstream>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
+namespace we { namespace util {
+
+  template < typename OA = boost::archive::text_oarchive
+           , typename IA = boost::archive::text_iarchive
+           >
+  struct codec
+  {
+    typedef OA oarchive;
+    typedef IA iarchive;
+
+    template <typename T>
+    static void encode (std::ostream & s, const T & t)
+    {
+      oarchive ar ( s, boost::archive::no_header );
+      ar << BOOST_SERIALIZATION_NVP (t);
+    }
+
+    template <typename T>
+    static std::string encode (const T & t)
+    {
+      std::ostringstream oss;
+      encode (oss, t);
+      return oss.str();
+    }
+
+    template <typename T>
+    static void decode (std::istream & s, T & t)
+    {
+      iarchive ar ( s, boost::archive::no_header );
+      ar >> BOOST_SERIALIZATION_NVP (t);
+    }
+
+    template <typename T>
+    static void decode (const std::string & s, T & t)
+    {
+      std::istringstream iss (s);
+      decode (iss, t);
+    }
+
+    template <typename T>
+    static T decode (std::istream & s)
+    {
+      T t;
+      decode (s, t);
+      return t;
+    }
+
+    template <typename T>
+    static T decode (const std::string & s)
+    {
+      T t;
+      decode (s, t);
+      return t;
+    }
+  };
+}}
+
+#endif
