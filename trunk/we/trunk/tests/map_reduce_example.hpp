8bc318f569a02cadb8660e784b091d916421e164
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-04-27 23:01:45 +0000

    added generator for map_reduce example network
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2450 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/we/trunk/tests/map_reduce_example.hpp
@@ -0,0 +1,18 @@
+#ifndef WE_TESTS_MAP_REDUCE_EXAMPLE_HPP
+#define WE_TESTS_MAP_REDUCE_EXAMPLE_HPP 1
+
+namespace we {
+  namespace tests {
+    template <typename Activity, typename Transition>
+    struct map_reduce
+    {
+      static Activity generate()
+      {
+        Activity act;
+        return act;
+      }
+    };
+  }
+}
+
+#endif
