876a5da643216bf5179384c1e555d86df27716a7
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-04-27 23:02:14 +0000

    added direction tags
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2455 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/we/trunk/we/type/port.hpp
+++ trunk/we/trunk/we/type/port.hpp
@@ -1,7 +1,7 @@
 /*
  * =====================================================================================
  *
- *       Filename:  port.hpp
+ *       filename:  port.hpp
  *
  *    Description:  port description of a transition
  *
@@ -24,92 +24,158 @@
 #include <boost/serialization/nvp.hpp>
 #include <we/type/id.hpp>
 
-namespace we { namespace type {
-  enum PortDirection
-  {
-    PORT_IN
-  , PORT_OUT
-  , PORT_IN_OUT
-  };
+namespace we
+{
+  namespace type {
 
-  template <typename SignatureType, typename IdType = petri_net::pid_t, typename Traits = petri_net::traits::id_traits<IdType> >
-  struct port
-  {
-  public:
-    typedef SignatureType sig_type;
-    typedef IdType pid_type;
-    typedef Traits pid_traits;
+    enum PortDirection
+      {
+        PORT_IN
+      , PORT_OUT
+      , PORT_IN_OUT
+      };
 
-    port ()
-      : name_("default")
-      , direction_(PORT_IN_OUT)
-      , associated_place_(pid_traits::invalid())
-    {}
+    namespace detail {
+      namespace direction
+      {
+        struct i_port
+        {
+          static const PortDirection value = PORT_IN;
+        };
 
-    template <typename Signature>
-    port (const std::string & name, PortDirection direction, const Signature & signature)
-      : name_(name)
-      , direction_(direction)
-      , signature_(signature)
-      , associated_place_(pid_traits::invalid())
-    {}
+        struct o_port
+        {
+          static const PortDirection value = PORT_OUT;
+        };
 
-    template <typename Signature, typename PlaceId>
-    port (const std::string & name, PortDirection direction, const Signature & signature, const PlaceId place_id)
-      : name_(name)
-      , direction_(direction)
-      , signature_(signature)
-      , associated_place_(place_id)
-    {}
+        struct io_port
+        {
+          static const PortDirection value = PORT_IN_OUT;
+        };
 
-    const std::string & name() const { return name_; }
-    PortDirection direction() const { return direction_; }
-    const sig_type & signature() const { return signature_; }
-    const pid_type & associated_place() const { return associated_place_; }
+        template <int Dir>
+        struct tag {};
 
-    inline bool is_input (void) const { return direction_ == PORT_IN || direction_ == PORT_IN_OUT; }
-    inline bool is_output (void) const { return direction_ == PORT_OUT || direction_ == PORT_IN_OUT; }
-    inline bool has_associated_place (void) const { return associated_place_ != pid_traits::invalid(); }
-  private:
-    std::string name_;
-    PortDirection direction_;
-    sig_type signature_;
-    pid_type associated_place_; //! associated to a place within a network, only reasonable for transitions with a subnet
+        template <>
+        struct tag<PORT_IN>
+        {
+          typedef i_port type;
+        };
 
-    friend class boost::serialization::access;
-    template<typename Archive>
-    void serialize (Archive & ar, const unsigned int)
-    {
-      ar & BOOST_SERIALIZATION_NVP(name_);
-      ar & BOOST_SERIALIZATION_NVP(direction_);
-      ar & BOOST_SERIALIZATION_NVP(signature_);
-      ar & BOOST_SERIALIZATION_NVP(associated_place_);
+        template <>
+        struct tag<PORT_OUT>
+        {
+          typedef o_port type;
+        };
+
+        template <>
+        struct tag<PORT_IN_OUT>
+        {
+          typedef io_port type;
+        };
+      }
     }
-  };
+    template < typename SignatureType
+             , typename IdType = petri_net::pid_t
+             , typename Traits = petri_net::traits::id_traits<IdType>
+             >
+    struct port
+    {
+    public:
+      typedef SignatureType sig_type;
+      typedef IdType pid_type;
+      typedef Traits pid_traits;
 
-  template <typename T, typename I>
-  std::ostream & operator << ( std::ostream & os, const port <T, I> & p )
-  {
-    os << "{port, "
-       << (p.direction() == PORT_IN ? "in" : (p.direction() == PORT_OUT ? "out" : "inout"))
-       << ", "
-       << p.name()
-       << ", "
-       << p.signature()
-       << ", "
-       ;
+      typedef detail::direction::i_port I_PORT;
+      typedef detail::direction::o_port O_PORT;
+      typedef detail::direction::io_port IO_PORT;
 
-       if (p.associated_place() == port<T,I>::pid_traits::invalid())
-       {
-          os << "n/a";
-       }
-       else
-       {
-          os << p.associated_place();
-       }
-    os << "}";
-    return os;
+      port ()
+        : name_("default")
+        , direction_(IO_PORT::value)
+        , associated_place_(pid_traits::invalid())
+      {}
+
+      template <typename Signature>
+      port (const std::string & name, PortDirection direction, const Signature & signature)
+        : name_(name)
+        , direction_(direction)
+        , signature_(signature)
+        , associated_place_(pid_traits::invalid())
+      {}
+
+      template <typename Signature, typename PlaceId>
+      port (const std::string & name, PortDirection direction, const Signature & signature, const PlaceId place_id)
+        : name_(name)
+        , direction_(direction)
+        , signature_(signature)
+        , associated_place_(place_id)
+      {}
+
+      template <typename Signature, typename Dir>
+      static
+      port create ( const std::string & name, const Signature & signature )
+      {
+        return port<Signature, IdType, Traits> ( name, Dir::value, signature );
+      }
+
+      template <typename Signature, typename PlaceId, typename Dir>
+      static
+      port create ( const std::string & name, const Signature & signature, const PlaceId place_id )
+      {
+        return port<Signature, IdType, Traits> ( name, Dir::value, signature, place_id );
+      }
+
+      const std::string & name() const { return name_; }
+      PortDirection direction() const { return direction_; }
+      const sig_type & signature() const { return signature_; }
+      const pid_type & associated_place() const { return associated_place_; }
+
+      inline bool is_input (void) const { return direction_ == PORT_IN || direction_ == PORT_IN_OUT; }
+      inline bool is_output (void) const { return direction_ == PORT_OUT || direction_ == PORT_IN_OUT; }
+      inline bool has_associated_place (void) const { return associated_place_ != pid_traits::invalid(); }
+    private:
+      std::string name_;
+      PortDirection direction_;
+      sig_type signature_;
+      //! associated to a place within a network, only reasonable for transitions with a subnet
+      pid_type associated_place_;
+
+      friend class boost::serialization::access;
+      template<typename Archive>
+      void serialize (Archive & ar, const unsigned int)
+      {
+        ar & BOOST_SERIALIZATION_NVP(name_);
+        ar & BOOST_SERIALIZATION_NVP(direction_);
+        ar & BOOST_SERIALIZATION_NVP(signature_);
+        ar & BOOST_SERIALIZATION_NVP(associated_place_);
+      }
+    };
+
+    template <typename T, typename I>
+    std::ostream & operator << ( std::ostream & os, const port <T, I> & p )
+    {
+      os << "{port, "
+         << (p.direction() == PORT_IN ? "in" : (p.direction() == PORT_OUT ? "out" : "inout"))
+         << ", "
+         << p.name()
+         << ", "
+         << p.signature()
+         << ", "
+        ;
+
+      if (p.associated_place() == port<T,I>::pid_traits::invalid())
+      {
+        os << "n/a";
+      }
+      else
+      {
+        os << p.associated_place();
+      }
+      os << "}";
+      return os;
+    }
   }
-}}
+}
 
 #endif
