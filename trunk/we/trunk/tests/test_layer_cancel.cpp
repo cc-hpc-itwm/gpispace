5943440efff9c6c9c77c03b49e1382764f4beac8
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-05-15 11:04:51 +0000

    added test_layer_cancel to test cancellation
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2575 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/we/trunk/tests/test_layer_cancel.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <stdint.h>
+
+#include <we/we.hpp>
+#include <we/mgmt/layer.hpp>
+
+typedef uint64_t id_type;
+
+typedef we::mgmt::layer<id_type, we::activity_t> layer_t;
+
+static inline id_type generate_id ()
+{
+  static id_type id(0);
+  return ++id;
+}
+
+template <typename L>
+struct daemon_t
+{
+  daemon_t ()
+    : layer (this, &generate_id)
+  {}
+
+  void submit (const id_type & id, const std::string &enc)
+  {
+    std::cout << "submitted id = " << id << std::endl;
+    layer.print_statistics (std::cerr);
+    layer.failed (id, enc);
+  }
+
+  bool cancel (const id_type & id, const std::string &)
+  {
+    std::cout << "cancelling id = " << id << std::endl;
+    layer.cancelled (id);
+    return true;
+  }
+
+  bool finished (const id_type & id, const std::string &)
+  {
+    std::cout << "finished id = " << id << std::endl;
+    return false;
+  }
+
+  bool failed (const id_type & id, const std::string &)
+  {
+    std::cout << "failed id = " << id << std::endl;
+    return false;
+  }
+
+  bool cancelled (const id_type & id)
+  {
+    std::cout << "cancelled id = " << id << std::endl;
+    return false;
+  }
+
+  L layer;
+};
+
+int main ()
+{
+  daemon_t<layer_t> daemon;
+  layer_t & layer = daemon.layer;
+
+  {
+    we::transition_t mod_call
+      ( "module call"
+      , we::transition_t::mod_type ("m", "f")
+      );
+    we::activity_t act (mod_call);
+    layer.submit (generate_id(), layer_t::policy::codec::encode(act));
+
+    sleep (1);
+    layer.print_statistics (std::cerr);
+  }
+
+  {
+    we::transition_t expr
+      ( "expression"
+      , we::transition_t::expr_type ("${out} := 3L")
+      );
+    we::activity_t act (expr);
+    layer.submit (generate_id(), layer_t::policy::codec::encode(act));
+
+    sleep (1);
+    layer.print_statistics (std::cerr);
+  }
+
+  return EXIT_SUCCESS;
+}
