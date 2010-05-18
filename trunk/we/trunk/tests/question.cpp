b9aeadd22726e5ea2bf655fca138bf19f06e59ef
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-05-18 12:54:10 +0000

    fixed typo
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2629 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/we/trunk/tests/question.cpp
+++ trunk/we/trunk/tests/question.cpp
@@ -17,9 +17,9 @@ WE_MOD_INITIALIZE_START (question);
   std::cerr << "The answer to life, the universe and everything := " << the_answer << std::endl;
   WE_REGISTER_FUN (question);
 }
-WE_MOD_INITIALIZE_END (answer);
+WE_MOD_INITIALIZE_END (question);
 
-WE_MOD_FINALIZE_START (answer);
+WE_MOD_FINALIZE_START (question);
 {
 }
-WE_MOD_FINALIZE_END (answer);
+WE_MOD_FINALIZE_END (question);
