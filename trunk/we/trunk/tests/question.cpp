b49177a337ab6d9140d7962d20f7ccd993863f00
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-05-18 12:53:34 +0000

    added question() method
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2613 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/we/trunk/tests/question.cpp
+++ trunk/we/trunk/tests/question.cpp
@@ -3,10 +3,19 @@
 
 #include "answer.hpp"
 
+static void question (void *, const we::loader::input_t & input, we::loader::output_t & output)
+{
+  std::cerr << "input := " << input << std::endl;
+  we::loader::put_output (output, "out", (long)get_answer());
+  std::cerr << "answer := " << get_answer() << std::endl;
+}
+
+
 WE_MOD_INITIALIZE_START (question);
 {
   std::cerr << "The answer to life, the universe and everything := " << get_answer() << std::endl;
   std::cerr << "The answer to life, the universe and everything := " << the_answer << std::endl;
+  WE_REGISTER_FUN (question);
 }
 WE_MOD_INITIALIZE_END (answer);
 
