115b5b57fcd71802f02756f552e3c49c719670cf
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-05-18 09:55:10 +0000

    moved exceptions to correct namespace
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2608 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/we/trunk/we/loader/exceptions.hpp
+++ trunk/we/trunk/we/loader/exceptions.hpp
@@ -1,106 +1,109 @@
-#ifndef SDPA_MODULES_EXCEPTIONS_HPP
-#define SDPA_MODULES_EXCEPTIONS_HPP 1
+#ifndef WE_LOADER_EXCEPTIONS_HPP
+#define WE_LOADER_EXCEPTIONS_HPP 1
 
-#include <sdpa/SDPAException.hpp>
+#include <stdexcept>
 
-namespace sdpa {
-namespace modules {
-  /**
-    All exceptions related to a module.
+namespace we
+{
+  namespace loader
+  {
+    /**
+       All exceptions related to a module.
     */
-  class ModuleException : public sdpa::SDPAException {
-  public:
-    ModuleException(const std::string &a_reason, const std::string &a_module)
-    : sdpa::SDPAException(a_reason), module_(a_module) {}
+    class ModuleException : public std::runtime_error {
+    public:
+      ModuleException(const std::string &a_reason, const std::string &a_module)
+        : std::runtime_error(a_reason), module_(a_module) {}
 
-    virtual ~ModuleException() throw() {}
-    const std::string &module() const { return module_; }
-  private:
-    std::string module_;
-  };
+      virtual ~ModuleException() throw() {}
+      const std::string &module() const { return module_; }
+    private:
+      std::string module_;
+    };
 
-  class ModuleNotLoaded : public ModuleException {
-  public:
-    explicit
-    ModuleNotLoaded(const std::string &a_module)
-      : ModuleException("the desired module is not loaded: " + a_module, a_module) {}
-    virtual ~ModuleNotLoaded() throw() {}
-  };
+    class ModuleNotLoaded : public ModuleException {
+    public:
+      explicit
+      ModuleNotLoaded(const std::string &a_module)
+        : ModuleException("the desired module is not loaded: " + a_module, a_module) {}
+      virtual ~ModuleNotLoaded() throw() {}
+    };
 
-  class ModuleLoadFailed : public ModuleException {
-  public:
-    explicit
-    ModuleLoadFailed(const std::string &a_reason, const std::string &a_module, const std::string &a_file)
-      : ModuleException(a_reason, a_module), file_(a_file) {}
-    virtual ~ModuleLoadFailed() throw() {}
+    class ModuleLoadFailed : public ModuleException {
+    public:
+      explicit
+      ModuleLoadFailed(const std::string &a_reason, const std::string &a_module, const std::string &a_file)
+        : ModuleException(a_reason, a_module), file_(a_file) {}
+      virtual ~ModuleLoadFailed() throw() {}
 
-    const std::string &file() const { return file_; }
-  private:
-    std::string file_;
-  };
+      const std::string &file() const { return file_; }
+    private:
+      std::string file_;
+    };
 
-  /**
-    All exceptions related to a specific function.
+    /**
+       All exceptions related to a specific function.
     */
-  class FunctionException : public ModuleException {
-  public:
-    FunctionException(const std::string &a_reason, const std::string &a_module, const std::string &a_function)
-      : ModuleException(a_reason, a_module), function_(a_function) {}
+    class FunctionException : public ModuleException {
+    public:
+      FunctionException(const std::string &a_reason, const std::string &a_module, const std::string &a_function)
+        : ModuleException(a_reason, a_module), function_(a_function) {}
 
-    virtual ~FunctionException() throw() {}
+      virtual ~FunctionException() throw() {}
 
-    const std::string &function() const { return function_; }
-  private:
-    std::string function_;
-  };
+      const std::string &function() const { return function_; }
+    private:
+      std::string function_;
+    };
 
-  class FunctionNotFound : public FunctionException {
-  public:
-    FunctionNotFound(const std::string &a_module, const std::string &a_function)
-      : FunctionException("function could not be found: "+a_function, a_module, a_function) {}
-    virtual ~FunctionNotFound() throw() {}
-  };
+    class FunctionNotFound : public FunctionException {
+    public:
+      FunctionNotFound(const std::string &a_module, const std::string &a_function)
+        : FunctionException("function could not be found: "+a_function, a_module, a_function) {}
+      virtual ~FunctionNotFound() throw() {}
+    };
 
-  class DuplicateFunction : public FunctionException {
-  public:
-    DuplicateFunction(const std::string &a_module, const std::string &a_function)
-      : FunctionException("duplicate function detected: " + a_module+"."+a_function, a_module, a_function) {}
-    virtual ~DuplicateFunction() throw() {}
-  };
+    class DuplicateFunction : public FunctionException {
+    public:
+      DuplicateFunction(const std::string &a_module, const std::string &a_function)
+        : FunctionException("duplicate function detected: " + a_module+"."+a_function, a_module, a_function) {}
+      virtual ~DuplicateFunction() throw() {}
+    };
 
-  class MissingFunctionArgument : public FunctionException {
-  public:
-    MissingFunctionArgument(const std::string &a_module
-                          , const std::string &a_function
-                          , const std::string &expected_arguments)
-      : FunctionException("missing argument(s): " + expected_arguments, a_module, a_function)
-      , arguments_(expected_arguments)
-    { }
+    class MissingFunctionArgument : public FunctionException {
+    public:
+      MissingFunctionArgument(const std::string &a_module
+                             , const std::string &a_function
+                             , const std::string &expected_arguments)
+        : FunctionException("missing argument(s): " + expected_arguments, a_module, a_function)
+        , arguments_(expected_arguments)
+      { }
 
-    virtual ~MissingFunctionArgument() throw() {}
+      virtual ~MissingFunctionArgument() throw() {}
 
-    const std::string &arguments() const { return arguments_; }
-  private:
-    std::string arguments_;
-  };
+      const std::string &arguments() const { return arguments_; }
+    private:
+      std::string arguments_;
+    };
 
-  class BadFunctionArgument : public FunctionException {
-  public:
-    BadFunctionArgument(const std::string &a_module, const std::string &a_function,
-                        const std::string &a_expected, const std::string &a_actual,
-                        const std::string &a_value="")
-      : FunctionException("bad function argument", a_module, a_function),
-        expected_(a_expected), actual_(a_actual), value_(a_value) {}
-    virtual ~BadFunctionArgument() throw() {}
+    class BadFunctionArgument : public FunctionException {
+    public:
+      BadFunctionArgument(const std::string &a_module, const std::string &a_function,
+                         const std::string &a_expected, const std::string &a_actual,
+                         const std::string &a_value="")
+        : FunctionException("bad function argument", a_module, a_function),
+          expected_(a_expected), actual_(a_actual), value_(a_value) {}
+      virtual ~BadFunctionArgument() throw() {}
 
-    const std::string &expected() const { return expected_; }
-    const std::string &actual() const { return actual_; }
-    const std::string &value() const { return value_; }
-  private:
-    std::string expected_;
-    std::string actual_;
-    std::string value_;
-  };
-}}
+      const std::string &expected() const { return expected_; }
+      const std::string &actual() const { return actual_; }
+      const std::string &value() const { return value_; }
+    private:
+      std::string expected_;
+      std::string actual_;
+      std::string value_;
+    };
+  }
+}
 
 #endif
