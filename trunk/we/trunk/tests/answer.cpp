39abfc4862397954793f327aedc8abc0effb6b28
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-05-18 12:53:47 +0000

    removed allocation
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2619 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/we/trunk/tests/answer.cpp
+++ trunk/we/trunk/tests/answer.cpp
@@ -12,15 +12,14 @@ int get_answer ()
 
 static void answer (void * state, const we::loader::input_t & input, we::loader::output_t & output)
 {
-  int ans = *((int*)(state));
-  std::cerr << "state := " << ans << std::endl;
+  std::cerr << "state := " << state << std::endl;
   std::cerr << "input := " << input << std::endl;
   we::loader::put_output (output, "out", 42L);
 }
 
 WE_MOD_INITIALIZE_START (answer);
 {
-  mod->state( new int(42) );
+  mod->state((void*)(0x42));
   the_answer = 42;
   WE_REGISTER_FUN (answer);
 }
@@ -28,7 +27,5 @@ WE_MOD_INITIALIZE_END (answer);
 
 WE_MOD_FINALIZE_START (answer);
 {
-  int * state = (int*)(mod->state(0));
-  if (state) delete state;
 }
 WE_MOD_FINALIZE_END (answer);
