6107b9b84c3dca2d620400581d0827f58ccd3f19
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-05-18 20:12:05 +0000

    added a generic module loader test that calls selftest() in each loaded module
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2640 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/we/trunk/tests/test_load_module.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <list>
+#include <string>
+
+#include <we/util/show.hpp>
+#include <we/loader/loader.hpp>
+
+int main (int ac, char **av)
+{
+  we::loader::loader loader;
+
+  if (ac < 2)
+  {
+    std::cerr << "usage: " << av[0] << " module..." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::list<std::string> failed;
+  for (int i = 1; i < ac; ++i)
+  {
+    const std::string mod_name ("mod-" + ::util::show(i));
+    try
+    {
+      loader.load (mod_name, av[i]);
+    }
+    catch (const std::exception & ex)
+    {
+      std::cerr << "load of module " << av[i] << " failed: " << ex.what() << std::endl;
+      failed.push_back (av[i]);
+      continue;
+    }
+
+    try
+    {
+      we::loader::input_t inp;
+      we::loader::output_t out;
+
+      loader[mod_name] ("selftest", inp, out);
+    }
+    catch (const std::exception &ex)
+    {
+      std::cerr << "could not run module self-test: " << ex.what() << std::endl;
+      failed.push_back (av[i]);
+    }
+  }
+
+  std::cerr << loader << std::endl;
+
+  if (failed.size())
+  {
+    std::cerr << "The following modules could not be loaded/tested:" << std::endl;
+    for (std::list<std::string>::const_iterator m(failed.begin()); m != failed.end(); ++m)
+    {
+      std::cerr << "\t" << *m << std::endl;
+    }
+  }
+
+  return (failed.empty() ? EXIT_SUCCESS : EXIT_FAILURE);
+}
