8213bd312ee91172485440cb3b1d61923f2595f4
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-05-19 17:35:52 +0000

    FindFhgLog fixed to locate static and shared library
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2657 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/modules/trunk/FindFhgLog.cmake
+++ trunk/modules/trunk/FindFhgLog.cmake
@@ -5,14 +5,45 @@
 # * FHGLOG_LIBRARY The lib to link to (currently only a static unix lib) 
 # * FHGLOG_INCLUDE_DIR
 
-message(STATUS "FindFhglog check")
+if (NOT FhgLog_FIND_QUIETLY)
+  message(STATUS "FindFhgLog check")
+endif (NOT FhgLog_FIND_QUIETLY)
 
 if(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_CURRENT_SOURCE_DIR})
-  include(FindPackageHelper)
-  check_package(FHGLOG fhglog/fhglog.hpp fhglog 1.0)
+  #  include(FindPackageHelper)
+  #  check_package(FhgLog fhglog/fhglog.hpp fhglog 1.0)
+
+  find_path (FhgLog_INCLUDE_DIR
+	NAMES "fhglog/fhglog.hpp"
+	HINTS ${FHGLOG_HOME} ENV FHGLOG_HOME
+	PATH_SUFFIXES include
+  )
+
+  find_library (FhgLog_LIBRARY
+	NAMES libfhglog.a
+	HINTS ${FHGLOG_HOME} ENV FHGLOG_HOME
+	PATH_SUFFIXES lib
+  )
+  find_library (FhgLog_LIBRARY_SHARED
+	NAMES libfhglog.so
+	HINTS ${FHGLOG_HOME} ENV FHGLOG_HOME
+	PATH_SUFFIXES lib
+  )
+
+  if (FhgLog_INCLUDE_DIR AND FhgLog_LIBRARY)
+	set (FhgLog_FOUND TRUE)
+	if (NOT FhgLog_FIND_QUIETLY)
+	  message (STATUS "Found FhgLog headers in ${FhgLog_INCLUDE_DIR} and libraries ${FhgLog_LIBRARY} ${FhgLog_LIBRARY_SHARED}")
+	endif (NOT FhgLog_FIND_QUIETLY)
+  else (FhgLog_INCLUDE_DIR AND FhgLog_LIBRARY)
+	if (FhgLog_FIND_REQUIRED)
+	  message (FATAL_ERROR "FhgLog could not be found!")
+	endif (FhgLog_FIND_REQUIRED)
+  endif (FhgLog_INCLUDE_DIR AND FhgLog_LIBRARY)
+
 else(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_CURRENT_SOURCE_DIR})
-  set(FHGLOG_FOUND true)
-  set(FHGLOG_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/fhglog ${CMAKE_BINARY_DIR}/fhglog)
-  set(FHGLOG_LIBRARY_DIR "")
-  set(FHGLOG_LIBRARY fhglog)
+  set(FhgLog_FOUND true)
+  set(FhgLog_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/fhglog ${CMAKE_BINARY_DIR}/fhglog)
+  set(FhgLog_LIBRARY_DIR "")
+  set(FhgLog_LIBRARY fhglog)
 endif(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_CURRENT_SOURCE_DIR})
