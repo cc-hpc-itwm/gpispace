98acaa56dc95f29820323ed8ed248efb027d83f8
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-05-20 16:19:22 +0000

    added boost program options
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2667 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/we/trunk/kdm/simple_generator.cpp
+++ trunk/we/trunk/kdm/simple_generator.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
+#include <boost/program_options.hpp>
 
 #include <we/we.hpp>
 #include "simple_generator.hpp"
@@ -13,8 +14,44 @@
 #include "module.hpp"
 #include "context.hpp"
 
+namespace po = boost::program_options;
+
 int main (int argc, char ** argv)
 {
+  po::options_description desc("options");
+
+  std::string cfg_file;
+  std::string mod_path;
+  std::vector<std::string> mods_to_load;
+
+  desc.add_options()
+    ("help", "this message")
+    ("cfg", po::value<std::string>(&cfg_file)->default_value("/scratch/KDM/KDM.conf"), "config file")
+    ("mod-path", po::value<std::string>(&mod_path)->default_value("/scratch/KDM/"), "modules")
+    ("load", po::value<std::vector<std::string> >(&mods_to_load), "modules to load a priori")
+    ;
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
+
+  if (vm.count("help"))
+    {
+      std::cout << desc << std::endl;
+      return EXIT_SUCCESS;
+    }
+
+  we::loader::loader loader;
+
+  loader.append_search_path (mod_path);
+  for ( std::vector<std::string>::const_iterator m (mods_to_load.begin())
+      ; m != mods_to_load.end()
+      ; ++m
+      )
+  {
+    loader.load (*m, *m);
+  }
+
   we::transition_t simple_trans (kdm::kdm<we::activity_t>::generate());
 
   we::activity_t act ( simple_trans );
@@ -22,9 +59,9 @@ int main (int argc, char ** argv)
   act.input().push_back
     ( we::input_t::value_type
       ( we::token_t ( "config_file"
-                , literal::STRING
-                , std::string ((argc > 1) ? argv[1] : "/scratch/KDM.conf")
-                )
+                    , literal::STRING
+                    , cfg_file
+                    )
       , simple_trans.input_port_by_name ("config_file")
       )
     );
@@ -35,7 +72,6 @@ int main (int argc, char ** argv)
     ofs << we::util::text_codec::encode (act);
   }
 
-  we::loader::loader loader;
   struct exec_context ctxt (loader);
   act.execute (ctxt);
 
