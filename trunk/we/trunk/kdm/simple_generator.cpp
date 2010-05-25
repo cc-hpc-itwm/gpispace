0830b94df33b26c9d7dd0bbc5c52c5e575435ae8
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-05-25 23:08:44 +0000

    generator does not execute anymore, just generate the net and write to stdout
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2711 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/we/trunk/kdm/simple_generator.cpp
+++ trunk/we/trunk/kdm/simple_generator.cpp
@@ -8,11 +8,11 @@
 #include "simple_generator.hpp"
 
 // specific
-#include "kdm_simple.hpp"
+//#include "kdm_simple.hpp"
 
 // generic
-#include "module.hpp"
-#include "context.hpp"
+//#include "module.hpp"
+//#include "context.hpp"
 
 namespace po = boost::program_options;
 
@@ -21,14 +21,10 @@ int main (int argc, char ** argv)
   po::options_description desc("options");
 
   std::string cfg_file;
-  std::string mod_path;
-  std::vector<std::string> mods_to_load;
 
   desc.add_options()
     ("help", "this message")
-    ("cfg", po::value<std::string>(&cfg_file)->default_value("/scratch/KDM/KDM.conf"), "config file")
-    ("mod-path", po::value<std::string>(&mod_path)->default_value("/scratch/KDM/"), "modules")
-    ("load", po::value<std::vector<std::string> >(&mods_to_load), "modules to load a priori")
+    ("cfg", po::value<std::string>(&cfg_file)->default_value("/p/herc/itwm/hpc/soft/sdpa/sdpa/share/kdm.pnet"), "config file")
     ;
 
   po::variables_map vm;
@@ -37,20 +33,9 @@ int main (int argc, char ** argv)
 
   if (vm.count("help"))
     {
-      std::cout << desc << std::endl;
+      std::cerr << desc << std::endl;
       return EXIT_SUCCESS;
     }
-
-  we::loader::loader loader;
-
-  loader.append_search_path (mod_path);
-  for ( std::vector<std::string>::const_iterator m (mods_to_load.begin())
-      ; m != mods_to_load.end()
-      ; ++m
-      )
-  {
-    loader.load (*m, *m);
-  }
 
   we::transition_t simple_trans (kdm::kdm<we::activity_t>::generate());
 
@@ -67,23 +52,7 @@ int main (int argc, char ** argv)
     );
 
   // dump activity for test purposes
-  {
-    std::ofstream ofs ("kdm_simple.pnet");
-    ofs << we::util::text_codec::encode (act);
-  }
-
-  struct exec_context ctxt (loader);
-  act.execute (ctxt);
-
-  we::mgmt::type::detail::printer<we::activity_t, std::ostream> printer (act, std::cout);
-  printer << "output := "
-          << act.output()
-          << std::endl;
-
-  if ( act.output().empty() )
-  {
-    return EXIT_FAILURE;
-  }
-
+  std::cout << we::util::text_codec::encode (act);
+  
   return EXIT_SUCCESS;
 }
