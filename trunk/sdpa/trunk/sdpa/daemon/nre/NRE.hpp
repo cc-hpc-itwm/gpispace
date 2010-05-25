22447076a791736e2e96df7ce29487a8fead6a07
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-05-25 23:08:49 +0000

    added notification calls
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2714 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/sdpa/daemon/nre/NRE.hpp
+++ trunk/sdpa/trunk/sdpa/daemon/nre/NRE.hpp
@@ -28,6 +28,8 @@
 
 #include <boost/pointer_cast.hpp>
 
+#include <we/we.hpp>
+
 using namespace std;
 
 
@@ -303,31 +305,36 @@ bool  NRE<T, U>::cancel(const id_type& activityId, const reason_type& reason )
 template <typename T, typename U>
 void NRE<T, U>::activityCreated( const id_type& id, const std::string& data )
 {
-	notifyObservers( NotificationEvent( id, data, NotificationEvent::STATE_CREATED) );
+	const std::string name (we::util::text_codec::decode<we::activity_t> (data).transition().name());
+	notifyObservers( NotificationEvent( id, name, NotificationEvent::STATE_CREATED) );
 }
 
 template <typename T, typename U>
 void NRE<T, U>::activityStarted( const id_type& id, const std::string& data )
 {
-	notifyObservers( NotificationEvent( id, data, NotificationEvent::STATE_STARTED) );
+	const std::string name (we::util::text_codec::decode<we::activity_t> (data).transition().name());
+	notifyObservers( NotificationEvent( id, name, NotificationEvent::STATE_STARTED) );
 }
 
 template <typename T, typename U>
 void NRE<T, U>::activityFinished( const id_type& id, const std::string& data )
 {
-	notifyObservers( NotificationEvent( id, data, NotificationEvent::STATE_FINISHED) );
+	const std::string name (we::util::text_codec::decode<we::activity_t> (data).transition().name());
+	notifyObservers( NotificationEvent( id, name, NotificationEvent::STATE_FINISHED) );
 }
 
 template <typename T, typename U>
 void NRE<T, U>::activityFailed( const id_type& id, const std::string& data )
 {
-	notifyObservers( NotificationEvent( id, data, NotificationEvent::STATE_FAILED) );
+	const std::string name (we::util::text_codec::decode<we::activity_t> (data).transition().name());
+	notifyObservers( NotificationEvent( id, name, NotificationEvent::STATE_FAILED) );
 }
 
 template <typename T, typename U>
 void NRE<T, U>::activityCancelled( const id_type& id, const std::string& data )
 {
-	notifyObservers( NotificationEvent( id, data, NotificationEvent::STATE_CANCELLED) );
+	const std::string name (we::util::text_codec::decode<we::activity_t> (data).transition().name());
+	notifyObservers( NotificationEvent( id, name, NotificationEvent::STATE_CANCELLED) );
 }
 
 template <typename T, typename U>
