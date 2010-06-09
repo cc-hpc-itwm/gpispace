ecb1af8d6f677c58dba113d01ea1ad676735f502
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2010-06-09 15:11:35 +0000

    the simple net prepared for parallel loadTT
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2734 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/we/trunk/kdm/simple_generator_parallel_loadTT.cpp
@@ -0,0 +1,58 @@
+#include <sstream>
+#include <fstream>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/program_options.hpp>
+
+#include <we/we.hpp>
+#include "simple_generator_parallel_loadTT.hpp"
+
+// specific
+//#include "kdm_simple.hpp"
+
+// generic
+//#include "module.hpp"
+//#include "context.hpp"
+
+namespace po = boost::program_options;
+
+int main (int argc, char ** argv)
+{
+  po::options_description desc("options");
+
+  std::string cfg_file;
+
+  desc.add_options()
+    ("help", "this message")
+    ("cfg", po::value<std::string>(&cfg_file)->default_value("/p/herc/itwm/hpc/soft/sdpa/sdpa/share/kdm.pnet"), "config file")
+    ;
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
+
+  if (vm.count("help"))
+    {
+      std::cerr << desc << std::endl;
+      return EXIT_SUCCESS;
+    }
+
+  we::transition_t simple_trans (kdm::kdm<we::activity_t>::generate());
+
+  we::activity_t act ( simple_trans );
+
+  act.input().push_back
+    ( we::input_t::value_type
+      ( we::token_t ( "config_file"
+                    , literal::STRING
+                    , cfg_file
+                    )
+      , simple_trans.input_port_by_name ("config_file")
+      )
+    );
+
+  // dump activity for test purposes
+  std::cout << we::util::text_codec::encode (act);
+  
+  return EXIT_SUCCESS;
+}
