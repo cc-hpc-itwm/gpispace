66f0027557c41ff02c07ff4a3718a8dcfe0424ec
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-06-23 08:35:19 +0000

    added prefrences
    From: Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2790 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/sdpa/types.hpp
+++ trunk/sdpa/trunk/sdpa/types.hpp
@@ -5,16 +5,32 @@
 #include <string>
 #include <list>
 #include <map>
+#include <set>
 
 #include <sdpa/JobId.hpp>
 
 namespace sdpa {
-  typedef sdpa::JobId job_id_t;
-  typedef std::string job_desc_t;
-  typedef std::string location_t;
-  typedef std::string worker_id_t;
-  typedef std::string status_t;
-  typedef std::string job_result_t;
+	typedef sdpa::JobId job_id_t;
+	typedef std::string job_desc_t;
+	typedef std::string location_t;
+	typedef std::string worker_id_t;
+	typedef std::string status_t;
+	typedef std::string job_result_t;
+	typedef std::list<int> list_of_ranks;
+	typedef std::set<int> set_of_ranks;
+
+	struct preferences_t
+	{
+		bool mandatory(){ return mandatory_; }
+		const list_of_ranks& ranks() { return ranks_; }
+		const set_of_ranks& excluded() { return excluded_ranks_; }
+	  private:
+		bool mandatory_;
+		list_of_ranks ranks_;
+		set_of_ranks  excluded_ranks_;
+	};
+
+	typedef std::map<sdpa::JobId, sdpa::preferences_t> preferences_map_t;
 }
 
 #endif
