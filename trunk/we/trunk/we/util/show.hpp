39a20339bcaa4e51f0ce2d4c4eee17427c113dae
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-06-29 14:50:00 +0000

    show is able to print std::pair
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2847 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/we/trunk/we/util/show.hpp
+++ trunk/we/trunk/we/util/show.hpp
@@ -20,25 +20,46 @@ namespace util
     return x;
   }
 
+  template <typename T1, typename T2>
+  inline std::string show (std::pair<T1, T2> const & p, const std::string & delimiter = ", ", const std::string & start_end = "()")
+  {
+    std::ostringstream s;
+    if (start_end.size() > 0)
+      s << start_end[0];
+
+    s << show (p.first);
+    s << delimiter;
+    s << show (p.second);
+
+    if (start_end.size() > 1)
+      s << start_end[1];
+    else if (start_end.size() > 0)
+      s << start_end[0];
+
+    return s.str();
+  }
+
   template <typename InputIterator>
   inline std::string show (InputIterator first, InputIterator last, const std::string & delimiter = ", ", const std::string & start_end = "[]")
   {
     InputIterator start (first);
     std::ostringstream s;
 
-    if (start_end.size() == 2)
+    if (start_end.size() > 0)
       s << start_end[0];
 
     while (first != last)
     {
       if (first != start)
         s << delimiter;
-      s << *first;
+      s << show(*first);
       ++first;
     }
 
-    if (start_end.size() == 2)
+    if (start_end.size() > 1)
       s << start_end[1];
+    else if (start_end.size() > 0)
+      s << start_end[0];
 
     return s.str();
   }
