95f81f39bda6535f5f070cfff7e793adc99aa63c
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-06-30 16:49:31 +0000

    reduced max queue size to 3
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2927 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/seda/trunk/seda/constants.cpp
+++ trunk/seda/trunk/seda/constants.cpp
@@ -25,7 +25,7 @@
 #include <limits>
 namespace seda {
     //    std::size_t SEDA_MAX_QUEUE_SIZE = std::numeric_limits<std::size_t>::max();
-    std::size_t SEDA_MAX_QUEUE_SIZE = 100;
+    std::size_t SEDA_MAX_QUEUE_SIZE = 3;
     unsigned long SEDA_DEFAULT_TIMEOUT = 500;
     const std::string SEDA_SYSTEM_STAGE_NAME = "seda.system.stage";
 }
