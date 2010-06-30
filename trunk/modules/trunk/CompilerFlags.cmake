c0e9a927a4ce1fcc12526ca6fc3054a46597b9dc
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-06-30 16:48:49 +0000

    added central cmake file that contains compiler flags
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2898 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/modules/trunk/CompilerFlags.cmake
@@ -0,0 +1,31 @@
+if (${CMAKE_BUILD_TYPE} MATCHES "Release")
+  add_definitions("-DNDEBUG")
+endif (${CMAKE_BUILD_TYPE} MATCHES "Release")
+
+if (${CMAKE_CXX_COMPILER_ID} MATCHES "GNU")
+  set(CMAKE_CXX_FLAGS "-W -Wall -Wextra -Wno-non-virtual-dtor -frepo -fno-implicit-templates")
+
+  # release flags
+  set(CMAKE_CXX_FLAGS_RELEASE "-O3")
+
+  # debug flags
+  set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g -ggdb -Wreturn-type -fno-omit-frame-pointer")
+	#    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wunused-variable -Wunused-parameter")
+	#    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wunused-function -Wunused")
+  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Woverloaded-virtual -Wno-system-headers")
+
+  # gprof and gcov support
+  set(CMAKE_CXX_FLAGS_PROFILE "-O0 -g -ggdb -Wreturn-type -Woverloaded-virtual")
+  set(CMAKE_CXX_FLAGS_PROFILE "${CMAKE_CXX_FLAGS_PROFILE} -Wno-system-headers -pg")
+  set(CMAKE_CXX_FLAGS_PROFILE "${CMAKE_CXX_FLAGS_PROFILE} -fprofile-arcs -ftest-coverage")
+endif (${CMAKE_CXX_COMPILER_ID} MATCHES "GNU")
+
+# TODO: we need to check the compiler here, gcc does not know about those flags, is this The Right Thing To Do (TM)?
+if (${CMAKE_CXX_COMPILER_ID} MATCHES "Intel")
+  set(CMAKE_CXX_FLAGS "-wd383 -wd981")
+  message(STATUS "compiler: ${CMAKE_CXX_COMPILER_MAJOR}.${CMAKE_CXX_COMPILER_MINOR}")
+  if (${CMAKE_CXX_COMPILER_MAJOR} GREATER 9)
+    message(STATUS "Warning: adding __aligned__=ignored to the list of definitions")
+    add_definitions("-D__aligned__=ignored")
+  endif (${CMAKE_CXX_COMPILER_MAJOR} GREATER 9)
+endif (${CMAKE_CXX_COMPILER_ID} MATCHES "Intel")
