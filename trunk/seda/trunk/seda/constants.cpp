626e9f3c22c772691724cb73bb8327c7bfa16edf
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-07-01 13:37:12 +0000

    increased max queue size to 16
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2961 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/seda/trunk/seda/constants.cpp
+++ trunk/seda/trunk/seda/constants.cpp
@@ -24,8 +24,7 @@
 
 #include <limits>
 namespace seda {
-    //    std::size_t SEDA_MAX_QUEUE_SIZE = std::numeric_limits<std::size_t>::max();
-    std::size_t SEDA_MAX_QUEUE_SIZE = 3;
+    std::size_t SEDA_MAX_QUEUE_SIZE = 16;
     unsigned long SEDA_DEFAULT_TIMEOUT = 500;
     const std::string SEDA_SYSTEM_STAGE_NAME = "seda.system.stage";
 }
