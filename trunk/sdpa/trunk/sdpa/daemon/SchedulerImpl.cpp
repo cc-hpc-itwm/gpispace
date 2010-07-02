409dd9a5301da07eb193df1ab0587e84c3c85b2c
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2010-07-02 13:47:46 +0000

    commented log-if
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@2984 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/sdpa/daemon/SchedulerImpl.cpp
+++ trunk/sdpa/trunk/sdpa/daemon/SchedulerImpl.cpp
@@ -87,12 +87,12 @@ void SchedulerImpl::delWorker( const Worker::worker_id_t& worker_id ) throw (Wor
 		re_schedule( &pWorker->submitted() );
 		re_schedule( &pWorker->pending() );
 
-		 LOG_IF( FATAL
+		/* LOG_IF( FATAL
 			  ,  w->second->pending().size()
 			  || w->second->submitted().size()
 			  || w->second->acknowledged().size()
 			  , "tried to remove worker " << workerId << " while there are still jobs scheduled!"
-			  );
+			  );*/
 
 		pWorker.reset();
 	}
