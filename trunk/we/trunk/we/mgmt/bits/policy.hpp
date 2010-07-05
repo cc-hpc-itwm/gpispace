73bbf1bdcce33240f1e7d31efd434ecfca772ec8
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-07-05 16:52:37 +0000

    added WE_NUM_EXTRACTORS and WE_NUM_INJECTORS configuration variables (number of threads in mgmt/layer)
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@3003 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/we/trunk/we/mgmt/bits/policy.hpp
+++ trunk/we/trunk/we/mgmt/bits/policy.hpp
@@ -20,6 +20,7 @@
 #define WE_MGMT_LAYER_BITS_POLICY_HPP 1
 
 #include <string>
+#include <we/we-config.hpp>
 #include <we/util/codec.hpp>
 #include <we/mgmt/bits/execution_policy.hpp>
 
@@ -93,8 +94,8 @@ namespace we
         typedef def::validator<typename Traits::activity_type, true> validator;
         typedef execution_policy<typename Traits::activity_type> exec_policy;
 
-        static const size_t NUM_EXTRACTORS = 4;
-        static const size_t NUM_INJECTORS  = 4;
+        static const size_t NUM_EXTRACTORS = WE_NUM_EXTRACTORS;
+        static const size_t NUM_INJECTORS  = WE_NUM_INJECTORS;
 
         static const size_t COMMAND_QUEUE_SIZE   = 0;
         static const size_t INJECTOR_QUEUE_SIZE  = 0;
