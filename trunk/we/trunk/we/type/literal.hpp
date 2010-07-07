bc508ff7acf4fc1f90d4b996bf43f0ec0dfb4f03
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2010-07-07 09:48:07 +0000

    literal parses all basic types by itself, parses hex-long too
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@3047 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/we/trunk/we/type/literal.hpp
+++ trunk/we/trunk/we/type/literal.hpp
@@ -156,18 +156,82 @@ namespace literal
     return c;
   }
 
+  static inline bool ishexdigit (const char & c)
+  {
+    switch (c)
+      {
+      case '0'...'9':
+      case 'a'...'e':
+      case 'A'...'E': return true;
+      default: return false;
+      }
+  }
+
+  static inline long hexval (const char & c)
+  {
+    switch (c)
+      {
+      case '0'...'9': return       c - '0';
+      case 'a'...'e': return 10 + (c - 'a');
+      case 'A'...'E': return 10 + (c - 'A');
+      default:
+        throw std::runtime_error ("hexval for non hexdigit");
+      }
+  }
+
+  static inline bool isdecdigit (const char & c)
+  {
+    return isdigit (c);
+  }
+
+  static inline long decval (const char & c)
+  {
+    return c - '0';
+  }
+
+  static inline long generic_read_ulong ( bool (*isdig)(const char &)
+                                        , long (*val)(const char &)
+                                        , const long base
+                                        , expr::parse::position & pos
+                                        )
+  {
+    long l (0);
+
+    while (!pos.end() && isdig(*pos))
+      {
+        l *= base;
+        l += val (*pos);
+        ++pos;
+      }
+    
+    return l;
+  }
+
   static long read_ulong (expr::parse::position & pos)
   {
     if (pos.end() || !isdigit(*pos))
-      throw expr::exception::parse::expected ("digit", pos());
+      {
+        throw expr::exception::parse::expected ("digit", pos());
+      }
 
     long l (0);
-      
-    while (!pos.end() && isdigit(*pos))
+
+    while (!pos.end() && *pos == '0')
       {
-        l *= 10;
-        l += *pos - '0';
         ++pos;
+      }
+
+    if (!pos.end())
+      {
+        if (*pos == 'x')
+          {
+            ++pos;
+            l = generic_read_ulong (ishexdigit, hexval, 16, pos);
+          }
+        else
+          {
+            l = generic_read_ulong (isdecdigit, decval, 10, pos);
+          }
       }
 
     return l;
@@ -235,6 +299,21 @@ namespace literal
       }
   }
 
+  inline void require (const std::string & what, expr::parse::position & pos)
+  {
+    std::string::const_iterator what_pos (what.begin());
+    const std::string::const_iterator what_end (what.end());
+
+    while (what_pos != what_end)
+      if (pos.end() || *pos != *what_pos)
+        throw expr::exception::parse::expected
+          ("'" + std::string (what_pos, what_end) + "'", pos());
+      else
+        {
+          ++pos; ++what_pos;
+        }
+  }
+
   static void read (type & v, expr::parse::position & pos)
   {
     if (pos.end())
@@ -243,6 +322,9 @@ namespace literal
 
     switch (*pos)
       {
+      case 't': ++pos; require ("rue", pos); v = true; break;
+      case 'f': ++pos; require ("alse", pos); v = false; break;
+      case '[': ++pos; require ("]", pos); v = control(); break;
       case '\'':
         {
           const unsigned int open (pos());
