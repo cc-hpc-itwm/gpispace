aadb8a829c09083e7c3f49e86d7e08e8052f9141
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2010-07-14 13:06:17 +0000

    added type for properties
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@3136 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/we/trunk/tests/test_property.cpp
@@ -0,0 +1,86 @@
+// mirko.rahn@itwm.fraunhofer.de
+
+#include <we/type/property.hpp>
+
+#include <iostream>
+
+namespace prop = we::type::property;
+
+static prop::type p;
+
+void set (const std::string & path, const std::string & val)
+{
+  std::cout << "# set " << path << std::endl;
+
+  p.set (path, val);
+
+  std::cout << p << std::endl;
+}
+
+void get (const std::string & path)
+{
+  std::cout << "# get " << path << " => " << std::endl;
+
+  try
+    {
+      std::cout << p.get (path) << std::endl;
+    }
+  catch (const prop::exception::missing_binding & m)
+    {
+      std::cout << "MISSING: " << m.what() << std::endl;
+    }
+}
+
+void get_val (const std::string & path)
+{
+  std::cout << "# get_val " << path << " => " << std::endl;
+
+  try
+    {
+      std::cout << p.get_val (path) << std::endl;
+    }
+  catch (const prop::exception::missing_binding & m)
+    {
+      std::cout << "MISSING: " << m.what() << std::endl;
+    }
+  catch (const prop::exception::not_a_val & m)
+    {
+      std::cout << "NOT A VALUE: " << m.what() << std::endl;
+    }
+}
+
+void del (const std::string & path)
+{
+  std::cout << "# del " << path << std::endl;
+
+  p.del (path);
+
+  std::cout << p << std::endl;
+}
+
+int
+main ()
+{
+  set ("A.A.A", "value_of (A.A.A)");
+  get ("A.A.A");
+  get ("A.A.B");
+  get_val ("A.A.B");
+  get_val ("A.A");
+  get ("A");
+  get ("A.A");
+  set ("A.A", "value_of (A.A)");
+  set ("A", "value_of (A)");
+  set ("A.A.A", "value_of (A.A.A)");
+  set ("A.A.B", "value_of (A.A.B)");
+  set ("A.A.C", "value_of (A.A.C)");
+  set ("A.B.A", "value_of (A.B.A)");
+  set ("A.B.B", "value_of (A.B.B)");
+  set ("A.C.A", "value_of (A.C.A)");
+  set ("B.A.A", "value_of (B.A.A)");
+  del ("A.B");
+  del ("A.A.A");
+  get_val ("B.A.A");
+  get_val ("B.A");
+
+  return EXIT_SUCCESS;
+}
---
+++ trunk/we/trunk/we/type/property.hpp
@@ -0,0 +1,410 @@
+// mirko.rahn@itwm.fraunhofer.de
+
+#ifndef _WE_TYPE_PROPERTY_HPP
+#define _WE_TYPE_PROPERTY_HPP
+
+#include <boost/variant.hpp>
+#include <boost/unordered_map.hpp>
+
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
+#include <sstream>
+#include <iostream>
+
+#include <boost/serialization/nvp.hpp>
+#include <boost/serialization/map.hpp>
+
+namespace we
+{
+  namespace type
+  {
+    namespace property
+    {
+      typedef std::string key_type;
+      typedef std::string value_type;
+
+      struct type;
+
+      typedef boost::variant < boost::recursive_wrapper<type>
+                             , value_type
+                             > mapped_type;
+
+      typedef std::vector<key_type> path_type;
+      typedef boost::unordered_map<key_type, mapped_type> map_type;
+
+      // ******************************************************************* //
+
+      namespace util
+      {
+        path_type split (const std::string & s, const char & sep = '.')
+        {
+          path_type path;
+          key_type key;
+
+          for ( std::string::const_iterator pos (s.begin())
+              ; pos != s.end()
+              ; ++pos
+              )
+            {
+              if (*pos == sep)
+                {
+                  path.push_back (key);
+                  key.clear();
+                }
+              else
+                {
+                  key.push_back (*pos);
+                }
+            }
+          path.push_back (key);
+
+          return path;
+        }
+      }
+
+      // ******************************************************************* //
+
+      namespace exception
+      {
+        class missing_binding : public std::runtime_error
+        {
+        private:
+          std::string nice ( path_type::const_iterator pos
+                           , const path_type::const_iterator end
+                           )
+          {
+            std::ostringstream s;
+
+            s << "missing binding for ";
+
+            for (; std::distance (pos, end) > 1; ++pos)
+              {
+                s << *pos << ".";
+              }
+            
+            s << *pos;
+
+            return s.str();
+          }
+
+        public:
+          missing_binding ( path_type::const_iterator pos
+                          , const path_type::const_iterator end
+                          )
+            : std::runtime_error (nice (pos, end))
+          {}
+        };
+
+        class empty_path : public std::runtime_error
+        {
+        public:
+          empty_path (const std::string & pre)
+            : std::runtime_error (pre + ": empty path")
+          {}
+        };
+
+        class not_a_map : public std::runtime_error
+        {
+        public:
+          not_a_map (const std::string & pre)
+            : std::runtime_error (pre + ": not a map")
+          {}
+        };
+
+        class not_a_val : public std::runtime_error
+        {
+        public:
+          not_a_val (const std::string & pre)
+            : std::runtime_error (pre + ": not a val")
+          {}
+        };
+      }
+
+      // ******************************************************************* //
+
+      namespace visitor
+      {
+        template<typename T>
+        class mk_type : public boost::static_visitor<T>
+        {
+        public:
+          T operator () (T & t) const { return t; }
+
+          template<typename V>
+          T operator () (V &) const { return T(); }
+        };
+
+        template<typename T>
+        class get_map : public boost::static_visitor<T>
+        {
+        public:
+          T operator () (T & t) const { return t; }
+
+          template<typename V>
+          T operator () (V &) const
+          {
+            throw exception::not_a_map ("visitor::get_map");
+          }
+        };
+
+        class get_val : public boost::static_visitor<value_type &>
+        {
+        public:
+          value_type & operator () (value_type & v) const { return v; }
+
+          template<typename T>
+          value_type & operator () (T &) const
+          {
+            throw exception::not_a_val ("visitor::get_val");
+          }
+        };
+      }
+
+      // ******************************************************************* //
+
+      struct type
+      {
+      private:
+        map_type map;
+
+        friend class boost::serialization::access;
+        template<typename Archive>
+        void serialize (Archive & ar, const unsigned int)
+        {
+          ar & BOOST_SERIALIZATION_NVP(map);
+        }
+
+        // ----------------------------------------------------------------- //
+
+        template<typename IT>
+        const mapped_type & get (IT pos, IT end, IT zero) const
+        {
+          if (pos == end)
+            {
+              throw exception::empty_path ("get");
+            }
+
+          map_type::const_iterator map_pos (map.find (*pos));
+
+          if (map_pos == map.end())
+            {
+              throw exception::missing_binding (zero, end);
+            }
+
+          if (std::distance (pos, end) == 1)
+            {
+              return map_pos->second;
+            }
+          else
+            {
+              const type & t 
+                ( boost::apply_visitor ( visitor::get_map<const type &>()
+                                       , map_pos->second
+                                       )
+                );
+              
+              return t.get (pos + 1, end, zero);
+            }
+        }
+
+        template<typename IT>
+        const value_type & get_val (IT pos, IT end, IT zero) const
+        {
+          return
+            boost::apply_visitor ( visitor::get_val()
+                                 , get (pos, end, zero)
+                                 );
+        }
+
+        // ----------------------------------------------------------------- //
+
+      public:
+        type () : map () {}
+
+        map_type & get_map (void) { return map; }
+        const map_type & get_map (void) const { return map; }
+
+        // ----------------------------------------------------------------- //
+
+        template<typename IT>
+        void set (IT pos, IT end, const value_type & val)
+        {
+          if (pos == end)
+            {
+              throw exception::empty_path ("set");
+            }
+
+          if (std::distance (pos, end) == 1)
+            {
+              map[*pos] = val;
+            }
+          else
+            {
+              type t ( boost::apply_visitor ( visitor::mk_type<type>()
+                                            , map[*pos]
+                                            )
+                     );
+
+              t.set (pos + 1, end, val);
+              
+              map[*pos] = t;
+            }
+        }
+
+        void set (const path_type & path, const value_type & val)
+        {
+          set (path.begin(), path.end(), val);
+        }
+
+        void set (const std::string & path, const value_type & val)
+        {
+          set (util::split (path), val);
+        }
+
+        // ----------------------------------------------------------------- //
+
+        template<typename IT>
+        const mapped_type & get (IT pos, IT end) const
+        {
+          return get (pos, end, pos);
+        }
+
+        const mapped_type & get (const path_type & path) const
+        {
+          return get (path.begin(), path.end());
+        }
+
+        const mapped_type & get (const std::string & path) const
+        {
+          return (get (util::split (path)));
+        }
+
+        // ----------------------------------------------------------------- //
+
+        template<typename IT>
+        const value_type & get_val (IT pos, IT end) const
+        {
+          return get_val (pos, end, pos);
+        }
+
+        const value_type & get_val (const path_type & path) const
+        {
+          return get_val (path.begin(), path.end());
+        }
+
+        const value_type & get_val (const std::string & path) const
+        {
+          return get_val (util::split (path));
+        }
+
+        // ----------------------------------------------------------------- //
+
+        template<typename IT>
+        void del (IT pos, IT end)
+        {
+          if (pos == end)
+            {
+              throw exception::empty_path ("del");
+            }
+
+          if (map.find (*pos) != map.end())
+            {
+              if (std::distance (pos, end) == 1)
+                {
+                  map.erase (*pos);
+                }
+              else
+                {
+                  type & t ( boost::apply_visitor ( visitor::get_map<type &>()
+                                                  , map[*pos]
+                                                  )
+                           );
+              
+                  t.del (pos + 1, end);
+                }
+            }
+        }
+
+        void del (const path_type & path)
+        {
+          del (path.begin(), path.end());
+        }
+
+        void del (const std::string & path)
+        {
+          del (util::split (path));
+        }
+
+        // ----------------------------------------------------------------- //
+
+        friend std::ostream & operator << (std::ostream &, const type &);
+      };
+
+      // ******************************************************************* //
+
+      namespace visitor
+      {
+        class show : public boost::static_visitor<>
+        {
+        private:
+          std::ostream & s;
+          unsigned int l;
+
+          void level (void) const
+          {
+            for (unsigned int i (0); i < l; ++i)
+              {
+                s << "  ";
+              }
+          }
+
+        public:
+          show ( std::ostream & _s
+               , const unsigned int _l = 0
+               ) 
+            : s (_s)
+            , l (_l)
+          {}
+
+          void operator () (const value_type & v) const
+          {
+            level(); s << v << std::endl;
+          }
+
+          void operator () (const type & t) const
+          {
+            for ( map_type::const_iterator pos (t.get_map().begin())
+                ; pos != t.get_map().end()
+                ; ++pos
+                )
+              {
+                level(); s << pos->first << ":" << std::endl;
+
+                boost::apply_visitor (show (s, l+1), pos->second);
+              }
+          }
+        };
+      }
+
+      // ******************************************************************* //
+
+      std::ostream & operator << (std::ostream & s, const type & t)
+      {
+        for ( map_type::const_iterator pos (t.map.begin())
+            ; pos != t.map.end()
+            ; ++pos
+            )
+          {
+            s << pos->first << ":" << std::endl;
+
+            boost::apply_visitor (visitor::show (s, 1), pos->second);
+          }
+
+        return s;
+      }
+    }
+  }
+}
+
+#endif
