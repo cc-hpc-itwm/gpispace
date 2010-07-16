a3cff0fd1f7653f10b501cc75e8e098ab48dcde8
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2010-07-16 10:09:21 +0000

    reorg kdm
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@3175 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/application/kdm/libexec/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-pushd /p/hpc/sdpa/ap/git/KDM_VM
-
-./libexec/test_load_module ./libexec/libfvm-pc.so ./build.gcc/libkdm.so
-
-popd
--- trunk/application/kdm/run.sh
+++
@@ -1,6 +0,0 @@
-/p/hpc/sdpa/ap/git/SDPA/trunk/we/trunk/build/tests/test_layer   \
- --cfg      /p/hpc/sdpa/ap/git/KDM_VM/Kirchhoff_Model.xml       \
- --mod-path /p/hpc/sdpa/ap/git/KDM_VM/build.gcc                 \
- --load     /p/hpc/sdpa/ap/git/KDM_VM/libexec/libfvm-pc.so      \
- --load     /p/hpc/sdpa/ap/git/KDM_VM/build.gcc/libsinc.so      \
-                                                         2>&1 | tee out.txt
