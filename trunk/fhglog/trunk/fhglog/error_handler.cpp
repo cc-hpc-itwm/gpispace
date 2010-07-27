7df5a92ea5c9900571d542e3d3f24bff7e7f4e83
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-07-27 12:28:30 +0000

    flushing streams before exit
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@3327 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/fhglog/trunk/fhglog/error_handler.cpp
+++ trunk/fhglog/trunk/fhglog/error_handler.cpp
@@ -1,12 +1,14 @@
 #include <fhglog/fhglog-config.hpp>
 #include <iostream>
 #include <unistd.h>
+#include <cstdio>
 #include "error_handler.hpp"
 
 static void DefaultErrorHandler (void)
 {
 #if defined(FHGLOG_EXIT_ON_ERROR) && FHGLOG_EXIT_ON_ERROR == 1
   std::clog << "Program termination triggered by FhgLog..." << std::endl;
+  fflush(NULL);
   _exit (FHGLOG_EXIT_ON_ERROR_CODE);
 #endif
 }
