4a4834734c0e1fa046b4891b0954c5a10111a1f3
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2010-08-03 14:51:30 +0000

    example application for better get/put
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@3439 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/application/example/put_get/put_get.cpp
@@ -0,0 +1,65 @@
+#include <we/loader/macros.hpp>
+
+#include <fhglog/fhglog.hpp>
+
+#include <fvm-pc/pc.hpp>
+#include <fvm-pc/util.hpp>
+
+static void run ( void *
+                , const we::loader::input_t & input
+                , we::loader::output_t & output
+                )
+{
+  const value::type & a (input.value ("a"));
+
+  LOG (INFO, "a " << a);
+
+  const value::type & a_start (value::get_field ("start", a));
+  const value::type & a_end (value::get_field ("end", a));
+
+  LOG (INFO, "a_start " << a_start);
+  LOG (INFO, "a_end " << a_end);
+
+  const double & a_start_x
+    (value::get_literal_value<double> (value::get_field ("start.x", a)));
+  const double & a_start_y
+    (value::get_literal_value<double> (value::get_field ("y", a_start)));
+
+  const double & a_end_x
+    (value::get_literal_value<double> (value::get_field ("x", a_end)));
+  const double & a_end_y
+    (value::get_literal_value<double> (value::get_field ("y", a_end)));
+
+  LOG (INFO, "a_start_x " << a_start_x);
+  LOG (INFO, "a_start_y " << a_start_y);
+  LOG (INFO, "a_end_x " << a_end_x);
+  LOG (INFO, "a_end_y " << a_end_y);
+
+  const double & stretch (we::loader::get_input<double> (input, "stretch"));
+
+  LOG (INFO, "stretch " << stretch);
+
+  value::structured_t b_start;
+  value::structured_t b_end;
+  value::structured_t b;
+
+  b_start["x"] = stretch * a_start_x;
+  b_start["y"] = stretch * a_start_y;
+  b_end["x"] = stretch * a_end_x;
+  b_end["y"] = stretch * a_end_y;
+  b["start"] = b_start;
+  b["end"] = b_end;
+
+  we::loader::put_output (output, "b", b);
+}
+
+WE_MOD_INITIALIZE_START (put_get);
+{
+  WE_REGISTER_FUN (run);
+}
+WE_MOD_INITIALIZE_END (put_get);
+
+WE_MOD_FINALIZE_START (put_get);
+{
+}
+WE_MOD_FINALIZE_END (put_get);
