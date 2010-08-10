30c210e07fab0a3356ba372201650e7744017df5
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-08-10 08:31:00 +0000

    new format() function
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@3512 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/fhglog/trunk/fhglog/format.hpp
@@ -0,0 +1,117 @@
+#ifndef FHG_LOG_FORMAT_HPP
+#define FHG_LOG_FORMAT_HPP 1
+
+#include <string>
+#include <fhglog/LogEvent.hpp>
+#include <fhglog/format_flag.hpp>
+
+namespace fhg
+{
+  namespace log
+  {
+    inline
+    std::ostream & format( std::ostream & os
+                         , const std::string &_fmt
+                         , const LogEvent &evt
+                         )
+    {
+      std::string::const_iterator c(_fmt.begin());
+      std::string::size_type len(_fmt.size());
+      while (len > 0)
+      {
+        if (*c == fhg::log::fmt::flag::OPEN::value)
+        {
+          ++c; --len;
+          if (0 == len)
+          {
+            throw std::runtime_error
+              ("Format string ends with a single opening character!");
+          }
+
+          // what do we have
+          switch (*c)
+          {
+#define HDL(f)                                                         \
+            case fmt::format_traits<fmt::flag:: f>::flag::value:        \
+              fmt::format_traits<fmt::flag:: f>::flag::format(evt, os, *c); \
+            break
+
+            HDL(OPEN);
+            HDL(SHORT_SEVERITY);
+            HDL(SEVERITY);
+            HDL(FILE);
+            HDL(PATH);
+            HDL(FUNCTION);
+            HDL(MODULE);
+            HDL(LINE);
+            HDL(MESSAGE);
+            HDL(DATE);
+            HDL(TSTAMP);
+            HDL(TID);
+            HDL(PID);
+            HDL(LOGGER);
+            HDL(ENDL);
+          default:
+            HDL(UNDEF);
+#undef HDL
+          }
+        }
+        else
+        {
+          os << *c;
+        }
+
+        if (len)
+        {
+          ++c; --len;
+        }
+      }
+      return os;
+    }
+
+    inline
+    std::string format(const std::string & fmt, const LogEvent &evt)
+    {
+      std::ostringstream os;
+      format (os, fmt, evt);
+      return os.str();
+    }
+
+    inline
+    void check_format(const std::string & fmt)
+    {
+      std::ostringstream os;
+      format (os, fmt, LogEvent());
+    }
+
+    struct default_format
+    {
+      static std::string const & SHORT()
+      {
+        static std::string f("%s: %l %p:%L - %m%n");
+        return f;
+      }
+      static std::string const & LONG()
+      {
+        static std::string f("%t %S %l pid:%R thread:%T %p:%L (%F) - %m%n");
+        return f;
+      }
+    };
+
+    struct formatter
+    {
+      formatter (const std::string & s = default_format::SHORT())
+        : fmt_(s)
+      {}
+
+      std::string operator () (const LogEvent & e) const
+      {
+        return format(fmt_, e);
+      }
+    private:
+      std::string fmt_;
+    };
+  }
+}
+
+#endif
---
+++ trunk/fhglog/trunk/fhglog/format_flag.hpp
@@ -0,0 +1,248 @@
+#ifndef FHG_LOG_FORMAT_FLAG_HPP
+#define FHG_LOG_FORMAT_FLAG_HPP 1
+
+#include <fhglog/LogEvent.hpp>
+namespace fhg
+{
+  namespace log
+  {
+    namespace fmt
+    {
+      namespace flag
+      {
+        struct OPEN
+        {
+          static const char value = '%';
+          //        static const char *description = "starting character";
+          static std::ostream & format( const LogEvent &
+                                      , std::ostream &os
+                                      , const char
+                                      )
+          {
+            return os << value;
+          }
+        };
+
+        struct SHORT_SEVERITY
+        {
+          static const char value = 's';
+          //        static const char *description = "the level of the event";
+          static std::ostream & format( const LogEvent &e
+                                      , std::ostream &os
+                                      , const char
+                                      )
+          {
+            return os << e.severity().str()[0];
+          }
+        };
+
+        struct SEVERITY
+        {
+          static const char value        = 'S';
+          //        static const char *description = "the level of the event";
+          static std::ostream & format( const LogEvent &e
+                                      , std::ostream &os
+                                      , const char
+                                      )
+          {
+            return os << e.severity().str();
+          }
+        };
+
+        struct FILE
+        {
+          static const char value            = 'p';
+          //        static const char *description = "just the filename";
+          static std::ostream & format( const LogEvent &e
+                                      , std::ostream &os
+                                      , const char
+                                      )
+          {
+            return os << e.file();
+          }
+        };
+        struct PATH
+        {
+          static const char value            = 'P';
+          //        static const char *description = "complete path of the file";
+          static std::ostream & format( const LogEvent &e
+                                      , std::ostream &os
+                                      , const char
+                                      )
+          {
+            return os << e.path();
+          }
+        };
+        struct FUNCTION
+        {
+          static const char value            = 'F';
+          //        static const char *description = "function (full signature)";
+          static std::ostream & format( const LogEvent &e
+                                      , std::ostream &os
+                                      , const char
+                                      )
+          {
+            return os << e.function();
+          }
+        };
+        struct MODULE
+        {
+          static const char value            = 'M';
+          //        static const char *description = "module (i.e. filename without extension)";
+          static std::ostream & format( const LogEvent &e
+                                      , std::ostream &os
+                                      , const char
+                                      )
+          {
+            return os << e.module();
+          }
+        };
+        struct LINE
+        {
+          static const char value            = 'L';
+          //        static const char *description = "line of the log";
+          static std::ostream & format( const LogEvent &e
+                                      , std::ostream &os
+                                      , const char
+                                      )
+          {
+            return os << e.line();
+          }
+        };
+        struct MESSAGE
+        {
+          static const char value            = 'm';
+          //        static const char *description = "the logmessage";
+          static std::ostream & format( const LogEvent &e
+                                      , std::ostream &os
+                                      , const char
+                                      )
+          {
+            return os << e.message();
+          }
+        };
+        struct DATE
+        {
+          static const char value            = 'd';
+          //        static const char *description = "when was the event created (date string)";
+          static std::ostream & format( const LogEvent &e
+                                      , std::ostream &os
+                                      , const char
+                                      )
+          {
+            return os << e.tstamp();
+          }
+        };
+        struct TSTAMP
+        {
+          static const char value            = 't';
+          //        static const char *description = "when was the event created (unix timestamp)";
+          static std::ostream & format( const LogEvent &e
+                                      , std::ostream &os
+                                      , const char
+                                      )
+          {
+            return os << e.tstamp();
+          }
+        };
+        struct TID
+        {
+          static const char value            = 'T';
+          //        static const char *description = "by which thread has it been created";
+          static std::ostream & format( const LogEvent &e
+                                      , std::ostream &os
+                                      , const char
+                                      )
+          {
+            return os << e.tid();
+          }
+        };
+        struct PID
+        {
+          static const char value            = 'R';
+          //        static const char *description = "by which process has it been created";
+          static std::ostream & format( const LogEvent &e
+                                      , std::ostream &os
+                                      , const char
+                                      )
+          {
+            return os << e.pid();
+          }
+        };
+        struct LOGGER
+        {
+          static const char value            = 'l';
+          //        static const char *description = "the logger via which this event came";
+          static std::ostream & format( const LogEvent &e
+                                      , std::ostream &os
+                                      , const char
+                                      )
+          {
+            return os << e.logged_via();
+          }
+        };
+        struct ENDL
+        {
+          static const char value            = 'n';
+          //        static const char *description = "new line";
+          static std::ostream & format( const LogEvent &
+                                      , std::ostream &os
+                                      , const char
+                                      )
+          {
+            return os << std::endl;
+          }
+        };
+        struct UNDEF
+        {
+          static const char value            = '-';
+          //        static const char *description = "placeholder for undefined flag";
+          static std::ostream & format( const LogEvent &
+                                      , std::ostream &
+                                      , const char real
+                                      )
+          {
+            std::ostringstream s;
+            s << real;
+            throw std::runtime_error ("format code not defined: " + s.str());
+          }
+        };
+      }
+
+      template <typename T = flag::UNDEF>
+      struct format_traits
+      {
+        static const bool is_flag = false;
+        typedef T flag;
+      };
+
+#define DECLARE_FLAG(cls)                             \
+      template<>                                      \
+      struct format_traits<flag::cls>                 \
+      {                                               \
+        static const bool is_flag = true;             \
+        typedef flag:: cls flag;                      \
+      }
+
+      DECLARE_FLAG(OPEN);
+      DECLARE_FLAG(SHORT_SEVERITY);
+      DECLARE_FLAG(SEVERITY);
+      DECLARE_FLAG(FILE);
+      DECLARE_FLAG(PATH);
+      DECLARE_FLAG(FUNCTION);
+      DECLARE_FLAG(MODULE);
+      DECLARE_FLAG(LINE);
+      DECLARE_FLAG(MESSAGE);
+      DECLARE_FLAG(DATE);
+      DECLARE_FLAG(TSTAMP);
+      DECLARE_FLAG(TID);
+      DECLARE_FLAG(PID);
+      DECLARE_FLAG(LOGGER);
+      DECLARE_FLAG(ENDL);
+
+#undef DECLARE_FLAG
+    }
+  }
+}
+
+#endif
