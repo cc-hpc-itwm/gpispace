a56cfe353cf74ff05d24167fe3b9e67a5f371f64
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-08-11 17:24:28 +0000

    added memory.hpp to define shared_ptr correctly
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@3548 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/fhgcom/fhgcom/memory.hpp
@@ -0,0 +1,14 @@
+#ifndef FHG_COM_MEMORY_HPP
+#define FHG_COM_MEMORY_HPP 1
+
+#include <fhglog/fhglog-config.hpp>
+
+namespace fhg
+{
+  namespace com
+  {
+    using fhg::log::shared_ptr;
+  }
+}
+
+#endif
