4666c6daecbd8b0f0cd977ea3a62f4d4de762aa4
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-08-11 17:24:06 +0000

    added starting point for communication library
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@3535 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/fhgcom/bin/fhgcomd.cpp
@@ -0,0 +1,11 @@
+// chat server example from boost.org
+//    http://www.boost.org/doc/libs/1_43_0/doc/html/boost_asio/example/chat/chat_server.cpp
+
+#include <fhglog/fhglog.hpp>
+
+int main(int ac, char *av[])
+{
+  FHGLOG_SETUP(ac,av);
+
+  return 0;
+}
