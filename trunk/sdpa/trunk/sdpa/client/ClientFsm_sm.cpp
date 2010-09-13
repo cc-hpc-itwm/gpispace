75ea992c2129e1215bb07cb766adbdeb86fa46f3
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-09-13 12:55:59 +0000

    sdpa: added generated smc files
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@3643 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/sdpa/trunk/sdpa/client/ClientFsm_sm.cpp
@@ -0,0 +1,532 @@
+
+//
+// Finite state machine for the SDPA client
+//
+
+#if __GNUC__ >4 || ( __GNUC__==4 && __GNUC_MINOR__ > 1)
+#  pragma GCC diagnostic ignored "-Wall"
+#  pragma GCC diagnostic ignored "-Wunused"
+#  pragma GCC diagnostic ignored "-Weffc++"
+#  pragma GCC diagnostic ignored "-Wunused-parameter"
+#endif
+
+
+
+#include "Client.hpp"
+#include <sdpa/types.hpp>
+#include <sdpa/client/types.hpp>
+#include <seda/IEvent.hpp>
+#include "ClientFsm_sm.h"
+
+using namespace statemap;
+
+namespace sdpa
+{
+    namespace client
+    {
+        // Static class declarations.
+        ClientFsm_Unconfigured ClientFsm::Unconfigured("ClientFsm::Unconfigured", 0);
+        ClientFsm_Configuring ClientFsm::Configuring("ClientFsm::Configuring", 1);
+        ClientFsm_Configured ClientFsm::Configured("ClientFsm::Configured", 2);
+
+        const int ClientContext::MIN_INDEX = 0;
+        const int ClientContext::MAX_INDEX = 2;
+        ClientState* ClientContext::_States[] = 
+{
+            &ClientFsm::Unconfigured,
+            &ClientFsm::Configuring,
+            &ClientFsm::Configured
+        };
+
+        ClientState& ClientContext::valueOf(int stateId)
+        {
+            if (stateId < MIN_INDEX || stateId > MAX_INDEX)
+            {
+                throw (
+                    IndexOutOfBoundsException(
+                        stateId, MIN_INDEX, MAX_INDEX));
+            }
+
+            return (static_cast<ClientState&>(*(_States[stateId])));
+        }
+
+        void ClientState::Cancel(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Default(context);
+            return;
+        }
+
+        void ClientState::CancelAck(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Default(context);
+            return;
+        }
+
+        void ClientState::ConfigNok(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Default(context);
+            return;
+        }
+
+        void ClientState::ConfigOk(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Default(context);
+            return;
+        }
+
+        void ClientState::Delete(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Default(context);
+            return;
+        }
+
+        void ClientState::DeleteAck(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Default(context);
+            return;
+        }
+
+        void ClientState::Error(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Default(context);
+            return;
+        }
+
+        void ClientState::Query(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Default(context);
+            return;
+        }
+
+        void ClientState::Results(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Default(context);
+            return;
+        }
+
+        void ClientState::Retrieve(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Default(context);
+            return;
+        }
+
+        void ClientState::Shutdown(ClientContext& context)
+        {
+            Default(context);
+            return;
+        }
+
+        void ClientState::Start(ClientContext& context, const sdpa::client::config_t & cfg)
+        {
+            Default(context);
+            return;
+        }
+
+        void ClientState::StatusReply(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Default(context);
+            return;
+        }
+
+        void ClientState::Submit(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Default(context);
+            return;
+        }
+
+        void ClientState::SubmitAck(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Default(context);
+            return;
+        }
+
+        void ClientState::Unknown(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Default(context);
+            return;
+        }
+
+        void ClientState::Default(ClientContext& context)
+        {
+            throw (
+                TransitionUndefinedException(
+                    context.getState().getName(),
+                    context.getTransition()));
+
+            return;
+        }
+
+        void ClientFsm_Unconfigured::Default(ClientContext& context)
+        {
+
+
+            return;
+        }
+
+        void ClientFsm_Unconfigured::Shutdown(ClientContext& context)
+        {
+            Client& ctxt = context.getOwner();
+
+            ClientState& EndStateName = context.getState();
+
+            context.clearState();
+            try
+            {
+                ctxt.action_shutdown();
+                context.setState(EndStateName);
+            }
+            catch (...)
+            {
+                context.setState(EndStateName);
+                throw;
+            }
+
+            return;
+        }
+
+        void ClientFsm_Unconfigured::Start(ClientContext& context, const sdpa::client::config_t & cfg)
+        {
+            Client& ctxt = context.getOwner();
+
+            (context.getState()).Exit(context);
+            context.clearState();
+            try
+            {
+                ctxt.action_configure(cfg);
+                context.setState(ClientFsm::Configuring);
+            }
+            catch (...)
+            {
+                context.setState(ClientFsm::Configuring);
+                throw;
+            }
+            (context.getState()).Entry(context);
+
+            return;
+        }
+
+        void ClientFsm_Configuring::ConfigNok(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Client& ctxt = context.getOwner();
+
+            (context.getState()).Exit(context);
+            context.clearState();
+            try
+            {
+                ctxt.action_config_nok();
+                ctxt.action_store_reply(evt);
+                context.setState(ClientFsm::Unconfigured);
+            }
+            catch (...)
+            {
+                context.setState(ClientFsm::Unconfigured);
+                throw;
+            }
+            (context.getState()).Entry(context);
+
+            return;
+        }
+
+        void ClientFsm_Configuring::ConfigOk(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Client& ctxt = context.getOwner();
+
+            (context.getState()).Exit(context);
+            context.clearState();
+            try
+            {
+                ctxt.action_config_ok();
+                ctxt.action_store_reply(evt);
+                context.setState(ClientFsm::Configured);
+            }
+            catch (...)
+            {
+                context.setState(ClientFsm::Configured);
+                throw;
+            }
+            (context.getState()).Entry(context);
+
+            return;
+        }
+
+        void ClientFsm_Configuring::Default(ClientContext& context)
+        {
+
+
+            return;
+        }
+
+        void ClientFsm_Configured::Cancel(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Client& ctxt = context.getOwner();
+
+            ClientState& EndStateName = context.getState();
+
+            context.clearState();
+            try
+            {
+                ctxt.action_cancel(evt);
+                context.setState(EndStateName);
+            }
+            catch (...)
+            {
+                context.setState(EndStateName);
+                throw;
+            }
+
+            return;
+        }
+
+        void ClientFsm_Configured::CancelAck(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Client& ctxt = context.getOwner();
+
+            ClientState& EndStateName = context.getState();
+
+            context.clearState();
+            try
+            {
+                ctxt.action_store_reply(evt);
+                context.setState(EndStateName);
+            }
+            catch (...)
+            {
+                context.setState(EndStateName);
+                throw;
+            }
+
+            return;
+        }
+
+        void ClientFsm_Configured::Delete(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Client& ctxt = context.getOwner();
+
+            ClientState& EndStateName = context.getState();
+
+            context.clearState();
+            try
+            {
+                ctxt.action_delete(evt);
+                context.setState(EndStateName);
+            }
+            catch (...)
+            {
+                context.setState(EndStateName);
+                throw;
+            }
+
+            return;
+        }
+
+        void ClientFsm_Configured::DeleteAck(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Client& ctxt = context.getOwner();
+
+            ClientState& EndStateName = context.getState();
+
+            context.clearState();
+            try
+            {
+                ctxt.action_store_reply(evt);
+                context.setState(EndStateName);
+            }
+            catch (...)
+            {
+                context.setState(EndStateName);
+                throw;
+            }
+
+            return;
+        }
+
+        void ClientFsm_Configured::Error(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Client& ctxt = context.getOwner();
+
+            ClientState& EndStateName = context.getState();
+
+            context.clearState();
+            try
+            {
+                ctxt.action_store_reply(evt);
+                context.setState(EndStateName);
+            }
+            catch (...)
+            {
+                context.setState(EndStateName);
+                throw;
+            }
+
+            return;
+        }
+
+        void ClientFsm_Configured::Query(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Client& ctxt = context.getOwner();
+
+            ClientState& EndStateName = context.getState();
+
+            context.clearState();
+            try
+            {
+                ctxt.action_query(evt);
+                context.setState(EndStateName);
+            }
+            catch (...)
+            {
+                context.setState(EndStateName);
+                throw;
+            }
+
+            return;
+        }
+
+        void ClientFsm_Configured::Results(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Client& ctxt = context.getOwner();
+
+            ClientState& EndStateName = context.getState();
+
+            context.clearState();
+            try
+            {
+                ctxt.action_store_reply(evt);
+                context.setState(EndStateName);
+            }
+            catch (...)
+            {
+                context.setState(EndStateName);
+                throw;
+            }
+
+            return;
+        }
+
+        void ClientFsm_Configured::Retrieve(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Client& ctxt = context.getOwner();
+
+            ClientState& EndStateName = context.getState();
+
+            context.clearState();
+            try
+            {
+                ctxt.action_retrieve(evt);
+                context.setState(EndStateName);
+            }
+            catch (...)
+            {
+                context.setState(EndStateName);
+                throw;
+            }
+
+            return;
+        }
+
+        void ClientFsm_Configured::Shutdown(ClientContext& context)
+        {
+            Client& ctxt = context.getOwner();
+
+            ClientState& EndStateName = context.getState();
+
+            context.clearState();
+            try
+            {
+                ctxt.action_shutdown();
+                context.setState(EndStateName);
+            }
+            catch (...)
+            {
+                context.setState(EndStateName);
+                throw;
+            }
+
+            return;
+        }
+
+        void ClientFsm_Configured::StatusReply(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Client& ctxt = context.getOwner();
+
+            ClientState& EndStateName = context.getState();
+
+            context.clearState();
+            try
+            {
+                ctxt.action_store_reply(evt);
+                context.setState(EndStateName);
+            }
+            catch (...)
+            {
+                context.setState(EndStateName);
+                throw;
+            }
+
+            return;
+        }
+
+        void ClientFsm_Configured::Submit(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Client& ctxt = context.getOwner();
+
+            ClientState& EndStateName = context.getState();
+
+            context.clearState();
+            try
+            {
+                ctxt.action_submit(evt);
+                context.setState(EndStateName);
+            }
+            catch (...)
+            {
+                context.setState(EndStateName);
+                throw;
+            }
+
+            return;
+        }
+
+        void ClientFsm_Configured::SubmitAck(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Client& ctxt = context.getOwner();
+
+            ClientState& EndStateName = context.getState();
+
+            context.clearState();
+            try
+            {
+                ctxt.action_store_reply(evt);
+                context.setState(EndStateName);
+            }
+            catch (...)
+            {
+                context.setState(EndStateName);
+                throw;
+            }
+
+            return;
+        }
+
+        void ClientFsm_Configured::Unknown(ClientContext& context, const seda::IEvent::Ptr & evt)
+        {
+            Client& ctxt = context.getOwner();
+
+            ClientState& EndStateName = context.getState();
+
+            context.clearState();
+            try
+            {
+                ctxt.action_store_reply(evt);
+                context.setState(EndStateName);
+            }
+            catch (...)
+            {
+                context.setState(EndStateName);
+                throw;
+            }
+
+            return;
+        }
+    }
+}
---
+++ trunk/sdpa/trunk/sdpa/client/ClientFsm_sm.h
@@ -0,0 +1,248 @@
+#ifndef _H_CLIENTFSM_SM
+#define _H_CLIENTFSM_SM
+
+#define SMC_USES_IOSTREAMS
+
+#include <statemap.h>
+
+namespace sdpa
+{
+    namespace client
+    {
+        // Forward declarations.
+        class ClientFsm;
+        class ClientFsm_Unconfigured;
+        class ClientFsm_Configuring;
+        class ClientFsm_Configured;
+        class ClientFsm_Default;
+        class ClientState;
+        class ClientContext;
+        class Client;
+
+        class ClientState :
+            public statemap::State
+        {
+        public:
+
+            ClientState(const char *name, int stateId)
+            : statemap::State(name, stateId)
+            {};
+
+            virtual void Entry(ClientContext&) {};
+            virtual void Exit(ClientContext&) {};
+
+            virtual void Cancel(ClientContext& context, const seda::IEvent::Ptr & evt);
+            virtual void CancelAck(ClientContext& context, const seda::IEvent::Ptr & evt);
+            virtual void ConfigNok(ClientContext& context, const seda::IEvent::Ptr & evt);
+            virtual void ConfigOk(ClientContext& context, const seda::IEvent::Ptr & evt);
+            virtual void Delete(ClientContext& context, const seda::IEvent::Ptr & evt);
+            virtual void DeleteAck(ClientContext& context, const seda::IEvent::Ptr & evt);
+            virtual void Error(ClientContext& context, const seda::IEvent::Ptr & evt);
+            virtual void Query(ClientContext& context, const seda::IEvent::Ptr & evt);
+            virtual void Results(ClientContext& context, const seda::IEvent::Ptr & evt);
+            virtual void Retrieve(ClientContext& context, const seda::IEvent::Ptr & evt);
+            virtual void Shutdown(ClientContext& context);
+            virtual void Start(ClientContext& context, const sdpa::client::config_t & cfg);
+            virtual void StatusReply(ClientContext& context, const seda::IEvent::Ptr & evt);
+            virtual void Submit(ClientContext& context, const seda::IEvent::Ptr & evt);
+            virtual void SubmitAck(ClientContext& context, const seda::IEvent::Ptr & evt);
+            virtual void Unknown(ClientContext& context, const seda::IEvent::Ptr & evt);
+
+        protected:
+
+            virtual void Default(ClientContext& context);
+        };
+
+        class ClientFsm
+        {
+        public:
+
+            static ClientFsm_Unconfigured Unconfigured;
+            static ClientFsm_Configuring Configuring;
+            static ClientFsm_Configured Configured;
+        };
+
+        class ClientFsm_Default :
+            public ClientState
+        {
+        public:
+
+            ClientFsm_Default(const char *name, int stateId)
+            : ClientState(name, stateId)
+            {};
+
+        };
+
+        class ClientFsm_Unconfigured :
+            public ClientFsm_Default
+        {
+        public:
+            ClientFsm_Unconfigured(const char *name, int stateId)
+            : ClientFsm_Default(name, stateId)
+            {};
+
+            void Default(ClientContext& context);
+            void Shutdown(ClientContext& context);
+            void Start(ClientContext& context, const sdpa::client::config_t & cfg);
+        };
+
+        class ClientFsm_Configuring :
+            public ClientFsm_Default
+        {
+        public:
+            ClientFsm_Configuring(const char *name, int stateId)
+            : ClientFsm_Default(name, stateId)
+            {};
+
+            void ConfigNok(ClientContext& context, const seda::IEvent::Ptr & evt);
+            void ConfigOk(ClientContext& context, const seda::IEvent::Ptr & evt);
+            void Default(ClientContext& context);
+        };
+
+        class ClientFsm_Configured :
+            public ClientFsm_Default
+        {
+        public:
+            ClientFsm_Configured(const char *name, int stateId)
+            : ClientFsm_Default(name, stateId)
+            {};
+
+            void Cancel(ClientContext& context, const seda::IEvent::Ptr & evt);
+            void CancelAck(ClientContext& context, const seda::IEvent::Ptr & evt);
+            void Delete(ClientContext& context, const seda::IEvent::Ptr & evt);
+            void DeleteAck(ClientContext& context, const seda::IEvent::Ptr & evt);
+            void Error(ClientContext& context, const seda::IEvent::Ptr & evt);
+            void Query(ClientContext& context, const seda::IEvent::Ptr & evt);
+            void Results(ClientContext& context, const seda::IEvent::Ptr & evt);
+            void Retrieve(ClientContext& context, const seda::IEvent::Ptr & evt);
+            void Shutdown(ClientContext& context);
+            void StatusReply(ClientContext& context, const seda::IEvent::Ptr & evt);
+            void Submit(ClientContext& context, const seda::IEvent::Ptr & evt);
+            void SubmitAck(ClientContext& context, const seda::IEvent::Ptr & evt);
+            void Unknown(ClientContext& context, const seda::IEvent::Ptr & evt);
+        };
+
+        class ClientContext :
+            public statemap::FSMContext
+        {
+        public:
+
+            ClientContext(Client& owner)
+            : _owner(owner)
+            {
+                setState(ClientFsm::Unconfigured);
+                ClientFsm::Unconfigured.Entry(*this);
+            };
+
+            Client& getOwner() const
+            {
+                return (_owner);
+            };
+
+            ClientState& getState() const
+            {
+                if (_state == NULL)
+                {
+                    throw statemap::StateUndefinedException();
+                }
+
+                return (dynamic_cast<ClientState&>(*_state));
+            };
+
+            void Cancel(const seda::IEvent::Ptr & evt)
+            {
+                (getState()).Cancel(*this, evt);
+            };
+
+            void CancelAck(const seda::IEvent::Ptr & evt)
+            {
+                (getState()).CancelAck(*this, evt);
+            };
+
+            void ConfigNok(const seda::IEvent::Ptr & evt)
+            {
+                (getState()).ConfigNok(*this, evt);
+            };
+
+            void ConfigOk(const seda::IEvent::Ptr & evt)
+            {
+                (getState()).ConfigOk(*this, evt);
+            };
+
+            void Delete(const seda::IEvent::Ptr & evt)
+            {
+                (getState()).Delete(*this, evt);
+            };
+
+            void DeleteAck(const seda::IEvent::Ptr & evt)
+            {
+                (getState()).DeleteAck(*this, evt);
+            };
+
+            void Error(const seda::IEvent::Ptr & evt)
+            {
+                (getState()).Error(*this, evt);
+            };
+
+            void Query(const seda::IEvent::Ptr & evt)
+            {
+                (getState()).Query(*this, evt);
+            };
+
+            void Results(const seda::IEvent::Ptr & evt)
+            {
+                (getState()).Results(*this, evt);
+            };
+
+            void Retrieve(const seda::IEvent::Ptr & evt)
+            {
+                (getState()).Retrieve(*this, evt);
+            };
+
+            void Shutdown()
+            {
+                (getState()).Shutdown(*this);
+            };
+
+            void Start(const sdpa::client::config_t & cfg)
+            {
+                (getState()).Start(*this, cfg);
+            };
+
+            void StatusReply(const seda::IEvent::Ptr & evt)
+            {
+                (getState()).StatusReply(*this, evt);
+            };
+
+            void Submit(const seda::IEvent::Ptr & evt)
+            {
+                (getState()).Submit(*this, evt);
+            };
+
+            void SubmitAck(const seda::IEvent::Ptr & evt)
+            {
+                (getState()).SubmitAck(*this, evt);
+            };
+
+            void Unknown(const seda::IEvent::Ptr & evt)
+            {
+                (getState()).Unknown(*this, evt);
+            };
+
+            static ClientState& valueOf(int stateId);
+
+        private:
+
+            Client& _owner;
+
+        private:
+
+            const static int MIN_INDEX;
+            const static int MAX_INDEX;
+            static ClientState* _States[];
+        };
+    }
+
+}
+
+#endif // _H_CLIENTFSM_SM
