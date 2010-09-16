f549759c5e046e5ce13b4116f55a04f47b87b87a
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-09-16 11:26:06 +0000

    build: fixed build script
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@3668 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/main/trunk/bitten/build.sh
+++ trunk/main/trunk/bitten/build.sh
@@ -7,15 +7,15 @@ if [ $# -ne 3 ]; then
 fi
 
 build_type="$1"
-build_path="$2"
-source_path="$3"
-echo "bitten build.sh type=${build_type} src=${sourcepath} bld=${build_path}"
+source_path="$2"
+build_path="$3"
+echo "bitten build.sh type=${build_type} src=${source_path} bld=${build_path}"
 
 echo "creating build directory..."
 
-mkdir -p "${build_dir}" || exit 1
-pushd "${build_dir}"
-cmake "${source_dir}" -DCMAKE_BUILD_TYPE=${build_type} -DENABLE_MONITOR_GUI=No || exit 1
+mkdir -p "${build_path}" || exit 1
+pushd "${build_path}"
+cmake "${source_path}" -DCMAKE_BUILD_TYPE=${build_type} -DENABLE_MONITOR_GUI=No || exit 1
 make -s all || exit 1
 ctest
 popd
