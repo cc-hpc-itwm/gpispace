2b8f7fe8d93814407c7df6c4867fb01cc9c26599
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-09-21 10:09:06 +0000

    selftest: GPI startup
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@3727 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/main/trunk/bitten/selftest.sh
+++ trunk/main/trunk/bitten/selftest.sh
@@ -10,6 +10,10 @@ pcd="nre-pcd"
 
 logfile=/tmp/sdpa-selftest.log
 
+# error codes:
+ESUC=0
+EERR=1
+EGPI=4
 
 function log()
 {
@@ -17,4 +21,25 @@ function log()
     echo "$*" >> ${logfile}
 }
 
-log test 1 2 3 4
+function is_alive ()
+{
+    name=$1
+    pid=$2
+    kill -0 $pid >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+	return 0
+    else
+	return 1
+    fi
+}
+
+:> ${logfile}
+log "initializing selftest..."
+log "starting sdpa-gpi..."
+${sdpa_gpi} >> ${logfile} 2>&1 &
+sdpa_gpi_pid=$!
+sleep 2
+if ! is_alive "sdpa_gpi" $sdpa_gpi_pid ; then
+    echo "SDPA-GPI interface could not be started!"
+    exit ${EGPI}
+fi
