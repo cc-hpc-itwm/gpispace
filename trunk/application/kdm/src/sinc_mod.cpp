ca545caae7bdb93b1a755e0d6a7b67ce8904be3f
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-09-28 10:33:51 +0000

    sinc: added getSincInterpolator(threadId, float dt) call to get a SincInterpolator object
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@3806 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/application/kdm/src/sinc_mod.cpp
+++ trunk/application/kdm/src/sinc_mod.cpp
@@ -1,5 +1,67 @@
 #include <we/loader/macros.hpp>
+#include <limits>
 #include "sinc_mod.hpp"
+
+namespace sinc
+{
+  struct sinc_data_t
+  {
+    sinc_data_t (float _dt = std::numeric_limits<float>::quiet_NaN())
+      : dt(_dt)
+    { }
+
+    void init()
+    {
+      s.init(dt);
+    }
+
+    // generic part ends here
+    SincInterpolator s;
+
+    // params
+    float dt;
+  };
+
+  struct sinc_data_mgr
+  {
+    sinc_data_t * get(unsigned int tid, float dt)
+    {
+      sinc_data_t * sd = interpolators[tid];
+      if (sd == NULL)
+      {
+        sd = new sinc_data_t(dt);
+        interpolators[tid] = sd;
+
+        sd->init();
+      }
+      else if ((dt - sd->dt) > std::numeric_limits<float>::epsilon())
+      {
+        sd->dt = dt;
+        sd->init();
+      }
+
+      return sd;
+    }
+
+    ~sinc_data_mgr() throw()
+    {
+      while (! interpolators.empty())
+      {
+        sinc_data_t * sd = interpolators.begin()->second;
+        interpolators.erase (interpolators.begin());
+
+        if (sd) delete sd;
+      }
+    }
+    std::map<unsigned int, sinc::sinc_data_t*> interpolators;
+  };
+}
+
+SincInterpolator * getSincInterpolator (unsigned int thread_id, float dt)
+{
+  static sinc::sinc_data_mgr sd_mgr;
+  return &(sd_mgr.get(thread_id, dt)->s);
+}
 
 static SincInterpolator ** sinc_int_array (0);
 static unsigned int NThread (0);
--- trunk/application/kdm/src/sinc_mod.hpp
+++ trunk/application/kdm/src/sinc_mod.hpp
@@ -1,6 +1,8 @@
 #ifndef KDM_SINC_MOD_HPP
 #define KDM_SINC_MOD_HPP 1
 
+#include <vector>
+#include <cassert>
 #include "utils/sincinterpolator.h"
 
 #ifdef __cplusplus
@@ -8,8 +10,8 @@ extern "C"
 {
   extern SincInterpolator ** SincIntArray();
   extern void initSincIntArray (const unsigned int num_threads, float);
+  extern SincInterpolator * getSincInterpolator (unsigned int thread_id, float dt);
 }
-
 #endif
 
 #endif
