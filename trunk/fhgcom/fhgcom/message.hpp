b3820df45e98a6ea636243887509fe7e4ccf4c02
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-10-21 11:00:23 +0000

    fhgcom: message uses vector<char>
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@3890 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/fhgcom/fhgcom/message.hpp
+++ trunk/fhgcom/fhgcom/message.hpp
@@ -1,6 +1,8 @@
 #ifndef FHG_COM_MESSAGE_HPP
 #define FHG_COM_MESSAGE_HPP 1
 
+#include <vector>
+
 namespace fhg
 {
   namespace com
@@ -8,64 +10,46 @@ namespace fhg
     struct message_t
     {
       message_t ()
-        : data_(0)
-        , length_(0)
+        : data_()
       {}
 
       explicit
       message_t (std::size_t len)
-        : data_(0)
-        , length_(len)
-      {
-        assert (len > 0);
-        data_ = new char [len];
-      }
+        : data_(len)
+      {}
 
-      message_t (char ** data, std::size_t len)
-        : data_(*data)
-        , length_(len)
-      {
-        *data = 0;
-      }
+      message_t (const char * d, std::size_t len)
+        : data_(d, d+len)
+      {}
 
       message_t (const message_t & other)
-        : data_ (0)
-        , length_(0)
-      {
-        *this = other;
-      }
+        : data_(other.data_)
+      {}
 
       ~message_t ()
-      {
-        if (data_)
-        {
-          delete [] data_; data_ = 0;
-        }
-        length_ = 0;
-      }
+      {}
 
       message_t & operator= (const message_t & rhs)
       {
         if (this != &rhs)
         {
-          this->~message_t ();
-          if (rhs.data_)
-          {
-            length_ = rhs.length_;
-            data_ = new char [length_];
-            memcpy (data_, rhs.data_, length_);
-          }
+          data_ = rhs.data_;
         }
         return *this;
       }
 
-      std::size_t length () const { return length_; }
-      const char * data () const { return data_; }
-      char * data () { return data_; }
-      void clear () { this->~message_t(); }
+      void resize (std::size_t n)
+      {
+        data_.resize (n);
+      }
+
+      std::vector<char> & data () { return data_; }
+      const std::vector<char> & data () const { return data_; }
+
+      const char * buf () const { return &data_[0]; }
+      std::size_t size () const { return data_.size(); }
     private:
-      char * data_;
-      std::size_t length_;
+      std::vector<char> data_;
     };
   }
 }
--- trunk/fhgcom/tests/test_message.cpp
+++ trunk/fhgcom/tests/test_message.cpp
@@ -8,8 +8,7 @@ BOOST_AUTO_TEST_CASE ( test_message_constr_default )
   using namespace fhg::com;
 
   message_t msg;
-  BOOST_REQUIRE ( msg.length() == 0 );
-  BOOST_REQUIRE ( msg.data() == 0 );
+  BOOST_REQUIRE ( msg.size () == 0 );
 }
 
 BOOST_AUTO_TEST_CASE ( test_message_constr_with_length )
@@ -17,31 +16,28 @@ BOOST_AUTO_TEST_CASE ( test_message_constr_with_length )
   using namespace fhg::com;
 
   message_t msg (10);
-  BOOST_REQUIRE ( msg.length() == 10 );
-  BOOST_REQUIRE ( msg.data() != 0 );
+  BOOST_REQUIRE ( msg.size() == 10 );
 }
 
 BOOST_AUTO_TEST_CASE ( test_message_constr_with_data )
 {
   using namespace fhg::com;
 
-  char * data = new char [42];
-  char * ptr = data;
-  message_t msg ( &data, 42 );
-  BOOST_REQUIRE ( msg.length() == 42 );
-  BOOST_REQUIRE ( msg.data() == ptr );
-  BOOST_REQUIRE ( data == 0 );
+  const char * data = "Hello World!";
+  message_t msg ( data, strlen(data)+1 );
+  BOOST_REQUIRE ( msg.size() == (strlen(data)+1) );
 }
 
 BOOST_AUTO_TEST_CASE ( test_message_constr_copy )
 {
   using namespace fhg::com;
-  message_t msg1 ( 10 );
+  message_t msg1 ( "abc", 4 );
   message_t msg2 ( msg1 );
-  BOOST_REQUIRE ( msg1.length() == 10 );
-  BOOST_REQUIRE ( msg2.length() == 10 );
-  BOOST_REQUIRE ( msg2.data () != 0 );
-  BOOST_REQUIRE ( msg1.data () != msg2.data() );
+  BOOST_REQUIRE ( msg1.size() == 4 );
+  BOOST_REQUIRE ( std::string(msg1.buf()) == "abc" );
+
+  BOOST_REQUIRE ( msg2.size() == 4 );
+  BOOST_REQUIRE ( std::string(msg2.buf()) == "abc" );
 }
 
 BOOST_AUTO_TEST_CASE ( test_message_assignment )
@@ -50,7 +46,5 @@ BOOST_AUTO_TEST_CASE ( test_message_assignment )
   message_t msg1;
   message_t msg2 ( 42 );
   msg1 = msg2;
-  BOOST_REQUIRE ( msg1.length() == 42 );
-  BOOST_REQUIRE ( msg1.data () != 0 );
-  BOOST_REQUIRE ( msg1.data () != msg2.data() );
+  BOOST_REQUIRE ( msg1.size() == 42 );
 }
