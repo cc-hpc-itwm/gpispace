7250105afff154d8067eaa6dc7c8afa4b130ac87
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-10-21 11:01:43 +0000

    p2p::header: different message types are possible
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@3930 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/fhgcom/fhgcom/header.hpp
+++ trunk/fhgcom/fhgcom/header.hpp
@@ -37,26 +37,45 @@ namespace fhg
       std::ostream & operator<<(std::ostream & os, address_t const &);
       std::string to_string (address_t const & a);
 
+      struct type_of_message_traits
+      {
+        // user data 0 -> 7
+        static const int USER_DATA_PACKET = 0x0;
+
+        // system data 8 -> 15
+        static const int SYSTEM_PACKET_FLAG = 0x8;
+        static const int HELLO_PACKET = 0x8;
+
+        inline static bool is_user_data (const int tom)
+        {
+          return (tom & SYSTEM_PACKET_FLAG) == 0;
+        }
+        inline static bool is_system_data (const int tom)
+        {
+          return (tom & SYSTEM_PACKET_FLAG) != 0;
+        }
+      };
+
       struct header_t
       {
         header_t ()
           : version (0)
           , flags (0)
-          , payload (0)
+          , type_of_msg (0)
+          , length (0)
         {
           memset (&src, 0, sizeof(address_t));
           memset (&dst, 0, sizeof(address_t));
         }
 
-        int16_t version :  4; // set to fixed 0 currently
-        int16_t flags   : 12; // set to fixed 0 currently
-        int16_t _reserved;    // reserved bits for later use
-        uint32_t payload;    // size of payload in bytes
-        address_t src;         // unique source address
-        address_t dst;         // unique destination address
+        uint32_t version     :  4; // set to fixed 0 currently
+        uint32_t flags       : 12; // set to fixed 0 currently
+        uint32_t type_of_msg :  4; // see type_of_message_traits
+        uint32_t reserved    : 12; // reserved bits for later use
+        uint32_t length;           // size of payload in bytes
+        address_t src;             // unique source address
+        address_t dst;             // unique destination address
       } __attribute__ ((packed));
-
-      void translate_name (std::string const & name, address_t & addr);
     }
   }
 }
