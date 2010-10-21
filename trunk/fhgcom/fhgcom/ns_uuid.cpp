4e843f8d4b4ebe235bcf107f72266ac6f2c3db64
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-10-21 11:01:34 +0000

    added a namespace uuid for fhg com
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@3925 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/fhgcom/fhgcom/ns_uuid.cpp
@@ -0,0 +1,3 @@
+#include "ns_uuid.hpp"
+
+const char * fhg::com::p2p::FHG_COM_NS_UUID = "c9fe00cb-d9f7-432e-9235-66b7929b6e2a";
---
+++ trunk/fhgcom/fhgcom/ns_uuid.hpp
@@ -0,0 +1,15 @@
+#ifndef FHG_COM_NS_UUID_HPP
+#define FHG_COM_NS_UUID_HPP 1
+
+namespace fhg
+{
+  namespace com
+  {
+    namespace p2p
+    {
+      extern const char * FHG_COM_NS_UUID;
+    }
+  }
+}
+
+#endif
