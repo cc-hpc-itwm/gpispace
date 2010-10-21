88f709d6c757f254910b9d31468ab77fa19a7a3a
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-10-21 11:00:34 +0000

    fhgcom: added test case for global kvs: kvs::put(), kvs::get()
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@3896 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/fhgcom/tests/test_global_kvs.cpp
@@ -0,0 +1,40 @@
+#define BOOST_TEST_MODULE GlobalKVSTest
+#include <boost/test/unit_test.hpp>
+
+#include <iostream>
+
+#include <fhgcom/kvs/kvsd.hpp>
+#include <fhgcom/kvs/kvsc.hpp>
+#include <fhgcom/io_service_pool.hpp>
+#include <fhgcom/tcp_server.hpp>
+
+#include <boost/thread.hpp>
+
+BOOST_AUTO_TEST_CASE ( put_get_test )
+{
+  using namespace fhg::com;
+
+  const std::string host ("localhost");
+  const std::string port ("1234");
+
+  io_service_pool pool (1);
+  kvs::server::kvsd kvsd ("");
+  tcp_server server ( pool
+                    , kvsd
+                    , host
+                    , port
+                    , true
+                    );
+
+  server.start ();
+  boost::thread thrd (boost::bind ( &fhg::com::io_service_pool::run, &pool ));
+
+  kvs::get_or_create_global_kvs (host, port);
+  kvs::put ("test_global_kvs", 42);
+  std::string val (kvs::get ("test_global_kvs").begin()->second);
+
+  BOOST_CHECK_EQUAL (val, "42");
+
+  pool.stop();
+  thrd.join();
+}
