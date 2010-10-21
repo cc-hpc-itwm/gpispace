a3f9f4b1eec60290c39fa3eb640d7fbbec7edbf7
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-10-21 11:01:39 +0000

    added test for adresses
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@3928 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/fhgcom/tests/test_p2p.cpp
@@ -0,0 +1,39 @@
+#define BOOST_TEST_MODULE P2PTest
+#include <boost/test/unit_test.hpp>
+
+#include <iostream>
+#include <fhgcom/header.hpp>
+
+BOOST_AUTO_TEST_CASE ( test_address )
+{
+  using namespace fhg::com::p2p;
+  address_t a1;
+  BOOST_CHECK_EQUAL (16ul, sizeof (a1));
+
+  header_t hdr;
+  BOOST_CHECK_EQUAL (40ul, sizeof (hdr));
+}
+
+BOOST_AUTO_TEST_CASE ( test_address_translation_equal )
+{
+  using namespace fhg::com::p2p;
+  address_t a1;
+  translate_name ("name-1", a1);
+
+  address_t a2;
+  translate_name ("name-1", a2);
+
+  BOOST_CHECK_EQUAL ( 0, memcmp (&a1, &a2, sizeof (address_t)) );
+}
+
+BOOST_AUTO_TEST_CASE ( test_address_translation_unique )
+{
+  using namespace fhg::com::p2p;
+  address_t a1;
+  translate_name ("name-1", a1);
+
+  address_t a2;
+  translate_name ("name-2", a2);
+
+  BOOST_CHECK ( memcmp (&a1, &a2, sizeof (address_t)) != 0 );
+}
