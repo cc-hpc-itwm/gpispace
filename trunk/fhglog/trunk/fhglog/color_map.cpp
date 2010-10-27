4a9b93d6697b8fc12b344115da7da8050faecac1
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-10-27 23:40:34 +0000

    fhglog: adjusted colors
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@3970 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/fhglog/trunk/fhglog/color_map.cpp
+++ trunk/fhglog/trunk/fhglog/color_map.cpp
@@ -8,12 +8,15 @@ namespace fhg
   {
     color_map_t::color_map_t ()
     {
+      // flags:
+      //    0 - normal
+      //    5 - blink
       m_color_table[LogLevel::TRACE] = color_escape_code(FG_WHITE, BG_DEFAULT, 0);
       m_color_table[LogLevel::DEBUG] = color_escape_code(FG_WHITE, BG_DEFAULT, 0);
       m_color_table[LogLevel::INFO]  = color_escape_code(FG_CYAN);
       m_color_table[LogLevel::WARN]  = color_escape_code(FG_YELLOW);
       m_color_table[LogLevel::ERROR] = color_escape_code(FG_RED);
-      m_color_table[LogLevel::FATAL] = color_escape_code(FG_MAGENTA, BG_DEFAULT, 5); // blink
+      m_color_table[LogLevel::FATAL] = color_escape_code(FG_MAGENTA, BG_DEFAULT, 0);
     }
 
     std::string const & color_map_t::reset_escape_code ()
