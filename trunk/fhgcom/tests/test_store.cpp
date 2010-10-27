1ce39d2fb7bf037dfa3b0c8d560b2c69a8b5bd04
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-10-27 21:01:30 +0000

    fhgcom: fixed test_store
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@3952 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/fhgcom/tests/test_store.cpp
+++ trunk/fhgcom/tests/test_store.cpp
@@ -1,32 +1,36 @@
 #include <boost/test/minimal.hpp>
 
 #include <fhgcom/kvs/store.hpp>
+#include <fhgcom/kvs/backend.hpp>
 
 int test_main(int, char *[])
 {
+  using namespace fhg::com::kvs;
   // create max size with cache of size 1
-  fhg::com::kvs::store store (1);
+  typedef basic_store<detail::null_backend, detail::cache> store_t;
+  detail::null_backend b;
+  store_t store (b, 1);
 
   BOOST_REQUIRE( store.cache_size() == 1 );
   BOOST_REQUIRE( store.num_cached() == 0 );
 
-  fhg::com::kvs::put (store, "fhgcom.tests.test_store.i", "42");
+  put (store, "fhgcom.tests.test_store.i", "42");
   BOOST_REQUIRE( store.num_cached() == 1 );
   {
-    int i (fhg::com::kvs::get<int> (store, "fhgcom.tests.test_store.i"));
+    int i (get<int> (store, "fhgcom.tests.test_store.i"));
     BOOST_REQUIRE( i == 42 );
   }
 
-  fhg::com::kvs::put (store, "fhgcom.tests.test_store.j", "23");
+  put (store, "fhgcom.tests.test_store.j", "23");
   BOOST_REQUIRE( store.num_cached() == 1 );
   {
-    int j (fhg::com::kvs::get<int> (store, "fhgcom.tests.test_store.j"));
+    int j (get<int> (store, "fhgcom.tests.test_store.j"));
     BOOST_REQUIRE( j == 23 );
   }
 
   try
   {
-    fhg::com::kvs::get<int> (store, "fhgcom.tests.test_store.i");
+    get<int> (store, "fhgcom.tests.test_store.i");
     BOOST_FAIL( "expected no_such exception" );
   }
   catch (std::exception const &)
