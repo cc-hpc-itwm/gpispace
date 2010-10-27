45e9f6a0dce5b9086029205acac5095c1499aca6
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-10-27 21:01:26 +0000

    util: added semaphore and event synchronization elements
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@3950 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/util/fhg/util/thread/event.hpp
@@ -0,0 +1,70 @@
+#ifndef FHG_UTIL_EVENT_HPP
+#define FHG_UTIL_EVENT_HPP 1
+
+#include <fhg/util/thread/semaphore.hpp>
+
+namespace fhg
+{
+  namespace util
+  {
+    namespace thread
+    {
+      template <typename T>
+      class event : boost::noncopyable
+      {
+        T                         m_event;
+        semaphore                 m_signal;
+        semaphore                 m_semaphore;
+      public:
+        typedef T value_type;
+
+        explicit
+        event ()
+          : m_event ()
+          , m_signal (0)
+          , m_semaphore(1)
+        {}
+
+        template <typename U>
+        explicit
+        event (U const & u)
+          : m_event (u)
+          , m_signal (0)
+          , m_semaphore(1)
+        {}
+
+        template <typename U>
+        void wait(U & u)
+        {
+          m_signal.P();
+
+          m_semaphore.P();
+          u = m_event;
+          m_semaphore.V();
+        }
+
+        template <typename U>
+        void notify(U const & u)
+        {
+          m_semaphore.P();
+          m_event = u;
+          m_semaphore.V();
+
+          m_signal.V();
+        }
+
+        void notify()
+        {
+          notify<T> (T());
+        }
+
+        std::size_t count () const
+        {
+          return m_signal.count();
+        }
+      };
+    }
+  }
+}
+
+#endif
