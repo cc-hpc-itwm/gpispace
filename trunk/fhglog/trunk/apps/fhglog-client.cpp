a458aba9939f762498ad8c11ef5c2cc4c63074e6
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-10-27 21:01:49 +0000

    fhglog: more sophisticated fhglog client
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@3963 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/fhglog/trunk/apps/fhglog-client.cpp
+++ trunk/fhglog/trunk/apps/fhglog-client.cpp
@@ -19,52 +19,84 @@
 #include <sstream> // ostringstream
 #include <fhglog/fhglog.hpp>
 #include <fhglog/remote/RemoteAppender.hpp>
+#include <boost/program_options.hpp>
 
-void usage()
-{
-  std::cerr << "usage: fhglog-client [-h] ip:port message..." << std::endl;
-  std::cerr << "\thost:port - server to use (default: " << FHGLOG_DEFAULT_HOST << ":" << FHGLOG_DEFAULT_PORT << ")" << std::endl;
-  std::cerr << "\tmessage - the message to log" << std::endl;
-}
+namespace po = boost::program_options;
 
 int main (int argc, char **argv)
 {
   using namespace fhg::log;
 
-  std::string message("hello server!");
-  std::string server(FHGLOG_DEFAULT_LOCATION);
+  po::options_description desc("options");
 
-  if (argc <= 1)
+  unsigned short port (FHGLOG_DEFAULT_PORT);
+  std::string host ("localhost");
+  std::string file ("fhglog-client.cpp");
+  int line (0);
+  std::string message;
+  std::string tag("default");
+  int level (LogLevel::DEF_LEVEL);
+
+  desc.add_options()
+    ("help,h", "this message")
+    ("port,P", po::value<unsigned short>(&port)->default_value(port), "port to connect to")
+    ("host,H", po::value<std::string>(&host)->default_value(host), "host to send to")
+    ("message,m", po::value<std::string>(&message), "message to send")
+    ("priority,p", po::value<int>(&level)->default_value(level), "log level to use (0-5)")
+    ("tag,t", po::value<std::string>(&tag)->default_value(tag), "category tag of the message")
+    ("file,F", po::value<std::string>(&file)->default_value(file), "filename of the event")
+    ("line,L", po::value<int>(&line)->default_value(line), "line number of the event")
+    ("version,V", "print version information")
+    ("dumpversion", "dump version information")
+    ;
+
+  po::variables_map vm;
+  try
   {
-    usage();
-    return 1;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+  }
+  catch (std::exception const & ex)
+  {
+    std::cerr << "invalid argument: " << ex.what() << std::endl;
+    std::cerr << "try " << argv[0] << " -h to get some help" << std::endl;
+    return EXIT_FAILURE;
+  }
+  po::notify(vm);
+
+  if (vm.count("version"))
+  {
+    std::cerr << "FhgLog Client v" << FHGLOG_VERSION << std::endl;
+    return EXIT_SUCCESS;
   }
 
-  if (argc > 1)
+  if (vm.count("dumpversion"))
   {
-    if (std::string(argv[1]) == "-h")
-    {
-      usage();
-      return 0;
-    }
-    server = argv[1];
+    std::cerr << FHGLOG_VERSION << std::endl;
+    return EXIT_SUCCESS;
   }
 
-  logger_t log(getLogger());
-  log.addAppender(Appender::ptr_t(new remote::RemoteAppender("remote", server)));
-
-  std::ostringstream msg;
-  for (int i = 2; ;)
+  if (vm.count("help"))
   {
-    if (i == argc) break;
-
-    msg << argv[i];
-    ++i;
-
-    if (i < argc)
-      msg << " ";
+    std::cerr << "usage: " << argv[0] << " [options]" << std::endl;
+    std::cerr << desc << std::endl;
+    return EXIT_SUCCESS;
   }
 
-  log.log(FHGLOG_MKEVENT_HERE(INFO, msg.str()));
+  std::ostringstream sstr;
+  sstr << host << ":" << port;
+  std::string location (sstr.str());
+
+  remote::RemoteAppender r ("remote", location);
+
+  if (level < LogLevel::MIN_LEVEL || level > LogLevel::MAX_LEVEL)
+    level = LogLevel::DEF_LEVEL;
+  LogEvent e( (LogLevel::Level)level
+            , file
+            , ""
+            , line
+            , message
+            );
+  e.logged_via() = tag;
+  r.append (e);
   return 0;
 }
