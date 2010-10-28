6c362853197c1906a25005d7a3f18e654860cf93
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-10-28 16:48:43 +0000

    sdpa: preparations for tcp communication
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@3986 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/sdpa/trunk/sdpa/com/NetworkStrategy.hpp
@@ -0,0 +1,6 @@
+#ifndef SDPA_COM_NETWORK_STRATEGY_HPP
+#define SDPA_COM_NETWORK_STRATEGY_HPP 1
+
+#include <fhgcom/peer.hpp>
+
+#endif
--- trunk/sdpa/trunk/sdpa/com/manager.tcc
+++
@@ -1,2 +0,0 @@
-// manager implementation header file -*- C++ -*-
-
