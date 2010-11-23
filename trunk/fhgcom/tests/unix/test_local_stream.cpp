4827084dbc5f5ca9b4821b9bcce00b97323b5108
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-11-23 12:54:07 +0000

    testcase (not compilable) to demonstrate usage of the domain socket interface
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@4112 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/fhgcom/tests/unix/test_local_stream.cpp
@@ -0,0 +1,21 @@
+#define BOOST_TEST_MODULE UnixStream
+#include <boost/test/unit_test.hpp>
+
+#include <fhgcom/unix/connection.hpp>
+
+struct backend
+{
+  on_connect (
+};
+
+BOOST_AUTO_TEST_CASE (test_server)
+{
+  using namespace fhg::com::unix;
+
+  typedef server_t<backend<connection_t<message_t> > > my_server;
+  my_server unix_stream ("/tmp/foo");
+
+  unix_stream.start ();
+
+  unix_stream.stop();
+}
