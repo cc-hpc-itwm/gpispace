4642f950c10fe584748ba4f89ddf9dad8c03effb
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-11-24 18:17:10 +0000

    added a descriptive comment
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@4147 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/util/fhg/util/ini-parser.hpp
+++ trunk/util/fhg/util/ini-parser.hpp
@@ -6,22 +6,73 @@
 
 #include <boost/function.hpp>
 
+/*
+  simple ini-file parser with some additional features:
+
+  %include <path>
+     includes the specified path (relative paths are also possible)
+
+     not yet implemented ;-(
+
+  section-ids:
+
+      [section]         no section id
+         key = val
+      [section ""]      empty section id
+         key = val
+      [section "id"]    section-id 'id'
+         key = val
+
+      this enables stuff like:
+
+      [appender "console"]
+         level = 1
+         type  = stream
+         sink  = stderr
+      [appender "file"]
+         level = 2
+         type  = file
+         sink  = /var/log/...
+      [appender "net"]
+         level = 1
+         type = net
+         sink = 127.0.0.1:1234
+      [logger ""]
+         level = 0
+         appender = console,file,net
+
+    in a flat view this can be represented as:
+
+      appender.console.level = 1
+      appender.console.type = stream
+
+    and so on, section-ids can contain '.', so that
+    stuff like
+
+      logging.appender.console.level = 1
+
+    can be represented as
+
+      [logging "appender.console"]
+        level = 1
+*/
+
 namespace fhg
 {
-  namespace util
-  {
-    namespace ini
-    {
-      typedef boost::function<int ( std::string const & section
-                                  , std::string const * section_id // 3 states: set, empty, notset (=NULL)
-                                  , std::string const & key
-                                  , std::string const & value
-                                  ) > entry_handler_t;
+namespace util
+{
+namespace ini
+{
+  typedef boost::function<int ( std::string const & section
+                              , std::string const * section_id // 3 states: set, empty, notset (=NULL)
+                              , std::string const & key
+                              , std::string const & value
+                              ) > entry_handler_t;
 
-      void parse (std::istream & is, entry_handler_t handler);
-      void parse (std::string const & path, entry_handler_t handler);
-    }
-  }
+  void parse (std::istream & is, entry_handler_t handler);
+  void parse (std::string const & path, entry_handler_t handler);
+}
+}
 }
 
 #include "ini-parser.tcc"
