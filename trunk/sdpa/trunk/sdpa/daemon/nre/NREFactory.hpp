49b24c554f480c5d408565fff1fee355318db985
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2010-11-25 10:15:41 +0000

    added NRE factory
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@4156 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/sdpa/trunk/sdpa/daemon/nre/NREFactory.hpp
@@ -0,0 +1,99 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  NRE.hpp
+ *
+ *    Description:  Contains the NRE class
+ *
+ *        Version:  1.0
+ *        Created:
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dr. Tiberiu Rotaru, tiberiu.rotaru@itwm.fraunhofer.de
+ *        Company:  Fraunhofer ITWM
+ *
+ * =====================================================================================
+ */
+#ifndef SDPA_NRE_FACTORY_HPP
+#define SDPA_NRE_FACTORY_HPP 1
+
+#include <sdpa/daemon/nre/NRE.hpp>
+#include <sdpa/daemon/EmptyWorkflowEngine.hpp>
+
+
+namespace sdpa {
+namespace daemon {
+
+		template <typename T, typename U >
+		struct NREFactory
+		{
+			static typename sdpa::shared_ptr<NRE<U> > create(   const std::string& name
+												   , const std::string& url
+												   , const std::string& masterName
+												   //, const std::string& masterUrl
+												   , const std::string& workerUrl
+												   , const std::string guiUrl = "127.0.0.1:9000"
+												   , bool bLaunchNrePcd = false
+												   , const std::string & fvmPCBinary = ""
+												   , const std::vector<std::string> & fvmPCSearchPath = std::vector<std::string>()
+												   , const std::vector<std::string> & fvmPCPreLoad = std::vector<std::string>()
+												   )
+			{
+
+
+
+				typename sdpa::shared_ptr<NRE<U> > pNRE( new NRE<U>( name
+													   , url
+													   , masterName
+													   //, masterUrl
+													   , workerUrl
+													   , guiUrl
+													   , bLaunchNrePcd
+													   , fvmPCBinary
+													   , fvmPCSearchPath
+													   , fvmPCPreLoad
+													   ) );
+
+				pNRE->template create_workflow_engine<T>();
+				return pNRE;
+			}
+		};
+
+		template <typename U>
+		struct NREFactory<void, U>
+		{
+			static typename sdpa::shared_ptr<NRE<U> > create(  const std::string& name
+												   , const std::string& url
+												   , const std::string& masterName
+												   //, const std::string& masterUrl
+												   , const std::string& workerUrl
+												   , const std::string guiUrl = "127.0.0.1:9000"
+												   , bool bLaunchNrePcd = false
+												   , const std::string & fvmPCBinary = ""
+												   , const std::vector<std::string> & fvmPCSearchPath = std::vector<std::string>()
+												   , const std::vector<std::string> & fvmPCPreLoad = std::vector<std::string>()
+												   )
+			{
+				typename sdpa::shared_ptr<NRE<U> > pNRE( new NRE<U>(   name
+														   , url
+														   , masterName
+														   //, masterUrl
+														   , workerUrl
+														   , guiUrl
+														   , bLaunchNrePcd
+														   , fvmPCBinary
+														   , fvmPCSearchPath
+														   , fvmPCPreLoad
+														   )
+                      );
+
+				return pNRE;
+			}
+		};
+
+	}
+}
+
+
+#endif //SDPA_NRE_HPP
