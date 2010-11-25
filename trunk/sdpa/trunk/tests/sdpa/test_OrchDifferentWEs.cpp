1664435a80359dd29bea385a0cb709e90dbb0842
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2010-11-25 15:39:37 +0000

    added test for the aggregator without WE
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@4170 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/tests/sdpa/test_OrchDifferentWEs.cpp
+++ trunk/sdpa/trunk/tests/sdpa/test_OrchDifferentWEs.cpp
@@ -182,6 +182,111 @@ struct MyFixture
 
 BOOST_FIXTURE_TEST_SUITE( test_orchestrator_empty_we, MyFixture )
 
+BOOST_AUTO_TEST_CASE( testOrchAandAggNoWe )
+{
+	LOG( DEBUG, "***** testOrchestratorEmptyWe *****"<<std::endl);
+	string strGuiUrl   = "";
+	string workerUrl = "127.0.0.1:5500";
+	string addrOrch = "127.0.0.1";
+	string addrAgg = "127.0.0.1";
+	string addrNRE = "127.0.0.1";
+
+	typedef void OrchWorkflowEngine;
+
+	bool bLaunchNrePcd = true;
+
+	m_strWorkflow = read_workflow("workflows/stresstest.pnet");
+	LOG( DEBUG, "The test workflow is "<<m_strWorkflow);
+
+	LOG( DEBUG, "Create Orchestrator with an empty workflow engine ...");
+	sdpa::daemon::Orchestrator::ptr_t ptrOrch = sdpa::daemon::OrchestratorFactory<void>::create("orchestrator_0", addrOrch);
+	sdpa::daemon::Orchestrator::start(ptrOrch);
+
+	LOG( DEBUG, "Create the Aggregator ...");
+	sdpa::daemon::Aggregator::ptr_t ptrAgg = sdpa::daemon::AggregatorFactory<void>::create("aggregator_0", addrAgg,"orchestrator_0");
+	sdpa::daemon::Aggregator::start(ptrAgg);
+
+	std::vector<std::string> v_fake_PC_search_path;
+	v_fake_PC_search_path.push_back(TESTS_EXAMPLE_STRESSTEST_MODULES_PATH);
+
+	std::vector<std::string> v_module_preload;
+	v_module_preload.push_back(TESTS_FVM_PC_FAKE_MODULE);
+
+	// use external scheduler and real GWES
+	LOG( DEBUG, "Create the NRE ...");
+	sdpa::daemon::NRE<sdpa::nre::worker::NreWorkerClient>::ptr_t
+		ptrNRE_0 = sdpa::daemon::NREFactory<RealWorkflowEngine, sdpa::nre::worker::NreWorkerClient>::create("NRE_0",
+				                             addrNRE,"aggregator_0",
+				                             workerUrl,
+				                             strGuiUrl,
+				                             bLaunchNrePcd,
+				                             TESTS_NRE_PCD_BIN_PATH,
+				                             v_fake_PC_search_path,
+				                             v_module_preload );
+
+	try {
+		sdpa::daemon::NRE<sdpa::nre::worker::NreWorkerClient>::start(ptrNRE_0);
+	}
+	catch (const std::exception &ex) {
+		LOG( FATAL, "Could not start NRE: " << ex.what());
+		LOG( WARN, "TODO: implement NRE-PCD fork/exec with a RestartStrategy->restart()");
+
+		sdpa::daemon::NRE<sdpa::nre::worker::NreWorkerClient>::shutdown(ptrNRE_0);
+		sdpa::daemon::Aggregator::shutdown(ptrAgg);
+		sdpa::daemon::Orchestrator::shutdown(ptrOrch);
+
+		return;
+	}
+
+	sdpa::client::config_t config = sdpa::client::ClientApi::config();
+
+	std::vector<std::string> cav;
+	cav.push_back("--orchestrator=orchestrator_0");
+	config.parse_command_line(cav);
+
+	sdpa::client::ClientApi::ptr_t ptrCli = sdpa::client::ClientApi::create( config );
+	ptrCli->configure_network( config );
+
+	for( int k=0; k<m_nITER; k++ )
+	{
+		sdpa::job_id_t job_id_user = ptrCli->submitJob(m_strWorkflow);
+
+		LOG( DEBUG, "*****JOB #"<<k<<"******");
+
+		std::string job_status = ptrCli->queryJob(job_id_user);
+		LOG( DEBUG, "The status of the job "<<job_id_user<<" is "<<job_status);
+
+		while( job_status.find("Finished") == std::string::npos &&
+			   job_status.find("Failed") == std::string::npos &&
+			   job_status.find("Cancelled") == std::string::npos)
+		{
+			job_status = ptrCli->queryJob(job_id_user);
+			LOG( DEBUG, "The status of the job "<<job_id_user<<" is "<<job_status);
+
+			usleep(5*m_sleep_interval);
+		}
+
+		LOG( DEBUG, "User: retrieve results of the job "<<job_id_user);
+		ptrCli->retrieveResults(job_id_user);
+
+		LOG( DEBUG, "User: delete the job "<<job_id_user);
+		ptrCli->deleteJob(job_id_user);
+	}
+
+	sdpa::daemon::NRE<sdpa::nre::worker::NreWorkerClient>::shutdown(ptrNRE_0);
+	sdpa::daemon::Aggregator::shutdown(ptrAgg);
+	sdpa::daemon::Orchestrator::shutdown(ptrOrch);
+
+	ptrCli->shutdown_network();
+    ptrCli.reset();
+	seda::StageRegistry::instance().stopAll();
+	seda::StageRegistry::instance().clear();
+
+	sleep(1);
+	LOG( DEBUG, "The test case testOrchestratorEmptyWe terminated!");
+}
+
+#if 0
 BOOST_AUTO_TEST_CASE( testOrchestratorNoWe )
 {
 	LOG( DEBUG, "***** testOrchestratorEmptyWe *****"<<std::endl);
@@ -597,5 +702,6 @@ BOOST_AUTO_TEST_CASE( testOrchestratorEmptyWe2Aggs )
 	sleep(1);
 	LOG( DEBUG, "The test case testOrchestratorEmptyWe terminated!");
 }
+#endif
 
 BOOST_AUTO_TEST_SUITE_END()
