632e589f99b11f571c43c2c8807866c819775a26
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2010-12-08 11:12:04 +0000

    renamed FindReadline.cmake to FindLibReadline.cmake
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@4250 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/modules/trunk/FindLibReadline.cmake
@@ -0,0 +1,39 @@
+# -*- mode: cmake; -*-
+# locates the libreadline
+# This file defines:
+# * LibReadline_FOUND if libreadline was found
+# * LibReadline_LIBRARY The lib to link to (currently only a static unix lib)
+# * LibReadline_LIBRARY_SHARED The lib to link to (currently only a static unix lib)
+# * LibReadline_INCLUDE_DIR
+
+if (NOT LibReadline_FIND_QUIETLY)
+  message(STATUS "FindLibReadline check")
+endif (NOT LibReadline_FIND_QUIETLY)
+
+find_path (LibReadline_INCLUDE_DIR
+  NAMES "readline/readline.h"
+  HINTS ${LIBREADLINE_HOME} ENV LIBREADLINE_HOME
+  PATH_SUFFIXES include
+  )
+
+find_library (LibReadline_LIBRARY
+  NAMES libreadline.a
+  HINTS ${LIBREADLINE_HOME} ENV LIBREADLINE_HOME
+  PATH_SUFFIXES lib lib64
+  )
+find_library (LibReadline_LIBRARY_SHARED
+  NAMES libreadline.so
+  HINTS ${LIBREADLINE_HOME} ENV LIBREADLINE_HOME
+  PATH_SUFFIXES lib lib64
+  )
+
+if (LibReadline_INCLUDE_DIR AND LibReadline_LIBRARY)
+  set (LibReadline_FOUND TRUE)
+  if (NOT LibReadline_FIND_QUIETLY)
+    message (STATUS "Found LibReadline headers in ${LibReadline_INCLUDE_DIR} and libraries ${LibReadline_LIBRARY} ${LibReadline_LIBRARY_SHARED}")
+  endif (NOT LibReadline_FIND_QUIETLY)
+else (LibReadline_INCLUDE_DIR AND LibReadline_LIBRARY)
+  if (LibReadline_FIND_REQUIRED)
+    message (FATAL_ERROR "LibReadline could not be found!")
+  endif (LibReadline_FIND_REQUIRED)
+endif (LibReadline_INCLUDE_DIR AND LibReadline_LIBRARY)
