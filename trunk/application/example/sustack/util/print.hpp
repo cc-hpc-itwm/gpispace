0fd811bc904259dd7f26ea9652f6d4676334cb63
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2011-01-10 10:32:46 +0000

    make sustack running correctly
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@4410 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/application/example/sustack/util/print.hpp
+++ trunk/application/example/sustack/util/print.hpp
@@ -6,6 +6,7 @@
 #include <pnetc/type/package_to_be_written.hpp>
 
 #include <iostream>
+#include <iomanip>
 #include <sstream>
 
 namespace print
@@ -45,6 +46,76 @@ namespace print
 
     os << ::print::loaded_package (p.loaded_package)
        << " for offset " << p.offset;
+
+    return os.str();
+  }
+
+  inline void add_line ( std::ostringstream & os
+                       , const std::string & line
+                       , std::string & old
+                       , unsigned int & c
+                       )
+  {
+    if (old == line)
+      {
+        ++c;
+      }
+    else
+      {
+        os << line;
+
+        if (c > 1)
+          {
+            os << "[" << c << "]" << std::endl;
+          }
+
+        c = 0;
+        old = line;
+      }
+  }
+
+  inline std::string trace ( void * pos
+                           , const unsigned int & size
+                           )
+  {
+    unsigned int l (1);
+    int * a ((int *)pos);
+
+    std::ostringstream os;
+
+    std::string old ("");
+    std::string line ("");
+    unsigned int c (0);
+
+    for (unsigned int i (0); i < size / sizeof(int); ++i, ++a, ++l)
+      {
+        std::ostringstream f;
+
+        f << std::setw(9) << std::hex << *a;
+
+        if (l % 8 == 0)
+          {
+            f << std::endl;
+
+            line += f.str();
+
+            add_line (os, line, old, c);
+
+            line.clear();
+          }
+        else
+          {
+            line += f.str();
+          }
+      }
+
+    std::ostringstream f;
+
+    f << std::endl;
+
+    line += f.str();
+
+    add_line (os, line, old, c);
 
     return os.str();
   }
