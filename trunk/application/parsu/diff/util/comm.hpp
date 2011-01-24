4f25b83e6f565e34fb8bbb26c46ae68211966e17
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2011-01-24 13:32:23 +0000

    op2 -> diff for implementation with Daniels superoperators
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@4590 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/application/parsu/diff/util/comm.hpp
@@ -0,0 +1,66 @@
+#ifndef _COMM_HPP
+#define _COMM_HPP 1
+
+#include <pnetc/type/config.hpp>
+#include <pnetc/type/loaded_package.hpp>
+
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+#include <fvm-pc/pc.hpp>
+
+#include <print.hpp>
+
+namespace comm
+{
+  inline void put
+  ( const ::pnetc::type::config::config config
+  , const ::pnetc::type::loaded_package::loaded_package package
+  , const long shmem_offset = 0
+  )
+  {
+    const long size (package.package.num.trace * package.package.size.trace);
+
+    LOG (INFO, "comm::put " << ::print::loaded_package (package)
+        << " offset " << package.slot * config.size.bunch
+        << " to shmem_offset " << shmem_offset
+        << " (" << size << " bytes)"
+        );
+
+    waitComm ( fvmPutGlobalData
+               ( static_cast<fvmAllocHandle_t> (config.handle.data)
+               , package.slot * config.size.bunch
+               , size
+               , shmem_offset
+               , static_cast<fvmAllocHandle_t> (config.handle.scratch)
+               )
+             );
+  }
+
+  inline void get
+  ( const ::pnetc::type::config::config config
+  , const ::pnetc::type::loaded_package::loaded_package package
+  , const long shmem_offset = 0
+  )
+  {
+    const long size (package.package.num.trace * package.package.size.trace);
+
+    LOG (INFO, "comm::get " << ::print::loaded_package (package)
+        << " offset " << package.slot * config.size.bunch
+        << " from shmem_offset " << shmem_offset
+        << " (" << size << " bytes)"
+        );
+
+    waitComm ( fvmGetGlobalData
+               ( static_cast<fvmAllocHandle_t> (config.handle.data)
+               , package.slot * config.size.bunch
+               , size
+               , shmem_offset
+               , static_cast<fvmAllocHandle_t> (config.handle.scratch)
+               )
+             );
+  }
+}
+
+#endif
