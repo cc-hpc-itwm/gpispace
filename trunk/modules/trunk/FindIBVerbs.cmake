5275894f8f4eb6b3e1c49be2d45f401888d98c8d
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-01-30 14:07:18 +0000

    added cmake find module for libibverbs
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@4669 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/modules/trunk/FindIBVerbs.cmake
@@ -0,0 +1,34 @@
+# -*- mode: cmake; -*-
+# locates the ibverbs library
+# This file defines:
+# * IBverbs_FOUND if libibverbs was found
+# * IBverbs_LIBRARY The lib to link to (currently only a static unix lib)
+# * IBverbs_LIBRARY_SHARED The lib to link to (currently only a static unix lib)
+# * IBverbs_INCLUDE_DIR
+
+if (NOT IBverbs_FIND_QUIETLY)
+  message(STATUS "FindIBverbs check")
+endif (NOT IBverbs_FIND_QUIETLY)
+
+find_path (IBverbs_INCLUDE_DIR
+  NAMES "infiniband/verbs.h"
+  HINTS ${LIBIBVERBS_HOME} ENV LIBIBVERBS_HOME
+  PATH_SUFFIXES include
+  )
+
+find_library (IBverbs_LIBRARY
+  NAMES ibverbs
+  HINTS ${LIBIBVERBS_HOME} ENV LIBIBVERBS_HOME
+  PATH_SUFFIXES lib lib64
+  )
+
+if (IBverbs_INCLUDE_DIR AND IBverbs_LIBRARY)
+  set (IBverbs_FOUND TRUE)
+  if (NOT IBverbs_FIND_QUIETLY)
+    message (STATUS "Found IBverbs headers in ${IBverbs_INCLUDE_DIR} and libraries ${IBverbs_LIBRARY} ${IBverbs_LIBRARY_SHARED}")
+  endif (NOT IBverbs_FIND_QUIETLY)
+else (IBverbs_INCLUDE_DIR AND IBverbs_LIBRARY)
+  if (IBverbs_FIND_REQUIRED)
+    message (FATAL_ERROR "IBverbs could not be found!")
+  endif (IBverbs_FIND_REQUIRED)
+endif (IBverbs_INCLUDE_DIR AND IBverbs_LIBRARY)
