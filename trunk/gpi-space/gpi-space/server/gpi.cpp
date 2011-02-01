009441bf08db8f30c23a9938fd842998446b88ea
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-02-01 16:13:33 +0000

    removed "real" namespace
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@4697 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/gpi-space/gpi-space/server/gpi.cpp
+++ trunk/gpi-space/gpi-space/server/gpi.cpp
@@ -17,248 +17,245 @@ namespace gpi
   {
     typedef boost::function<void (int)> signal_handler_t;
 
-    namespace real
+    gpi_api_t::gpi_api_t ()
+      : m_ac (0)
+      , m_av (NULL)
+      , m_startup_done (false)
+      , m_rank (std::numeric_limits<rank_t>::max())
+      , m_num_nodes (0)
+      , m_mem_size (0)
+    { }
+
+    gpi_api_t::~gpi_api_t ()
     {
-      gpi_api_t::gpi_api_t ()
-        : m_ac (0)
-        , m_av (NULL)
-        , m_startup_done (false)
-        , m_rank (std::numeric_limits<rank_t>::max())
-        , m_num_nodes (0)
-        , m_mem_size (0)
-      { }
-
-      gpi_api_t::~gpi_api_t ()
+      try
       {
-        try
-        {
-          stop ();
-        }
-        catch (std::exception const & ex)
-        {
-          LOG(WARN, "could not stop real::gpi_api_t on rank " << rank());
-        }
+        stop ();
       }
-
-      void gpi_api_t::init (int ac, char *av[])
+      catch (std::exception const & ex)
       {
-        m_ac = ac;
-        m_av = av;
+        LOG(WARN, "could not stop real::gpi_api_t on rank " << rank());
       }
+    }
 
-      void gpi_api_t::set_memory_size (const gpi::size_t sz)
-      {
-        m_mem_size = sz;
-      }
+    void gpi_api_t::init (int ac, char *av[])
+    {
+      m_ac = ac;
+      m_av = av;
+    }
 
-      // wrapped C function calls
-      void gpi_api_t::start (const gpi::timeout_t timeout)
-      {
-        // fire up a thread that handles signals
+    void gpi_api_t::set_memory_size (const gpi::size_t sz)
+    {
+      m_mem_size = sz;
+    }
+
+    // wrapped C function calls
+    void gpi_api_t::start (const gpi::timeout_t timeout)
+    {
+      // fire up a thread that handles signals
 
 
-        // 1. register alarm signal handler
-        /*
+      // 1. register alarm signal handler
+      /*
         signal_handler_t handler
-          (boost::bind ( &gpi_api_t::handle_alarm
-                       , this
-                       , _1
-                       )
-          );
-        */
+        (boost::bind ( &gpi_api_t::handle_alarm
+        , this
+        , _1
+        )
+        );
+      */
 
-        alarm (timeout);
+      alarm (timeout);
 
-        // 2. start alarm
-        // 3. start gpi
-        // 4a. if alarm before start_gpi returns
-        //        - restore old alarm handler
-        //        - throw
-        // 4b. else
-        //        - restore old alarm handler
-      }
+      // 2. start alarm
+      // 3. start gpi
+      // 4a. if alarm before start_gpi returns
+      //        - restore old alarm handler
+      //        - throw
+      // 4b. else
+      //        - restore old alarm handler
+    }
 
-      void gpi_api_t::stop ()
-      {
-      }
+    void gpi_api_t::stop ()
+    {
+    }
 
-      void gpi_api_t::kill ()
-      {
-      }
+    void gpi_api_t::kill ()
+    {
+    }
 
-      gpi::size_t gpi_api_t::number_of_counters () const
-      {
-        return 0;
-      }
+    gpi::size_t gpi_api_t::number_of_counters () const
+    {
+      return 0;
+    }
 
-      gpi::size_t gpi_api_t::number_of_queues () const
-      {
-        return 0;
-      }
+    gpi::size_t gpi_api_t::number_of_queues () const
+    {
+      return 0;
+    }
 
-      gpi::size_t gpi_api_t::queue_depth () const
-      {
-        return 0;
-      }
+    gpi::size_t gpi_api_t::queue_depth () const
+    {
+      return 0;
+    }
 
-      gpi::version_t gpi_api_t::version () const
-      {
-        return 0;
-      }
+    gpi::version_t gpi_api_t::version () const
+    {
+      return 0;
+    }
 
-      gpi::port_t gpi_api_t::port () const
-      {
-        return 0;
-      }
+    gpi::port_t gpi_api_t::port () const
+    {
+      return 0;
+    }
 
-      gpi::size_t gpi_api_t::number_of_nodes () const
-      {
-        return 1;
-      }
+    gpi::size_t gpi_api_t::number_of_nodes () const
+    {
+      return 1;
+    }
 
-      std::string gpi_api_t::hostname (const gpi::rank_t) const
-      {
-        return "localhost";
-      }
+    std::string gpi_api_t::hostname (const gpi::rank_t) const
+    {
+      return "localhost";
+    }
 
-      gpi::rank_t gpi_api_t::rank () const
-      {
-        return 0;
-      }
+    gpi::rank_t gpi_api_t::rank () const
+    {
+      return 0;
+    }
 
-      void * gpi_api_t::dma_ptr (void)
-      {
-        return 0;
-      }
+    void * gpi_api_t::dma_ptr (void)
+    {
+      return 0;
+    }
 
-      void gpi_api_t::set_network_type (const gpi::network_type_t)
-      {
-      }
+    void gpi_api_t::set_network_type (const gpi::network_type_t)
+    {
+    }
 
-      void gpi_api_t::set_port (const gpi::port_t)
-      {
-      }
+    void gpi_api_t::set_port (const gpi::port_t)
+    {
+    }
 
-      void gpi_api_t::set_mtu (const gpi::size_t)
-      {
-      }
+    void gpi_api_t::set_mtu (const gpi::size_t)
+    {
+    }
 
-      void gpi_api_t::set_number_of_processes (const gpi::size_t)
-      {
-      }
+    void gpi_api_t::set_number_of_processes (const gpi::size_t)
+    {
+    }
 
-      bool gpi_api_t::ping (const gpi::rank_t) const
-      {
-        return false;
-      }
-      bool gpi_api_t::ping (std::string const & hostname) const
-      {
-        return false;
-      }
-      bool gpi_api_t::is_master (void) const
-      {
-        return true;
-      }
-      bool gpi_api_t::is_slave (void) const
-      {
-        return false;
-      }
+    bool gpi_api_t::ping (const gpi::rank_t) const
+    {
+      return false;
+    }
+    bool gpi_api_t::ping (std::string const & hostname) const
+    {
+      return false;
+    }
+    bool gpi_api_t::is_master (void) const
+    {
+      return true;
+    }
+    bool gpi_api_t::is_slave (void) const
+    {
+      return false;
+    }
 
-      void gpi_api_t::barrier (void) const
-      {
-      }
+    void gpi_api_t::barrier (void) const
+    {
+    }
 
-      void gpi_api_t::read_dma ( const offset_t local_offset
-                               , const offset_t remote_offset
-                               , const size_t amount
-                               , const rank_t from_node
-                               , const queue_desc_t queue
-                               )
-      {
-      }
-      void gpi_api_t::write_dma ( const offset_t local_offset
-                                , const offset_t remote_offset
-                                , const size_t amount
-                                , const rank_t to_node
-                                , const queue_desc_t queue
-                                )
-      {
-      }
+    void gpi_api_t::read_dma ( const offset_t local_offset
+                             , const offset_t remote_offset
+                             , const size_t amount
+                             , const rank_t from_node
+                             , const queue_desc_t queue
+                             )
+    {
+    }
+    void gpi_api_t::write_dma ( const offset_t local_offset
+                              , const offset_t remote_offset
+                              , const size_t amount
+                              , const rank_t to_node
+                              , const queue_desc_t queue
+                              )
+    {
+    }
 
-      void gpi_api_t::send_dma ( const offset_t local_offset
-                               , const size_t amount
-                               , const rank_t to_node
-                               , const queue_desc_t queue
-                               )
-      {
-      }
-      void gpi_api_t::recv_dma ( const offset_t local_offset
-                               , const size_t size
-                               , const rank_t from_node
-                               , const queue_desc_t queue
-                               )
-      {
-      }
+    void gpi_api_t::send_dma ( const offset_t local_offset
+                             , const size_t amount
+                             , const rank_t to_node
+                             , const queue_desc_t queue
+                             )
+    {
+    }
+    void gpi_api_t::recv_dma ( const offset_t local_offset
+                             , const size_t size
+                             , const rank_t from_node
+                             , const queue_desc_t queue
+                             )
+    {
+    }
 
-      size_t gpi_api_t::wait_dma (const queue_desc_t queue)
-      {
-        return 0;
-      }
+    size_t gpi_api_t::wait_dma (const queue_desc_t queue)
+    {
+      return 0;
+    }
 
-      void gpi_api_t::send_passive ( const offset_t local_offset
-                                   , const size_t size
-                                   , const rank_t to_rank
-                                   )
-      {
-      }
+    void gpi_api_t::send_passive ( const offset_t local_offset
+                                 , const size_t size
+                                 , const rank_t to_rank
+                                 )
+    {
+    }
 
-      void gpi_api_t::recv_passive ( const offset_t local_offset
-                                   , const size_t size
-                                   , rank_t & from_rank
-                                   )
-      {
-      }
-      void gpi_api_t::wait_passive ( void )
-      {
-      }
+    void gpi_api_t::recv_passive ( const offset_t local_offset
+                                 , const size_t size
+                                 , rank_t & from_rank
+                                 )
+    {
+    }
+    void gpi_api_t::wait_passive ( void )
+    {
+    }
 
-      // ***** private functions
-      void gpi_api_t::signal_handler ()
-      {
-        // signal handler thread
-        char buf[1024];
-        sigset_t restrict;
-        sigfillset (&restrict);
-        int sig (0);
+    // ***** private functions
+    void gpi_api_t::signal_handler ()
+    {
+      // signal handler thread
+      char buf[1024];
+      sigset_t restrict;
+      sigfillset (&restrict);
+      int sig (0);
 
-        for (;;)
+      for (;;)
+      {
+        int ec = sigwait (&restrict, &sig);
+        if (ec == 0)
         {
-          int ec = sigwait (&restrict, &sig);
-          if (ec == 0)
-          {
-            handle_signal (sig);
-          }
-          else
-          {
-            LOG( ERROR
-               , "sigwait() returned an error [" << ec << "]: " << strerror_r ( ec
-                                                                              , buf
-                                                                              , sizeof(buf)
-                                                                              )
-               );
-          }
+          handle_signal (sig);
+        }
+        else
+        {
+          LOG( ERROR
+             , "sigwait() returned an error [" << ec << "]: " << strerror_r ( ec
+                                                                            , buf
+                                                                            , sizeof(buf)
+                                                                            )
+             );
         }
       }
+    }
 
-      void gpi_api_t::handle_signal (int sig)
+    void gpi_api_t::handle_signal (int sig)
+    {
+      switch (sig)
       {
-        switch (sig)
-        {
-        case SIGALRM:
-          m_startup_done = false;
-          break;
-        };
-      }
+      case SIGALRM:
+        m_startup_done = false;
+        break;
+      };
     }
   }
 }
--- trunk/gpi-space/gpi-space/server/gpi.hpp
+++ trunk/gpi-space/gpi-space/server/gpi.hpp
@@ -44,94 +44,91 @@ namespace gpi
       };
     }
 
-    namespace real
+    class gpi_api_t
     {
-      class gpi_api_t
-      {
-      public:
-        gpi_api_t ();
-        ~gpi_api_t();
+    public:
+      gpi_api_t ();
+      ~gpi_api_t();
 
-        void init (int ac, char *av[]);
+      void init (int ac, char *av[]);
 
-        void set_memory_size (const gpi::size_t);
+      void set_memory_size (const gpi::size_t);
 
-        // wrapped C function calls
-        void start (const gpi::timeout_t timeout);
-        void stop ();
-        void kill ();
+      // wrapped C function calls
+      void start (const gpi::timeout_t timeout);
+      void stop ();
+      void kill ();
 
-        gpi::size_t number_of_counters () const;
-        gpi::size_t number_of_queues () const;
-        gpi::size_t queue_depth () const;
-        gpi::version_t version () const;
-        gpi::port_t port () const;
-        gpi::size_t number_of_nodes () const;
-        std::string hostname (const gpi::rank_t) const;
-        gpi::rank_t rank () const;
-        void * dma_ptr (void);
+      gpi::size_t number_of_counters () const;
+      gpi::size_t number_of_queues () const;
+      gpi::size_t queue_depth () const;
+      gpi::version_t version () const;
+      gpi::port_t port () const;
+      gpi::size_t number_of_nodes () const;
+      std::string hostname (const gpi::rank_t) const;
+      gpi::rank_t rank () const;
+      void * dma_ptr (void);
 
-        void set_network_type (const gpi::network_type_t);
-        void set_port (const gpi::port_t);
-        void set_mtu (const gpi::size_t);
-        void set_number_of_processes (const gpi::size_t);
+      void set_network_type (const gpi::network_type_t);
+      void set_port (const gpi::port_t);
+      void set_mtu (const gpi::size_t);
+      void set_number_of_processes (const gpi::size_t);
 
-        bool ping (const gpi::rank_t) const;
-        bool ping (std::string const & hostname) const;
-        bool is_master (void) const;
-        bool is_slave (void) const;
+      bool ping (const gpi::rank_t) const;
+      bool ping (std::string const & hostname) const;
+      bool is_master (void) const;
+      bool is_slave (void) const;
 
-        void barrier (void) const;
+      void barrier (void) const;
 
-        void read_dma ( const offset_t local_offset
-                      , const offset_t remote_offset
-                      , const size_t amount
-                      , const rank_t from_node
-                      , const queue_desc_t queue
-                      );
-        void write_dma ( const offset_t local_offset
-                       , const offset_t remote_offset
-                       , const size_t amount
-                       , const rank_t to_node
-                       , const queue_desc_t queue
-                       );
+      void read_dma ( const offset_t local_offset
+                    , const offset_t remote_offset
+                    , const size_t amount
+                    , const rank_t from_node
+                    , const queue_desc_t queue
+                    );
+      void write_dma ( const offset_t local_offset
+                     , const offset_t remote_offset
+                     , const size_t amount
+                     , const rank_t to_node
+                     , const queue_desc_t queue
+                     );
 
-        void send_dma ( const offset_t local_offset
-                      , const size_t amount
-                      , const rank_t to_node
-                      , const queue_desc_t queue
-                      );
-        void recv_dma ( const offset_t local_offset
-                      , const size_t size
-                      , const rank_t from_node
-                      , const queue_desc_t queue
-                      );
+      void send_dma ( const offset_t local_offset
+                    , const size_t amount
+                    , const rank_t to_node
+                    , const queue_desc_t queue
+                    );
+      void recv_dma ( const offset_t local_offset
+                    , const size_t size
+                    , const rank_t from_node
+                    , const queue_desc_t queue
+                    );
 
-        size_t wait_dma (const queue_desc_t queue);
+      size_t wait_dma (const queue_desc_t queue);
 
-        void send_passive ( const offset_t local_offset
-                          , const size_t size
-                          , const rank_t to_rank
-                          );
-        void recv_passive ( const offset_t local_offset
-                          , const size_t size
-                          , rank_t & from_rank
-                          );
-        void wait_passive ( void );
+      void send_passive ( const offset_t local_offset
+                        , const size_t size
+                        , const rank_t to_rank
+                        );
+      void recv_passive ( const offset_t local_offset
+                        , const size_t size
+                        , rank_t & from_rank
+                        );
+      void wait_passive ( void );
 
-      private:
-        void handle_signal (int);
-        void signal_handler (void); // thread entry
+    private:
+      void handle_signal (int);
+      void signal_handler (void); // thread entry
 
-        int   m_ac;
-        char **m_av;
-        bool  m_startup_done;
-        mutable rank_t m_rank;
-        mutable size_t m_num_nodes;
-        mutable size_t m_mem_size;
-        boost::shared_ptr<boost::thread> m_signal_handler;
-      };
-    }
+      int   m_ac;
+      char **m_av;
+      bool  m_startup_done;
+      mutable rank_t m_rank;
+      mutable size_t m_num_nodes;
+      mutable size_t m_mem_size;
+      boost::shared_ptr<boost::thread> m_signal_handler;
+    };
   }
 }
 
