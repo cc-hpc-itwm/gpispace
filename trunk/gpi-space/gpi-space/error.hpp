2d4527b2e9ec976d257984158bdd6058cf49ea8c
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-02-01 16:13:31 +0000

    easier error code generation
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@4696 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/gpi-space/gpi-space/error.hpp
+++ trunk/gpi-space/gpi-space/error.hpp
@@ -1,6 +1,8 @@
 #ifndef GPI_SPACE_ERROR_HPP
 #define GPI_SPACE_ERROR_HPP 1
 
+#include <string>
+
 namespace gpi
 {
   namespace error
@@ -11,50 +13,55 @@ namespace gpi
         {
           success = 0,
           unknown,
-          timeout
+          timeout,
+          config_error,
+          env_check_failed,
+          startup_failed,
+          write_dma_failed,
+          read_dma_failed,
+          passive_send_failed,
+          passive_recv_failed,
+          internal_error,
         };
     }
 
     class code_t
     {
     public:
-      explicit code_t (int val)
+      explicit code_t (int val, std::string const & txt)
         : m_val (val)
+        , m_txt (txt)
       {}
 
       virtual ~code_t () {}
-      virtual const char * name() const = 0;
+      const char * name() const { return m_txt.c_str(); };
       int value () const { return m_val; }
     private:
       int m_val;
+      std::string m_txt;
     };
 
-    struct success : public code_t
-    {
-      success ()
-        : code_t (errc::success)
-      {}
+#define MK_CODE_T(name)                         \
+    struct name : public code_t                 \
+    {                                           \
+      name ()                                   \
+        : code_t (errc::name, #name)            \
+      {}                                        \
+    }
 
-      const char * name () const { return "success"; }
-    };
+    MK_CODE_T(success);
+    MK_CODE_T(unknown);
+    MK_CODE_T(timeout);
+    MK_CODE_T(config_error);
+    MK_CODE_T(env_check_failed);
+    MK_CODE_T(startup_failed);
+    MK_CODE_T(write_dma_failed);
+    MK_CODE_T(read_dma_failed);
+    MK_CODE_T(passive_send_failed);
+    MK_CODE_T(passive_recv_failed);
+    MK_CODE_T(internal_error);
 
-    struct unknown : public code_t
-    {
-      unknown ()
-        : code_t (errc::unknown)
-      {}
-
-      const char * name () const { return "success"; }
-    };
-
-    struct timeout : public code_t
-    {
-      timeout ()
-        : code_t (errc::timeout)
-      {}
-
-      const char * name () const { return "timeout"; }
-    };
+#undef MK_CODE_T
   }
 }
 
