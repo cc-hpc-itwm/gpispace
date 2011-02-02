765d1a791ade2f582e071fb4a192646381ab309f
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-02-02 16:31:39 +0000

    typedef for the shmem segment identifier
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@4748 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/gpi-space/gpi-space/pc/type/typedefs.hpp
+++ trunk/gpi-space/gpi-space/pc/type/typedefs.hpp
@@ -2,6 +2,7 @@
 #define GPI_SPACE_PC_TYPE_DEFINITIONS_HPP 1
 
 #include <inttypes.h>
+#include <limits.h>
 
 namespace gpi
 {
@@ -13,10 +14,13 @@ namespace gpi
       typedef uint64_t offset_t;
       typedef uint64_t id_t;
       typedef uint64_t ref_count_t;
+      typedef uint64_t time_t;
       typedef id_t segment_id_t;
       typedef id_t handle_id_t;
-      typedef uint32_t mode_t; // r(4) u(4) g(4) o(4)
-      typedef uint64_t time_t;
+
+      typedef uint32_t error_t;
+      typedef uint16_t mode_t; // r(8) u(3) g(3) o(3)???
+      typedef char path_t[PATH_MAX+1];
     }
   }
 }
