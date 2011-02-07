9f3b4e927adafdcca3b1912f0695162356b14797
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-02-07 10:04:55 +0000

    added client api class
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@4785 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/gpi-space/gpi-space/pc/client/api.cpp
@@ -0,0 +1,131 @@
+#include "api.hpp"
+
+#include <sys/un.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+
+#include <boost/bind.hpp>
+
+#include <gpi-space/signal_handler.hpp>
+
+#include <fhglog/minimal.hpp>
+
+static int close_socket (const int fd)
+{
+  shutdown (fd, SHUT_RDWR);
+  return close (fd);
+}
+
+static int open_socket (std::string const & path)
+{
+  int sfd, err;
+  struct sockaddr_un addr;
+
+  sfd = socket (AF_UNIX, SOCK_STREAM, 0);
+  if (sfd < 0)
+  {
+    return -errno;
+  }
+
+  memset (&addr, 0, sizeof(addr));
+  addr.sun_family = AF_UNIX;
+  strncpy (addr.sun_path, path.c_str(), sizeof(addr.sun_path) - 1);
+
+  err = connect (sfd, (struct sockaddr*)&addr, sizeof(struct sockaddr_un));
+  if (err < 0)
+  {
+    err = -errno;
+    close_socket (sfd);
+    return err;
+  }
+
+  return sfd;
+}
+
+namespace gpi
+{
+  namespace pc
+  {
+    namespace client
+    {
+      api_t::api_t (std::string const & path)
+        : m_path (path)
+        , m_socket (-1)
+        , m_connected (false)
+      {
+        m_signal_connection =
+          gpi::signal::handler().connect
+          ( SIGPIPE, boost::bind( &api_t::connection_lost
+                                , this
+                                , _1
+                                )
+          );
+      }
+
+      api_t::~api_t ()
+      {
+        try
+        {
+          stop ();
+        }
+        catch (std::exception const & ex)
+        {
+          LOG(ERROR, "could not stop pc client api: " << ex.what());
+        }
+      }
+
+      void api_t::start ()
+      {
+        if (m_connected)
+        {
+          stop ();
+        }
+
+        m_socket = open_socket (m_path);
+        if (m_socket < 0)
+        {
+          throw std::runtime_error (strerror(errno));
+        }
+        else
+        {
+          m_connected = true;
+        }
+      }
+
+      void api_t::stop ()
+      {
+        if (m_connected)
+        {
+          close_socket (m_socket);
+          m_socket = -1;
+          m_connected = false;
+        }
+      }
+
+      bool api_t::is_connected () const
+      {
+        return m_connected;
+      }
+
+      int api_t::write (const void * buf, size_t sz)
+      {
+        return ::write (m_socket, buf, sz);
+      }
+
+      int api_t::read (void * buf, size_t sz)
+      {
+        return ::read (m_socket, buf, sz);
+      }
+
+      int api_t::connection_lost (int)
+      {
+        stop();
+        return 0;
+      }
+    }
+  }
+}
---
+++ trunk/gpi-space/gpi-space/pc/client/api.hpp
@@ -0,0 +1,53 @@
+#ifndef GPI_SPACE_PC_CLIENT_API_HPP
+#define GPI_SPACE_PC_CLIENT_API_HPP 1
+
+#include <string>
+
+#include <boost/noncopyable.hpp>
+#include <boost/thread.hpp>
+
+#include <gpi-space/signal_handler.hpp>
+#include <gpi-space/pc/type/typedefs.hpp>
+#include <gpi-space/pc/segment/segment.hpp>
+
+namespace gpi
+{
+  namespace pc
+  {
+    namespace client
+    {
+      class api_t : public boost::noncopyable
+      {
+      public:
+        explicit
+        api_t (std::string const & path);
+
+        ~api_t ();
+
+        void start ();
+        void stop ();
+
+        bool is_connected () const;
+
+        // api to gpi
+        int write (const void *buf, size_t sz);
+        int read (void *buf, size_t sz);
+
+        type::handle_id_t alloc ( const type::segment_id_t
+                                , const type::size_t
+                                );
+
+        void free (const type::handle_id_t);
+      private:
+        int connection_lost (int);
+
+        std::string m_path;
+        int m_socket;
+        bool m_connected;
+        gpi::signal::handler_t::connection_t m_signal_connection;
+      };
+    }
+  }
+}
+
+#endif
