f17a3c7b4014329dd051e3056e892ae6439fa0ed
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2011-02-10 15:23:50 +0000

    fuse: simple seek test
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@4843 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/fuse/test/seek.c
@@ -0,0 +1,111 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string.h>
+#include <errno.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define FAILURE(fmt,...)                       \
+  do                                           \
+    {                                          \
+      printf ( "FAILURE: [%u]: " fmt ": %s\n"  \
+             , __LINE__                        \
+             , __VA_ARGS__                     \
+             , strerror (errno)                \
+             );                                \
+                                               \
+      exit (EXIT_FAILURE);                     \
+    }                                          \
+  while (0)
+
+void run ( const char * filename
+         , const off_t pos
+         , const size_t count
+         )
+{
+  int fd = open (filename, O_RDONLY);
+
+  if (fd < 0)
+    {
+      FAILURE ("cannot open file %s for reading", filename);
+    }
+
+  struct stat sbuf;
+
+  if (fstat(fd, &sbuf) < 0)
+    {
+      FAILURE ("cannot stat %i (%s)", fd, filename);
+    }
+
+  printf ("dev %u\n", sbuf.st_dev);
+  printf ("ino %u\n", sbuf.st_ino);
+  printf ("mode %u\n", sbuf.st_mode);
+  printf ("nlink %u\n", sbuf.st_nlink);
+  printf ("uid %u\n", sbuf.st_uid);
+  printf ("gid %u\n", sbuf.st_gid);
+  printf ("rdev %u\n", sbuf.st_rdev);
+  printf ("size %u\n", sbuf.st_size);
+  printf ("bklsize %u\n", sbuf.st_blksize);
+  printf ("blocks %u\n", sbuf.st_blocks);
+  printf ("atime %u\n", sbuf.st_atime);
+  printf ("mtime %u\n", sbuf.st_mtime);
+  printf ("ctime %u\n", sbuf.st_ctime);
+
+  if (lseek (fd, pos, SEEK_SET) == (off_t) -1)
+    {
+      FAILURE ("cannot seek %i (%s): %u", fd, filename, pos);
+    }
+
+  char * buf = malloc (count);
+
+  if (!buf)
+    {
+      FAILURE ("cannot allocate %u bytes", count);
+    }
+
+  size_t left = count;
+
+  while (left > 0)
+    {
+      ssize_t r = read (fd, buf, count);
+
+      if (r < 0)
+        {
+          FAILURE ("cannot read %i (%s)", fd, filename);
+        }
+
+      left -= r;
+    }
+
+  printf ("read:\n");
+
+  for (size_t i = 0; i < count; ++i)
+    {
+      printf ("%c", buf[i]);
+    }
+
+  printf ("\n");
+
+  free (buf);
+
+  if (close (fd) < 0)
+    {
+      FAILURE ("when closing file %s", filename);
+    }
+}
+
+int
+main ()
+{
+  run ("gpi/0/name", 2, 1);
+  run ("gpi/0/data", (1<<20), (1<<20));
+
+  printf ("SUCCESS\n");
+
+  return EXIT_SUCCESS;
+}
