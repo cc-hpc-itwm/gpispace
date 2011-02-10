c001620e30a4fd5b2f1b8747e0ab04a29b23297c
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2011-02-10 09:54:35 +0000

    replace USE_GWES option with USE_REAL_WE
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@4830 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/sdpa/engine/IWorkflowEngine.hpp
+++ trunk/sdpa/trunk/sdpa/engine/IWorkflowEngine.hpp
@@ -23,13 +23,15 @@
 #include <boost/serialization/list.hpp>
 #include <boost/serialization/access.hpp>
 #include <boost/serialization/shared_ptr.hpp>
-
 #include <boost/serialization/assume_abstract.hpp>
-#include <we/mgmt/basic_layer.hpp>
-#include <we/mgmt/bits/traits.hpp>
-#include <we/mgmt/bits/signal.hpp>
-#include <we/we.hpp>
-#include <we/mgmt/layer.hpp>
+
+#ifdef USE_REAL_WE
+	#include <we/mgmt/basic_layer.hpp>
+	#include <we/mgmt/bits/traits.hpp>
+	#include <we/mgmt/bits/signal.hpp>
+	#include <we/we.hpp>
+	#include <we/mgmt/layer.hpp>
+#endif
 
 // Assume ids of type string
 typedef std::string id_type;
@@ -37,9 +39,56 @@ typedef std::string result_type;
 typedef std::string reason_type;
 typedef std::string encoded_type;
 
-typedef we::preference_t preference_t;
-typedef we::mgmt::activity_information_t activity_information_t;
-typedef we::mgmt::basic_layer<id_type, result_type, reason_type, encoded_type> IWorkflowEngine;
-typedef we::mgmt::layer<id_type, we::activity_t> RealWorkflowEngine;
+#ifdef USE_REAL_WE
+	typedef we::preference_t preference_t;
+	typedef we::mgmt::activity_information_t activity_information_t;
+	typedef we::mgmt::basic_layer<id_type, result_type, reason_type, encoded_type> IWorkflowEngine;
+#else
+	//typedef int preference_t;
+
+      struct preference_t
+      {
+		preference_t(): mandatory_(false) {}
+
+        bool is_mandatory (void) const { return mandatory_; }
+        bool is_wanted (const rank_type rank) const { return false;}
+        bool is_excluded (rank_type rank) const { return false;}
+
+        rank_list_type & ranks(void)
+        {
+          return ranks_;
+        }
+
+        rank_list_type const & ranks(void) const
+        {
+          return ranks_;
+        }
+
+        exclude_set_type & exclusion (void)
+        {
+          return excluded_ranks_;
+        }
+
+        exclude_set_type const & exclusion (void) const
+        {
+          return excluded_ranks_;
+        }
+
+        bool empty (void) const
+        {
+          return ranks_.empty();
+        }
+
+      private:
+        bool mandatory_;
+        rank_list_type ranks_;
+        exclude_set_type excluded_ranks_;
+      };
+
+
+
+	typedef void activity_information_t;
+    struct IWorkflowEngine{};
+#endif
 
 #endif //IWORKFLOWENGINE_HPP
