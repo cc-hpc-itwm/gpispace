6364186daff4532eb4834e88c922c5c8c8a03e4f
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-02-15 20:04:06 +0000

    added common exception in exception.hpp
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@4869 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/gpi-space/gpi-space/exception.hpp
@@ -0,0 +1,43 @@
+#ifndef GPI_SPACE_EXCEPTION_HPP
+#define GPI_SPACE_EXCEPTION_HPP 1
+
+#include <gpi-space/types.hpp>
+#include <gpi-space/error.hpp>
+
+namespace gpi
+{
+  namespace exception
+  {
+    struct gpi_error : public std::runtime_error
+    {
+      explicit
+      gpi_error (gpi::error::code_t const & ec)
+        : std::runtime_error (ec.name())
+        , value (ec.value())
+        , user_message (ec.detail())
+        , message ("gpi::error[" + boost::lexical_cast<std::string>(value) + "]: " + ec.name())
+      {}
+
+      explicit
+      gpi_error (gpi::error::code_t const & ec, std::string const & m)
+        : std::runtime_error (ec.name ())
+        , value (ec.value())
+        , user_message (m)
+        , message ("gpi::error[" + boost::lexical_cast<std::string>(value) + "]: " + ec.name() + ": " + user_message)
+      {}
+
+      virtual ~gpi_error () throw () {}
+
+      virtual const char * what () const throw ()
+      {
+        return message.c_str();
+      }
+
+      int value;
+      const std::string user_message;
+      const std::string message;
+    };
+  }
+}
+
+#endif
