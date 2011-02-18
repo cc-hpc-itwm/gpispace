dbf42b2c2d545b3c513ee6ca1873c896152229a9
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-02-18 01:02:33 +0000

    hopefully fixed segment test case
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@5003 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/gpi-space/tests/test_segment.cpp
+++ trunk/gpi-space/tests/test_segment.cpp
@@ -2,6 +2,7 @@
 
 #define BOOST_TEST_MODULE GpiSpaceSegmentTest
 #include <boost/test/unit_test.hpp>
+#include <boost/lexical_cast.hpp>
 
 #include <fhglog/minimal.hpp>
 
@@ -40,8 +41,19 @@ BOOST_FIXTURE_TEST_SUITE( suite, F )
 BOOST_AUTO_TEST_CASE ( segment_create_test )
 {
   using namespace gpi::pc::segment;
-  segment_t seg ("foo", 1024, gpi::pc::type::segment::SEG_INVAL);
-  seg.create ();
+  std::string name ("seg-test-");
+  name += boost::lexical_cast<std::string>(getpid());
+  segment_t seg (name, 1024);
+  try
+  {
+    seg.create ();
+    seg.close ();
+    seg.unlink();
+  } catch (...)
+  {
+    seg.unlink();
+    throw;
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
