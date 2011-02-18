c3b70bc737115a14cf96b1d0ff3dc5bf13460f5c
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2011-02-18 15:32:50 +0000

    updated the boost meta state machine
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@5019 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/sdpa/daemon/jobFSM/BMSM/JobFSM.hpp
+++ trunk/sdpa/trunk/sdpa/daemon/jobFSM/BMSM/JobFSM.hpp
@@ -18,10 +18,6 @@
 #ifndef JOB_FSM_BMSM_HPP
 #define JOB_FSM_BMSM_HPP 1
 
-#define BOOST_MPL_CFG_NO_PREPROCESSED_HEADERS
-#define BOOST_MPL_LIMIT_VECTOR_SIZE 30 //or whatever you need
-#define BOOST_MPL_LIMIT_MAP_SIZE 30 //or whatever you need
-
 #include <iostream>
 // back-end
 #include <boost/msm/back/state_machine.hpp>
@@ -35,6 +31,9 @@
 #include <sdpa/types.hpp>
 #include <boost/serialization/access.hpp>
 #include <boost/thread.hpp>
+
+#include <sdpa/events/JobResultsReplyEvent.hpp>
+#include <sdpa/events/DeleteJobAckEvent.hpp>
 
 //using namespace sdpa;
 //using namespace sdpa::daemon;
@@ -125,7 +124,7 @@ namespace sdpa {
 			};
 
 			// Pick a back-end
-			class JobFSM : public msm::back::state_machine<JobFSM_>, public sdpa::daemon::Job
+			class JobFSM : public msm::back::state_machine<JobFSM_>, public sdpa::daemon::JobImpl
 			{
 			public:
 				typedef sdpa::shared_ptr<JobFSM> Ptr;
@@ -136,7 +135,7 @@ namespace sdpa {
 						const sdpa::job_desc_t desc = "",
 						const sdpa::daemon::IComm* pHandler = NULL,
 						const sdpa::job_id_t &parent = sdpa::job_id_t::invalid_job_id())
-						: job_impl_(id, desc, pHandler, parent)
+						: JobImpl(id, desc, pHandler, parent)
 						  , SDPA_INIT_LOGGER("sdpa.fsm.bmsm.JobFSM")
 				{
 					SDPA_LOG_DEBUG("State machine created");
@@ -147,30 +146,30 @@ namespace sdpa {
 					SDPA_LOG_DEBUG("State machine destroyed");
 				}
 
-				const job_id_t& id() const { return job_impl_.id(); }
-				const job_id_t& parent() const { return job_impl_.parent(); }
-				const job_desc_t& description() const { return job_impl_.description(); }
-				const job_result_t& result() const { return job_impl_.result(); }
-				void set_icomm(sdpa::daemon::IComm* pArgComm) { job_impl_.set_icomm(pArgComm); }
-
-				bool is_marked_for_deletion() { return job_impl_.is_marked_for_deletion(); }
-				bool mark_for_deletion() { return job_impl_.mark_for_deletion(); }
-
-				bool is_local(){ return job_impl_.is_local(); }
-				void set_local(bool b){ job_impl_.set_local(b); }
-
-				std::string print_info() { return job_impl_.print_info(); }
-
-				unsigned long& walltime() { return job_impl_.walltime(); }
-				void setResult(const sdpa::job_result_t& res) { return job_impl_.setResult(res); }
-
 				//transitions
 				void CancelJob(const sdpa::events::CancelJobEvent* pEvt) {lock_type lock(mtx_); process_event(*pEvt);}
 				void CancelJobAck(const sdpa::events::CancelJobAckEvent* pEvt) {lock_type lock(mtx_); process_event(*pEvt);}
-				void DeleteJob(const sdpa::events::DeleteJobEvent* pEvt) {lock_type lock(mtx_); process_event(*pEvt);}
+
+				void DeleteJob(const sdpa::events::DeleteJobEvent* pEvt, sdpa::daemon::IComm*  ptr_comm)
+				{
+					lock_type lock(mtx_);
+					process_event(*pEvt);
+
+					if(ptr_comm)
+					{
+						sdpa::events::DeleteJobAckEvent::Ptr pDelJobReply(new sdpa::events::DeleteJobAckEvent(pEvt->to(), pEvt->from(), id(), pEvt->id()) );
+						//send ack to master
+						ptr_comm->sendEventToMaster(pDelJobReply);
+					}
+					else
+						SDPA_LOG_ERROR("Could not send delete reply. Invalid communication handler!");
+
+				}
+
 				void JobFailed(const sdpa::events::JobFailedEvent* pEvt) {lock_type lock(mtx_); process_event(*pEvt);}
 				void JobFinished(const sdpa::events::JobFinishedEvent* pEvt) {lock_type lock(mtx_); process_event(*pEvt);}
-				void QueryJobStatus(const sdpa::events::QueryJobStatusEvent* pEvt)
+
+				void QueryJobStatus(const sdpa::events::QueryJobStatusEvent* pEvt, sdpa::daemon::IComm* pDaemon )
 				{
 					// attention, no action called!
 					lock_type lock(mtx_);
@@ -178,30 +177,66 @@ namespace sdpa {
 
 					//LOG(TRACE, "The status of the job "<<id()<<" is " << getStatus()<<"!!!");
 					sdpa::status_t status = getStatus();
-					if(job_impl_.icomm())
+					if(pDaemon)
 					{
 						sdpa::events::JobStatusReplyEvent::Ptr
 						        pStatReply(new sdpa::events::JobStatusReplyEvent( pEvt->to(), pEvt->from(), id(), status));
 
-						job_impl_.icomm()->sendEventToMaster(pStatReply);
+						pDaemon->sendEventToMaster(pStatReply);
 					}
 					else
 						SDPA_LOG_WARN("Could not send back job status reply. Invalid communication handler!");
 
 				}
-				void RetrieveJobResults(const sdpa::events::RetrieveJobResultsEvent* pEvt) {lock_type lock(mtx_); process_event(*pEvt);}
+
+				void RetrieveJobResults(const sdpa::events::RetrieveJobResultsEvent* pEvt, sdpa::daemon::IComm* ptr_comm)
+				{
+					lock_type lock(mtx_);
+					process_event(*pEvt);
+					if(ptr_comm)
+					{
+						const sdpa::events::JobResultsReplyEvent::Ptr pResReply( new sdpa::events::JobResultsReplyEvent( pEvt->to(), pEvt->from(), id(), result() ));
+
+						// reply the results to master
+						ptr_comm->sendEventToMaster(pResReply);
+					}
+					else
+						SDPA_LOG_ERROR("Could not send results. Invalid communication handler!");
+				}
+
 				void Dispatch() { MSMDispatchEvent DispEvt;lock_type lock(mtx_); process_event(DispEvt);}
 
 				// actions
-				void action_run_job() { job_impl_.action_run_job(); }
-				void action_cancel_job(const sdpa::events::CancelJobEvent& e) { job_impl_.action_cancel_job(e); }
-				void action_cancel_job_from_pending(const sdpa::events::CancelJobEvent& e){ job_impl_.action_cancel_job_from_pending(e); }
-				void action_cancel_job_ack(const sdpa::events::CancelJobAckEvent& e){ job_impl_.action_cancel_job_ack(e); }
-				void action_delete_job(const sdpa::events::DeleteJobEvent& e){ job_impl_.action_delete_job(e); }
+				void action_run_job() {
+					sdpa::daemon::JobImpl::action_run_job();
+				}
 
-				void action_job_failed(const sdpa::events::JobFailedEvent& e){ job_impl_.action_job_failed(e); }
-				void action_job_finished(const sdpa::events::JobFinishedEvent& e){ job_impl_.action_job_finished(e); }
-				void action_retrieve_job_results(const sdpa::events::RetrieveJobResultsEvent& e){ job_impl_.action_retrieve_job_results(e); }
+				void action_cancel_job(const sdpa::events::CancelJobEvent& e) {
+					sdpa::daemon::JobImpl::action_cancel_job(e);
+				}
+
+				void action_cancel_job_from_pending(const sdpa::events::CancelJobEvent& e){
+					sdpa::daemon::JobImpl::action_cancel_job_from_pending(e);
+				}
+
+				void action_cancel_job_ack(const sdpa::events::CancelJobAckEvent& e) {
+					sdpa::daemon::JobImpl::action_cancel_job_ack(e);
+				}
+
+				void action_delete_job(const sdpa::events::DeleteJobEvent& e){
+					sdpa::daemon::JobImpl::action_delete_job(e);
+				}
+
+				void action_job_failed(const sdpa::events::JobFailedEvent& e){
+					sdpa::daemon::JobImpl::action_job_failed(e);
+				}
+				void action_job_finished(const sdpa::events::JobFinishedEvent& e){
+					sdpa::daemon::JobImpl::action_job_finished(e);
+				}
+
+				void action_retrieve_job_results(const sdpa::events::RetrieveJobResultsEvent& e){
+					sdpa::daemon::JobImpl::action_retrieve_job_results(e);
+				}
 
 				sdpa::status_t getStatus()
 				{
@@ -214,19 +249,19 @@ namespace sdpa {
 				template <class Archive>
 				void serialize(Archive& ar, const unsigned int)
 				{
-					ar.register_type(static_cast<sdpa::daemon::JobImpl*>(NULL));
-					//ar.register_type(static_cast<JobFSM*>(NULL));
+					//ar.register_type(static_cast<sdpa::daemon::JobImpl*>(NULL));
+					//ar.register_type(static_cast<JobFSM_*>(NULL));
 
-					ar & boost::serialization::base_object<Job>(*this);
+					ar & boost::serialization::base_object<JobImpl>(*this);
 					ar & boost::serialization::base_object<msm::back::state_machine<JobFSM_> >(*this);
-					ar & job_impl_;
+					//ar & job_impl_;
 				}
 
 				friend class boost::serialization::access;
 
 			private:
 				mutex_type mtx_;
-				sdpa::daemon::JobImpl job_impl_;
+
 				SDPA_DECLARE_LOGGER();
 			};
 		}
