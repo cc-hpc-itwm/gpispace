960e527527f48e42e83148d493e698b33d900e75
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-02-21 03:14:55 +0000

    added fhg revision library that automatically embeds the current revision information into it
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@5030 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/util/fhg/revision.hpp
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  revision.hpp
+ *
+ *    Description:  just contains the revision information of the project
+ *
+ *        Version:  1.0
+ *        Created:  02/20/2011 01:06:13 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Alexander Petry (petry), alexander.petry@itwm.fraunhofer.de
+ *        Company:  Fraunhofer ITWM
+ *
+ * =====================================================================================
+ */
+
+#ifndef FHG_UTIL_REVISION_HPP
+#define FHG_UTIL_REVISION_HPP 1
+
+namespace fhg
+{
+  extern const char * project_revision ();
+}
+
+#endif
