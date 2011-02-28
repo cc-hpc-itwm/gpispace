ecda30b06244394d4a567622e384cf9f3e724e10
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-02-28 09:08:17 +0000

    logging tab implemented
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@5115 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa-gui/sdpa-gui/main.cpp
+++ trunk/sdpa-gui/sdpa-gui/main.cpp
@@ -1,11 +1,27 @@
+#include <iostream>
+
+#include <QtCore/QString>
 #include <QtGui/QApplication>
 #include "monitorwindow.hpp"
 
-int main(int argc, char *argv[])
+int main(int ac, char *av[])
 {
-    QApplication a(argc, argv);
-    MonitorWindow w;
-    w.show();
+  unsigned short exe_port (0);
+  unsigned short log_port (2438);
 
-    return a.exec();
+  if (ac < 2)
+  {
+    std::cerr << "usage: " << av[0] << " gui-port [log-port]" << std::endl;
+    return 1;
+  }
+
+  exe_port = QString(av[1]).toUShort ();
+  if (ac > 2)
+    log_port = QString(av[2]).toUShort ();
+
+  QApplication a(ac, av);
+  MonitorWindow w(exe_port, log_port);
+  w.show();
+
+  return a.exec();
 }
