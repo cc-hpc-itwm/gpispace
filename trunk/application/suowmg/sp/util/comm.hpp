95be90e18ef2395fb1bac2d1c43146b1688f12fd
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2011-03-04 16:23:57 +0000

    prepared the suowmg parallel for shot parallelization
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@5144 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/application/suowmg/sp/util/comm.hpp
@@ -0,0 +1,80 @@
+#ifndef _COMM_HPP
+#define _COMM_HPP 1
+
+#include <pnetc/type/config.hpp>
+#include <pnetc/type/loaded_package.hpp>
+
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+#include <fvm-pc/pc.hpp>
+
+#include <print.hpp>
+
+namespace comm
+{
+  inline void put
+  ( const ::pnetc::type::config::config config
+  , const ::pnetc::type::loaded_package::loaded_package package
+  , const long shmem_offset = 0
+  )
+  {
+    LOG (INFO, "comm::put " << ::print::loaded_package (package)
+        << " offset " << package.slot * config.size.bunch
+        << " to shmem_offset " << shmem_offset
+        << " (" << package.package.size * config.size.trace << " bytes)"
+        );
+
+    try
+      {
+    waitComm ( fvmPutGlobalData
+               ( static_cast<fvmAllocHandle_t> (config.handle.data)
+               , package.slot * config.size.bunch
+               , package.package.size * config.size.trace
+               , shmem_offset
+               , static_cast<fvmAllocHandle_t> (config.handle.scratch)
+               )
+             );
+      }
+    catch (...)
+      {
+        LOG (INFO, "*** PUT FAILED");
+
+        throw;
+      }
+  }
+
+  inline void get
+  ( const ::pnetc::type::config::config config
+  , const ::pnetc::type::loaded_package::loaded_package package
+  , const long shmem_offset = 0
+  )
+  {
+    LOG (INFO, "comm::get " << ::print::loaded_package (package)
+        << " offset " << package.slot * config.size.bunch
+        << " from shmem_offset " << shmem_offset
+        << " (" << package.package.size * config.size.trace << " bytes)"
+        );
+
+    try
+      {
+    waitComm ( fvmGetGlobalData
+               ( static_cast<fvmAllocHandle_t> (config.handle.data)
+               , package.slot * config.size.bunch
+               , package.package.size * config.size.trace
+               , shmem_offset
+               , static_cast<fvmAllocHandle_t> (config.handle.scratch)
+               )
+             );
+      }
+    catch (...)
+      {
+        LOG (INFO, "*** GET FAILED");
+
+        throw;
+      }
+  }
+}
+
+#endif
