144fcbe724c17bab8d3d59188032a001e08170f0
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-03-10 12:05:44 +0000

    added memory_region in addition to memory_location
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@5160 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/gpi-space/gpi-space/pc/type/memory_location.hpp
+++ trunk/gpi-space/gpi-space/pc/type/memory_location.hpp
@@ -19,8 +19,13 @@ namespace gpi
     {
       struct memory_location_t
       {
-        gpi::pc::type::handle_id_t handle;
-        gpi::pc::type::offset_t    offset;
+        gpi::pc::type::handle_t handle;
+        gpi::pc::type::offset_t offset;
+
+        memory_location_t ()
+          : handle (0)
+          , offset (0)
+        {}
 
       private:
         friend class boost::serialization::access;
@@ -32,12 +37,40 @@ namespace gpi
         }
       };
 
+      // handle[+off=0][,size=0]
+      struct memory_region_t
+      {
+        memory_location_t     location;
+        gpi::pc::type::size_t size;
+
+        memory_region_t ()
+          : location ()
+          , size (0)
+        {}
+      private:
+        friend class boost::serialization::access;
+        template<typename Archive>
+        void serialize (Archive & ar, const unsigned int /*version*/)
+        {
+          ar & BOOST_SERIALIZATION_NVP( location );
+          ar & BOOST_SERIALIZATION_NVP( size );
+        }
+      };
+
       inline
       std::ostream & operator<< ( std::ostream & os
                                 , const memory_location_t & loc
                                 )
       {
         return os << handle_t(loc.handle) << "+" << loc.offset;
+      }
+
+      inline
+      std::ostream & operator<< ( std::ostream & os
+                                , const memory_region_t & reg
+                                )
+      {
+        return os << reg.location << "," << reg.size;
       }
 
       inline
@@ -67,6 +100,37 @@ namespace gpi
         else
         {
           loc.offset = 0;
+        }
+        return is;
+      }
+
+      inline
+      std::istream & operator>> ( std::istream & is
+                                , memory_region_t & reg
+                                )
+      {
+        // read a string
+        std::string tmp;
+        is >> tmp;
+
+        std::string::size_type split_pos (tmp.find(','));
+        reg.location =
+            boost::lexical_cast<memory_location_t>(tmp.substr( 0
+                                                             , split_pos
+                                                             )
+                                                  );
+        if (split_pos != std::string::npos)
+        {
+          reg.size =
+              boost::lexical_cast<size_t>
+                (tmp.substr ( split_pos+1
+                            , tmp.size() - split_pos
+                            )
+                );
+        }
+        else
+        {
+          reg.size = 0;
         }
         return is;
       }
