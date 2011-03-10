f4fc8c7d648f1410ec628d43d3051794a7d0db75
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-03-10 19:39:09 +0100

    added task and task-queue abstraction

---
+++ trunk/gpi-space/gpi-space/pc/memory/task.hpp
@@ -0,0 +1,63 @@
+#ifndef GPI_SPACE_PC_MEMORY_TASK_HPP
+#define GPI_SPACE_PC_MEMORY_TASK_HPP
+
+#include <boost/noncopyable.hpp>
+#include <boost/function.hpp>
+#include <boost/exception_ptr.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/condition_variable.hpp>
+
+namespace gpi
+{
+  namespace pc
+  {
+    namespace memory
+    {
+      namespace task_state
+      {
+        enum state {pending, executing, finished, failed, cancelled};
+      }
+
+      class task_t : boost::noncopyable
+      {
+      public:
+        typedef task_state::state state;
+        typedef boost::function<void (void)> function_type;
+
+        template <typename F>
+        task_t (std::string const nme, F fun)
+          : m_state (task_state::pending)
+          , m_name (nme)
+          , m_func (fun)
+        {}
+
+        ~task_t ();
+
+        void execute ();
+        void cancel ();
+        void reset ();
+        void wait ();
+
+        std::string const & get_name () const;
+        boost::exception_ptr get_error () const;
+        state get_state () const;
+        bool has_failed () const;
+      private:
+        typedef boost::mutex mutex_type;
+        typedef boost::unique_lock<mutex_type> lock_type;
+        typedef boost::condition_variable condition_type;
+
+        void set_state (const state);
+
+        mutable mutex_type m_mutex;
+        condition_type m_state_changed;
+        state m_state;
+        const std::string m_name;
+        function_type m_func;
+        boost::exception_ptr m_error;
+      };
+    }
+  }
+}
+
+#endif
---
+++ trunk/gpi-space/tests/test_task.cpp
@@ -0,0 +1,118 @@
+#define BOOST_TEST_MODULE GpiSpaceTaskTest
+#include <boost/test/unit_test.hpp>
+
+#include <boost/exception/diagnostic_information.hpp>
+#include <boost/bind.hpp>
+
+#include <fhglog/minimal.hpp>
+#include <boost/make_shared.hpp>
+#include <gpi-space/pc/memory/task.hpp>
+#include <gpi-space/pc/memory/task_queue.hpp>
+
+struct SetupLogging
+{
+  SetupLogging ()
+  {
+    FHGLOG_SETUP();
+  }
+};
+
+BOOST_GLOBAL_FIXTURE( SetupLogging );
+
+struct F
+{
+  F()
+  {}
+
+  ~F()
+  {}
+};
+
+static void task_executed (int *i)
+{
+  (*i)++;
+}
+
+static void task_erroneous ()
+{
+  throw std::runtime_error ("function failed");
+}
+
+BOOST_FIXTURE_TEST_SUITE( suite, F )
+
+BOOST_AUTO_TEST_CASE ( simple_task )
+{
+  using namespace gpi::pc::memory;
+  int executed (0);
+  task_t task ("simple_task", boost::bind (task_executed, &executed));
+
+  BOOST_CHECK_EQUAL (task.get_state(), task_state::pending);
+
+  task.execute ();
+  task.wait ();
+
+  BOOST_CHECK_EQUAL (task.get_state(), task_state::finished);
+  BOOST_CHECK_EQUAL (executed, 1);
+}
+
+BOOST_AUTO_TEST_CASE ( simple_task_cancel )
+{
+  using namespace gpi::pc::memory;
+  int executed (0);
+  task_t task ("simple_task", boost::bind (task_executed, &executed));
+
+  BOOST_CHECK_EQUAL (task.get_state(), task_state::pending);
+
+  task.cancel ();
+  task.execute ();
+  task.wait ();
+
+  BOOST_CHECK_EQUAL (task.get_state(), task_state::cancelled);
+  BOOST_CHECK_EQUAL (executed, 0);
+}
+
+BOOST_AUTO_TEST_CASE ( simple_task_failed )
+{
+  using namespace gpi::pc::memory;
+  int executed (0);
+  task_t task ("simple_task", task_erroneous);
+
+  BOOST_CHECK_EQUAL (task.get_state(), task_state::pending);
+
+  task.execute ();
+  task.wait ();
+
+  BOOST_CHECK_EQUAL (task.get_state(), task_state::failed);
+}
+
+BOOST_AUTO_TEST_CASE ( task_queue )
+{
+  using namespace gpi::pc::memory;
+  int executed (0);
+  task_queue_t q;
+
+  const int count (10);
+  for (int i = 0; i < count; ++i)
+  {
+    q.push
+      (boost::make_shared<task_t>( "simple_task"
+                                 , boost::bind(task_executed, &executed)
+                                 )
+      );
+  }
+
+  BOOST_CHECK_EQUAL (q.empty(), false);
+  BOOST_CHECK_EQUAL (q.size(),  size_t(count));
+
+  while (!q.empty())
+  {
+    task_queue_t::task_ptr t (q.pop());
+    t->execute();
+  }
+
+  BOOST_CHECK_EQUAL (q.empty(), true);
+  BOOST_CHECK_EQUAL (q.size(),  0u);
+  BOOST_CHECK_EQUAL (executed, count);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
