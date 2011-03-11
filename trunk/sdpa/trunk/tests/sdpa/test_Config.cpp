bf436c70cfb876eb0b1fa1c0749e8c275f129898
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2011-03-11 09:50:21 +0000

    updated test_Config
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@5169 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/tests/sdpa/test_Config.cpp
+++ trunk/sdpa/trunk/tests/sdpa/test_Config.cpp
@@ -9,6 +9,7 @@
 #include <sdpa/logging.hpp>
 
 using namespace sdpa;
+using boost::property_tree::ptree;
 
 struct MyFixture
 {
@@ -17,17 +18,48 @@ struct MyFixture
 	 SDPA_DECLARE_LOGGER();
 };
 
-BOOST_FIXTURE_TEST_SUITE( test_Config, MyFixture )
+BOOST_FIXTURE_TEST_SUITE( test_PropTree, MyFixture )
 
-BOOST_AUTO_TEST_CASE( testPopulate)
+BOOST_AUTO_TEST_CASE( testWriteIni )
 {
-	sdpa::util::Config::ptr_t cfg = sdpa::util::Config::create();
-	sdpa::util::CommonConfiguration()(*cfg);
-	cfg->parse_env();
-	cfg->parse_file("...");
-	cfg->parse("./program blah blah blah");
+	std::cout<<"-----------------testWriteIni---------------"<<std::endl;
+	ptree root;
+    ptree wave_packet;
 
-	BOOST_CHECK(cfg->is_set("sdpa.logging.level"));
+    wave_packet.put( "width", "1" );
+    wave_packet.put( "position", "2.0" );
+
+    ptree calculation_parameters;
+    calculation_parameters.put( "levels", "15" );
+    calculation_parameters.put<double>( "stuff", 23.888 );
+
+    root.push_front( ptree::value_type( "calculation parameters", calculation_parameters ) );
+    root.push_front( ptree::value_type( "wave packet", wave_packet ));
+
+    write_ini( "config.txt", root );
+}
+
+BOOST_AUTO_TEST_CASE( testReadIni )
+{
+	std::cout<<"-----------------testReadIni----------------"<<std::endl;
+	std::string configFile("config.txt");
+	ptree propTree;
+	read_ini(configFile, propTree);
+
+	write_ini( std::cout, propTree );
+}
+
+
+BOOST_AUTO_TEST_CASE( testGetPropVal )
+{
+	std::cout<<"-----------------testGetPropVal-------------"<<std::endl;
+	std::string configFile("config.txt");
+	ptree propTree;
+	read_ini(configFile, propTree);
+
+	double x = propTree.get<double>("calculation parameters.stuff");
+	std::cout<<"Stuff = "<<x;
 }
 
 BOOST_AUTO_TEST_SUITE_END()
+
