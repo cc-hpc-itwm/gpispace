801cfc263fc73fb93edc5df95c3942d933ffff59
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2011-03-28 08:57:47 +0000

    asin workflow with correct asianopt.cpp, defer calculations to the workflow, just return the sum1 (==sum value) and sum2 (==sum value^2)
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@5228 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/application/asian/asianopt.cpp
+++ trunk/application/asian/asianopt.cpp
@@ -22,7 +22,7 @@
 // ****************************************************************************
 
 
-double AsianMonteCarlo ( 
+double AsianMonteCarlo (
   double &Ergebnis,
   double &StdDev,
   param_t *pstParam,
@@ -41,12 +41,12 @@ double AsianMonteCarlo (
   int ran = 0;
 #ifdef __USE_GNU
   FILE *fp = fopen("/dev/urandom", "r");
-	
+
   fread(&ran,4,1,fp);
   fclose(fp);
 #else /* __LINUX__ */
   ran = time(NULL);
-  
+
 #endif /* __LINUX__ */
 
   NormalNumber Norm (0,1.0);
@@ -60,7 +60,7 @@ double AsianMonteCarlo (
   {
     return -999.999;
   };
-  
+
 	// Berechnungs des arithmetischen Mittels der Dividende als Bond
   double DivMean = Div.Mean(pstParam->m_nFirstFixing,LastFixing,
                             pstParam->m_dr,pstParam->m_dT,TimeV,GewV);
@@ -68,15 +68,15 @@ double AsianMonteCarlo (
   double S0 = Div.FitS(0.0,
                        pstParam->m_dT,
                        pstParam->m_dr,
-                       pstParam->m_dS);	
-  
-  //double TimeSteps = LastFixing-1; 
-	
+                       pstParam->m_dS);
+
+  //double TimeSteps = LastFixing-1;
+
   double S1,S2;    // Spot
   double GA1,GA2;  // Geometrische Mittel
   double A1,A2;    // Arithmetisce Mittel
   double StdNorm;  // Variable f�r Zufallszahl
-	
+
   double mu = (pstParam->m_dr-pstParam->m_dd- pstParam->m_dSigma*pstParam->m_dSigma/2.0); // Drift
   double Sum1=0;          // Summen zur Berechnung der Varianz und des Erwartungswerts
   double Sum2=0;
@@ -95,7 +95,7 @@ double AsianMonteCarlo (
   double EfGew = 0.0;
   for (int i=pstParam->m_nFirstFixing; i<=LastFixing; i++) EfGew += GewV[i];
 
-  
+
   // Festlegung der Berechnungskoeffizienten
   switch (Art)
   {
@@ -105,7 +105,7 @@ double AsianMonteCarlo (
       case FloC : Ak=-1.0;Sk= 1.0;Kk=-1.0;pstParam->m_dK=DivMean;break;
       default : printf("Switcherror");
   };
-	
+
   // Berechnung der anal. L�sung der Controle Variate
   Dividenden leer;
   if (CVBool==true)
@@ -118,39 +118,39 @@ double AsianMonteCarlo (
     valueana = 0.0;
     cv = 0.0;
   };
-	
+
   for (long int j=1; j<=pstParam->m_nn; j++)
   {
     DeltaT = TimeV[pstParam->m_nFirstFixing]-TimeV[0];
     GewT   = GewV[pstParam->m_nFirstFixing];
     StdNorm = Norm();
-			 
+
     // Spot1, Spot2 (AntiThetic)
     S1 = exp(mu*DeltaT-pstParam->m_dSigma*sqrt(DeltaT)*StdNorm);
     S2 = exp(mu*DeltaT+pstParam->m_dSigma*sqrt(DeltaT)*StdNorm);
-			 
+
     // Initialisierung des Aritmetischen Mittels
     A1 = S1*GewT;
     A2 = S2*GewT;
-			 
+
     // Initialisierung des Geomtrischen Mittels
     GA1 = pow(S1,GewV[pstParam->m_nFirstFixing]);
     GA2 = pow(S2,GewV[pstParam->m_nFirstFixing]);
-			 
+
     // printf("\nS1: %f   S2:  %f",S1,S2);
-    
+
     for (int i=(pstParam->m_nFirstFixing+1); i<=LastFixing; i++)
     {
       DeltaT = TimeV[i]-TimeV[i-1];
-					
+
       StdNorm = Norm();
-					
+
       G = GewV[i];
-      
+
       S1 = S1*exp(mu*DeltaT-pstParam->m_dSigma*sqrt(DeltaT)*StdNorm);
       S2 = S2*exp(mu*DeltaT+pstParam->m_dSigma*sqrt(DeltaT)*StdNorm);
       //printf("S0=%f S1=%f, S2=%f, G=%f, EfGew=%f\n", S0, S1, S2, G, EfGew);
-      
+
       // Arithmetisches Mittel
       A1 += S1*G;
       A2 += S2*G;
@@ -158,7 +158,7 @@ double AsianMonteCarlo (
       // Geometrisches Mittel
       GA1 *= pow(S1,G);
       GA2 *= pow(S2,G);
-					
+
 
     }
 
@@ -168,10 +168,10 @@ double AsianMonteCarlo (
 
     GA1 = S0 * pow(GA1,1/EfGew);
     GA2 = S0 * pow(GA2,1/EfGew);
-    //printf(">>%f + %f + %f = %f\n", 
+    //printf(">>%f + %f + %f = %f\n",
     //         Sk*S1*S0 ,Ak*A1,  Kk*pstParam->m_dK,
     //         Sk*S1*S0 + Ak*A1 + Kk*pstParam->m_dK);
-      
+
     value = 0.5*(max(Sk*S1*S0 + Ak*A1 + Kk*pstParam->m_dK,0.0)+
                  max(Sk*S2*S0 + Ak*A2 + Kk*pstParam->m_dK,0.0));
     valuecv = 0.5*(max(Sk*S1*S0 + Ak*GA1 + Kk*pstParam->m_dK,0.0)+
@@ -188,15 +188,16 @@ double AsianMonteCarlo (
   //printf("\nX-Sum1 = %f\n",Sum1);
   //printf("X-Sum2 = %f\n",Sum2);
   // compute final result
-  double Wert    = exp(-pstParam->m_dr * pstParam->m_dT) * ( Sum1 / pstParam->m_nn ); 
-  //+cv*valueana*exp(r*T) ); --> ignore this part now since valueana is 0 always!
-  double Varianz = ( 1.0/pstParam->m_nn * ( Sum2 - 1.0/pstParam->m_nn * Sum1 * Sum1 ) ) / pstParam->m_nn;
 
-  Ergebnis =Wert;
-  StdDev   =sqrt(Varianz)*exp(-pstParam->m_dr*pstParam->m_dT);
+//   double Wert    = exp(-pstParam->m_dr * pstParam->m_dT) * ( Sum1 / pstParam->m_nn );
+//   //+cv*valueana*exp(r*T) ); --> ignore this part now since valueana is 0 always!
+//   double Varianz = ( 1.0/pstParam->m_nn * ( Sum2 - 1.0/pstParam->m_nn * Sum1 * Sum1 ) ) / pstParam->m_nn;
 
-  //Ergebnis = Sum1;
-  //StdDev   = Sum2;
+//   Ergebnis =Wert;
+//   StdDev   =sqrt(Varianz)*exp(-pstParam->m_dr*pstParam->m_dT);
+
+  Ergebnis = Sum1;
+  StdDev   = Sum2;
 
 	return 0;
 };
@@ -204,7 +205,7 @@ double AsianMonteCarlo (
 
 
 #if 0
-  
+
 //==============================================================================
 //======                   Hilfsfunktion f�r GeometricDiscret              =====
 //==============================================================================
@@ -214,8 +215,8 @@ void swapdouble (double &a, double &b)
 {
 	double h = a; a = b; b = h;
 }
-  
- 
+
+
 // ***************************************************************************
 // *****        Spreadberechnung mittels numerischer Integration         *****
 // *****        Call: max(X1-X2-K,0)                                     *****
@@ -238,7 +239,7 @@ double newspread ( double M1,
 				   bool CallPutFlag)
 
 {
-	if (K == 0.0) 
+	if (K == 0.0)
 	{
 		if (CallPutFlag==false)
 		{
@@ -253,10 +254,10 @@ double newspread ( double M1,
 	}
 	else
 	{
-	
-		
+
+
 		const double step = 0.02;
-		
+
 		double A1,A2,A3;
 		double phi1,phi2,phi3;
 		double v1,v2,v3;
@@ -266,20 +267,20 @@ double newspread ( double M1,
 		double value;
 
 		A1 = A2 = A3 = 0.0;
-		
+
 		double d = ( M1 - log(K) ) / sqrt(Var1);
-		
-		if (K>0) 
-		{							
+
+		if (K>0)
+		{
 			for (double v=-6.0; v<=6.0-step; v += step)
 			{
-						
+
 				v1 = v + rho * sqrt(Var1);
 				v2 = v + sqrt(Var2);
-				v3 = v;						
-						
+				v3 = v;
+
 				NormDens = NormalDensity(v);
-				
+
 				phi1 = - log( 1.0 + 1.0/K * exp(M2 + v1 * sqrt(Var2)) ) / sqrt(Var1);
 				phi2 = - log( 1.0 + 1.0/K * exp(M2 + v2 * sqrt(Var2)) ) / sqrt(Var1);
 				phi3 = - log( 1.0 + 1.0/K * exp(M2 + v3 * sqrt(Var2)) ) / sqrt(Var1);
@@ -287,22 +288,22 @@ double newspread ( double M1,
 				d1 = ( d + rho*v1  + phi1 ) / rho2 + rho2 * sqrt(Var1);
 				d2 = ( d + rho*v2  + phi2 ) / rho2 ;
 				d3 = ( d + rho*v   + phi3 ) / rho2;
-				
+
 				A1 += NormDens * N(d1);
 				A2 += NormDens * N(d2);
 				A3 += NormDens * N(d3);
-				
+
 			};
 
-			
+
 			value = ( exp(M1+0.5*Var1) * A1 - exp(M2+0.5*Var2) * A2 - K * A3 ) * step * disc;
 
-			
+
 			if (CallPutFlag==false)
 			{
 				value = value - ( exp(M1+0.5*Var1)  - exp(M2+0.5*Var2) - K) * disc;
 			};
-		
+
 		}
 		else
 		{
@@ -365,20 +366,20 @@ param_t *pstParam,
   bool Arit)
 {
 
-  
+
 if (!CheckAsianParameters (pstParam,LastFixing,TimeV,GewV,Div))
   {
 	  return -999.999;
   };
-  
-  
+
+
   double DivMean = Div.Mean(pstParam->m_nFirstFixing,LastFixing,r,T,TimeV,GewV);
-  double So = Div.FitS(0.0,T,r,S);	
-  
+  double So = Div.FitS(0.0,T,r,S);
+
   if ( (DivMean != 0) && (Arit==false)) return -999.999;
 
-  
-  
+
+
   //int TimeSteps = TimeV.size()-1;
   int i,j;
   double EfGew = 0.0;
@@ -397,12 +398,12 @@ if (!CheckAsianParameters (pstParam,LastFixing,TimeV,GewV,Div))
   {
 	  Tm += TimeV[i]*GewV[i]*GewV[LastFixing];
 	  Mm += GewV[i] * ( log(So) + (r-d-0.5*sigma*sigma)*TimeV[i] ) ;
-		
+
   };
 
   Mm /= EfGew;
   Tm /= (EfGew*EfGew);
-  
+
   for (i=pstParam->m_nFirstFixing ; i<=LastFixing; i++)
   {
 	  for(j=pstParam->m_nFirstFixing; j<=LastFixing; j++)
@@ -413,12 +414,12 @@ if (!CheckAsianParameters (pstParam,LastFixing,TimeV,GewV,Div))
 
   Vmrho = Vm / (EfGew*EfGew);
   Vm    = Vm * (sigma*sigma) / (EfGew*EfGew);
-   
-  
+
+
   // Berechnung des Strikes
-  
-  
- if (Arit==true) 
+
+
+ if (Arit==true)
  {
 	  for (i=pstParam->m_nFirstFixing; i<=LastFixing; i++)
 	  {
@@ -434,36 +435,36 @@ else
 	 Km = 0.0;
  };
 
-  
-if (Art==FixC || Art==FixP) 
+
+if (Art==FixC || Art==FixP)
 {
 	Km = K-Km;
 }
 
-printf("Km:%f  K:%f",Km,K); 
-  
+printf("Km:%f  K:%f",Km,K);
+
   bool CallPutFlag;
-  
+
   if ( Art==FixC || Art==FloC)
   {
 	  CallPutFlag=true;
-  } 
+  }
   else
   {
 	  CallPutFlag=false;
   };
-  
+
   if (Art==FixC || Art==FixP)
   {
 	return TWBlackScholes(CallPutFlag,Mm,Vm,Km,exp(-r*T));
   }
   else
-  {	  
+  {
 	  double rho = Tm / ( sqrt(T*Vmrho)*GewV[LastFixing]/EfGew);
 	  Ms = log(So) + (r-d-0.5*sigma*sigma) * T;
 	  Vs = sigma*sigma*T;
 	  return  newspread(Ms,Mm,Vs,Vm,rho,Km,exp(-r*T),CallPutFlag);
-  };			      
+  };
 
 };
 
@@ -482,8 +483,8 @@ double TWBlackScholes ( bool CallPutFlag,
 	double d2,d1;
 
 	if (CallPutFlag==true) {Th = 1.0;} else {Th = -1.0;};
-	
-	if (Strike>0) 
+
+	if (Strike>0)
 	{
 		d2 = (Mean - log(Strike)) / sqrt(Var);
 		d1 = d2 + sqrt(Var);
@@ -501,7 +502,7 @@ double TWBlackScholes ( bool CallPutFlag,
 
 #endif /* 0 */
 
-bool CheckAsianParameters ( 
+bool CheckAsianParameters (
   param_t *pstParam,
   int LastFixing,
   double *TimeV,
@@ -512,14 +513,14 @@ bool CheckAsianParameters (
 
 {
 	bool OK = true;
-	if ( pstParam->m_dS<=0.0 || pstParam->m_dK<0.0 || 
+	if ( pstParam->m_dS<=0.0 || pstParam->m_dK<0.0 ||
          pstParam->m_dSigma<=0.0 ||
          pstParam->m_dr<0.0 || pstParam->m_dT <= 0.0) OK = false;
 	//if (TimeV.size() != GewV.size() || TimeV.size() <2 ) OK = false;
 	if (pstParam->m_nFirstFixing >= LastFixing || pstParam->m_nFirstFixing <= 0) OK = false;
 	//if (LastFixing > TimeV.size() || Div.FitS(0.0,T,r,S) <= 0) OK = false;
     if (Div.Mean(pstParam->m_nFirstFixing,LastFixing, pstParam->m_dr,
-                 pstParam->m_dT, TimeV, GewV) < 0.0) OK = false;	
+                 pstParam->m_dT, TimeV, GewV) < 0.0) OK = false;
 	return OK;
 };
 
