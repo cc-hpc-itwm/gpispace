3978c73e857197322bd9c94bbd7780b3c8a420ce
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-04-11 11:46:22 +0200

    added split into iterator function

--- trunk/util/fhg/util/split.hpp
+++ trunk/util/fhg/util/split.hpp
@@ -4,6 +4,7 @@
 #define _FHG_UTIL_SPLIT_HPP
 
 #include <iterator>
+#include <string>
 
 namespace fhg
 {
@@ -19,21 +20,52 @@ namespace fhg
       T key;
 
       for (typename T::const_iterator pos (x.begin()); pos != x.end(); ++pos)
+      {
+        if (*pos == s)
         {
-          if (*pos == s)
-            {
-              path.push_back (key);
-              key.clear();
-            }
-          else
-            {
-              key.push_back (*pos);
-            }
+          path.push_back (key);
+          key.clear();
         }
+        else
+        {
+          key.push_back (*pos);
+        }
+      }
 
       path.push_back (key);
 
       return path;
+    }
+
+
+    inline std::pair<std::string, std::string> split_string(const std::string &val
+                                                           , const std::string &sep)
+    {
+      std::string::size_type split_pos = val.find(sep);
+
+      const std::string first = val.substr(0, split_pos);
+      if (split_pos != std::string::npos)
+      {
+        const std::string second = val.substr(split_pos+1);
+        return std::make_pair(first, second);
+      }
+      else
+      {
+        return std::make_pair(first, "");
+      }
+    }
+
+    template <typename OutputIterator>
+    inline void split ( const std::string & s
+                      , std::string const & sep
+                      , OutputIterator out
+                      )
+    {
+      if (s.empty())
+        return;
+      std::pair<std::string, std::string> h_t (split_string(s, sep));
+      *out++ = h_t.first;
+      split (h_t.second, sep, out);
     }
   }
 }
