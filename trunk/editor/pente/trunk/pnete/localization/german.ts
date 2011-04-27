13c9b2a0185a32c940a041fe37a86de423009e33
Bernd Loerwald <bernd.loerwald@itwm.fraunhofer.de>
2011-04-27 16:37:45 +0000

    We now support localization. Localization for german added.
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@5478 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/editor/pente/trunk/pnete/localization/german.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>fhg::pnete::graph::Transition</name>
+    <message>
+        <location filename="../graph/Transition.cpp" line="108"/>
+        <source>Delete Transition</source>
+        <translation type="unfinished">Transition entfernen</translation>
+    </message>
+</context>
+<context>
+    <name>fhg::pnete::ui::MainWindow</name>
+    <message>
+        <location filename="../ui/MainWindow.cpp" line="55"/>
+        <source>SDPA editor</source>
+        <translation type="unfinished">SDPA Editor</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.cpp" line="59"/>
+        <source>Save</source>
+        <translation type="unfinished">Speichern</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.cpp" line="61"/>
+        <source>Close</source>
+        <translation type="unfinished">Schließen</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.cpp" line="71"/>
+        <source>File</source>
+        <translation type="unfinished">Datei</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.cpp" line="124"/>
+        <source>Library</source>
+        <translation type="unfinished">Bibliothek</translation>
+    </message>
+</context>
+<context>
+    <name>fhg::pnete::ui::TransitionLibraryModel</name>
+    <message>
+        <location filename="../ui/TransitionLibraryModel.cpp" line="140"/>
+        <source>Transition</source>
+        <translation type="unfinished">Transition</translation>
+    </message>
+</context>
+</TS>
--- trunk/editor/pente/trunk/pnete/pnete.cpp
+++ trunk/editor/pente/trunk/pnete/pnete.cpp
@@ -1,10 +1,16 @@
 #include <QApplication>
+#include <QTranslator>
 
 #include "ui/MainWindow.hpp"
 
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
+  
+  QTranslator translator;
+  //! \todo embed?
+  translator.load("german", QCoreApplication::applicationDirPath() + "/../../pnete/localization/"); 
+  a.installTranslator(&translator);
   
   fhg::pnete::ui::MainWindow w;
   //! \todo Decide where to put them.
