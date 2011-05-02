eb4916110ed5fd22b1d36663a113e4ed8b2153fd
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2011-05-02 11:12:33 +0000

    mark as obsolete deleteNonResponsiveWorkers within the Scheduler interface
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@5547 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/sdpa/daemon/Scheduler.hpp
+++ trunk/sdpa/trunk/sdpa/daemon/Scheduler.hpp
@@ -38,14 +38,15 @@ namespace daemon {
 
 	 virtual const Worker::ptr_t& findWorker(const Worker::worker_id_t&  ) throw(WorkerNotFoundException) = 0;
 
-     virtual const sdpa::job_id_t getNextJob(const Worker::worker_id_t& worker_id, const sdpa::job_id_t &last_job_id) throw (NoJobScheduledException, WorkerNotFoundException) =0;
+         virtual const sdpa::job_id_t getNextJob(const Worker::worker_id_t& worker_id, const sdpa::job_id_t &last_job_id) throw (NoJobScheduledException, WorkerNotFoundException) =0;
 	 virtual void deleteWorkerJob(const Worker::worker_id_t& worker_id, const sdpa::job_id_t &job_id ) throw (JobNotDeletedException, WorkerNotFoundException) = 0;
 
 	 virtual void acknowledgeJob(const Worker::worker_id_t& worker_id, const sdpa::job_id_t& job_id) throw(WorkerNotFoundException, JobNotFoundException) = 0;
 
 	 virtual void addWorker( const Worker::worker_id_t& workerId, unsigned int rank, const sdpa::worker_id_t& agent_uuid = "") throw (WorkerAlreadyExistException) = 0;
 	 virtual void delWorker( const Worker::worker_id_t& workerId) throw (WorkerNotFoundException) = 0;
-     virtual void deleteNonResponsiveWorkers ( sdpa::util::time_type const & ) = 0;
+         //virtual void deleteNonResponsiveWorkers ( sdpa::util::time_type const & ) = 0;
+	 virtual void detectTimedoutWorkers( sdpa::util::time_type const & timeout ) = 0;
 
 	 virtual size_t numberOfWorkers() = 0;
 	 virtual void notifyWorkers(const sdpa::events::ErrorEvent::error_code_t& ) = 0;
@@ -57,7 +58,7 @@ namespace daemon {
 	 //virtual void re_schedule(Worker::JobQueue* pQueue ) = 0;
 	 virtual void re_schedule(const Worker::worker_id_t& ) throw (WorkerNotFoundException) = 0;
 	 virtual bool has_job(const sdpa::job_id_t& job_id) = 0;
-    virtual void delete_job(const sdpa::job_id_t & job_id) = 0;
+         virtual void delete_job(const sdpa::job_id_t & job_id) = 0;
 
 	 //virtual void update_request_time(const sdpa::util::time_type& new_time ) = 0;
 
