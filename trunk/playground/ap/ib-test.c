2141cf8ff136c88733fab31518b0ce9a92cc2f2d
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-05-30 13:41:07 +0000

    opening only one device and printing some hints how to fix errors
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@5673 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/playground/ap/ib-test.c
+++ trunk/playground/ap/ib-test.c
@@ -14,15 +14,33 @@ int main(int ac, char *av[])
   struct ibv_device ** device;
   int num_devices;
   int i;
-  long size;
+  int rc;
+  long long size;
+  int dev;
 
   if (ac < 2)
   {
-    fprintf(stderr, "usage: %s #bytes\n", av[0]);
+    fprintf(stderr, "usage: %s #bytes [device-id]\n", av[0]);
     return EXIT_FAILURE;
   }
 
+  if (ac > 2)
+  {
+    dev = atoi(av[2]);
+    if (dev < 0)
+    {
+      fprintf(stderr, "invalid device, must be nonzero\n");
+      return EXIT_FAILURE;
+    }
+  }
+  else
+  {
+    dev = 0;
+  }
+
   size = atoll (av[1]);
+
+  printf ("page-size = %d\n", sysconf(_SC_PAGESIZE));
 
   printf ("allocating %lld bytes...\n", size);
 
@@ -37,6 +55,8 @@ int main(int ac, char *av[])
   if (mlock(mem,size) != 0)
   {
     printf("failed: mlock(%p, %lld): %s\n", mem, size, strerror(errno));
+    printf("  hint: check ulimit -l\n");
+    printf("  hint: it should be set to unlimited\n");
     return 3;
   }
 
@@ -57,21 +77,44 @@ int main(int ac, char *av[])
     printf ("\tdevice-%d : %s\n", i, ibv_get_device_name(device[i]));
   }
 
-  // open all devices
-  printf ("opening devices...\n");
-  struct ibv_context ** context = malloc(num_devices * sizeof(struct ibv_device*));
-
-  for (i = 0; i < num_devices; ++i)
+  if (dev >= num_devices)
   {
-    context[i] = ibv_open_device(device[i]);
-    if (! context[i])
-    {
-      printf ("could not open device: %s\n", ibv_get_device_name(device[i]));
-    }
+    fprintf(stderr, "device %d is out-of-range!\n", dev);
+    return EXIT_FAILURE;
+  }
+
+  printf ("opening device %d...\n", dev);
+
+  struct ibv_context * context = ibv_open_device(device[dev]);
+  if (0 == context)
+  {
+    fprintf (stderr, "could not open device: %s\n", ibv_get_device_name(device[dev]));
+    return EXIT_FAILURE;
+  }
+
+  struct ibv_device_attr dev_attr;
+  rc = ibv_query_device (context, &dev_attr);
+  if (rc != 0)
+  {
+    fprintf (stderr, "could not query device: ibv_query_device(%x, %x): %s"
+            , context
+            , &dev_attr
+            , strerror(rc)
+            );
+    return EXIT_FAILURE;
+  }
+
+  printf ("device info:\n");
+  printf ("\tmax_mr      = %d\n", dev_attr.max_mr);
+  printf ("\tmax_mr_size = %llu\n", dev_attr.max_mr_size);
+
+  if (size > dev_attr.max_mr_size)
+  {
+    printf("*** your specified size exeeds the max size of one registered memory block\n");
   }
 
   printf ("allocating protection domain\n");
-  struct ibv_pd *pd = ibv_alloc_pd(context[0]);
+  struct ibv_pd *pd = ibv_alloc_pd(context);
 
   printf ("registering memory...\n");
 
@@ -84,7 +127,8 @@ int main(int ac, char *av[])
 
   if (0 == mr)
   {
-    printf ("failed: ibv_reg_mr(%p, %p, %lld, %x): %s\n", pd,mem,size,flags,strerror(errno));
+    printf ("failed: ibv_reg_mr(%p, %p, %ull, %x): %s\n", pd,mem,size,flags,strerror(errno));
+    printf ("   hint: try to check the parameter 'log_mtts_per_seg' of the mlx4_core kernel module\n");
     return 5;
   }
   else
@@ -96,16 +140,13 @@ int main(int ac, char *av[])
 
   ibv_dealloc_pd (pd);
 
-  for (i = 0; i < num_devices; ++i)
-  {
-    ibv_close_device (context[i]);
-  }
-  free (context);
+  ibv_close_device (context);
 
   ibv_free_device_list (device);
-  device = 0;
 
   free (mem);
+
+  printf ("check successful\n");
 
   return EXIT_SUCCESS;
 }
