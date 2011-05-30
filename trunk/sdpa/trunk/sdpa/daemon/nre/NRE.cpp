69896573e7d159e642fccb653b31c348b1b44db7
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2011-05-30 09:39:17 +0000

    updated the NRE's implementation
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@5654 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/sdpa/daemon/nre/NRE.cpp
+++ trunk/sdpa/trunk/sdpa/daemon/nre/NRE.cpp
@@ -27,15 +27,14 @@ using namespace sdpa::events;
 template <typename U>
 void NRE<U>::action_configure(const StartUpEvent &se)
 {
-	GenericDaemon::action_configure (se);
+    GenericDaemon::action_configure (se);
+    SDPA_LOG_INFO("Configuring myeself (nre)...");
 
-	SDPA_LOG_INFO("Configuring myeself (nre)...");
+    // should set/update this dynamically, as a function of number of workers and their
+    // processing capacities
 
-	// should set/update this dynamically, as a function of number of workers and their
-	// processing capacities
-
-	cfg().put("nmax_ext_job_req", 2U);
-	cfg().put("polling interval", 50 * 1000); //0.1s
+    cfg().put("nmax_ext_job_req", 2U);
+    cfg().put("polling interval", 1000 * 1000); //1s
 }
 
 template <typename U>
@@ -43,67 +42,71 @@ void NRE<U>::action_config_ok(const ConfigOkEvent& e)
 {
     GenericDaemon::action_config_ok (e);
 
-	// should be overriden by the orchestrator, aggregator and NRE
-	SDPA_LOG_INFO("Configuration (nre) was ok");
+    // should be overriden by the orchestrator, aggregator and NRE
+    SDPA_LOG_INFO("Configuration (nre) was ok");
 
-	cfg().print();
+    cfg().print();
 }
 
 template <typename U>
 void NRE<U>::handleJobFinishedEvent(const JobFinishedEvent* pEvt )
 {
-  assert (pEvt);
+    assert (pEvt);
 
-	// check if the message comes from outside/slave or from WFE
-	// if it comes from a slave, one should inform WFE -> subjob
-	// if it comes from WFE -> concerns the master job
+    // check if the message comes from outside/slave or from WFE
+    // if it comes from a slave, one should inform WFE -> subjob
+    // if it comes from WFE -> concerns the master job
 
-  DLOG(TRACE, "handleJobFinishedEvent(" << pEvt->job_id() << ")");
+    DLOG(TRACE, "handleJobFinishedEvent(" << pEvt->job_id() << ")");
 
-	//put the job into the state Finished
-	Job::ptr_t pJob;
-	try {
-		pJob = ptr_job_man_->findJob(pEvt->job_id());
-		pJob->JobFinished(pEvt);
-	}
-	catch(JobNotFoundException const &){
-		SDPA_LOG_ERROR("Job "<<pEvt->job_id()<<" not found!");
-		return;
-	}
+    //put the job into the state Finished
+    Job::ptr_t pJob;
+    try {
+        pJob = ptr_job_man_->findJob(pEvt->job_id());
+        pJob->JobFinished(pEvt);
+    }
+    catch(JobNotFoundException const &)
+    {
+        SDPA_LOG_ERROR("Job "<<pEvt->job_id()<<" not found!");
+        return;
+    }
 
-	if( pEvt->from() == sdpa::daemon::WE || !hasWorkflowEngine() ) // use a predefined variable here of type enum or use typeid
-	{
-		try {
-			// forward it up
-			JobFinishedEvent::Ptr pEvtJobFinished( new JobFinishedEvent(name(), master(), pEvt->job_id(), pEvt->result()) );
-			// send the event to the master
-			sendEventToMaster(pEvtJobFinished);
-			// delete it from the map when you receive a JobFinishedAckEvent!
-		}
-		catch(QueueFull const &)
-		{
-			SDPA_LOG_ERROR("Failed to send to the master output stage "<<ptr_to_master_stage_->name()<<" a JobFinishedEvent");
-		}
-		catch(seda::StageNotFound const &)
-		{
+    if( pEvt->from() == sdpa::daemon::WE || !hasWorkflowEngine() ) // use a predefined variable here of type enum or use typeid
+    {
+        try {
+            // forward it up
+            JobFinishedEvent::Ptr pEvtJobFinished( new JobFinishedEvent( name(),
+                                                                         pJob->owner()/*master()*/,
+                                                                         pEvt->job_id(),
+                                                                         pEvt->result()) );
+            // send the event to the master
+            sendEventToMaster(pEvtJobFinished);
+            // delete it from the map when you receive a JobFinishedAckEvent!
+        }
+        catch(QueueFull const &)
+        {
+            SDPA_LOG_ERROR("Failed to send to the master output stage "<<ptr_to_master_stage_->name()<<" a JobFinishedEvent");
+        }
+        catch(seda::StageNotFound const &)
+        {
             SDPA_LOG_ERROR("Stage not found when trying to submit JobFinishedEvent");
             throw;
-		}
-		catch(std::exception const & ex)
-		{
-			SDPA_LOG_ERROR("Exception during stage->send: " << ex.what());
+        }
+        catch(std::exception const & ex)
+        {
+            SDPA_LOG_ERROR("Exception during stage->send: " << ex.what());
             throw;
-		}
-		catch(...)
-		{
+        }
+        catch(...)
+        {
             SDPA_LOG_ERROR("Unknown exception during stage->send!");
             throw;
-		}
-	}
-        else
-        {
-          LOG(ERROR, "got JobFinished event from suspicious source: " << pEvt->from());
         }
+    }
+    else
+    {
+        LOG(ERROR, "got JobFinished event from suspicious source: " << pEvt->from());
+    }
 }
 
 template <typename U>
@@ -111,57 +114,61 @@ void NRE<U>::handleJobFailedEvent(const JobFailedEvent* pEvt )
 {
   assert (pEvt);
 
-	// check if the message comes from outside/slave or from WFE
-	// if it comes from a slave, one should inform WFE -> subjob
-	// if it comes from WFE -> concerns the master job
+  // check if the message comes from outside/slave or from WFE
+  // if it comes from a slave, one should inform WFE -> subjob
+  // if it comes from WFE -> concerns the master job
 
   DLOG(TRACE, "handleJobFailedEvent(" << pEvt->job_id() << ")");
 
-	//put the job into the state Finished
-	Job::ptr_t pJob;
-	try {
-		pJob = ptr_job_man_->findJob(pEvt->job_id());
-		pJob->JobFailed(pEvt);
-	}
-	catch(JobNotFoundException const &)
-	{
-		SDPA_LOG_ERROR("Job "<<pEvt->job_id()<<" not found!");
-        return;
-	}
+  //put the job into the state Finished
+  Job::ptr_t pJob;
+  try {
+      pJob = ptr_job_man_->findJob(pEvt->job_id());
+      pJob->JobFailed(pEvt);
+  }
+  catch(JobNotFoundException const &)
+  {
+      SDPA_LOG_ERROR("Job "<<pEvt->job_id()<<" not found!");
+      return;
+  }
 
-	if( pEvt->from() == sdpa::daemon::WE ||  !hasWorkflowEngine() ) // use a predefined variable here of type enum or use typeid
-	{
-		// the message comes from GWES, this is a local job
-		// if I'm not the orchestrator
-		//send JobFinished event to the master if daemon == aggregator || NRE
+  if( pEvt->from() == sdpa::daemon::WE ||  !hasWorkflowEngine() ) // use a predefined variable here of type enum or use typeid
+  {
+      // the message comes from GWES, this is a local job
+      // if I'm not the orchestrator
+      //send JobFinished event to the master if daemon == aggregator || NRE
 
-		try {
-			// forward it up
-			JobFailedEvent::Ptr pEvtJobFailedEvent(new JobFailedEvent(name(), master(), pEvt->job_id(), pEvt->result()));
+      try {
+          // forward it up
+          JobFailedEvent::Ptr pEvtJobFailedEvent(new JobFailedEvent(  name(),
+                                                                      pJob->owner(),
+                                                                      pEvt->job_id(),
+                                                                      pEvt->result()));
 
-			// send the event to the master
-			sendEventToMaster(pEvtJobFailedEvent);
-			// delete it from the map when you receive a JobFaileddAckEvent!
-		}
-		catch(QueueFull const &)
-		{
-			SDPA_LOG_WARN("Failed to send to the master output stage "<<ptr_to_master_stage_->name()<<" a JobFailedEvent");
-		}
-		catch(seda::StageNotFound const &)
-		{
-			SDPA_LOG_FATAL("Stage not found when trying to send JobFailedEvent");
-			throw;
-		}
-		catch(std::exception const & ex)
-		{
-			SDPA_LOG_FATAL("Exception during stage->send: " << ex.what());
-            throw;
-		}
-		catch(...) {
-            SDPA_LOG_FATAL("Unknown exception during stage->send!");
-            throw;
-		}
-	}
+          // send the event to the master
+          sendEventToMaster(pEvtJobFailedEvent);
+          // delete it from the map when you receive a JobFaileddAckEvent!
+      }
+      catch(QueueFull const &)
+      {
+          SDPA_LOG_WARN("Failed to send to the master output stage "<<ptr_to_master_stage_->name()<<" a JobFailedEvent");
+      }
+      catch(seda::StageNotFound const &)
+      {
+          SDPA_LOG_FATAL("Stage not found when trying to send JobFailedEvent");
+          throw;
+      }
+      catch(std::exception const & ex)
+      {
+          SDPA_LOG_FATAL("Exception during stage->send: " << ex.what());
+          throw;
+      }
+      catch(...)
+      {
+          SDPA_LOG_FATAL("Unknown exception during stage->send!");
+          throw;
+      }
+  }
 }
 
 template <typename U>
@@ -178,17 +185,17 @@ void  NRE<U>::cancelNotRunning(sdpa::job_id_t const & job)
 
     try
     {
-      ptr_workflow_engine_->cancelled(job);
-      ptr_job_man_->deleteJob(job);
+        ptr_workflow_engine_->cancelled(job);
+        ptr_job_man_->deleteJob(job);
     }
     catch (std::exception const & ex)
     {
-      LOG(WARN, "could not cancel job on the workflow engine: " << ex.what());
+        LOG(WARN, "could not cancel job on the workflow engine: " << ex.what());
     }
   }
   catch(const JobNotFoundException &)
   {
-    LOG(WARN, "job_cancelled(" << job << ") failed: no such job");
+      LOG(WARN, "job_cancelled(" << job << ") failed: no such job");
   }
 }
 
@@ -209,11 +216,11 @@ void  NRE<U>::handleCancelJobEvent(const CancelJobEvent* pEvt )
 
         if(is_orchestrator())
         {
-          // send immediately an acknowledgment to the component that requested the cancellation
-          CancelJobAckEvent::Ptr pCancelAckEvt(new CancelJobAckEvent(name(), pEvt->from(), pEvt->job_id(), pEvt->id()));
+            // send immediately an acknowledgment to the component that requested the cancellation
+            CancelJobAckEvent::Ptr pCancelAckEvt(new CancelJobAckEvent(name(), pEvt->from(), pEvt->job_id(), pEvt->id()));
 
-          // only if the job was already submitted
-          sendEventToMaster(pCancelAckEvt);
+            // only if the job was already submitted
+            sendEventToMaster(pCancelAckEvt);
         }
     }
     catch(const JobNotFoundException &)
@@ -248,7 +255,7 @@ void  NRE<U>::handleCancelJobEvent(const CancelJobEvent* pEvt )
         }
         catch(const std::exception& ex)
         {
-          SDPA_LOG_ERROR("Unexpected exception occurred:"<<ex.what());
+            SDPA_LOG_ERROR("Unexpected exception occurred:"<<ex.what());
         }
     }
     else // a Cancel message came from the upper level -> forward cancellation request to WE
@@ -268,16 +275,16 @@ void  NRE<U>::handleCancelJobAckEvent(const CancelJobAckEvent* pEvt)
 
     try
     {
-            Job::ptr_t pJob(ptr_job_man_->findJob(pEvt->job_id()));
+        Job::ptr_t pJob(ptr_job_man_->findJob(pEvt->job_id()));
 
-            // update the job status to "Cancelled"
-            pJob->CancelJobAck(pEvt);
-            SDPA_LOG_DEBUG("The job state is: "<<pJob->getStatus());
+        // update the job status to "Cancelled"
+        pJob->CancelJobAck(pEvt);
+        SDPA_LOG_DEBUG("The job state is: "<<pJob->getStatus());
     }
     catch (std::exception const & ex)
     {
-            LOG(WARN, "could not find job: " << ex.what());
-            return;
+        LOG(WARN, "could not find job: " << ex.what());
+        return;
     }
 
     // the acknowledgment comes from WE or from a slave and there is no WE
@@ -293,11 +300,11 @@ void  NRE<U>::handleCancelJobAckEvent(const CancelJobAckEvent* pEvt)
 
             try
             {
-                    ptr_job_man_->deleteJob(pEvt->job_id());
+                ptr_job_man_->deleteJob(pEvt->job_id());
             }
             catch(const JobNotDeletedException&)
             {
-                    LOG( WARN, "the JobManager could not delete the job: "<< pEvt->job_id());
+                LOG( WARN, "the JobManager could not delete the job: "<< pEvt->job_id());
             }
         }
     }
@@ -416,58 +423,57 @@ void NRE<U>::notifyActivityFailed( const id_type& id, const std::string& data )
 template <typename U>
 void NRE<U>::notifyActivityCancelled( const id_type& id, const std::string& data )
 {
-	if(hasWorkflowEngine())
-	{
-        activity_information_t info;
-        ptr_workflow_engine_->fill_in_info (id, info);
-        const std::string act_name (info.name);
-        notifyObservers( NotificationEvent( id, act_name, NotificationEvent::STATE_CANCELLED) );
-	}
+    if(hasWorkflowEngine())
+    {
+      activity_information_t info;
+      ptr_workflow_engine_->fill_in_info (id, info);
+      const std::string act_name (info.name);
+      notifyObservers( NotificationEvent( id, act_name, NotificationEvent::STATE_CANCELLED) );
+    }
 }
 
 template <typename U>
 void NRE<U>::backup( std::ostream& ofs )
 {
-	try {
-		//std::string strArchiveName(name()+".bkp");
-		//SDPA_LOG_DEBUG("Backup the agent "<<name()<<" to file "<<strArchiveName);
+    try {
+        //std::string strArchiveName(name()+".bkp");
+        //SDPA_LOG_DEBUG("Backup the agent "<<name()<<" to file "<<strArchiveName);
 
-		boost::archive::text_oarchive oa(ofs);
-		oa.register_type(static_cast<JobManager*>(NULL));
-		oa.register_type(static_cast<JobImpl*>(NULL));
-		oa.register_type(static_cast<JobFSM*>(NULL));
-		oa << ptr_job_man_;
+        boost::archive::text_oarchive oa(ofs);
+        oa.register_type(static_cast<JobManager*>(NULL));
+        oa.register_type(static_cast<JobImpl*>(NULL));
+        oa.register_type(static_cast<JobFSM*>(NULL));
+        oa << ptr_job_man_;
 
-		//oa.register_type(static_cast<SchedulerNRE<U>*>(NULL));
-		//oa.register_type(static_cast<SchedulerImpl*>(NULL));
-		//oa<<ptr_scheduler_;
-	}
-	catch(exception &e)
-	{
-		cout <<"Exception occurred: "<< e.what() << endl;
-		return;
-	}
+        //oa.register_type(static_cast<SchedulerNRE<U>*>(NULL));
+        //oa.register_type(static_cast<SchedulerImpl*>(NULL));
+        //oa<<ptr_scheduler_;
+    }
+    catch(exception &e)
+    {
+        cout <<"Exception occurred: "<< e.what() << endl;
+        return;
+    }
 }
 
 template <typename U>
 void NRE<U>::recover( std::istream& ifs )
 {
 
-	try {
+    try {
+        boost::archive::text_iarchive ia(ifs);
+        ia.register_type(static_cast<JobManager*>(NULL));
+        ia.register_type(static_cast<JobImpl*>(NULL));
+        ia.register_type(static_cast<JobFSM*>(NULL));
+        // restore the schedule from the archive
+        ia >> ptr_job_man_;
 
-		boost::archive::text_iarchive ia(ifs);
-		ia.register_type(static_cast<JobManager*>(NULL));
-		ia.register_type(static_cast<JobImpl*>(NULL));
-		ia.register_type(static_cast<JobFSM*>(NULL));
-		// restore the schedule from the archive
-		ia >> ptr_job_man_;
-
-		//ia.register_type(static_cast<SchedulerNRE<U>*>(NULL));
-		//ia.register_type(static_cast<SchedulerImpl*>(NULL));
-		//ia>> ptr_scheduler_;
-	}
-	catch(exception &e)
-	{
-		cout <<"Exception occurred: " << e.what() << endl;
-	}
+        //ia.register_type(static_cast<SchedulerNRE<U>*>(NULL));
+        //ia.register_type(static_cast<SchedulerImpl*>(NULL));
+        //ia>> ptr_scheduler_;
+    }
+    catch(exception &e)
+    {
+        cout <<"Exception occurred: " << e.what() << endl;
+    }
 }
