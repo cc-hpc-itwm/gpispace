2ec58b8c9822377dd43b3333b2fa04455f0ce3b7
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2011-05-30 09:40:18 +0000

    updated the NRE to take a list of master names as parameter
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@5655 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/sdpa/daemon/nre/NRE.hpp
+++ trunk/sdpa/trunk/sdpa/daemon/nre/NRE.hpp
@@ -26,128 +26,125 @@
 #include <boost/pointer_cast.hpp>
 
 namespace sdpa {
-	namespace daemon {
-	  template <typename U>
-	  class NRE : public dsm::DaemonFSM,  public sdpa::daemon::Observable {
-	  public:
-		typedef sdpa::shared_ptr<NRE<U> > ptr_t;
+  namespace daemon {
+      template <typename U>
+      class NRE : public dsm::DaemonFSM,  public sdpa::daemon::Observable {
+      public:
+            typedef sdpa::shared_ptr<NRE<U> > ptr_t;
 
-		SDPA_DECLARE_LOGGER();
+            SDPA_DECLARE_LOGGER();
 
-		NRE(   const std::string& name = ""
-			   , const std::string& url = ""
-			   , const std::string& masterName = ""
-			   //, const std::string& masterUrl = ""
-			   , const std::string& workerUrl = ""
-			   , const std::string& guiUrl = ""
-			   // TODO: fixme, this is ugly
-			   , bool bLaunchNrePcd = false
-			   , const std::string & fvmPCBinary = ""
-			   , const std::vector<std::string> & fvmPCSearchPath = std::vector<std::string>()
-			   , const std::vector<std::string> & fvmPCPreLoad = std::vector<std::string>() )
+            NRE(   const std::string& name = ""
+                       , const std::string& url = ""
+                       , const std::vector<std::string> arrMasterNames = std::vector<std::string>()
+                       //, const std::string& masterUrl = ""
+                       , const std::string& workerUrl = ""
+                       , const std::string& guiUrl = ""
+                       // TODO: fixme, this is ugly
+                       , bool bLaunchNrePcd = false
+                       , const std::string & fvmPCBinary = ""
+                       , const std::vector<std::string> & fvmPCSearchPath = std::vector<std::string>()
+                       , const std::vector<std::string> & fvmPCPreLoad = std::vector<std::string>() )
 
-				: dsm::DaemonFSM( name, NULL )
-                , SDPA_INIT_LOGGER(name)
-                , url_(url)
-                , masterName_(masterName)
-                , workerUrl_(workerUrl)
-                , m_guiService("SDPA", guiUrl)
-                , bLaunchNrePcd_(bLaunchNrePcd)
-                , nre_pcd_binary_(fvmPCBinary)
-                , nre_pcd_search_path_(fvmPCSearchPath)
-                , nre_pcd_pre_load_(fvmPCPreLoad)
-		{
-			SDPA_LOG_DEBUG("NRE's constructor called ...");
+            : dsm::DaemonFSM( name, arrMasterNames, NULL )
+              , SDPA_INIT_LOGGER(name)
+              , url_(url)
+              //, masterName_(masterName)
+              , workerUrl_(workerUrl)
+              , m_guiService("SDPA", guiUrl)
+              , bLaunchNrePcd_(bLaunchNrePcd)
+              , nre_pcd_binary_(fvmPCBinary)
+              , nre_pcd_search_path_(fvmPCSearchPath)
+              , nre_pcd_pre_load_(fvmPCPreLoad)
+            {
+              SDPA_LOG_DEBUG("NRE's constructor called ...");
 
-			try
-			{
-				if(!guiUrl.empty())
-				{
-					// logging gui service
-					m_guiService.open ();
-					// attach gui observer
-					SDPA_LOG_INFO("Logging GUI observer at " << guiUrl << " attached...");
-					attach_observer(&m_guiService);
-				}
-			}
-			catch (std::exception const & ex)
-			{
-			  SDPA_LOG_ERROR ("GUI observer at " << guiUrl << " could not be attached: " << ex.what());
-			}
-		}
+              try
+              {
+                  if(!guiUrl.empty())
+                  {
+                    // logging gui service
+                    m_guiService.open ();
+                    // attach gui observer
+                    SDPA_LOG_INFO("Logging GUI observer at " << guiUrl << " attached...");
+                    attach_observer(&m_guiService);
+                  }
+              }
+              catch (std::exception const & ex)
+              {
+                  SDPA_LOG_ERROR ("GUI observer at " << guiUrl << " could not be attached: " << ex.what());
+              }
+            }
 
-		virtual ~NRE()
-		{
-			SDPA_LOG_DEBUG("NRE's destructor called ...");
+            virtual ~NRE()
+            {
+              SDPA_LOG_DEBUG("NRE's destructor called ...");
 
-			//daemon_stage_ = NULL;
-			detach_observer( &m_guiService );
-		}
+              //daemon_stage_ = NULL;
+              detach_observer( &m_guiService );
+            }
 
-		void action_configure( const sdpa::events::StartUpEvent& );
-		void action_config_ok( const sdpa::events::ConfigOkEvent& );
+            void action_configure( const sdpa::events::StartUpEvent& );
+            void action_config_ok( const sdpa::events::ConfigOkEvent& );
 
-		bool cancel(const id_type&, const reason_type & );
+            bool cancel(const id_type&, const reason_type & );
 
-		void handleJobFinishedEvent(const sdpa::events::JobFinishedEvent* );
-		void handleJobFailedEvent(const sdpa::events::JobFailedEvent* );
-		void handleCancelJobEvent(const CancelJobEvent* );
-		void handleCancelJobAckEvent(const CancelJobAckEvent* );
+            void handleJobFinishedEvent(const sdpa::events::JobFinishedEvent* );
+            void handleJobFailedEvent(const sdpa::events::JobFailedEvent* );
+            void handleCancelJobEvent(const CancelJobEvent* );
+            void handleCancelJobAckEvent(const CancelJobAckEvent* );
 
-		void notifyActivityCreated( const id_type& id, const std::string& data );
-		void notifyActivityStarted( const id_type& id, const std::string& data );
-		void notifyActivityFinished( const id_type& id, const std::string& data );
-		void notifyActivityFailed( const id_type& id, const std::string& data );
-		void notifyActivityCancelled( const id_type& id, const std::string& data );
+            void notifyActivityCreated( const id_type& id, const std::string& data );
+            void notifyActivityStarted( const id_type& id, const std::string& data );
+            void notifyActivityFinished( const id_type& id, const std::string& data );
+            void notifyActivityFailed( const id_type& id, const std::string& data );
+            void notifyActivityCancelled( const id_type& id, const std::string& data );
 
-		const std::string url() const {return url_;}
-		const std::string masterName() const { return masterName_; }
-		//const std::string& masterUrl() const { return masterUrl_; }
+            const std::string url() const {return url_;}
 
-		template <class Archive>
-		void serialize(Archive& ar, const unsigned int)
-		{
-			ar & boost::serialization::base_object<DaemonFSM>(*this);
-			ar & url_; //boost::serialization::make_nvp("url_", url_);
-			ar & masterName_; //boost::serialization::make_nvp("url_", masterName_);
-			//ar & masterUrl_; //boost::serialization::make_nvp("url_", masterUrl_);
-			ar & workerUrl_;
-			//ar & m_guiService;
-		}
+            template <class Archive>
+            void serialize(Archive& ar, const unsigned int)
+            {
+              ar & boost::serialization::base_object<DaemonFSM>(*this);
+              ar & url_; //boost::serialization::make_nvp("url_", url_);
+              ar & workerUrl_;
+              //ar & m_guiService;
+            }
 
-		void cancelNotRunning(sdpa::job_id_t const & job);
+            void cancelNotRunning(sdpa::job_id_t const & job);
 
-		virtual void backup( std::ostream& );
-		virtual void recover( std::istream& );
+            virtual void backup( std::ostream& );
+            virtual void recover( std::istream& );
 
-		friend class boost::serialization::access;
+            friend class boost::serialization::access;
 
-	  protected:
+      protected:
 
-		Scheduler* create_scheduler()
-		{
-                  DLOG(TRACE, "creating nre scheduler...");
-                  return new SchedulerNRE<U>( this
-                                            , workerUrl_
-                                            , bLaunchNrePcd_
-                                            , nre_pcd_binary_
-                                            , nre_pcd_search_path_
-                                            , nre_pcd_pre_load_
-                                            );
-		}
+            Scheduler* create_scheduler()
+            {
+              DLOG(TRACE, "creating nre scheduler...");
 
-		std::string url_;
-		std::string masterName_;
-		//std::string masterUrl_;
-		std::string workerUrl_;
-		NotificationService m_guiService;
-		bool bLaunchNrePcd_;
-        std::string nre_pcd_binary_;
-        std::vector<std::string> nre_pcd_search_path_;
-        std::vector<std::string> nre_pcd_pre_load_;
-	  };
-	}
-}
+              return new SchedulerNRE<U>( this
+                                        , workerUrl_
+                                        , bLaunchNrePcd_
+                                        , nre_pcd_binary_
+                                        , nre_pcd_search_path_
+                                        , nre_pcd_pre_load_
+                                        );
+            }
+
+            std::string url_;
+            //std::string masterName_;
+            //std::string masterUrl_;
+            std::string workerUrl_;
+            NotificationService m_guiService;
+            bool bLaunchNrePcd_;
+
+            std::string nre_pcd_binary_;
+            std::vector<std::string> nre_pcd_search_path_;
+            std::vector<std::string> nre_pcd_pre_load_;
+      };
+}}
 
 // Implementation
 #include <sdpa/daemon/nre/NRE.cpp>
