c64e85a33bbed78cd5fce176c37c33e3792b2dc2
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-06-14 14:42:09 +0000

    removed bloat-includes from fhglog/fhglog.hpp
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@5732 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/fhglog/trunk/fhglog/fhglog.hpp
+++ trunk/fhglog/trunk/fhglog/fhglog.hpp
@@ -5,18 +5,6 @@
 #include    <fhglog/error_handler.hpp>
 #include	<fhglog/LogMacros.hpp>
 #include	<fhglog/LoggerApi.hpp>
-#include	<fhglog/Appender.hpp>
-#include    <fhglog/Filter.hpp>
-#include    <fhglog/LogEvent.hpp>
-
-#include	<fhglog/StreamAppender.hpp>
-#include	<fhglog/SynchronizedAppender.hpp>
-#include	<fhglog/FileAppender.hpp>
-#include	<fhglog/CompoundAppender.hpp>
-
-#if defined(FHGLOG_WITH_REMOTE_LOGGING)
-#   include <fhglog/remote/RemoteAppender.hpp>
-#endif
 
 #include    <fhglog/Configuration.hpp>
 
