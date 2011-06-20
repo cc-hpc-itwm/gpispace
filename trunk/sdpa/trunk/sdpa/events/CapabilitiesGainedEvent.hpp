dc951ca580298131b7269b27cc9321271c9739ab
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2011-06-20 08:46:16 +0000

    added CapabilitiesLostEvent and CapabilitiesGainedEvent
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@5764 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/sdpa/trunk/sdpa/events/CapabilitiesGainedEvent.hpp
@@ -0,0 +1,64 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  CapabilitiesGainedEvent.hpp
+ *
+ *    Description:  CapabilitiesGainedEvent
+ *
+ *        Version:  1.0
+ *        Created:
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dr. Tiberiu Rotaru, tiberiu.rotaru@itwm.fraunhofer.de
+ *        Company:  Fraunhofer ITWM
+ *
+ * =====================================================================================
+ */
+#ifndef SDPA_CAPABILITIES_GAINED_EVENT
+#define SDPA_CAPABILITIES_GAINED_EVENT 1
+
+#include <sdpa/sdpa-config.hpp>
+
+#ifdef USE_BOOST_SC
+#   include <boost/statechart/event.hpp>
+namespace sc = boost::statechart;
+#endif
+#include <sdpa/events/MgmtEvent.hpp>
+#include <sdpa/types.hpp>
+
+namespace sdpa { namespace events {
+#ifdef USE_BOOST_SC
+	class CapabilitiesGainedEvent : public MgmtEvent, public sc::event<CapabilitiesGainedEvent> {
+#else
+	class CapabilitiesGainedEvent : public MgmtEvent {
+#endif
+	public:
+		typedef sdpa::shared_ptr<CapabilitiesGainedEvent> Ptr;
+
+        CapabilitiesGainedEvent() : MgmtEvent()
+          , last_job_id_("")
+        {}
+
+		CapabilitiesGainedEvent( const address_t& from , const address_t& to, const sdpa::capabilities_set_t& cpbs = capabilities_set_t() )
+          : MgmtEvent(from, to)
+          , capabilities_(cpbs)
+        {}
+
+		virtual ~CapabilitiesGainedEvent() { }
+
+		const sdpa::job_id_t& capabilities() const { return capabilities_; }
+		sdpa::job_id_t& capabilities() { return capabilities_; }
+
+		std::string str() const { return "CapabilitiesGainedEvent"; }
+
+        virtual void handleBy(EventHandler *handler)
+        {
+          handler->handleCapabilitiesGainedEvent(this);
+        }
+	private:
+		sdpa::capabilities_set_t capabilities_;
+	};
+}}
+
+#endif
---
+++ trunk/sdpa/trunk/sdpa/events/CapabilitiesLostEvent.hpp
@@ -0,0 +1,64 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  CapabilitiesLostEvent.hpp
+ *
+ *    Description:  CapabilitiesLostEvent
+ *
+ *        Version:  1.0
+ *        Created:
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dr. Tiberiu Rotaru, tiberiu.rotaru@itwm.fraunhofer.de
+ *        Company:  Fraunhofer ITWM
+ *
+ * =====================================================================================
+ */
+#ifndef SDPA_CAPABILITIES_LOST_EVENT
+#define SDPA_CAPABILITIES_LOST_EVENT 1
+
+#include <sdpa/sdpa-config.hpp>
+
+#ifdef USE_BOOST_SC
+#   include <boost/statechart/event.hpp>
+namespace sc = boost::statechart;
+#endif
+#include <sdpa/events/MgmtEvent.hpp>
+#include <sdpa/types.hpp>
+
+namespace sdpa { namespace events {
+#ifdef USE_BOOST_SC
+	class CapabilitiesLostEvent : public MgmtEvent, public sc::event<CapabilitiesLostEvent> {
+#else
+	class CapabilitiesLostEvent : public MgmtEvent {
+#endif
+	public:
+		typedef sdpa::shared_ptr<CapabilitiesLostEvent> Ptr;
+
+        CapabilitiesLostEvent() : MgmtEvent()
+          , last_job_id_("")
+        {}
+
+		CapabilitiesLostEvent( const address_t& from , const address_t& to, const sdpa::capabilities_set_t& cpbs = capabilities_set_t() )
+          : MgmtEvent(from, to)
+          , capabilities_(cpbs)
+        {}
+
+		virtual ~CapabilitiesLostEvent() { }
+
+		const sdpa::job_id_t& capabilities() const { return capabilities_; }
+		sdpa::job_id_t& capabilities() { return capabilities_; }
+
+		std::string str() const { return "CapabilitiesLostEvent"; }
+
+        virtual void handleBy(EventHandler *handler)
+        {
+          handler->handleCapabilitiesLostEvent(this);
+        }
+	private:
+		sdpa::capabilities_set_t capabilities_;
+	};
+}}
+
+#endif
