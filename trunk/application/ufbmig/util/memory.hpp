34fab170f9d1ecb8680aca90d77e0703689d67ca
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2011-07-04 14:36:05 +0000

    init/finalize with config communication over filesystem
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@5774 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/application/ufbmig/util/memory.hpp
@@ -0,0 +1,50 @@
+#ifndef _H_UFBMIG_UTIL_ALLOC
+#define _H_UFBMIG_UTIL_ALLOC 1
+
+#include <fvm-pc/pc.hpp>
+
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+namespace ufbmig
+{
+  namespace util
+  {
+    namespace memory
+    {
+      namespace global
+      {
+        inline long alloc (const std::size_t size, const std::string & descr)
+        {
+          const fvmAllocHandle_t handle (fvmGlobalAlloc (size));
+
+          if (handle == 0)
+            {
+              std::ostringstream oss;
+
+              oss << "alloc failed: " << descr << " (" << size << " bytes)";
+
+              throw std::runtime_error (oss.str());
+            }
+
+          return static_cast<long> (handle);
+        }
+
+        inline void free (const long h)
+        {
+          const fvmAllocHandle_t handle (static_cast<fvmAllocHandle_t> (h));
+
+          if (h != 0)
+            {
+              fvmGlobalFree (handle);
+            }
+
+          return;
+        }
+      } // namespace global
+    } // namespace memory
+  } // namespace util
+} // namespace ufbmig
+
+#endif
---
+++ trunk/application/ufbmig/util/rw.hpp
@@ -0,0 +1,59 @@
+#ifndef _H_UFBMIG_UTIL_RW
+#define _H_UFBMIG_UTIL_RW 1
+
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+namespace ufbmig
+{
+  namespace util
+  {
+    namespace serialize
+    {
+      template<typename T>
+      inline void write (const std::string & filename, const T & x)
+      {
+        std::ofstream f (filename.c_str());
+
+        if (!f.good())
+          {
+            std::ostringstream oss;
+
+            oss << "serialize::write to file " << filename << " failed";
+
+            throw std::runtime_error (oss.str());
+          }
+
+        boost::archive::text_oarchive oa (f, boost::archive::no_header);
+
+        oa << x;
+      }
+
+      template<typename T>
+      inline void read (const std::string & filename, T & x)
+      {
+        std::ifstream f (filename.c_str());
+
+        if (!f.good())
+          {
+            std::ostringstream oss;
+
+            oss << "serialize::read from file " << filename << " failed";
+
+            throw std::runtime_error (oss.str());
+          }
+
+        boost::archive::text_iarchive oa (f, boost::archive::no_header);
+
+        oa >> x;
+      }
+    } // namespace serialize
+  } // namespace util
+} // namespace ufbmig
+
+#endif
