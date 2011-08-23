900039a40cb8b884604944c2294b347fc033020d
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-08-23 12:36:58 +0000

    added events.hpp containing includes to all events
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@5895 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/sdpa/trunk/sdpa/events/events.hpp
@@ -0,0 +1,33 @@
+#ifndef SDPA_EVENTS_EVENTS_HPP
+#define SDPA_EVENTS_EVENTS_HPP 1
+
+#include <sdpa/events/CancelJobAckEvent.hpp>
+#include <sdpa/events/CancelJobEvent.hpp>
+#include <sdpa/events/ConfigNokEvent.hpp>
+#include <sdpa/events/ConfigOkEvent.hpp>
+#include <sdpa/events/ConfigReplyEvent.hpp>
+#include <sdpa/events/ConfigRequestEvent.hpp>
+#include <sdpa/events/DeleteJobAckEvent.hpp>
+#include <sdpa/events/DeleteJobEvent.hpp>
+#include <sdpa/events/ErrorEvent.hpp>
+#include <sdpa/events/InterruptEvent.hpp>
+#include <sdpa/events/JobFailedAckEvent.hpp>
+#include <sdpa/events/JobFailedEvent.hpp>
+#include <sdpa/events/JobFinishedAckEvent.hpp>
+#include <sdpa/events/JobFinishedEvent.hpp>
+#include <sdpa/events/JobResultsReplyEvent.hpp>
+#include <sdpa/events/JobStatusReplyEvent.hpp>
+#include <sdpa/events/LifeSignEvent.hpp>
+#include <sdpa/events/QueryJobStatusEvent.hpp>
+#include <sdpa/events/RequestJobEvent.hpp>
+#include <sdpa/events/RetrieveJobResultsEvent.hpp>
+#include <sdpa/events/RunJobEvent.hpp>
+#include <sdpa/events/StartUpEvent.hpp>
+#include <sdpa/events/SubmitJobAckEvent.hpp>
+#include <sdpa/events/SubmitJobEvent.hpp>
+#include <sdpa/events/WorkerRegistrationAckEvent.hpp>
+#include <sdpa/events/WorkerRegistrationEvent.hpp>
+#include <sdpa/events/CapabilitiesGainedEvent.hpp>
+#include <sdpa/events/CapabilitiesLostEvent.hpp>
+
+#endif
