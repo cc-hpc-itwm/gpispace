998d01d88b0557946d0f5a338a9b6c802382b60b
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-08-23 12:37:29 +0000

    enhanced the fhg_add_plugin macro to accept multiple source files correctly
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@5910 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/modules/trunk/fhg_plugin_macros.cmake
+++ trunk/modules/trunk/fhg_plugin_macros.cmake
@@ -1,12 +1,31 @@
-macro(FHG_ADD_PLUGIN name sources)
-  add_library(${name}-plugin MODULE ${sources})
-  set_target_properties(${name}-plugin PROPERTIES OUTPUT_NAME ${name})
-  set_target_properties(${name}-plugin PROPERTIES PREFIX "")
-  string(TOUPPER ${name} upper_name)
-  if (BUILTIN_${upper_name}_PLUGIN)
-    add_library(${name}-plugin.static ${sources})
-    set_target_properties(${name}-plugin.static PROPERTIES OUTPUT_NAME ${name})
-    set_target_properties(${name}-plugin.static PROPERTIES COMPILE_FLAGS "-DFHG_STATIC_PLUGIN=1")
-    list(APPEND builtin-plugins "${name}-plugin.static")
+include(cmake_parse_arguments)
+include(car_cdr_macros)
+
+macro(FHG_ADD_PLUGIN)
+  PARSE_ARGUMENTS(PLUGIN
+    "EXPORTS;LINK_LIBRARIES"
+    "BUILTIN;QUIET"
+    ${ARGN}
+    )
+  CAR(PLUGIN_NAME ${PLUGIN_DEFAULT_ARGS})
+  CDR(PLUGIN_SOURCES ${PLUGIN_DEFAULT_ARGS})
+
+  if (QUIET)
+  else()
+    message(STATUS "**** Adding plugin ${PLUGIN_NAME} with sources ${PLUGIN_SOURCES}")
+  endif()
+
+  add_library(${PLUGIN_NAME}-plugin MODULE ${PLUGIN_SOURCES})
+  set_target_properties(${PLUGIN_NAME}-plugin PROPERTIES OUTPUT_NAME ${PLUGIN_NAME})
+  set_target_properties(${PLUGIN_NAME}-plugin PROPERTIES PREFIX "")
+  target_link_libraries(${PLUGIN_NAME}-plugin ${PLUGIN_LINK_LIBRARIES})
+
+  string(TOUPPER ${PLUGIN_NAME} upper_name)
+  if (PLUGIN_BUILTIN OR BUILTIN_${upper_name}_PLUGIN)
+    add_library(${PLUGIN_NAME}-plugin.static ${PLUGIN_SOURCES})
+    set_target_properties(${PLUGIN_NAME}-plugin.static PROPERTIES OUTPUT_NAME ${PLUGIN_NAME})
+    set_target_properties(${PLUGIN_NAME}-plugin.static PROPERTIES COMPILE_FLAGS "-DFHG_STATIC_PLUGIN=1")
+    target_link_libraries(${PLUGIN_NAME}-plugin.static ${PLUGIN_LINK_LIBRARIES})
+    list(APPEND builtin-plugins "${PLUGIN_NAME}-plugin.static")
   endif()
 endmacro()
