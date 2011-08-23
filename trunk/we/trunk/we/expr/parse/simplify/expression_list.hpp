0b331c4fca925f8c8fc873385eb6a6465b060889
Bernd Loerwald <bernd.loerwald@itwm.fraunhofer.de>
2011-08-23 09:59:36 +0000

    Added expr::parse::simplify::expression_list to hold the parser's node_stack.
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@5867 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/we/trunk/we/expr/parse/simplify/expression_list.hpp
@@ -0,0 +1,44 @@
+// bernd.loerwald@itwm.fraunhofer.de
+
+#ifndef _EXPR_PARSE_SIMPLIFY_EXPRESSION_LIST_HPP
+#define _EXPR_PARSE_SIMPLIFY_EXPRESSION_LIST_HPP 1
+
+#include <we/expr/parse/parser.hpp>
+
+namespace expr
+{
+  namespace parse
+  {
+    namespace simplify
+    {
+      struct expression_list
+      {
+        typedef parser::nd_stack_t node_stack_t;
+        typedef node_stack_t::value_type node_t;
+        typedef node_stack_t::iterator node_stack_it_t;
+        typedef node_stack_t::const_iterator node_stack_const_it_t;
+
+      private:
+        node_stack_t _node_stack;
+
+      public:
+        const node_stack_t& node_stack() const
+        {
+          return _node_stack;
+        }
+
+        explicit expression_list (const parser & p)
+        : _node_stack (p.begin(), p.end())
+        {}
+
+        node_stack_it_t begin () { return _node_stack.begin(); }
+        node_stack_it_t end () { return _node_stack.end(); }
+
+        node_stack_const_it_t begin () const { return _node_stack.begin(); }
+        node_stack_const_it_t end () const { return _node_stack.end(); }
+      };
+    }
+  }
+}
+
+#endif
