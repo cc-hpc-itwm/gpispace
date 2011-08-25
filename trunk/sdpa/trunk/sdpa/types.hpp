74fbf1825b2aa33fe119191b708067f2274bbdba
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2011-08-25 12:10:06 +0000

    added MasterInfo class
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@5928 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/sdpa/types.hpp
+++ trunk/sdpa/trunk/sdpa/types.hpp
@@ -15,9 +15,32 @@ namespace sdpa {
 	typedef std::string status_t;
 	typedef std::string job_result_t;
 	typedef std::list<sdpa::worker_id_t> worker_id_list_t;
-	typedef std::vector<std::string> master_list_t;
-	typedef std::pair<std::string,std::string> capability_t;
 
+	class MasterInfo
+	{
+	public:
+		MasterInfo(const std::string& name  = "", bool registered = false )
+		: name_(name)
+		, registered_(registered)
+		{}
+
+		std::string name() const { return name_; }
+		bool is_registered() const { return registered_; }
+		void set_registered(bool b) { registered_ = b; }
+
+		 template <class Archive>
+		  void serialize(Archive& ar, const unsigned int)
+		  {
+			  ar & name_;
+		  }
+	private:
+		std::string name_;
+		bool registered_;
+	};
+
+	typedef std::vector<MasterInfo> master_info_list_t;
+
+	/*typedef std::pair<std::string,std::string> capability_t;
 
 	struct comp_capabilities
 	{
@@ -28,6 +51,10 @@ namespace sdpa {
 	};
 
 	typedef std::set<capability_t, comp_capabilities > capabilities_set_t;
+	*/
+
+	typedef std::string capability_t;
+	typedef std::set<capability_t > capabilities_set_t;
 }
 
 #endif
