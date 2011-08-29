5e0dc819d809e78ef8fbb11b4d9c9e5ce1f09ce4
Bernd Loerwald <bernd.loerwald@itwm.fraunhofer.de>
2011-08-29 09:02:10 +0000

    Moved simplfication code from test to seperate header.
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@5963 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/we/trunk/tests/test_expr_simplify.cpp
+++ trunk/we/trunk/tests/test_expr_simplify.cpp
@@ -9,27 +9,22 @@
 #include <we/expr/parse/parser.hpp>
 #include <we/expr/parse/util/get_names.hpp>
 
-#include <we/expr/parse/simplify/ssa_tree.hpp>
-#include <we/expr/parse/simplify/expression_list.hpp>
-#include <we/expr/parse/simplify/numbering_and_propagation_pass.hpp>
-#include <we/expr/parse/simplify/dead_code_elimination_pass.hpp>
-#include <we/expr/parse/simplify/bad_access_fix_and_unnumbering_pass.hpp>
+#include <we/expr/parse/simplify/simplify.hpp>
 
-static inline expr::parse::node::key_vec_t
+static inline expr::parse::simplify::key_type
 key_vec_from_string (const std::string & in)
 {
-  expr::parse::node::key_vec_t result;
+  expr::parse::simplify::key_type result;
   boost::algorithm::split (result, in, boost::algorithm::is_any_of ("."));
   return result;
 }
 
 static inline void
 add_to_bindings_list ( const std::string & name
-                     , const expr::parse::simplify::tree_node_type & tree
                      , expr::parse::util::name_set_t & bindings
                      )
 {
-  bindings.insert( tree.get_ssa_name (key_vec_from_string (name)));
+  bindings.insert (key_vec_from_string (name));
 }
 
 int
@@ -75,46 +70,32 @@ main (int argc, char ** argv)
      "([]);\n"
     );
 
+  expr::parse::util::name_set_t needed_bindings;
+  add_to_bindings_list ("___loadTT_generate_init_state", needed_bindings);
+  add_to_bindings_list ("__generate_offset_credits_trigger_when_amount_state", needed_bindings);
+  add_to_bindings_list ("__generate_volume_credits_trigger_when_amount_state", needed_bindings);
+  add_to_bindings_list ("_extract_number_of_volume_credits_b", needed_bindings);
+  add_to_bindings_list ("state", needed_bindings);
+  add_to_bindings_list ("_init_wait_wait", needed_bindings);
+  add_to_bindings_list ("object", needed_bindings);
+  add_to_bindings_list ("wait", needed_bindings);
+  add_to_bindings_list ("b", needed_bindings);
+  add_to_bindings_list ("volume", needed_bindings);
+  add_to_bindings_list ("state", needed_bindings);
+
   expr::parse::parser parser (input);
 
-  expr::parse::util::name_set_t names (expr::parse::util::get_names (parser));
+  try
+  {
+    expr::parse::parser simplified_parser
+        (expr::parse::simplify::simplification_pass (parser, needed_bindings));
+    std::cout << simplified_parser;
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << "exception during simplification: " << e.what() << std::endl;
+  }
 
-  expr::parse::simplify::tree_node_type tree
-    (expr::parse::simplify::create_from_name_set
-      (names, expr::parse::simplify::line_type()));
-
-  expr::parse::simplify::expression_list expressions (parser);
-
-        //  * add ssa numbering
-        //  * copy and constant propagation
-
-  expr::parse::simplify::numbering_and_propagation_pass (expressions, tree);
-
-        //  * dead-code elimination
-        //  * copy propagation fixup
-
-  expr::parse::util::name_set_t needed_bindings;
-  add_to_bindings_list ("___loadTT_generate_init_state", tree, needed_bindings);
-  add_to_bindings_list ("__generate_offset_credits_trigger_when_amount_state", tree, needed_bindings);
-  add_to_bindings_list ("__generate_volume_credits_trigger_when_amount_state", tree, needed_bindings);
-  add_to_bindings_list ("_extract_number_of_volume_credits_b", tree, needed_bindings);
-  add_to_bindings_list ("state", tree, needed_bindings);
-  add_to_bindings_list ("_init_wait_wait", tree, needed_bindings);
-  add_to_bindings_list ("object", tree, needed_bindings);
-  add_to_bindings_list ("wait", tree, needed_bindings);
-  add_to_bindings_list ("b", tree, needed_bindings);
-  add_to_bindings_list ("volume", tree, needed_bindings);
-  add_to_bindings_list ("state", tree, needed_bindings);
-
-  expr::parse::simplify::dead_code_elimination_pass
-      (expressions, needed_bindings);
-
-  expr::parse::simplify::bad_access_fix_and_unnumbering_pass
-      (expressions, tree);
-
-  expr::parse::parser result_parser (expressions.node_stack());
-
-  std::cout << result_parser;
 
   return EXIT_SUCCESS;
 }
---
+++ trunk/we/trunk/we/expr/parse/simplify/simplify.hpp
@@ -0,0 +1,59 @@
+// bernd.loerwald@itwm.fraunhofer.de
+
+#ifndef _EXPR_PARSE_SIMPLIFY_SIMPLIFY_HPP
+#define _EXPR_PARSE_SIMPLIFY_SIMPLIFY_HPP 1
+
+#include <we/expr/parse/parser.hpp>
+#include <we/expr/parse/util/get_names.hpp>
+
+#include <we/expr/parse/simplify/ssa_tree.hpp>
+#include <we/expr/parse/simplify/expression_list.hpp>
+#include <we/expr/parse/simplify/numbering_and_propagation_pass.hpp>
+#include <we/expr/parse/simplify/dead_code_elimination_pass.hpp>
+
+namespace expr
+{
+  namespace parse
+  {
+    namespace simplify
+    {
+      inline util::name_set_t
+      get_ssa_names (const util::name_set_t & n, tree_node_type & tree)
+      {
+        util::name_set_t ssa_names;
+
+        for ( util::name_set_t::const_iterator it (n.begin()), end (n.end())
+            ; it != end
+            ; ++it
+            )
+        {
+          ssa_names.insert (tree.get_current_name (*it));
+        }
+
+        return ssa_names;
+      }
+
+      inline parser
+      simplification_pass ( const parser & parser
+                          , const util::name_set_t & needed_bindings)
+      {
+        tree_node_type tree;
+        tree.initialize ();
+
+        expression_list expressions (parser);
+
+        numbering_and_propagation_pass (expressions, tree);
+
+        util::name_set_t needed_bindings_ssa
+            (get_ssa_names (needed_bindings, tree));
+
+        dead_code_elimination_and_unnumbering_pass
+            (expressions, needed_bindings_ssa);
+
+        return parse::parser (expressions.nodes());
+      }
+    }
+  }
+}
+
+#endif
