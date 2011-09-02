ddefd503d0a5d0d83a12d7860578389fd600ef4d
Bernd Loerwald <bernd.loerwald@itwm.fraunhofer.de>
2011-09-02 12:20:15 +0000

    main is now in a class inheriting from QApplication with several functions rather than one large function.
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@6039 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/editor/pente/trunk/pnete/pnete.cpp
+++ trunk/editor/pente/trunk/pnete/pnete.cpp
@@ -1,126 +1,177 @@
-#include <unistd.h>
+// bernd.loerwald@itwm.fraunhofer.de
+
+#include "pnete.hpp"
 
 #include <iostream>
+#include <stdexcept>
 
-#include <QApplication>
-#include <QTranslator>
 #include <QSettings>
 #include <QPixmap>
-#include <QSplashScreen>
 #include <QLocale>
 #include <QLibraryInfo>
-
-#include <iostream>
 
 #include "ui/MainWindow.hpp"
 
 #define BE_PENTE 1
 
-//! \todo inherit from QApplication and do these things in a class.
-int main(int argc, char *argv[])
+int main (int argc, char *argv[])
 {
-                                                                                // hardcoded constants
-  QApplication a(argc, argv);
-  Q_INIT_RESOURCE(resources);
+  Q_INIT_RESOURCE (resources);
 
-  QCoreApplication::setApplicationName("pnete");
-  //! \todo get svn revision.
-  QCoreApplication::setApplicationVersion("0.1");
-  QCoreApplication::setOrganizationDomain("itwm.fhg.de");
-  QCoreApplication::setOrganizationName("Fraunhofer ITWM");
-
-#ifdef BE_PENTE
-  QPixmap pixmap(":/pente.png");
-  QSplashScreen splash(pixmap);
-  splash.show();
-  a.processEvents();
-#endif
-
-  QTranslator qtTranslator;
-  QString locale (QLocale::system().name());
-
-  qtTranslator.load ("qt_" + locale, QLibraryInfo::location(QLibraryInfo::TranslationsPath));
-  a.installTranslator(&qtTranslator);
-
-  QTranslator translator;
-  translator.load(locale, ":/localization/");
-  a.installTranslator(&translator);
-
-  QSettings settings;
-
-  settings.beginGroup("transitionLibrary");
-
-  QStringList arguments = QCoreApplication::arguments();
-  if(arguments.contains("--make-config"))
-  {
-    if(arguments.size() <= arguments.indexOf("--make-config") + 1)
-    {
-      std::cerr << "Please also specify a path containing lib/, statoil/ and user/!" << std::endl;
-      return EXIT_FAILURE;
-    }
-
-    QString pnetedir = arguments.at(arguments.indexOf("--make-config") + 1);
-
-    settings.setValue("basePath", QString("%1/lib").arg(pnetedir));
-
-    //! \todo this should not be default!
-    settings.beginWriteArray("trustedPaths");
-    settings.setArrayIndex(0);
-    settings.setValue("path", QString("%1/statoil").arg(pnetedir));
-    settings.endArray();
-
-    settings.beginWriteArray("userPaths");
-    settings.setArrayIndex(0);
-    settings.setValue("path", QString("%1/user").arg(pnetedir));
-    settings.endArray();
-  }
-
-  QString load;
-
-  if (arguments.contains("--load"))
-    {
-      if(arguments.size() <= arguments.indexOf("--load") + 1)
-        {
-          std::cerr << "--load requires a path" << std::endl;
-          return EXIT_FAILURE;
-        }
-
-      load = arguments.at (arguments.indexOf("--load") + 1);
-    }
-
-  if(!settings.contains("basePath"))
-  {
-    //! \todo error message.
-    std::cerr << "There is no base path set for the transition library." << std::endl;
-    std::cerr << "Please run \"" << qPrintable(arguments.at(0)) << " --make-config <path>\", where path contains lib." << std::endl;
-    return EXIT_FAILURE;
-  }
-
-  fhg::pnete::ui::MainWindow w (load);
-  w.setTransitionLibraryPath(settings.value("basePath").toString());
-
-  int numTrusted = settings.beginReadArray("trustedPaths");
-  for(int i = 0; i < numTrusted; ++i)
-  {
-    settings.setArrayIndex(i);
-    w.addTransitionLibraryUserPath(settings.value("path").toString(), true);
-  }
-  settings.endArray();
-
-  int numUser = settings.beginReadArray("userPaths");
-  for(int i = 0; i < numUser; ++i)
-  {
-    settings.setArrayIndex(i);
-    w.addTransitionLibraryUserPath(settings.value("path").toString(), false);
-  }
-  settings.endArray();
-
-  w.show();
-#ifdef BE_PENTE
-  splash.finish(&w);
-#endif
-
-  return a.exec();
+  fhg::pnete::PetriNetEditor pente (argc, argv);
+  pente.startup ();
+  return pente.exec ();
 }
 
-#undef BE_PENTE
+namespace fhg
+{
+  namespace pnete
+  {
+    void PetriNetEditor::showSplashScreen ()
+    {
+      _splash.show ();
+      processEvents ();
+    }
+
+    void PetriNetEditor::hideSplashScreen ()
+    {
+      _splash.finish (_mainWindow);
+    }
+
+    PetriNetEditor::PetriNetEditor (int & argc, char *argv[])
+    : QApplication (argc, argv)
+    , _splash (QPixmap (":/pente.png"))
+    , _mainWindow (NULL)
+    {
+      setApplicationName ("pnete");
+      //! \todo Get SVN revision.
+      setApplicationVersion ("0.1");
+      setOrganizationDomain ("itwm.fhg.de");
+      setOrganizationName ("Fraunhofer ITWM");
+    }
+
+    void PetriNetEditor::startup ()
+    {
+#ifdef BE_PENTE
+      showSplashScreen ();
+#endif
+
+      setupLocalization ();
+      processCommandLine ();
+      createMainWindow ();
+      createTransitionLibrary ();
+
+#ifdef BE_PENTE
+      hideSplashScreen();
+#endif
+    }
+
+    PetriNetEditor::~PetriNetEditor ()
+    {
+      _mainWindow->hide();
+      //! \todo This segfaults.
+      delete _mainWindow;
+      _mainWindow = NULL;
+    }
+
+    void PetriNetEditor::setupLocalization ()
+    {
+      const QString& locale (QLocale::system().name());
+
+      _qtTranslator.load ( "qt_" + locale
+                        , QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+      installTranslator (&_qtTranslator);
+
+      _penteTranslator.load (locale, ":/localization/");
+      installTranslator (&_penteTranslator);
+    }
+
+    void PetriNetEditor::processCommandLine ()
+    {
+      const QStringList& args (arguments ());
+      if (args.contains ("--make-config"))
+      {
+        QSettings settings;
+        settings.beginGroup ("transitionLibrary");
+
+        if(args.size () <= args.indexOf ("--make-config") + 1)
+        {
+          std::cerr << "Please also specify a path containing lib/, statoil/ and user/!" << std::endl;
+          throw std::runtime_error ("Please specify base path.");
+        }
+
+        QString pnetedir = args.at (args.indexOf ("--make-config") + 1);
+
+        settings.setValue ("basePath", QString ("%1/lib").arg (pnetedir));
+
+        //! \todo this should not be default!
+        settings.beginWriteArray ("trustedPaths");
+        settings.setArrayIndex (0);
+        settings.setValue ("path", QString ("%1/statoil").arg (pnetedir));
+        settings.endArray ();
+
+        settings.beginWriteArray ("userPaths");
+        settings.setArrayIndex (0);
+        settings.setValue ("path", QString ("%1/user").arg (pnetedir));
+        settings.endArray ();
+
+        settings.endGroup ();
+      }
+    }
+
+    void PetriNetEditor::createMainWindow ()
+    {
+      QString load;
+
+      const QStringList& args (arguments ());
+      if (args.contains ("--load"))
+      {
+        if(args.size () <= args.indexOf ("--load") + 1)
+        {
+          std::cerr << "--load requires a path" << std::endl;
+          throw std::runtime_error ("Please specify filename.");
+        }
+
+        load = args.at (args.indexOf ("--load") + 1);
+      }
+
+      _mainWindow = new ui::MainWindow (load);
+      _mainWindow->show ();
+    }
+
+    void PetriNetEditor::createTransitionLibrary ()
+    {
+      QSettings settings;
+
+      settings.beginGroup ("transitionLibrary");
+
+      if (!settings.contains ("basePath"))
+      {
+        //! \todo error message.
+        std::cerr << "There is no base path set for the transition library." << std::endl;
+        std::cerr << "Please run \"" << qPrintable (arguments ().at (0)) << " --make-config <path>\", where path contains lib." << std::endl;
+        throw std::runtime_error ("Please configure first.");
+      }
+
+      _mainWindow->setTransitionLibraryPath (settings.value ("basePath").toString ());
+
+      const int numTrusted (settings.beginReadArray ("trustedPaths"));
+      for (int i (0); i < numTrusted; ++i)
+      {
+        settings.setArrayIndex (i);
+        _mainWindow->addTransitionLibraryUserPath (settings.value ("path").toString (), true);
+      }
+      settings.endArray();
+
+      const int numUser (settings.beginReadArray ("userPaths"));
+      for (int i (0); i < numUser; ++i)
+      {
+        settings.setArrayIndex (i);
+        _mainWindow->addTransitionLibraryUserPath (settings.value ("path").toString (), false);
+      }
+      settings.endArray ();
+
+      settings.endGroup ();
+    }
+  }
+}
---
+++ trunk/editor/pente/trunk/pnete/pnete.hpp
@@ -0,0 +1,44 @@
+// bernd.loerwald@itwm.fraunhofer.de
+
+#ifndef _PENTE_HPP
+#define _PENTE_HPP 1
+
+#include <QApplication>
+#include <QTranslator>
+#include <QSplashScreen>
+
+namespace fhg
+{
+  namespace pnete
+  {
+    namespace ui
+    {
+      class MainWindow;
+    }
+
+    class PetriNetEditor : public QApplication
+    {
+    private:
+      QSplashScreen _splash;
+      QTranslator _qtTranslator;
+      QTranslator _penteTranslator;
+      ui::MainWindow* _mainWindow;
+
+      void showSplashScreen ();
+      void hideSplashScreen ();
+
+    public:
+      PetriNetEditor (int & argc, char *argv[]);
+      virtual ~PetriNetEditor ();
+
+      void startup ();
+
+      void setupLocalization ();
+      void processCommandLine ();
+      void createMainWindow ();
+      void createTransitionLibrary ();
+    };
+  }
+}
+
+#endif _PENTE_HPP
