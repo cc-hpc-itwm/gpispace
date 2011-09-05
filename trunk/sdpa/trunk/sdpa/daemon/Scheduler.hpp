e5cb69ece581a1bea4998872822b01eac8e2f295
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-09-05 15:57:41 +0000

    removed duplicate function
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@6080 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/sdpa/daemon/Scheduler.hpp
+++ trunk/sdpa/trunk/sdpa/daemon/Scheduler.hpp
@@ -68,7 +68,6 @@ namespace daemon {
 
          virtual void reschedule(const sdpa::job_id_t &job) = 0;
 	 virtual void reschedule(const Worker::worker_id_t& ) throw (WorkerNotFoundException) = 0;
-	 virtual void reschedule( const sdpa::job_id_t& job_id ) = 0;
 	 virtual void reschedule(Worker::JobQueue* pQueue ) = 0;
 
 	 virtual bool has_job(const sdpa::job_id_t& job_id) = 0;
