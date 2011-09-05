bbce7103d4c53d1ea9b9e16402c3ba82a08d59c7
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-09-05 15:57:27 +0000

    sending gui notifications
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@6074 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/plugin/gui.cpp
+++ trunk/sdpa/trunk/plugin/gui.cpp
@@ -1,6 +1,12 @@
 #include <fhglog/minimal.hpp>
 #include <fhg/plugin/plugin.hpp>
 
+#include <fhglog/remote/RemoteAppender.hpp>
+
+#include <sdpa/daemon/NotificationEvent.hpp>
+
+#include <boost/archive/text_oarchive.hpp>
+
 #include "observable.hpp"
 #include "observer.hpp"
 #include "task_event.hpp"
@@ -11,6 +17,29 @@ class GuiObserverPlugin : FHG_PLUGIN
 public:
   FHG_PLUGIN_START()
   {
+    m_url = fhg_kernel()->get("url", "");
+    if ("" == m_url)
+    {
+      MLOG(ERROR, "no GUI URL specified, please set plugin.gui.url");
+      FHG_PLUGIN_FAILED(EINVAL);
+    }
+
+    try
+    {
+      m_destination = fhg::log::Appender::ptr_t
+        (new fhg::log::remote::RemoteAppender ( "gui"
+                                              , m_url
+                                              )
+        );
+    }
+    catch (std::exception const &ex)
+    {
+      MLOG(ERROR, "could not start appender to url: " << m_url << ": " << ex.what());
+      FHG_PLUGIN_FAILED(EINVAL);
+    }
+
+    MLOG(INFO, "GUI sending events to " << m_url);
+
     FHG_PLUGIN_STARTED();
   }
 
@@ -49,11 +78,16 @@ public:
     try
     {
       task_event_t const & t (boost::any_cast<task_event_t>(evt));
-      MLOG(INFO, "*** TASK EVENT: " << t.id << " (" << t.name << ")" << " state = " << t.state);
+      MLOG(TRACE, "*** TASK EVENT: " << t.id << " (" << t.name << ")" << " state = " << t.state);
+      m_destination->append(FHGLOG_MKEVENT_HERE(INFO, encode(t)));
     }
     catch (boost::bad_any_cast const &ex)
     {
       MLOG(WARN, "got bad notification event: " << ex.what());
+    }
+    catch (std::exception const & ex)
+    {
+      MLOG(ERROR, "could not send event: " << ex.what());
     }
   }
 private:
@@ -64,7 +98,43 @@ private:
     m_observed.remove(o);
   }
 
+  static inline std::string encode (const task_event_t & e)
+  {
+    sdpa::daemon::NotificationEvent n_evt;
+    n_evt.activity_id() = e.id;
+    n_evt.activity_name() = e.name;
+    n_evt.activity_result() = e.output;
+    switch (e.state)
+    {
+    case task_event_t::ENQUEUED:
+      n_evt.activity_state() = sdpa::daemon::NotificationEvent::STATE_CREATED;
+      break;
+    case task_event_t::DEQUEUED:
+      n_evt.activity_state() = sdpa::daemon::NotificationEvent::STATE_STARTED;
+      break;
+    case task_event_t::FINISHED:
+      n_evt.activity_state() = sdpa::daemon::NotificationEvent::STATE_FINISHED;
+      break;
+    case task_event_t::FAILED:
+      n_evt.activity_state() = sdpa::daemon::NotificationEvent::STATE_FAILED;
+      break;
+    case task_event_t::CANCELED:
+      n_evt.activity_state() = sdpa::daemon::NotificationEvent::STATE_CANCELLED;
+      break;
+    default:
+      n_evt.activity_state() = sdpa::daemon::NotificationEvent::STATE_IGNORE;
+      break;
+    }
+
+    std::ostringstream sstr;
+    boost::archive::text_oarchive ar(sstr);
+    ar << n_evt;
+    return sstr.str();
+  }
+
   std::list<observe::Observable*> m_observed;
+  std::string m_url;
+  fhg::log::Appender::ptr_t m_destination;
 };
 
 EXPORT_FHG_PLUGIN( gui
