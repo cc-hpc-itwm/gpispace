d862842b11363e20775c683bb0c02ce6d831217f
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2011-09-16 07:20:52 +0000

    add hasher for shared_ptr, cosmetics
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@6238 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/editor/pente/trunk/pnete/util.hpp
+++ trunk/editor/pente/trunk/pnete/util.hpp
@@ -1,7 +1,12 @@
 // bernd.loerwald@itwm.fraunhofer.de
 
-#ifndef _UTIL_HPP
-#define _UTIL_HPP 1
+#ifndef _PNETE_UTIL_HPP
+#define _PNETE_UTIL_HPP 1
+
+#include <boost/shared_ptr.hpp>
+
+#include <QWidget>
+#include <QObject>
 
 namespace fhg
 {
@@ -27,6 +32,15 @@ namespace fhg
       }
       return qobject_cast<T*> (object);
     }
+    template<class T>
+    class ptr_hasher
+    {
+    public:
+      std::size_t operator () (const boost::shared_ptr<T>& key) const
+      {
+        return reinterpret_cast<std::size_t> (key.get());
+      }
+    };
   }
 }
 
