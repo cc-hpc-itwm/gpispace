c61bc32c4f7f42707d0a0aa238300f4d150ed4f2
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-10-19 13:01:09 +0000

    locating static and shared library of libhwloc
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@6576 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/modules/trunk/FindHWLOC.cmake
+++ trunk/modules/trunk/FindHWLOC.cmake
@@ -3,7 +3,8 @@
 # This file defines:
 # * HWLOC_FOUND if libreadline was found
 # * HWLOC_LIBRARY The lib to link to (currently only a static unix lib)
-# * HWLOC_LIBRARY_SHARED The lib to link to (currently only a static unix lib)
+# * HWLOC_STATIC_LIBRARY
+# * HWLOC_SHARED_LIBRARY The lib to link to (currently only a static unix lib)
 # * HWLOC_INCLUDE_DIR
 
 if (NOT HWLOC_FIND_QUIETLY)
@@ -16,16 +17,29 @@ find_path (HWLOC_INCLUDE_DIR
   PATH_SUFFIXES include
   )
 
-find_library (HWLOC_LIBRARY
-  NAMES hwloc
+find_library (HWLOC_STATIC_LIBRARY
+  NAMES libhwloc.a
+  HINTS ${HWLOC_HOME} ENV HWLOC_HOME
+  PATH_SUFFIXES lib lib64
+  )
+find_library (HWLOC_SHARED_LIBRARY
+  NAMES libhwloc.so
   HINTS ${HWLOC_HOME} ENV HWLOC_HOME
   PATH_SUFFIXES lib lib64
   )
 
+if (HWLOC_STATIC_LIBRARY)
+  set(HWLOC_LIBRARY ${HWLOC_STATIC_LIBRARY})
+else()
+  if (HWLOC_SHARED_LIBRARY)
+    set(HWLOC_LIBRARY ${HWLOC_SHARED_LIBRARY})
+  endif()
+endif()
+
 if (HWLOC_INCLUDE_DIR AND HWLOC_LIBRARY)
   set (HWLOC_FOUND TRUE)
   if (NOT HWLOC_FIND_QUIETLY)
-    message (STATUS "Found HWLOC headers in ${HWLOC_INCLUDE_DIR} and libraries ${HWLOC_LIBRARY} ${HWLOC_LIBRARY_SHARED}")
+    message (STATUS "Found HWLOC headers in ${HWLOC_INCLUDE_DIR} and libraries ${HWLOC_LIBRARY} ${HWLOC_SHARED_LIBRARY}")
   endif ()
 else ()
   if (HWLOC_FIND_REQUIRED)
