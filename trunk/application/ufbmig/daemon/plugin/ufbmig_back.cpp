c96793e39fe49aad6cbf7f3cc4ae42983e5f0a47
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-10-19 09:58:25 +0000

    submitting some fake jobs
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@6569 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/application/ufbmig/daemon/plugin/ufbmig_back.cpp
+++ trunk/application/ufbmig/daemon/plugin/ufbmig_back.cpp
@@ -14,6 +14,16 @@
 #include "sdpactl.hpp"
 #include "sdpac.hpp"
 
+namespace job_type
+{
+  enum code
+    {
+      INITIALIZE,
+      CALCULATE,
+      FINALIZE,
+    };
+}
+
 struct job_info_t
 {
   int         type;     // INIT, CALC, FINAL, ...
@@ -53,12 +63,26 @@ public:
     FHG_PLUGIN_STOPPED();
   }
 
+  void registerFrontend(ufbmig::Frontend* f)
+  {
+    // insert a listener that is called when:
+    //     initialize finihed
+    //          initialize_done(error)
+    //     calculate finished
+    //          calculate_done(error)
+    //     finalize finished
+    //          finalize_done(error)
+    m_frontend = f;
+  }
+
   int initialize(std::string const &desc)
   {
+    MLOG(INFO, "submitting INITIALIZE workflow");
+
     job_info_t job_info;
     int ec;
 
-    job_info.type = 0;
+    job_info.type = job_type::INITIALIZE;
 
     // write xml desc to file
 
@@ -78,33 +102,78 @@ public:
     //    std::string res;
     //    sdpa_c->execute(read_workflow_from_file("/u/herc/petry/pnet/fail.pnet"), res);
 
-    return 0;
+    return ec;
   }
 
-  void registerFrontend(ufbmig::Frontend* f)
+  int update_salt_mask (const char *data, size_t len)
   {
-    // insert a listener that is called when:
-    //     initialize finihed
-    //          initialize_done(error)
-    //     calculate finished
-    //          calculate_done(error)
-    //     finalize finished
-    //          finalize_done(error)
-    m_frontend = f;
+    MLOG(INFO, "updating SALTMASK");
+    return 0;
   }
 
   int calculate()
   {
-    return 0;
+    MLOG(INFO, "submitting CALCULATE workflow");
+
+    job_info_t job_info;
+    int ec;
+
+    job_info.type = job_type::CALCULATE;
+
+    // write xml desc to file
+
+    // read workflow from file
+    //    use wfe plugin?
+    //        Workflow wfe->parse(string);
+    //        Workflow.put("port", value);
+    //        Workflow.get<>("port");
+    // place tokens
+    // submit job
+    ec = sdpa_c->submit( read_workflow_from_file(m_wf_path_initialize)
+                       , job_info.id
+                       );
+
+    // append job_info_t
+    // start kernel task to query job state
+    //    std::string res;
+    //    sdpa_c->execute(read_workflow_from_file("/u/herc/petry/pnet/fail.pnet"), res);
+
+    return ec;
   }
 
   int finalize()
   {
-    return 0;
+    MLOG(INFO, "submitting FINALIZE workflow");
+
+    job_info_t job_info;
+    int ec;
+
+    job_info.type = job_type::FINALIZE;
+
+    // write xml desc to file
+
+    // read workflow from file
+    //    use wfe plugin?
+    //        Workflow wfe->parse(string);
+    //        Workflow.put("port", value);
+    //        Workflow.get<>("port");
+    // place tokens
+    // submit job
+    ec = sdpa_c->submit( read_workflow_from_file(m_wf_path_initialize)
+                       , job_info.id
+                       );
+
+    // append job_info_t
+    // start kernel task to query job state
+    //    std::string res;
+    //    sdpa_c->execute(read_workflow_from_file("/u/herc/petry/pnet/fail.pnet"), res);
+
+    return ec;
   }
 
   int cancel()
   {
+    MLOG(INFO, "CANCELLING running workflows");
     return 0;
   }
 
@@ -123,11 +192,6 @@ public:
   }
 
   size_t salt_mask_size ()
-  {
-    return 0;
-  }
-
-  int update_salt_mask (const char *buffer, size_t buffer_size, size_t offset)
   {
     return 0;
   }
--- trunk/application/ufbmig/daemon/plugin/ufbmig_back.hpp
+++ trunk/application/ufbmig/daemon/plugin/ufbmig_back.hpp
@@ -12,6 +12,7 @@ namespace ufbmig
     virtual ~Backend() {}
 
     virtual int initialize(std::string const &) = 0;
+    virtual int update_salt_mask(const char *data, size_t len) = 0;
     virtual int calculate() = 0;
     virtual int finalize() = 0;
     virtual int cancel() = 0;
