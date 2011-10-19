2a8cf02ce49f0f987c2728e511c5bb1da1d73994
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-10-19 09:58:08 +0000

    starting listener within the ufbmig frontend
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@6559 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/application/ufbmig/daemon/plugin/ufbmig_front.cpp
+++ trunk/application/ufbmig/daemon/plugin/ufbmig_front.cpp
@@ -5,12 +5,52 @@
 #include <fhglog/minimal.hpp>
 #include <fhg/plugin/plugin.hpp>
 
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <cstdlib>
+#include <iostream>
+
+#include "ServerCommunication.h"
+#include "Server.h"
+#include "Message.h"
+#include "ServerSettings.hpp"
+
+static const std::string SERVER_APP_NAME("UfBMig (SDPA)");
+static const std::string SERVER_APP_VERS("0.0.1");
+
 class UfBMigFrontImpl : FHG_PLUGIN
                       , public ufbmig::Frontend
 {
 public:
+  UfBMigFrontImpl ()
+  {
+    // start server communication
+    PSProMigIF::StartServer::registerInstance
+      ( SERVER_APP_NAME
+      , new PSProMigIF::ServerHelper(SERVER_APP_VERS)
+      );
+
+    m_server = PSProMigIF::StartServer::getInstance(SERVER_APP_NAME);
+    m_server->handleExceptionsByLibrary(false);
+    m_server->addCommunication (new PSProMigIF::ServerCommunicationListen());
+  }
+
   FHG_PLUGIN_START()
   {
+    try
+    {
+      // start server control object
+      m_server->start();
+    }
+    catch (PSProMigIF::StartServer::StartServerException const &ex)
+    {
+      LOG(ERROR, "could not start server connection: " << ex.what());
+      FHG_PLUGIN_FAILED(ETIMEDOUT);
+      // TODO:
+      //   mark plugin as incomplete, try to start connection again...
+    }
+
     FHG_PLUGIN_STARTED();
   }
 
@@ -41,19 +81,25 @@ public:
 
   void initialize_done (int)
   {
+    return;
   }
 
   void calculate_done (int)
   {
+    return;
   }
 
   void finalize_done (int)
   {
+    return;
   }
 
   void cancel_done (int)
   {
+    return;
   }
+private:
+  PSProMigIF::StartServer* m_server;
 };
 
 EXPORT_FHG_PLUGIN( ufbmig_front
