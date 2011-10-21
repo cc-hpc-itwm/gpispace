016208d24c540e76435898e718cd01c197dff3c5
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-10-21 16:37:42 +0000

    fhg_add_plugin macro is able to install files
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@6612 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/modules/trunk/fhg_plugin_macros.cmake
+++ trunk/modules/trunk/fhg_plugin_macros.cmake
@@ -3,14 +3,14 @@ include(car_cdr_macros)
 
 macro(FHG_ADD_PLUGIN)
   PARSE_ARGUMENTS(PLUGIN
-    "EXPORTS;LINK_LIBRARIES"
-    "BUILTIN;QUIET"
+    "EXPORTS;LINK_LIBRARIES;HEADERS;HEADER_DESTINATION;PLUGIN_DESTINATION;COMPONENT"
+    "BUILTIN;QUIET;INSTALL"
     ${ARGN}
     )
   CAR(PLUGIN_NAME ${PLUGIN_DEFAULT_ARGS})
   CDR(PLUGIN_SOURCES ${PLUGIN_DEFAULT_ARGS})
 
-  if (QUIET)
+  if (PLUGIN_QUIET)
   else()
     message(STATUS "**** Adding plugin ${PLUGIN_NAME} with sources ${PLUGIN_SOURCES}")
   endif()
@@ -20,6 +20,13 @@ macro(FHG_ADD_PLUGIN)
   set_target_properties(${PLUGIN_NAME}-plugin PROPERTIES PREFIX "")
   target_link_libraries(${PLUGIN_NAME}-plugin ${PLUGIN_LINK_LIBRARIES})
 
+  if(PLUGIN_INSTALL)
+    install(TARGETS ${PLUGIN_NAME}-plugin LIBRARY DESTINATION ${PLUGIN_PLUGIN_DESTINATION} COMPONENT ${PLUGIN_COMPONENT})
+    if (PLUGIN_HEADERS)
+      install(FILES ${PLUGIN_HEADERS} DESTINATION ${PLUGIN_HEADER_DESTINATION} COMPONENT ${PLUGIN_COMPONENT})
+    endif()
+  endif()
+
   string(TOUPPER ${PLUGIN_NAME} upper_name)
   if (PLUGIN_BUILTIN OR BUILTIN_${upper_name}_PLUGIN)
     add_library(${PLUGIN_NAME}-plugin.static ${PLUGIN_SOURCES})
@@ -27,5 +34,8 @@ macro(FHG_ADD_PLUGIN)
     set_target_properties(${PLUGIN_NAME}-plugin.static PROPERTIES COMPILE_FLAGS "-DFHG_STATIC_PLUGIN=1")
     target_link_libraries(${PLUGIN_NAME}-plugin.static ${PLUGIN_LINK_LIBRARIES})
     list(APPEND builtin-plugins "${PLUGIN_NAME}-plugin.static")
+    if (PLUGIN_INSTALL)
+      install(TARGETS ${PLUGIN_NAME}-plugin.static ARCHIVE DESTINATION ${PLUGIN_PLUGIN_DESTINATION} COMPONENT ${PLUGIN_COMPONENT})
+    endif()
   endif()
 endmacro()
