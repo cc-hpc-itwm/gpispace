5f594228455f0164db03676668d8fa848139ce6b
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-10-21 16:37:38 +0000

    added pnet macros to automatically build pnets
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@6610 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/modules/trunk/pnet_macros.cmake
@@ -0,0 +1,63 @@
+include(cmake_parse_arguments)
+include(car_cdr_macros)
+
+macro(PNET_COMPILE)
+  PARSE_ARGUMENTS(PNET
+    "INCLUDES;GENERATE;OUTPUT;FLAGS;INSTALL;DEPENDS"
+    "QUIET;BUILD"
+    ${ARGN}
+    )
+  CAR(PNET_NAME ${PNET_DEFAULT_ARGS})
+  CDR(PNET_SOURCES ${PNET_DEFAULT_ARGS})
+
+  set(PNETC_LOCATION pnetc)
+  if (TARGET pnetc)
+    get_target_property(PNETC_LOCATION pnetc LOCATION)
+  endif()
+
+  set (PNET__default_flags "--Woverwrite-file=false")
+
+  if (PNET_QUIET)
+  else()
+    message(STATUS "**** Adding pnet ${PNET_NAME} with source ${PNET_SOURCES}")
+  endif()
+
+  if (PNET_INCLUDES)
+    foreach(p ${PNET_INCLUDES})
+      set (${PNET_FLAGS} "${PNET_FLAGS} -I ${p}")
+    endforeach()
+  endif()
+
+  if (PNET_OUTPUT)
+  else()
+    set(PNET_OUTPUT ${PNET_NAME}.pnet)
+  endif()
+
+  set(PNETC_ARGS ${PNET__default_flags} ${PNET_FLAGS} ${PNET_SOURCES} -o ${PNET_OUTPUT})
+
+  if (PNET_GENERATE)
+    set(PNETC_ARGS ${PNETC_ARGS} -g ${PNET_GENERATE})
+  endif()
+
+  add_custom_target(pnet-${PNET_NAME} ALL
+    COMMAND ${PNETC_LOCATION} ${PNETC_ARGS}
+    DEPENDS ${PNETC_LOCATION} ${PNET_DEPENDS}
+    COMMENT "compiling petri-net: ${PNET_NAME}"
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    SOURCES ${PNET_SOURCES}
+    )
+  if (PNET_BUILD)
+    add_custom_command(TARGET pnet-${PNET_NAME} POST_BUILD
+      COMMAND make -C ${PNET_GENERATE}
+      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+      )
+  endif()
+
+  if (TARGET pnetc)
+    add_dependencies(pnet-${PNET_NAME} pnetc)
+  endif()
+
+  if (PNET_INSTALL)
+    install (FILES ${PNET_SOURCES} ${PNET_OUTPUT} DESTINATION ${PNET_INSTALL})
+  endif()
+endmacro()
