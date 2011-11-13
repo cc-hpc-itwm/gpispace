c4446b495fd6bf457d760b8cf97546e29c7483e3
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-11-13 23:22:30 +0000

    fixed restarting of SDPA via ufbmigd
      - we need to fork() before calling system() to be able to close open
        file descriptors...
    
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@6932 c3adf61f-8968-0410-94d0-83abfdf586cf

--- trunk/sdpa/trunk/plugin/sdpactl.cpp
+++ trunk/sdpa/trunk/plugin/sdpactl.cpp
@@ -1,3 +1,6 @@
+#include <sys/types.h> // fok
+#include <unistd.h> // fork
+#include <sys/wait.h> // waitpid
 #include <stdlib.h> // system
 
 #include "sdpactl.hpp"
@@ -6,6 +9,90 @@
 
 #include <fhglog/minimal.hpp>
 #include <fhg/plugin/plugin.hpp>
+
+typedef int (*ChildFunction)(void);
+
+namespace helper
+{
+  static void close_fds ()
+  {
+    for (int fd = 0 ; fd < 1024 ; ++fd)
+    {
+      close (fd);
+    }
+  }
+
+  static int do_start ()
+  {
+    int ec = 0;
+    ec += system("sdpa start gpi");
+    ec += system("sdpa start orch");
+    ec += system("sdpa start agg");
+    ec += system("sdpa start nre");
+    ec += system("sdpa start drts");
+    return ec;
+  }
+
+  static int do_stop ()
+  {
+    int ec = 0;
+    ec += system("sdpa stop drts");
+    ec += system("sdpa stop nre");
+    ec += system("sdpa stop agg");
+    ec += system("sdpa stop orch");
+    sleep(3);
+    return ec;
+  }
+
+  static int do_restart ()
+  {
+    int ec = 0;
+    ec += do_stop ();
+    ec += do_start ();
+    return ec;
+  }
+
+  static int run_in_child (ChildFunction fun)
+  {
+    pid_t child = fork ();
+    if (child == 0)
+    {
+      close_fds ();
+      int ec = 0;
+
+      ec += fun();
+
+      _exit (ec);
+
+      // never reached
+      //   return ec;
+    }
+    else if (child < 0)
+    {
+      int ec = errno;
+      MLOG(ERROR, "could not fork: " << strerror(ec));
+      return ec;
+    }
+    else
+    {
+      int status;
+      pid_t ec = waitpid(child, &status, 0);
+
+      if (WIFEXITED(status))
+      {
+        return WEXITSTATUS(status);
+      }
+      else if (WIFSIGNALED(status))
+      {
+        return WTERMSIG(status);
+      }
+      else
+      {
+        return EFAULT;
+      }
+    }
+  }
+}
 
 class ControlImpl : FHG_PLUGIN
                   , public sdpa::Control
@@ -23,32 +110,17 @@ public:
 
   int start ()
   {
-    int ec = 0;
-    ec += system("sdpa start gpi");
-    ec += system("sdpa start orch");
-    ec += system("sdpa start agg");
-    ec += system("sdpa start nre");
-    ec += system("sdpa start drts");
-    return ec;
+    return helper::run_in_child (&helper::do_start);
   }
 
   int restart ()
   {
-    int ec = 0;
-    ec += stop ();
-    ec += start ();
-    return ec;
+    return helper::run_in_child (&helper::do_restart);
   }
 
   int stop ()
   {
-    int ec = 0;
-    ec += system("sdpa stop drts");
-    ec += system("sdpa stop nre");
-    ec += system("sdpa stop agg");
-    ec += system("sdpa stop orch");
-    sleep(3);
-    return ec;
+    return helper::run_in_child (&helper::do_stop);
   }
 };
 
