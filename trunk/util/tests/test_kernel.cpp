71b0270c8e3c10073ab51d13e9b797ccb023252b
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2011-12-14 12:19:05 +0000

    added testcase for start/stop of plugin::kernel
    From: Alexander Petry <alexander.petry@itwm.fraunhofer.de>
    
    git-svn-id: https://svn.itwm.fhg.de/svn/SDPA@7113 c3adf61f-8968-0410-94d0-83abfdf586cf

---
+++ trunk/util/tests/test_kernel.cpp
@@ -0,0 +1,101 @@
+#include <boost/test/included/unit_test.hpp>
+
+#include <fhg/plugin/plugin.hpp>
+#include <fhg/plugin/core/kernel.hpp>
+
+#include <vector>
+#include <boost/filesystem.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/thread.hpp>
+#include <boost/bind.hpp>
+
+namespace fs = boost::filesystem;
+using namespace boost::unit_test;
+
+typedef boost::shared_ptr <fhg::core::kernel_t> kernel_ptr_t;
+typedef std::vector<fs::path> path_list_t;
+
+static fs::path hello_plugin;
+static fs::path world_plugin;
+static fs::path helloworld_plugin;
+
+void test_start_stop_kernel ()
+{
+  kernel_ptr_t krnl(new fhg::core::kernel_t);
+
+  boost::thread krnl_thread
+    (boost::bind (&fhg::core::kernel_t::run, krnl));
+
+  // load some plugins
+  krnl->load_plugin (hello_plugin.string());
+  krnl->load_plugin (world_plugin.string());
+  krnl->load_plugin (helloworld_plugin.string());
+
+  boost::this_thread::sleep (boost::posix_time::seconds(2));
+
+  krnl->stop();
+  krnl_thread.join();
+  krnl->unload_all();
+}
+
+void test_restart_kernel ()
+{
+  kernel_ptr_t krnl(new fhg::core::kernel_t);
+
+  {
+    boost::thread krnl_thread
+      (boost::bind (&fhg::core::kernel_t::run, krnl));
+
+    // load some plugins
+    krnl->load_plugin (hello_plugin.string());
+    krnl->load_plugin (world_plugin.string());
+    krnl->load_plugin (helloworld_plugin.string());
+
+    boost::this_thread::sleep (boost::posix_time::seconds(2));
+
+    krnl->stop();
+    krnl_thread.join();
+    krnl->unload_all();
+  }
+
+  {
+    boost::thread krnl_thread
+      (boost::bind (&fhg::core::kernel_t::run, krnl));
+
+    // load some plugins
+    krnl->load_plugin (hello_plugin.string());
+    krnl->load_plugin (world_plugin.string());
+    krnl->load_plugin (helloworld_plugin.string());
+
+    boost::this_thread::sleep (boost::posix_time::seconds(2));
+
+    krnl->stop();
+    krnl_thread.join();
+    krnl->unload_all();
+  }
+}
+
+test_suite*
+init_unit_test_suite (int ac, char *av[])
+{
+  if (ac <= 1)
+  {
+    throw boost::unit_test::framework::setup_error
+      ("I need the path to the directory where {hello,world,helloworld}.so plugins are");
+  }
+
+  fs::path prefix = fs::path(av[1]);
+  hello_plugin = prefix / "hello.so";
+  world_plugin = prefix / "world.so";
+  helloworld_plugin = prefix / "helloworld.so";
+
+  {
+    test_suite* ts1 = BOOST_TEST_SUITE( "plugin_kernel" );
+    ts1->add( BOOST_TEST_CASE(&test_start_stop_kernel));
+    ts1->add( BOOST_TEST_CASE(&test_restart_kernel));
+
+    framework::master_test_suite().add( ts1 );
+  }
+
+  return 0;
+}
