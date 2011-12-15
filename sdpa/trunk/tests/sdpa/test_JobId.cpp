04b5b810a4148c1c03982ae6f4d62b90bcb322f7
rotaru <rotaru@itwm.fraunhofer.de>
2011-12-15 14:04:48 +0100

    indentation fixes

--- sdpa/trunk/tests/sdpa/test_JobId.cpp
+++ sdpa/trunk/tests/sdpa/test_JobId.cpp
@@ -15,9 +15,9 @@ using namespace sdpa;
 
 struct MyFixture
 {
-	MyFixture() :SDPA_INIT_LOGGER("sdpa.tests.testJobId"){}
-	~MyFixture(){}
-	 SDPA_DECLARE_LOGGER();
+   MyFixture() :SDPA_INIT_LOGGER("sdpa.tests.testJobId"){}
+   ~MyFixture(){}
+   SDPA_DECLARE_LOGGER();
 };
 
 BOOST_FIXTURE_TEST_SUITE( test_JobId, MyFixture )
