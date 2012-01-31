e00d0895d01f63bad52f7a5216e5b6dfba76ab22
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2012-01-31 14:45:20 +0100

    fhgkernel intercepts sigsegv and sigabrt
      - a backtrace is printed directly to stderr and also logged as an
        ERROR to fhglog

--- util/bin/fhgkernel.cpp
+++ util/bin/fhgkernel.cpp
@@ -1,3 +1,11 @@
+#include <execinfo.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ucontext.h>
+#include <unistd.h>
+
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <signal.h>
@@ -14,37 +22,159 @@
 #include <fhg/plugin/plugin.hpp>
 #include <fhg/plugin/core/kernel.hpp>
 
+// the following code was borrowed and slightly adapted from:
+//
+//   http://stackoverflow.com/questions/77005/how-to-generate-a-stacktrace-when-my-gcc-c-app-crashes
+//
+// by: jschmier, http://stackoverflow.com/users/203667/jschmier
+
+/* This structure mirrors the one found in /usr/include/asm/ucontext.h */
+typedef struct _sig_ucontext {
+ unsigned long     uc_flags;
+ struct ucontext   *uc_link;
+ stack_t           uc_stack;
+ struct sigcontext uc_mcontext;
+ sigset_t          uc_sigmask;
+} sig_ucontext_t;
+
+void crit_err_hdlr(int sig_num, siginfo_t * info, void * ucontext)
+{
+ void *             array[50];
+ void *             caller_address;
+ char **            messages;
+ int                size, i;
+ sig_ucontext_t *   uc;
+
+ uc = (sig_ucontext_t *)ucontext;
+
+#if __WORDSIZE == 32
+ /* Get the address at the time the signal was raised from the EIP (x86) */
+ caller_address = (void *) uc->uc_mcontext.eip;
+#else /* __WORDSIZE == 64 */
+ caller_address = (void *) uc->uc_mcontext.rip;
+#endif /* __WORDSIZE == 64 */
+
+ fprintf(stderr, "signal %d (%s), address is %p from %p\n",
+  sig_num, strsignal(sig_num), info->si_addr,
+  (void *)caller_address);
+
+ size = backtrace(array, 50);
+
+ /* overwrite sigaction with caller's address */
+ array[1] = caller_address;
+
+ messages = backtrace_symbols(array, size);
+
+ /* skip first stack frame (points here) */
+ for (i = 1; i < size && messages != NULL; ++i)
+ {
+  fprintf(stderr, "[bt]: (%d) %s\n", i, messages[i]);
+ }
+
+ std::ostringstream log_message;
+ log_message << "received signal "
+             << sig_num << " (" << strsignal(sig_num) << "),"
+             << " address is " << (void*)info->si_addr
+             << " from " << (void*)(caller_address)
+             << std::endl
+   ;
+ for (i = 1; i < size && messages != NULL; ++i)
+ {
+   log_message << "[bt]: (" << i << ") " << messages[i] << std::endl;
+ }
+
+ LOG(ERROR, log_message.str());
+
+ free(messages);
+
+ _exit(EXIT_FAILURE);
+}
+
+// END OF BORROWED CODE
+
 static fhg::core::kernel_t *kernel = 0;
 
-static void at_exit ()
+static void shutdown_kernel ()
 {
   if (kernel) kernel->stop();
 }
 
-static void sig_term(int)
+static void handle_sig_pipe() {}
+static void handle_sig_child()
 {
-  if (kernel) kernel->stop ();
+  waitpid(-1, 0, WNOHANG);
 }
 
-static void handle_sig_pipe()
+void sigterm_hdlr(int sig_num, siginfo_t * info, void * ucontext)
 {
+  shutdown_kernel();
 }
 
-static void sig_pipe(int)
+void sigpipe_hdlr(int sig_num, siginfo_t * info, void * ucontext)
 {
   if (kernel)
     kernel->schedule("kernel", "sigpipe", &handle_sig_pipe);
 }
 
-static void wait_on_child()
-{
-  waitpid(-1, 0, WNOHANG);
-}
-
-static void sig_chld(int)
+void sigchild_hdlr(int sig_num, siginfo_t * info, void * ucontext)
 {
   if (kernel)
-    kernel->schedule("kernel", "wait_on_child", &wait_on_child);
+    kernel->schedule("kernel", "wait_on_child", &handle_sig_child);
+}
+
+void install_signal_handler()
+{
+  struct sigaction sigact;
+
+  // install critical handlers
+  sigact.sa_sigaction = crit_err_hdlr;
+  sigact.sa_flags = SA_RESTART | SA_SIGINFO;
+
+  if (sigaction(SIGSEGV, &sigact, (struct sigaction *)NULL) != 0)
+  {
+    fprintf(stderr, "error setting signal handler for %d (%s)\n",
+           SIGSEGV, strsignal(SIGSEGV));
+
+    exit(EXIT_FAILURE);
+  }
+
+  if (sigaction(SIGABRT, &sigact, (struct sigaction *)NULL) != 0)
+  {
+    fprintf(stderr, "error setting signal handler for %d (%s)\n",
+           SIGABRT, strsignal(SIGABRT));
+
+    exit(EXIT_FAILURE);
+  }
+
+  // install non-critical handlers
+  sigact.sa_sigaction = sigterm_hdlr;
+  sigact.sa_flags = SA_RESTART | SA_SIGINFO;
+
+  if (sigaction(SIGTERM, &sigact, (struct sigaction *)NULL) != 0)
+  {
+    fprintf(stderr, "error setting signal handler for %d (%s)\n",
+           SIGTERM, strsignal(SIGTERM));
+
+    exit(EXIT_FAILURE);
+  }
+
+  sigact.sa_sigaction = sigchild_hdlr;
+  if (sigaction(SIGCHLD, &sigact, (struct sigaction *)NULL) != 0)
+  {
+    fprintf(stderr, "error setting signal handler for %d (%s)\n",
+           SIGCHLD, strsignal(SIGCHLD));
+
+    exit(EXIT_FAILURE);
+  }
+
+  sigact.sa_sigaction = sigpipe_hdlr;
+  if (sigaction(SIGPIPE, &sigact, (struct sigaction *)NULL) != 0)
+  {
+    fprintf(stderr, "error setting signal handler for %d (%s)\n",
+           SIGPIPE, strsignal(SIGPIPE));
+
+    exit(EXIT_FAILURE);
+  }
 }
 
 int main(int ac, char **av)
@@ -101,10 +231,7 @@ int main(int ac, char **av)
 
   kernel = new fhg::core::kernel_t (state_path);
 
-  signal (SIGINT, sig_term);
-  signal (SIGTERM, sig_term);
-  signal (SIGCHLD, sig_chld);
-  signal (SIGPIPE, sig_pipe);
+  install_signal_handler();
 
   keep_going = vm.count("keep-going") != 0;
 
@@ -141,7 +268,7 @@ int main(int ac, char **av)
     }
   }
 
-  atexit(&at_exit);
+  atexit(&shutdown_kernel);
   int rc = kernel->run();
   MLOG(TRACE, "shutting down... (" << rc << ")");
   MLOG(TRACE, "killing children...");
