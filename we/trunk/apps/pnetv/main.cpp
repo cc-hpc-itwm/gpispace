59488f7e2d5876d516722f5c581be612f752450a
Egor Derevenetc <egor.derevenetc@itwm.fhg.de>
2012-02-03 18:55:38 +0100

    Tests for pnetv added

--- we/trunk/apps/pnetv/main.cpp
+++ we/trunk/apps/pnetv/main.cpp
@@ -2,16 +2,26 @@
 #include <iostream>
 
 #include <jpn/common/Foreach.h>
+#include <jpn/common/Unreachable.h>
 
 #include <jpna/Parsing.h>
 #include <jpna/PetriNet.h>
 #include <jpna/Verification.h>
 
 int main(int argc, char *argv[]) {
-    if (!argc) {
-        std::cout << "Usage: pnetv FILE..." << std::endl;
-        return EXIT_FAILURE;
+    if (argc <= 1) {
+        std::cout << "Usage: pnetv FILE..." << std::endl
+                  << "Load compiled Petri nets from given files and check them for termination." << std::endl
+                  << "Possible exit codes:" << std::endl
+                  << " 0 - termination is guaranteed" << std::endl
+                  << " 1 - wrong arguments" << std::endl
+                  << " 2 - I/O error" << std::endl
+                  << " 3 - loops are possible" << std::endl
+                  << " 4 - loops are guaranteed" << std::endl;
+        return 1;
     }
+
+    int exitCode = 0;
 
     for (int i = 1; i < argc; ++i) {
         boost::ptr_vector<jpna::PetriNet> petriNets;
@@ -24,15 +34,31 @@ int main(int argc, char *argv[]) {
             foreach (const jpna::PetriNet &petriNet, petriNets) {
                 std::cout << petriNet.name() << ": ";
                 std::cout.flush();
-                std::cout << jpna::verify(petriNet) << std::endl;
+                jpna::VerificationResult result = jpna::verify(petriNet);
+                std::cout << result << std::endl;
+
+                switch (result.result()) {
+                    case jpna::VerificationResult::TERMINATES:
+                        break;
+                    case jpna::VerificationResult::MAYBE_LOOPS:
+                        if (exitCode == 0) {
+                            exitCode = 3;
+                        }
+                        break;
+                    case jpna::VerificationResult::LOOPS:
+                        exitCode = 4;
+                        break;
+                    default:
+                        jpn::unreachable();
+                }
             }
         } catch (const std::exception &e) {
             std::cerr << filename << ":" << e.what() << std::endl;
-            return EXIT_FAILURE;
+            return 2;
         }
     }
 
-    return EXIT_SUCCESS;
+    return exitCode;
 }
 
 /* vim:set et sts=4 sw=4: */
