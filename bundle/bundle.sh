5ad54cc20a55a9fea426d884027b67a4b11d37c9
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2012-02-21 22:33:52 -0600

    enhanced bundling script

--- bundle/bundle.sh
+++ bundle/bundle.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+prefix=/usr/local
+exclude=()
+verbose=false
+dry=false
+force=false
+keep_going=false
+
+function debug ()
+{
+    $verbose && echo >&2 $@
+}
+
+function dry_run ()
+{
+    $dry && echo $@ || $@ || $keep_going || exit 1
+}
+
 function locate_file ()
 {
   local file="$1" ; shift
@@ -16,37 +33,87 @@ function locate_file ()
 
 function is_filtered ()
 {
-  return 1
+    for pattern in ${exclude[@]} ; do
+        if echo "$1" | grep -q "$pattern" ; then
+            return 0
+        fi
+    done
+    return 1
 }
 
 function bundle_dependencies ()
 {
-  local file="$1" ; shift
-  local dst="$1"; shift
-  OLDIFS="$IFS"
-  export IFS="
+    local file="$1" ; shift
+    local dst="$1"; shift
+    local lvl="${1:-0}"; shift
+    local indent=$(( lvl * 4 ))
+    OLDIFS="$IFS"
+    export IFS="
 "
-  for dep_and_path in $(ldd "$file" | grep '=>' | grep '/' | awk '{printf("%s:%s\n", $1, $3)}') ; do
-     dep=$(echo "$dep_and_path" | cut -d: -f 1)
-     pth=$(echo "$dep_and_path" | cut -d: -f 2)
-     if is_filtered "$dep" ; then
-	 continue
-     fi
-#     path=$(locate_file "$dep")
-     path="$pth"
-     if [ -n "$path" ] ; then
-        if [ ! -e "$dst/$dep" ] ; then
-          echo >&2 cp "$path" "$dst/$dep"
-          cp "$path" "$dst/$dep"
-          echo "$path"
-          bundle_dependencies "$path" "$dst"
+    for dep_and_path in $(ldd "$file" | grep '=>' | grep '/' | awk '{printf("%s:%s\n", $1, $3)}') ; do
+        dep=$(echo "$dep_and_path" | cut -d: -f 1)
+        pth=$(echo "$dep_and_path" | cut -d: -f 2)
+        debug $(printf "%${indent}s" "") "$file <- $pth"
+        if is_filtered "$dep" ; then
+            debug "dependency '$dep' was filtered by pattern '$pattern'"
+            continue
         fi
-     fi
-  done
-  IFS="$OLDIFS"
+        path="$pth"
+        if [ -n "$path" ] ; then
+            if ! test -e "$dst/$dep" || $force ; then
+                debug $(printf "%$((indent + 2))s" "") cp "$path" "$dst/$dep"
+                dry_run cp "$path" "$dst/$dep"
+                bundle_dependencies "$path" "$dst" $(( lvl + 1 ))
+            fi
+        fi
+    done
+    IFS="$OLDIFS"
 }
+
+shiftcount=0
+while getopts ":hvnkfp:x:" opt ; do
+    case $opt in
+        h)
+            usage
+            exit 0
+            ;;
+        v)
+            verbose=true
+            shiftcount=$(( shiftcount + 1 ))
+            ;;
+        n)
+            dry=true
+            shiftcount=$(( shiftcount + 1 ))
+            ;;
+        k)
+            keep_going=true
+            shiftcount=$(( shiftcount + 1 ))
+            ;;
+        f)
+            force=true
+            shiftcount=$(( shiftcount + 1 ))
+            ;;
+        p)
+            prefix=$OPTARG
+            shiftcount=$(( shiftcount + 2 ))
+            ;;
+        x)
+            exclude=( ${exclude[@]} $OPTARG )
+            shiftcount=$(( shiftcount + 2 ))
+            ;;
+        \?)
+            echo "invalid option: -$OPTARG" >&2
+            echo "try: $(basename $0) -h" >&2
+            exit 1
+            ;;
+    esac
+done
+
+shift $shiftcount
+
+dry_run mkdir -p "$prefix/lib"
 
 for bin ;  do
    echo >&2 "# bundling dependencies for '$bin'"
-   bundle_dependencies "$bin" "$CMAKE_INSTALL_PREFIX/lib"
+   bundle_dependencies "$bin" "$prefix/lib"
 done
