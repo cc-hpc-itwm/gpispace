130fc9904b6c62b15710ae86ae34fbbabefe23ac
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2012-05-10 16:01:17 +0200

    drts-job: added result code type
    	- the result code identifies what happened to the job and why it might
    	  have failed.

--- sdpa/plugins/job.cpp
+++ sdpa/plugins/job.cpp
@@ -7,10 +7,12 @@ namespace drts
           , Job::Owner const &owner
           )
     : m_id (jobid.value)
-    , m_description (description.value)
+    , m_input_description (description.value)
     , m_owner (owner.value)
     , m_state (Job::PENDING)
     , m_result ()
+    , m_result_code (Job::NO_FAILURE)
+    , m_message ("")
     , m_entered(boost::posix_time::microsec_clock::universal_time())
     , m_started(boost::posix_time::from_time_t(0))
     , m_completed(boost::posix_time::from_time_t(0))
