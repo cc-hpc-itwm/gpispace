e815cb5d9538e4d5d6be291056c57297d288f7d2
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2012-05-10 16:01:17 +0200

    drts-job: added result code type
    	- the result code identifies what happened to the job and why it might
    	  have failed.

--- sdpa/plugins/job.hpp
+++ sdpa/plugins/job.hpp
@@ -27,6 +27,16 @@ namespace drts
       , CANCELED
       };
 
+    enum result_code_t
+      {
+        NO_FAILURE                = 0
+      , MODULE_CALL_SEGFAULTED    = 9
+      , MODULE_CALL_FAILED        = 11
+      , MODULE_NOT_FOUND          = 50
+      , MODULE_LOAD_FAILED        = 51
+      , MODULE_UNDEFINED_FUNCTION = 52
+      };
+
     struct ID
     {
       explicit ID(std::string const &s)
@@ -66,22 +76,57 @@ namespace drts
     state_t cmp_and_swp_state( state_t expected
                              , state_t newstate
                              );
-    inline void set_state (state_t s) { lock_type lck(m_mutex); m_state = s; }
+    inline Job& set_state (state_t s) {
+      lock_type    lck(m_mutex); m_state = s;
+      return *this;
+    }
 
     std::string const & id() const { return m_id; }
-    std::string const & description() const { return m_description; }
+    std::string const & description() const { return m_input_description; }
     std::string const & owner() const { return m_owner; }
 
-    std::string const & result() const { lock_type lck(m_mutex); return m_result; }
-    void set_result(std::string const &r) { lock_type lck(m_mutex); m_result = r; }
+    std::string const & result() const {
+      lock_type lck(m_mutex); return m_result;
+    }
+    Job & set_result(std::string const &r) {
+      lock_type lck(m_mutex); m_result = r;
+      return *this;
+    }
+
+    result_code_t result_code () const {
+      lock_type lck(m_mutex); return m_result_code;
+    }
+
+    Job& set_result_code (const result_code_t rc) {
+      lock_type lck(m_mutex); m_result_code = rc;
+      return *this;
+    }
+
+    std::string const & message() const {
+      lock_type lck(m_mutex); return m_message;
+    }
+
+    Job& set_message (std::string const &s) {
+      lock_type lck(m_mutex); m_message = s;
+      return *this;
+    }
 
     boost::posix_time::ptime const & entered () const { return m_entered; }
     boost::posix_time::ptime const & started () const { return m_started; }
     boost::posix_time::ptime const & completed () const { return m_completed; }
 
-    void entered   (boost::posix_time::ptime const &t) { m_entered = t; }
-    void started   (boost::posix_time::ptime const &t) { m_started = t; }
-    void completed (boost::posix_time::ptime const &t) { m_completed = t; }
+    Job& entered   (boost::posix_time::ptime const &t) {
+      m_entered = t;
+      return *this;
+    }
+    Job& started   (boost::posix_time::ptime const &t) {
+      m_started = t;
+      return *this;
+    }
+    Job& completed (boost::posix_time::ptime const &t) {
+      m_completed = t;
+      return *this;
+    }
   private:
     Job () {}
 
@@ -90,7 +135,7 @@ namespace drts
     void serialize (Archive & ar, const unsigned int version)
     {
       ar & BOOST_SERIALIZATION_NVP(m_id);
-      ar & BOOST_SERIALIZATION_NVP(m_description);
+      ar & BOOST_SERIALIZATION_NVP(m_input_description);
       ar & BOOST_SERIALIZATION_NVP(m_owner);
       ar & BOOST_SERIALIZATION_NVP(m_state);
       ar & BOOST_SERIALIZATION_NVP(m_result);
@@ -101,16 +146,24 @@ namespace drts
         ar & BOOST_SERIALIZATION_NVP(m_started);
         ar & BOOST_SERIALIZATION_NVP(m_completed);
       }
+
+      if (version > 1)
+      {
+        ar & BOOST_SERIALIZATION_NVP(m_result_code);
+        ar & BOOST_SERIALIZATION_NVP(m_message);
+      }
     }
 
     inline void    state (state_t s) { lock_type lck(m_mutex); m_state = s; }
     mutable mutex_type m_mutex;
 
-    std::string m_id;
-    std::string m_description;
-    std::string m_owner;
-    state_t m_state;
-    std::string m_result;
+    std::string   m_id;
+    std::string   m_input_description;
+    std::string   m_owner;
+    state_t       m_state;
+    std::string   m_result;
+    result_code_t m_result_code;
+    std::string   m_message;
 
     // timestamps
     boost::posix_time::ptime m_entered;
@@ -120,6 +173,6 @@ namespace drts
 
 }
 
-BOOST_CLASS_VERSION(drts::Job, 1);
+BOOST_CLASS_VERSION(drts::Job, 2);
 
 #endif
