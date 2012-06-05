2eb8db79b1a18101a9791008fecfd8aadce0c7eb
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2012-06-05 10:47:26 +0200

    separate part of net with transition functions

---
+++ we/we/net_with_transition_function.hpp
@@ -0,0 +1,72 @@
+// mirko.rahn@itwm.fraunhofer.de
+
+#ifndef _NET_WITH_TRANSITION_FUNCTION_HPP
+#define _NET_WITH_TRANSITION_FUNCTION_HPP
+
+#include <we/net.hpp>
+
+namespace petri_net
+{
+  template<typename Place, typename Transition, typename Edge, typename Token>
+  class net_with_transition_function
+    : public net<Place, Transition, Edge, Token>
+  {
+  private:
+    typedef net<Place, Transition, Edge, Token> super;
+
+    typedef Function::Transition::Traits<Token> tf_traits;
+    typedef typename tf_traits::fun_t trans_t;
+    typedef boost::unordered_map<tid_t, trans_t> trans_map_t;
+
+  public:
+    typedef typename tf_traits::output_t output_t;
+
+  private:
+    trans_map_t trans;
+
+  public:
+    net_with_transition_function ( const std::string & _name = "noname"
+                                 , const pid_t & _places = 10
+                                 , const tid_t & _transitions = 10
+                                 )
+      : super (_name, _places, _transitions)
+      , trans ()
+    {}
+
+    void
+    set_transition_function ( const tid_t & tid
+                            , const trans_t & f
+                            )
+    {
+      trans[tid] = f;
+    }
+
+    output_t run_activity (const typename super::activity_t & activity) const
+    {
+      const typename trans_map_t::const_iterator f (trans.find (activity.tid));
+
+      if (f == trans.end())
+        {
+          throw exception::no_such ("transition id in run_activity");
+        }
+
+      return f->second(activity.input, activity.output_descr);
+    }
+
+    tid_t fire (const tid_t & tid)
+    {
+      const typename super::activity_t activity (super::extract_activity (tid));
+      const output_t output (run_activity (activity));
+      inject_activity_result (output);
+      return tid;
+    }
+
+    template<typename Engine>
+    tid_t fire_random (Engine & engine)
+    {
+      return fire (super::enabled_transitions().random (engine));
+    }
+  };
+} // namespace petri_net
+
+#endif // _NET_WITH_TRANSITION_FUNCTION_HPP
