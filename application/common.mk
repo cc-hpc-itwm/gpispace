df994709e7c2b87cfd4f95d2e20204d34e81af52
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2012-07-11 14:51:29 +0200

    more detailed common.mk/showconfig

--- application/common.mk
+++ application/common.mk
@@ -76,6 +76,14 @@ ifndef XML
   endif
 endif
 
+ifndef DEP_XML
+  ifndef XML
+    $(error variable XMLL undefined but needed to derive variable DEP_XML)
+  else
+    DEP_XML = $(XML).d
+  endif
+endif
+
 ifndef NET
   ifndef MAIN
     $(error variable MAIN undefined but needed to derive variable NET)
@@ -143,37 +151,45 @@ endif
 ###############################################################################
 
 DEP += $(CURDIR)/Makefile
-PATH_LIB = $(GEN)/pnetc/op
+PATH_LIB += $(GEN)/pnetc/op
 WE_EXEC_LIBPATHS += $(PATH_LIB)
 CXXINCLUDEPATHS += $(SDPA_INCLUDE)
-
-DEP_XML = $(XML).d
 
 ###############################################################################
 
 PNET2DOT += $(addprefix --not-starts-with ,$(NOT_STARTS_WITH))
 PNET2DOT += $(addprefix --not-ends-with ,$(NOT_ENDS_WITH))
-
-PNETC_NOINLINE += $(PNETC)
-PNETC_NOINLINE += --no-inline true
-PNETC_NOINLINE += --synthesize-virtual-places true
+PNET2DOT += $(PNET2DOT_OPTS)
 
 PNETC += $(addprefix -I,$(SDPA_XML_LIB))
 PNETC += --gen-cxxflags=-O3
 PNETC += $(addprefix --gen-cxxflags=-I,$(CXXINCLUDEPATHS))
 PNETC += $(addprefix --gen-ldflags=-L,$(CXXLIBRARYPATHS))
 PNETC += --force-overwrite-file=true
+PNETC += $(PNETC_OPTS)
+
+PNETC_NOINLINE += $(PNETC)
+PNETC_NOINLINE += --no-inline true
+PNETC_NOINLINE += --synthesize-virtual-places true
+
+PNETPUT += $(addprefix -p ,$(PUT_PORT))
+PNETPUT += $(PNETPUT_OPTS)
+
+PNETV += $(PNETV_OPTS)
 
 # does not work for paths that contain spaces
 pathify = $(subst $() ,:,$(1))
 
-WE_EXEC = LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$(call pathify,$(CXXLIBRARYPATHS))
+ifneq ($(CXXLIBRARYPATHS),)
+  WE_EXEC = LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$(call pathify,$(CXXLIBRARYPATHS))
+endif
 WE_EXEC += $(WE_EXEC_ENV)
 WE_EXEC += $(WE_EXEC_CMD)
 WE_EXEC += --w $(WE_EXEC_WORKER)
 WE_EXEC += $(addprefix --load ,$(WE_EXEC_LOAD))
 WE_EXEC += $(addprefix -L,$(WE_EXEC_LIBPATHS))
 WE_EXEC += -o /dev/null
+WE_EXEC += $(WE_EXEC_OPTS)
 
 ###############################################################################
 
@@ -191,7 +207,7 @@ verify: $(NET_VERIFICATION)
 ###############################################################################
 
 $(DEP_XML): $(XML)
-	$(PNETC) -i $(XML) -o /dev/null $(PNETC_OPTS) -MM -MT '$(DEP_XML)' > $@
+	$(PNETC) -i $(XML) -o /dev/null -MM -MT '$(DEP_XML)' > $@
 
 ifneq ($(wildcard $(DEP_XML)),)
   include $(DEP_XML)
@@ -200,13 +216,13 @@ endif
 ###############################################################################
 
 $(NET): $(DEP_XML) $(XML) $(DEP)
-	$(PNETC) -i $(XML) $(PNETC_OPTS) -o $@
+	$(PNETC) -i $(XML) -o $@
 
 $(NET_NOINLINE): $(DEP_XML) $(XML) $(DEP)
-	$(PNETC_NOINLINE) -i $(XML) $(PNETC_OPTS) -o $@
+	$(PNETC_NOINLINE) -i $(XML) -o $@
 
 $(PUT): $(NET)
-	$(PNETPUT) --if $(NET) --of $(PUT) $(addprefix -p ,$(PUT_PORT))
+	$(PNETPUT) --if $(NET) --of $(PUT)
 
 ###############################################################################
 
@@ -216,7 +232,7 @@ $(NET_VERIFICATION): $(NET)
 ###############################################################################
 
 $(GEN): $(DEP_XML) $(XML) $(DEP)
-	$(PNETC) -i $(XML) -o /dev/null $(PNETC_OPTS) -g $@
+	$(PNETC) -i $(XML) -o /dev/null -g $@
 	$(TOUCH) $@
 
 lib: $(GEN)
@@ -277,38 +293,70 @@ help:
 .PHONY: showconfig
 
 showconfig:
+	@echo "*** Needed parameters:"
+	@echo
 	@echo "MAIN = $(MAIN)"
 	@echo
 	@echo "SDPA_HOME = $(SDPA_HOME)"
 	@echo
-	@echo "TEE = $(TEE)"
-	@echo "DOT = $(DOT)"
-	@echo "RM = $(RM)"
+	@echo "*** External programs:"
+	@echo
+	@echo "TEE   = $(TEE)"
+	@echo "DOT   = $(DOT)"
+	@echo "RM    = $(RM)"
 	@echo "TOUCH = $(TOUCH)"
 	@echo
+	@echo "*** GPI-Space paths:"
+	@echo
 	@echo "SDPA_INCLUDE = $(SDPA_INCLUDE)"
-	@echo "SDPA_BIN = $(SDPA_BIN)"
+	@echo "SDPA_BIN     = $(SDPA_BIN)"
 	@echo "SDPA_XML_LIB = $(SDPA_XML_LIB)"
 	@echo
-	@echo "XML = $(XML)"
-	@echo "NET = $(NET)"
-	@echo "NET_NOINLINE = $(NET_NOINLINE)"
-	@echo "NET_VERIFICATION = $(NET_VERIFICATION)"
-	@echo "PUT = $(PUT)"
-	@echo "GEN = $(GEN)"
-	@echo "OUT = $(OUT)"
-	@echo "PS = $(PS)"
-	@echo "PS_NOINLINE = $(PS_NOINLINE)"
+	@echo "*** Files:"
 	@echo
-	@echo "DEP = $(DEP)"
-	@echo "PATH_LIB = $(PATH_LIB)"
+	@echo "XML              = $(XML)"
+	@echo "DEP_XML          = $(DEP_XML)"
+	@echo "NET              = $(NET)"
+	@echo "NET_NOINLINE     = $(NET_NOINLINE)"
+	@echo "NET_VERIFICATION = $(NET_VERIFICATION)"
+	@echo "PUT              = $(PUT)"
+	@echo "GEN              = $(GEN)"
+	@echo "OUT              = $(OUT)"
+	@echo "PS               = $(PS)"
+	@echo "PS_NOINLINE      = $(PS_NOINLINE)"
+	@echo
+	@echo "*** Dependencies and options:"
+	@echo
+	@echo "DEP              = $(DEP)"
+	@echo "PATH_LIB         = $(PATH_LIB)"
 	@echo "WE_EXEC_LIBPATHS = $(WE_EXEC_LIBPATHS)"
 	@echo
-	@echo "CXXINCLUDEPATHS = $(CXXINCLUDEPATHS)"
-	@echo "CXXLIBRARYPATHS = $(CXXLIBRARYPATHS)"
+	@echo "CXXINCLUDEPATHS  = $(CXXINCLUDEPATHS)"
+	@echo "CXXLIBRARYPATHS  = $(CXXLIBRARYPATHS)"
 	@echo
-	@echo "PNETC = $(PNETC)"
+	@echo "NOT_STARTS_WITH  = $(NOT_STARTS_WITH)"
+	@echo "NOT_ENDS_WITH    = $(NOT_ENDS_WITH)"
+	@echo "PNET2DOT_OPTS    = $(PNET2DOT_OPTS)"
+	@echo
+	@echo "PNETC_OPTS       = $(PNETC_OPTS)"
+	@echo
+	@echo "PUT_PORT         = $(PUT_PORT)"
+	@echo "PNETPUT_OPTS     = $(PNETPUT_OPTS)"
+	@echo
+	@echo "PNETV_OPTS       = $(PNETV_OPTS)"
+	@echo
+	@echo "WE_EXEC_ENV      = $(WE_EXEC_ENV)"
+	@echo "WE_EXEC_CMD      = $(WE_EXEC_CMD)"
+	@echo "WE_EXEC_WORKER   = $(WE_EXEC_WORKER)"
+	@echo "WE_EXEC_LOAD     = $(WE_EXEC_LOAD)"
+	@echo "WE_EXEC_LIBPATHS = $(WE_EXEC_LIBPATHS)"
+	@echo "WE_EXEC_OPTS     = $(WE_EXEC_OPTS)"
+	@echo
+	@echo "*** Derived commands:"
+	@echo
+	@echo "PNETC          = $(PNETC)"
 	@echo "PNETC_NOINLINE = $(PNETC_NOINLINE)"
-	@echo "PNET2DOT = $(PNET2DOT)"
-	@echo "PNETPUT = $(PNETPUT)"
-	@echo "WE_EXEC = $(WE_EXEC)"
+	@echo "PNET2DOT       = $(PNET2DOT)"
+	@echo "PNETPUT        = $(PNETPUT)"
+	@echo "PNETV          = $(PNETV)"
+	@echo "WE_EXEC        = $(WE_EXEC)"
