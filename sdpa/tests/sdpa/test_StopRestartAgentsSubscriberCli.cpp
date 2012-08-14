6b4a1b8e2073d0ea9345706708a6ce78da1b59ad
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2012-08-14 11:12:47 +0200

    updated tests, reduced the number of test-cases

--- sdpa/tests/sdpa/test_StopRestartAgentsSubscriberCli.cpp
+++ sdpa/tests/sdpa/test_StopRestartAgentsSubscriberCli.cpp
@@ -85,16 +85,8 @@ struct MyFixture
 
 		m_pool = new fhg::com::io_service_pool(1);
 		m_kvsd = new fhg::com::kvs::server::kvsd ("");
-		m_serv = new fhg::com::tcp_server ( *m_pool
-										  , *m_kvsd
-										  , kvs_host ()
-										  , kvs_port ()
-										  , true
-										  );
-		m_thrd = new boost::thread (boost::bind ( &fhg::com::io_service_pool::run
-												, m_pool
-												)
-								   );
+		m_serv = new fhg::com::tcp_server ( *m_pool, *m_kvsd, kvs_host (), kvs_port (), true);
+		m_thrd = new boost::thread( boost::bind ( &fhg::com::io_service_pool::run, m_pool ));
 
 		m_serv->start();
 
@@ -302,7 +294,7 @@ void MyFixture::run_client_subscriber()
 			ptrCli.reset();
 			return;
 
-			boost::this_thread::sleep(boost::posix_time::seconds(3));
+			boost::this_thread::sleep(boost::posix_time::seconds(1));
 		}
 	}
 
@@ -372,7 +364,7 @@ BOOST_AUTO_TEST_CASE( testStop_2Agents_NoDrts_push_1)
 	ptrAgent0.reset();
 	LOG( INFO, "Shutdown agent \"agent_o\". The recovery string is "<<strBackupAgent0);
 
-	boost::this_thread::sleep(boost::posix_time::seconds(3));
+	boost::this_thread::sleep(boost::posix_time::seconds(1));
 
 	// now try to recover the system
 	sdpa::daemon::Agent::ptr_t ptrRecAgent0 = sdpa::daemon::AgentFactory<EmptyWorkflowEngine>::create("agent_0", addrAgent0, arrAgent0MasterInfo, MAX_CAP );
@@ -437,66 +429,6 @@ BOOST_AUTO_TEST_CASE( testStop_AgentNoWE_push)
 	LOG( DEBUG, "The test case testStop_AgentNoWE_push terminated!");
 }
 
-
-BOOST_AUTO_TEST_CASE( testStop_2AgentsAndDrts_Req)
-{
-	LOG( DEBUG, "testStop_2AgentsAndDrts_Req");
-	//guiUrl
-	string guiUrl   	= "";
-	string workerUrl 	= "127.0.0.1:5500";
-	string addrOrch 	= "127.0.0.1";
-	string addrAgent0 	= "127.0.0.1";
-	string addrAgent1	= "127.0.0.1";
-
-	std::string strBackupAgent0;
-	std::string strBackupAgent1;
-
-	typedef void OrchWorkflowEngine;
-
-	m_strWorkflow = read_workflow("workflows/stresstest.pnet");
-	LOG( DEBUG, "The test workflow is "<<m_strWorkflow);
-
-	sdpa::daemon::Orchestrator::ptr_t ptrOrch = sdpa::daemon::OrchestratorFactory<void>::create("orchestrator_0", addrOrch, MAX_CAP);
-	ptrOrch->start_agent(true, strBackupOrch);
-
-	sdpa::master_info_list_t arrAgent0MasterInfo(1, MasterInfo("orchestrator_0"));
-	sdpa::daemon::Agent::ptr_t ptrAgent0 = sdpa::daemon::AgentFactory<EmptyWorkflowEngine>::create("agent_0", addrAgent0, arrAgent0MasterInfo, MAX_CAP );
-	ptrAgent0->start_agent(true, strBackupAgent0);
-
-	sdpa::master_info_list_t arrAgent1MasterInfo(1, MasterInfo("agent_0"));
-	sdpa::daemon::Agent::ptr_t ptrAgent1 = sdpa::daemon::AgentFactory<EmptyWorkflowEngine>::create("agent_1", addrAgent1, arrAgent1MasterInfo, MAX_CAP );
-	ptrAgent1->start_agent(true, strBackupAgent1);
-
-	sdpa::shared_ptr<fhg::core::kernel_t> drts( create_drts("drts", "agent_1") );
-	boost::thread drts_thread = boost::thread(&fhg::core::kernel_t::run, drts);
-
-	boost::thread threadClient = boost::thread(boost::bind(&MyFixture::run_client_subscriber, this));
-
-	ptrAgent0->shutdown(strBackupAgent0);
-	ptrAgent0.reset();
-	LOG( INFO, "Shutdown agent \"agent_o\". The recovery string is "<<strBackupAgent0);
-
-	boost::this_thread::sleep(boost::posix_time::seconds(3));
-
-	// now try to recover the system
-	sdpa::daemon::Agent::ptr_t ptrRecAgent0 = sdpa::daemon::AgentFactory<EmptyWorkflowEngine>::create("agent_0", addrAgent0, arrAgent0MasterInfo, MAX_CAP );
-
-	LOG( INFO, "Re-start \"agent_0\". The recovery string is "<<strBackupAgent0);
-	ptrRecAgent0->start_agent(true, strBackupAgent0);
-
-	threadClient.join();
-	LOG( INFO, "The client thread joined the main thread!");
-
-	drts->stop();
-	drts_thread.join();
-
-	ptrAgent1->shutdown();
-	ptrRecAgent0->shutdown();
-	ptrOrch->shutdown();
-
-	LOG( DEBUG, "The test case testStop_2AgentsAndDrts_Req terminated!");
-}
-
 BOOST_AUTO_TEST_CASE( testStop_2AgentsAndDrts_Push_RealWE)
 {
 	LOG( DEBUG, "testStop_2AgentsAndDrts_Push_RealWE");
@@ -535,7 +467,7 @@ BOOST_AUTO_TEST_CASE( testStop_2AgentsAndDrts_Push_RealWE)
 	ptrAgent0.reset();
 	LOG( INFO, "Shutdown agent \"agent_o\". The recovery string is "<<strBackupAgent0);
 
-	boost::this_thread::sleep(boost::posix_time::seconds(3));
+	//boost::this_thread::sleep(boost::posix_time::seconds(1));
 
 	// now try to recover the system
 	sdpa::daemon::Agent::ptr_t ptrRecAgent0 = sdpa::daemon::AgentFactory<RealWorkflowEngine>::create("agent_0", addrAgent0, arrAgent0MasterInfo, MAX_CAP );
@@ -556,7 +488,6 @@ BOOST_AUTO_TEST_CASE( testStop_2AgentsAndDrts_Push_RealWE)
 	LOG( DEBUG, "The test case testStop_2AgentsAndDrts_Push_RealWE terminated!");
 }
 
-/*
 BOOST_AUTO_TEST_CASE( testStop_AgentRealWE_push)
 {
 	LOG( DEBUG, "testStop_AgentRealWE_push");
@@ -607,6 +538,5 @@ BOOST_AUTO_TEST_CASE( testStop_AgentRealWE_push)
 
 	LOG( DEBUG, "The test case testStop_AgentRealWE_push terminated!");
 }
-*/
 
 BOOST_AUTO_TEST_SUITE_END()
