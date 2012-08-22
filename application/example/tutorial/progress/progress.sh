dadda3e6238f8dd4bdbf89db338f243452cf33da
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2012-08-22 15:01:33 +0200

    tutorial: progress example workflow finished

--- application/example/tutorial/progress/progress.sh
+++ application/example/tutorial/progress/progress.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
 
-bar_name="$1"
-if [ -z "$bar_name" ] ; then
-	echo >&2 "usage: $(basename $0) progress-bar-name"
-	exit 1
+app_name="$1"
+if [ -z "$app_name" ] ; then
+    echo >&2 "usage: $(basename $0) progress-app-name"
+    exit 1
 fi
 
 fhgkvsc=$(which fhgkvsc)
 if [ $? -ne 0 ] ; then
-  echo >&2 "fhgkvsc not found in PATH, please load sdpa"
+    echo >&2 "fhgkvsc not found in PATH, please load sdpa"
 fi
 
-maximum=$(${fhgkvsc} -f -g progress.${bar_name}.maximum -v 100 2>/dev/null)
+phase=$(${fhgkvsc} -f -g "progress.${app_name}.phase" -v "n/a" 2>/dev/null)
+
+maximum=$(${fhgkvsc} -f -g "progress.${app_name}.maximum" -v 100 2>/dev/null)
 if [ $? -ne 0 ] ; then
-	echo >&2 "could not get progress maximum"
-	exit 3
+    echo >&2 "could not get progress maximum"
+    exit 3
 fi
 
 while true ; do
-	current=$(${fhgkvsc} -f -g progress.${bar_name}.current)
-	if [ $? -ne 0  ] ; then
-		echo >&2 "no such progress available: $bar_name"
-		exit 2
-	fi
-	while [ $current -ne $maximum ] ; do
-	  printf "%s: " $bar_name
-	  ./show-progress.sh $current $maximum '\r'
-	  sleep 0.5
-	  current=$(${fhgkvsc} -f -g progress.${bar_name}.current)
-	done
+    current=$(${fhgkvsc} -f -g "progress.${app_name}.current" -v 0)
+    echo -n "$app_name ($phase): "
+    ./show-progress.sh $current $maximum '\r'
+    if [ $current -eq $maximum ] ; then
+        break
+    fi
+    sleep 0.5
 done
-
-exit 0
+echo
