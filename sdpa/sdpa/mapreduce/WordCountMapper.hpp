456808ec9697f483553e51beb5641dd1c9264926
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2012-09-18 16:42:58 +0200

    updated the mapreduce classes

--- sdpa/sdpa/mapreduce/WordCountMapper.hpp
+++ sdpa/sdpa/mapreduce/WordCountMapper.hpp
@@ -54,7 +54,53 @@ inline void WordCountMapTask::run()
   ifs.close();
 }
 
-
 typedef MapReduce<WordCountMapTask> WordCountMapper;
+
+template <>
+inline std::string WordCountMapper::hash(const std::string& key, const std::vector<std::string>& workerIdList )
+{
+  size_t nWorkers = workerIdList.size();
+  int i = 0;
+
+  if((key[0]>=65 && key[0]<=90) )
+  {
+    i = (key[0] - 'a')%nWorkers;
+  }
+  else
+  if( key[0]>=97 && key[0]<=122 )
+  {
+    i = (key[0] - 'A')%nWorkers;
+  }
+
+  // it is guaranteed that at least one worker is selcted -> workerIdList[0]
+  return workerIdList[i];
+}
+
+ /* create a map structure, mapping the workerId to the tasks
+  * thare are to be assigned to it
+ */
+template <>
+inline void WordCountMapper::partitionate(const WordCountMapper::TaskT& mapTask, std::vector<std::string>& workerIdList )
+{
+  // initialize the array of map tasks
+  BOOST_FOREACH(const std::string& workerId, workerIdList)
+  {
+    TaskT task(workerId);
+    addTask(workerId, task);
+  }
+
+  // now, just distribute the list of pairs [<word, count>] from mapTask
+  // across the ẃorkers (create the corresponding buckets (tasks to be assigned)
+  BOOST_FOREACH(const WordCountMapper::TaskT::OutKeyValPairT& pairKeyVal, mapTask.outKeyValueMap() )
+  {
+    size_t nWorkers = workerIdList.size();
+
+    // map <word, count> -> bucket
+    InKeyT key = pairKeyVal.first;
+    std::string workerId = hash(key, workerIdList);
+
+    mapOfTasks_[workerId].emit(key, pairKeyVal.second);
+  }
+}
 
 #endif //WORD_COUNT_MAPPER_HPP
