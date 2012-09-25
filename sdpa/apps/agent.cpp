dff99021f3a5cc3f369f477e9e4a45cb1ab2d013
rotaru <rotaru@itwm.fraunhofer.de>
2012-09-25 16:24:39 +0200

    re-indentation within agent and orchestrator app files

--- sdpa/apps/agent.cpp
+++ sdpa/apps/agent.cpp
@@ -122,21 +122,20 @@ int main (int argc, char **argv)
             // check if the folder exists
             if( !bfs::is_directory(st) )             // true - is directory
             {
-                LOG(ERROR, "The path "<<backup_folder<<" does not represent a folder!" );
-                bDoBackup = false;
+              LOG(ERROR, "The path "<<backup_folder<<" does not represent a folder!" );
+              bDoBackup = false;
             }
             else
             {
-                LOG(INFO, "Backup the agent into the file "<<backup_folder<<"/"<<backup_file );
-                bDoBackup = true;
+              LOG(INFO, "Backup the agent into the file "<<backup_folder<<"/"<<backup_file );
+              bDoBackup = true;
             }
             break;
 
         case FILE_DEF:
-                LOG( INFO
-                   , "Backup folder not specified! No backup file will be created!");
-                bDoBackup = false;
-            break;
+              LOG( INFO, "Backup folder not specified! No backup file will be created!");
+              bDoBackup = false;
+              break;
 
         case FLDANDFILE_DEF:
             LOG(INFO, "The backup folder is set to "<<backup_folder );
@@ -144,13 +143,13 @@ int main (int argc, char **argv)
             // check if the folder exists
             if( !bfs::is_directory(st) )             // true - is directory
             {
-                LOG(FATAL, "The path "<<backup_folder<<" does not represent a folder!" );
-                bDoBackup = false;
+              LOG(FATAL, "The path "<<backup_folder<<" does not represent a folder!" );
+              bDoBackup = false;
             }
             else
             {
-                LOG(INFO, "Backup the agent into the file "<<backup_folder<<"/"<<backup_file );
-                bDoBackup = true;
+              LOG(INFO, "Backup the agent into the file "<<backup_folder<<"/"<<backup_file );
+              bDoBackup = true;
             }
             break;
 
@@ -244,60 +243,60 @@ int main (int argc, char **argv)
 
         try
         {
-                sdpa::daemon::Agent::ptr_t ptrAgent = sdpa::daemon::AgentFactory<RealWorkflowEngine>::create(   agentName,
-                                                                                                                                                                                                                agentUrl,
-                                                                                                                                                                                                                listMasterInfo,
-                                                                                                                                                                                                                MAX_CAP,
-                                                                                                                                                                                                                false,
-                                                                                                                                                                                                                agentRank,
-                                                                                                                                                                                                                appGuiUrl ); //, orchUrl );
+          sdpa::daemon::Agent::ptr_t ptrAgent = sdpa::daemon::AgentFactory<RealWorkflowEngine>::create(   agentName,
+                                                                                                          agentUrl,
+                                                                                                          listMasterInfo,
+                                                                                                          MAX_CAP,
+                                                                                                          false,
+                                                                                                          agentRank,
+                                                                                                          appGuiUrl ); //, orchUrl );
 
-                bool bUseRequestModel(fhg::util::read_bool(requestMode));
+          bool bUseRequestModel(fhg::util::read_bool(requestMode));
 
-                if(bDoBackup)
-                  ptrAgent->start_agent(bUseRequestModel, bkp_path/backup_file);
-                else
-                  ptrAgent->start_agent(bUseRequestModel);
+          if(bDoBackup)
+            ptrAgent->start_agent(bUseRequestModel, bkp_path/backup_file);
+          else
+            ptrAgent->start_agent(bUseRequestModel);
 
-                LOG(DEBUG, "waiting for signals...");
-                sigset_t waitset;
-                int sig(0);
-                int result(0);
+          LOG(DEBUG, "waiting for signals...");
+          sigset_t waitset;
+          int sig(0);
+          int result(0);
 
-                sigfillset(&waitset);
-                sigprocmask(SIG_BLOCK, &waitset, NULL);
+          sigfillset(&waitset);
+          sigprocmask(SIG_BLOCK, &waitset, NULL);
 
-                bool signal_ignored = true;
-                while (signal_ignored)
-                {
-                  result = sigwait(&waitset, &sig);
-                  if (result == 0)
-                  {
-                        LOG(DEBUG, "got signal: " << sig);
-                        switch (sig)
-                        {
-                        case SIGTERM:
-                        case SIGINT:
-                          signal_ignored = false;
-                          break;
-                        default:
-                          LOG(INFO, "ignoring signal: " << sig);
-                          break;
-                        }
-                  }
-                  else
-                  {
-                          LOG(ERROR, "error while waiting for signal: " << result);
-                  }
-                }
+          bool signal_ignored = true;
+          while (signal_ignored)
+          {
+            result = sigwait(&waitset, &sig);
+            if (result == 0)
+            {
+              LOG(DEBUG, "got signal: " << sig);
+              switch (sig)
+              {
+              case SIGTERM:
+              case SIGINT:
+                signal_ignored = false;
+                break;
+              default:
+                LOG(INFO, "ignoring signal: " << sig);
+                break;
+              }
+            }
+            else
+            {
+              LOG(ERROR, "error while waiting for signal: " << result);
+            }
+          }
 
-                LOG(INFO, "terminating...");
+          LOG(INFO, "terminating...");
 
-                ptrAgent->shutdown();
+          ptrAgent->shutdown();
         }
         catch ( std::exception& )
         {
-            std::cout<<"Could not start the Agent!"<<std::endl;
+          std::cout<<"Could not start the Agent!"<<std::endl;
         }
 
 }
