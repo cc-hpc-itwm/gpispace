b2bb49a841ae8a8e6a30b29ee845226ba89a2858
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2012-09-28 19:09:42 +0200

    gpi-space: added preliminary sfs_area files for the shared-file-system area

---
+++ gpi-space/gpi-space/pc/memory/sfs_area.cpp
@@ -0,0 +1,149 @@
+#include "sfs_area.hpp"
+
+#include <fhglog/minimal.hpp>
+#include <boost/make_shared.hpp>
+
+#include <gpi-space/pc/global/topology.hpp>
+
+namespace gpi
+{
+  namespace pc
+  {
+    namespace memory
+    {
+      sfs_area_t::sfs_area_t ( const gpi::pc::type::process_id_t creator
+                             , const std::string & path
+                             , const gpi::pc::type::size_t size
+                             , const gpi::pc::type::flags_t flags
+                             , const std::string & meta_data
+                             )
+        : area_t ( sfs_area_t::area_type
+                 , creator
+                 , path
+                 , size
+                 , flags
+                 )
+        , m_ptr (0)
+        , m_path (path)
+        , m_meta (meta_data)
+        , m_size (size)
+        , m_min_local_offset (0)
+        , m_max_local_offset (size)
+      {
+        CLOG( TRACE
+            , "gpi.memory"
+            , "SFS memory created:"
+            <<" size: " << size
+            <<" range:"
+            <<" ["
+            << m_min_local_offset << "," << m_max_local_offset
+            << "]"
+            << " path: " << m_path
+            );
+      }
+
+      sfs_area_t::~sfs_area_t ()
+      { }
+
+      area_t::grow_direction_t
+      sfs_area_t::grow_direction (const gpi::pc::type::flags_t flgs) const
+      {
+        if (gpi::flag::is_set (flgs, gpi::pc::type::handle::F_GLOBAL))
+        {
+          return GROW_UP;
+        }
+        else
+        {
+          return GROW_DOWN;
+        }
+      }
+
+      void *
+      sfs_area_t::ptr ()
+      {
+        return m_ptr;
+      }
+
+      bool
+      sfs_area_t::is_allowed_to_attach (const gpi::pc::type::process_id_t) const
+      {
+        return false;
+      }
+
+      void
+      sfs_area_t::check_bounds ( const gpi::pc::type::handle::descriptor_t &hdl
+                               , const gpi::pc::type::offset_t start
+                               , const gpi::pc::type::size_t   amount
+                               ) const
+      {
+        if (! (start < hdl.size && (start + amount) <= hdl.size))
+        {
+          CLOG( ERROR
+              , "gpi.memory"
+              , "out-of-bounds access:"
+              << " hdl=" << hdl
+              << " size=" << hdl.size
+              << " range=["<<start << ", " << start + amount << "]"
+              );
+          throw std::invalid_argument
+            ("out-of-bounds: access to handle outside boundaries");
+        }
+      }
+
+      void
+      sfs_area_t::alloc_hook (const gpi::pc::type::handle::descriptor_t &hdl)
+      {
+        if (  gpi::flag::is_set (hdl.flags, gpi::pc::type::handle::F_GLOBAL)
+           && hdl.creator != (gpi::pc::type::process_id_t)(-1)
+           )
+        {
+          gpi::pc::global::topology().alloc ( descriptor ().id
+                                            , hdl.id
+                                            , hdl.offset
+                                            , hdl.size
+                                            , hdl.local_size
+                                            , hdl.name
+                                            );
+        }
+      }
+
+      void
+      sfs_area_t::free_hook (const gpi::pc::type::handle::descriptor_t &hdl)
+      {
+        if (gpi::flag::is_set (hdl.flags, gpi::pc::type::handle::F_GLOBAL))
+        {
+          gpi::pc::global::topology().free(hdl.id);
+        }
+      }
+
+      bool
+      sfs_area_t::is_range_local( const gpi::pc::type::handle::descriptor_t &hdl
+                                , const gpi::pc::type::offset_t begin
+                                , const gpi::pc::type::offset_t end
+                                ) const
+      {
+        return true;
+      }
+
+      gpi::pc::type::size_t
+      sfs_area_t::get_local_size ( const gpi::pc::type::size_t size
+                                 , const gpi::pc::type::flags_t flgs
+                                 ) const
+      {
+        return size;
+      }
+
+      int
+      sfs_area_t::get_specific_transfer_tasks ( const gpi::pc::type::memory_location_t src
+                                              , const gpi::pc::type::memory_location_t dst
+                                              , area_t & dst_area
+                                              , gpi::pc::type::size_t amount
+                                              , gpi::pc::type::size_t queue
+                                              , task_list_t & tasks
+                                              )
+      {
+        throw std::runtime_error ("not yet implemented");
+      }
+    }
+  }
+}
---
+++ gpi-space/gpi-space/pc/memory/sfs_area.hpp
@@ -0,0 +1,78 @@
+#ifndef GPI_SPACE_PC_MEMORY_SFS_AREA_HPP
+#define GPI_SPACE_PC_MEMORY_SFS_AREA_HPP
+
+#include <gpi-space/pc/type/segment_type.hpp>
+#include <gpi-space/pc/memory/memory_area.hpp>
+
+namespace gpi
+{
+  namespace pc
+  {
+    namespace memory
+    {
+      class sfs_area_t : public area_t
+      {
+      public:
+        static const type::segment::segment_type area_type = gpi::pc::type::segment::SEG_SFS;
+
+        sfs_area_t ( const gpi::pc::type::process_id_t creator
+                   , const std::string & path
+                   , const gpi::pc::type::size_t size
+                   , const gpi::pc::type::flags_t flags
+                   , const std::string & meta_data
+                   );
+
+        ~sfs_area_t ();
+
+      protected:
+        int get_type_id () const;
+
+        void check_bounds ( const gpi::pc::type::handle::descriptor_t &
+                          , const gpi::pc::type::offset_t start
+                          , const gpi::pc::type::size_t   amount
+                          ) const;
+
+        bool is_allowed_to_attach (const gpi::pc::type::process_id_t) const;
+        grow_direction_t grow_direction (const gpi::pc::type::flags_t) const;
+
+        void alloc_hook (const gpi::pc::type::handle::descriptor_t &);
+        void  free_hook (const gpi::pc::type::handle::descriptor_t &);
+
+        int get_specific_transfer_tasks ( const gpi::pc::type::memory_location_t src
+                                        , const gpi::pc::type::memory_location_t dst
+                                        , area_t & dst_area
+                                        , gpi::pc::type::size_t amount
+                                        , gpi::pc::type::size_t queue
+                                        , task_list_t & tasks
+                                        );
+      private:
+        bool is_range_local ( const gpi::pc::type::handle::descriptor_t &
+                            , const gpi::pc::type::offset_t begin
+                            , const gpi::pc::type::size_t   range_size
+                            ) const;
+
+        gpi::pc::type::offset_t
+        handle_to_global_offset ( const gpi::pc::type::offset_t
+                                , const gpi::pc::type::size_t per_node_size
+                                ) const;
+
+        void *ptr ();
+
+        gpi::pc::type::size_t get_local_size ( const gpi::pc::type::size_t size
+                                             , const gpi::pc::type::flags_t flags
+                                             ) const;
+
+
+        void * m_ptr;
+        std::string m_path;
+        std::string m_meta;
+
+        gpi::pc::type::size_t   m_size;
+        gpi::pc::type::offset_t m_min_local_offset;
+        gpi::pc::type::offset_t m_max_local_offset;
+      };
+    }
+  }
+}
+
+#endif // GPI_SPACE_PC_MEMORY_AREA_HPP
