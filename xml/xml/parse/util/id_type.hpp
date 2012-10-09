034093debf05e09e9c80fc9831f6b75ac8763dc2
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2012-10-09 16:08:32 +0200

    xml: addition: Add util::id_type for use as unique id in types.

---
+++ xml/xml/parse/util/id_type.hpp
@@ -0,0 +1,22 @@
+// bernd.loerwald@itwm.fraunhofer.de
+
+#ifndef XML_UTIL_ID_TYPE_HPP
+#define XML_UTIL_ID_TYPE_HPP
+
+#include <boost/cstdint.hpp>
+
+namespace fhg
+{
+  namespace xml
+  {
+    namespace parse
+    {
+      namespace util
+      {
+        typedef boost::uint_fast64_t id_type;
+      }
+    }
+  }
+}
+
+#endif
