5bc3b8483a28764243cc5d0a6270e7f7976d57c5
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2012-10-10 12:11:40 +0200

    gpi-space: added constructor to error_t message

--- gpi-space/gpi-space/pc/proto/error.hpp
+++ gpi-space/gpi-space/pc/proto/error.hpp
@@ -28,6 +28,16 @@ namespace gpi
           errc code;
           std::string detail;
 
+          error_t ()
+            : code (success)
+            , detail ("sucess")
+          {}
+
+          explicit
+          error_t (errc ec, std::string const &det="")
+            : code (ec)
+            , detail (det)
+          {}
         private:
           friend class boost::serialization::access;
           template<typename Archive>
