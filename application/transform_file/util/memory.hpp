27db13cb43c2eb5c189b0a11b170c6220b205df2
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2012-10-10 13:33:50 +0200

    added new application, transform_file: convert all words to upper-case

---
+++ application/transform_file/util/memory.hpp
@@ -0,0 +1,50 @@
+#ifndef _H_TRANSFORM_FILE_UTIL_ALLOC
+#define _H_TRANSFORM_FILE_UTIL_ALLOC 1
+
+#include <fvm-pc/pc.hpp>
+
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+namespace transform_file
+{
+  namespace util
+  {
+    namespace memory
+    {
+      namespace global
+      {
+        inline long alloc (const std::size_t size, const std::string & descr)
+        {
+          const fvmAllocHandle_t handle (fvmGlobalAlloc (size, descr.c_str()));
+
+          if (handle == 0)
+            {
+              std::ostringstream oss;
+
+              oss << "alloc failed: " << descr << " (" << size << " bytes)";
+
+              throw std::runtime_error (oss.str());
+            }
+
+          return static_cast<long> (handle);
+        }
+
+        inline void free (const long h)
+        {
+          const fvmAllocHandle_t handle (static_cast<fvmAllocHandle_t> (h));
+
+          if (handle != 0)
+          {
+            fvmGlobalFree (handle);
+          }
+
+          return;
+        }
+      } // namespace global
+    } // namespace memory
+  } // namespace util
+} // namespace transform_file
+
+#endif
