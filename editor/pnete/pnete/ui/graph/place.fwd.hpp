655e93ad477144e357a0f2f5dfed03029835eb61
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2012-10-12 18:54:52 +0200

    xml, util, editor: addition: Add forward declaration headers for some files.

---
+++ editor/pnete/pnete/ui/graph/place.fwd.hpp
@@ -0,0 +1,20 @@
+// bernd.loerwald@itwm.fraunhofer.de
+
+#ifndef _FHG_PNETE_UI_GRAPH_PLACE_FWD_HPP
+#define _FHG_PNETE_UI_GRAPH_PLACE_FWD_HPP 1
+
+namespace fhg
+{
+  namespace pnete
+  {
+    namespace ui
+    {
+      namespace graph
+      {
+        class place_item;
+      }
+    }
+  }
+}
+
+#endif
