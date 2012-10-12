0673f99686d444f4e3cfd289285ca0c5a26f6d05
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2012-10-12 19:00:52 +0200

    util: addition: Add an exception (base) class, that adds a backtrace   to what().
      Inherits std::runtime_error, adds \n\n${backtrace} to given reason.
    
      Backtraces are pretty printed. This might not always work right,
      given there are different styles used by backtrace() on different
      OS. Pretty printing includes demangling C++ names for enhanced
      readbility.

---
+++ util/fhg/util/backtracing_exception.fwd.hpp
@@ -0,0 +1,14 @@
+// bernd.loerwald@itwm.fraunhofer.de
+
+#ifndef _FHG_UTIL_BACKTRACING_EXCEPTION_FWD_HPP
+#define _FHG_UTIL_BACKTRACING_EXCEPTION_FWD_HPP
+
+namespace fhg
+{
+  namespace util
+  {
+    class backtracing_exception;
+  }
+}
+
+#endif
