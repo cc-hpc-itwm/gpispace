0d6f933d5e6d1d81a9f7d38ae96fbbe150e4bb75
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2012-10-15 09:39:23 +0200

    editor: addition: Add handles to net, place and transition.
      A handle has  an id for the element it is  referencing. Nets not yet
      have ids, therefore  a net handle holds a  reference. When access to
      the referenced element is requested,  it will be retrieved and never
      cashed to prevent dangling references. This might be slow, but is at
      least safe.

---
+++ editor/pnete/pnete/data/handle/place.cpp
@@ -0,0 +1,52 @@
+// bernd.loerwald@itwm.fraunhofer.de
+
+#include <pnete/data/handle/place.hpp>
+
+#include <pnete/data/handle/net.hpp>
+
+//! \todo Only include place.hpp and net.hpp?
+#include <xml/parse/types.hpp>
+
+#include <fhg/util/backtracing_exception.hpp>
+
+#include <boost/optional.hpp>
+
+namespace fhg
+{
+  namespace pnete
+  {
+    namespace data
+    {
+      namespace handle
+      {
+        place::place (const place_type& place, const handle::net& net)
+          : _place_id (place.id())
+          , _net (net)
+        { }
+
+        place::place_type place::operator()() const
+        {
+          const boost::optional<place_type> place
+            (net()().place_by_id (_place_id));
+          if (!place)
+          {
+            throw fhg::util::backtracing_exception
+              ("INVALID HANDLE: place id not found");
+          }
+          return *place;
+        }
+
+        const handle::net& place::net() const
+        {
+          return _net;
+        }
+
+        bool place::operator== (const place& other) const
+        {
+          return _place_id == other._place_id && _net == other._net;
+        }
+      }
+    }
+  }
+}
+
---
+++ editor/pnete/pnete/data/handle/transition.hpp
@@ -0,0 +1,45 @@
+// bernd.loerwald@itwm.fraunhofer.de
+
+#ifndef _FHG_PNETE_DATA_HANDLE_TRANSITION_HPP
+#define _FHG_PNETE_DATA_HANDLE_TRANSITION_HPP 1
+
+#include <pnete/data/handle/net.hpp>
+
+#include <xml/parse/type/transition.fwd.hpp>
+
+#include <xml/parse/util/id_type.hpp>
+
+namespace fhg
+{
+  namespace pnete
+  {
+    namespace data
+    {
+      namespace handle
+      {
+        class transition
+        {
+        private:
+          typedef ::xml::parse::type::transition_type transition_type;
+
+        public:
+          transition ( const transition_type& transition
+                     , const handle::net& net
+                     );
+
+          transition_type operator()() const;
+
+          const handle::net& net() const;
+
+          bool operator== (const transition& other) const;
+
+        private:
+          ::fhg::xml::parse::util::id_type _transition_id;
+          handle::net _net;
+        };
+      }
+    }
+  }
+}
+
+#endif
