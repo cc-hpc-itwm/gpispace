6880ba96a0544a18cfefdff108050ef89e11992c
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2012-10-16 16:46:21 +0200

    xml: addition: add typed ids (for connect and transition as of now.)

--- xml/xml/parse/util/id_type.hpp
+++ xml/xml/parse/util/id_type.hpp
@@ -18,5 +18,24 @@ namespace fhg
     }
   }
 }
+//! \todo The remainder of xml/ is in ::xml::parse, not
+//! ::fhg::xml::parse. One should move them all to the same namespace.
+namespace xml
+{
+  namespace parse
+  {
+    namespace id
+    {
+      //! \todo Let them be of some class to prevent casting.
+#define MAKE_ID(name)                           \
+      typedef ::fhg::xml::parse::util::id_type name
+
+      MAKE_ID (connect);
+      MAKE_ID (transition);
+
+#undef MAKE_ID
+    }
+  }
+}
 
 #endif
