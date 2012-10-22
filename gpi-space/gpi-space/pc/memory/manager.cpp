c87623842f8d19a7cbc08c3519f09c2aa59e22d7
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2012-10-22 20:03:13 +0200

    gpi-space: enhanced error message in exceptions
      - added handle or memory id to the error messages

--- gpi-space/gpi-space/pc/memory/manager.cpp
+++ gpi-space/gpi-space/pc/memory/manager.cpp
@@ -128,7 +128,9 @@ namespace gpi
           area_map_t::iterator area_it (m_areas.find(mem_id));
           if (area_it == m_areas.end())
           {
-            throw std::runtime_error ("no such memory");
+            throw std::runtime_error ( "no such memory: "
+                                     + boost::lexical_cast<std::string>(mem_id)
+                                     );
           }
 
           area_ptr area (area_it->second);
@@ -177,7 +179,9 @@ namespace gpi
         area_map_t::iterator area (m_areas.find(mem_id));
         if (area == m_areas.end())
         {
-          throw std::runtime_error ("no such memory area");
+          throw std::runtime_error ( "no such memory: "
+                                   + boost::lexical_cast<std::string>(mem_id)
+                                   );
         }
 
         if (proc_id)
@@ -196,7 +200,9 @@ namespace gpi
         area_map_t::iterator area (m_areas.find(mem_id));
         if (area == m_areas.end())
         {
-          throw std::runtime_error ("no such memory area");
+          throw std::runtime_error ( "no such memory: "
+                                   + boost::lexical_cast<std::string>(mem_id)
+                                   );
         }
 
         if (proc_id)
@@ -254,13 +260,13 @@ namespace gpi
         }
 
         m_areas [area->get_id ()] = area;
+        area->init ();
 
         LOG(TRACE, "memory registered:" << area->descriptor ());
 
         memory_added (area->get_id ());
       }
 
-        area->init ();
       manager_t::area_ptr
       manager_t::get_area (const gpi::pc::type::segment_id_t mem_id)
       {
@@ -274,7 +280,9 @@ namespace gpi
         area_map_t::const_iterator area_it (m_areas.find (mem_id));
         if (area_it == m_areas.end())
         {
-          throw std::runtime_error ("no such memory");
+          throw std::runtime_error ( "no such memory: "
+                                   + boost::lexical_cast<std::string>(mem_id)
+                                   );
         }
         return area_it->second;
       }
@@ -297,7 +305,9 @@ namespace gpi
         }
         else
         {
-          throw std::runtime_error ("no such handle");
+          throw std::runtime_error ( "no such handle: "
+                                   + boost::lexical_cast<std::string>(hdl)
+                                   );
         }
       }
 
@@ -566,7 +576,9 @@ namespace gpi
           area_map_t::iterator area_it (m_areas.find (seg_id));
           if (area_it == m_areas.end ())
           {
-            throw std::runtime_error ("no such memory");
+            throw std::runtime_error ( "no such memory: "
+                                     + boost::lexical_cast<std::string>(seg_id)
+                                     );
           }
 
           area_ptr area (area_it->second);
