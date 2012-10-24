3b762113a27946813e6a66780d068ee83f96b8ff
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2012-10-24 16:17:46 +0200

    util: added documentation to 'setproctitle'

--- util/fhg/util/setproctitle.h
+++ util/fhg/util/setproctitle.h
@@ -5,7 +5,18 @@
 extern "C" {
 #endif
 
-int setproctitle (const char *title, int argc, char *argv[]);
+  /** Lets you change the program name displayed by top and ps and such.
+
+      beware: the implementation  uses the 'exec' syscall, so  make sure to call
+      it at the beginning of your program.
+
+      @param  title the  new program  title,  if it  is already  the same,  this
+      function is a nop.
+
+      @param argc the original argc
+      @param argv the original argv
+   */
+  int setproctitle (const char *title, int argc, char *argv[]);
 
 #ifdef __cplusplus
 }
