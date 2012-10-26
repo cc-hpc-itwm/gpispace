ece80d0936f9af1d7784d566caf58bdf1057f098
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2012-10-26 12:21:45 +0200

    implementation: pnetc: move quoting and wrappiung functions into anonymous namespace

--- xml/xml/parse/pnetc.cpp
+++ xml/xml/parse/pnetc.cpp
@@ -18,149 +18,156 @@
 
 namespace po = boost::program_options;
 
-namespace xml
-{
-  namespace parse
+namespace {
+  template<typename Stream>
+  class wrapping_word_stream
   {
-    //! \todo move stuff to anonymous namespace
+  private:
+    const std::size_t _max_len;
+    mutable std::size_t _len;
+    Stream& _stream;
+  public:
+    wrapping_word_stream (Stream& stream, const std::size_t max = 75)
+      : _max_len (max)
+      , _len (0)
+      , _stream (stream)
+    {}
 
-    // ********************************************************************* //
-
-    namespace dependencies
+    void put (const std::string& w) const
     {
-      template<typename Stream>
-      class wrapping_word_stream
-      {
-      private:
-        const std::size_t _max_len;
-        mutable std::size_t _len;
-        Stream& _stream;
-      public:
-        wrapping_word_stream (Stream& stream, const std::size_t max = 75)
-          : _max_len (max)
-          , _len (0)
-          , _stream (stream)
-        {}
-
-        void put (const std::string& w) const
+      if (_len + w.size() > _max_len)
         {
-          if (_len + w.size() > _max_len)
-            {
-              _stream << " \\"; newl(); append (" ");
-            }
-          else if (_len > 0)
-            {
-              append (" ");
-            }
-
-          append (w);
+          _stream << " \\"; newl(); append (" ");
+        }
+      else if (_len > 0)
+        {
+          append (" ");
         }
 
-        void append (const std::string& s) const
+      append (w);
+    }
+
+    void append (const std::string& s) const
+    {
+      _stream << s;
+
+      _len += s.size();
+    }
+
+    void newl () const
+    {
+      _stream << std::endl;
+
+      _len = 0;
+    }
+  };
+
+  class quote
+  {
+  private:
+    std::string _quoted;
+
+  public:
+    quote (const std::string& s) : _quoted ()
+    {
+      std::string::const_iterator pos (s.begin());
+      const std::string::const_iterator end (s.end());
+
+      while (pos != end)
         {
-          _stream << s;
-
-          _len += s.size();
-        }
-
-        void newl () const
-        {
-          _stream << std::endl;
-
-          _len = 0;
-        }
-      };
-
-      class quote
-      {
-      private:
-        std::string _quoted;
-
-      public:
-        quote (const std::string& s) : _quoted ()
-        {
-          std::string::const_iterator pos (s.begin());
-          const std::string::const_iterator end (s.end());
-
-          while (pos != end)
+          switch (*pos)
             {
-              switch (*pos)
-                {
-                case ' ': _quoted += "\\ "; break;
-                case '$': _quoted += "$$"; break;
-                default: _quoted += *pos; break;
-                }
-
-              ++pos;
+            case ' ': _quoted += "\\ "; break;
+            case '$': _quoted += "$$"; break;
+            default: _quoted += *pos; break;
             }
-        };
 
-        operator const std::string& () const { return _quoted; }
-      };
+          ++pos;
+        }
+    };
+
+    operator const std::string& () const { return _quoted; }
+  };
 
 
-      inline std::string quote_for_list (const std::string& s)
+  std::string quote_for_list (const std::string& s)
+  {
+    std::string quoted;
+    std::string::const_iterator pos (s.begin());
+    const std::string::const_iterator end (s.end());
+
+    while (pos != end)
       {
-        std::string quoted;
-        std::string::const_iterator pos (s.begin());
-        const std::string::const_iterator end (s.end());
-
-        while (pos != end)
+        switch (*pos)
           {
-            switch (*pos)
-              {
-              case ' ': quoted += "\\ "; break;
-              default: quoted += *pos; break;
-              }
-
-            ++pos;
+          case ' ': quoted += "\\ "; break;
+          default: quoted += *pos; break;
           }
 
-        return quoted;
+        ++pos;
       }
 
-      inline std::string quote_for_list (const boost::filesystem::path& p)
+    return quoted;
+  }
+
+  std::string quote_for_list (const boost::filesystem::path& p)
+  {
+    return quote_for_list (p.string());
+  }
+
+  template<typename Stream>
+  void write_dependencies ( const xml::parse::state::type& state
+                          , const std::string& input
+                          , Stream& stream
+                          )
+  {
+    wrapping_word_stream<Stream> wrapping_stream (stream);
+
+    if (state.dependencies_target().size() > 0)
       {
-        return quote_for_list (p.string());
+        BOOST_FOREACH ( const std::string& target
+                      , state.dependencies_target()
+                      )
+          {
+            wrapping_stream.put (target);
+          }
       }
 
-      template<typename Stream>
-      void mk ( const state::type& state
-              , const std::string& input
-              , Stream& stream
-              )
+    if (state.dependencies_target_quoted().size() > 0)
       {
-        wrapping_word_stream<Stream> wrapping_stream (stream);
-
-        if (state.dependencies_target().size() > 0)
+        BOOST_FOREACH ( const std::string& target
+                      , state.dependencies_target_quoted()
+                      )
           {
-            BOOST_FOREACH ( const std::string& target
-                          , state.dependencies_target()
-                          )
-              {
-                wrapping_stream.put (target);
-              }
+            wrapping_stream.put (quote (target));
           }
+      }
 
-        if (state.dependencies_target_quoted().size() > 0)
+    if (  (state.dependencies_target().size() == 0)
+       && (state.dependencies_target_quoted().size() == 0)
+       )
+      {
+        wrapping_stream.put (input);
+      }
+
+    wrapping_stream.append (":");
+
+    BOOST_FOREACH ( const boost::filesystem::path& path
+                  , state.dependencies()
+                  )
+      {
+        const std::string& dep (path.string());
+
+        if (dep != input)
           {
-            BOOST_FOREACH ( const std::string& target
-                          , state.dependencies_target_quoted()
-                          )
-              {
-                wrapping_stream.put (quote (target));
-              }
+            wrapping_stream.put (dep);
           }
+      }
 
-        if (  (state.dependencies_target().size() == 0)
-           && (state.dependencies_target_quoted().size() == 0)
-           )
-          {
-            wrapping_stream.put (input);
-          }
+    wrapping_stream.newl();
 
-        wrapping_stream.append (":");
-
+    if (state.dependencies_add_phony_targets())
+      {
         BOOST_FOREACH ( const boost::filesystem::path& path
                       , state.dependencies()
                       )
@@ -169,31 +176,23 @@ namespace xml
 
             if (dep != input)
               {
-                wrapping_stream.put (dep);
-              }
-          }
-
-        wrapping_stream.newl();
-
-        if (state.dependencies_add_phony_targets())
-          {
-            BOOST_FOREACH ( const boost::filesystem::path& path
-                          , state.dependencies()
-                          )
-              {
-                const std::string& dep (path.string());
-
-                if (dep != input)
-                  {
-                    wrapping_stream.newl();
-                    wrapping_stream.append (dep);
-                    wrapping_stream.append(":");
-                    wrapping_stream.newl();
-                  }
+                wrapping_stream.newl();
+                wrapping_stream.append (dep);
+                wrapping_stream.append(":");
+                wrapping_stream.newl();
               }
           }
       }
-    }
+  }
+} // anonymous namespace
+
+namespace xml
+{
+  namespace parse
+  {
+    //! \todo move stuff to anonymous namespace
+
+    // ********************************************************************* //
 
     type::function_type
     frontend (state::type & state, const std::string & input)
@@ -255,7 +254,7 @@ namespace xml
               throw error::could_not_open_file (file);
             }
 
-          dependencies::mk (state, input, stream);
+          write_dependencies (state, input, stream);
         }
 
       if (state.list_dependencies().size() > 0)
@@ -268,11 +267,11 @@ namespace xml
               throw error::could_not_open_file (file);
             }
 
-          stream << dependencies::quote_for_list (input) << std::endl;
+          stream << quote_for_list (input) << std::endl;
 
           BOOST_FOREACH (const boost::filesystem::path& p, state.dependencies())
             {
-              stream << dependencies::quote_for_list(p) << std::endl;
+              stream << quote_for_list(p) << std::endl;
             }
         }
 
