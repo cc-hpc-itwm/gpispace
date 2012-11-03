9f073d4db26dcf0c7ae0a6df75375b2291adc05a
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2012-11-03 13:03:37 +0100

    implement: added remove_prefix

---
+++ util/fhg/util/remove_prefix.hpp
@@ -0,0 +1,15 @@
+// mirko.rahn@itwm.fraunhofer.de
+
+#pragma once
+
+#include <string>
+
+namespace fhg
+{
+  namespace util
+  {
+    std::string remove_prefix ( const std::string& prefix
+                              , const std::string& word
+                              );
+  }
+}
