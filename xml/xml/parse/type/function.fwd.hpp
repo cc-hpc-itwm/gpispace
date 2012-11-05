820e0cdd7a6efc3fa7a0b6ca0858c4c5a163b6b9
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2012-11-05 16:46:06 +0100

    refactor: delete superfluous typedefs

--- xml/xml/parse/type/function.fwd.hpp
+++ xml/xml/parse/type/function.fwd.hpp
@@ -9,12 +9,6 @@ namespace xml
   {
     namespace type
     {
-      // typedef xml::util::unique<port_type,id::port>::elements_type ports_type;
-      // typedef std::list<std::string> conditions_type;
-      // typedef xml::util::unique<function_type,id::function>::elements_type functions_type;
-      // typedef xml::util::unique<template_type,id::tmpl>::elements_type templates_type;
-      // typedef xml::util::unique<specialize_type,id::specialize>::elements_type specializes_type;
-
       struct function_type;
     }
   }
