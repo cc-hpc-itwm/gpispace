5ff709b8dd9d02a3fd5bf17fdb66a3e3ddc3191e
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2012-11-14 16:05:05 +0100

    editor: introduce handle base class, which holds _id, provides get(),   get_ref() and operator==() as well as id() to reduce code duplication.

---
+++ editor/pnete/pnete/data/handle/meta_base.fwd.hpp
@@ -0,0 +1,21 @@
+// bernd.loerwald@itwm.fraunhofer.de
+
+#ifndef FHG_PNETE_DATA_HANDLE_META_BASE_FWD_HPP
+#define FHG_PNETE_DATA_HANDLE_META_BASE_FWD_HPP
+
+namespace fhg
+{
+  namespace pnete
+  {
+    namespace data
+    {
+      namespace handle
+      {
+        template<typename ID_TYPE, typename XML_TYPE>
+          class meta_base;
+      }
+    }
+  }
+}
+
+#endif
