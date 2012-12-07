febcea870cbd6eee09fa61946dfba8a06a021c86
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2012-12-07 11:49:27 +0100

    refactor: simplify putget

--- we/we/loader/putget.hpp
+++ we/we/loader/putget.hpp
@@ -15,95 +15,41 @@ namespace we
 {
   namespace loader
   {
-    // ********************************************************************** //
-    // PUT
-
-    // on port, subtoken by path
-    inline void put ( output_t & o
-                    , const std::string & key
-                    , const value::path_type & path
-                    , const value::type & val
-                    )
+    template<typename Path>
+    void put ( output_t& o
+             , const std::string& key
+             , const Path& path
+             , const value::type& val
+             )
     {
-      value::container::bind<value::path_type> (o, key, path, val);
+      value::container::bind<Path> (o, key, path, val);
     }
-
-    inline void put ( output_t & o
-                    , const std::string & key
-                    , const std::string & path
-                    , const value::type & val
-                    )
-    {
-      value::container::bind<std::string> (o, key, path, val);
-    }
-
-    // on port, subliteral by string-path
-    template<typename T>
-    inline void put ( output_t & o
-                    , const std::string & key
-                    , const value::path_type & path
-                    , const T & val
-                    )
-    {
-      put (o, key, path, value::type (val));
-    }
-
-    template <typename T>
-    inline void put ( output_t & o
-                    , const std::string & key
-                    , const std::string & path
-                    , const T & val
-                    )
-    {
-      put (o, key, path, value::type (val));
-    }
-
-    // ********************************************************************** //
-    // GET
 
     // get with an additional path into the value
-    template <typename T>
-    inline typename value::visitor::get<T const &>::result_type
-    get ( const input_t & i
-        , const std::string & key
-        , const value::path_type & path_in_value
-        )
-    {
-      return value::get<T>(path_in_value, value::container::value (i, key));
-    }
-
-    template <typename T>
-    inline typename value::visitor::get<T const &>::result_type
-    get ( const input_t & i
-        , const std::string & key
-        , const std::string & path_in_value
-        )
+    template<typename T, typename Path>
+    const T& get ( const input_t& i
+                 , const std::string& key
+                 , const Path& path_in_value
+                 )
     {
       return value::get<T>(path_in_value, value::container::value (i, key));
     }
 
     // get from an earlier extracted value::type
     template <typename T>
-    inline typename value::visitor::get<T const &>::result_type
-    get (const value::type & v)
+    const T& get (const value::type & v)
     {
       return value::get<T>(v);
     }
 
     template <typename T>
-    inline typename value::visitor::get<T const &>::result_type
-    get ( const value::type & v
-        , const value::path_type & path_in_value
-        )
+    const T& get (const value::type& v, const value::path_type& path_in_value)
     {
       return value::get<T>(path_in_value, v);
     }
 
     template <typename T>
-    inline typename value::visitor::get<T const &>::result_type
-    get ( const value::type & v
-        , const std::string & path_in_value
-        )
+    const T& get (const value::type& v, const std::string& path_in_value)
     {
       return value::get<T>(path_in_value, v);
     }
