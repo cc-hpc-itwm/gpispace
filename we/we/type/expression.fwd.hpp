4ef3ebb469ec74f2f76350629ba02e4d26cc76ba
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2012-12-07 13:02:23 +0100

    refactor: add fwds for we/type/{exp,mod,net}

---
+++ we/we/type/expression.fwd.hpp
@@ -0,0 +1,14 @@
+// mirko.rahn@itwm.fhg.de
+
+#ifndef WE_TYPE_EXPRESSION_FWD_HPP
+#define WE_TYPE_EXPRESSION_FWD_HPP
+
+namespace we
+{
+  namespace type
+  {
+    struct expression_t;
+  }
+}
+
+#endif
---
+++ we/we/type/net.fwd.hpp
@@ -0,0 +1,12 @@
+// mirko.rahn@itwm.fhg.de
+
+#ifndef _WE_TYPE_NET_FWD_HPP
+#define _WE_TYPE_NET_FWD_HPP
+
+namespace petri_net
+{
+  class net;
+}
+
+#endif
+
