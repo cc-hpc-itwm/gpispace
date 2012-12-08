a8f61a3f5346d77b0e51a34c3c66428ba502d586
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2012-12-09 00:56:52 +0100

    refactor: add connection.cpp

---
+++ we/we/type/connection.cpp
@@ -0,0 +1,47 @@
+// mirko.rahn@itwm.fraunhofer.de
+
+#include <we/type/connection.hpp>
+
+#include <stdexcept>
+
+namespace petri_net
+{
+  namespace edge
+  {
+    bool is_pt_read (const type& e)
+    {
+      return e == PT_READ;
+    }
+    bool is_PT (const type& e)
+    {
+      return (e == PT || e == PT_READ);
+    }
+
+    std::string enum_to_string (const type& e)
+    {
+      switch (e)
+	{
+	case PT: return "in";
+	case PT_READ: return "read";
+	case TP: return "out";
+	}
+
+      throw std::runtime_error ("enum_to_string for invalid enum value");
+    }
+  }
+
+  connection_t::connection_t () 
+    : type()
+    , tid()
+    , pid()
+  {}
+
+  connection_t::connection_t ( const edge::type& _type
+	 		     , const tid_t& _tid
+		 	     , const pid_t& _pid
+			     )
+    : type (_type)
+    , tid (_tid)
+    , pid (_pid)
+  {}
+}
