cf620f08b66e544e4113fdeff168f256df86ced4
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2012-12-14 14:03:16 +0100

    new: loader searches modules recursively
      - 'sdpa init' only adds '$SDPA_LIBEXEC/apps' and ~/.sdpa/modules to
        the list of module directories

--- we/we/loader/loader.cpp
+++ we/we/loader/loader.cpp
@@ -201,22 +201,53 @@ namespace we {
       return ec;
     }
 
+    static bool search_directory_for_file ( const boost::filesystem::path & path
+                                          , const boost::filesystem::path & file
+                                          , boost::filesystem::path & result
+                                          )
+    {
+      namespace fs = boost::filesystem;
+
+      try
+      {
+        fs::recursive_directory_iterator dir_iter =
+          fs::recursive_directory_iterator (path, fs::symlink_option::recurse);
+        const fs::recursive_directory_iterator end =
+          fs::recursive_directory_iterator ();
+        while (dir_iter != end)
+        {
+          const fs::path entry = *dir_iter;
+
+          if (  fs::is_regular_file (entry)
+             && entry.filename () == file
+             )
+          {
+            result = entry;
+            return true;
+          }
+          ++dir_iter;
+        }
+      }
+      catch (fs::filesystem_error const &ex)
+      {
+        MLOG ( ERROR, "exception during directory scan in '"
+             << path << "': " << ex.what ()
+             );
+        return false;
+      }
+
+      return false;
+    }
+
     bool loader::locate (const std::string & module, boost::filesystem::path & path_found)
     {
       boost::unique_lock<boost::recursive_mutex> lock(mtx_);
       namespace fs = boost::filesystem;
-      const std::string file_name (module_traits<Module>::file_name (module));
+      const fs::path file_name = module_traits<Module>::file_name (module);
       for (search_path_t::const_iterator dir (search_path_.begin()); dir != search_path_.end(); ++dir)
       {
-        if (! fs::exists (*dir))
-          continue;
-
-        fs::path path (*dir / file_name);
-        if (fs::exists (path))
-        {
-          path_found = path;
+        if (search_directory_for_file (*dir, file_name, path_found))
           return true;
-        }
       }
       return false;
     }
