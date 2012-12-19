ead77df8efc572ba99e16583819b52d141ccac54
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2012-12-19 15:29:21 +0100

    xml: add test for compiling an xml file to we and hard compare the result with a reference file
      This adds a test for simple pipe elimination called token (copied
      from we/tests/simple_pipe/).

--- xml/xml/tests/parser_fixture.hpp
+++ xml/xml/tests/parser_fixture.hpp
@@ -6,7 +6,10 @@
 #include <xml/parse/parser.hpp>
 #include <xml/parse/state.hpp>
 
+#include <we/mgmt/type/activity.hpp>
+
 #include <boost/filesystem.hpp>
+#include <boost/optional.hpp>
 #include <boost/test/unit_test.hpp>
 
 namespace
@@ -15,19 +18,63 @@ namespace
   {
     parser_fixture()
       : state()
+      , function (boost::none)
     {
-      BOOST_REQUIRE (boost::unit_test::framework::master_test_suite().argc >= 2);
+      BOOST_REQUIRE (boost::unit_test::framework::master_test_suite().argc >= 3);
       path_to_xpnets = boost::unit_test::framework::master_test_suite().argv[1];
+      path_to_pnets = boost::unit_test::framework::master_test_suite().argv[2];
     }
 
-    ::xml::parse::id::ref::function parse (const boost::filesystem::path& path)
+    void parse (const boost::filesystem::path& path)
     {
-      return ::xml::parse::just_parse (state, (path_to_xpnets / path).string());
+      function = ::xml::parse::just_parse (state, (path_to_xpnets / path).string());
+    }
+
+    void post_processing_passes()
+    {
+      ::xml::parse::post_processing_passes (*function, &state);
+    }
+
+    void dump_xml()
+    {
+      ::xml::parse::dump_xml (*function, state);
+    }
+
+    ::we::mgmt::type::activity_t xml_to_we()
+    {
+      return ::xml::parse::xml_to_we (*function, state);
+    }
+
+    std::string content_of_file (const boost::filesystem::path& path) const
+    {
+      std::ifstream reference_file (path.string().c_str(), std::ios::in);
+
+      BOOST_REQUIRE (reference_file);
+
+      std::string reference;
+      reference_file.seekg (0, std::ios::end);
+      reference.resize (reference_file.tellg());
+      reference_file.seekg (0, std::ios::beg);
+      reference_file.read (&reference[0], reference.size());
+      reference_file.close();
+
+      return reference;
+    }
+
+    void require_same ( const ::we::mgmt::type::activity_t& activity
+                      , const boost::filesystem::path& path
+                      )
+    {
+      BOOST_REQUIRE_EQUAL ( content_of_file (path_to_pnets / path)
+                          , activity.to_string()
+                          );
     }
 
     ::xml::parse::state::type state;
+    boost::optional< ::xml::parse::id::ref::function> function;
 
     boost::filesystem::path path_to_xpnets;
+    boost::filesystem::path path_to_pnets;
   };
 }
 
