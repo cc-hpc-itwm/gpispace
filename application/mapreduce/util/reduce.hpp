716ed5c9e2f2985547c4600a9d35f76c5010c9cb
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2013-01-14 09:34:27 +0100

    mapreduce: re-worked the reduce methods

--- application/mapreduce/util/reduce.hpp
+++ application/mapreduce/util/reduce.hpp
@@ -30,16 +30,17 @@ namespace mapreduce
     		  throw std::runtime_error ("could not reduce: key := " + key + ", empty list of values!");
     	  }
 
-    	  if( list_in_values.size()==1 )
+    	  BOOST_FOREACH(const std::string& str_item, list_in_values)
     	  {
-    		  list_out_values.push_back(list_in_values.front());
-    		  return list_out_values;
-    	  }
+    		  std:string str_val(str_item);
+    		  while( boost::algorithm::starts_with(str_val, "[") && boost::algorithm::ends_with(str_val, "]") )
+			  {
+				  std::string val = str_val.substr(1, str_val.size()-2);
+				  str_val = val;
+			  }
 
-    	  BOOST_FOREACH(const std::string& str_val, list_in_values)
-    	  {
-    		  //MLOG(INFO, "Reduce the item "<<str_val);
-    		  boost::char_separator<char> sep("[] ");
+    		  boost::char_separator<char> sep(" ");
+
     		  boost::tokenizer<boost::char_separator<char> > tok(str_val, sep);
     		  for(boost::tokenizer<boost::char_separator<char> >::iterator it = tok.begin(); it != tok.end(); ++it)
     		  {
@@ -47,7 +48,15 @@ namespace mapreduce
     				  total += boost::lexical_cast<int>(*it);
     			  }
     			  catch (boost::bad_lexical_cast const &) {
-    				  throw std::runtime_error ("could not reduce: key := " + key + " val := " + *it);
+    				  if(boost::algorithm::starts_with(*it, "[") && boost::algorithm::ends_with(*it, "]") )
+    				  {
+    					  std::list<string> list_in_interm_values;
+    					  list_in_interm_values.push_back(*it);
+    					  std::list<std::string> list_out_interm_val = reduce(key, list_in_interm_values);
+    					  total += boost::lexical_cast<int>(*list_out_interm_val.begin());
+    				  }
+    				  else
+    					  throw std::runtime_error ("could not reduce: key := " + key + " val := " + *it);
     			  }
     		  }
     	  }
@@ -59,58 +68,20 @@ namespace mapreduce
     	  return list_out_values;
       }
 
-      std::string get_reduced_item(std::string& key, std::list<std::string>& list_values )
-	  {
-		  std::ostringstream oss;
-		  oss<<key<<PAIRSEP<<"[";
-
-		  for( std::list<std::string>::iterator it=list_values.begin(); it!=list_values.end(); it++ )
-		  {
-			  oss<<*it;
-
-			  if( boost::next(it) != list_values.end() )
-				  oss<<SPCH;
-			  else
-				  oss<<"]";
-		  }
-
-		  return oss.str();
-	   }
-
-      size_t store(std::string& key, std::list<std::string>& list_values, char* reduce_buff, size_t last_pos, const long& n_max_size)
-      {
-    	  std::stringstream sstr;
-
-    	  std::string str_pair = get_reduced_item(key, list_values);
-    	  size_t item_size = str_pair.size();
-
-    	  if(last_pos+item_size>n_max_size)
-    	  {
-    		  throw(std::runtime_error("Not enough place left for performing a reduce operation!"));
-    	  }
-    	  else
-    	  {
-    		  memcpy(reduce_buff+last_pos, str_pair.data(), item_size);
-    		  size_t new_pos = last_pos + item_size;
-    		  reduce_buff[new_pos++]=SPCH;
-
-    		  return new_pos;
-    	  }
-      }
-
       void write(std::string& key, std::list<std::string>& list_values, std::ofstream& ofs )
       {
-    	  std::string item = get_reduced_item( key, list_values );
-		  ofs<<item<<std::endl;
+    	  key_val_pair_t kvp(key, list2str(list_values));
+    	  std::string str_pair = kvpair2str(kvp);
+		  ofs<<str_pair<<std::endl;
       }
 
-      void reduce_array(const int part_id, const size_t red_slot_size, const std::vector<std::string>& arr_items, char* ptr_shmem, size_t& last_pos )
+      void reduce_arr_buff(const int part_id, const size_t red_slot_size, const std::vector<std::string>& arr_items, char* ptr_shmem, size_t& last_pos )
       {
     	  std::list<std::string> list_in_values;
     	  key_val_pair_t kv_pair, kv_pair_next;
 
     	  try {
-    		  kv_pair = get_key_val(arr_items[0]);
+    		  kv_pair = str2kvpair(arr_items[0]);
     	  }
 		  catch(const std::exception& exc ){
 			  throw std::runtime_error("Invalid key-value pair ("+ arr_items[0] + ") :" + +exc.what());
@@ -122,7 +93,7 @@ namespace mapreduce
     	  for(std::vector<std::string>::const_iterator it=arr_items.begin(); it != arr_items.end(); it++ )
     	  {
     		  try {
-    			  kv_pair_next = ::mapreduce::util::get_key_val(*it);
+    			  kv_pair_next = ::mapreduce::util::str2kvpair(*it);
     		  }
     		  catch(const std::exception& exc ){
     			  throw std::runtime_error("Invalid key-value pair ("+ *it + ") :" +exc.what());
@@ -133,7 +104,7 @@ namespace mapreduce
     			  std::list<std::string> list_out_values = ::mapreduce::util::reduce(last_key, list_in_values);
 
     			  try {
-    				  last_pos = ::mapreduce::util::store( last_key, list_out_values, ptr_shmem, last_pos, red_slot_size );
+    				  last_pos = ::mapreduce::util::write_to_buff( last_key, list_out_values, ptr_shmem, last_pos, red_slot_size );
     			  }
     			  catch(const std::exception& exc) {
     				  throw std::runtime_error("Reduce slot "+boost::lexical_cast<std::string>(part_id)+":"+exc.what());
@@ -152,7 +123,7 @@ namespace mapreduce
 			  std::list<std::string> list_out_values = ::mapreduce::util::reduce( last_key, list_in_values );
 
 			  try {
-				last_pos = ::mapreduce::util::store( last_key, list_out_values, ptr_shmem, last_pos, red_slot_size);
+				last_pos = ::mapreduce::util::write_to_buff( last_key, list_out_values, ptr_shmem, last_pos, red_slot_size);
 			  }
 			  catch(const std::exception& exc) {
 				  throw std::runtime_error("Reduce slot "+boost::lexical_cast<std::string>(part_id)+":"+exc.what());
@@ -160,17 +131,94 @@ namespace mapreduce
 		  }
       }
 
-      void reduce_and_write_array(const std::vector<std::string>& arr_items,  const std::string& str_part_in_file )
+      // assume that the both input arrays are sorted reduced !!!!
+      void merge_and_reduce_arr_buff(const int part_id, const size_t red_slot_size, const std::vector<std::string>& arr_items_1, const std::vector<std::string>& arr_items_2, char* ptr_shmem, size_t& last_pos )
+   	  {
+   		  std::list<std::string> list_in_values;
+
+   		  MLOG(INFO, "Merge and reduce ...");
+   		  std::vector<std::string>::const_iterator it_1 = arr_items_1.begin();
+   		  std::vector<std::string>::const_iterator it_2 = arr_items_2.begin();
+
+   		  key_val_pair_t kv_pair_arr_1 = str2kvpair(*it_1);
+   		  key_val_pair_t kv_pair_arr_2 = str2kvpair(*it_2);
+
+   		  // the array and the file are already reduced
+   		  while( it_1 != arr_items_1.end() && it_2 != arr_items_2.end() )
+   		  {
+   			  if( kv_pair_arr_1.first < kv_pair_arr_2.first )
+   			  {
+   				  last_pos = ::mapreduce::util::write_to_buff( *it_1++, ptr_shmem, last_pos, red_slot_size );
+   				  if( it_1 != arr_items_1.end() )
+   					  kv_pair_arr_1 = str2kvpair(*it_1);
+   			  }
+   			  else
+   			  if( kv_pair_arr_1.first > kv_pair_arr_2.first )
+   			  {
+   				 last_pos = ::mapreduce::util::write_to_buff( *it_2++, ptr_shmem, last_pos, red_slot_size );
+   				  if( it_2 != arr_items_2.end() )
+   					  kv_pair_arr_2 = str2kvpair(*it_2);
+   			  }
+   			  else
+   			  if( kv_pair_arr_1.first == kv_pair_arr_2.first )
+   			  {
+   				  std::list<std::string> list_in_values;
+   				  list_in_values.push_back(kv_pair_arr_1.second);
+   				  list_in_values.push_back(kv_pair_arr_2.second);
+
+   				  if( !list_in_values.empty() )
+   				  {
+   					  std::list<std::string> list_out_values = ::mapreduce::util::reduce(kv_pair_arr_1.first, list_in_values);
+
+   					  try {
+   						 last_pos = ::mapreduce::util::write_to_buff(kv_pair_arr_1.first, list_out_values, ptr_shmem, last_pos, red_slot_size );
+   					  }
+   					  catch(const std::exception& exc)
+   					  {
+   						  throw std::runtime_error(exc.what());
+   					  }
+   				  }
+
+   				  if( ++it_1 != arr_items_1.end() )
+   					  if(!it_1->empty())
+   						  kv_pair_arr_1 = str2kvpair(*it_1);
+
+   				  if( ++it_2 != arr_items_2.end() )
+   					  if(!it_2->empty())
+   						  kv_pair_arr_2 = str2kvpair(*it_2);
+
+   				  if( kv_pair_arr_1.second.empty() || kv_pair_arr_2.second.empty() )
+   				  {
+   					  MLOG(FATAL, "HoobaBoobaa, empty values!!!!!!");
+   					  throw std::runtime_error("Empty values!");
+   				  }
+   			  }
+   		  }
+
+   		  if( it_1 == arr_items_1.end() )
+   		  {
+   			  while( it_2 != arr_items_2.end() )
+   				last_pos = ::mapreduce::util::write_to_buff( *it_2++, ptr_shmem, last_pos, red_slot_size );
+   		  }
+
+   		  if( it_2 == arr_items_2.end() )
+   		  {
+   			  while( it_1 != arr_items_1.end() )
+   				  last_pos = ::mapreduce::util::write_to_buff( *it_1++, ptr_shmem, last_pos, red_slot_size );
+   		  }
+   	  }
+
+      void reduce_arr_file(const std::vector<std::string>& arr_items,  const std::string& str_part_in_file )
       {
     	  std::list<std::string> list_in_values;
-    	  key_val_pair_t kv_pair = get_key_val(arr_items[0]);
+    	  key_val_pair_t kv_pair = str2kvpair(arr_items[0]);
     	  std::string last_key = kv_pair.first;
 
     	  std::ofstream ofs( str_part_in_file.data() );
 
     	  for(std::vector<std::string>::const_iterator it=arr_items.begin(); it != arr_items.end(); it++ )
     	  {
-    		  ::mapreduce::util::key_val_pair_t kv_pair_next = ::mapreduce::util::get_key_val(*it);
+    		  ::mapreduce::util::key_val_pair_t kv_pair_next = ::mapreduce::util::str2kvpair(*it);
     		  std::string key = kv_pair_next.first;
 
     		  if( key != last_key )
@@ -212,44 +260,58 @@ namespace mapreduce
       void combine(const std::vector<std::string>& arr_items, std::vector<std::string>& arr_reduced_items )
 	  {
 		  std::list<std::string> list_in_values;
-		  key_val_pair_t kv_pair = get_key_val(arr_items[0]);
+		  key_val_pair_t kv_pair, kv_pair_next;
+
+		  try {
+			  kv_pair = str2kvpair(arr_items[0]);
+		  }
+		  catch(const std::exception& exc ){
+			  throw std::runtime_error("Invalid key-value pair ("+ arr_items[0] + ") :" + +exc.what());
+		  }
+
 		  std::string last_key = kv_pair.first;
 
 		  for(std::vector<std::string>::const_iterator it=arr_items.begin(); it != arr_items.end(); it++ )
 		  {
-			  ::mapreduce::util::key_val_pair_t kv_pair_next = ::mapreduce::util::get_key_val(*it);
-			  std::string key = kv_pair_next.first;
+			  try {
+				  kv_pair_next = ::mapreduce::util::str2kvpair(*it);
+			  }
+			  catch(const std::exception& exc ){
+				  throw std::runtime_error("Invalid key-value pair ("+ *it + ") :" +exc.what());
+			  }
 
-			  if( key != last_key )
+			  if( kv_pair_next.first != last_key && !list_in_values.empty() )
 			  {
 				  std::list<std::string> list_out_values = ::mapreduce::util::reduce(last_key, list_in_values);
 
 				  try {
-					  std::string str_red_item = get_reduced_item(last_key, list_out_values);
-					  arr_reduced_items.push_back(str_red_item);
+					  key_val_pair_t kvp(last_key, list2str(list_out_values));
+					  std::string str_pair = kvpair2str(kvp);
+					  arr_reduced_items.push_back(str_pair);
 				  }
 				  catch(const std::exception& exc) {
-					  throw std::runtime_error(exc.what());
+					  throw std::runtime_error(std::string("Combine exception: ")+exc.what());
 				  }
 
-				  last_key = key;
-				  list_in_values.clear();
-				  list_in_values.push_back(kv_pair_next.second);
-			  }
-			  else
-				  list_in_values.push_back(kv_pair_next.second);
+					last_key = kv_pair_next.first;
+					list_in_values.clear();
+					list_in_values.push_back(kv_pair_next.second);
+				}
+				else
+					list_in_values.push_back(kv_pair_next.second);
 		  }
 
 		  if(!list_in_values.empty())
 		  {
-			  std::list<std::string> list_out_values = ::mapreduce::util::reduce(last_key, list_in_values);
+			  std::list<std::string> list_out_values = ::mapreduce::util::reduce( last_key, list_in_values );
 
 			  try {
-				  std::string str_red_item = get_reduced_item(last_key, list_out_values);
-				  arr_reduced_items.push_back(str_red_item);
+				  key_val_pair_t kvp(last_key, list2str(list_out_values));
+				  std::string str_pair = kvpair2str(kvp);
+				  arr_reduced_items.push_back(str_pair);
 			  }
 			  catch(const std::exception& exc) {
-				  throw std::runtime_error(exc.what());
+				  throw std::runtime_error(std::string("Combine exception: ")+exc.what());
 			  }
 		  }
 	  }
@@ -272,16 +334,14 @@ namespace mapreduce
           return f_size;
       }
 
-      void merge_and_reduce_buff_into_file(const std::vector<std::string>& arr_items, const std::string& str_part_in_file)
+      void merge_and_reduce_arr_file(const std::vector<std::string>& arr_items, const std::string& str_part_in_file)
 	  {
 		  std::list<std::string> list_in_values;
-		  key_val_pair_t kv_pair = get_key_val(arr_items[0]);
-		  std::string last_key = kv_pair.first;
 
 		  if( !file_exists(str_part_in_file) )
 		  {
 			  MLOG(INFO, "Create new file "<<str_part_in_file);
-			  reduce_and_write_array(arr_items, str_part_in_file);
+			  reduce_arr_file(arr_items, str_part_in_file);
 			  return;
 		  }
 		  MLOG(INFO, "The file "<<str_part_in_file<<" already exists on disk!!!");
@@ -290,7 +350,7 @@ namespace mapreduce
 		  if( file_size(str_part_in_file.data()) == 0 )
 		  {
 			  MLOG(INFO, "The reduce file "<<str_part_in_file<<" exists but has the length 0!");
-			  reduce_and_write_array(arr_items, str_part_in_file);
+			  reduce_arr_file(arr_items, str_part_in_file);
 			  return;
 		  }
 
@@ -301,33 +361,32 @@ namespace mapreduce
 
 		  MLOG(INFO, "Merge and reduce ...");
 		  std::vector<std::string>::const_iterator it = arr_items.begin();
-		  key_val_pair_t kv_pair_arr = get_key_val(*it);
+		  key_val_pair_t kv_pair_arr = str2kvpair(*it);
 		  char str_curr_line[256];
 		  ifs.getline(str_curr_line, 256);
 
-		  key_val_pair_t kv_pair_line_file = get_key_val(str_curr_line);
+		  key_val_pair_t kv_pair_line_file = str2kvpair(str_curr_line);
 
 		  // the array and the file are already reduced
 		  while( it != arr_items.end() && !ifs.eof() )
 		  {
 			  if( kv_pair_arr.first<kv_pair_line_file.first )
 			  {
-				  //MLOG(INFO,  "store the element "<<*it);
 				  ofs<<*it++<<std::endl;
 				  if( it != arr_items.end() )
-					  kv_pair_arr = get_key_val(*it);
+					  kv_pair_arr = str2kvpair(*it);
 			  }
 
 			  if( kv_pair_arr.first>kv_pair_line_file.first )
 			  {
-				  // store the element
+				  // write_to_buff the element
 				  ofs<<str_curr_line<<std::endl;
 				  if(!ifs.eof())
 				  {
 					 str_curr_line[0]='\0';
 					 ifs.getline(str_curr_line, 256);
 					 if(strcmp(str_curr_line, "") )
-						 kv_pair_line_file = get_key_val(str_curr_line);
+						 kv_pair_line_file = str2kvpair(str_curr_line);
 				  }
 			  }
 
@@ -353,7 +412,7 @@ namespace mapreduce
 
 				  if( ++it != arr_items.end() )
 				  {
-					  kv_pair_arr = get_key_val(*it);
+					  kv_pair_arr = str2kvpair(*it);
 				  }
 
 				  if(!ifs.eof())
@@ -361,7 +420,7 @@ namespace mapreduce
 					  str_curr_line[0] = '\0';
 					  ifs.getline(str_curr_line, 256);
 					  if(strcmp(str_curr_line, "") )
-						  kv_pair_line_file = get_key_val(str_curr_line);
+						  kv_pair_line_file = str2kvpair(str_curr_line);
 				  }
 			  }
 		  }
