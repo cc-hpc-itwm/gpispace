1cd19a60a40e030178dbe69b90546a06fbe37ea2
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2013-01-21 16:23:32 +0100

    mapreduce: added new folder test

---
+++ application/mapreduce/test/test_merge_and_reduce_buff.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <stdexcept>
+#include <util/reduce.hpp>
+#include <fhglog/fhglog.hpp>
+#include <vector>
+
+using namespace mapreduce::util;
+
+void test1()
+{
+	std::string arr0[] = {"a@1", "a@1", "b@1", "c@1", "c@1", "c@1", "e@1", "h@1", "i@1"};
+	std::string arr1[] = {"a@1", "b@1", "c@1", "e@1", "e@1", "g@1", "h@1", "i@1"};
+
+	std::vector<std::string> arr_items_0;
+	for(int k=0; k<9; k++)
+		arr_items_0.push_back(arr0[k]);
+
+	std::vector<std::string> arr_items_1;
+	for(int i=0; i<8; i++)
+		arr_items_1.push_back(arr1[i]);
+
+	char* ptr_shmem = new char[1000];
+	size_t last_pos = merge_and_reduce_arr_buff_rep( 0, 1000000, arr_items_0, arr_items_1, ptr_shmem );
+	ptr_shmem[last_pos]=0;
+	std::cout<<"The result is: \""<<ptr_shmem<<"\""<<std::endl;
+
+}
+
+void test2()
+{
+	key_val_pair_t kv_pair("John", "Doe");
+	std::string str_pair(kvpair2str(kv_pair));
+
+	std::cout<<"The given pair is: <"<<kv_pair.first<<","<<kv_pair.second<<">"<<std::endl;
+	std::cout<<"The encoded string is: "<<str_pair<<std::endl;
+
+
+	key_val_pair_t kv_pair_new = str2kvpair(str_pair);
+	std::cout<<"The decoded pair is: <"<<kv_pair_new.first<<","<<kv_pair_new.second<<">"<<std::endl;
+
+}
+
+int main ()
+{
+	test1();
+	test2();
+
+	return 0;
+}
