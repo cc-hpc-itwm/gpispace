813eb045bac01b1aa9a32ed721ae1ba8a5ca1d01
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-01-28 21:55:41 +0100

    we: add transition.fwd

---
+++ we/we/type/transition.fwd.hpp
@@ -0,0 +1,14 @@
+// mirko.rahn@itwm.fraunhofer.de
+
+#ifndef _WE_TYPE_TRANSITION_FWD_HPP
+#define _WE_TYPE_TRANSITION_FWD_HPP
+
+namespace we
+{
+  namespace type
+  {
+    struct transition_t;
+  }
+}
+
+#endif
