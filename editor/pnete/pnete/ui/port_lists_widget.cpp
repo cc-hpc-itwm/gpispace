4b659d4afe1e97d62df21d0bb49420a34a8ad394
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-01-31 16:26:59 +0100

    editor: update port lists when ports get added, renamed, retyped or removed

--- editor/pnete/pnete/ui/port_lists_widget.cpp
+++ editor/pnete/pnete/ui/port_lists_widget.cpp
@@ -2,19 +2,17 @@
 
 #include <pnete/ui/port_lists_widget.hpp>
 
-#include <pnete/data/handle/function.hpp>
 #include <pnete/ui/ComboItemDelegate.hpp>
 
 #include <xml/parse/type/function.hpp>
 
 #include <boost/foreach.hpp>
+#include <boost/range/adaptor/filtered.hpp>
 
 #include <QGroupBox>
 #include <QHeaderView>
 #include <QObject>
 #include <QSplitter>
-#include <QStandardItem>
-#include <QStandardItemModel>
 #include <QTableWidget>
 #include <QVBoxLayout>
 #include <QWidget>
@@ -25,6 +23,141 @@ namespace fhg
   {
     namespace ui
     {
+      namespace detail
+      {
+        namespace
+        {
+          bool same_direction ( const ::xml::parse::id::ref::port& id
+                              , const we::type::PortDirection& direction
+                              )
+          {
+            return id.get().direction() == direction;
+          }
+          data::handle::port make_handle ( const ::xml::parse::id::ref::port& id
+                                         , const data::handle::function& function
+                                         )
+          {
+            return data::handle::port (id, function.document());
+          }
+        }
+
+        ports_model::ports_model ( const data::handle::function& function
+                                 , const we::type::PortDirection& direction
+                                 , QObject* parent
+                                 )
+          : QAbstractTableModel (parent)
+          , _function (function)
+          , _direction (direction)
+        {
+          function.connect_to_change_mgr
+            (this, "port_added", "data::handle::port");
+
+
+          BOOST_FOREACH ( const data::handle::port& handle
+                        , function.get().ports().ids()
+                        | boost::adaptors::filtered
+                          (boost::bind (same_direction, _1, _direction))
+                        | boost::adaptors::transformed
+                          (boost::bind (make_handle, _1, _function))
+                        )
+          {
+            _ports << handle;
+
+            handle.connect_to_change_mgr
+              ( this
+              , "name_set", "type_or_name_changed"
+              , "data::handle::port, QString"
+              );
+            handle.connect_to_change_mgr
+              ( this
+              , "type_set", "type_or_name_changed"
+              , "data::handle::port, QString"
+              );
+
+            handle.connect_to_change_mgr
+              (this, "port_deleted", "data::handle::port");
+          }
+        }
+
+        void ports_model::port_added
+          (const QObject*, const data::handle::port& handle)
+        {
+          if ( handle.get().parent()->id() == _function.id()
+             && same_direction (handle.id(), _direction)
+             )
+          {
+            _ports << handle;
+            reset();
+          }
+        }
+
+        void ports_model::port_deleted
+          (const QObject*, const data::handle::port& changed)
+        {
+          if (_ports.contains (changed))
+          {
+            _ports.removeAll (changed);
+            reset();
+          }
+        }
+
+        void ports_model::type_or_name_changed
+          (const QObject*, const data::handle::port& changed, const QString&)
+        {
+          if (_ports.contains (changed))
+          {
+            reset();
+          }
+        }
+
+        int ports_model::rowCount (const QModelIndex&) const
+        {
+          return _ports.size();
+        }
+
+        int ports_model::columnCount (const QModelIndex&) const
+        {
+          return 2;
+        }
+
+        QVariant ports_model::data (const QModelIndex& index, int role) const
+        {
+          if (index.isValid() && role == Qt::DisplayRole)
+          {
+            switch (index.column())
+            {
+            case 0:
+              return QString::fromStdString
+                (_ports.at (index.row()).get().name());
+            case 1:
+              return QString::fromStdString
+                (_ports.at (index.row()).get().type());
+            }
+          }
+
+          return QVariant();
+        }
+
+        QVariant ports_model::headerData ( int section
+                                         , Qt::Orientation orientation
+                                         , int role
+                                         ) const
+        {
+          if (role == Qt::DisplayRole && orientation == Qt::Horizontal)
+          {
+            switch (section)
+            {
+            case 0:
+              return tr ("port_name");
+            case 1:
+              return tr ("port_type");
+            }
+          }
+
+          return QVariant();
+        }
+      }
+
       namespace
       {
         QGroupBox* port_list_box
@@ -38,43 +171,19 @@ namespace fhg
           //! \todo adjust column sizes automatically
           QTableView* table (new QTableView);
 
-          QStandardItemModel* model (new QStandardItemModel (0, 2, table));
-
-          model->setColumnCount (2);
+          table->setModel (new detail::ports_model (function, which, table));
 
           table->setItemDelegateForColumn
             (1, new ComboBoxItemDelegate (types, table));
-
-          model->setHorizontalHeaderItem (0, new QStandardItem ("Name"));
-          model->setHorizontalHeaderItem (1, new QStandardItem ("Type"));
 
           table->verticalHeader()->hide();
 
           int row (0);
 
-          //! \todo Should use handles and special items to allow
-          //! editing. Also, this should be weaved in, not pulled.
-          BOOST_FOREACH ( const ::xml::parse::type::port_type& port
-                        , function.get().ports().values()
-                        )
-          {
-            if (port.direction() == which)
-            {
-              model->setItem
-                (row, 0, new QStandardItem (QString::fromStdString(port.name())));
-              model->setItem
-                (row, 1, new QStandardItem (QString::fromStdString(port.type())));
-              ++row;
-            }
-          }
-
           table->resizeRowsToContents();
           table->resizeColumnsToContents();
 
-          table->setModel (model);
-
           table->setSelectionMode (QAbstractItemView::NoSelection);
-
 
           QGroupBox* group_box
             ( new QGroupBox ( QObject::tr ( which == we::type::PORT_IN
--- editor/pnete/pnete/ui/port_lists_widget.hpp
+++ editor/pnete/pnete/ui/port_lists_widget.hpp
@@ -3,8 +3,11 @@
 #ifndef _FHG_PNETE_UI_PORT_LISTS_WIDGET_HPP
 #define _FHG_PNETE_UI_PORT_LISTS_WIDGET_HPP 1
 
-#include <pnete/data/handle/function.fwd.hpp>
+#include <pnete/data/handle/function.hpp>
+#include <pnete/data/handle/port.hpp>
 
+#include <QAbstractTableModel>
+#include <QList>
 #include <QObject>
 #include <QWidget>
 
@@ -17,6 +20,38 @@ namespace fhg
   {
     namespace ui
     {
+      //! \note Only exposed as it needs to be moced.
+      namespace detail
+      {
+       class ports_model : public QAbstractTableModel
+        {
+          Q_OBJECT;
+
+        public:
+          ports_model ( const data::handle::function&
+                      , const we::type::PortDirection&
+                      , QObject* parent = NULL
+                      );
+
+          int rowCount (const QModelIndex& = QModelIndex()) const;
+          int columnCount (const QModelIndex& = QModelIndex()) const;
+
+          QVariant data (const QModelIndex&, int = Qt::DisplayRole) const;
+          QVariant headerData (int, Qt::Orientation, int = Qt::DisplayRole) const;
+
+        public slots:
+          void port_added (const QObject*, const data::handle::port&);
+          void type_or_name_changed
+            (const QObject*, const data::handle::port&, const QString&);
+          void port_deleted (const QObject*, const data::handle::port&);
+
+        private:
+          data::handle::function _function;
+          we::type::PortDirection _direction;
+          QList<data::handle::port> _ports;
+        };
+      }
+
       class port_lists_widget : public QWidget
       {
         Q_OBJECT;
