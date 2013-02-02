a045af83576c254c62197fd86e199abd7e9333ba
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-02-02 04:07:10 +0100

    we: separate impl for literal/cpp

---
+++ we/we/type/literal/cpp.cpp
@@ -0,0 +1,119 @@
+// mirko.rahn@itwm.fraunhofer.de
+
+#include <we/type/literal/cpp.hpp>
+
+#include <boost/unordered_map.hpp>
+#include <boost/unordered_set.hpp>
+
+namespace literal
+{
+  namespace cpp
+  {
+    namespace
+    {
+      struct info
+      {
+      private:
+        boost::unordered_map<type_name_t, std::string> _trans;
+        boost::unordered_map<type_name_t, std::string> _inc;
+
+      public:
+        info ()
+          : _trans()
+          , _inc()
+        {
+          _trans[literal::CONTROL()]   = "we::type::literal::control";
+          _trans[literal::BOOL()]      = "bool";
+          _trans[literal::LONG()]      = "long";
+          _trans[literal::DOUBLE()]    = "double";
+          _trans[literal::CHAR()]      = "char";
+          _trans[literal::STRING()]    = "std::string";
+          _trans[literal::BITSET()]    = "bitsetofint::type";
+          _trans[literal::STACK()]     = "std::deque<long>";
+          _trans[literal::MAP()]       = "std::map<long, long>";
+          _trans[literal::SET()]       = "std::set<long>";
+          _trans[literal::BYTEARRAY()] = "bytearray::type";
+
+          _inc[literal::CONTROL()]   = "we/type/literal/control.hpp";
+          _inc[literal::BOOL()]      = "";
+          _inc[literal::LONG()]      = "";
+          _inc[literal::DOUBLE()]    = "";
+          _inc[literal::CHAR()]      = "";
+          _inc[literal::STRING()]    = "string";
+          _inc[literal::BITSET()]    = "we/type/bitsetofint.hpp";
+          _inc[literal::STACK()]     = "deque";
+          _inc[literal::MAP()]       = "map";
+          _inc[literal::SET()]       = "set";
+          _inc[literal::BYTEARRAY()] = "we/type/bytearray.hpp";
+        }
+
+        const std::string& translate (const type_name_t& t) const
+        {
+          return _trans.at (t);
+        }
+
+        const std::string& include (const type_name_t& t) const
+        {
+          return _inc.at (t);
+        }
+
+        bool known (const type_name_t& t) const
+        {
+          return _trans.find (t) != _trans.end();
+        }
+      };
+    }
+
+    const std::string& translate (const type_name_t& t)
+    {
+      static info i;
+
+      return i.translate (t);
+    }
+    const std::string& include (const type_name_t& t)
+    {
+      static info i;
+
+      return i.include (t);
+    }
+
+    bool known (const type_name_t& t)
+    {
+      static info n;
+
+      return n.known (t);
+    }
+
+    namespace
+    {
+      struct names_reserved
+      {
+      public:
+        names_reserved ()
+          : _s()
+        {
+          _s.insert ("control");
+          _s.insert ("bool");
+          _s.insert ("long");
+          _s.insert ("double");
+          _s.insert ("char");
+        }
+
+        bool reserved (const std::string& x) const
+        {
+          return _s.find (x) != _s.end();
+        }
+
+      private:
+        boost::unordered_set<std::string> _s;
+      };
+    }
+
+    bool reserved (const std::string& x)
+    {
+      static names_reserved n;
+
+      return n.reserved (x);
+    }
+  }
+}
--- we/we/type/literal/cpp.hpp
+++ we/we/type/literal/cpp.hpp
@@ -5,116 +5,15 @@
 
 #include <we/type/literal/name.hpp>
 
-#include <boost/unordered_map.hpp>
-#include <boost/unordered_set.hpp>
-
 namespace literal
 {
   namespace cpp
   {
-    struct info
-    {
-    public:
-      typedef boost::unordered_map<type_name_t, std::string> map_type;
+    const std::string& translate (const type_name_t&);
+    const std::string& include (const type_name_t&);
 
-    private:
-      map_type trans;
-      map_type inc;
-
-    public:
-      info (void) : trans (), inc ()
-      {
-        trans[literal::CONTROL()]   = "we::type::literal::control";
-        trans[literal::BOOL()]      = "bool";
-        trans[literal::LONG()]      = "long";
-        trans[literal::DOUBLE()]    = "double";
-        trans[literal::CHAR()]      = "char";
-        trans[literal::STRING()]    = "std::string";
-        trans[literal::BITSET()]    = "bitsetofint::type";
-        trans[literal::STACK()]     = "std::deque<long>";
-        trans[literal::MAP()]       = "std::map<long, long>";
-        trans[literal::SET()]       = "std::set<long>";
-        trans[literal::BYTEARRAY()] = "bytearray::type";
-
-        inc[literal::CONTROL()]   = "we/type/literal/control.hpp";
-        inc[literal::BOOL()]      = "";
-        inc[literal::LONG()]      = "";
-        inc[literal::DOUBLE()]    = "";
-        inc[literal::CHAR()]      = "";
-        inc[literal::STRING()]    = "string";
-        inc[literal::BITSET()]    = "we/type/bitsetofint.hpp";
-        inc[literal::STACK()]     = "deque";
-        inc[literal::MAP()]       = "map";
-        inc[literal::SET()]       = "set";
-        inc[literal::BYTEARRAY()] = "we/type/bytearray.hpp";
-      }
-
-      const map_type::mapped_type & translate (const type_name_t & t) const
-      {
-        return trans.at (t);
-      }
-
-      const map_type::mapped_type & include (const type_name_t & t) const
-      {
-        return inc.at (t);
-      }
-
-      bool known (const type_name_t & t) const
-      {
-        return trans.find (t) != trans.end();
-      }
-    };
-
-    inline const info::map_type::mapped_type & translate (const type_name_t & t)
-    {
-      static info i;
-
-      return i.translate (t);
-    }
-
-    inline const info::map_type::mapped_type & include (const type_name_t & t)
-    {
-      static info i;
-
-      return i.include (t);
-    }
-
-    inline bool known (const type_name_t & t)
-    {
-      static info n;
-
-      return n.known (t);
-    }
-
-    struct names_reserved
-    {
-    private:
-      typedef boost::unordered_set<std::string> set_type;
-
-      set_type s;
-
-    public:
-      names_reserved (void) : s ()
-      {
-        s.insert ("control");
-        s.insert ("bool");
-        s.insert ("long");
-        s.insert ("double");
-        s.insert ("char");
-      }
-
-      bool reserved (const set_type::value_type & x) const
-      {
-        return (s.find (x) != s.end());
-      }
-    };
-
-    inline bool reserved (const std::string & x)
-    {
-      static names_reserved n;
-
-      return n.reserved (x);
-    }
+    bool known (const type_name_t&);
+    bool reserved (const std::string&);
   }
 }
 
