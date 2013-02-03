0eef1af9c59fe88e6bed424f5674796c09489ab4
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-02-03 09:05:20 +0100

    fix: make gcc happy about naming

--- util/fhg/util/rmap/traits.hpp
+++ util/fhg/util/rmap/traits.hpp
@@ -37,16 +37,16 @@ namespace fhg
       };
 
 #define FHG_UTIL_RMAP_TRAITS()                                          \
-      typedef traits<Key,Mapped> traits;                                \
+      typedef traits<Key,Mapped> traits_type;                           \
                                                                         \
-      typedef typename traits::key_type              key_type;          \
-      typedef typename traits::mapped_type           mapped_type;       \
-      typedef typename traits::keys_type             keys_type;         \
-      typedef typename traits::const_ref_mapped_type const_ref_mapped_type; \
-      typedef typename traits::ref_mapped_type       ref_mapped_type;   \
-      typedef typename traits::variant_type          variant_type;      \
-      typedef typename traits::map_type              map_type;          \
-      typedef typename traits::query_result_type     query_result_type
+      typedef typename traits_type::key_type              key_type;     \
+      typedef typename traits_type::mapped_type           mapped_type;  \
+      typedef typename traits_type::keys_type             keys_type;    \
+      typedef typename traits_type::const_ref_mapped_type const_ref_mapped_type; \
+      typedef typename traits_type::ref_mapped_type       ref_mapped_type; \
+      typedef typename traits_type::variant_type          variant_type; \
+      typedef typename traits_type::map_type              map_type;     \
+      typedef typename traits_type::query_result_type     query_result_type
     }
   }
 }
