5a1df3cb431b6cba7965908c904af5d6bc551aa7
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-02-07 12:08:41 +0100

    util: add option to enable/disable stat

--- util/fhg/util/stat.hpp
+++ util/fhg/util/stat.hpp
@@ -17,6 +17,18 @@ namespace fhg
       void inc (const std::string&);
       void start (const std::string&);
       void stop (const std::string&);
+
+#ifndef NSTATISTICS
+#define FHG_UTIL_STAT_START(key)  ::fhg::util::stat::stop (key)
+#define FHG_UTIL_STAT_STOP(key)   ::fhg::util::stat::stop (key)
+#define FHG_UTIL_STAT_INC(key)    ::fhg::util::stat::stop (key)
+#define FHG_UTIL_STAT_OUT(stream) ::fhg::util::stat::out (stream)
+#else
+#define FHG_UTIL_STAT_START(key)
+#define FHG_UTIL_STAT_STOP(key)
+#define FHG_UTIL_STAT_INC(key)
+#define FHG_UTIL_STAT_OUT(stream)
+#endif
     }
   }
 }
