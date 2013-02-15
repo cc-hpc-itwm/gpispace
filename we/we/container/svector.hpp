eaa9cf82c9328304e41a7e7a0cef1905572f8a4c
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-02-15 17:53:20 +0100

    we: cosmetics

--- we/we/container/svector.hpp
+++ we/we/container/svector.hpp
@@ -15,49 +15,49 @@ namespace svector
 {
   struct type
   {
-  private:
-    typedef std::vector<petri_net::transition_id_type>::iterator it_type;
-    typedef std::pair<it_type,it_type> pit_t;
-
-    std::vector<petri_net::transition_id_type> vec;
-
   public:
     void insert (const petri_net::transition_id_type& x)
     {
-      const pit_t pit (std::equal_range (vec.begin(), vec.end(), x));
+      const pit_t pit (std::equal_range (_vec.begin(), _vec.end(), x));
 
       if (pit.first == pit.second)
       {
-        vec.insert (pit.second, x);
+        _vec.insert (pit.second, x);
       }
     }
 
     void erase (const petri_net::transition_id_type& x)
     {
-      const pit_t pit (std::equal_range (vec.begin(), vec.end(), x));
+      const pit_t pit (std::equal_range (_vec.begin(), _vec.end(), x));
 
       if (pit.first != pit.second)
       {
-        vec.erase (pit.first);
+        _vec.erase (pit.first);
       }
     }
 
     bool elem (const petri_net::transition_id_type& x) const
     {
-      return std::binary_search (vec.begin(), vec.end(), x);
+      return std::binary_search (_vec.begin(), _vec.end(), x);
+    }
+
+    bool empty() const
+    {
+      return _vec.empty();
     }
 
     template<typename Engine>
     const petri_net::transition_id_type& random (Engine& engine) const
     {
-      boost::uniform_int<std::size_t> rand (0, vec.size()-1);
-      return vec.at (rand (engine));
+      boost::uniform_int<std::size_t> rand (0, _vec.size()-1);
+      return _vec.at (rand (engine));
     }
 
-    bool empty() const
-    {
-      return vec.empty();
-    }
+  private:
+    typedef std::vector<petri_net::transition_id_type>::iterator it_type;
+    typedef std::pair<it_type,it_type> pit_t;
+
+    std::vector<petri_net::transition_id_type> _vec;
   };
 }
 
