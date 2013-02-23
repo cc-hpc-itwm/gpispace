b368e9c02a092436f97033b79a5b5c86a76bc400
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-02-23 23:01:53 +0100

    we: save position and distance in cross/iterators

--- we/we/util/cross.fwd.hpp
+++ we/we/util/cross.fwd.hpp
@@ -3,10 +3,18 @@
 #ifndef _WE_UTIL_CROSS_FWD_HPP
 #define _WE_UTIL_CROSS_FWD_HPP
 
+#include <we/type/value.hpp>
+
+#include <cstddef>
+
 namespace we
 {
   namespace util
   {
+    typedef std::pair< std::list<value::type>::iterator
+                     , std::size_t
+                     > pos_and_distance_type;
+
     class cross_type;
   }
 }
