70e232c4182a01820aa716ac4a1f1c605905d9a0
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-02-27 11:34:57 +0100

    cmake: fix: fhg_add_test called return() inside a macro(), producing bad behavior
        there is no returning in macros, thus it is calling return() in
        the calling function / file.

--- modules/fhg_test_macros.cmake
+++ modules/fhg_test_macros.cmake
@@ -10,34 +10,32 @@ macro(FHG_ADD_TEST)
   CAR(TEST_SOURCE ${TEST_DEFAULT_ARGS})
   CDR(TEST_ADDITIONAL_SOURCES ${TEST_DEFAULT_ARGS})
 
-  if (NOT BUILD_TESTING)
-    return()
+  if (BUILD_TESTING)
+    if (NOT TEST_PROJECT)
+      set(TEST_PROJECT "${PROJECT_NAME}")
+    endif()
+
+    if (NOT TEST_STANDALONE)
+      set(TEST_LINK_LIBRARIES ${TEST_LINK_LIBRARIES} ${Boost_UNIT_TEST_LIBRARIES} ${Boost_LIBRARIES})
+    endif()
+
+    # get the filename without extension
+    string(REGEX REPLACE "(.*/)?(.*)\\.c.*" "${TEST_PROJECT}_\\2" tc_name ${TEST_SOURCE})
+
+    if (NOT TEST_QUIET)
+      message(STATUS "**** Adding test ${tc_name} ${TEST_ARGS} (${TEST_DESCRIPTION})")
+    endif()
+
+    add_executable(${tc_name} ${TEST_SOURCE} ${TEST_ADDITIONAL_SOURCES})
+    if (TEST_COMPILE_FLAGS)
+      set_target_properties(${tc_name} PROPERTIES COMPILE_FLAGS ${TEST_COMPILE_FLAGS})
+    endif()
+    target_link_libraries(${tc_name} ${TEST_LINK_LIBRARIES})
+    get_target_property(TC_LOC ${tc_name} LOCATION)
+    add_test (${tc_name} ${TC_LOC} ${TEST_ARGS})
+
+    foreach (d ${TEST_DEPENDS})
+      add_dependencies(${tc_name} ${d})
+    endforeach()
   endif()
-
-  if (NOT TEST_PROJECT)
-    set(TEST_PROJECT "${PROJECT_NAME}")
-  endif()
-
-  if (NOT TEST_STANDALONE)
-    set(TEST_LINK_LIBRARIES ${TEST_LINK_LIBRARIES} ${Boost_UNIT_TEST_LIBRARIES} ${Boost_LIBRARIES})
-  endif()
-
-  # get the filename without extension
-  string(REGEX REPLACE "(.*/)?(.*)\\.c.*" "${TEST_PROJECT}_\\2" tc_name ${TEST_SOURCE})
-
-  if (NOT TEST_QUIET)
-    message(STATUS "**** Adding test ${tc_name} ${TEST_ARGS} (${TEST_DESCRIPTION})")
-  endif()
-
-  add_executable(${tc_name} ${TEST_SOURCE} ${TEST_ADDITIONAL_SOURCES})
-  if (TEST_COMPILE_FLAGS)
-    set_target_properties(${tc_name} PROPERTIES COMPILE_FLAGS ${TEST_COMPILE_FLAGS})
-  endif()
-  target_link_libraries(${tc_name} ${TEST_LINK_LIBRARIES})
-  get_target_property(TC_LOC ${tc_name} LOCATION)
-  add_test (${tc_name} ${TC_LOC} ${TEST_ARGS})
-
-  foreach (d ${TEST_DEPENDS})
-    add_dependencies(${tc_name} ${d})
-  endforeach()
 endmacro()
