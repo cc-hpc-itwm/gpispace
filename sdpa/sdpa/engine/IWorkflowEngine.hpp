5cfb8f7f664009f04dd8437c88594330893c9df8
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-03-05 16:01:30 +0100

    sdpa: move scheduler-only type & enum to schedulerimpl

--- sdpa/sdpa/engine/IWorkflowEngine.hpp
+++ sdpa/sdpa/engine/IWorkflowEngine.hpp
@@ -12,15 +12,6 @@ typedef we::mgmt::basic_layer::result_type result_type;
 typedef we::mgmt::basic_layer::reason_type reason_type;
 typedef we::mgmt::basic_layer::encoded_type encoded_type;
 
-enum ExecutionState
-{
-  ACTIVITY_FINISHED,
-  ACTIVITY_FAILED,
-	ACTIVITY_CANCELLED,
-};
-
-typedef std::pair<ExecutionState, result_type> execution_result_t;
-
 typedef we::type::requirement_t requirement_t;
 typedef std::list<requirement_t> requirement_list_t;
 
