6bdd1eb7af59f3da56f35f599a222d09b82c52c9
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-03-07 14:24:32 +0100

    util: parse/error

---
+++ util/fhg/util/parse/error.cpp
@@ -0,0 +1,40 @@
+// mirko.rahn@itwm.fraunhofer.de
+
+#include <fhg/util/parse/error.hpp>
+
+#include <sstream>
+
+namespace fhg
+{
+  namespace util
+  {
+    namespace parse
+    {
+      namespace error
+      {
+        namespace detail
+        {
+          std::string nice (const std::string& msg, const position& inp)
+          {
+            std::ostringstream oss;
+
+            oss << "PARSE ERROR [" << inp() << "]: " << msg << std::endl;
+            oss << inp.consumed() << " " << inp.rest() << std::endl;
+
+            for (std::size_t i (0); i < inp(); ++i)
+            {
+              oss << " ";
+            }
+            oss << "^" << std::endl;
+
+            return oss.str();
+          }
+        }
+
+        expected::expected (const std::string& what, const position& inp)
+          : generic (boost::format ("expected '%1%'") % what, inp)
+        {}
+      }
+    }
+  }
+}
