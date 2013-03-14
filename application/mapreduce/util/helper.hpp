f74da00f40f4db3a5f8efd26549f6b62138c72f0
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-03-14 09:06:07 +0100

    fix: do not give wrong type modifiers in initialization of constant

--- application/mapreduce/util/helper.hpp
+++ application/mapreduce/util/helper.hpp
@@ -32,8 +32,8 @@
 #include <fstream>
 
 
-const int US = 1000000.0L;
-const int MS = 1000.0L;
+const int US = 1000000;
+const int MS = 1000;
 const int KEY_MAX_SIZE = 50;
 
 const char SHRPCH = '#';
