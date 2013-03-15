eda3e37440a2f70c76ada52e3f579bfe1d3af661
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2013-03-15 13:37:12 +0100

    sdpa: fix: removed most of the sleeps in test_Atomic (only the poll interval is left)

--- sdpa/tests/sdpa/test_Atomic.cpp
+++ sdpa/tests/sdpa/test_Atomic.cpp
@@ -64,65 +64,7 @@ using namespace sdpa::tests;
 
 #define NO_GUI ""
 
-static std::string current_kvs_port = "0";
-static const std::string kvs_host () { static std::string s("localhost"); return s; }
-static const std::string & kvs_port () { return current_kvs_port; }
-
-struct KVSSetup
-{
-  KVSSetup ()
-    : m_pool (0)
-    , m_kvsd (0)
-    , m_serv (0)
-    , m_thrd (0)
-  {
-    FHGLOG_SETUP();
-
-    m_pool = new fhg::com::io_service_pool(1);
-    m_kvsd = new fhg::com::kvs::server::kvsd ("");
-    m_serv = new fhg::com::tcp_server ( *m_pool
-                                      , *m_kvsd
-                                      , kvs_host ()
-                                      , kvs_port ()
-                                      , true
-                                      );
-    m_thrd = new boost::thread (boost::bind ( &fhg::com::io_service_pool::run
-                                            , m_pool
-                                            )
-                               );
-
-    m_serv->start();
-
-    LOG(INFO, "kvs daemon is listening on port " << m_serv->port ());
-
-    current_kvs_port = boost::lexical_cast<std::string>(m_serv->port());
-
-    fhg::com::kvs::global::get_kvs_info().init( kvs_host()
-                                              , kvs_port()
-                                              , boost::posix_time::seconds(10)
-                                              , 3
-                                              );
-
-  }
-
-  ~KVSSetup ()
-  {
-    m_serv->stop ();
-    m_pool->stop ();
-    m_thrd->join ();
-
-    delete m_thrd;
-    delete m_serv;
-    delete m_kvsd;
-    delete m_pool;
-  }
-
-  fhg::com::io_service_pool *m_pool;
-  fhg::com::kvs::server::kvsd *m_kvsd;
-  fhg::com::tcp_server *m_serv;
-  boost::thread *m_thrd;
-};
-
+#include "kvs_setup_fixture.hpp"
 BOOST_GLOBAL_FIXTURE (KVSSetup);
 
 struct MyFixture
@@ -231,7 +173,7 @@ void MyFixture::run_client()
 				job_status = ptrCli->queryJob(job_id_user);
 				LOG( DEBUG, "The status of the job "<<job_id_user<<" is "<<job_status);
 
-				boost::this_thread::sleep(boost::posix_time::seconds(10));
+				boost::this_thread::sleep(boost::posix_time::seconds(1));
 			}
 			catch(const sdpa::client::ClientException& cliExc)
 			{
@@ -243,8 +185,6 @@ void MyFixture::run_client()
 					ptrCli.reset();
 					return;
 				}
-
-				boost::this_thread::sleep(boost::posix_time::seconds(3));
 			}
 		}
 
@@ -253,7 +193,6 @@ void MyFixture::run_client()
 		try {
 				LOG( DEBUG, "User: retrieve results of the job "<<job_id_user);
 				ptrCli->retrieveResults(job_id_user);
-				boost::this_thread::sleep(boost::posix_time::seconds(3));
 		}
 		catch(const sdpa::client::ClientException& cliExc)
 		{
@@ -263,8 +202,6 @@ void MyFixture::run_client()
 			ptrCli->shutdown_network();
 			ptrCli.reset();
 			return;
-
-			boost::this_thread::sleep(boost::posix_time::seconds(3));
 		}
 
 		nTrials = 0;
@@ -272,7 +209,6 @@ void MyFixture::run_client()
 		try {
 			LOG( DEBUG, "User: delete the job "<<job_id_user);
 			ptrCli->deleteJob(job_id_user);
-			boost::this_thread::sleep(boost::posix_time::seconds(3));
 		}
 		catch(const sdpa::client::ClientException& cliExc)
 		{
@@ -281,13 +217,10 @@ void MyFixture::run_client()
 			ptrCli->shutdown_network();
 			ptrCli.reset();
 			return;
-
-			boost::this_thread::sleep(boost::posix_time::seconds(3));
 		}
 	}
 
 	ptrCli->shutdown_network();
-	boost::this_thread::sleep(boost::posix_time::microseconds(5*m_sleep_interval));
   ptrCli.reset();
 }
 
