fb119ed972ec137845e2777b5e50cbe61db9e4c1
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-03-15 11:21:41 +0100

    fix: delete unused variable

--- sdpa/plugins/sdpac.cpp
+++ sdpa/plugins/sdpac.cpp
@@ -147,7 +147,7 @@ public:
               );
     if (request(req, rep) == 0)
     {
-      if (CancelJobAckEvent* ack = dynamic_cast<CancelJobAckEvent*>(rep.get()))
+      if (dynamic_cast<CancelJobAckEvent*>(rep.get()))
       {
         return 0;
       }
