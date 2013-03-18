3a4a1f244b273fdc9b2874a86531b1f8a6f90ac1
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2013-03-18 12:53:48 +0100

    gspcnet: new: added frame_util header containing set/get with templates

---
+++ gspcnet/gspc/net/frame_util.hpp
@@ -0,0 +1,40 @@
+#ifndef GSPC_NET_FRAME_UTIL_HPP
+#define GSPC_NET_FRAME_UTIL_HPP
+
+#include <boost/lexical_cast.hpp>
+
+#include <gspc/net/frame.hpp>
+
+namespace gspc
+{
+  namespace net
+  {
+    template <typename T>
+    frame & frame_set_header ( frame & f
+                             , frame::key_type const &key
+                             , T const &val
+                             )
+    {
+      return f.set_header (key, boost::lexical_cast<frame::value_type>(val));
+    }
+
+    template <typename T>
+    T frame_get_header ( frame const & f
+                       , frame::key_type const &key
+                       , T const &dflt
+                       )
+    {
+      boost::optional<std::string> entry = f.get_header (key);
+      if (entry)
+      {
+        return boost::lexical_cast<T>(*entry);
+      }
+      else
+      {
+        return dflt;
+      }
+    }
+  }
+}
+
+#endif
