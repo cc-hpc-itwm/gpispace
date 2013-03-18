5cc648117250c515b60f7d006b08acd1fdf436fd
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-03-18 15:56:22 +0100

    sdpa-gui: instead of scrolling every 200ms, scroll every 0ms, which is after each event loop. this might degrade performance again

--- editor/src/pnete/ui/log_monitor.cpp
+++ editor/src/pnete/ui/log_monitor.cpp
@@ -466,7 +466,7 @@ void log_monitor::toggleFollowLogging (bool follow)
   connect (sender(), SIGNAL (toggled (bool)), log_follower, SLOT (deleteLater()));
 
   //! \todo Configurable refresh rate.
-  static const int refresh_rate (200 /*ms*/);
+  static const int refresh_rate (0 /*ms*/);
   log_follower->start (refresh_rate);
 }
 
