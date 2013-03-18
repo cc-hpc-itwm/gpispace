eb1bee8efda7b16ebb185c7c2c78b919db2548a7
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2013-03-18 01:18:46 +0100

    gspcnet: new: added error.hpp

---
+++ gspcnet/gspc/net/error.hpp
@@ -0,0 +1,15 @@
+#ifndef GSPC_NET_ERROR_HPP
+#define GSPC_NET_ERROR_HPP
+
+namespace gspc
+{
+  namespace net
+  {
+    enum error_code_t
+      {
+        E_INTERNAL_ERROR = 500
+      };
+  }
+}
+
+#endif
