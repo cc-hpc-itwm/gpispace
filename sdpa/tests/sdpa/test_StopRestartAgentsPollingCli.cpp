c97aa6f17b0c0726242979c8582df28e9c1f5af0
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2013-03-20 18:00:23 +0100

    gpi-space: tests: rmoved irelevant test -> an agent has always a we attached

--- sdpa/tests/sdpa/test_StopRestartAgentsPollingCli.cpp
+++ sdpa/tests/sdpa/test_StopRestartAgentsPollingCli.cpp
@@ -188,7 +188,7 @@ void MyFixture::run_client_polling()
 	}
 
 	ptrCli->shutdown_network();
-  ptrCli.reset();
+	ptrCli.reset();
 }
 
 BOOST_FIXTURE_TEST_SUITE( test_StopRestartAgents, MyFixture );
@@ -225,7 +225,7 @@ BOOST_AUTO_TEST_CASE( testStop_2Agents_NoDrts_push_1)
 	boost::thread threadClient = boost::thread(boost::bind(&MyFixture::run_client_polling, this));
 
 	ptrAgent0->shutdown(strBackupAgent0);
-	LOG( INFO, "Shutdown agent \"agent_o\". The recovery string is "<<strBackupAgent0);
+	LOG( INFO, "Shutdown agent \"agent_0\". The recovery string is "<<strBackupAgent0);
 
 	boost::this_thread::sleep(boost::posix_time::seconds(1));
 
@@ -274,7 +274,7 @@ BOOST_AUTO_TEST_CASE( testStop_AgentWithDrts_push)
 	boost::thread threadClient = boost::thread(boost::bind(&MyFixture::run_client_polling, this));
 
 	ptrAgent->shutdown(strBackupAgent);
-	LOG( INFO, "Shutdown the agent \"agent_o\". The recovery string is "<<strBackupAgent);
+	LOG( INFO, "Shutdown the agent \"agent_0\". The recovery string is "<<strBackupAgent);
 
 	boost::this_thread::sleep(boost::posix_time::seconds(1));
 
@@ -297,53 +297,6 @@ BOOST_AUTO_TEST_CASE( testStop_AgentWithDrts_push)
 	LOG( DEBUG, "The test case testStop_AgentWithDrts_push terminated!");
 }
 
-BOOST_AUTO_TEST_CASE( testStop_AgentNoWE_push)
-{
-	LOG( DEBUG, "testStop_AgentNoWE_push");
-	//guiUrl
-	string guiUrl   	= "";
-	string workerUrl 	= "127.0.0.1:5500";
-	string addrOrch 	= "127.0.0.1";
-	string addrAgent 	= "127.0.0.1";
-
-	typedef void OrchWorkflowEngine;
-
-	m_strWorkflow = read_workflow("workflows/transform_file.pnet");
-	//LOG( DEBUG, "The test workflow is "<<m_strWorkflow);
-
-	sdpa::daemon::Orchestrator::ptr_t ptrOrch = sdpa::daemon::OrchestratorFactory<void>::create("orchestrator_0", addrOrch, MAX_CAP);
-	ptrOrch->start_agent(false, strBackupOrch);
-
-	sdpa::master_info_list_t arrAgentMasterInfo(1, MasterInfo("orchestrator_0"));
-	sdpa::daemon::Agent::ptr_t ptrAgent = sdpa::daemon::AgentFactory<void>::create("agent_0", addrAgent, arrAgentMasterInfo, MAX_CAP );
-	ptrAgent->start_agent(false, strBackupAgent);
-
-	sdpa::shared_ptr<fhg::core::kernel_t> drts_0( createDRTSWorker("drts_0", "agent_0", "", TESTS_TRANSFORM_FILE_MODULES_PATH, kvs_host(), kvs_port()) );
-	boost::thread drts_0_thread = boost::thread(&fhg::core::kernel_t::run, drts_0);
-
-	boost::thread threadClient = boost::thread(boost::bind(&MyFixture::run_client_polling, this));
-
-	ptrAgent->shutdown(strBackupAgent);
-	LOG( INFO, "Shutdown the agent \"agent_o\". The recovery string is "<<strBackupAgent);
-
-	// now try to recover the system
-	sdpa::daemon::Agent::ptr_t ptrRecAgent = sdpa::daemon::AgentFactory<void>::create("agent_0", addrAgent, arrAgentMasterInfo, MAX_CAP );
-
-	LOG( INFO, "Re-start the agent. The recovery string is "<<strBackupAgent);
-	ptrRecAgent->start_agent(false, strBackupAgent);
-
-	if( threadClient.joinable() )
-		threadClient.join();
-	LOG( INFO, "The client thread joined the main thread!" );
-
-	drts_0->stop();
-	drts_0_thread.join();
-
-	ptrRecAgent->shutdown();
-	ptrOrch->shutdown();
-
-	LOG( DEBUG, "The test case testStop_AgentNoWE_push terminated!");
-}
 
 BOOST_AUTO_TEST_CASE( testStop_AgentEmptyWE_push)
 {
@@ -373,7 +326,7 @@ BOOST_AUTO_TEST_CASE( testStop_AgentEmptyWE_push)
 	boost::thread threadClient = boost::thread(boost::bind(&MyFixture::run_client_polling, this));
 
 	ptrAgent->shutdown(strBackupAgent);
-	LOG( INFO, "Shutdown the agent \"agent_o\". The recovery string is "<<strBackupAgent);
+	LOG( INFO, "Shutdown the agent \"agent_0\". The recovery string is "<<strBackupAgent);
 
 	boost::this_thread::sleep(boost::posix_time::seconds(3));
 
@@ -423,7 +376,7 @@ BOOST_AUTO_TEST_CASE( testStop_AgentRealWE_push)
 	boost::thread threadClient = boost::thread(boost::bind(&MyFixture::run_client_polling, this));
 
 	ptrAgent->shutdown(strBackupAgent);
-	LOG( INFO, "Shutdown the agent \"agent_o\". The recovery string is "<<strBackupAgent);
+	LOG( INFO, "Shutdown the agent \"agent_0\". The recovery string is "<<strBackupAgent);
 
 	boost::this_thread::sleep(boost::posix_time::seconds(3));
 
