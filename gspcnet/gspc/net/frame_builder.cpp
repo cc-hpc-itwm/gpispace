c75229b9868f7d288eb5c24e946017ac7b3dd4a9
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2013-03-20 23:52:37 +0100

    gspcnet: added more frame factory functions

--- gspcnet/gspc/net/frame_builder.cpp
+++ gspcnet/gspc/net/frame_builder.cpp
@@ -26,6 +26,95 @@ namespace gspc
         r_id.apply_to (f);
         return f;
       }
+
+      frame connected_frame (gspc::net::header::version const &version)
+      {
+        frame f ("CONNECTED");
+        version.apply_to (f);
+        return f;
+      }
+
+      frame message_frame (frame const & send_frame)
+      {
+        frame f (send_frame);
+        f.set_command ("MESSAGE");
+        return f;
+      }
+
+      frame send_frame (gspc::net::header::destination const & dst)
+      {
+        frame f ("SEND");
+        dst.apply_to (f);
+        return f;
+      }
+
+      frame subscribe_frame ( gspc::net::header::destination const &dst
+                            , gspc::net::header::id const &id
+                            )
+      {
+        frame f ("SUBSCRIBE");
+        dst.apply_to (f);
+        id.apply_to (f);
+        return f;
+      }
+
+      frame unsubscribe_frame (gspc::net::header::id const &id)
+      {
+        frame f ("UNSUBSCRIBE");
+        id.apply_to (f);
+        return f;
+      }
+
+      frame ack_frame (gspc::net::header::id const &id)
+      {
+        frame f ("ACK");
+        id.apply_to (f);
+        return f;
+      }
+
+      frame nack_frame (gspc::net::header::id const &id)
+      {
+        frame f ("NACK");
+        id.apply_to (f);
+        return f;
+      }
+
+      frame begin_frame (gspc::net::header::transaction const &trans)
+      {
+        frame f ("BEGIN");
+        trans.apply_to (f);
+        return f;
+      }
+
+      frame commit_frame (gspc::net::header::transaction const &trans)
+      {
+        frame f ("COMMIT");
+        trans.apply_to (f);
+        return f;
+      }
+
+      frame abort_frame (gspc::net::header::transaction const &trans)
+      {
+        frame f ("ABORT");
+        trans.apply_to (f);
+        return f;
+      }
+
+      frame connect_frame ()
+      {
+        frame f ("CONNECT");
+        //f.set_header ("login", "...");
+        //f.set_header ("passcode", "...");
+        //f.set_header ("heart-beat", "1000,1000");
+        //f.set_header ("accept-version","1.0,1.2");
+        return f;
+      }
+
+      frame disconnect_frame ()
+      {
+        frame f ("DISCONNECT");
+        return f;
+      }
     }
   }
 }
--- gspcnet/gspc/net/frame_builder.hpp
+++ gspcnet/gspc/net/frame_builder.hpp
@@ -10,8 +10,30 @@ namespace gspc
   {
     namespace make
     {
+      frame const & heartbeat_frame ();
+
+      // server -> client frames
       frame error_frame (int ec, const char *message);
       frame receipt_frame (gspc::net::header::receipt const &);
+      frame connected_frame (gspc::net::header::version const &);
+      frame message_frame (frame const & send_frame);
+
+      frame send_frame (gspc::net::header::destination const &);
+      frame subscribe_frame ( gspc::net::header::destination const &
+                            , gspc::net::header::id const &
+                            );
+      frame unsubscribe_frame (gspc::net::header::id const &);
+
+      frame ack_frame (gspc::net::header::id const &);
+      frame nack_frame (gspc::net::header::id const &);
+
+      // transaction related
+      frame begin_frame (gspc::net::header::transaction const &);
+      frame commit_frame (gspc::net::header::transaction const &);
+      frame abort_frame (gspc::net::header::transaction const &);
+
+      frame connect_frame ();
+      frame disconnect_frame ();
     }
   }
 }
