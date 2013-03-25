aa0d756316a2fa7119364772cb4c397d60c3a23e
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-03-25 16:04:58 +0100

    xml: test: add tests for error_duplicate_connect

--- xml/xml/tests/diagnostics.cpp
+++ xml/xml/tests/diagnostics.cpp
@@ -33,6 +33,24 @@ namespace
         BOOST_REQUIRE_EQUAL (e.what(), expected_what.str().c_str());
       }
     }
+
+    template<typename Ex>
+    void require_exception_from_parse
+      ( const std::string& exception_name
+      , const boost::format& expected_what
+      )
+    {
+      try
+      {
+        parse();
+
+        BOOST_FAIL (boost::format ("exception <%1%> missing") % exception_name);
+      }
+      catch (const Ex& e)
+      {
+        BOOST_REQUIRE_EQUAL (e.what(), expected_what.str().c_str());
+      }
+    }
   };
 }
 
@@ -66,5 +84,57 @@ BOOST_FIXTURE_TEST_CASE (error_duplicate_external_function, fixture)
                     )
     % xml::parse::util::position_type (NULL, NULL, xpnet, 10, 9)
     % xml::parse::util::position_type (NULL, NULL, xpnet, 5, 9)
+    );
+}
+
+BOOST_FIXTURE_TEST_CASE (error_duplicate_connect_in_read, fixture)
+{
+  set_parse_input ("connect_in_read_same.xpnet");
+
+  require_exception_from_parse<xml::parse::error::duplicate_connect>
+    ( "error::duplicate_connect"
+    , boost::format ( "ERROR: duplicate connect-read P <-> A for transition"
+                      " foo in %1% (existing connection is connect-in)"
+                    )
+    % xpnet
+    );
+}
+
+BOOST_FIXTURE_TEST_CASE (error_duplicate_connect_in_in, fixture)
+{
+  set_parse_input ("connect_in_in_same.xpnet");
+
+  require_exception_from_parse<xml::parse::error::duplicate_connect>
+    ( "error::duplicate_connect"
+    , boost::format ( "ERROR: duplicate connect-in P <-> A for transition"
+                      " foo in %1% (existing connection is connect-in)"
+                    )
+    % xpnet
+    );
+}
+
+BOOST_FIXTURE_TEST_CASE (error_duplicate_connect_read_read, fixture)
+{
+  set_parse_input ("connect_read_read_same.xpnet");
+
+  require_exception_from_parse<xml::parse::error::duplicate_connect>
+    ( "error::duplicate_connect"
+    , boost::format ( "ERROR: duplicate connect-read P <-> A for transition"
+                      " foo in %1% (existing connection is connect-read)"
+                    )
+    % xpnet
+    );
+}
+
+BOOST_FIXTURE_TEST_CASE (error_duplicate_connect_out_out, fixture)
+{
+  set_parse_input ("connect_out_out_same.xpnet");
+
+  require_exception_from_parse<xml::parse::error::duplicate_connect>
+    ( "error::duplicate_connect"
+    , boost::format ( "ERROR: duplicate connect-out P <-> A for transition"
+                      " foo in %1% (existing connection is connect-out)"
+                    )
+    % xpnet
     );
 }
