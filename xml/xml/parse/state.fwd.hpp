cd5989a2109f7530ae50ba256275e13c6266eb98
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-03-25 20:05:48 +0100

    xml: cleanup state.fwd.hpp

--- xml/xml/parse/state.fwd.hpp
+++ xml/xml/parse/state.fwd.hpp
@@ -9,18 +9,6 @@ namespace xml
   {
     namespace state
     {
-      // namespace fs = boost::filesystem;
-
-      // typedef std::vector<std::string> search_path_type;
-      // typedef std::vector<fs::path> in_progress_type;
-      // typedef std::set<fs::path> dependencies_type;
-
-      // typedef std::vector<std::string> gen_param_type;
-
-      struct key_value_t;
-
-      // typedef std::list<key_value_t> key_values_t;
-
       struct type;
     }
   }
