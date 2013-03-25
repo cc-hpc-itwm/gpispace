4ce90819698741cfb8b2ba6493882fae5c4415a6
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2013-03-25 17:40:44 +0100

    gspcnet: serve uses location splitter

--- gspcnet/gspc/net/serve.cpp
+++ gspcnet/gspc/net/serve.cpp
@@ -1,13 +1,95 @@
 #include "serve.hpp"
 
+#include <string>
+
+#include <boost/system/system_error.hpp>
+
+#include <gspc/net/util.hpp>
 #include <gspc/net/server.hpp>
 
 namespace gspc
 {
   namespace net
   {
-    server_t *serve (const char * url, server::queue_manager_t &qmgr)
+    static
+    server_t *s_new_unix_server ( std::string const & path
+                                , server::queue_manager_t &qmgr
+                                , boost::system::error_code & ec
+                                )
     {
+      using namespace boost::system;
+
+      if (path.empty ())
+      {
+        ec = errc::make_error_code (errc::invalid_argument);
+        return (server_t*)(0);
+      }
+
+      ec = errc::make_error_code (errc::not_supported);
+      return (server_t*)(0);
+    }
+
+    static
+    server_t *s_new_tcp_server ( std::string const & location
+                               , server::queue_manager_t &qmgr
+                               , boost::system::error_code & ec
+                               )
+    {
+      using namespace boost::system;
+
+      std::string host;
+      std::string port;
+
+      if (split_host_port (location, host, port) != std::string::npos)
+      {
+        ec = errc::make_error_code (errc::invalid_argument);
+      }
+      else
+      {
+        ec = errc::make_error_code (errc::not_supported);
+      }
+
+      return (server_t*)(0);
+    }
+
+    server_t *serve ( const char *url
+                    , server::queue_manager_t &qmgr
+                    )
+    {
+      boost::system::error_code ec;
+      server_t *server = serve (url, qmgr, ec);
+
+      if (ec)
+      {
+        if (server) delete server;
+        throw boost::system::system_error (ec);
+      }
+
+      return server;
+    }
+
+    server_t *serve ( const char *url
+                    , server::queue_manager_t & qmgr
+                    , boost::system::error_code & ec
+                    )
+    {
+      using namespace boost::system;
+
+      std::string s_url (url);
+
+      ec = errc::make_error_code (errc::success);
+
+      if (s_url.find ("unix://") == 0)
+      {
+        return s_new_unix_server (s_url.substr (7), qmgr, ec);
+      }
+      else if (s_url.find ("tcp://") == 0)
+      {
+        return s_new_tcp_server (s_url.substr (6), qmgr, ec);
+      }
+
+      ec = errc::make_error_code (errc::wrong_protocol_type);
+
       return (server_t*)(0);
     }
   }
--- gspcnet/gspc/net/serve.hpp
+++ gspcnet/gspc/net/serve.hpp
@@ -4,11 +4,19 @@
 #include <gspc/net/server_fwd.hpp>
 #include <gspc/net/server/queue_manager_fwd.hpp>
 
+#include <boost/system/error_code.hpp>
+
 namespace gspc
 {
   namespace net
   {
-    server_t *serve (const char *url, server::queue_manager_t &);
+    server_t *serve ( const char *url
+                    , server::queue_manager_t &
+                    );
+    server_t *serve ( const char *url
+                    , server::queue_manager_t &
+                    , boost::system::error_code & ec
+                    );
   }
 }
 
