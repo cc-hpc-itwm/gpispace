6789b376280fcf1b522f92d393cea5530ed5cf86
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-03-25 15:40:06 +0100

    editor: make use of fhg::util::temporary_path

--- editor/src/pnete/ui/editor_window.cpp
+++ editor/src/pnete/ui/editor_window.cpp
@@ -21,6 +21,7 @@
 #include <pnete/ui/transition_library_view.hpp>
 
 #include <fhg/plugin/core/kernel.hpp>
+#include <fhg/util/temporary_path.hpp>
 
 #include <sdpa/plugins/sdpactl.hpp>
 #include <sdpa/plugins/sdpac.hpp>
@@ -652,27 +653,6 @@ namespace fhg
           we::loader::loader& loader;
         };
 
-        struct temporary_path_type
-        {
-          temporary_path_type()
-            : _path (boost::filesystem::unique_path())
-          {
-            boost::filesystem::create_directories (_path);
-          }
-
-          ~temporary_path_type()
-          {
-            boost::filesystem::remove_all (_path);
-          }
-
-          operator boost::filesystem::path() const
-          {
-            return _path;
-          }
-
-          boost::filesystem::path _path;
-        };
-
         boost::optional<std::string> get_env (const std::string& name)
         {
           const char *var (getenv (name.c_str()));
@@ -889,7 +869,7 @@ namespace fhg
       void editor_window::execute_remote_inputs_via_prompt()
       try
       {
-        const temporary_path_type temporary_path;
+        const fhg::util::temporary_path temporary_path;
 
         we::mgmt::type::activity_t activity
           (prepare_activity (_accessed_widgets, temporary_path));
@@ -965,7 +945,7 @@ namespace fhg
       void editor_window::execute_locally_inputs_via_prompt()
       try
       {
-        const temporary_path_type temporary_path;
+        const fhg::util::temporary_path temporary_path;
 
         we::mgmt::type::activity_t activity
           (prepare_activity (_accessed_widgets, temporary_path));
@@ -1010,7 +990,7 @@ namespace fhg
       void editor_window::execute_locally_inputs_from_file()
       try
       {
-        const temporary_path_type temporary_path;
+        const fhg::util::temporary_path temporary_path;
 
         we::mgmt::type::activity_t activity
           (prepare_activity (_accessed_widgets, temporary_path));
