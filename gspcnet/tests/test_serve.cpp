ad2a7315d991ad707560e5611084966aa82321a5
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2013-03-26 16:28:03 +0100

    gspcnet: added simple test case for unix sockets

--- gspcnet/tests/test_serve.cpp
+++ gspcnet/tests/test_serve.cpp
@@ -25,3 +25,21 @@ BOOST_AUTO_TEST_CASE (test_serve_tcp_socket_start_stop_loop)
     BOOST_REQUIRE (server);
   }
 }
+
+BOOST_AUTO_TEST_CASE (test_serve_unix_socket)
+{
+  static const size_t NUM_ITERATIONS = 1000;
+
+  gspc::net::server::queue_manager_t qmgr;
+
+  for (size_t i = 0 ; i < NUM_ITERATIONS ; ++i)
+  {
+    gspc::net::server_ptr_t server =
+      gspc::net::serve ("unix://socket.foo", qmgr);
+    BOOST_REQUIRE (server);
+
+    BOOST_CHECK (fs::exists ("socket.foo"));
+  }
+
+  fs::remove ("socket.foo");
+}
