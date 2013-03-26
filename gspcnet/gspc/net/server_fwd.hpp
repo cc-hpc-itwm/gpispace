3aa08639315f27a0a80045dcc7e80e62e1e62d56
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2013-03-26 11:23:56 +0100

    gspcnet: added typedef server_ptr_t to boost::shared_ptr

--- gspcnet/gspc/net/server_fwd.hpp
+++ gspcnet/gspc/net/server_fwd.hpp
@@ -1,11 +1,15 @@
 #ifndef GSPC_NET_SERVER_FWD_HPP
 #define GSPC_NET_SERVER_FWD_HPP
 
+#include <boost/shared_ptr.hpp>
+
 namespace gspc
 {
   namespace net
   {
     class server_t;
+
+    typedef boost::shared_ptr<server_t> server_ptr_t;
   }
 }
 
