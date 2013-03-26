f280b76f67b63f034df0bf864bdd87f1bf18b590
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2013-03-26 16:25:04 +0100

    gspcnet: demo daemon is able to listen on a number of urls

--- gspcnet/bin/gspcnet-daemon.cpp
+++ gspcnet/bin/gspcnet-daemon.cpp
@@ -1,19 +1,45 @@
 #include <iostream>
 
+#include <vector>
 #include <gspc/net.hpp>
 
 int main (int argc, char *argv[])
 {
+  if (argc == 1)
+  {
+    std::cerr << "usage: gspcnetd url..." << std::endl;
+    return 1;
+  }
+
+  std::vector<gspc::net::server_ptr_t> servers;
   gspc::net::server::queue_manager_t qmgr;
 
-  std::string url = argc > 1 ? argv[1] : "tcp://localhost:5000";
+  for (int i = 1 ; i < argc ; ++i)
+  {
+    try
+    {
+      gspc::net::server_ptr_t server =
+        gspc::net::serve (argv [i], qmgr);
 
-  gspc::net::server_ptr_t server =
-    gspc::net::serve (url, qmgr);
+      std::cout << "listening on: "
+                << server->url ()
+                << std::endl;
 
-  std::cout << "use the following url to connect: "
-            << server->url ()
-            << std::endl;
+      servers.push_back (server);
+    }
+    catch (std::exception const &ex)
+    {
+      std::cerr << "could not listen on '" << argv [i] << "': "
+                << ex.what ()
+                << std::endl;
+    }
+  }
+
+  if (servers.empty ())
+  {
+    std::cerr << "nothing to listen on!" << std::endl;
+    return 2;
+  }
 
   pause ();
 }
