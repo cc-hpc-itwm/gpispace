8c9120aea24c8dc0ede972c0d477d66f894cfcd1
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-04-13 20:44:05 -0500

    value: enriched interface of poke

--- playground/mr/we/type/value/poke.hpp
+++ playground/mr/we/type/value/poke.hpp
@@ -11,10 +11,16 @@ namespace pnet
   {
     namespace value
     {
+      value_type& poke ( const std::list<std::string>::const_iterator&
+                       , const std::list<std::string>::const_iterator&
+                       , value_type&
+                       , const value_type&
+                       );
       value_type& poke ( const std::list<std::string>& path
                        , value_type& node
                        , const value_type& value
                        );
+      value_type& poke (const std::string&, value_type&, const value_type&);
     }
   }
 }
