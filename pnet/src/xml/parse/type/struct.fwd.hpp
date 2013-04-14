a15f45ec1f7beaab830303adb69082d4fff32d21
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-04-13 21:58:54 -0500

    xml: struct.fwd: delete commented definitions

--- pnet/src/xml/parse/type/struct.fwd.hpp
+++ pnet/src/xml/parse/type/struct.fwd.hpp
@@ -3,12 +3,6 @@
 #ifndef _XML_PARSE_TYPE_STRUCT_FWD_HPP
 #define _XML_PARSE_TYPE_STRUCT_FWD_HPP
 
-// #include <we/type/signature/types.hpp>
-
-// #include <list>
-
-// #include <boost/unordered/unordered_map_fwd.hpp>
-
 namespace xml
 {
   namespace parse
@@ -16,19 +10,10 @@ namespace xml
     namespace type
     {
       struct structure_type;
-
-      // typedef std::list<struct_t> structs_type;
     }
 
     namespace structure_type
     {
-      // typedef boost::unordered_map< signature::field_name_t
-      //                             , type::struct_t
-      //                             > set_type;
-      // typedef boost::unordered_map< signature::field_name_t
-      //                             , std::string
-      //                             > forbidden_type;
-
       class get_literal_type_name;
 
       class resolve;
