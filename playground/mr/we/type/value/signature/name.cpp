8c5209dc886f5949e6d854eb3988efa6b89ee794
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-04-14 00:10:31 -0500

    value: added signature/name and signature/of_type

---
+++ playground/mr/we/type/value/signature/name.cpp
@@ -0,0 +1,39 @@
+// mirko.rahn@itwm.fraunhofer.de
+
+#include <we/type/value/signature/name.hpp>
+
+namespace pnet
+{
+  namespace type
+  {
+    namespace value
+    {
+#define NAME(_name,_value)                      \
+      const std::string& _name()                \
+      {                                         \
+        static const std::string x (_value);    \
+                                                \
+        return x;                               \
+      }
+
+      NAME (CONTROL, "control");
+      NAME (BOOL, "bool");
+      NAME (INT, "int");
+      NAME (LONG, "long");
+      NAME (UINT, "uint");
+      NAME (ULONG, "ulong");
+      NAME (FLOAT, "float");
+      NAME (DOUBLE, "double");
+      NAME (CHAR, "char");
+      NAME (STRING, "string");
+      NAME (BITSET,"bitset");
+      NAME (BYTEARRAY, "bytearray");
+      NAME (LIST, "list");
+      NAME (VECTOR, "vector");
+      NAME (SET, "SET");
+      NAME (MAP, "map");
+
+#undef NAME
+    }
+  }
+}
---
+++ playground/mr/we/type/value/signature/name.hpp
@@ -0,0 +1,38 @@
+// mirko.rahn@itwm.fraunhofer.de
+
+#ifndef PNET_SRC_WE_TYPE_VALUE_SIGNATURE_NAME_HPP
+#define PNET_SRC_WE_TYPE_VALUE_SIGNATURE_NAME_HPP
+
+#include <string>
+
+namespace pnet
+{
+  namespace type
+  {
+    namespace value
+    {
+#define NAME(_name) const std::string& _name()
+
+      NAME (CONTROL);
+      NAME (BOOL);
+      NAME (INT);
+      NAME (LONG);
+      NAME (UINT);
+      NAME (ULONG);
+      NAME (FLOAT);
+      NAME (DOUBLE);
+      NAME (CHAR);
+      NAME (STRING);
+      NAME (BITSET);
+      NAME (BYTEARRAY);
+      NAME (LIST);
+      NAME (VECTOR);
+      NAME (SET);
+      NAME (MAP);
+
+#undef NAME
+    }
+  }
+}
+
+#endif
