aa7d7de8af6b76c0ada2f88d9c3a69a58f740508
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2013-04-18 09:28:45 +0200

    gscpnet: use get_option from option.hpp instead of local version

--- gspcnet/gspc/net/serve.cpp
+++ gspcnet/gspc/net/serve.cpp
@@ -8,6 +8,7 @@
 
 #include <fhg/util/url.hpp>
 
+#include <gspc/net/option.hpp>
 #include <gspc/net/limits.hpp>
 #include <gspc/net/server.hpp>
 
@@ -19,36 +20,6 @@ namespace gspc
 {
   namespace net
   {
-    typedef fhg::util::url_t::arg_map_t option_map_t;
-
-    template <typename T>
-    T s_get_option ( option_map_t const &opts
-                   , std::string const & key
-                   , T const &dflt
-                   , boost::system::error_code & ec
-                   )
-    {
-      using namespace boost::system;
-
-      option_map_t::const_iterator it = opts.find (key);
-      if (it != opts.end ())
-      {
-        try
-        {
-          return boost::lexical_cast<T>(it->second);
-        }
-        catch (boost::bad_lexical_cast const &)
-        {
-          ec = errc::make_error_code (errc::invalid_argument);
-          return dflt;
-        }
-      }
-      else
-      {
-        return dflt;
-      }
-    }
-
     template <typename Server>
     void s_set_options ( Server *server
                        , option_map_t const &opts
@@ -56,18 +27,18 @@ namespace gspc
                        )
     {
       server->set_queue_length
-        (s_get_option ( opts
-                      , "queue_length"
-                      , limits::max_pending_frames_per_connection ()
-                      , ec
-                      )
+        (get_option ( opts
+                    , "queue_length"
+                    , limits::max_pending_frames_per_connection ()
+                    , ec
+                    )
         );
       server->set_thread_pool_size
-        (s_get_option ( opts
-                      , "threads"
-                      , 1u
-                      , ec
-                      )
+        (get_option ( opts
+                    , "threads"
+                    , 1u
+                    , ec
+                    )
         );
     }
 
