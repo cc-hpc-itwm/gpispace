c7a85782b0d62c6fc938b8ba03b09a710533a5cf
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-04-23 22:40:59 -0500

    we: added exception.hpp and exception.cpp

---
+++ playground/mr/we/exception.cpp
@@ -0,0 +1,64 @@
+// mirko.rahn@itwm.fraunhofer.de
+
+#include <we/exception.hpp>
+
+#include <we/type/signature/show.hpp>
+#include <we/type/value/path/join.hpp>
+#include <we/type/value/show.hpp>
+
+namespace pnet
+{
+  namespace exception
+  {
+    type_error::type_error (const std::string& msg)
+      : std::runtime_error ((boost::format ("type error: %1%") % msg).str())
+    {}
+    type_error::type_error (const boost::format& f)
+      : std::runtime_error ((boost::format ("type error: %1%") % f).str())
+    {}
+    type_mismatch::type_mismatch
+    ( const type::signature::signature_type& signature
+    , const type::value::value_type& value
+    , const std::list<std::string>& path
+    )
+      : type_error
+        ( ( boost::format ( "type mismatch for field %2%: expected %1%"
+                          ", value has %3%"
+                          )
+          % "<<signature>>"
+          % type::value::path::join (path)
+          % "<<signature (value)>>"
+          ).str()
+        )
+      , _signature (signature)
+      , _value (value)
+      , _path (path)
+    {}
+    missing_field::missing_field
+    ( const type::signature::signature_type& signature
+    , const std::list<std::string>& path
+    )
+      : type_error
+        ( ( boost::format ("missing field %2% of type %1%")
+          % "<<signature>>"
+          % type::value::path::join (path)
+          ).str()
+        )
+      , _signature (signature)
+      , _path (path)
+    {}
+    unknown_field::unknown_field
+    ( const type::value::value_type& value
+    , const std::list<std::string>& path
+    )
+      : type_error
+        ( ( boost::format ("unknown field %1% with value %2%")
+          % type::value::path::join (path)
+          % "<<value>>"
+          ).str()
+        )
+      , _value (value)
+      , _path (path)
+    {}
+  }
+}
