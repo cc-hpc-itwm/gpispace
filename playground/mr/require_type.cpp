0ae8117901167ce71bc62fb686b389d4525a5b39
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-04-23 22:14:42 -0500

    we: added prototype, dummy implementation and test for require_type

---
+++ playground/mr/require_type.cpp
@@ -0,0 +1,12 @@
+// mirko.rahn@itwm.fraunhofer.de
+
+#define BOOST_TEST_MODULE pnet_require_type
+#include <boost/test/unit_test.hpp>
+
+#include <we/require_type.hpp>
+
+#include <sstream>
+
+BOOST_AUTO_TEST_CASE (signature_show)
+{
+}
---
+++ playground/mr/we/require_type.cpp
@@ -0,0 +1,11 @@
+// mirko.rahn@itwm.fraunhofer.de
+
+#include <we/require_type.hpp>
+
+namespace pnet
+{
+  void require_type ( const type::value::value_type& value
+                    , const type::signature::signature_type& signature
+                    )
+  {}
+}
