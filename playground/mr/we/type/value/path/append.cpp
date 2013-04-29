5b8179ae6a72d86a2591baf3ebfa774c4e8eac63
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-04-29 13:09:59 +0200

    value: path: added RAII append

---
+++ playground/mr/we/type/value/path/append.cpp
@@ -0,0 +1,27 @@
+// mirko.rahn@itwm.fraunhofer.de
+
+#include <we/type/value/path/append.hpp>
+
+namespace pnet
+{
+  namespace type
+  {
+    namespace value
+    {
+      namespace path
+      {
+        append::append ( std::list<std::string>& path
+                       , const std::string& key
+                       )
+          : _path (path)
+        {
+          _path.push_back (key);
+        }
+        append::~append()
+        {
+          _path.pop_back();
+        }
+      }
+    }
+  }
+}
---
+++ playground/mr/we/type/value/path/append.hpp
@@ -0,0 +1,34 @@
+// mirko.rahn@itwm.fraunhofer.de
+
+#ifndef PNET_SRC_WE_TYPE_VALUE_PATH_APPEND_HPP
+#define PNET_SRC_WE_TYPE_VALUE_PATH_APPEND_HPP
+
+#include <list>
+#include <string>
+
+namespace pnet
+{
+  namespace type
+  {
+    namespace value
+    {
+      namespace path
+      {
+        class append
+        {
+        public:
+          append (std::list<std::string>&, const std::string&);
+          ~append();
+          operator std::list<std::string>&() const
+          {
+            return _path;
+          }
+        private:
+          std::list<std::string>& _path;
+        };
+      }
+    }
+  }
+}
+
+#endif
