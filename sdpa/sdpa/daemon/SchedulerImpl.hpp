66e37f38a9d37517f14ad3f5b564db1f431d5f99
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2013-04-30 16:48:04 +0200

    scheduler: added alternative signature for getNextJob method

--- sdpa/sdpa/daemon/SchedulerImpl.hpp
+++ sdpa/sdpa/daemon/SchedulerImpl.hpp
@@ -75,6 +75,7 @@ namespace sdpa {
 	    void declare_jobs_failed( const Worker::worker_id_t&, Worker::JobQueue* pQueue );
 
 	    virtual void getWorkerList(sdpa::worker_id_list_t&);
+	    void getListWorkersNotFull(sdpa::worker_id_list_t& workerList);
 	    virtual Worker::worker_id_t getWorkerId(unsigned int rank);
 
 	    virtual void setLastTimeServed(const worker_id_t& wid, const sdpa::util::time_type& servTime);
@@ -86,6 +87,7 @@ namespace sdpa {
 	    virtual void getWorkerCapabilities(const sdpa::worker_id_t&, sdpa::capabilities_set_t& cpbset);
 
 	    virtual const sdpa::job_id_t getNextJob(const Worker::worker_id_t& worker_id, const sdpa::job_id_t &last_job_id) throw (NoJobScheduledException, WorkerNotFoundException);
+	    const sdpa::job_id_t getNextJob(const Worker::ptr_t ptrWorker, const sdpa::job_id_t &last_job_id) throw (NoJobScheduledException);
 	    virtual void deleteWorkerJob(const Worker::worker_id_t& worker_id, const sdpa::job_id_t &job_id ) throw (JobNotDeletedException, WorkerNotFoundException);
 
 	    virtual void acknowledgeJob(const Worker::worker_id_t& worker_id, const sdpa::job_id_t& job_id) throw(WorkerNotFoundException, JobNotFoundException);
