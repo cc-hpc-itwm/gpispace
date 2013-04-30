2cae660891273180b1a14532d6844179d00028a2
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-04-30 16:39:42 +0200

    we: added we/field

---
+++ playground/mr/we/field.hpp
@@ -0,0 +1,54 @@
+// mirko.rahn@itwm.fraunhofer.de
+
+#ifndef PNET_SRC_WE_FIELD_HPP
+#define PNET_SRC_WE_FIELD_HPP
+
+#include <we/exception.hpp>
+#include <we/type/signature.hpp>
+#include <we/type/value.hpp>
+
+#include <list>
+#include <string>
+
+namespace pnet
+{
+  class path
+  {
+  public:
+    path (const std::string&);
+    ~path();
+
+    const std::list<std::string>::const_iterator& key() const;
+    const std::list<std::string>::const_iterator& end() const;
+    const std::list<std::string>& operator() () const;
+
+  private:
+    const std::list<std::string>::const_iterator _key;
+    const std::list<std::string>::const_iterator _end;
+  };
+
+  const type::value::value_type& field ( const path&
+                                       , const type::value::value_type&
+                                       , const type::signature::signature_type&
+                                       );
+
+  template<typename T>
+    const T& field_as ( const path& p
+                      , const type::value::value_type& v
+                      , const type::signature::signature_type& signature
+                      )
+  {
+    const type::value::value_type& value (field (p, v, signature));
+
+    const T* x (boost::get<const T> (&value));
+
+    if (!x)
+    {
+      throw exception::type_mismatch (signature, value, p());
+    }
+
+    return *x;
+  }
+}
+
+#endif
