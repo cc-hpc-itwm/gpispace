831cb8578554b2d7a38bab3640fd43737f78479f
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-05-14 17:46:00 +0200

    ctrlui: code cosmetics

--- playground/bl/node_state_widget.cpp
+++ playground/bl/node_state_widget.cpp
@@ -1427,25 +1427,28 @@ try
   prefix::node_state_widget* node_widget
     (new prefix::node_state_widget (host, port, legend_widget, log, content));
 
-  QGroupBox* sort_box (new QGroupBox (QObject::tr ("sort"), sidebar));
+  content->setWidget (node_widget);
+  content->setWidgetResizable (true);
 
   {
-    QPushButton* sort_by_state
-      (new QPushButton (QObject::tr ("by state"), sort_box));
-    QPushButton* sort_by_name
-      (new QPushButton (QObject::tr ("by name"), sort_box));
+    QGroupBox* sort_box (new QGroupBox (QObject::tr ("sort"), sidebar));
 
-    node_widget->connect
-      (sort_by_state, SIGNAL (clicked()), SLOT (sort_by_state()));
-    node_widget->connect
-      (sort_by_name, SIGNAL (clicked()), SLOT (sort_by_name()));
+    {
+      QPushButton* sort_by_state
+        (new QPushButton (QObject::tr ("by state"), sort_box));
+      QPushButton* sort_by_name
+        (new QPushButton (QObject::tr ("by name"), sort_box));
 
-    QVBoxLayout* layout (new QVBoxLayout (sort_box));
-    layout->addWidget (sort_by_state);
-    layout->addWidget (sort_by_name);
-  }
+      node_widget->connect
+        (sort_by_state, SIGNAL (clicked()), SLOT (sort_by_state()));
+      node_widget->connect
+        (sort_by_name, SIGNAL (clicked()), SLOT (sort_by_name()));
 
-  {
+      QVBoxLayout* layout (new QVBoxLayout (sort_box));
+      layout->addWidget (sort_by_state);
+      layout->addWidget (sort_by_name);
+    }
+
     QPushButton* clear_log
       (new QPushButton (QObject::tr ("clear log"), sidebar));
     log->connect (clear_log, SIGNAL (clicked()), SLOT (clear()));
@@ -1461,11 +1464,6 @@ try
     layout->addStretch();
     layout->addWidget (follow_logging);
     layout->addWidget (clear_log);
-  }
-
-  {
-    content->setWidget (node_widget);
-    content->setWidgetResizable (true);
   }
 
   {
