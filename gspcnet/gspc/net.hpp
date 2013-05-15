04d103889910fae0d05fd56c656d97c2f2659707
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2013-05-15 16:30:30 +0200

    gspcnet: added header_util to top-level include

--- gspcnet/gspc/net.hpp
+++ gspcnet/gspc/net.hpp
@@ -6,6 +6,7 @@
 #include <gspc/net/frame.hpp>
 #include <gspc/net/frame_io.hpp>
 #include <gspc/net/frame_handler.hpp>
+#include <gspc/net/header_util.hpp>
 #include <gspc/net/serve.hpp>
 #include <gspc/net/server.hpp>
 #include <gspc/net/server/queue_manager.hpp>
