1b95f60401e9d337dcdd63410795df64b04d6ee4
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2013-05-22 18:44:15 +0200

    rif: added typedef for env_t

--- gspcrif/gspc/rif/types.hpp
+++ gspcrif/gspc/rif/types.hpp
@@ -3,13 +3,15 @@
 
 #include <string>
 #include <vector>
+#include <map>
 
 namespace gspc
 {
   namespace rif
   {
-    typedef std::string        arg_t;
-    typedef std::vector<arg_t> argv_t;
+    typedef std::string                        arg_t;
+    typedef std::vector<arg_t>                 argv_t;
+    typedef std::map<std::string, std::string> env_t;
   }
 }
 
