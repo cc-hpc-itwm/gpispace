9f3c07a4fde7a33e015fbcd580258b7f5d8e5a78
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2013-05-23 13:12:07 +0200

    rif: refactored pipe handling into pipe_t

---
+++ gspcrif/gspc/rif/pipe.cpp
@@ -0,0 +1,104 @@
+#include "pipe.hpp"
+
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <boost/system/system_error.hpp>
+
+namespace gspc
+{
+  namespace rif
+  {
+    pipe_t::pipe_t ()
+    {
+      m_fd [0] = m_fd [1] = -1;
+    }
+
+    pipe_t::~pipe_t ()
+    {
+      close_rd ();
+      close_wr ();
+    }
+
+    ssize_t pipe_t::read (void *buffer, size_t len)
+    {
+      return ::read (rd (), buffer, len);
+    }
+
+    ssize_t pipe_t::write (const void *buffer, size_t len)
+    {
+      return ::write (wr (), buffer, len);
+    }
+
+    int pipe_t::open (int flags, bool close_on_exec)
+    {
+      int rc;
+
+      rc = ::pipe (&m_fd[0]);
+      if (rc < 0)
+      {
+        return rc;
+      }
+
+      for (size_t i = 0 ; i < 2 ; ++i)
+      {
+
+        rc = ::fcntl (m_fd [i], F_SETFL, flags);
+        if (rc < 0)
+        {
+          int err = errno;
+          close ();
+          errno = err;
+          return rc;
+        }
+      }
+
+      if (close_on_exec)
+      {
+        for (size_t i = 0 ; i < 2 ; ++i)
+        {
+          rc = ::fcntl (m_fd [i], F_SETFD, FD_CLOEXEC);
+          if (rc < 0)
+          {
+            int err = errno;
+            close ();
+            errno = err;
+            return rc;
+          }
+        }
+      }
+
+      return 0;
+    }
+
+    int pipe_t::close ()
+    {
+      int rc = 0;
+      rc = close_rd () < 0 ? -1 : rc;
+      rc = close_wr () < 0 ? -1 : rc;
+      return rc;
+    }
+
+    int pipe_t::close_rd ()
+    {
+      int rc = 0;
+      if (m_fd [0] >= 0)
+      {
+        rc = ::close (m_fd [0]);
+        m_fd [0] = -1;
+      }
+      return rc;
+    }
+
+    int pipe_t::close_wr ()
+    {
+      int rc = 0;
+      if (m_fd [1] >= 0)
+      {
+        rc = ::close (m_fd [1]);
+        m_fd [1] = -1;
+      }
+      return rc;
+    }
+  }
+}
