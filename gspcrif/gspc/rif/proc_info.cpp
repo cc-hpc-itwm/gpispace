581f4c616da3fb715b4727c3209e924d0e343a2f
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2013-05-27 16:52:23 +0200

    rif: new: added proc_info implementation

---
+++ gspcrif/gspc/rif/proc_info.cpp
@@ -0,0 +1,26 @@
+#include "proc_info.hpp"
+
+#include "process.hpp"
+
+namespace gspc
+{
+  namespace rif
+  {
+    proc_info_t::proc_info_t ()
+      : m_proc (-1)
+      , m_pid (-1)
+      , m_argv ()
+      , m_env ()
+      , m_status ()
+    {}
+
+    void proc_info_t::assign_from (process_t const &p)
+    {
+      m_proc = p.id ();
+      m_pid = p.pid ();
+      m_argv = p.argv ();
+      m_env = p.env ();
+      m_status = p.status ();
+    }
+  }
+}
---
+++ gspcrif/gspc/rif/proc_info.hpp
@@ -0,0 +1,36 @@
+#ifndef GSPC_RIF_PROC_INFO_HPP
+#define GSPC_RIF_PROC_INFO_HPP
+
+#include <boost/optional.hpp>
+#include <gspc/rif/types.hpp>
+
+namespace gspc
+{
+  namespace rif
+  {
+    class process_t;
+
+    class proc_info_t
+    {
+    public:
+      proc_info_t ();
+      ~proc_info_t () {}
+
+      pid_t pid () const { return m_pid; }
+      proc_t id () const { return m_proc; }
+      argv_t const & argv () const { return m_argv; }
+      env_t const & env () const { return m_env; }
+      boost::optional<int> status () const { return m_status; }
+
+      void assign_from (process_t const &);
+    private:
+      proc_t m_proc;
+      pid_t  m_pid;
+      argv_t m_argv;
+      env_t  m_env;
+      boost::optional<int> m_status;
+    };
+  }
+}
+
+#endif
