c12848012abf267a127dfa307145d50523db50ee
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2013-06-26 15:24:23 +0200

    mapreduce: get rid of obsolete helper clas

--- application/mapreduce/util/types.hpp
+++ application/mapreduce/util/types.hpp
@@ -22,56 +22,8 @@ namespace mapreduce
   namespace util
   {
     typedef std::pair<std::string, std::string> key_val_pair_t;
-
-    /*class key_val_pair_t
-    {
-    public:
-
-    	key_val_pair_t(const std::string& inKey = "", const std::string& inVal = "")
-      	  : first(inKey),
-      	    second(inVal)
-      	  {}
-
-    	void print()
-    	{
-    		std::cout<<key()<<":"<<val();
-    	}
-
-    	template<class Archive>
-    	void serialize(Archive & ar, const unsigned int )
-    	{
-    		ar & first;
-    		ar & second;
-    	}
-
-    	std::string key() { return first; }
-    	std::string val() { return second; }
-
-    	std::string set_key(const std::string& key) { first=key; }
-    	std::string set_val(const std::string& val) { second=val; }
-
-    	std::string encode()
-    	{
-    		std::stringstream osstr;
-    		boost::archive::binary_oarchive ar(osstr);
-    		ar << *this;
-    		return osstr.str();
-    	}
-
-    	void decode (const std::string& strenc)
-    	{
-    		std::stringstream sstr(strenc);
-    		boost::archive::binary_iarchive ar(sstr);
-    		ar >> *this;
-    	}
-
-    	public: // make it later private
-
-    	std::string first;
-    	std::string second;
-    };*/
-
     typedef std::list<key_val_pair_t> list_key_val_pairs_t;
+    typedef std::vector<std::string> set_of_mapped_items_t;
   }
 }
 
