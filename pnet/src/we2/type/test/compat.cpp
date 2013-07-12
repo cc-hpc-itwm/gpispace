a1d9bcdc54ad963e8a034a960646238c697d9a89
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-07-12 16:53:34 +0200

    we2: complete the test for compat::we2we2

--- pnet/src/we2/type/test/compat.cpp
+++ pnet/src/we2/type/test/compat.cpp
@@ -32,11 +32,33 @@ BOOST_AUTO_TEST_CASE (we2we2)
   CHECK1 (bitsetofint::type());
   CHECK1 (bitsetofint::type().ins (23));
   {
+    bytearray::type ba;
+    CHECK1 (ba);
+    ba.push_back ('a');
+    CHECK1 (ba);
+  }
+  {
     std::list<pnet::type::value::value_type> l;
     ::literal::stack_type s;
     CHECK (s, l);
     l.push_back (23L);
     s.push_back (23L);
-    //    CHECK (s, l);
+    CHECK (s, l);
+  }
+  {
+    std::map<pnet::type::value::value_type, pnet::type::value::value_type> m2;
+    ::literal::map_type m;
+    CHECK (m, m2);
+    m[1] = 2;
+    m2[1L] = 2L;
+    CHECK (m, m2);
+  }
+  {
+    std::set<pnet::type::value::value_type> s2;
+    ::literal::set_type s;
+    CHECK (s, s2);
+    s.insert (1);
+    s2.insert (1L);
+    CHECK (s, s2);
   }
 }
