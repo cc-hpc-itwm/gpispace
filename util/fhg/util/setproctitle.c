31592513308af4414982c60d03cfe3c6aaba07de
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2013-07-26 11:57:30 +0200

    util: chg: better implementation of setproctitle

--- util/fhg/util/setproctitle.c
+++ util/fhg/util/setproctitle.c
@@ -4,24 +4,34 @@
 #include <malloc.h>
 #include <string.h>
 
-int setproctitle (const char *title, int argc, char *argv [])
+int setproctitle (const char *title, int ac, char *const av [])
 {
-  if (strcmp (argv [0], title) == 0)
-  {
-    return 0; // already changed
-  }
-  else
-  {
-    // create a copy of argv
-    char ** new_argv = (char **)malloc ( (argc+1) * sizeof (char*));
-    new_argv [0] = strdup (title);
-    int i = 0;
-    for (i = 1 ; i < argc ; ++i)
-    {
-      new_argv [i] = strdup (argv [i]);
-    }
-    new_argv [argc] = 0;
+  char exe [4096];
+  memset (exe, 0, sizeof(exe));
 
-    return execvp (argv [0], new_argv);
+  if (strcmp (title, av [0]) == 0)
+  {
+    return 0;
   }
+
+#ifdef __linux__
+  if (readlink ("/proc/self/exe", exe, sizeof(exe)-1) < 0)
+  {
+    strncpy (exe, av [0], sizeof (exe));
+  }
+#else
+# error "please implement 'setproctitle (title, argc, argv)' for your OS"
+#endif
+
+  // create a copy of argv, might reside on a read-only page
+  char ** new_argv = (char **)malloc ( (ac+1) * sizeof (char*));
+  int i = 0;
+  new_argv [0] = strdup (title);
+  for (i = 1; i < ac; ++i)
+  {
+    new_argv [i] = strdup (av [i]);
+  }
+  new_argv [i] = 0;
+
+  return execvp (exe, new_argv);
 }
