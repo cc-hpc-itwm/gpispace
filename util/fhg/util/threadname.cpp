e803e7684b00ffbcff36e75b3d0f2330262a4fbe
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-08-07 17:32:27 +0200

    avoid to name unused parameter

--- util/fhg/util/threadname.cpp
+++ util/fhg/util/threadname.cpp
@@ -6,34 +6,46 @@ namespace fhg
 {
   namespace util
   {
+#ifdef HAVE_PTHREAD_SETNAME
+
     int set_threadname (boost::thread & thrd, std::string const &name)
     {
-#ifdef HAVE_PTHREAD_SETNAME
       return pthread_setname_np ( thrd.native_handle ()
                                 , name.c_str ()
                                 );
-#else
-      return 0;
-#endif
     }
 
     int get_threadname (boost::thread & thrd, std::string &name)
     {
-      int rc = 0;
-
-#ifdef HAVE_PTHREAD_SETNAME
       char buf [128];
 
-      rc = pthread_getname_np ( thrd.native_handle ()
-                              , buf
-                              , sizeof (buf)
-                              );
-      buf [sizeof(buf) - 1] = 0;
-      if (rc == 0)
-        name = buf;
-#endif
+      const int ec (pthread_getname_np ( thrd.native_handle ()
+                                       , buf
+                                       , sizeof (buf)
+                                       )
+                   );
 
-      return rc;
+      buf [sizeof(buf) - 1] = '\0';
+
+      if (!ec)
+      {
+        name = buf;
+      }
+
+      return ec;
     }
+
+#else
+
+    int set_threadname (boost::thread &, std::string const &)
+    {
+      return 0;
+    }
+    int get_threadname (boost::thread & , std::string &)
+    {
+      return 0;
+    }
+
+#endif
   }
 }
