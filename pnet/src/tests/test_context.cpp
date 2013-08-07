6d73325d2c784bb853bc8ca06acb1a6d3588e9ce
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-08-07 11:17:14 +0200

    pnet: test_context: use new we2/value/type

--- pnet/src/tests/test_context.cpp
+++ pnet/src/tests/test_context.cpp
@@ -5,21 +5,26 @@
 
 #include <we/expr/eval/context.hpp>
 
-#include <we/type/value.hpp>
-#include <we/type/value/read.hpp>
-#include <we/type/value/show.hpp>
+#include <we2/type/value.hpp>
+#include <we2/type/value/read.hpp>
+
+#include <we2/type/compat.hpp>
 
 BOOST_AUTO_TEST_CASE (basic)
 {
   typedef expr::eval::context context_t;
+  typedef pnet::type::value::value_type value_type;
+
+  using pnet::type::value::read;
+  using pnet::type::compat::COMPAT;
 
   context_t c;
 
-  value::type x (value::read ("0L"));
-  value::type y (value::read ("[]"));
-  value::type z (value::read ("[x:=0.0,y:=\"a_string\"]"));
-  value::type z_x (value::read ("0.0"));
-  value::type z_y (value::read ("\"a_string\""));
+  value_type x (read ("0L"));
+  value_type y (read ("[]"));
+  value_type z (read ("struct [x:=0.0,y:=\"a_string\"]"));
+  value_type z_x (read ("0.0"));
+  value_type z_y (read ("\"a_string\""));
 
   std::list<std::string> key_z_x;
   key_z_x.push_back ("z");
@@ -41,11 +46,11 @@ BOOST_AUTO_TEST_CASE (basic)
   c.bind ("y", y);
   c.bind ("z", z);
 
-  BOOST_REQUIRE_EQUAL (c.value ("x"), x);
-  BOOST_REQUIRE_EQUAL (c.value ("y"), y);
-  BOOST_REQUIRE_EQUAL (c.value ("z"), z);
-  BOOST_REQUIRE_EQUAL (c.value (key_z_x), z_x);
-  BOOST_REQUIRE_EQUAL (c.value (key_z_y), z_y);
+  BOOST_REQUIRE (COMPAT (c.value ("x")) == x);
+  BOOST_REQUIRE (COMPAT (c.value ("y")) == y);
+  BOOST_REQUIRE (COMPAT (c.value ("z")) == z);
+  BOOST_REQUIRE (COMPAT (c.value (key_z_x)) == z_x);
+  BOOST_REQUIRE (COMPAT (c.value (key_z_y)) == z_y);
 
   try
   {
@@ -71,7 +76,7 @@ BOOST_AUTO_TEST_CASE (basic)
 
     c.bind ("z.x", pnet::type::value::value_type (z_x_new));
 
-    BOOST_REQUIRE_EQUAL (c.value (key_z_x), value::type (z_x_new));
+    BOOST_REQUIRE (COMPAT (c.value (key_z_x)) == value_type (z_x_new));
   }
 
   {
@@ -79,11 +84,11 @@ BOOST_AUTO_TEST_CASE (basic)
 
     c.bind ("z", pnet::type::value::value_type (z_new));
 
-    BOOST_REQUIRE_EQUAL (c.value ("z"), value::type (z_new));
+    BOOST_REQUIRE (COMPAT (c.value ("z")) == value_type (z_new));
   }
 
-  BOOST_REQUIRE_EQUAL (c.value ("x"), x);
-  BOOST_REQUIRE_EQUAL (c.value ("y"), y);
+  BOOST_REQUIRE (COMPAT (c.value ("x")) == x);
+  BOOST_REQUIRE (COMPAT (c.value ("y")) == y);
 
   try
   {
@@ -108,6 +113,10 @@ BOOST_AUTO_TEST_CASE (basic)
 BOOST_AUTO_TEST_CASE (reference)
 {
   typedef expr::eval::context context_t;
+  typedef pnet::type::value::value_type value_type;
+
+  using pnet::type::value::read;
+  using pnet::type::compat::COMPAT;
 
   context_t c;
 
@@ -115,17 +124,17 @@ BOOST_AUTO_TEST_CASE (reference)
   const std::string key_b ("b");
   const std::string key_c ("c");
 
-  const value::type value_a (0.0);
-  const value::type value_b = std::string("a_string");
-  const value::type value_c (value::read ("[x:=1,y:=[a:=2,b:=3]]"));
+  const value_type value_a (0.0);
+  const value_type value_b = std::string("a_string");
+  const value_type value_c (read ("struct [x:=1L,y:=struct [a:=2L,b:=3L]]"));
 
   c.bind_ref (key_a, value_a);
   c.bind_ref (key_b, value_b);
   c.bind_ref (key_c, value_c);
 
-  BOOST_REQUIRE_EQUAL (c.value (key_a), value_a);
-  BOOST_REQUIRE_EQUAL (c.value (key_b), value_b);
-  BOOST_REQUIRE_EQUAL (c.value (key_c), value_c);
+  BOOST_REQUIRE (COMPAT (c.value (key_a)) == value_a);
+  BOOST_REQUIRE (COMPAT (c.value (key_b)) == value_b);
+  BOOST_REQUIRE (COMPAT (c.value (key_c)) == value_c);
 
   std::list<std::string> key_c_x;
   key_c_x.push_back ("c");
@@ -139,7 +148,7 @@ BOOST_AUTO_TEST_CASE (reference)
   key_c_y_b.push_back ("y");
   key_c_y_b.push_back ("b");
 
-  BOOST_REQUIRE_EQUAL (c.value (key_c_x), value::type (1L));
-  BOOST_REQUIRE_EQUAL (c.value (key_c_y_a), value::type (2L));
-  BOOST_REQUIRE_EQUAL (c.value (key_c_y_b), value::type (3L));
+  BOOST_REQUIRE (COMPAT (c.value (key_c_x)) == value_type (1L));
+  BOOST_REQUIRE (COMPAT (c.value (key_c_y_a)) == value_type (2L));
+  BOOST_REQUIRE (COMPAT (c.value (key_c_y_b)) == value_type (3L));
 }
