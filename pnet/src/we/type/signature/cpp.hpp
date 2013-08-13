0d054868ce71a04a66984946e64b6974c5e9da87
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-08-13 16:12:17 +0200

    pnet: gen: make use of the new type when getting the neccessary includes

--- pnet/src/we/type/signature/cpp.hpp
+++ pnet/src/we/type/signature/cpp.hpp
@@ -3,14 +3,8 @@
 #ifndef _WE_TYPE_SIGNATURE_CPP_HPP
 #define _WE_TYPE_SIGNATURE_CPP_HPP
 
-#include <we/type/signature.hpp>
-
-#include <we/type/literal/cpp.hpp>
-
 #include <fhg/util/cpp/include.hpp>
 #include <fhg/util/cpp/include_guard.hpp>
-
-namespace cpp_util = fhg::util::cpp;
 
 #include <iostream>
 #include <string>
@@ -19,58 +13,14 @@ namespace cpp_util = fhg::util::cpp;
 #include <boost/unordered_set.hpp>
 
 #include <we2/type/signature/cpp.hpp>
+#include <we2/type/signature/names.hpp>
+#include <we2/type/signature/is_literal.hpp>
 #include <we2/type/compat.sig.hpp>
 
 namespace signature
 {
   namespace cpp
   {
-    namespace visitor
-    {
-      typedef boost::unordered_set<std::string> seen_type;
-
-      template<typename Stream>
-      class cpp_includes : public boost::static_visitor<void>
-      {
-      private:
-        Stream& os;
-        seen_type & seen;
-
-      public:
-        cpp_includes ( Stream& _os
-                     , seen_type & _seen
-                     )
-          : os (_os), seen (_seen)
-        {}
-
-        void operator () (const std::string & t) const
-        {
-          if (!literal::cpp::known (t))
-            {
-              if (seen.find (t) == seen.end())
-                {
-                  os << cpp_util::include ("pnetc/type/" + t + ".hpp");
-
-                  seen.insert (t);
-                }
-            }
-        }
-
-        void operator () (const structured_t & map) const
-        {
-          for ( structured_t::const_iterator field (map.begin())
-              ; field != map.end()
-              ; ++field
-              )
-            {
-              boost::apply_visitor ( cpp_includes (os, seen)
-                                   , field->second
-                                   );
-            }
-        }
-      };
-    }
-
     template<typename Stream>
       void cpp_header
       ( Stream& os
@@ -79,19 +29,25 @@ namespace signature
       , const boost::filesystem::path & defpath
       )
     {
-      os << cpp_util::include_guard::open ("PNETC_TYPE_" + n);
+      const pnet::type::signature::signature_type sig
+        (pnet::type::compat::COMPAT (s, n));
 
-      visitor::seen_type seen;
+      os << fhg::util::cpp::include_guard::open ("PNETC_TYPE_" + n);
 
-      boost::apply_visitor ( visitor::cpp_includes<Stream> (os, seen)
-                           , s.desc()
-                           );
+      const boost::unordered_set<std::string> names
+        (pnet::type::signature::names (sig));
 
-      os << pnet::type::signature::cpp::header_signature
-        (pnet::type::compat::COMPAT (s, n))
-         << std::endl;
+      BOOST_FOREACH (const std::string& tname, names)
+      {
+        if (!pnet::type::signature::is_literal (tname))
+        {
+          os << fhg::util::cpp::include ("pnetc/type/" + tname + ".hpp");
+        }
+      }
 
-      os << cpp_util::include_guard::close();
+      os << pnet::type::signature::cpp::header_signature (sig) << std::endl;
+
+      os << fhg::util::cpp::include_guard::close();
     }
 
     template<typename Stream>
@@ -102,13 +58,14 @@ namespace signature
       , const boost::filesystem::path & defpath
       )
     {
+      const pnet::type::signature::signature_type sig
+        (pnet::type::compat::COMPAT (s, n));
+
       os << "// defined in " << defpath << std::endl;
 
-      os << cpp_util::include ("pnetc/type/" + n + ".hpp");
+      os << fhg::util::cpp::include ("pnetc/type/" + n + ".hpp");
 
-      os << pnet::type::signature::cpp::impl_signature
-        (pnet::type::compat::COMPAT (s, n))
-         << std::endl;
+      os << pnet::type::signature::cpp::impl_signature (sig) << std::endl;
     }
   }
 }
