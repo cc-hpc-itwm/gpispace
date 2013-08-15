4373b730ae62c0c23e8c508642adda8a6038d7b6
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-08-15 12:53:57 +0200

    pnet: implement we2/sig/specialize

---
+++ pnet/src/we2/type/signature/apply.hpp
@@ -0,0 +1,84 @@
+// mirko.rahn@itwm.fraunhofer.de
+
+#ifndef PNET_SRC_WE_TYPE_SIGNATURE_APPLY_HPP
+#define PNET_SRC_WE_TYPE_SIGNATURE_APPLY_HPP
+
+#include <we2/type/signature.hpp>
+
+namespace pnet
+{
+  namespace type
+  {
+    namespace signature
+    {
+      template<typename P>
+        class apply_field_struct : public boost::static_visitor<>
+      {
+      public:
+        apply_field_struct (P p)
+          : _p (p)
+        {}
+
+        void operator() (std::pair<std::string, structure_type>& s) const
+        {
+          _p._field_struct (s);
+        }
+      private:
+        P _p;
+      };
+
+      template<typename P>
+        class apply_field : public boost::static_visitor<>
+      {
+      public:
+        apply_field (P p)
+          : _p (p)
+        {}
+
+        void operator() (std::pair<std::string, std::string>& f) const
+        {
+          _p._field (f);
+        }
+        void operator() (structured_type& s) const
+        {
+          boost::apply_visitor (apply_field_struct<P> (_p), s);
+        }
+      private:
+        P _p;
+      };
+
+      template<typename P>
+        class apply_struct : public boost::static_visitor<>
+      {
+      public:
+        apply_struct (P p)
+          : _p (p)
+        {}
+        void operator() (std::pair<std::string, structure_type>& s) const
+        {
+          _p._struct (s);
+        }
+      private:
+        P _p;
+      };
+
+      template<typename P>
+        void apply (P p, structured_type& structured)
+      {
+        boost::apply_visitor (apply_struct<P> (p), structured);
+      }
+      template<typename P>
+        void apply (P p, field_type& field)
+      {
+        boost::apply_visitor (apply_field<P> (p), field);
+      }
+      template<typename P>
+        void apply (P p, std::pair<std::string, structure_type>& s)
+      {
+        p._struct (s);
+      }
+    }
+  }
+}
+
+#endif
