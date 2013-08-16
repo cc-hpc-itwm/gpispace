2443b64ce18ef3f055eb39c3ca79634f61924601
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-08-16 13:41:05 +0200

    pnet: cleanup module_call.hpp

--- pnet/src/we/loader/module_call.hpp
+++ pnet/src/we/loader/module_call.hpp
@@ -1,57 +1,52 @@
 #ifndef SDPA_DAEMON_NRE_MODULE_CALL_HPP
 #define SDPA_DAEMON_NRE_MODULE_CALL_HPP 1
 
+#include <we/expr/eval/context.hpp>
 #include <we/loader/loader.hpp>
-#include <we/loader/types.hpp>
-
 #include <we/mgmt/type/activity.hpp>
-#include <we/type/module_call.hpp>
-#include <we/type/id.hpp>
-#include <we/type/port.hpp>
-
 #include <we/require_type.hpp>
+#include <we/type/id.hpp>
+#include <we/type/module_call.hpp>
+#include <we/type/port.hpp>
 
 #include <boost/foreach.hpp>
 
 namespace module
 {
-  static void call (we::loader::loader & loader, we::mgmt::type::activity_t & act, const we::type::module_call_t & module_call)
+  static void call ( we::loader::loader& loader
+                   , we::mgmt::type::activity_t& act
+                   , const we::type::module_call_t& module_call
+                   )
   {
-    // construct context
-    typedef we::loader::input_t context_t;
-    typedef we::mgmt::type::activity_t::output_t output_t;
-    typedef we::type::port_t port_t;
-    typedef we::type::transition_t::const_iterator port_iterator;
-
-    context_t context;
-
     typedef std::pair< pnet::type::value::value_type
                      , petri_net::port_id_type
-                     > tp_type;
+                     > token_on_port_type;
+    typedef std::pair< petri_net::port_id_type
+                     , we::type::port_t
+                     > port_by_id_type;
 
-    BOOST_FOREACH (const tp_type& tp, act.input())
+    expr::eval::context context;
+    expr::eval::context mod_output;
+
+    BOOST_FOREACH (const token_on_port_type& token_on_port, act.input())
     {
-      context.bind_ref ( act.transition().name_of_port (tp.second)
-                       , tp.first
+      context.bind_ref ( act.transition().name_of_port (token_on_port.second)
+                       , token_on_port.first
                        );
     }
 
-    we::loader::output_t mod_output;
-
     loader[module_call.module()] (module_call.function(), context, mod_output);
 
-    typedef std::pair<std::string, pnet::type::value::value_type> kv_type;
-
-    BOOST_FOREACH ( const we::type::transition_t::port_map_t::value_type& ip
+    BOOST_FOREACH ( const port_by_id_type& port_by_id
                   , act.transition().ports()
                   )
     {
-      const petri_net::port_id_type& port_id (ip.first);
-      const we::type::port_t& port (ip.second);
+      const petri_net::port_id_type& port_id (port_by_id.first);
+      const we::type::port_t& port (port_by_id.second);
 
       if (port.is_output())
       {
-        act.add_output ( output_t::value_type
+        act.add_output ( token_on_port_type
                          (pnet::require_type ( mod_output.value (port.name())
                                              , port.signature()
                                              , port.name()
