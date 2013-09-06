ac0a1160be76413a1c6f2c9552347bc666f6a65b
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2013-09-06 15:31:25 +0200

    util: new: added to_hex/from_hex converter functions

---
+++ util/fhg/util/hex.hpp
@@ -0,0 +1,15 @@
+#ifndef FHG_UTIL_HEX_HPP
+#define FHG_UTIL_HEX_HPP
+
+#include <string>
+
+namespace fhg
+{
+  namespace util
+  {
+    std::string to_hex (std::string const &);
+    std::string from_hex (std::string const &);
+  }
+}
+
+#endif
