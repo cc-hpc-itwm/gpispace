2db068fa61a37ea158d11949dcb6d36c598461bd
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-09-16 17:20:24 +0200

    sdpa: formatting, remove default-dtor, ..

--- sdpa/sdpa/daemon/NotificationEvent.hpp
+++ sdpa/sdpa/daemon/NotificationEvent.hpp
@@ -13,90 +13,95 @@
 #include <sstream>
 #include <string>
 
-namespace sdpa { namespace daemon {
-
-namespace
+namespace sdpa
 {
-  boost::optional<std::string> nice_name (const std::string& act)
-  try
+  namespace daemon
   {
-    const we::mgmt::type::activity_t activity (act);
+    namespace
+    {
+      boost::optional<std::string> nice_name (const std::string& act)
+      try
+      {
+        const we::mgmt::type::activity_t activity (act);
 
-    const we::type::module_call_t mod_call
-      (boost::get<we::type::module_call_t> (activity.transition().data()));
+        const we::type::module_call_t mod_call
+          (boost::get<we::type::module_call_t> (activity.transition().data()));
 
-    return mod_call.module() + ":" + mod_call.function();
-  }
-  catch (boost::bad_get const &)
-  {
-    return boost::none;
+        return mod_call.module() + ":" + mod_call.function();
+      }
+      catch (boost::bad_get const &)
+      {
+        return boost::none;
+      }
+    }
+
+    class NotificationEvent
+    {
+    public:
+      enum state_t
+      {
+        STATE_CREATED
+      , STATE_STARTED
+      , STATE_FINISHED
+      , STATE_FAILED
+      , STATE_CANCELLED
+      , STATE_MIN = STATE_CREATED
+      , STATE_MAX = STATE_CANCELLED
+      };
+
+      NotificationEvent
+        ( const std::string &source
+        , const std::string &activity_id
+        , const std::string &activity_name
+        , const state_t &activity_state
+        , const boost::optional<std::string>& activity_encoded = boost::none
+        )
+          : m_component (source)
+          , a_id_(activity_id)
+          , a_name_ ( activity_encoded
+                    ? nice_name (*activity_encoded).get_value_or (activity_name)
+                    : activity_name
+                    )
+          , a_state_(activity_state)
+      {}
+
+      NotificationEvent (const std::string encoded)
+      {
+        std::istringstream stream (encoded);
+        boost::archive::text_iarchive archive (stream);
+        archive & m_component;
+        archive & a_id_;
+        archive & a_name_;
+        archive & a_state_;
+      }
+      std::string encoded() const
+      {
+        std::ostringstream stream;
+        boost::archive::text_oarchive archive (stream);
+        archive & m_component;
+        archive & a_id_;
+        archive & a_name_;
+        archive & a_state_;
+        return stream.str();
+      }
+
+      const std::string &component() const { return m_component; }
+      std::string &component() { return m_component; }
+
+      const std::string &activity_id() const   { return a_id_; }
+      std::string &activity_id()               { return a_id_; }
+      const std::string &activity_name() const { return a_name_; }
+      std::string &activity_name()             { return a_name_; }
+      const state_t &activity_state() const      { return a_state_; }
+      state_t &activity_state()                  { return a_state_; }
+
+    private:
+      std::string m_component;
+      std::string a_id_;
+      std::string a_name_;
+      state_t     a_state_;
+    };
   }
 }
-
-  class NotificationEvent
-  {
-  public:
-    enum state_t
-    {
-      STATE_CREATED
-    , STATE_STARTED
-    , STATE_FINISHED
-    , STATE_FAILED
-    , STATE_CANCELLED
-    , STATE_MIN = STATE_CREATED
-    , STATE_MAX = STATE_CANCELLED
-    };
-
-    NotificationEvent( const std::string &source
-                     , const std::string &activity_id
-                     , const std::string &activity_name
-                     , const state_t &activity_state
-                     , const boost::optional<std::string>& activity_encoded = boost::none
-                     )
-      : m_component (source)
-      , a_id_(activity_id)
-      , a_name_ ( activity_encoded
-                ? nice_name (*activity_encoded).get_value_or (activity_name)
-                : activity_name
-                )
-      , a_state_(activity_state)
-    {}
-
-    NotificationEvent (const std::string encoded)
-    {
-      std::istringstream stream (encoded);
-      boost::archive::text_iarchive archive (stream);
-      archive & m_component;
-      archive & a_id_;
-      archive & a_name_;
-      archive & a_state_;
-    }
-    std::string encoded() const
-    {
-      std::ostringstream stream;
-      boost::archive::text_oarchive archive (stream);
-      archive & m_component;
-      archive & a_id_;
-      archive & a_name_;
-      archive & a_state_;
-      return stream.str();
-    }
-
-    const std::string &component() const { return m_component; }
-    std::string &component() { return m_component; }
-
-    const std::string &activity_id() const   { return a_id_; }
-    std::string &activity_id()               { return a_id_; }
-    const std::string &activity_name() const { return a_name_; }
-    std::string &activity_name()             { return a_name_; }
-    const state_t &activity_state() const      { return a_state_; }
-    state_t &activity_state()                  { return a_state_; }
-  private:
-    std::string m_component;
-    std::string a_id_;
-    std::string a_name_;
-    state_t     a_state_;
-  };
-}}
 
 #endif
--- sdpa/sdpa/daemon/NotificationService.hpp
+++ sdpa/sdpa/daemon/NotificationService.hpp
@@ -1,91 +1,70 @@
-/*
- * =====================================================================================
- *
- *       Filename:  NotificationService.hpp
- *
- *    Description:  implements a notication service
- *
- *        Version:  1.0
- *        Created:  11/18/2009 11:48:19 PM
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Alexander Petry (petry), alexander.petry@itwm.fraunhofer.de
- *        Company:  Fraunhofer ITWM
- *
- * =====================================================================================
- */
+// alexander.petry@itwm.fraunhofer.de, bernd.loerwald@itwm.fraunhofer.de
 
 #ifndef SDPA_UTIL_NOTIFICATION_SERVICE_HPP
-#define SDPA_UTIL_NOTIFICATION_SERVICE_HPP 1
+#define SDPA_UTIL_NOTIFICATION_SERVICE_HPP
 
 #include <fhglog/ThreadedAppender.hpp>
 #include <fhglog/remote/RemoteAppender.hpp>
-#include <sdpa/memory.hpp>
 
 #include <sdpa/daemon/Observer.hpp>
 
 #include <sdpa/daemon/NotificationEvent.hpp>
 
-#include <boost/archive/text_iarchive.hpp>
-#include <boost/archive/text_oarchive.hpp>
-
-namespace sdpa { namespace daemon {
-  template <class EventType>
-  class BasicNotificationService : public Observer
+namespace sdpa
+{
+  namespace daemon
   {
-  public:
-    typedef EventType event_t;
-
-    BasicNotificationService(const std::string &service, const std::string &destination_location)
-      : service_(service)
-      , m_destination_location (destination_location)
-    {}
-
-    void update(const boost::any &arg)
+    class NotificationService : public Observer
     {
-      try
+    public:
+      NotificationService ( const std::string& service
+                          , const std::string& destination_location
+                          )
+        : service_(service)
+        , m_destination_location (destination_location)
+      {}
+
+      virtual void update (const boost::any& arg)
       {
-        notify(boost::any_cast<event_t>(arg));
+        try
+        {
+          notify (boost::any_cast<NotificationEvent> (arg));
+        }
+        catch (const boost::bad_any_cast& ex)
+        {
+          DLOG(TRACE, "NotificationService: could not update: " << ex.what());
+        }
       }
-      catch (const boost::bad_any_cast &ex)
+      void notify (const NotificationEvent evt)
       {
-        DLOG(TRACE, "NotificationService: could not update: " << ex.what());
+        if (destination_)
+        {
+          destination_->append (FHGLOG_MKEVENT_HERE (TRACE, evt.encoded()));
+        }
       }
-    }
 
-    void open ()
-    {
-      destination_.reset (new fhg::log::ThreadedAppender
-                         (fhg::log::Appender::ptr_t
-                         (new fhg::log::remote::RemoteAppender( service_
-                                                              , m_destination_location
-                                                              )
-                         )));
-    }
+      void open()
+      {
+        destination_.reset ( new fhg::log::ThreadedAppender
+                             ( fhg::log::Appender::ptr_t
+                               ( new fhg::log::remote::RemoteAppender
+                                 (service_, m_destination_location)
+                               )
+                             )
+                           );
+      }
 
-    std::string location() { return m_destination_location; }
+      std::string location() const
+      {
+        return m_destination_location;
+      }
 
-    ~BasicNotificationService() {}
-
-    void notify(const event_t &evt)
-    {
-      if (destination_)
-        destination_->append(FHGLOG_MKEVENT_HERE(TRACE, encode(evt)));
-    }
-
-  private:
-    std::string encode(const event_t &evt) const
-    {
-      return evt.encoded();
-    }
-
-    std::string service_;
-    std::string m_destination_location;
-    fhg::log::Appender::ptr_t destination_;
-  };
-
-  typedef BasicNotificationService<NotificationEvent> NotificationService;
-}}
+    private:
+      std::string service_;
+      std::string m_destination_location;
+      fhg::log::Appender::ptr_t destination_;
+    };
+  }
+}
 
 #endif
