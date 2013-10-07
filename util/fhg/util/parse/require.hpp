dc0e674a2c7ff7c87922483ace1b8cd498c20a29
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-10-07 18:07:25 +0200

    util: add parse::require::{token,character,string,boolean}

---
+++ util/fhg/util/parse/require.hpp
@@ -0,0 +1,33 @@
+// bernd.loerwald@itwm.fraunhofer.de
+
+#ifndef FHG_UTIL_PARSE_REQUIRE_HPP
+#define FHG_UTIL_PARSE_REQUIRE_HPP
+
+#include <fhg/util/parse/position.hpp>
+
+namespace fhg
+{
+  namespace util
+  {
+    namespace parse
+    {
+      namespace require
+      {
+        //! \note skip spaces, require what
+        void token (fhg::util::parse::position&, const std::string& what);
+
+        //! \note skip spaces, single-tick-character ('x')
+        char character (fhg::util::parse::position&);
+
+        //! \note skip spaces, double-tick-string ("foobar")
+        std::string string (fhg::util::parse::position&);
+
+        //! \note skip spaces, '0' or 'false' or 'no' or 'off' == false,
+        //!                    '1' or 'on' or 'true' or 'yes' == true
+        bool boolean (fhg::util::parse::position&);
+      }
+    }
+  }
+}
+
+#endif
