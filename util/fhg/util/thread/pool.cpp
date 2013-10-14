dffb41bbe0c2bdc7fe0c6d02a6d68899e159f741
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2013-10-14 11:19:32 +0200

    util: new: lock the thread pool in start/stop

--- util/fhg/util/thread/pool.cpp
+++ util/fhg/util/thread/pool.cpp
@@ -28,6 +28,8 @@ namespace fhg
 
     void pool_t::start ()
     {
+      unique_lock lock (m_mutex);
+
       if (m_threads.size ())
         return;
 
@@ -46,6 +48,8 @@ namespace fhg
 
     void pool_t::stop ()
     {
+      unique_lock lock (m_mutex);
+
       m_stop = true;
 
       for (std::size_t i = 0 ; i != m_threads.size () ; ++i)
