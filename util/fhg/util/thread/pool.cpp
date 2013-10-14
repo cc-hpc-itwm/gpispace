04ad32db3d052ff335cb144958e2d592a73d8cdf
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2013-10-14 11:19:14 +0200

    util: chg: pool::stop() fixed iterate over thread list

--- util/fhg/util/thread/pool.cpp
+++ util/fhg/util/thread/pool.cpp
@@ -48,7 +48,7 @@ namespace fhg
     {
       m_stop = true;
 
-      for (std::size_t i = 0 ; i != m_nthread ; ++i)
+      for (std::size_t i = 0 ; i != m_threads.size () ; ++i)
       {
         m_threads [i]->interrupt ();
         m_threads [i]->join ();
