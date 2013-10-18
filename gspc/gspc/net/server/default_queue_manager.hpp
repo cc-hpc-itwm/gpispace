834d00e019e5b1ff647d364d49e4e58daa313808
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2013-10-18 16:55:56 +0200

    gspc: new: default queue manager added

---
+++ gspc/gspc/net/server/default_queue_manager.hpp
@@ -0,0 +1,17 @@
+#ifndef GSPC_NET_SERVER_DEFAULT_QUEUE_MANAGER_HPP
+#define GSPC_NET_SERVER_DEFAULT_QUEUE_MANAGER_HPP
+
+#include <gspc/net/server/queue_manager_fwd.hpp>
+
+namespace gspc
+{
+  namespace net
+  {
+    namespace server
+    {
+      queue_manager_t & default_queue_manager ();
+    }
+  }
+}
+
+#endif
