3c452979883dc2fc8e5a5025f5d08270bd684250
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2013-10-29 14:40:08 +0100

    sdpa: fixed indentation

--- sdpa/sdpa/engine/IWorkflowEngine.hpp
+++ sdpa/sdpa/engine/IWorkflowEngine.hpp
@@ -17,22 +17,23 @@ typedef we::type::requirement_t requirement_t;
 typedef std::list<requirement_t> requirement_list_t;
 typedef we::type::schedule_data schedule_data;
 
-struct job_requirements_t
+class job_requirements_t
 {
-	typedef requirement_list_t::const_iterator const_iterator;
+public:
+  typedef requirement_list_t::const_iterator const_iterator;
 
-	job_requirements_t(const requirement_list_t& r_list, const schedule_data& schedule_data) :
-		m_requirementList(r_list), m_scheduleData(schedule_data)
-	{
-	}
+  job_requirements_t(const requirement_list_t& r_list, const schedule_data& schedule_data)
+    : m_requirementList(r_list), m_scheduleData(schedule_data)
+  {}
 
-	void add(const requirement_t& req) {m_requirementList.push_back(req); }
+  void add(const requirement_t& req) {m_requirementList.push_back(req); }
 
-	long numWorkers() { return m_scheduleData.num_worker()?m_scheduleData.num_worker().get():1; }
-	const requirement_list_t& getReqList() const { return m_requirementList; }
+  long numWorkers() { return m_scheduleData.num_worker()?m_scheduleData.num_worker().get():1; }
+  const requirement_list_t& getReqList() const { return m_requirementList; }
+
 private:
-	requirement_list_t m_requirementList;
-	schedule_data m_scheduleData;
+  requirement_list_t m_requirementList;
+  schedule_data m_scheduleData;
 };
 
 #endif //IWORKFLOWENGINE_HPP
