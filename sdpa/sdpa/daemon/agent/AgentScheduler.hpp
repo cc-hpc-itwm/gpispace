67c62757a1e56913691a38e2ff51b2132d0d494d
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2013-10-29 16:08:19 +0100

    scheduler: fixed indentation

--- sdpa/sdpa/daemon/agent/AgentScheduler.hpp
+++ sdpa/sdpa/daemon/agent/AgentScheduler.hpp
@@ -26,39 +26,36 @@ using namespace sdpa::events;
 using namespace std;
 
 namespace sdpa {
-	namespace daemon {
-  class AgentScheduler : public SchedulerImpl {
+  namespace daemon {
+    class AgentScheduler : public SchedulerImpl
+    {
+    public:
+      AgentScheduler(sdpa::daemon::IAgent* pCommHandler = NULL,  bool use_request_model=true)
+        : SchedulerImpl(pCommHandler, use_request_model),
+          SDPA_INIT_LOGGER(pCommHandler?pCommHandler->name()+"::Scheduler":"Scheduler")
+      {}
 
-  public:
-	 AgentScheduler(sdpa::daemon::IAgent* pCommHandler = NULL,  bool use_request_model=true):
-	   SchedulerImpl(pCommHandler, use_request_model),
-	   SDPA_INIT_LOGGER(pCommHandler?pCommHandler->name()+"::Scheduler":"Scheduler")
-	{
-	}
+      virtual ~AgentScheduler()
+      {
+        try {
+            LOG(TRACE, "destructing SchedulerAgg");
+            stop();
+        }
+        catch (std::exception const & ex) {
+            LOG(ERROR, "could not stop SchedulerAgg: " << ex.what());
+        }
+      }
 
-	 virtual ~AgentScheduler()
-	 {
-		 try
-		 {
-			 LOG(TRACE, "destructing SchedulerAgg");
-			 stop();
-		 }
-		 catch (std::exception const & ex)
-		 {
-			 LOG(ERROR, "could not stop SchedulerAgg: " << ex.what());
-		 }
-	 }
+      friend class boost::serialization::access;
 
-	 friend class boost::serialization::access;
+      template <class Archive>
+      void serialize(Archive& ar, const unsigned int /* file_version */)
+      {
+        ar & boost::serialization::base_object<SchedulerImpl>(*this);
+      }
 
-	 template <class Archive>
-	 void serialize(Archive& ar, const unsigned int /* file_version */)
-	 {
-		 ar & boost::serialization::base_object<SchedulerImpl>(*this);
-	 }
-
-  private:
-	  SDPA_DECLARE_LOGGER();
+    private:
+      SDPA_DECLARE_LOGGER();
   };
 }}
 
