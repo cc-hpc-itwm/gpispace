e8d0d7b45f311cb3f1aac82eae46d10ca528118e
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-10-30 19:23:14 +0100

    sdpa: RequestJobEvent: whitespace cosmetics

--- sdpa/sdpa/events/RequestJobEvent.hpp
+++ sdpa/sdpa/events/RequestJobEvent.hpp
@@ -8,47 +8,54 @@
 
 #include <boost/serialization/base_object.hpp>
 
-namespace sdpa { namespace events {
-  class RequestJobEvent : public MgmtEvent
+namespace sdpa
+{
+  namespace events
   {
+    class RequestJobEvent : public MgmtEvent
+    {
     public:
       typedef sdpa::shared_ptr<RequestJobEvent> Ptr;
 
       RequestJobEvent()
-        : MgmtEvent("", "")
-        , last_job_id_(sdpa::job_id_t::invalid_job_id())
+        : MgmtEvent ("", "")
+        , _last_job_id (sdpa::job_id_t::invalid_job_id())
       {}
 
-      RequestJobEvent(const address_t &a_from
-                    , const address_t &a_to
-                    , const sdpa::job_id_t &a_job_id = sdpa::job_id_t::invalid_job_id())
-        : MgmtEvent(a_from, a_to)
-        , last_job_id_(a_job_id)
+      RequestJobEvent ( const address_t& a_from
+                      , const address_t& a_to
+                      , const sdpa::job_id_t& a_job_id
+                      = sdpa::job_id_t::invalid_job_id()
+                      )
+        : MgmtEvent (a_from, a_to)
+        , _last_job_id (a_job_id)
+      {}
+
+      const sdpa::job_id_t& last_job_id() const
       {
+        return _last_job_id;
       }
-
-      const sdpa::job_id_t & last_job_id() const { return last_job_id_; }
-
       std::string str() const
       {
         return "RequestJobEvent(" + last_job_id ().str () + ")";
       }
-
-      virtual void handleBy(EventHandler *handler)
+      virtual void handleBy (EventHandler* handler)
       {
-        handler->handleRequestJobEvent(this);
+        handler->handleRequestJobEvent (this);
       }
-    private:
-      sdpa::job_id_t last_job_id_;
 
-    friend class boost::serialization::access;
-    template <typename Archive>
-    void serialize (Archive& ar, const unsigned int)
-    {
-      ar & boost::serialization::base_object<MgmtEvent> (*this);
-      ar & last_job_id_;
-    }
-  };
-}}
+    private:
+      sdpa::job_id_t _last_job_id;
+
+      friend class boost::serialization::access;
+      template <typename Archive>
+      void serialize (Archive& ar, const unsigned int)
+      {
+        ar & boost::serialization::base_object<MgmtEvent> (*this);
+        ar & _last_job_id;
+      }
+    };
+  }
+}
 
 #endif
