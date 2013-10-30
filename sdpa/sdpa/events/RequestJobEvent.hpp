b625aec2ab13cb09109687436d2b261e9cacb46d
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-10-30 19:18:52 +0100

    sdpa: RequestJobEvent: delete long header

--- sdpa/sdpa/events/RequestJobEvent.hpp
+++ sdpa/sdpa/events/RequestJobEvent.hpp
@@ -1,20 +1,3 @@
-/*
- * =====================================================================================
- *
- *       Filename:  RequestJobEvent.hpp
- *
- *    Description:  RequestJobEvent
- *
- *        Version:  1.0
- *        Created:
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Dr. Tiberiu Rotaru, tiberiu.rotaru@itwm.fraunhofer.de
- *        Company:  Fraunhofer ITWM
- *
- * =====================================================================================
- */
 #ifndef SDPA_REQUESTJOBEVENT_HPP
 #define SDPA_REQUESTJOBEVENT_HPP 1
 
