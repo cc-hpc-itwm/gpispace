20ef700e5417abb9dd358398db76fb7def69bb05
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-10-30 19:38:08 +0100

    sdpa: CancelJobAckEvent: delete long header

--- sdpa/sdpa/events/CancelJobAckEvent.hpp
+++ sdpa/sdpa/events/CancelJobAckEvent.hpp
@@ -1,20 +1,3 @@
-/*
- * =====================================================================================
- *
- *       Filename:  CancelJobAckEvent.hpp
- *
- *    Description:  CancelJobAckEvent
- *
- *        Version:  1.0
- *        Created:
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Dr. Tiberiu Rotaru, tiberiu.rotaru@itwm.fraunhofer.de
- *        Company:  Fraunhofer ITWM
- *
- * =====================================================================================
- */
 #ifndef SDPA_CANCELJOBACKEVENT_HPP
 #define SDPA_CANCELJOBACKEVENT_HPP 1
 
