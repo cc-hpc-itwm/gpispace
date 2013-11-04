c56a71203eb77809f63b1845dac72636953c636f
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-11-04 17:16:11 +0100

    build: in Findasciidoc also search for a2x, the tool chain wrapper

--- modules/Findasciidoc.cmake
+++ modules/Findasciidoc.cmake
@@ -4,20 +4,32 @@
 # * ASCIIDOC_FOUND if asciidoc was found
 # * ASCIIDOC_EXECUTABLE the path to the 'asciidoc' binary
 # * ASCIIDOC_BINDIR the path containg the 'asciidoc' binary
-# * ASCIIDOC_VERSION the version of 'asciidoc', if found
+# * A2X_FOUND if asciidoc was found
+# * A2X_EXECUTABLE the path to the 'asciidoc' binary
+# * A2X_BINDIR the path containg the 'asciidoc' binary
+
+include (FindPackageHandleStandardArgs)
 
 find_program (ASCIIDOC_EXECUTABLE
   NAMES asciidoc
   PATH_SUFFIXES bin
 )
-
-include (FindPackageHandleStandardArgs)
 find_package_handle_standard_args (asciidoc
   DEFAULT_MSG ASCIIDOC_EXECUTABLE
 )
-
 mark_as_advanced (ASCIIDOC_EXECUTABLE)
-
 get_filename_component (ASCIIDOC_BINDIR
   ${ASCIIDOC_EXECUTABLE} PATH
+)
+
+find_program (A2X_EXECUTABLE
+  NAMES a2x
+  PATH_SUFFIXES bin
+)
+find_package_handle_standard_args (a2x
+  DEFAULT_MSG A2X_EXECUTABLE
+)
+mark_as_advanced (A2X_EXECUTABLE)
+get_filename_component (A2X_BINDIR
+  ${A2X_EXECUTABLE} PATH
 )
--- modules/doc.cmake
+++ modules/doc.cmake
@@ -4,7 +4,7 @@ include (car_cdr_macros)
 find_package (source_highlite QUIET)
 find_package (asciidoc QUIET)
 
-macro (ADD_DOC)
+macro (ADD_ASCIIDOC)
   parse_arguments (DOC
     "INPUT;OUTPUT;BACKEND"
     ""
@@ -20,7 +20,37 @@ macro (ADD_DOC)
               -b ${DOC_BACKEND}
               -o ${CMAKE_CURRENT_BINARY_DIR}/${DOC_OUTPUT}
               ${CMAKE_CURRENT_SOURCE_DIR}/${DOC_INPUT}
-      )
-    add_custom_target (${DOC_TARGET} DEPENDS ${DOC_OUTPUT})
+      DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${DOC_INPUT}
+    )
+    add_custom_target (${DOC_TARGET}
+      ALL
+      DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${DOC_OUTPUT}
+    )
+  endif()
+endmacro()
+
+macro (ADD_A2X)
+  parse_arguments (DOC
+    "INPUT;FORMAT;OPTION"
+    ""
+    ${ARGN}
+  )
+  car (DOC_TARGET ${DOC_DEFAULT_ARGS})
+
+  if (A2X_FOUND AND SOURCE_HIGHLITE_FOUND)
+    add_custom_command (
+      OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/a2x-html-${DOC_FORMAT}
+      COMMAND mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/a2x-html &&
+              ${A2X_EXECUTABLE}
+             -f ${DOC_FORMAT} ${DOC_OPTION}
+             --no-xmllint
+             --destination-dir=${CMAKE_CURRENT_BINARY_DIR}/a2x-html
+             ${CMAKE_CURRENT_SOURCE_DIR}/${DOC_INPUT}
+      DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${DOC_INPUT}
+    )
+    add_custom_target (${DOC_TARGET}
+      ALL
+      DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/a2x-html-${DOC_FORMAT}
+    )
   endif()
 endmacro()
