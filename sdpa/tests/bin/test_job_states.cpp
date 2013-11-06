41285dac0012768680dfe736ebbae887464f9615
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-11-06 15:22:16 +0100

    sdpa: use boost.test instead of self written checks

--- sdpa/tests/bin/test_job_states.cpp
+++ sdpa/tests/bin/test_job_states.cpp
@@ -1,51 +1,19 @@
-/*
- * =====================================================================================
- *
- *       Filename:  test_codec.cpp
- *
- *    Description:  tests the encoding/decoding of events
- *
- *        Version:  1.0
- *        Created:  10/30/2009 01:43:03 AM
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Alexander Petry (petry), alexander.petry@itwm.fraunhofer.de
- *        Company:  Fraunhofer ITWM
- *
- * =====================================================================================
- */
+#define BOOST_TEST_MODULE show_job_states
 
-#include <iostream>
+#include <boost/test/unit_test.hpp>
 
 #include <sdpa/job_states.hpp>
 
-#define TOSTR_(x) #x
-#define TOSTR(x) TOSTR_(x)
-
-#define CHECK_STATUS_TO_STR(s, txt)                                     \
-  if (sdpa::status::show ((s)) != txt)                                  \
+#define CHECK_SHOW(val, str)                                            \
+  BOOST_AUTO_TEST_CASE (val)                                            \
   {                                                                     \
-    std::cerr << "E: status check for: " << TOSTR(s) << " failed" << std::endl; \
-    std::cerr << "     expected: " << txt << std::endl;                 \
-    std::cerr << "          got: " << sdpa::status::show ((s)) << std::endl; \
-    ++errcount;                                                         \
-  }
+    BOOST_REQUIRE_EQUAL (sdpa::status::show (sdpa::status::val), str);  \
+  }                                                                     \
 
-int main(int, char **)
-{
-  int errcount;
-
-  errcount = 0;
-
-  CHECK_STATUS_TO_STR (sdpa::status::PENDING,  "SDPA::Pending");
-  CHECK_STATUS_TO_STR (sdpa::status::SUSPENDED, "SDPA::Suspended");
-  CHECK_STATUS_TO_STR (sdpa::status::RUNNING, "SDPA::Running");
-  CHECK_STATUS_TO_STR (sdpa::status::FINISHED, "SDPA::Finished");
-  CHECK_STATUS_TO_STR (sdpa::status::FAILED, "SDPA::Failed");
-  CHECK_STATUS_TO_STR (sdpa::status::CANCELED, "SDPA::Canceled");
-  CHECK_STATUS_TO_STR (sdpa::status::UNKNOWN, "SDPA::Unknown");
-  CHECK_STATUS_TO_STR (42, "Strange job state");
-
-  return errcount;
-}
+CHECK_SHOW (PENDING,  "SDPA::Pending")
+CHECK_SHOW (SUSPENDED, "SDPA::Suspended")
+CHECK_SHOW (RUNNING, "SDPA::Running")
+CHECK_SHOW (FINISHED, "SDPA::Finished")
+CHECK_SHOW (FAILED, "SDPA::Failed")
+CHECK_SHOW (CANCELED, "SDPA::Canceled")
+CHECK_SHOW (UNKNOWN, "SDPA::Unknown")
