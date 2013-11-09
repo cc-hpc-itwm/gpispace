506e3d06915d443dcb6ca76ea4328d7b6fec4559
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-11-09 12:26:43 +0100

    pnet: transition: delete unused ctor

--- pnet/src/we/type/transition.hpp
+++ pnet/src/we/type/transition.hpp
@@ -117,23 +117,6 @@ namespace we { namespace type {
       template <typename Type>
       transition_t ( const std::string& name
                    , Type const& typ
-                   , const std::string& _condition = "true"
-                   )
-        : name_ (name)
-        , data_ (typ)
-        , internal_ (detail::is_internal<Type>::value)
-        , condition_(_condition)
-        , outer_to_inner_()
-        , inner_to_outer_()
-        , ports_()
-        , port_id_counter_ (0)
-        , prop_()
-        , _requirements()
-      { }
-
-      template <typename Type>
-      transition_t ( const std::string& name
-                   , Type const& typ
                    , condition::type const& _condition
                    , bool intern
                    , const we::type::property::type& prop
