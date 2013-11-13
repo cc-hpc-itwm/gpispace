34934b8fd5baf515d0c2ca7e1aee768727aae8f0
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2013-11-13 11:08:36 +0100

    sdpa: added new events corresponding to the stalled/running states

---
+++ sdpa/sdpa/events/JobRunningEvent.hpp
@@ -0,0 +1,47 @@
+#ifndef SDPA_JOB_RUNNING_EVENT_HPP
+#define SDPA_JOB_RUNNING_EVENT_HPP 1
+
+#include <string>
+
+#include <sdpa/memory.hpp>
+#include <sdpa/types.hpp>
+#include <sdpa/events/JobEvent.hpp>
+
+namespace sdpa
+{
+  namespace events
+  {
+    class JobRunningEvent : public sdpa::events::JobEvent
+    {
+    public:
+      typedef sdpa::shared_ptr<JobEvent> Ptr;
+
+      JobRunningEvent ( const address_t& a_from
+               , const address_t& a_to
+               , const sdpa::job_id_t& a_job_id
+               )
+        : sdpa::events::JobEvent (a_from, a_to, a_job_id)
+      {}
+
+      std::string str() const
+      {
+        return "JobStalledEvent(" + job_id ().str () + ")";
+      }
+
+      virtual void handleBy (EventHandler* handler)
+      {
+        //handler->handleJobStalledEvent (this);
+      }
+
+    private:
+      friend class boost::serialization::access;
+      template <class Archive>
+      void serialize (Archive & ar, unsigned int)
+      {
+       ar & boost::serialization::base_object<JobEvent> (*this);
+      }
+    };
+  }
+}
+
+#endif
