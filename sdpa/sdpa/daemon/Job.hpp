5de3ca86fff9c3302d08c74216370e398eaeb643
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-11-13 12:44:10 +0100

    sdpa: let state be called canceling instead of cancelling

--- sdpa/sdpa/daemon/Job.hpp
+++ sdpa/sdpa/daemon/Job.hpp
@@ -59,7 +59,7 @@ namespace sdpa {
       struct Running :        public boost::msm::front::state<>{};
       struct Finished :       public boost::msm::front::state<>{};
       struct Failed :         public boost::msm::front::state<>{};
-      struct Cancelling : 	  public boost::msm::front::state<>{};
+      struct Canceling : 	  public boost::msm::front::state<>{};
       struct Cancelled :      public boost::msm::front::state<>{};
 
       struct MSMDispatchEvent {};
@@ -89,7 +89,7 @@ namespace sdpa {
         //      +---------------+-------------------------------------------+------------------+---------------------+-----
         a_row<  Running,    	sdpa::events::JobFinishedEvent,             Finished,       	&sm::action_job_finished>,
         a_row<  Running,    	sdpa::events::JobFailedEvent,               Failed,         	&sm::action_job_failed >,
-        _row<   Running,    	sdpa::events::CancelJobEvent,       		Cancelling>,
+        _row<   Running,    	sdpa::events::CancelJobEvent,       		Canceling>,
         _row<   Running,    	MSMRescheduleEvent,                 		Pending >,
         _row<   Running,	    MSMStalledEvent,        					Stalled >,
         //      +---------------+-------------------------------------------+-------------------+---------------------+-----
@@ -99,9 +99,9 @@ namespace sdpa {
         a_irow< Failed,     	sdpa::events::DeleteJobEvent,                                   &sm::action_delete_job >,
         _irow<  Failed,     	sdpa::events::RetrieveJobResultsEvent>,
         //      +---------------+-------------------------------------------+-------------------+---------------------+-----
-        _row<   Cancelling, 	sdpa::events::CancelJobAckEvent,     		Cancelled>,
-        a_row<  Cancelling, 	sdpa::events::JobFinishedEvent,      		Cancelled, 			&sm::action_job_finished>,
-        a_row<  Cancelling, 	sdpa::events::JobFailedEvent,               Cancelled, 			&sm::action_job_failed>,
+        _row<   Canceling, 	sdpa::events::CancelJobAckEvent,     		Cancelled>,
+        a_row<  Canceling, 	sdpa::events::JobFinishedEvent,      		Cancelled, 			&sm::action_job_finished>,
+        a_row<  Canceling, 	sdpa::events::JobFailedEvent,               Cancelled, 			&sm::action_job_failed>,
         //      +---------------+-------------------------------------------+-------------------+---------------------+-----
         a_irow< Cancelled,  	sdpa::events::DeleteJobEvent,                                	&sm::action_delete_job >,
         _irow<  Cancelled,  	sdpa::events::RetrieveJobResultsEvent>
