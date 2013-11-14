ca474a05b011bd1e7315bce2288657124fc8925b
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-11-14 17:34:21 +0100

    sdpa: not being able to send events to slave is a serious error and should not be ignored

--- sdpa/sdpa/daemon/GenericDaemon.cpp
+++ sdpa/sdpa/daemon/GenericDaemon.cpp
@@ -1133,22 +1133,8 @@ void GenericDaemon::sendEventToMaster(const sdpa::events::SDPAEvent::Ptr& pEvt)
 
 void GenericDaemon::sendEventToSlave(const sdpa::events::SDPAEvent::Ptr& pEvt)
 {
-  try {
-    to_slave_stage()->send(pEvt);
-    DLOG(TRACE, "Sent " <<pEvt->str()<<" to "<<pEvt->to());
-  }
-  catch(const QueueFull&)
-  {
-    DMLOG (WARN, "Could not send event. The queue is full!");
-  }
-  catch(const seda::StageNotFound& )
-  {
-    DMLOG (ERROR, "Stage "<<to_slave_stage()->name()<<" not found!");
-  }
-  catch(const std::exception& ex)
-  {
-    DMLOG (WARN, "Could not send event. Exception occurred: "<<ex.what());
-  }
+  to_slave_stage()->send(pEvt);
+  DLOG(TRACE, "Sent " <<pEvt->str()<<" to "<<pEvt->to());
 }
 
 Worker::ptr_t const & GenericDaemon::findWorker(const Worker::worker_id_t& worker_id ) const
