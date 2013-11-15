8b48a577b92724907a99be4f8baf2dbbf641f9c3
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-11-15 16:45:51 +0100

    sdoa: tests: add utils::client::cancel_job

--- sdpa/tests/sdpa/utils.hpp
+++ sdpa/tests/sdpa/utils.hpp
@@ -269,6 +269,25 @@ namespace utils
       throw retried_too_often ("delete_job");
     }
 
+    void cancel_job (sdpa::client::ClientApi::ptr_t c, const sdpa::job_id_t& id)
+    {
+      LOG (DEBUG, "Cancel job " << id);
+
+      for (int i (0); i < NMAXTRIALS; ++i)
+      {
+        try
+        {
+          return c->cancelJob (id);
+        }
+        catch (const sdpa::client::ClientException& ex)
+        {
+          LOG (DEBUG, ex.what());
+        }
+      }
+
+      throw retried_too_often ("cancel_job");
+    }
+
     namespace
     {
       struct shutdown_on_exit
@@ -311,6 +330,40 @@ namespace utils
         shutdown_on_exit _ (ptrCli);
 
         const sdpa::job_id_t job_id_user (submit_job (ptrCli, workflow));
+        wait_for_job_termination (ptrCli, job_id_user, boost::posix_time::seconds (1));
+        retrieve_job_results (ptrCli, job_id_user);
+        delete_job (ptrCli, job_id_user);
+      }
+      catch (const retried_too_often& ex)
+      {
+        BOOST_FAIL (ex.what());
+      }
+    }
+
+    void submit_job_and_cancel_and_wait_for_termination ( std::string workflow
+                                                        , std::string client_name
+                                                        , const orchestrator& orch
+                                                        )
+    {
+      std::vector<std::string> command_line;
+      command_line.push_back ("--orchestrator=" + orch.name());
+
+      try
+      {
+        sdpa::client::config_t config (sdpa::client::ClientApi::config());
+        config.parse_command_line (command_line);
+
+        sdpa::client::ClientApi::ptr_t ptrCli
+          ( sdpa::client::ClientApi::create ( config
+                                            , client_name
+                                            , client_name + ".apps.client.out"
+                                            )
+          );
+        ptrCli->configure_network( config );
+        shutdown_on_exit _ (ptrCli);
+
+        const sdpa::job_id_t job_id_user (submit_job (ptrCli, workflow));
+        cancel_job (ptrCli, job_id_user);
         wait_for_job_termination (ptrCli, job_id_user, boost::posix_time::seconds (1));
         retrieve_job_results (ptrCli, job_id_user);
         delete_job (ptrCli, job_id_user);
