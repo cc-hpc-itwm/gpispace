bcd6b23bd46dd9f6c3fb9603b208d6b0295b4cad
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-11-15 15:00:22 +0100

    sdpa: tests: formatting

--- sdpa/tests/sdpa/test_Capabilities.cpp
+++ sdpa/tests/sdpa/test_Capabilities.cpp
@@ -1,38 +1,14 @@
-/*
- * =====================================================================================
- *
- *       Filename:  test_Capabilities.cpp
- *
- *    Description:  test all components, each with a real gwes, using a real user client
- *
- *        Version:  1.0
- *        Created:
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Dr. Tiberiu Rotaru, tiberiu.rotaru@itwm.fraunhofer.de
- *        Company:  Fraunhofer ITWM
- *
- * =====================================================================================
- */
 #define BOOST_TEST_MODULE testCapabilities
 
-#include <boost/test/unit_test.hpp>
-#include "tests_config.hpp"
-#include <sdpa/daemon/orchestrator/Orchestrator.hpp>
-#include <sdpa/daemon/agent/AgentFactory.hpp>
-#include <sdpa/engine/IWorkflowEngine.hpp>
 #include "kvs_setup_fixture.hpp"
-
+#include "tests_config.hpp"
 #include <utils.hpp>
 
-namespace po = boost::program_options;
+#include <boost/test/unit_test.hpp>
 
-using namespace std;
+BOOST_GLOBAL_FIXTURE (KVSSetup)
 
-BOOST_GLOBAL_FIXTURE (KVSSetup);
-
-BOOST_AUTO_TEST_CASE( Test1 )
+BOOST_AUTO_TEST_CASE (Test1)
 {
   const std::string workflow
     (utils::require_and_read_file ("workflows/capabilities.pnet"));
@@ -68,7 +44,7 @@ BOOST_AUTO_TEST_CASE( Test1 )
   }
 }
 
-BOOST_AUTO_TEST_CASE( testCapabilities_NoMandatoryReq )
+BOOST_AUTO_TEST_CASE (testCapabilities_NoMandatoryReq)
 {
   const std::string workflow
     (utils::require_and_read_file ("workflows/capabilities_no_mandatory.pnet"));
