59b35dabc6b2aacfb954ed3943e8fb732e798174
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-11-15 15:57:27 +0100

    sdpa: tests: remove unused variables, class, functions, includes

--- sdpa/tests/sdpa/test_AgentsAndDrts.cpp
+++ sdpa/tests/sdpa/test_AgentsAndDrts.cpp
@@ -1,151 +1,14 @@
-/*
- * =====================================================================================
- *
- *       Filename:  test_AgentsAndDrts.cpp
- *
- *    Description:  test all components, each with a real gwes, using a real user client
- *
- *        Version:  1.0
- *        Created:
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Dr. Tiberiu Rotaru, tiberiu.rotaru@itwm.fraunhofer.de
- *        Company:  Fraunhofer ITWM
- *
- * =====================================================================================
- */
 #define BOOST_TEST_MODULE testAgentsAndDrts
 
-#include <utils.hpp>
-#include <boost/test/unit_test.hpp>
-#include "tests_config.hpp"
-#include <sdpa/daemon/orchestrator/Orchestrator.hpp>
-#include <sdpa/daemon/agent/AgentFactory.hpp>
-#include <sdpa/client/ClientApi.hpp>
-#include <sdpa/engine/EmptyWorkflowEngine.hpp>
-#include <tests/sdpa/CreateDrtsWorker.hpp>
 #include "kvs_setup_fixture.hpp"
+#include "tests_config.hpp"
+#include <utils.hpp>
 
-const int NMAXTRIALS=5;
-static int testNb = 0;
+#include <boost/test/unit_test.hpp>
 
-namespace po = boost::program_options;
+BOOST_GLOBAL_FIXTURE (KVSSetup)
 
-using namespace std;
-
-BOOST_GLOBAL_FIXTURE (KVSSetup);
-
-struct MyFixture
-{
-	MyFixture()
-			: m_nITER(1)
-			, m_sleep_interval(1000000)
-			, m_arrAggMasterInfo(1, MasterInfo("orchestrator_0"))
-	{
-
-		LOG(DEBUG, "Fixture's constructor called ...");
-	}
-
-	~MyFixture()
-	{
-		LOG(DEBUG, "Fixture's destructor called ...");
-
-		sstrOrch.str("");
-		sstrAgg.str("");
-
-		seda::StageRegistry::instance().stopAll();
-		seda::StageRegistry::instance().clear();
-	}
-
-	void run_client();
-
-	string read_workflow(string strFileName)
-	{
-		ifstream f(strFileName.c_str());
-		ostringstream os;
-		os.str("");
-
-    BOOST_REQUIRE (f.is_open());
-
-    char c;
-    while (f.get(c)) os<<c;
-    f.close();
-
-		return os.str();
-	}
-
-	int m_nITER;
-	int m_sleep_interval ;
-    std::string m_strWorkflow;
-
-	sdpa::master_info_list_t m_arrAggMasterInfo;
-
-	std::stringstream sstrOrch;
-	std::stringstream sstrAgg;
-
-	boost::thread m_threadClient;
-};
-
-void MyFixture::run_client()
-{
-	sdpa::client::config_t config = sdpa::client::ClientApi::config();
-
-	std::vector<std::string> cav;
-	cav.push_back("--orchestrator=orchestrator_0");
-	config.parse_command_line(cav);
-
-	std::ostringstream osstr;
-	osstr<<"sdpac_"<<testNb++;
-
-	sdpa::client::ClientApi::ptr_t ptrCli = sdpa::client::ClientApi::create( config, osstr.str(), osstr.str()+".apps.client.out" );
-	ptrCli->configure_network( config );
-
-	for( int k=0; k<m_nITER; k++ )
-	{
-		sdpa::job_id_t job_id_user;
-
-    LOG( DEBUG, "Submitting the following test workflow: \n"<<m_strWorkflow);
-    job_id_user = ptrCli->submitJob(m_strWorkflow);
-
-		LOG( DEBUG, "//////////JOB #"<<k<<"////////////");
-
-		std::string job_status = ptrCli->queryJob(job_id_user);
-		LOG( DEBUG, "The status of the job "<<job_id_user<<" is "<<job_status);
-
-		int nTrials = 0;
-		while( job_status.find("Finished") == std::string::npos &&
-			   job_status.find("Failed") == std::string::npos &&
-			   job_status.find("Canceled") == std::string::npos)
-		{
-			try {
-				job_status = ptrCli->queryJob(job_id_user);
-				LOG( DEBUG, "The status of the job "<<job_id_user<<" is "<<job_status);
-
-				boost::this_thread::sleep(boost::posix_time::seconds(1));
-			}
-			catch(const sdpa::client::ClientException& cliExc)
-			{
-        BOOST_REQUIRE_LT (nTrials, NMAXTRIALS);
-
-				boost::this_thread::sleep(boost::posix_time::seconds(1));
-			}
-		}
-
-    LOG( DEBUG, "User: retrieve results of the job "<<job_id_user);
-    ptrCli->retrieveResults(job_id_user);
-
-    LOG( DEBUG, "User: delete the job "<<job_id_user);
-    ptrCli->deleteJob(job_id_user);
-	}
-
-	ptrCli->shutdown_network();
-	ptrCli.reset();
-}
-
-BOOST_FIXTURE_TEST_SUITE( test_agents, MyFixture )
-
-BOOST_AUTO_TEST_CASE( testAgentsAndDrts1 )
+BOOST_AUTO_TEST_CASE (testAgentsAndDrts1)
 {
   const std::string workflow
     (utils::require_and_read_file ("workflows/transform_file.pnet"));
@@ -166,7 +29,7 @@ BOOST_AUTO_TEST_CASE( testAgentsAndDrts1 )
     (workflow, "sdpac", orchestrator);
 }
 
-BOOST_AUTO_TEST_CASE( testAgentsAndDrts2 )
+BOOST_AUTO_TEST_CASE (testAgentsAndDrts2)
 {
   const std::string workflow
     (utils::require_and_read_file ("workflows/transform_file.pnet"));
@@ -189,7 +52,7 @@ BOOST_AUTO_TEST_CASE( testAgentsAndDrts2 )
     (workflow, "sdpac", orchestrator);
 }
 
-BOOST_AUTO_TEST_CASE( testAgentsAndDrts3 )
+BOOST_AUTO_TEST_CASE (testAgentsAndDrts3)
 {
   const std::string workflow
     (utils::require_and_read_file ("workflows/transform_file.pnet"));
@@ -219,5 +82,3 @@ BOOST_AUTO_TEST_CASE( testAgentsAndDrts3 )
   utils::client::submit_job_and_wait_for_termination
     (workflow, "sdpac", orchestrator);
 }
-
-BOOST_AUTO_TEST_SUITE_END()
