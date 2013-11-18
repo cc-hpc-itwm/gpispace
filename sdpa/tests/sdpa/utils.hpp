cddcbecffc701bc30e38352fef64e73fcfb1ce82
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-11-18 14:43:12 +0100

    sdpa: tests: inline CreateDrtsWorker.hpp into utils.hpp

--- sdpa/tests/sdpa/utils.hpp
+++ sdpa/tests/sdpa/utils.hpp
@@ -3,13 +3,15 @@
 #ifndef SDPA_TEST_UTILS_HPP
 #define SDPA_TEST_UTILS_HPP
 
-#include <CreateDrtsWorker.hpp>
+#include "tests_config.hpp"
 
 #include <sdpa/client/ClientApi.hpp>
 #include <sdpa/daemon/agent/AgentFactory.hpp>
 #include <sdpa/daemon/orchestrator/Orchestrator.hpp>
+#include <sdpa/memory.hpp>
 
 #include <fhg/plugin/core/kernel.hpp>
+#include <fhg/plugin/plugin.hpp>
 
 #include <boost/bind.hpp>
 #include <boost/function.hpp>
@@ -135,6 +137,40 @@ namespace utils
         result.push_back (sdpa::MasterInfo (agent.name()));
       }
       return result;
+    }
+
+    sdpa::shared_ptr<fhg::core::kernel_t>
+      createDRTSWorker ( const std::string& drtsName
+                       , const std::string& masterName
+                       , const std::string& cpbList
+                       , const std::string& strModulesPath
+                       , const std::string& kvsHost
+                       , const std::string& kvsPort
+                       )
+    {
+      sdpa::shared_ptr<fhg::core::kernel_t> kernel(new fhg::core::kernel_t);
+      kernel->set_name (drtsName);
+
+      kernel->put ("plugin.kvs.host", kvsHost);
+      kernel->put ("plugin.kvs.port", kvsPort);
+
+      //see ~/.sdpa/configs/sdpa.rc
+
+      kernel->put ("plugin.drts.name", drtsName);
+      kernel->put ("plugin.drts.master", masterName);
+      kernel->put ("plugin.drts.backlog", "2");
+
+      if(!cpbList.empty())
+        kernel->put ("plugin.drts.capabilities", cpbList);
+
+      kernel->put ("plugin.wfe.library_path", strModulesPath);
+
+      kernel->load_plugin (TESTS_KVS_PLUGIN_PATH);
+      kernel->load_plugin (TESTS_WFE_PLUGIN_PATH);
+      kernel->load_plugin (TESTS_FVM_FAKE_PLUGIN_PATH);
+      kernel->load_plugin (TESTS_DRTS_PLUGIN_PATH);
+
+      return kernel;
     }
   }
 
