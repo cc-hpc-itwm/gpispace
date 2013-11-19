50c15f9104d6f435e16c7d819986a0c76d5795e1
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2013-11-19 16:28:49 +0100

    sdpa: removed method from the scheduler interface

--- sdpa/sdpa/daemon/scheduler/Scheduler.hpp
+++ sdpa/sdpa/daemon/scheduler/Scheduler.hpp
@@ -53,7 +53,6 @@ namespace daemon {
 
     virtual void getWorkerList(worker_id_list_t&) = 0;
     virtual Worker::worker_id_t getWorkerId(unsigned int rank) = 0;
-    virtual size_t numberOfWorkers() = 0;
 
     virtual bool addCapabilities(const sdpa::worker_id_t&, const sdpa::capabilities_set_t& cpbset) = 0;
     virtual void removeCapabilities(const sdpa::worker_id_t&, const sdpa::capabilities_set_t& cpbset) throw (WorkerNotFoundException) = 0;
