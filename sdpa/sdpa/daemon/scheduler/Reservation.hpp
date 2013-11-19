f959f823086aa05f29e7890f27665c5fb6ab1bb5
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2013-11-19 10:58:04 +0100

    sdpa: check if the worker exists  before storing a result, qualify types

--- sdpa/sdpa/daemon/scheduler/Reservation.hpp
+++ sdpa/sdpa/daemon/scheduler/Reservation.hpp
@@ -3,6 +3,7 @@
 #define SDPA_RESERVATION_HPP 1
 
 #include <boost/thread.hpp>
+#include <sdpa/types.hpp>
 
 namespace sdpa {
   namespace daemon {
@@ -10,31 +11,43 @@ namespace sdpa {
     {
     public:
       typedef enum {FINISHED, FAILED, CANCELED} result_type;
-      typedef std::map<worker_id_t, result_type> map_worker_result_t;
+      typedef std::map<sdpa::worker_id_t, result_type> map_worker_result_t;
 
-      Reservation(const job_id_t& job_id = "", const size_t& n=1) : m_job_id(job_id), m_capacity(n) {}
-      ~Reservation() {m_list_workers.clear(); m_map_worker_result.clear(); }
+      Reservation(const sdpa::job_id_t& job_id = "", const size_t& n=1) : m_job_id(job_id), m_capacity(n) {}
 
       size_t size() const { return m_list_workers.size(); }
-      void addWorker(const worker_id_t& wid) { m_list_workers.push_back(wid); }
-      void storeWorkerResult(const worker_id_t& wid, const result_type& result) {
-        map_worker_result_t::iterator it = m_map_worker_result.find(wid);
-        if( it != m_map_worker_result.end() ) {
-            it->second = result;
-        } else {
-            m_map_worker_result.insert(map_worker_result_t::value_type(wid, result));
+      void addWorker(const sdpa::worker_id_t& wid) { m_list_workers.push_back(wid); }
+
+      void storeWorkerResult(const sdpa::worker_id_t& wid, const result_type& result)
+      {
+        if(!hasWorker(wid))
+        {
+            std::string errMsg("No worker ");
+            errMsg+=wid;
+            errMsg+=" exists within the current reservation of the job ";
+            errMsg+=m_job_id;
+            throw std::runtime_error(errMsg);
+        }
+        else {
+          // take the rseult and store it!
+          map_worker_result_t::iterator it = m_map_worker_result.find(wid);
+          if( it != m_map_worker_result.end() ) {
+              it->second = result;
+          } else {
+              m_map_worker_result.insert(map_worker_result_t::value_type(wid, result));
+          }
         }
       }
 
-      void workerFinished(const worker_id_t& wid) {
+      void workerFinished(const sdpa::worker_id_t& wid) {
         storeWorkerResult(wid, FINISHED);
       }
 
-      void workerFailed(const worker_id_t& wid) {
+      void workerFailed(const sdpa::worker_id_t& wid) {
         storeWorkerResult(wid, FAILED);
       }
 
-      void workerCanceled(const worker_id_t& wid) {
+      void workerCanceled(const sdpa::worker_id_t& wid) {
         storeWorkerResult(wid, CANCELED);
       }
 
@@ -52,18 +65,18 @@ namespace sdpa {
       bool isEmpty() const { return m_list_workers.empty(); }
       bool acquired() const { return (size()==capacity()); }
 
-      worker_id_t headWorker() const { return m_list_workers.front(); }
-      bool hasWorker(const worker_id_t& wid) const { return find(m_list_workers.begin(), m_list_workers.end(), wid)!=m_list_workers.end(); }
-      worker_id_list_t getWorkerList() const { return m_list_workers; }
+      sdpa::worker_id_t headWorker() const { return m_list_workers.front(); }
+      bool hasWorker(const sdpa::worker_id_t& wid) const { return find(m_list_workers.begin(), m_list_workers.end(), wid)!=m_list_workers.end(); }
+      sdpa::worker_id_list_t getWorkerList() const { return m_list_workers; }
 
       size_t capacity() const { return m_capacity; }
 
-      job_id_t jobId() const { return m_job_id; }
+      sdpa::job_id_t jobId() const { return m_job_id; }
 
     private:
-      job_id_t m_job_id;
+      sdpa::job_id_t m_job_id;
       size_t m_capacity;
-      worker_id_list_t m_list_workers;
+      sdpa::worker_id_list_t m_list_workers;
       map_worker_result_t m_map_worker_result;
     };
   }
