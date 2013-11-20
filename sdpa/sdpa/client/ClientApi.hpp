06d371f2b9f5071b6d0f00d23f7fd10e47d59938
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-11-20 15:45:57 +0100

    sdpa: client: ClientApi now is nothing more than a typedef

--- sdpa/sdpa/client/ClientApi.hpp
+++ sdpa/sdpa/client/ClientApi.hpp
@@ -8,22 +8,7 @@ namespace sdpa
 {
   namespace client
   {
-    class ClientApi : Client
-    {
-    public:
-      using Client::config;
-
-      ClientApi (const config_t &cfg)
-        : Client (cfg)
-      {}
-
-      using Client::wait_for_terminal_state;
-      using Client::submitJob;
-      using Client::cancelJob;
-      using Client::queryJob;
-      using Client::deleteJob;
-      using Client::retrieveResults;
-    };
+    typedef Client ClientApi;
   }
 }
 
