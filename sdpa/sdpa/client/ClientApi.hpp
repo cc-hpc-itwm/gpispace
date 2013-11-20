1ac26ea2ff61a1b2eb3de22637161053148ac274
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-11-20 14:52:37 +0100

    sdpa: client: as ClientApi is only forwarding to a single member, just inherit privately and use those members

--- sdpa/sdpa/client/ClientApi.hpp
+++ sdpa/sdpa/client/ClientApi.hpp
@@ -12,56 +12,21 @@
 #include <boost/uuid/uuid_io.hpp>
 
 namespace sdpa { namespace client {
-  class ClientApi : boost::noncopyable {
+    class ClientApi : boost::noncopyable, Client
+  {
   public:
-    static config_t config()
-    {
-      return Client::config();
-    }
+    using Client::config;
 
     ClientApi (const config_t &cfg)
-      : pimpl (Client::ptr_t (new Client(cfg, "sdpac-" + boost::uuids::to_string (boost::uuids::random_generator()()))))
-    {
-    }
+      : Client (cfg, "sdpac-" + boost::uuids::to_string (boost::uuids::random_generator()()))
+    {}
 
-    sdpa::status::code wait_for_terminal_state (job_id_t id, job_info_t& job_info)
-    {
-      return pimpl->wait_for_terminal_state (id, job_info);
-    }
-
-    job_id_t submitJob(const job_desc_t &desc) throw (ClientException)
-    {
-      return pimpl->submitJob(desc);
-    }
-
-    void cancelJob(const job_id_t &jid) throw (ClientException)
-    {
-      return pimpl->cancelJob(jid);
-    }
-
-    status::code queryJob(const job_id_t &jid) throw (ClientException)
-    {
-      return pimpl->queryJob(jid);
-    }
-
-    status::code queryJob(const job_id_t &jid, job_info_t & status)
-    {
-      return pimpl->queryJob(jid, status);
-    }
-
-    void deleteJob(const job_id_t &jid) throw (ClientException)
-    {
-      return pimpl->deleteJob(jid);
-    }
-
-    result_t retrieveResults(const job_id_t &jid) throw (ClientException)
-    {
-      return pimpl->retrieveResults(jid);
-    }
-
-  private:
-
-    Client::ptr_t pimpl;
+    using Client::wait_for_terminal_state;
+    using Client::submitJob;
+    using Client::cancelJob;
+    using Client::queryJob;
+    using Client::deleteJob;
+    using Client::retrieveResults;
   };
 }}
 
