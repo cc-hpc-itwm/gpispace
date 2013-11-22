3c4a2fc97af17e1d922e292a22ff9f1df2c9b784
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-11-22 12:16:13 +0100

    seda, sdpa, fhglog: just use boost::shared_ptr instead of tr1 / boost::tr1 and macro switching

--- fhglog/fhglog/memory.hpp
+++ fhglog/fhglog/memory.hpp
@@ -1,18 +1,10 @@
 #ifndef FHGLOG_MEMORY_HPP
 #define FHGLOG_MEMORY_HPP 1
 
-#ifdef HAVE_CONFIG_H
-#include <fhglog/fhglog-config.hpp>
-#endif
-
-#if defined (USE_STL_TR1) && USE_STL_TR1 == 1
-#include <tr1/memory>
-#else
-#include <boost/tr1/memory.hpp>
-#endif
+#include <boost/shared_ptr.hpp>
 
 namespace fhg { namespace log {
-    using std::tr1::shared_ptr;
+    using boost::shared_ptr;
 }}
 
 #endif
