9faada063e0f6b90c5e4c8cad8f2348bde28dcd7
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-11-25 15:41:49 +0100

    sdpa: events: add macros for load/save_construct_data style serialization

--- sdpa/sdpa/events/Serialization.hpp
+++ sdpa/sdpa/events/Serialization.hpp
@@ -1,6 +1,39 @@
 #ifndef SDPA_EVENTS_SERIALIZATION_HPP
 #define SDPA_EVENTS_SERIALIZATION_HPP 1
 
+#define SAVE_TO_ARCHIVE(WHAT)                   \
+  _ARCHIVE << WHAT
+
+#define LOAD_FROM_ARCHIVE(TYPE, VARIABLE_NAME)  \
+  TYPE VARIABLE_NAME; _ARCHIVE >> VARIABLE_NAME
+
+
+#define SAVE_CONSTRUCT_DATA_DEF(TYPE, VARIABLE_NAME)                    \
+  template<class Archive> inline void save_construct_data               \
+    (Archive& _ARCHIVE, const TYPE* VARIABLE_NAME, const unsigned int)
+
+#define LOAD_CONSTRUCT_DATA_DEF(TYPE, VARIABLE_NAME)              \
+  template<class Archive> inline void load_construct_data         \
+    (Archive& _ARCHIVE, TYPE* VARIABLE_NAME, const unsigned int)
+
+
+#define SAVE_SDPAEVENT_CONSTRUCT_DATA(EVENT_VARIABLE)   \
+  SAVE_TO_ARCHIVE (EVENT_VARIABLE->from());             \
+  SAVE_TO_ARCHIVE (EVENT_VARIABLE->to())
+
+#define LOAD_SDPAEVENT_CONSTRUCT_DATA(FROM_VAR_NAME, TO_VAR_NAME)        \
+  LOAD_FROM_ARCHIVE (sdpa::events::SDPAEvent::address_t, FROM_VAR_NAME); \
+  LOAD_FROM_ARCHIVE (sdpa::events::SDPAEvent::address_t, TO_VAR_NAME)
+
+
+#define SAVE_JOBEVENT_CONSTRUCT_DATA(EVENT_VARIABLE)    \
+  SAVE_SDPAEVENT_CONSTRUCT_DATA (EVENT_VARIABLE);       \
+  SAVE_TO_ARCHIVE (EVENT_VARIABLE->job_id())
+
+#define LOAD_JOBEVENT_CONSTRUCT_DATA(FROM_VAR_NAME, TO_VAR_NAME, JOB_ID_VAR_NAME) \
+  LOAD_SDPAEVENT_CONSTRUCT_DATA (FROM_VAR_NAME, TO_VAR_NAME);           \
+  LOAD_FROM_ARCHIVE (sdpa::job_id_t, JOB_ID_VAR_NAME)
+
 #include <sdpa/events/CancelJobAckEvent.hpp>
 #include <sdpa/events/CancelJobEvent.hpp>
 #include <sdpa/events/CapabilitiesGainedEvent.hpp>
