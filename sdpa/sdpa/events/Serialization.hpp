dd410ae42738c27a7eb8a04f32118ec53a60faab
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-11-25 14:47:23 +0100

    sdpa: cosmetics: reorder includes

--- sdpa/sdpa/events/Serialization.hpp
+++ sdpa/sdpa/events/Serialization.hpp
@@ -1,40 +1,36 @@
 #ifndef SDPA_EVENTS_SERIALIZATION_HPP
 #define SDPA_EVENTS_SERIALIZATION_HPP 1
 
-#include <string>
-
-#include <boost/serialization/base_object.hpp>
-#include <boost/serialization/utility.hpp>
-#include <boost/serialization/list.hpp>
-#include <boost/serialization/map.hpp>
-#include <boost/serialization/assume_abstract.hpp>
-
-#include <sdpa/events/ErrorEvent.hpp>
-#include <sdpa/events/WorkerRegistrationEvent.hpp>
-#include <sdpa/events/WorkerRegistrationAckEvent.hpp>
-
-#include <sdpa/events/SubmitJobEvent.hpp>
-#include <sdpa/events/SubmitJobAckEvent.hpp>
-#include <sdpa/events/DeleteJobEvent.hpp>
-#include <sdpa/events/DeleteJobAckEvent.hpp>
-#include <sdpa/events/CancelJobEvent.hpp>
 #include <sdpa/events/CancelJobAckEvent.hpp>
-#include <sdpa/events/JobFailedEvent.hpp>
-#include <sdpa/events/JobFailedAckEvent.hpp>
-#include <sdpa/events/JobFinishedEvent.hpp>
-#include <sdpa/events/JobFinishedAckEvent.hpp>
-#include <sdpa/events/RetrieveJobResultsEvent.hpp>
-#include <sdpa/events/JobResultsReplyEvent.hpp>
-#include <sdpa/events/QueryJobStatusEvent.hpp>
-#include <sdpa/events/JobStatusReplyEvent.hpp>
+#include <sdpa/events/CancelJobEvent.hpp>
 #include <sdpa/events/CapabilitiesGainedEvent.hpp>
 #include <sdpa/events/CapabilitiesLostEvent.hpp>
-#include <sdpa/events/SubscribeEvent.hpp>
-#include <sdpa/events/SubscribeAckEvent.hpp>
+#include <sdpa/events/DeleteJobAckEvent.hpp>
+#include <sdpa/events/DeleteJobEvent.hpp>
+#include <sdpa/events/ErrorEvent.hpp>
+#include <sdpa/events/JobFailedAckEvent.hpp>
+#include <sdpa/events/JobFailedEvent.hpp>
+#include <sdpa/events/JobFinishedAckEvent.hpp>
+#include <sdpa/events/JobFinishedEvent.hpp>
+#include <sdpa/events/JobResultsReplyEvent.hpp>
 #include <sdpa/events/JobRunningEvent.hpp>
 #include <sdpa/events/JobStalledEvent.hpp>
-#include <boost/serialization/set.hpp>
+#include <sdpa/events/JobStatusReplyEvent.hpp>
+#include <sdpa/events/QueryJobStatusEvent.hpp>
+#include <sdpa/events/RetrieveJobResultsEvent.hpp>
+#include <sdpa/events/SubmitJobAckEvent.hpp>
+#include <sdpa/events/SubmitJobEvent.hpp>
+#include <sdpa/events/SubscribeAckEvent.hpp>
+#include <sdpa/events/SubscribeEvent.hpp>
+#include <sdpa/events/WorkerRegistrationAckEvent.hpp>
+#include <sdpa/events/WorkerRegistrationEvent.hpp>
 
+#include <boost/serialization/assume_abstract.hpp>
+#include <boost/serialization/base_object.hpp>
+#include <boost/serialization/list.hpp>
+#include <boost/serialization/map.hpp>
+#include <boost/serialization/set.hpp>
+#include <boost/serialization/utility.hpp>
 
 namespace boost
 {
