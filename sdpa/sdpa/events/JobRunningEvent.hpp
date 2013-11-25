473f63e576be08c5c4ca35d6c65690c7f09edfd7
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-11-25 16:52:16 +0100

    sdpa: events: serialize JobRunning|StalledEvent via construct_data

--- sdpa/sdpa/events/JobRunningEvent.hpp
+++ sdpa/sdpa/events/JobRunningEvent.hpp
@@ -16,10 +16,6 @@ namespace sdpa
     public:
       typedef sdpa::shared_ptr<JobEvent> Ptr;
 
-      JobRunningEvent()
-        : JobEvent ("", "", "")
-      {}
-
       JobRunningEvent ( const address_t& a_from
                , const address_t& a_to
                , const sdpa::job_id_t& a_job_id
@@ -36,15 +32,9 @@ namespace sdpa
       {
         handler->handleJobRunningEvent (this);
       }
-
-    private:
-      friend class boost::serialization::access;
-      template <class Archive>
-      void serialize (Archive & ar, unsigned int)
-      {
-       ar & boost::serialization::base_object<JobEvent> (*this);
-      }
     };
+
+    CONSTRUCT_DATA_DEFS_FOR_EMPTY_JOBEVENT_OVERLOAD (JobRunningEvent)
   }
 }
 
