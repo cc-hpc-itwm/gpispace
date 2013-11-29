910fcc3565abad5c002d0bb01abfff53b692c13e
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2013-11-29 10:31:44 +0100

    sdpa: tests: added new test for the case of a bad number of requested workers (see issue #226)

--- sdpa/tests/sdpa/test_Coallocation.cpp
+++ sdpa/tests/sdpa/test_Coallocation.cpp
@@ -56,3 +56,52 @@ BOOST_AUTO_TEST_CASE (testCoallocationWorkflow)
     , sdpa::status::FINISHED
     );
 }
+
+BOOST_AUTO_TEST_CASE (testBadCoallocationWorkflow)
+{
+  const std::string workflow
+    (utils::require_and_read_file ("workflows/coallocation_bad_test.pnet"));
+
+  const utils::orchestrator orchestrator
+    ("orchestrator_0", "127.0.0.1");
+  const utils::agent agent
+    ("agent_0", "127.0.0.1", orchestrator);
+
+  const utils::drts_worker worker_A_0
+    ( "drts_A_0", agent
+    , "A"
+    , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
+    , kvs_host(), kvs_port()
+    );
+  const utils::drts_worker worker_A_1
+    ( "drts_A_1", agent
+    , "A"
+    , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
+    , kvs_host(), kvs_port()
+    );
+
+  const utils::drts_worker worker_B_0
+    ( "drts_B_0", agent
+    , "B"
+    , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
+    , kvs_host(), kvs_port()
+    );
+  const utils::drts_worker worker_B_1
+    ( "drts_B_1", agent
+    , "B"
+    , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
+    , kvs_host(), kvs_port()
+    );
+  const utils::drts_worker worker_B_2
+    ( "drts_B_2", agent
+    , "B"
+    , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
+    , kvs_host(), kvs_port()
+    );
+
+  BOOST_REQUIRE_EQUAL
+    ( utils::client::submit_job_and_wait_for_termination_as_subscriber
+      (workflow, orchestrator)
+    , sdpa::status::FAILED
+    );
+}
