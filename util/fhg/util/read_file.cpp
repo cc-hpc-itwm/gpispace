4c3ceca6ad9e88bf241a9e7238bb6bb4326b941d
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-12-03 22:41:03 +0100

    util: simplify read_file, use istream_iterator

--- util/fhg/util/read_file.cpp
+++ util/fhg/util/read_file.cpp
@@ -2,6 +2,7 @@
 
 #include <fstream>
 #include <stdexcept>
+#include <iterator>
 
 namespace fhg
 {
@@ -15,26 +16,9 @@ namespace fhg
         throw std::runtime_error ("could not open: " + path);
       }
 
-      std::filebuf *pbuf = ifs.rdbuf ();
-
-      std::size_t fsize = pbuf->pubseekoff (0, ifs.end, ifs.in);
-      pbuf->pubseekpos (0, ifs.in);
-
-      if (fsize != (std::size_t)(-1))
-      {
-        char *buf = new char[fsize];
-        pbuf->sgetn (buf, fsize);
-
-        std::string s (buf, fsize);
-
-        delete [] buf;
-
-        return s;
-      }
-      else
-      {
-        throw std::runtime_error ("could not determine file-size: " + path);
-      }
+      return std::string ( std::istream_iterator<char> (ifs)
+                         , std::istream_iterator<char>()
+                         );
     }
   }
 }
