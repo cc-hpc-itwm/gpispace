21c288a37b5b07ff207cfa05b8a4f984e2d24d58
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-12-04 12:19:05 +0100

    fhgcom: tests: use BOOST_REQUIRE_EQUAL instead of BOOST_REQUIRE(==)

--- fhgcom/tests/test_message.cpp
+++ fhgcom/tests/test_message.cpp
@@ -8,7 +8,7 @@ BOOST_AUTO_TEST_CASE ( test_message_constr_default )
   using namespace fhg::com;
 
   message_t msg;
-  BOOST_REQUIRE ( msg.size () == 0 );
+  BOOST_REQUIRE_EQUAL ( msg.size (), 0 );
 }
 
 BOOST_AUTO_TEST_CASE ( test_message_constr_with_length )
@@ -16,7 +16,7 @@ BOOST_AUTO_TEST_CASE ( test_message_constr_with_length )
   using namespace fhg::com;
 
   message_t msg (10);
-  BOOST_REQUIRE ( msg.size() == 10 );
+  BOOST_REQUIRE_EQUAL ( msg.size(), 10 );
 }
 
 BOOST_AUTO_TEST_CASE ( test_message_constr_with_data )
@@ -25,7 +25,7 @@ BOOST_AUTO_TEST_CASE ( test_message_constr_with_data )
 
   const char * data = "Hello World!";
   message_t msg ( data, strlen(data)+1 );
-  BOOST_REQUIRE ( msg.size() == (strlen(data)+1) );
+  BOOST_REQUIRE_EQUAL ( msg.size(), (strlen(data)+1) );
 }
 
 BOOST_AUTO_TEST_CASE ( test_message_constr_copy )
@@ -33,11 +33,11 @@ BOOST_AUTO_TEST_CASE ( test_message_constr_copy )
   using namespace fhg::com;
   message_t msg1 ( "abc", 4 );
   message_t msg2 ( msg1 );
-  BOOST_REQUIRE ( msg1.size() == 4 );
-  BOOST_REQUIRE ( std::string(msg1.buf()) == "abc" );
+  BOOST_REQUIRE_EQUAL ( msg1.size(), 4 );
+  BOOST_REQUIRE_EQUAL ( std::string(msg1.buf()), "abc" );
 
-  BOOST_REQUIRE ( msg2.size() == 4 );
-  BOOST_REQUIRE ( std::string(msg2.buf()) == "abc" );
+  BOOST_REQUIRE_EQUAL ( msg2.size(), 4 );
+  BOOST_REQUIRE_EQUAL ( std::string(msg2.buf()), "abc" );
 }
 
 BOOST_AUTO_TEST_CASE ( test_message_assignment )
@@ -46,5 +46,5 @@ BOOST_AUTO_TEST_CASE ( test_message_assignment )
   message_t msg1;
   message_t msg2 ( 42 );
   msg1 = msg2;
-  BOOST_REQUIRE ( msg1.size() == 42 );
+  BOOST_REQUIRE_EQUAL ( msg1.size(), 42 );
 }
