49f55967c58e0c263c5486c5667309991261b05f
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-12-09 10:38:38 +0100

    build: remove outdated comments and windows-specific flags

--- modules/CompilerFlags.cmake
+++ modules/CompilerFlags.cmake
@@ -47,7 +47,6 @@ endif (${CMAKE_CXX_COMPILER_ID} MATCHES "GNU")
 # to avoid warnings when using gcc 4.5
 add_definitions ("-fno-strict-aliasing")
 
-if(NOT WIN32)
 if (${CMAKE_CXX_COMPILER_ID} MATCHES "GNU")
   include (CheckCXXSourceCompiles)
 
@@ -84,17 +83,7 @@ if (${CMAKE_CXX_COMPILER_ID} MATCHES "Clang")
   set(CMAKE_C_FLAGS   "${CFLAGS}   -fPIC ${FLAGS_WARNINGS} -ftemplate-depth=1024")
 endif()
 
-# TODO: we need to check the compiler here, gcc does not know about those flags, is this The Right Thing To Do (TM)?
 if (${CMAKE_CXX_COMPILER_ID} MATCHES "Intel")
   set(CMAKE_CXX_FLAGS "${CXXFLAGS} -wd191 -wd1170 -wd1292 -wd2196 -fPIC -fpic")
   set(CMAKE_C_FLAGS   "${CFLAGS}   -wd191 -wd1170 -wd1292 -wd2196 -fPIC -fpic")
-# why was that set, Alex, could you remember it? (10/2010)
-#  if (${CMAKE_CXX_COMPILER_MAJOR} GREATER 9)
-#    message(WARNING "adding __aligned__=ignored to the list of definitions")
-#    add_definitions("-D__aligned__=ignored")
-#  endif (${CMAKE_CXX_COMPILER_MAJOR} GREATER 9)
 endif (${CMAKE_CXX_COMPILER_ID} MATCHES "Intel")
-else(NOT WIN32)
-  set(CMAKE_CXX_FLAGS " /DWIN32 /D_WINDOWS /W3 /Zm1000 /EHsc /GR ")
-  set(CMAKE_CXX_FLAGS_RELEASE "/MD /O2 /Ob2 /D NDEBUG")
-endif(NOT WIN32)
