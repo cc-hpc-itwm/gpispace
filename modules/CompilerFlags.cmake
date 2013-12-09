95a1c2be723765aa6b64bb26c6856b600b477f8d
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-12-09 11:19:43 +0100

    build: remove dead code and properly format other

--- modules/CompilerFlags.cmake
+++ modules/CompilerFlags.cmake
@@ -47,41 +47,48 @@ endif()
 # to avoid warnings when using gcc 4.5
 add_definitions ("-fno-strict-aliasing")
 
+
+# other flags
+
+set (C_FLAGS "${C_FLAGS} -fpic -fPIC")
+
+set (C_FLAGS_RELEASE "${C_FLAGS_RELEASE} -O3")
+
 if (${CMAKE_CXX_COMPILER_ID} MATCHES "GNU")
-  set(CMAKE_CXX_FLAGS "$ENV{CXXFLAGS} -fpic -fPIC ${FLAGS_WARNING}")
-  set(CMAKE_C_FLAGS "$ENV{CFLAGS} -fpic -fPIC ${FLAGS_WARNING}")
+  set(C_FLAGS_RELEASE "${C_FLAGS_RELEASE} -fstack-protector-all")
 
-  set (CMAKE_REQUIRED_FLAGS "-Wno-ignored-qualifiers")
-  set (__CXX_FLAG_CHECK_SOURCE "int main () { return 0; }")
-  CHECK_CXX_SOURCE_COMPILES ("${__CXX_FLAG_CHECK_SOURCE}" W_NO_IGNORED_QUALIFIERS)
-  set (CMAKE_REQUIRED_FLAGS)
-  if (W_NO_IGNORED_QUALIFIERS)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-ignored-qualifiers")
-  endif (W_NO_IGNORED_QUALIFIERS)
+  set(C_FLAGS_DEBUG "${C_FLAGS_DEBUG} -O0")
+  set(C_FLAGS_DEBUG "${C_FLAGS_DEBUG} -ggdb")
+  set(C_FLAGS_DEBUG "${C_FLAGS_DEBUG} -fno-omit-frame-pointer")
 
-  # release flags
-  set(CMAKE_CXX_FLAGS_RELEASE "-O3 -fstack-protector-all ${FLAGS_WARNINGS}")
-  set(CMAKE_SHARED_LINKER_FLAGS_RELWITHDEBINFO_INIT
-    ${CMAKE_SHARED_LINKER_FLAGS_RELWITHDEBINFO_INIT} "-rdynamic")
+  set(C_FLAGS_PROFILE "${C_FLAGS_PROFILE} -O0")
+  set(C_FLAGS_PROFILE "${C_FLAGS_PROFILE} -ggdb")
+  set(C_FLAGS_PROFILE "${C_FLAGS_PROFILE} -pg")
+  set(C_FLAGS_PROFILE "${C_FLAGS_PROFILE} -fprofile-arcs")
+  set(C_FLAGS_PROFILE "${C_FLAGS_PROFILE} -ftest-coverage")
 
-  # debug flags
-  set(CMAKE_CXX_FLAGS_DEBUG
-      "-O0 -g -ggdb -fno-omit-frame-pointer ${FLAGS_WARNINGS}"
-     )
-  set(CMAKE_SHARED_LINKER_FLAGS_DEBUG_INIT ${CMAKE_SHARED_LINKER_FLAGS_DEBUG_INIT} "-rdynamic")
-
-  # gprof and gcov support
-  set(CMAKE_CXX_FLAGS_PROFILE
-      "-O0 -fprofile-arcs -ftest-coverage -pg -g -ggdb ${FLAGS_WARNINGS}"
-      CACHE STRING "Flags for Profile build"
-     )
+  # TODO: Are these required?!
+  set(CMAKE_SHARED_LINKER_FLAGS_RELWITHDEBINFO
+    "${CMAKE_SHARED_LINKER_FLAGS_RELWITHDEBINFO} -rdynamic")
+  set(CMAKE_SHARED_LINKER_FLAGS_DEBUG
+    "${CMAKE_SHARED_LINKER_FLAGS_DEBUG} -rdynamic")
 endif ()
+
 if (${CMAKE_CXX_COMPILER_ID} MATCHES "Clang")
-  set(CMAKE_CXX_FLAGS "${CXXFLAGS} -fPIC ${FLAGS_WARNINGS} -ftemplate-depth=1024")
-  set(CMAKE_C_FLAGS   "${CFLAGS}   -fPIC ${FLAGS_WARNINGS} -ftemplate-depth=1024")
+  set (C_FLAGS "${C_FLAGS} -ftemplate-depth=1024")
 endif()
 
-if (${CMAKE_CXX_COMPILER_ID} MATCHES "Intel")
-  set(CMAKE_CXX_FLAGS "${CXXFLAGS} ${FLAGS_WARNING} -fPIC -fpic")
-  set(CMAKE_C_FLAGS   "${CFLAGS}   ${FLAGS_WARNING} -fPIC -fpic")
-endif (${CMAKE_CXX_COMPILER_ID} MATCHES "Intel")
+
+# assemble
+
+set (C_FLAGS "${C_FLAGS} ${FLAGS_WARNING}")
+
+set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${C_FLAGS}")
+set (CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} ${C_FLAGS_DEBUG}")
+set (CMAKE_CXX_FLAGS_PROFILE "${CMAKE_CXX_FLAGS_PROFILE} ${C_FLAGS_PROFILE}")
+set (CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} ${C_FLAGS_RELEASE}")
+
+set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${C_FLAGS}")
+set (CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} ${C_FLAGS_DEBUG}")
+set (CMAKE_C_FLAGS_PROFILE "${CMAKE_C_FLAGS_PROFILE} ${C_FLAGS_PROFILE}")
+set (CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} ${C_FLAGS_RELEASE}")
