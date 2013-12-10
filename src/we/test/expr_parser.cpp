710ac034bb11fdeaf5181a65fb2f3f2b9185d49e
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-12-10 18:53:34 +0100

    we: test: inline timer.hpp to only use place

--- src/we/test/expr_parser.cpp
+++ src/we/test/expr_parser.cpp
@@ -8,12 +8,56 @@
 #include <we/type/value.hpp>
 #include <we/type/value/show.hpp>
 
-#include "timer.hpp"
+#include <sys/time.h>
+
+#include <iostream>
+#include <string>
 
 #include <iostream>
 #include <iomanip>
 
 #include <cstdlib>
+
+struct Timer_t
+{
+private:
+  double t;
+  const std::string msg;
+  const unsigned int k;
+  std::ostream & s;
+
+  double current_time()
+  {
+    struct timeval tv;
+
+    gettimeofday (&tv, NULL);
+
+    return (double(tv.tv_sec) + double (tv.tv_usec) * 1E-6);
+  }
+
+public:
+  explicit Timer_t ( const std::string & _msg
+                   , const unsigned int & _k = 1
+                   , std::ostream & _s = std::cout
+                   )
+    : t(-current_time())
+    , msg(_msg)
+    , k(_k)
+    , s(_s)
+  {}
+
+  ~Timer_t ()
+  {
+    t += current_time();
+
+    s << "time " << msg
+      << " [" << k << "]: "
+      << t
+      << " [" << t / double(k) << "]"
+      << " [" << double(k) / t << "]"
+      << std::endl;
+  }
+};
 
 #ifndef __APPLE__
 // malloc.h is deprecated on OSX.
