c1b4de84cf85640e9a91fe2dc806303d61e01a58
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-12-11 13:16:52 +0100

    mmgr: test: smap: check the output of print_max

--- src/mmgr/tests/smap.cpp
+++ src/mmgr/tests/smap.cpp
@@ -8,6 +8,10 @@
 
 #include <mmgr/smap.h>
 
+#include <iostream>
+#include <sstream>
+#include <string>
+
 static void
 get_atleast (const SMap_t sm, const Key_t key)
 {
@@ -61,27 +65,49 @@ smap_print_sorted (const SMap_t PCTree)
   smap_work_inorder (PCTree, &fPrint, NULL);
 }
 
-static Cut_t
-fPrintCut (const Key_t Key, const Value_t Value, const Level_t Level,
-           void *Pdat)
+namespace
 {
-  Level_t MaxLevel = *(PLevel_t) Pdat;
+  struct print_cut_state
+  {
+  public:
+    print_cut_state (Level_t max_level, std::ostream& os)
+      : _max_level (max_level)
+      , _os (os)
+    {}
 
-  for (Level_t i = 0; i < Level; ++i)
-    printf (" ");
+    Cut_t operator() ( const Key_t Key
+                     , const Value_t Value
+                     , const Level_t Level
+                     ) const
+    {
+      _os << std::string (Level, ' ') << Key << " " << Value << std::endl;
 
-  printf (FMT_Key_t " " FMT_Value_t "\n", Key, Value);
+      return (Level >= _max_level) ? Cut : NoCut;
+    }
 
-  return (Level >= MaxLevel) ? Cut : NoCut;;
-}
+  private:
+    Level_t _max_level;
+    std::ostream& _os;
+  };
 
+  Cut_t fPrintCut ( const Key_t Key
+                  , const Value_t Value
+                  , const Level_t Level
+                  , void* PDat
+                  )
+  {
+    return static_cast<print_cut_state*> (PDat)->operator() (Key, Value, Level);
+  }
 
-static void
-smap_print_max (const SMap_t sm, const Level_t MaxLevel)
-{
-  Level_t L = MaxLevel;
+  void smap_print_max ( std::ostream& os
+                      , const SMap_t sm
+                      , const Level_t MaxLevel
+                      )
+  {
+    print_cut_state s (MaxLevel, os);
 
-  smap_work_preorder (sm, &fPrintCut, &L);
+    smap_work_preorder (sm, &fPrintCut, &s);
+  }
 }
 
 BOOST_AUTO_TEST_CASE (smap)
@@ -169,7 +195,19 @@ BOOST_AUTO_TEST_CASE (smap)
   BOOST_REQUIRE_EQUAL (smap_ins (&sm, 1044, 44), False);
   BOOST_REQUIRE_EQUAL (smap_ins (&sm, 1013, 13), False);
 
-  smap_print_max (sm, 5);
+  {
+    std::ostringstream oss;
+    smap_print_max (oss, sm, 5);
+
+    BOOST_REQUIRE_EQUAL
+      ( "1023 23\n"
+        " 1013 13\n"
+        " 1099 99\n"
+        "  1044 44\n"
+      , oss.str()
+      );
+  }
+
   smap_print_sorted (sm);
   printf ("\n");
 
@@ -200,7 +238,77 @@ BOOST_AUTO_TEST_CASE (smap)
       dups += (was_there == True) ? 1 : 0;
     }
 
-  smap_print_max (sm, 5);
+  {
+    std::ostringstream oss;
+    smap_print_max (oss, sm, 5);
+
+    BOOST_REQUIRE_EQUAL
+      ("1018338111 0\n"
+       " 410317456 3\n"
+       "  156771579 10\n"
+       "   10861679 23\n"
+       "    5966934 54\n"
+       "     4463125 190\n"
+       "     10151106 152\n"
+       "    27304684 33\n"
+       "     11637943 226\n"
+       "     57765334 40\n"
+       "   206624788 15\n"
+       "    159055947 105\n"
+       "     157346747 1040\n"
+       "     177287748 134\n"
+       "    371612659 16\n"
+       "     224977038 29\n"
+       "     394083169 31\n"
+       "  976470493 6\n"
+       "   424377862 9\n"
+       "    423064182 26\n"
+       "     415820650 45\n"
+       "     423550893 455\n"
+       "    821559718 11\n"
+       "     644021682 12\n"
+       "     970921189 13\n"
+       "   986088075 71\n"
+       "    983101390 115\n"
+       "     979170834 250\n"
+       "     984812264 366\n"
+       "    990966349 149\n"
+       "     990425667 831\n"
+       "     996805036 284\n"
+       " 1710946552 1\n"
+       "  1162573936 4\n"
+       "   1096140465 76\n"
+       "    1042725381 126\n"
+       "     1038049013 146\n"
+       "     1087900298 147\n"
+       "    1117299594 87\n"
+       "     1113039640 242\n"
+       "     1148027986 107\n"
+       "   1443454879 5\n"
+       "    1311918041 39\n"
+       "     1249773945 55\n"
+       "     1393461975 66\n"
+       "    1504294775 18\n"
+       "     1470759563 36\n"
+       "     1536561341 21\n"
+       "  1876219691 2\n"
+       "   1780871119 37\n"
+       "    1779853520 88\n"
+       "     1747891044 123\n"
+       "     1780060076 99\n"
+       "    1867589939 44\n"
+       "     1801784273 52\n"
+       "     1874155639 192\n"
+       "   1936366940 7\n"
+       "    1888979153 20\n"
+       "     1880742071 58\n"
+       "     1935923590 32\n"
+       "    1988642125 8\n"
+       "     1985272747 17\n"
+       "     2050951043 22\n"
+      , oss.str()
+      );
+  }
 
   Size_t Size = smap_size (sm);
   Bytes = smap_memused (sm);
