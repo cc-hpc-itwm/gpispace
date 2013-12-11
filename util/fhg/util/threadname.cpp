eb1c0d2c0d935db124699e1014e86f8a9d469665
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-12-11 19:08:19 +0100

    util: delete unused function get_threadname

--- util/fhg/util/threadname.cpp
+++ util/fhg/util/threadname.cpp
@@ -15,33 +15,9 @@ namespace fhg
                                 );
     }
 
-    int get_threadname (boost::thread & thrd, std::string &name)
-    {
-      char buf [128];
-
-      const int ec (pthread_getname_np ( thrd.native_handle ()
-                                       , buf
-                                       , sizeof (buf)
-                                       )
-                   );
-
-      buf [sizeof(buf) - 1] = '\0';
-
-      if (!ec)
-      {
-        name = buf;
-      }
-
-      return ec;
-    }
-
 #else
 
     int set_threadname (boost::thread &, std::string const &)
-    {
-      return 0;
-    }
-    int get_threadname (boost::thread & , std::string &)
     {
       return 0;
     }
