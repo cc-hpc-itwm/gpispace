8f661c0151690aaf0fa54674df9880baef43ab21
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-12-12 22:14:21 +0100

    we: mgmt: delete exception.hpp

--- src/we/mgmt/layer.hpp
+++ src/we/mgmt/layer.hpp
@@ -6,7 +6,6 @@
 #include <we/mgmt/bits/descriptor.hpp>
 #include <we/mgmt/bits/execution_policy.hpp>
 #include <we/mgmt/bits/signal.hpp>
-#include <we/mgmt/exception.hpp>
 #include <we/mgmt/type/activity.hpp>
 #include <we/type/id.hpp>
 #include <we/type/net.hpp>
