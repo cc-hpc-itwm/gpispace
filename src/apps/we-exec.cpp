25924e17f2c8cccc797d7e5d5ec5ef1608b7b943
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-12-12 11:54:43 +0100

    we: we-exec: do not name locks, also add const to them

--- src/apps/we-exec.cpp
+++ src/apps/we-exec.cpp
@@ -133,24 +133,24 @@ namespace
 
     we::mgmt::layer::id_type gen_id()
     {
-      boost::unique_lock<boost::recursive_mutex> lock (mutex_);
+      boost::unique_lock<boost::recursive_mutex> const _ (mutex_);
       return boost::lexical_cast<std::string> (++_id);
     }
     void add_mapping ( const we::mgmt::layer::id_type& old_id
                      , const we::mgmt::layer::id_type& new_id
                      )
     {
-      boost::unique_lock<boost::recursive_mutex> lock (mutex_);
+      boost::unique_lock<boost::recursive_mutex> const _ (mutex_);
       id_map_[new_id] = old_id;
     }
     we::mgmt::layer::id_type get_mapping (const we::mgmt::layer::id_type& id)
     {
-      boost::unique_lock<boost::recursive_mutex> lock (mutex_);
+      boost::unique_lock<boost::recursive_mutex> const _ (mutex_);
       return id_map_.at (id);
     }
     void del_mapping (const we::mgmt::layer::id_type& id)
     {
-      boost::unique_lock<boost::recursive_mutex> lock (mutex_);
+      boost::unique_lock<boost::recursive_mutex> const _ (mutex_);
       id_map_.erase (id);
     }
 
