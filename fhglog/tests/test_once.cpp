38646095c49f37a42362dcc08d211736076e2f43
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-12-16 12:59:51 +0100

    fhglog: tests: use Boost.Test in once

--- fhglog/tests/test_once.cpp
+++ fhglog/tests/test_once.cpp
@@ -1,87 +1,25 @@
 // alexander.petry@itwm.fraunhofer.de
 
-#include <sstream> // ostringstream
-#include <vector>
+#define BOOST_TEST_MODULE once
+#include <boost/test/unit_test.hpp>
 
 #include <fhglog/fhglog.hpp>
-#include <fhglog/StreamAppender.hpp>
-#include <fhglog/format.hpp>
 
-class VectorAppender : public fhg::log::Appender
+#include <tests/utils.hpp>
+
+BOOST_FIXTURE_TEST_CASE (only_log_once_in_50, utils::logger_with_minimum_log_level)
 {
-public:
-  typedef std::vector<std::string> container_type;
+  const std::size_t N (50);
 
-  VectorAppender(const std::string &a_name, container_type & vec, const std::string & fmt)
-    : fhg::log::Appender(a_name)
-    , vec_ (vec)
-    , fmt_(fmt)
-  {}
+  std::size_t messages_logged (0);
+  log.addAppender ( fhg::log::Appender::ptr_t
+                    (new utils::counting_appender (&messages_logged))
+                  );
 
-  void append(const fhg::log::LogEvent &evt)
+  for (size_t i (0); i < N; ++i)
   {
-    vec_.push_back (fhg::log::format(fmt_, evt));
+    FHGLOG_DO_ONCE (LLOG (INFO, log, "iteration #" << i));
   }
 
-  const container_type & list () const
-  {
-    return vec_;
-  }
-
-  void flush () {}
-private:
-  container_type & vec_;
-  std::string fmt_;
-};
-
-static int test_once (const size_t N)
-{
-  using namespace fhg::log;
-
-  logger_t log(getLogger());
-  log.removeAllAppenders();
-  log.setLevel (LogLevel::MIN_LEVEL);
-
-  VectorAppender::container_type vec;
-  log.addAppender(Appender::ptr_t(new VectorAppender("vectorappender", vec, "%m")));
-
-  {
-    std::clog << "** testing appending only once " << N << " events...";
-    for (size_t i(0); i < N; ++i)
-    {
-      FHGLOG_DO_ONCE(LLOG(INFO, log, "iteration #" << i));
-    }
-    if (vec.size() != 1)
-    {
-      std::clog << "FAILED!" << std::endl;
-      std::clog << "\tmessages logged: " << vec.size() << std::endl;
-      std::clog << "\texpected: " << 1 << std::endl;
-      return 1;
-    }
-    else
-    {
-      std::clog << "OK!" << std::endl;
-    }
-    vec.clear();
-  }
-
-  log.removeAllAppenders();
-  return 0;
-}
-
-int main (int , char **)
-{
-  using namespace fhg::log;
-
-  int errcount(0);
-  logger_t log(getLogger());
-  log.removeAllAppenders();
-  log.setLevel (LogLevel::MIN_LEVEL);
-
-  VectorAppender::container_type vec;
-  log.addAppender(Appender::ptr_t(new VectorAppender("vectorappender", vec, "%m")));
-
-  errcount += test_once (50);
-
-  return errcount;
+  BOOST_REQUIRE_EQUAL (messages_logged, 1);
 }
