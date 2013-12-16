42fe321f6ca96f0aa5cc5edac514d8982960907b
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-12-16 15:14:30 +0100

    fhglog: tests: don't use a logger in synchronized_appender

--- fhglog/tests/synchronized_appender.cpp
+++ fhglog/tests/synchronized_appender.cpp
@@ -3,9 +3,7 @@
 #define BOOST_TEST_MODULE synchronized_appender
 #include <boost/test/unit_test.hpp>
 
-#include <fhglog/fhglog.hpp>
 #include <fhglog/SynchronizedAppender.hpp>
-#include <fhglog/NullAppender.hpp>
 
 #include <boost/foreach.hpp>
 #include <boost/ptr_container/ptr_vector.hpp>
@@ -18,30 +16,28 @@ namespace
   const std::size_t thread_count (100);
   const std::size_t message_count (1000);
 
-  void worker (fhg::log::logger_t* logger)
+  void worker (fhg::log::SynchronizedAppender* appender)
   {
     for (std::size_t i (0); i < message_count; ++i)
     {
-      logger->log (FHGLOG_MKEVENT_HERE (INFO, "hello"));
+      appender->append (FHGLOG_MKEVENT_HERE (ERROR, "hello"));
     }
   }
 }
 
-BOOST_FIXTURE_TEST_CASE (synchronized_appender, utils::logger_with_minimum_log_level)
+BOOST_AUTO_TEST_CASE (synchronized_appender)
 {
   std::size_t messages_logged (0);
-  log.addAppender ( fhg::log::Appender::ptr_t
-                    ( new fhg::log::SynchronizedAppender
-                      (new utils::counting_appender (&messages_logged))
-                    )
-                  );
+
+  fhg::log::SynchronizedAppender appender
+    (new utils::counting_appender (&messages_logged));
 
   {
     boost::ptr_vector<boost::thread> threads;
 
     for (std::size_t i (0); i < thread_count; ++i)
     {
-      threads.push_back (new boost::thread (worker, &log));
+      threads.push_back (new boost::thread (&worker, &appender));
     }
 
     BOOST_FOREACH (boost::thread& thread, threads)
