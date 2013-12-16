ab7abf975c93b427e4bcf3f34e3362a462d3fd3b
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-12-16 13:48:32 +0100

    fhglog: tests: use Boost.Test in remote_logging

--- fhglog/tests/remote_logging.cpp
+++ fhglog/tests/remote_logging.cpp
@@ -1,5 +1,8 @@
 // alexander.petry@itwm.fraunhofer.de
 
+#define BOOST_TEST_MODULE remote_logging
+#include <boost/test/unit_test.hpp>
+
 #include <sstream> // ostringstream
 #include <fhglog/fhglog.hpp>
 #include <fhglog/StreamAppender.hpp>
@@ -7,91 +10,58 @@
 #include <fhglog/remote/RemoteAppender.hpp>
 #include <fhglog/remote/LogServer.hpp>
 
-boost::asio::io_service io_service;
-
-class StopAppender : public fhg::log::Appender
+namespace
 {
-public:
-  StopAppender(boost::asio::io_service & s)
-    : fhg::log::Appender("stop")
-    , service_(s)
-  {}
-
-  void append(const fhg::log::LogEvent &)
+  class StopAppender : public fhg::log::Appender
   {
-    service_.stop();
-  }
-  void flush () {}
-  boost::asio::io_service & service_;
-};
+  public:
+    StopAppender(boost::asio::io_service & s)
+      : fhg::log::Appender("stop")
+      , service_(s)
+    {}
 
-int main ()
+    void append(const fhg::log::LogEvent &)
+    {
+      service_.stop();
+    }
+    void flush () {}
+    boost::asio::io_service & service_;
+  };
+}
+
+BOOST_AUTO_TEST_CASE (log_to_fake_remote_stream)
 {
-  using namespace fhg::log;
-
-  int errcount(0);
-
-  std::string message("hello server!");
-  std::string server(FHGLOG_DEFAULT_LOCATION);
-
-  logger_t log(getLogger());
-  logger_t rem(getLogger("remote"));
-
-  log.addAppender(Appender::ptr_t(new StreamAppender("console", std::clog, "%m%n")));
-  rem.addAppender(Appender::ptr_t(new remote::RemoteAppender("remote", server)));
-
-  CompoundAppender::ptr_t compound(new CompoundAppender("compound"));
+  fhg::log::CompoundAppender::ptr_t compound
+    (new fhg::log::CompoundAppender ("compound"));
 
   std::ostringstream logstream;
-  Appender::ptr_t appender
-    (new StreamAppender( "stringstream"
-                       , logstream
-                       , "%m"
-                       )
-    );
-  compound->addAppender (appender);
-  compound->addAppender (Appender::ptr_t(new StopAppender (io_service)));
+  compound->addAppender
+    (fhg::log::Appender::ptr_t (new fhg::log::StreamAppender ("s", logstream, "%m")));
 
+  boost::asio::io_service io_service;
+  compound->addAppender
+    (fhg::log::Appender::ptr_t (new StopAppender (io_service)));
   fhg::log::shared_ptr<fhg::log::remote::LogServer> logd
-    (new fhg::log::remote::LogServer ( compound
-                                     , io_service
-                                     , FHGLOG_DEFAULT_PORT
-                                     )
+    (new fhg::log::remote::LogServer (compound, io_service, FHGLOG_DEFAULT_PORT));
+
+  boost::thread service_thread
+    (boost::bind (&boost::asio::io_service::run, &io_service));
+
+  fhg::log::remote::RemoteAppender appender ("remote", FHGLOG_DEFAULT_LOCATION);
+  appender.append (FHGLOG_MKEVENT_HERE (ERROR, "hello server!"));
+
+  if (service_thread.joinable())
+  {
+    service_thread.join();
+  }
+
+  BOOST_REQUIRE_EQUAL (logstream.str(), "hello server!");
+}
+
+BOOST_AUTO_TEST_CASE (throw_with_unknown_host)
+{
+  BOOST_REQUIRE_THROW
+    ( fhg::log::remote::RemoteAppender ("remote", "unknown-host")
+    , std::runtime_error
     );
-  {
-    std::clog << "** testing remote logging...";
-    rem.log(FHGLOG_MKEVENT_HERE(ERROR, message));
-    io_service.run();
-    const std::string msg (logstream.str());
-    if (msg == "hello server!")
-    {
-      std::clog << "OK!" << std::endl;
-    }
-    else
-    {
-      std::clog << "FAILED!" << std::endl;
-      std::clog << "   expected: \"hello server!\"" << std::endl;
-      std::clog << "     actual: \"" << msg << "\"" << std::endl;
-      ++errcount;
-    }
-  }
-
-  {
-    std::clog << "** testing remote appender with illegal host...";
-    try
-    {
-      Appender::ptr_t remote_appender(new remote::RemoteAppender("remote", "unknown-host"));
-      remote_appender->append (FHGLOG_MKEVENT_HERE(ERROR, "BUMMER!"));
-      std::clog << "FAILED!" << std::endl;
-      std::clog << "   expected exception!" << std::endl;
-      ++errcount;
-    }
-    catch (std::exception const & ex)
-    {
-      std::clog << "OK!" << std::endl;
-      std::clog << "    exception text: " << ex.what() << std::endl;
-    }
-  }
-
-  return errcount;
 }
