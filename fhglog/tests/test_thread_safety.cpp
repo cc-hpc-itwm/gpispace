80d4bbde76263b264a037c740e988f7f4e446c0c
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-12-16 14:31:46 +0100

    fhglog: tests: use Boost.Test in thread_safety and actualy test something

--- fhglog/tests/test_thread_safety.cpp
+++ fhglog/tests/test_thread_safety.cpp
@@ -1,7 +1,11 @@
 // alexander.petry@itwm.fraunhofer.de
 
+#define BOOST_TEST_MODULE thread_safety
+#include <boost/test/unit_test.hpp>
+
 #include <sstream> // ostringstream
 #include <fhglog/fhglog.hpp>
+#include <fhglog/format.hpp>
 #include <fhglog/StreamAppender.hpp>
 #include <fhglog/SynchronizedAppender.hpp>
 #include <unistd.h> // usleep
@@ -11,14 +15,16 @@
 
 namespace
 {
-  void thread_function (std::string logger_name, std::size_t loop_count)
+  const std::size_t loop_count (1000);
+
+  void thread_function (std::string logger_name)
   {
     boost::this_thread::yield();
 
     for (std::size_t i (0); i < loop_count; ++i)
     {
       fhg::log::getLogger (logger_name).log
-        (FHGLOG_MKEVENT_HERE (DEBUG, boost::lexical_cast<std::string> (i)));
+        (FHGLOG_MKEVENT_HERE (ERROR, boost::lexical_cast<std::string> (i)));
 
       if (0 == (i % 50))
       {
@@ -26,27 +32,59 @@ namespace
       }
     }
   }
+
+  struct pushback_appender : public fhg::log::Appender
+  {
+    pushback_appender (std::vector<std::string>* container)
+      : fhg::log::Appender ("pushback_appender")
+      , _container (container)
+    {}
+
+    void append (const fhg::log::LogEvent &evt)
+    {
+      _container->push_back (fhg::log::format ("%m", evt));
+    }
+    void flush () {}
+
+    std::vector<std::string>* _container;
+  };
+
+  struct counter
+  {
+    counter() : _ (0) { }
+    std::string operator()() { return boost::lexical_cast<std::string> (_++); }
+    int _;
+  };
 }
 
-int main (int , char **)
+BOOST_AUTO_TEST_CASE (different_loggers)
 {
-  std::ostringstream logstream;
+  std::vector<std::string> reference (loop_count);
+  std::generate (reference.begin(), reference.end(), counter());
 
-  fhg::log::logger_t root (fhg::log::getLogger());
-  root.addAppender(fhg::log::Appender::ptr_t(new fhg::log::SynchronizedAppender(new fhg::log::StreamAppender("stringstream", logstream, "%m"))));
+  std::vector<std::string> output_0;
+  std::vector<std::string> output_1;
+  std::vector<std::string> output_2;
 
-  {
-    std::clog << "** testing multiple threads...";
-    std::size_t loop_count = 1000;
+  fhg::log::getLogger ("0").addAppender
+    (fhg::log::Appender::ptr_t (new pushback_appender (&output_0)));
+  fhg::log::getLogger ("1").addAppender
+    (fhg::log::Appender::ptr_t (new pushback_appender (&output_1)));
+  fhg::log::getLogger ("2").addAppender
+    (fhg::log::Appender::ptr_t (new pushback_appender (&output_2)));
 
-    boost::thread t0 (&thread_function, "0", loop_count);
-    boost::thread t1 (&thread_function, "0", loop_count);
-    boost::thread t2 (&thread_function, "0", loop_count);
+  boost::thread t0 (&thread_function, "0");
+  boost::thread t1 (&thread_function, "1");
+  boost::thread t2 (&thread_function, "2");
 
-    if (t2.joinable()) { t2.join(); }
-    if (t1.joinable()) { t1.join(); }
-    if (t0.joinable()) { t0.join(); }
+  if (t2.joinable()) { t2.join(); }
+  if (t1.joinable()) { t1.join(); }
+  if (t0.joinable()) { t0.join(); }
 
-    // didn't segfault...
-  }
+  BOOST_REQUIRE_EQUAL_COLLECTIONS
+    (output_0.begin(), output_0.end(), reference.begin(), reference.end());
+  BOOST_REQUIRE_EQUAL_COLLECTIONS
+    (output_1.begin(), output_1.end(), reference.begin(), reference.end());
+  BOOST_REQUIRE_EQUAL_COLLECTIONS
+    (output_2.begin(), output_2.end(), reference.begin(), reference.end());
 }
