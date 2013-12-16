add7b1999e428b9660f689b2a4d19876b5343591
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-12-16 11:10:33 +0100

    fhglog: tests: use Boost.Test in event (does not really test anything)

--- fhglog/tests/test_event.cpp
+++ fhglog/tests/test_event.cpp
@@ -1,67 +1,32 @@
-#include <sstream> // ostringstream
-#include <iostream>
-#include <ctime>   // time
-#include <unistd.h> // sleep
+#define BOOST_TEST_MODULE event
+#include <boost/test/unit_test.hpp>
 
-#include <fhglog/fhglog.hpp>
+#include <fhglog/LogEvent.hpp>
+#include <fhglog/LogLevel.hpp>
 
-int main (int, char **)
+//! \todo This should test way more
+BOOST_AUTO_TEST_CASE (event_ctor_should_regard_severity)
 {
-  using namespace fhg::log;
+  const fhg::log::LogEvent evt
+    (fhg::log::LogLevel::DEBUG, "file", "function", 34, "hello world!");
+  BOOST_REQUIRE_EQUAL (evt.severity().lvl(), fhg::log::LogLevel::DEBUG);
+}
 
-  int errcount(0);
+BOOST_AUTO_TEST_CASE (illegal_loglevel_should_throw)
+{
+  BOOST_REQUIRE_THROW
+    ( fhg::log::LogLevel
+      (static_cast<fhg::log::LogLevel::Level> (fhg::log::LogLevel::MAX_LEVEL+1))
+    , std::runtime_error
+    );
+}
 
-  {
-    std::clog << "** testing event creation...";
-    LogEvent evt(LogLevel::DEBUG, "tests/test_event.cpp", "main", 34, "hello world!");
-    if (evt.severity().lvl() != LogLevel::DEBUG)
-    {
-      std::clog << "FAILED!" << std::endl;
-      std::clog << "\tactual severity: " << evt.severity().lvl() << std::endl;
-      std::clog << "\texpected: " << LogLevel::DEBUG << std::endl;
-      ++errcount;
-    }
-    else
-    {
-      std::clog << "OK!" << std::endl;
-    }
-  }
-
-  {
-    std::clog << "** testing illegal loglevel...";
-    try {
-      LogEvent evt(static_cast<LogLevel::Level>(LogLevel::MAX_LEVEL+1)
-                 , "tests/test_event.cpp"
-                 , "main"
-                 , 34
-                 , "hello world!");
-
-      std::clog << "FAILED!" << std::endl;
-      std::clog << "\tloglevel was out of range, exception expected!" << std::endl;
-      ++errcount;
-    } catch (const std::exception &) {
-      std::clog << "OK!" << std::endl;
-    }
-  }
-
-  {
-    try {
-      LogEvent evt1(LogLevel::DEBUG
-                 , "tests/test_event.cpp"
-                 , "main"
-                 , 34
-                 , "hello world!");
-      LogEvent evt2(evt1);
-      LogEvent evt3(LogLevel::INFO
-                 , "tests/test_formatter.cpp"
-                 , "foo"
-                 , 42
-                 , "blah!");
-    } catch (const std::exception &ex) {
-      std::clog << "FAILED!" << std::endl;
-      std::clog << "\texception occured: " << ex.what() << std::endl;
-      ++errcount;
-    }
-  }
-  return errcount;
+//! \todo This does not really test anything
+BOOST_AUTO_TEST_CASE (ctor_should_not_throw_and_should_be_copyable)
+{
+  const fhg::log::LogEvent evt1
+    (fhg::log::LogLevel::DEBUG, "tests/test_event.cpp", "main", 34, "hello world!");
+  const fhg::log::LogEvent evt2 (evt1);
+  const fhg::log::LogEvent evt3
+    (fhg::log::LogLevel::INFO, "tests/test_formatter.cpp", "foo", 42, "blah!");
 }
