6490766c7a6ed60a4af0774d51ba2df2b93da565
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-12-17 17:54:51 +0100

    seda: remove unused constant SEDA_SYSTEM_STAGE_NAME

--- seda/seda/constants.hpp
+++ seda/seda/constants.hpp
@@ -28,7 +28,6 @@
 namespace seda {
     extern std::size_t SEDA_MAX_QUEUE_SIZE;
     extern unsigned long SEDA_DEFAULT_TIMEOUT;
-    extern const std::string SEDA_SYSTEM_STAGE_NAME;
 }
 
 #endif // !SEDA_CONSTANTS_HPP
