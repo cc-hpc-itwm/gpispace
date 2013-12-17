3d587c232320e04e06721153e828f30a6701b305
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-12-17 10:41:20 +0100

    fhglog: use boost::filesystem instead of self written code

--- fhglog/fhglog/util.hpp
+++ fhglog/fhglog/util.hpp
@@ -20,49 +20,18 @@
 #define FHGLOG_UTIL_HPP 1
 
 #include <string>
-#include <algorithm> // std::transform
-#include <cctype> // std::tolower
 
-#include <list>
-#include <string>
-#include <sstream> // stringstream
-#include <utility> // std::pair
+#include <boost/filesystem.hpp>
 
 namespace fhg { namespace log {
   inline std::string get_filename_from_path(const std::string &a_path)
   {
-    // TODO: the following should be coded with boost::filesystem due to platform
-    // independence
-    static const std::string path_sep("/");
-    std::string::size_type last_path_segment_idx = a_path.find_last_of(path_sep.c_str());
-    if (last_path_segment_idx == std::string::npos)
-    {
-      // return the whole path since we could not find a separator
-      return a_path;
-    }
-    else
-    {
-      // slit the path at that position and return the remaining part
-      return a_path.substr(last_path_segment_idx+1);
-    }
+    return boost::filesystem::path (a_path).filename().string();
   }
 
   inline std::string get_module_name_from_path(const std::string &a_path)
   {
-    std::string filename(get_filename_from_path(a_path));
-    std::transform(filename.begin(), filename.end(), filename.begin(), tolower);
-
-    std::string::size_type ext_start_idx = filename.find_last_of(".");
-    if (ext_start_idx == std::string::npos)
-    {
-      // return the whole path since we could not find a separator
-      return filename;
-    }
-    else
-    {
-      // split the filename and return everything up to the last "."
-      return filename.substr(0, ext_start_idx);
-    }
+    return boost::filesystem::path (a_path).stem().string();
   }
 }}
 
