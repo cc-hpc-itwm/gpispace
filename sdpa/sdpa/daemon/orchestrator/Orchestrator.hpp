a2f143ecd69d3c09e2348f26d1e381d40f6ad353
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2013-12-17 11:11:15 +0100

    sdpa: formatting

--- sdpa/sdpa/daemon/orchestrator/Orchestrator.hpp
+++ sdpa/sdpa/daemon/orchestrator/Orchestrator.hpp
@@ -35,8 +35,8 @@ namespace sdpa {
         url_(url)
       {}
 
-			static Orchestrator::ptr_t create
-        (const std::string& name, const std::string& url);
+      static Orchestrator::ptr_t create ( const std::string& name,
+                                          const std::string& url);
 
       virtual void handleJobFinishedEvent( const sdpa::events::JobFinishedEvent* );
       virtual void handleJobFailedEvent( const sdpa::events::JobFailedEvent* );
