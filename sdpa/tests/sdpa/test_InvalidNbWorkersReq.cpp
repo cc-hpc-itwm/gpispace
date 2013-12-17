fce43f7729c105b62bcf5601e30d2c5ac2e62cf2
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2013-12-17 17:47:28 +0100

    sdpa: added a separate test for an invalid number of workers required for a job

---
+++ sdpa/tests/sdpa/test_InvalidNbWorkersReq.cpp
@@ -0,0 +1,58 @@
+#define BOOST_TEST_MODULE testInvalidWorkerNbReq
+
+#include "kvs_setup_fixture.hpp"
+#include "tests_config.hpp"
+#include <utils.hpp>
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_GLOBAL_FIXTURE (KVSSetup)
+
+BOOST_AUTO_TEST_CASE (testInvalidNumberOfWorkersRequired)
+{
+  const std::string workflow
+    (utils::require_and_read_file ("workflows/coallocation_bad_test.pnet"));
+
+  const utils::orchestrator orchestrator
+    ("orchestrator_0", "127.0.0.1");
+  const utils::agent agent
+    ("agent_0", "127.0.0.1", orchestrator);
+
+  const utils::drts_worker worker_A_0
+    ( "drts_A_0", agent
+    , "A"
+    , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
+    , kvs_host(), kvs_port()
+    );
+  const utils::drts_worker worker_A_1
+    ( "drts_A_1", agent
+    , "A"
+    , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
+    , kvs_host(), kvs_port()
+    );
+
+  const utils::drts_worker worker_B_0
+    ( "drts_B_0", agent
+    , "B"
+    , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
+    , kvs_host(), kvs_port()
+    );
+  const utils::drts_worker worker_B_1
+    ( "drts_B_1", agent
+    , "B"
+    , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
+    , kvs_host(), kvs_port()
+    );
+  const utils::drts_worker worker_B_2
+    ( "drts_B_2", agent
+    , "B"
+    , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
+    , kvs_host(), kvs_port()
+    );
+
+  BOOST_REQUIRE_EQUAL
+    ( utils::client::submit_job_and_wait_for_termination_as_subscriber
+      (workflow, orchestrator)
+    , sdpa::status::FAILED
+    );
+}
