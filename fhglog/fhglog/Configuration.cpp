8c16eb4dad5530af55708e5e24d171a8793f1d02
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-12-17 14:46:33 +0100

    fhglog: move documentation to header

--- fhglog/fhglog/Configuration.cpp
+++ fhglog/fhglog/Configuration.cpp
@@ -40,19 +40,6 @@ namespace fhg
       public:
         DefaultConfiguration();
 
-        /*
-         * This configuration takes the following environment variables into account (case sensitive):
-         *
-         *  FHGLOG_level                     log everything with at least this level, defaults to TRACE
-         *  FHGLOG_color={auto,on,off}       colorized output
-         *  FHGLOG_format=log-format         defaults to the default format in Formatter
-         *  FHGLOG_to_console={stdout,stderr,stdlog} log to stdout, stderr, clog
-         *  FHGLOG_to_file=path to logfile   log to specified file
-         *  FHGLOG_to_server=ip:port         log to the specified server
-         *  FHGLOG_threaded={yes,true,1}     make logging appear from a separate thread (unsafe)
-         *  FHGLOG_disabled={anything}       disable logging if defined
-         *  FHGLOG_synch={anything}          make logging synchronized (very expensive!)
-         */
         void parse_environment();
         void configure();
         void parse_key_value(const std::string &key, const std::string &val);
