e7fbcec1c1382fd63781a29c6030a45b9afc82e1
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-12-18 16:55:50 +0100

    seda: tests: merge discard_strategy and seda::EventCountStrategy into one

--- seda/tests/seda/SedaStageTest.cpp
+++ seda/tests/seda/SedaStageTest.cpp
@@ -5,7 +5,6 @@
 
 #include <seda/Stage.hpp>
 #include <seda/IEvent.hpp>
-#include <seda/EventCountStrategy.hpp>
 
 using namespace seda::tests;
 
@@ -16,10 +15,47 @@ namespace
     virtual std::string str() const { return "dummy"; }
   };
 
-  struct discard_strategy : public Strategy
+  struct wait_for_n_events_strategy : public seda::Strategy
   {
-    discard_strategy() : Strategy ("discard") {}
-    void perform (const IEvent::Ptr&) {}
+    wait_for_n_events_strategy (unsigned int expected)
+      : seda::Strategy ("wait_for_n_events")
+      , _counter (0)
+      , _expected (expected)
+    {}
+
+    void perform (const seda::IEvent::Ptr&)
+    {
+      boost::mutex::scoped_lock _ (_counter_mutex);
+      ++_counter;
+
+      CPPUNIT_ASSERT (_counter <= _expected);
+      if (_counter == _expected)
+      {
+        _expected_count_reached.notify_all();
+      }
+    }
+    void wait() const
+    {
+      boost::mutex::scoped_lock _ (_counter_mutex);
+
+      while (_counter < _expected)
+      {
+        _expected_count_reached.wait (_);
+      }
+
+      CPPUNIT_ASSERT_EQUAL (_counter, _expected);
+    }
+    void reset()
+    {
+      boost::mutex::scoped_lock _ (_counter_mutex);
+
+      _counter = 0;
+    }
+
+    mutable boost::mutex _counter_mutex;
+    mutable boost::condition_variable _expected_count_reached;
+    unsigned int _counter;
+    unsigned int _expected;
   };
 }
 
@@ -33,54 +69,52 @@ SedaStageTest::tearDown() {}
 
 void
 SedaStageTest::testSendFoo() {
-    seda::Strategy::Ptr discard(new discard_strategy);
-    seda::EventCountStrategy::Ptr ecs(new seda::EventCountStrategy(discard));
-    discard = seda::Strategy::Ptr(ecs);
-    seda::Stage::Ptr stage(seda::Stage::Ptr (new seda::Stage ("discard", discard)));
+    const std::size_t numMsgs(1000);
+
+    wait_for_n_events_strategy* counter (new wait_for_n_events_strategy (numMsgs));
+    seda::Strategy::Ptr counter_shared (counter);
+
+    seda::Stage::Ptr stage
+      (seda::Stage::Ptr (new seda::Stage ("discard", counter_shared)));
 
     stage->start();
 
-    const std::size_t numMsgs(1000);
     for (std::size_t i=0; i < numMsgs; ++i) {
         stage->send(seda::IEvent::Ptr(new dummy_event));
     }
 
-    ecs->wait(numMsgs);
-
-    CPPUNIT_ASSERT_EQUAL(numMsgs, ecs->count());
+    counter->wait();
 
     stage->stop();
 }
 
 void
 SedaStageTest::testStartStop() {
-    seda::Strategy::Ptr discard(new discard_strategy);
-    seda::EventCountStrategy::Ptr ecs(new seda::EventCountStrategy(discard));
-    discard = seda::Strategy::Ptr(ecs);
-    seda::Stage::Ptr stage(seda::Stage::Ptr (new seda::Stage("discard", discard)));
-
     const std::size_t numMsgs(10);
 
+    wait_for_n_events_strategy* counter (new wait_for_n_events_strategy (numMsgs));
+    seda::Strategy::Ptr counter_shared (counter);
+
+    seda::Stage::Ptr stage
+      (seda::Stage::Ptr (new seda::Stage("discard", counter_shared)));
+
     stage->start();
 
     for (std::size_t i=0; i < numMsgs; ++i) {
         stage->send(seda::IEvent::Ptr(new dummy_event));
     }
-    ecs->wait(numMsgs);
 
-    CPPUNIT_ASSERT_EQUAL(numMsgs, ecs->count());
+    counter->wait();
 
     stage->stop();
-
-    ecs->reset();
-
+    counter->reset();
     stage->start();
+
     for (std::size_t i=0; i < numMsgs; ++i) {
         stage->send(seda::IEvent::Ptr(new dummy_event));
     }
-    ecs->wait(numMsgs);
 
-    CPPUNIT_ASSERT_EQUAL(numMsgs, ecs->count());
+    counter->wait();
 
     stage->stop();
 }
