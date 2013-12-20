c6f8d2fd00bfb813593bbc0d2f36e924451e4b3e
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2013-12-20 15:54:58 +0100

    sdpa: tests: calls to submitWorkflow() are not expected in this testcase, thus throw

--- sdpa/tests/sdpa/test_SimpleScheduler.cpp
+++ sdpa/tests/sdpa/test_SimpleScheduler.cpp
@@ -59,7 +59,7 @@ public:
   void submitWorkflow(const we::mgmt::layer::id_type& id, const we::mgmt::layer::encoded_type& )
   {
     DLOG(TRACE, "The agent is trying to forward the master job "<<id<<" to the workflow engine");
-    BOOST_REQUIRE(hasWorkflowEngine());
+    throw std::runtime_error ("trying to submit workflow in test casse which never should");
   }
 
   void sendEventToSelf(const sdpa::events::SDPAEvent::Ptr& pEvt)
