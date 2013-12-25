a64981c53baeab60cda436721d9bad1a5d6d8c8b
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-12-25 23:23:20 +0100

    fhglog: test: FATAL is max, so check_throw_fatal_if_max is meaningless

--- fhglog/tests/fatal_throws.cpp
+++ fhglog/tests/fatal_throws.cpp
@@ -16,19 +16,3 @@ BOOST_AUTO_TEST_CASE (exception_on_fatal)
     BOOST_REQUIRE_EQUAL (e.what(), msg);
   }
 }
-
-BOOST_AUTO_TEST_CASE (exception_on_fatal_when_level_is_maximal)
-{
-  fhg::log::getLogger().setLevel (fhg::log::LogLevel::MAX_LEVEL);
-
-  std::string const msg ("testing FATAL throws");
-
-  try
-  {
-    LOG (FATAL, msg);
-  }
-  catch (std::runtime_error const& e)
-  {
-    BOOST_REQUIRE_EQUAL (e.what(), msg);
-  }
-}
