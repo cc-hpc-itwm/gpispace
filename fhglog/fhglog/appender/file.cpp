6000100748c887062fefc0107cbe0049d411304a
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-12-25 18:31:08 +0100

    fhglog: appender: file: cosmetics: shorten header, whitespace, nameing

--- fhglog/fhglog/appender/file.cpp
+++ fhglog/fhglog/appender/file.cpp
@@ -1,53 +1,38 @@
-/*
- * =====================================================================================
- *
- *       Filename:  FileAppender.cpp
- *
- *    Description:  implements the file appender
- *
- *        Version:  1.0
- *        Created:  10/10/2009 02:36:10 AM
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Alexander Petry (petry), alexander.petry@itwm.fraunhofer.de
- *        Company:  Fraunhofer ITWM
- *
- * =====================================================================================
- */
+// alexander.petry@itwm.fraunhofer.de
 
 #include "file.hpp"
 
-#include <iostream> // std::clog
-#include <exception>
-
-using namespace fhg::log;
-
-FileAppender::FileAppender( const std::string &a_path
-                          , const std::string &fmt
-                          , int flush_interval
-                          , const std::ios_base::openmode &a_mode
-                          )
-  : stream_(a_path.c_str(), a_mode)
-  , fmt_(fmt)
-  , flush_interval_(flush_interval)
-  , event_count_(0)
+namespace fhg
 {
-  stream_.exceptions(std::ios_base::badbit | std::ios_base::failbit);
-}
-
-void FileAppender::flush()
-{
-  stream_.flush();
-}
-
-void FileAppender::append(const LogEvent &evt)
-{
-  stream_ << format(fmt_, evt);
-
-  if (++event_count_ >= flush_interval_)
+  namespace log
   {
-    flush();
-    event_count_ = 0;
+    FileAppender::FileAppender ( const std::string& path
+                               , const std::string& fmt
+                               , int flush_interval
+                               , const std::ios_base::openmode& mode
+                               )
+      : _stream (path.c_str(), mode)
+      , _format (fmt)
+      , _flush_interval (flush_interval)
+      , _event_count (0)
+    {
+      _stream.exceptions (std::ios_base::badbit | std::ios_base::failbit);
+    }
+
+    void FileAppender::flush()
+    {
+      _stream.flush();
+    }
+
+    void FileAppender::append (const LogEvent& event)
+    {
+      _stream << format (_format, event);
+
+      if (++_event_count >= _flush_interval)
+      {
+        flush();
+        _event_count = 0;
+      }
+    }
   }
 }
--- fhglog/fhglog/appender/file.hpp
+++ fhglog/fhglog/appender/file.hpp
@@ -1,20 +1,4 @@
-/*
- * =====================================================================================
- *
- *       Filename:  FileAppender.hpp
- *
- *    Description:  appends to files
- *
- *        Version:  1.0
- *        Created:  10/09/2009 11:24:48 PM
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Alexander Petry (petry), alexander.petry@itwm.fraunhofer.de
- *        Company:  Fraunhofer ITWM
- *
- * =====================================================================================
- */
+// alexander.petry@itwm.fraunhofer.de
 
 #ifndef FHG_LOG_FILE_APPENDER_HPP
 #define FHG_LOG_FILE_APPENDER_HPP 1
@@ -28,30 +12,34 @@
 
 #include <boost/shared_ptr.hpp>
 
-namespace fhg { namespace log {
-  class FileAppender : public Appender
-  {
-  public:
-    typedef boost::shared_ptr<FileAppender> ptr_t;
+namespace fhg
+{
+ namespace log
+ {
+   class FileAppender : public Appender
+   {
+   public:
+     typedef boost::shared_ptr<FileAppender> ptr_t;
 
-    FileAppender( const std::string &a_path
-                , const std::string &fmt
-                , int flush_interval = 5
-                , const std::ios_base::openmode &a_mode
-                    = std::ios_base::out
-                    | std::ios_base::app
-                    | std::ios_base::binary
-                );
+     FileAppender ( const std::string& path
+                  , const std::string& format
+                  , int flush_interval = 5
+                  , const std::ios_base::openmode& mode
+                  = std::ios_base::out
+                  | std::ios_base::app
+                  | std::ios_base::binary
+                  );
 
-    virtual void flush();
-    virtual void append(const LogEvent &evt);
+     virtual void flush();
+     virtual void append (const LogEvent&);
 
-  private:
-    std::ofstream stream_;
-    std::string const fmt_;
-    int const flush_interval_;
-    int event_count_;
-  };
-}}
+   private:
+     std::ofstream _stream;
+     std::string const _format;
+     int const _flush_interval;
+     int _event_count;
+   };
+ }
+}
 
 #endif
