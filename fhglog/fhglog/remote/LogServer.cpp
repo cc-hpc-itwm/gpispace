93e90c16ca2b415884c5dd5069584976ba56f205
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-12-25 14:23:33 +0100

    fhglog: LogServer: do not say 'using namespace' but write the namespace explicitely

--- fhglog/fhglog/remote/LogServer.cpp
+++ fhglog/fhglog/remote/LogServer.cpp
@@ -4,78 +4,97 @@
 
 #include "LogServer.hpp"
 
-using namespace fhg::log::remote;
-using boost::asio::ip::udp;
-
-LogServer::LogServer ( const fhg::log::Appender::ptr_t& appender
-                     , boost::asio::io_service& io_service
-                     , unsigned short port
-                     )
-  : appender_ (appender)
-  , socket_ (io_service, udp::endpoint (udp::v4(), port))
+namespace fhg
 {
-  LOG (INFO, "log server listening on " << udp::endpoint (udp::v4(), port));
-
-  boost::system::error_code ec;
-
-  socket_.set_option (boost::asio::socket_base::reuse_address (true), ec);
-
-  LOG_IF (WARN, ec, "could not set resuse address option: " << ec << ": " << ec.message());
-
-  socket_.async_receive_from
-    ( boost::asio::buffer (data_, max_length)
-    , sender_endpoint_
-    , boost::bind ( &LogServer::handle_receive_from
-                  , this
-                  , boost::asio::placeholders::error
-                  , boost::asio::placeholders::bytes_transferred
-                  )
-    );
-}
-
-void LogServer::handle_receive_from ( const boost::system::error_code& error
-                                    , size_t bytes_recv
-                                    )
-{
-  if (!error && bytes_recv > 0)
+  namespace log
   {
-    std::string const msg (data_ , data_ + bytes_recv);
-
-    if (msg == "QUIT deadbeef")
+    namespace remote
     {
-      LOG (INFO, "got QUIT message, shutting down.");
+      LogServer::LogServer ( const fhg::log::Appender::ptr_t& appender
+                           , boost::asio::io_service& io_service
+                           , unsigned short port
+                           )
+        : appender_ (appender)
+        , socket_
+          ( io_service
+          , boost::asio::ip::udp::endpoint (boost::asio::ip::udp::v4(), port)
+          )
+        {
+          LOG ( INFO, "log server listening on "
+              << boost::asio::ip::udp::endpoint ( boost::asio::ip::udp::v4()
+                                                , port
+                                                )
+              );
 
-      return;
-    }
+          boost::system::error_code ec;
 
-    try
-    {
-      LogEvent evt (LogEvent::from_string (msg));
+          socket_.set_option ( boost::asio::socket_base::reuse_address (true)
+                             , ec
+                             );
 
+          LOG_IF ( WARN, ec
+                 , "could not set resuse address option: "
+                 << ec << ": " << ec.message()
+                 );
+
+          socket_.async_receive_from
+            ( boost::asio::buffer (data_, max_length)
+            , sender_endpoint_
+            , boost::bind ( &LogServer::handle_receive_from
+                          , this
+                          , boost::asio::placeholders::error
+                          , boost::asio::placeholders::bytes_transferred
+                          )
+            );
+        }
+
+      void LogServer::handle_receive_from
+        ( const boost::system::error_code& error
+        , size_t bytes_recv
+        )
       {
-        std::ostringstream ostr;
-        ostr << sender_endpoint_;
-        evt.trace (ostr.str());
-      }
-      appender_->append (evt);
-    }
-    catch (std::exception const &ex)
-    {
-      LOG (WARN, "error during log-event decode: " << ex.what ());
-    }
-  }
-  else
-  {
-    LOG(ERROR, "error during receive: " << error);
-  }
+        if (!error && bytes_recv > 0)
+        {
+          std::string const msg (data_ , data_ + bytes_recv);
 
-  socket_.async_receive_from
-    ( boost::asio::buffer (data_, max_length)
-    , sender_endpoint_
-    , boost::bind ( &LogServer::handle_receive_from
-                  , this
-                  , boost::asio::placeholders::error
-                  , boost::asio::placeholders::bytes_transferred
-                  )
-    );
+          if (msg == "QUIT deadbeef")
+          {
+            LOG (INFO, "got QUIT message, shutting down.");
+
+            return;
+          }
+
+          try
+          {
+            LogEvent evt (LogEvent::from_string (msg));
+
+            {
+              std::ostringstream ostr;
+              ostr << sender_endpoint_;
+              evt.trace (ostr.str());
+            }
+            appender_->append (evt);
+          }
+          catch (std::exception const &ex)
+          {
+            LOG (WARN, "error during log-event decode: " << ex.what ());
+          }
+        }
+        else
+        {
+          LOG (ERROR, "error during receive: " << error);
+        }
+
+        socket_.async_receive_from
+          ( boost::asio::buffer (data_, max_length)
+          , sender_endpoint_
+          , boost::bind ( &LogServer::handle_receive_from
+                        , this
+                        , boost::asio::placeholders::error
+                        , boost::asio::placeholders::bytes_transferred
+                        )
+          );
+      }
+    }
+  }
 }
