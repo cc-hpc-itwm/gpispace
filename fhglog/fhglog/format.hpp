d4fb213efec16ddf1797157434ac5d9fb8329c4b
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-12-25 21:34:15 +0100

    fhglog: inline format_flag into format

--- fhglog/fhglog/format.hpp
+++ fhglog/fhglog/format.hpp
@@ -1,9 +1,16 @@
 #ifndef FHG_LOG_FORMAT_HPP
 #define FHG_LOG_FORMAT_HPP 1
 
-#include <string>
 #include <fhglog/event.hpp>
 #include <fhglog/format_flag.hpp>
+
+#include <fhg/util/first_then.hpp>
+
+#include <boost/filesystem.hpp>
+#include <boost/foreach.hpp>
+#include <boost/format.hpp>
+
+#include <string>
 
 namespace fhg
 {
@@ -19,7 +26,7 @@ namespace fhg
       std::string::size_type len(_fmt.size());
       while (len > 0)
       {
-        if (*c == fhg::log::fmt::flag::OPEN::value)
+        if (*c == '%')
         {
           ++c; --len;
           if (0 == len)
@@ -31,30 +38,70 @@ namespace fhg
           // what do we have
           switch (*c)
           {
-#define HDL(f)                                                         \
-            case fmt::format_traits<fmt::flag:: f>::flag::value:        \
-              fmt::format_traits<fmt::flag:: f>::flag::format(evt, os, *c); \
-            break
+          case '%': os << '%'; break;
+          case 's': os << evt.severity().str()[0]; break;
+          case 'S': os << evt.severity().str(); break;
+          case 'p': os << boost::filesystem::path (evt.path()).filename().string(); break;
+          case 'P': os << evt.path(); break;
+          case 'F': os << evt.function(); break;
+          case 'M': os << boost::filesystem::path (evt.path()).stem().string(); break;
+          case 'L': os << evt.line(); break;
+          case 'm': os << evt.message(); break;
+          case 'd':
+            {
+              char buf[128];
 
-            HDL(OPEN);
-            HDL(SHORT_SEVERITY);
-            HDL(SEVERITY);
-            HDL(FILE);
-            HDL(PATH);
-            HDL(FUNCTION);
-            HDL(MODULE);
-            HDL(LINE);
-            HDL(MESSAGE);
-            HDL(DATE);
-            HDL(TSTAMP);
-            HDL(TID);
-            HDL(PID);
-            HDL(TAGS);
-            HDL(LOGGER);
-            HDL(ENDL);
+              memset (buf, 0, sizeof (buf));
+
+              const time_t tm (static_cast<time_t> (evt.tstamp()));
+
+              ctime_r (&tm, buf);
+
+              buf[std::find (buf, buf + sizeof (buf), '\n') - buf] = 0;
+              os << buf;
+            }
+            break;
+          case 't':
+            {
+              std::ios_base::fmtflags const flags (os.flags());
+              os.unsetf (std::ios_base::scientific);
+              os.setf (std::ios_base::fixed);
+              os << evt.tstamp();
+              os.flags (flags);
+            }
+            break;
+          case 'T':
+            {
+              std::ios_base::fmtflags const flags (os.flags());
+              os << std::hex << evt.tid();
+              os.flags (flags);
+            }
+            break;
+          case 'R': os << evt.pid(); break;
+          case '#':
+            {
+              fhg::util::first_then<std::string> sep ("#", ",#");
+
+              BOOST_FOREACH (std::string const& tag, evt.tags())
+              {
+                os << sep << tag;
+              }
+            }
+            break;
+          case 'l':
+            {
+              fhg::util::first_then<std::string> sep ("", "->");
+
+              BOOST_FOREACH (std::string const& t, evt.trace())
+              {
+                os << sep << t;
+              }
+            }
+            break;
+          case 'n': os << "\n"; break;
           default:
-            HDL(UNDEF);
-#undef HDL
+            throw std::runtime_error
+              ((boost::format ("format code not defined: %1%") % *c).str());
           }
         }
         else
