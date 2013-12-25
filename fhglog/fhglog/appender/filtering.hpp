1143c9c96a5aef7c3b7231b5a8fdf68617f81715
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-12-25 18:45:16 +0100

    fhglog: appender: filtering: cosmetics: shorten header, whitespace, nameing

--- fhglog/fhglog/appender/filtering.hpp
+++ fhglog/fhglog/appender/filtering.hpp
@@ -1,20 +1,4 @@
-/*
- * =====================================================================================
- *
- *       Filename:  FilteringAppender.hpp
- *
- *    Description:  an appender that filters events before appending
- *
- *        Version:  1.0
- *        Created:  10/12/2009 10:57:28 PM
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Alexander Petry (petry), alexander.petry@itwm.fraunhofer.de
- *        Company:  Fraunhofer ITWM
- *
- * =====================================================================================
- */
+// alexander.petry@itwm.fraunhofer.de
 
 #ifndef FHG_LOG_FILTERING_APPENDER_HPP
 #define FHG_LOG_FILTERING_APPENDER_HPP 1
@@ -22,29 +6,39 @@
 #include <fhglog/Appender.hpp>
 #include <fhglog/Filter.hpp>
 
-namespace fhg { namespace log {
-  class FilteringAppender : public Appender
+namespace fhg
+{
+  namespace log
   {
+    class FilteringAppender : public Appender
+    {
     public:
-      FilteringAppender(const Appender::ptr_t &real_appender, const Filter::ptr_t &a_filter)
-        : _appender (real_appender)
-        , filter_(a_filter) { }
 
-      virtual void append(const LogEvent &evt)
+      FilteringAppender ( const Appender::ptr_t& appender
+                        , const Filter::ptr_t& filter
+                        )
+        : _appender (appender)
+        , _filter (filter)
+      {}
+
+      virtual void append (const LogEvent &evt)
       {
-        if (! (*filter_)(evt))
+        if (! (*_filter)(evt))
         {
-          _appender->append(evt);
+          _appender->append (evt);
         }
       }
-    virtual void flush()
-    {
-      _appender->flush();
-    }
+      virtual void flush()
+      {
+        _appender->flush();
+      }
+
     private:
-    Appender::ptr_t _appender;
-      Filter::ptr_t filter_;
-  };
-}}
+
+      Appender::ptr_t _appender;
+      Filter::ptr_t _filter;
+    };
+  }
+}
 
 #endif
