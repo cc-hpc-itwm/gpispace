e7b66869199d5930ae39f572f116961659ee920e
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-12-25 14:00:45 +0100

    fhglog: LogServer: cosmetics, shorten header, whitespace, linebreaks

--- fhglog/fhglog/remote/LogServer.cpp
+++ fhglog/fhglog/remote/LogServer.cpp
@@ -7,37 +7,46 @@
 using namespace fhg::log::remote;
 using boost::asio::ip::udp;
 
-LogServer::LogServer(const fhg::log::Appender::ptr_t &appender
-                   , boost::asio::io_service &io_service
-                   , unsigned short port)
-  : appender_(appender)
-    //  , io_service_(io_service)
-  , socket_(io_service, udp::endpoint(udp::v4(), port))
+LogServer::LogServer ( const fhg::log::Appender::ptr_t& appender
+                     , boost::asio::io_service& io_service
+                     , unsigned short port
+                     )
+  : appender_ (appender)
+  , socket_ (io_service, udp::endpoint (udp::v4(), port))
 {
-  LOG(INFO, "log server listening on " << udp::endpoint(udp::v4(), port));
+  LOG (INFO, "log server listening on " << udp::endpoint (udp::v4(), port));
+
   boost::system::error_code ec;
 
   socket_.set_option (boost::asio::socket_base::reuse_address (true), ec);
-  LOG_IF(WARN, ec, "could not set resuse address option: " << ec << ": " << ec.message());
 
-  socket_.async_receive_from(boost::asio::buffer(data_, max_length), sender_endpoint_,
-      boost::bind(&LogServer::handle_receive_from, this,
-        boost::asio::placeholders::error,
-        boost::asio::placeholders::bytes_transferred));
+  LOG_IF (WARN, ec, "could not set resuse address option: " << ec << ": " << ec.message());
 
+  socket_.async_receive_from
+    ( boost::asio::buffer (data_, max_length)
+    , sender_endpoint_
+    , boost::bind ( &LogServer::handle_receive_from
+                  , this
+                  , boost::asio::placeholders::error
+                  , boost::asio::placeholders::bytes_transferred
+                  )
+    );
 }
 
-void LogServer::handle_receive_from(const boost::system::error_code &error
-                                  , size_t bytes_recv)
+void LogServer::handle_receive_from ( const boost::system::error_code& error
+                                    , size_t bytes_recv
+                                    )
 {
   if (!error && bytes_recv > 0)
   {
     data_[bytes_recv] = 0;
-    std::string msg(data_);
+
+    std::string msg (data_);
 
     if (msg == "QUIT deadbeef")
     {
-      LOG(INFO, "got QUIT message, shutting down.");
+      LOG (INFO, "got QUIT message, shutting down.");
+
       return;
     }
 
@@ -62,12 +71,13 @@ void LogServer::handle_receive_from(const boost::system::error_code &error
     LOG(ERROR, "error during receive: " << error);
   }
 
-  socket_.async_receive_from( boost::asio::buffer(data_, max_length)
-                            , sender_endpoint_
-                            , boost::bind( &LogServer::handle_receive_from
-                                         , this
-                                         , boost::asio::placeholders::error
-                                         , boost::asio::placeholders::bytes_transferred
-                                         )
-                            );
+  socket_.async_receive_from
+    ( boost::asio::buffer (data_, max_length)
+    , sender_endpoint_
+    , boost::bind ( &LogServer::handle_receive_from
+                  , this
+                  , boost::asio::placeholders::error
+                  , boost::asio::placeholders::bytes_transferred
+                  )
+    );
 }
--- fhglog/fhglog/remote/LogServer.hpp
+++ fhglog/fhglog/remote/LogServer.hpp
@@ -1,49 +1,40 @@
-/*
- * =====================================================================================
- *
- *       Filename:  LogServer.hpp
- *
- *    Description:  implements a logging server
- *
- *        Version:  1.0
- *        Created:  10/19/2009 08:13:32 PM
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Alexander Petry (petry), alexander.petry@itwm.fraunhofer.de
- *        Company:  Fraunhofer ITWM
- *
- * =====================================================================================
- */
+// alexander.petry@itwm.fraunhofer.de
 
 #ifndef FHGLOG_REMOTE_LOG_SERVER_HPP
 #define FHGLOG_REMOTE_LOG_SERVER_HPP 1
 
+#include <fhglog/Appender.hpp>
+#include <fhglog/remote/RemoteAppender.hpp>
+
 #include <boost/asio.hpp>
 
-#include <fhglog/Appender.hpp> // to write received events to custom appender
-#include <fhglog/remote/RemoteAppender.hpp> // to forward events to another logserver
-
-namespace fhg { namespace log { namespace remote {
-  class LogServer
+namespace fhg
+{
+  namespace log
   {
-  public:
-    explicit
-    LogServer(const fhg::log::Appender::ptr_t &appender
-            , boost::asio::io_service &io_service
-            , unsigned short port);
+    namespace remote
+    {
+      class LogServer
+      {
+      public:
+        LogServer ( const fhg::log::Appender::ptr_t&
+                  , boost::asio::io_service&
+                  , unsigned short port
+                  );
 
-    void handle_receive_from(const boost::system::error_code &error
-                           , size_t bytes_recv);
-  private:
-    fhg::log::Appender::ptr_t appender_;
-    //    boost::asio::io_service &io_service_;
-    boost::asio::ip::udp::socket socket_;
-    boost::asio::ip::udp::endpoint sender_endpoint_;
+        void handle_receive_from ( const boost::system::error_code&
+                                 , size_t bytes_recv
+                                 );
+      private:
+        fhg::log::Appender::ptr_t appender_;
+        boost::asio::ip::udp::socket socket_;
+        boost::asio::ip::udp::endpoint sender_endpoint_;
 
-    enum { max_length = (2<<16) };
-    char data_[max_length];
-  };
-}}}
+        enum { max_length = (2<<16) };
+        char data_[max_length];
+      };
+    }
+  }
+}
 
 #endif
