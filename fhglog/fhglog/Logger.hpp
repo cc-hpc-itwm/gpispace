9c648ce2644c5f5fb4f8f06fc7fa7a86224aa026
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-12-25 21:00:20 +0100

    fhglog: Logger: cosmetics: whitespace, delete 'inline'

--- fhglog/fhglog/Logger.hpp
+++ fhglog/fhglog/Logger.hpp
@@ -12,51 +12,55 @@
 #include <boost/shared_ptr.hpp>
 
 /**
-  Common logging framework.
+   Common logging framework.
 
-  LoggerApi root_logger(Logger::get());
-  LoggerApi my_logger(Logger::get("module"));
+   LoggerApi root_logger(Logger::get());
+   LoggerApi my_logger(Logger::get("module"));
 
-  my_logger.log(LogEvent(...));
+   my_logger.log(LogEvent(...));
 */
-namespace fhg { namespace log {
+namespace fhg
+{
+  namespace log
+  {
 
-  /*
-   * This class wraps around a simple Logger object.
-   *
-   * The main reason to have a separate loggerapi class and an implementation
-   * is the following, when we want to disable logging completely, we should
-   * make sure that the classes using loggers as member variables still have
-   * the same size. In this case, sizeof(LoggerApi) == sizeof(void*) - that
-   * means in the disabled case, we can just allocate an empty void pointer.
-   *
-   */
-  class Logger {
+    /*
+     * This class wraps around a simple Logger object.
+     *
+     * The main reason to have a separate loggerapi class and an implementation
+     * is the following, when we want to disable logging completely, we should
+     * make sure that the classes using loggers as member variables still have
+     * the same size. In this case, sizeof(LoggerApi) == sizeof(void*) - that
+     * means in the disabled case, we can just allocate an empty void pointer.
+     *
+     */
+    class Logger
+    {
     public:
       typedef boost::shared_ptr<Logger> ptr_t;
 
       static Logger::ptr_t get();
-      static Logger::ptr_t get(const std::string &name, const std::string &base = "default");
+      static Logger::ptr_t get ( const std::string&name
+                               , const std::string&base = "default"
+                               );
 
-      explicit
-      Logger(const std::string &name);
-      Logger(const std::string &name, const Logger &inherit_from);
+      explicit Logger (const std::string &name);
+      Logger (const std::string& name, const Logger& inherit_from);
 
-      const std::string &name() const;
-      const std::string &parent() const;
-      void setLevel(const LogLevel &level);
-      void setFilter(const Filter::ptr_t &filter);
-      inline bool isLevelEnabled(const LogLevel &level) const
+      const std::string& name() const;
+      const std::string& parent() const;
+      void setLevel (const LogLevel& level);
+      void setFilter (const Filter::ptr_t& filter);
+      bool isLevelEnabled (const LogLevel& level) const
       {
         return level >= lvl_;
       }
-
-      inline bool isFiltered(const LogEvent &evt) const
+      bool isFiltered (const LogEvent& evt) const
       {
         return (*filter_)(evt);
       }
 
-      void log(const LogEvent &event);
+      void log (const LogEvent &event);
       void flush();
 
       const Appender::ptr_t &addAppender(const Appender::ptr_t &appender);
@@ -67,9 +71,10 @@ namespace fhg { namespace log {
       Filter::ptr_t filter_;
 
       std::list<Appender::ptr_t> appenders_;
-  };
+    };
 
     void terminate ();
-}}
+  }
+}
 
 #endif
