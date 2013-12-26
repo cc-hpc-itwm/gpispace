c618355ce6d5b59613b82e04a7d675a767175fa9
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-12-26 03:05:57 +0100

    fhglog: macros: cosmetics, whitespace, BOOST_CURRENT_FUNCTION, unique names (uuid), delete comments

--- fhglog/fhglog/LogMacros.hpp
+++ fhglog/fhglog/LogMacros.hpp
@@ -1,126 +1,129 @@
-#ifndef  FHG_LOG_LOGMACROS_INC
-#define  FHG_LOG_LOGMACROS_INC
+#ifndef FHG_LOG_LOGMACROS_INC
+#define FHG_LOG_LOGMACROS_INC
 
-#include <fhglog/LoggerApi.hpp>
 #include <fhglog/Configuration.hpp>
-#include <sstream>
+#include <fhglog/LoggerApi.hpp>
+
+#include <boost/current_function.hpp>
 #include <boost/filesystem.hpp>
+#include <boost/preprocessor/facilities/empty.hpp>
 
-namespace fhg { namespace log {
+#include <sstream>
 
-// convenience macro to create an event
-#define FHGLOG_MKEVENT_HERE(level, message) ::fhg::log::LogEvent(::fhg::log::level, __FILE__, __PRETTY_FUNCTION__, __LINE__, message)
-#define FHGLOG_MKEVENT(var, level, message) ::fhg::log::LogEvent var(::fhg::log::level, __FILE__, __PRETTY_FUNCTION__, __LINE__, message)
+namespace fhg
+{
+  namespace log
+  {
 
-#  define FHGLOG_FLUSH()                                                \
-    do                                                                  \
-    {                                                                   \
-      fhg::log::getLogger().flush();                                    \
-    } while (0)                                                         \
+#define FHGLOG_MKEVENT(var, level, message)             \
+    ::fhg::log::LogEvent var ( ::fhg::log::level        \
+                             , __FILE__                 \
+                             , BOOST_CURRENT_FUNCTION   \
+                             , __LINE__                 \
+                             , message                  \
+                             )
 
-    namespace detail
+#define FHGLOG_MKEVENT_HERE(level, message)             \
+    FHGLOG_MKEVENT (BOOST_PP_EMPTY(), level, message)
+
+    struct flush_at_end_of_scope_t
     {
-      struct flush_at_end_of_scope_t
+      ~flush_at_end_of_scope_t() throw ()
       {
-        ~flush_at_end_of_scope_t() throw ()
-        {
-          FHGLOG_FLUSH();
-        }
-      };
-    }
+        ::fhg::log::getLogger().flush();
+      }
+    };
 
-#  define FHGLOG_SETUP()                                                \
-    fhg::log::detail::flush_at_end_of_scope_t fhglog_flush_at_end_of_scope; \
-    (void)(fhglog_flush_at_end_of_scope);                               \
-    fhg::log::configure();
+#define FHGLOG_SETUP()                                                  \
+    ::fhg::log::flush_at_end_of_scope_t const _1e872d1a_6dcf_11e3_9e3b_13f239ecd3ca; \
+    ::fhg::log::configure()
 
 #ifndef FHGLOG_STRIP_LEVEL
-#  define FHGLOG_STRIP_LEVEL -1
+#define FHGLOG_STRIP_LEVEL -1
 #endif
 
-#  define __LOG(logger, level, msg)                                     \
-    do {                                                                \
-      if (  (::fhg::log::level > FHGLOG_STRIP_LEVEL)                    \
-         && logger.isLevelEnabled(::fhg::log::level)                    \
-         )                                                              \
-      {                                                                 \
-        std::ostringstream msg_;                                        \
-        msg_ << msg;                                                    \
-        FHGLOG_MKEVENT(__log_evt, level, msg_.str());                   \
-                                                                        \
-        if (! logger.isFiltered(__log_evt))                             \
-        {                                                               \
-          logger.log(__log_evt);                                        \
-        }                                                               \
-      }                                                                 \
-    } while(0)
+#define __LOG(logger, level, msg)                       \
+    do                                                  \
+    {                                                   \
+      if (  (::fhg::log::level > FHGLOG_STRIP_LEVEL)    \
+         && logger.isLevelEnabled (::fhg::log::level)   \
+         )                                              \
+      {                                                 \
+        std::ostringstream msg_;                        \
+        msg_ << msg;                                    \
+        FHGLOG_MKEVENT(__log_evt, level, msg_.str());   \
+                                                        \
+        if (not logger.isFiltered (__log_evt))          \
+        {                                               \
+          logger.log (__log_evt);                       \
+        }                                               \
+      }                                                 \
+    } while (0)
 
-// log to a specific logger
 #define LLOG(level, logger, msg) __LOG(logger, level, msg)
-#define LLOG_IF(level, logger, condition, msg)                          \
-    do                                                                  \
-    {                                                                   \
-      if (condition)                                                    \
-      {                                                                 \
-        LLOG(level, logger, msg);                                       \
-      }                                                                 \
-    }                                                                   \
+#define LLOG_IF(level, logger, condition, msg)  \
+    do                                          \
+    {                                           \
+      if (condition)                            \
+      {                                         \
+        LLOG (level, logger, msg);              \
+      }                                         \
+    }                                           \
     while (0)
 
     // log if some condition is true
-#define LOG_IF(level, condition, msg)                                   \
-    do                                                                  \
-    {                                                                   \
-      if (condition)                                                    \
-      {                                                                 \
-        LOG(level, msg);                                                \
-      }                                                                 \
-    }                                                                   \
+#define LOG_IF(level, condition, msg)           \
+    do                                          \
+    {                                           \
+      if (condition)                            \
+      {                                         \
+        LOG (level, msg);                       \
+      }                                         \
+    }                                           \
     while (0)
 
-// log to a logger with the name of the filename the statement is in
-#define MLOG(level, msg)                                                \
-    LLOG( level                                                         \
-        , ::fhg::log::getLogger                                         \
-          (::boost::filesystem::path (__FILE__).stem().string())        \
-        , msg                                                           \
-        )
+    // log to a logger with the name of the filename the statement is in
+#define MLOG(level, msg)                                          \
+    LLOG ( level                                                  \
+         , ::fhg::log::getLogger                                  \
+           (::boost::filesystem::path (__FILE__).stem().string()) \
+         , msg                                                    \
+         )
 
-#define MLOG_IF(level, condition, msg)                                  \
-    LLOG_IF( level                                                      \
-           , ::fhg::log::getLogger                                      \
-             (::boost::filesystem::path (__FILE__).stem().string())     \
-           , condition                                                  \
-           , msg                                                        \
-           )
+#define MLOG_IF(level, condition, msg)                                    \
+    LLOG_IF ( level                                                       \
+            , ::fhg::log::getLogger                                       \
+              (::boost::filesystem::path (__FILE__).stem().string())      \
+            , condition                                                   \
+            , msg                                                         \
+            )
 
     // log to a named logger (component)
-#define CLOG(level, component, msg)                                     \
-    LLOG( level                                                         \
-        , ::fhg::log::getLogger(component)                              \
-        , msg                                                           \
-        )
+#define CLOG(level, component, msg)             \
+    LLOG ( level                                \
+         , ::fhg::log::getLogger (component)    \
+         , msg                                  \
+         )
 
     // just log
-#define LOG(level, msg) LLOG(level, ::fhg::log::getLogger(), msg)
+#define LOG(level, msg) LLOG (level, ::fhg::log::getLogger(), msg)
 
 #ifdef NDEBUG
 
 #define DMLOG(level, msg)
 #define DLOG(level, msg)
 #define DLOG_IF(level, condition, msg)
-
 #define DMLOG_IF(level, condition, msg)
 
 #else
 
-#define DMLOG(level, msg) MLOG(level, msg)
-#define DLOG(level, msg) LOG(level, msg)
-#define DLOG_IF(level, condition, msg) LOG_IF(level, condition, msg)
-
-#define DMLOG_IF(level, condition, msg) MLOG_IF(level, condition, msg)
+#define DMLOG(level, msg) MLOG (level, msg)
+#define DLOG(level, msg) LOG (level, msg)
+#define DLOG_IF(level, condition, msg) LOG_IF (level, condition, msg)
+#define DMLOG_IF(level, condition, msg) MLOG_IF (level, condition, msg)
 
 #endif
+  }
+}
 
-}}
-#endif   /* ----- #ifndef FHG_LOG_LOGMACROS_INC  ----- */
+#endif
