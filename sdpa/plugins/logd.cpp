7b0f9f782b47a4c1796122253ad88ac3f81d33a9
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-12-26 13:00:36 +0100

    sdpa: plugin logd: use fhglog/appender/call instead of home grown FunctioAppender

--- sdpa/plugins/logd.cpp
+++ sdpa/plugins/logd.cpp
@@ -2,35 +2,13 @@
 
 #include <boost/function.hpp>
 
+#include <fhglog/appender/call.hpp>
 #include <fhglog/remote/server.hpp>
 #include <fhglog/Appender.hpp>
 #include <fhglog/fhglog.hpp>
 #include <fhg/plugin/plugin.hpp>
 
 #include <fhg/util/threadname.hpp>
-
-namespace detail
-{
-  class FunctionAppender : public fhg::log::Appender
-  {
-  public:
-    typedef boost::function<void (const fhg::log::LogEvent&)> event_handler_t;
-
-    FunctionAppender (event_handler_t h)
-      : m_handler (h)
-    {}
-
-    void append (fhg::log::LogEvent const &evt)
-    {
-      m_handler(evt);
-    }
-
-    void flush() {}
-  private:
-    event_handler_t m_handler;
-    //    unsigned short m_port;
-  };
-}
 
 typedef boost::shared_ptr<fhg::log::remote::LogServer> logserver_t;
 
@@ -44,7 +22,7 @@ public:
 
     m_log_server = logserver_t
       (new fhg::log::remote::LogServer
-      ( fhg::log::Appender::ptr_t (new detail::FunctionAppender
+      ( fhg::log::Appender::ptr_t (new fhg::log::appender::call
                                   ( boost::bind( &LogdPluginImpl::emit
                                                , this
                                                , _1
