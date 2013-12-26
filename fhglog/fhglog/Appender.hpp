a0f45fef087e51cf71a217607172bf2fb2fab8d3
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-12-26 01:10:02 +0100

    fhglog: appender: cosmetics: added author line, whitespace, no parameter names

--- fhglog/fhglog/Appender.hpp
+++ fhglog/fhglog/Appender.hpp
@@ -1,20 +1,27 @@
+// alexander.petry@itwm.fraunhofer.de
+
 #ifndef FHG_LOG_APPENDER_HPP
 #define FHG_LOG_APPENDER_HPP 1
 
-#include    <fhglog/event.hpp>
+#include <fhglog/event.hpp>
 
 #include <boost/shared_ptr.hpp>
 
-namespace fhg { namespace log {
-  class Appender {
+namespace fhg
+{
+  namespace log
+  {
+    class Appender
+    {
     public:
       typedef boost::shared_ptr<Appender> ptr_t;
 
       virtual ~Appender() {}
 
-      virtual void append(const LogEvent &evt) = 0;
-      virtual void flush (void) = 0;
-  };
-}}
+      virtual void append (const LogEvent&) = 0;
+      virtual void flush() = 0;
+    };
+  }
+}
 
 #endif
