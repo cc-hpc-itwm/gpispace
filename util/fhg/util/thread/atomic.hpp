bd6eae2c4ed19901f2938a61b0221a8f4ff7c85c
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-12-26 20:24:02 +0100

    util: strip down thread/atomic to just preinc and cast

--- util/fhg/util/thread/atomic.hpp
+++ util/fhg/util/thread/atomic.hpp
@@ -6,6 +6,7 @@
 #else
 #  include <boost/thread/mutex.hpp>
 #  include <boost/thread/locks.hpp>
+#  include <boost/utility.hpp>
 #endif
 
 namespace fhg
@@ -16,85 +17,35 @@ namespace fhg
     using std::atomic;
 #else
     template <typename value_type>
-    class atomic
+    class atomic : boost::noncopyable
     {
       typedef boost::lock_guard<boost::mutex> lock_type;
+
     public:
-      atomic ()
-        : m_value ()
+      atomic()
+        : _value()
       {}
 
       explicit
       atomic (value_type v)
-        : m_value (v)
+        : _value (v)
       {}
 
-      operator value_type () const
+      operator value_type() const
       {
-        lock_type lock (m_mutex);
-        return m_value;
+        lock_type const _ (_mutex);
+        return _value;
       }
 
       value_type operator++ ()
       {
-        lock_type lock (m_mutex);
-        ++m_value;
-        return m_value;
+        lock_type const _ (_mutex);
+        return ++_value;
       }
 
-      value_type operator++ (int)
-      {
-        lock_type lock (m_mutex);
-        value_type old (m_value);
-        ++m_value;
-        return old;
-      }
-
-      template <typename U>
-      atomic<value_type> & operator+= (U u)
-      {
-        lock_type lock (m_mutex);
-        m_value += u;
-        return *this;
-      }
-
-      template <typename U>
-      atomic<value_type> & operator-= (U u)
-      {
-        lock_type lock (m_mutex);
-        m_value -= u;
-        return *this;
-      }
-
-      template <typename U>
-      atomic<value_type> & operator= (U u)
-      {
-        lock_type lock (m_mutex);
-        m_value = u;
-        return *this;
-      }
-
-      atomic<value_type> & operator-- ()
-      {
-        lock_type lock (m_mutex);
-        --m_value;
-        return *this;
-      }
-
-      atomic<value_type> operator-- (int)
-      {
-        lock_type lock (m_mutex);
-        value_type old (m_value);
-        --m_value;
-        return atomic<value_type> (old);
-      }
     private:
-      //! \note Can't use boost::noncopyable, as there is an operator=<T>.
-      atomic (const atomic &);
-      atomic & operator=(const atomic &);
-
-      mutable boost::mutex m_mutex;
-      value_type m_value;
+      mutable boost::mutex _mutex;
+      value_type _value;
     };
 #endif
   }
