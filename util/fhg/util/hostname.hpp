1f090d2893f45b2f180d4268078c3bb0d72a1280
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-12-27 10:46:41 +0100

    util: added hostname

---
+++ util/fhg/util/hostname.hpp
@@ -0,0 +1,16 @@
+// mirko.rahn@itwm.fraunhofer.de
+
+#ifndef FHG_UTIL_HOSTNAME
+#define FHG_UTIL_HOSTNAME
+
+#include <string>
+
+namespace fhg
+{
+  namespace util
+  {
+    std::string const& hostname();
+  }
+}
+
+#endif
