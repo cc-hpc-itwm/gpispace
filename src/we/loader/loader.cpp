f6010cc098765ef9da323625048d545a707b0c60
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2013-12-28 01:11:54 +0100

    we: loader: operator[]: cosmetics: whitespace, ctor instead of assignment in decl, less else

--- src/we/loader/loader.cpp
+++ src/we/loader/loader.cpp
@@ -34,25 +34,29 @@ namespace we {
       }
     }
 
-    Module & loader::operator[] (const std::string &module)
+    Module& loader::operator[] (const std::string& module)
     {
-      boost::unique_lock<boost::recursive_mutex> lock(mtx_);
-      module_table_t::const_iterator mod = module_table_.find(module);
-      if (mod != module_table_.end()) {
+      boost::unique_lock<boost::recursive_mutex> const _ (mtx_);
+
+      module_table_t::const_iterator mod (module_table_.find (module));
+
+      if (mod != module_table_.end())
+      {
         return *mod->second;
-      } else {
-        const  boost::filesystem::path file_name ("lib" + module + ".so");
-
-        BOOST_FOREACH (boost::filesystem::path const& p, search_path_)
-        {
-          if (boost::filesystem::exists (p / file_name))
-          {
-            return *load (module, p / file_name);
-          }
-        }
-
-        throw ModuleLoadFailed("module '" + module + "' could not be located", module, "[not-found]");
       }
+
+      const boost::filesystem::path file_name ("lib" + module + ".so");
+
+      BOOST_FOREACH (boost::filesystem::path const& p, search_path_)
+      {
+        if (boost::filesystem::exists (p / file_name))
+        {
+          return *load (module, p / file_name);
+        }
+      }
+
+      throw ModuleLoadFailed
+        ("module '" + module + "' could not be located", module, "[not-found]");
     }
 
     loader::module_ptr_t loader::load (const boost::filesystem::path & path)
