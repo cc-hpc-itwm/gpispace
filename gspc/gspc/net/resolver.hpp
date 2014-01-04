f6ff13d2ef747944fbb1d2ffc23a53f9f50ad6be
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-01-04 19:41:56 +0100

    gspc: let gspc::net::resolver::resolve only forward to exception based implementation and catch exceptions, converting to error code. This is the usual exception-to-error-code unwrapping that can (and shall) easily be factored out.

--- gspc/gspc/net/resolver.hpp
+++ gspc/gspc/net/resolver.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <boost/system/error_code.hpp>
+#include <boost/system/system_error.hpp>
 
 namespace gspc
 {
@@ -16,7 +17,21 @@ namespace gspc
       static
       endpoint_type resolve ( std::string const &address
                             , boost::system::error_code &ec
-                            );
+                            )
+      {
+        try
+        {
+          ec = boost::system::errc::make_error_code
+            (boost::system::errc::success);
+          return resolve (address);
+        }
+        catch (const boost::system::system_error& ex)
+        {
+          ec = ex.code();
+          return endpoint_type();
+        }
+      }
+
       static
       endpoint_type resolve (std::string const &address);
     };
