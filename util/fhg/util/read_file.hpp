8f67bd592d5073d7a1fb566707c6815fb1c3e14a
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-01-05 18:22:56 +0100

    util: read_file: take boost::filesystem::path as argument not std::string, std::string is casted but not the other way around

--- util/fhg/util/read_file.hpp
+++ util/fhg/util/read_file.hpp
@@ -1,13 +1,15 @@
 #ifndef FHG_UTIL_READ_FILE_HPP
 #define FHG_UTIL_READ_FILE_HPP
 
+#include <boost/filesystem.hpp>
+
 #include <string>
 
 namespace fhg
 {
   namespace util
   {
-    std::string read_file (std::string const &path);
+    std::string read_file (boost::filesystem::path const&);
   }
 }
 
