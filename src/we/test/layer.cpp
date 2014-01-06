8a082be5e0054a45e15983a55cbe89e2d0be43e3
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-01-06 12:34:48 +0100

    we: layer: test: implement operator==

--- src/we/test/layer.cpp
+++ src/we/test/layer.cpp
@@ -14,14 +14,123 @@
 
 namespace we
 {
+  namespace type
+  {
+    namespace property
+    {
+      bool operator== (type const& lhs, type const& rhs)
+      {
+        return lhs.get_map() == rhs.get_map();
+      }
+    }
+  }
+}
+
+namespace condition
+{
+  bool operator== (type const& lhs, type const& rhs)
+  {
+    return lhs.expression() == rhs.expression();
+  }
+}
+
+namespace place
+{
+  bool operator== (type const& lhs, type const& rhs)
+  {
+    return lhs.name() == rhs.name()
+      && lhs.signature() == rhs.signature()
+      && lhs.property() == rhs.property();
+  }
+}
+
+namespace petri_net
+{
+  namespace
+  {
+    std::map< petri_net::place_id_type
+            , std::list<pnet::type::value::value_type>
+            > tokens (net const& n)
+    {
+      std::map< petri_net::place_id_type
+              , std::list<pnet::type::value::value_type>
+              > tokens_on_place;
+
+      BOOST_FOREACH ( petri_net::place_id_type const& place_id
+                    , n.places() | boost::adaptors::map_keys
+                    )
+      {
+        tokens_on_place.insert
+          (std::make_pair (place_id, n.get_token (place_id)));
+      }
+
+      return tokens_on_place;
+    }
+  }
+
+  bool operator== (net const& lhs, net const& rhs)
+  {
+    return lhs.places() == rhs.places()
+      && lhs.transitions() == rhs.transitions()
+      && lhs.transition_to_place() == rhs.transition_to_place()
+      && lhs.place_to_transition() == rhs.place_to_transition()
+      && tokens (lhs) == tokens (rhs);
+  }
+}
+
+namespace we
+{
+  namespace type
+  {
+    bool operator== (port_t const& lhs, port_t const& rhs)
+    {
+      return lhs.name() == rhs.name()
+        && lhs.direction() == rhs.direction()
+        && lhs.signature() == rhs.signature()
+        && lhs.associated_place() == rhs.associated_place()
+        && lhs.property() == rhs.property();
+    }
+
+    bool operator== (requirement_t const& lhs, requirement_t const& rhs)
+    {
+      return lhs.value() == rhs.value()
+        && lhs.is_mandatory() == rhs.is_mandatory();
+    }
+
+    bool operator== (module_call_t const& lhs, module_call_t const& rhs)
+    {
+      return lhs.module() == rhs.module()
+        && lhs.function() == rhs.function();
+    }
+
+    bool operator== (expression_t const& lhs, expression_t const& rhs)
+    {
+      return lhs.expression() == rhs.expression();
+    }
+
+    bool operator== (transition_t const& lhs, transition_t const& rhs)
+    {
+      return lhs.name() == rhs.name()
+        && lhs.data() == rhs.data()
+        && lhs.is_internal() == rhs.is_internal()
+        && lhs.condition() == rhs.condition()
+        && lhs.outer_to_inner() == rhs.outer_to_inner()
+        && lhs.inner_to_outer() == rhs.inner_to_outer()
+        && lhs.ports() == rhs.ports()
+        && lhs.prop() == rhs.prop()
+        && lhs.requirements() == rhs.requirements();
+    }
+  }
+
   namespace mgmt
   {
     namespace type
     {
       bool operator== (activity_t const& lhs, activity_t const& rhs)
       {
-        //! \todo really implement, to_string is unordered!
-        return lhs.to_string() == rhs.to_string();
+        return lhs.input() == rhs.input()
+          && lhs.output() == rhs.output()
+          && lhs.transition() == rhs.transition();
       }
     }
   }
