49d9aca44be2f3060c3a0706140971122461881e
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-01-09 00:12:25 +0100

    util: revision: shorten header

--- util/fhg/revision.hpp
+++ util/fhg/revision.hpp
@@ -1,20 +1,4 @@
-/*
- * =====================================================================================
- *
- *       Filename:  revision.hpp
- *
- *    Description:  just contains the revision information of the project
- *
- *        Version:  1.0
- *        Created:  02/20/2011 01:06:13 AM
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Alexander Petry (petry), alexander.petry@itwm.fraunhofer.de
- *        Company:  Fraunhofer ITWM
- *
- * =====================================================================================
- */
+// alexander.petry@itwm.fraunhofer.de
 
 #ifndef FHG_UTIL_REVISION_HPP
 #define FHG_UTIL_REVISION_HPP 1
