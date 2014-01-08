3058f22ed5dff440cf8628fa670dc7ec08df663d
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-01-08 16:45:35 +0100

    sdpa: remove cpp of JobId: is only a really tight wrapper around string and should be eliminated anyway

--- sdpa/sdpa/JobId.hpp
+++ sdpa/sdpa/JobId.hpp
@@ -9,28 +9,41 @@
 namespace sdpa {
   class JobId {
   public:
-    JobId();
-    JobId(const std::string &s);
-    JobId(const char *s);
+    //! \todo Remove default ctor: used for serialization only
+    JobId()
+      : id_()
+    {}
+
+    JobId(const std::string &s)
+      : id_(s)
+    {}
+
+    JobId(const char *s)
+      : id_(s)
+    {}
+
+    bool operator==(const JobId &rhs) const
+    {
+      return id_ == rhs.id_;
+    }
+    bool operator!=(const JobId &rhs) const
+    {
+      return !(*this == rhs);
+    }
+
+    bool operator<(const JobId &rhs) const
+    {
+      return id_ < rhs.id_;
+    }
 
     operator std::string() const { return id_; }
-
-    bool operator!=(const JobId& rhs) const;
-    bool operator==(const JobId& rhs) const;
-
-    bool operator<(const JobId& rhs) const;
-
     const std::string &str() const { return id_; }
-
-    static JobId invalid_job_id() { return ""; }
-    static bool is_invalid_job_id(const JobId &job_id) { return job_id.str().empty(); }
 
     template <class Archive>
     void serialize(Archive& ar, unsigned int /* version */)
     {
       ar &  boost::serialization::make_nvp("JobID", id_);
     }
-
 
   private:
     std::string id_;
