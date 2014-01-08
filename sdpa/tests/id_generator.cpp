b99054148a659d0cb74a00b257436c9c3c89e95e
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-01-08 17:15:01 +0100

    sdpa: tests: move sdpa/test_JobId to id_generator: does not test job ids at all

--- sdpa/tests/id_generator.cpp
+++ sdpa/tests/id_generator.cpp
@@ -1,16 +1,15 @@
-#define BOOST_TEST_MODULE job_id
-
+#define BOOST_TEST_MODULE id_generator
 #include <boost/test/unit_test.hpp>
-#include <sdpa/types.hpp>
+
 #include <sdpa/id_generator.hpp>
 
 BOOST_AUTO_TEST_CASE (id_generator)
 {
   sdpa::id_generator generator ("job");
 
-  const sdpa::job_id_t jid1 (generator.next());
-  const sdpa::job_id_t jid2 (generator.next());
-  const sdpa::job_id_t jid3 (generator.next());
+  const std::string jid1 (generator.next());
+  const std::string jid2 (generator.next());
+  const std::string jid3 (generator.next());
 
   BOOST_REQUIRE_NE (jid1, jid2);
   BOOST_REQUIRE_NE (jid1, jid3);
