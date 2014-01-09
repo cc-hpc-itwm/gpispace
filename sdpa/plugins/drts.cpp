6564d25de095050a189dda69291980a8f77f44bc
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-01-09 17:37:09 +0100

    sdpa: remove drts_on_cancel: untested, unused

--- sdpa/plugins/drts.cpp
+++ sdpa/plugins/drts.cpp
@@ -3,7 +3,6 @@
 #include "job.hpp"
 #include "wfe.hpp"
 #include "observable.hpp"
-#include "drts_callbacks.h"
 
 #include <errno.h>
 
@@ -654,7 +653,6 @@ public:
       {
         MLOG (TRACE, "trying to cancel running job " << e->job_id());
         m_wfe->cancel (e->job_id());
-        drts_on_cancel ();
       }
       else if (job_it->second->state() == drts::Job::FAILED)
       {
@@ -804,7 +802,6 @@ private:
       }
 
       { lock_type lck (m_job_in_progress_mutex); m_job_in_progress = false; }
-      drts_on_cancel_clear ();
 
       job_ptr_t job = m_pending_jobs.get();
 
