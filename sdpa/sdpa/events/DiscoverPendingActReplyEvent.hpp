e999961e8abb2091bf05234b79649e0fc905c807
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2014-01-09 17:34:30 +0100

    sdpa: added data members, access methods and serialization support to DiscoverPendingActReplyEvent

--- sdpa/sdpa/events/DiscoverPendingActReplyEvent.hpp
+++ sdpa/sdpa/events/DiscoverPendingActReplyEvent.hpp
@@ -2,6 +2,9 @@
 #define SDPA_DISCOVER_PENDING_ACT_REPLY_EVENT_HPP 1
 
 #include <sdpa/events/JobEvent.hpp>
+#include <we/type/value/show.hpp>
+#include <we/type/value/read.hpp>
+#include <sstream>
 
 namespace sdpa
 {
@@ -15,8 +18,12 @@ namespace sdpa
       DiscoverPendingActReplyEvent ( const address_t& a_from
                                     , const address_t& a_to
                                     , const sdpa::job_id_t& a_job_id
+                                    , const std::string& discover_id
+                                    , const pnet::type::value::value_type& discover_result
                                     )
         : sdpa::events::JobEvent (a_from, a_to, a_job_id)
+        , discover_id_(discover_id)
+        , discover_result_(discover_result)
       {}
 
       std::string str() const
@@ -28,9 +35,35 @@ namespace sdpa
       {
         //handler->handleDiscoverPendingActReplyEvent (this);
       }
+
+      const std::string& discover_id() const { return discover_id_; }
+      const pnet::type::value::value_type& discover_result() const
+      {
+        return discover_result_;
+      }
+    private:
+      std::string discover_id_;
+      pnet::type::value::value_type discover_result_;
     };
 
-    CONSTRUCT_DATA_DEFS_FOR_EMPTY_JOBEVENT_OVERLOAD (DiscoverPendingActReplyEvent)
+    SAVE_CONSTRUCT_DATA_DEF (DiscoverPendingActReplyEvent, e)
+     {
+       SAVE_JOBEVENT_CONSTRUCT_DATA (e);
+       SAVE_TO_ARCHIVE (e->discover_id());
+       std::ostringstream oss;
+       oss << pnet::type::value::show (e->discover_result());
+       std::string res(oss.str());
+       SAVE_TO_ARCHIVE (res);
+     }
+
+     LOAD_CONSTRUCT_DATA_DEF (DiscoverPendingActReplyEvent, e)
+     {
+       LOAD_JOBEVENT_CONSTRUCT_DATA (from, to, job_id);
+       LOAD_FROM_ARCHIVE (std::string, disc_id);
+       LOAD_FROM_ARCHIVE (std::string, res);
+       pnet::type::value::value_type disc_res(pnet::type::value::read (res));
+       ::new (e) DiscoverPendingActReplyEvent (from, to, job_id, disc_id, disc_res);
+     }
   }
 }
 
