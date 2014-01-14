04d62ec1cc821837e6a52c9bc8bbc5e677101c32
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-01-14 17:06:36 +0100

    util: tests: use hard number for number of threads to use in test to avoid testing a thread pool with only one thread

--- util/tests/pool.cpp
+++ util/tests/pool.cpp
@@ -8,7 +8,6 @@
 #include <vector>
 
 #include <fhg/util/thread/atomic.hpp>
-#include <fhg/util/get_cpucount.h>
 #include <fhg/util/thread/pool.hpp>
 
 static fhg::thread::atomic<int> s_work_count (0);
@@ -24,12 +23,9 @@ BOOST_AUTO_TEST_CASE (simple_pool)
   static const size_t NUM_ITERATIONS = 100;
 
   using namespace fhg::thread;
-  int ncpu = fhg_get_cpucount ();
-  if (ncpu < 0)
-    ncpu = 1;
 
   {
-    pool_t pool (ncpu);
+    pool_t pool (13);
 
     for (size_t i = 0 ; i < NUM_ITERATIONS; ++i)
       pool.execute (s_work);
