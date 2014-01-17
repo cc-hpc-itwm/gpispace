cc4774c376e9c5dc9c7b8d0ba1d0d589ca8ad0f4
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-01-17 12:16:45 +0100

    drts: remove unused includes, typedefs and mutex

--- src/drts/worker/master.hpp
+++ src/drts/worker/master.hpp
@@ -2,23 +2,14 @@
 #define DRTS_MASTER_HPP 1
 
 #include <string>
-#include <list>
-#include <ctime>
 
-#include <boost/thread.hpp>
-#include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/utility.hpp>
 
 namespace drts
 {
   class Master : boost::noncopyable
   {
-    typedef boost::mutex mutex_type;
-    typedef boost::condition_variable condition_type;
-    typedef boost::unique_lock<mutex_type> lock_type;
-    typedef boost::posix_time::ptime time_type;
   public:
-
     explicit Master(std::string const& name);
 
     inline bool is_connected () const
@@ -34,8 +25,6 @@ namespace drts
     std::string const & name() const { return m_name; }
 
   private:
-    mutable mutex_type m_stats_mutex;
-
     std::string m_name;
     bool _is_connected;
   };
