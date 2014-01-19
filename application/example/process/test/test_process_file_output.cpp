18ba5771ddd510b60f062317028aa1fb99f69988
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-01-19 23:11:20 +0100

    test_process_file_output: made BOOST_UNIT_TEST

--- application/example/process/test/test_process_file_output.cpp
+++ application/example/process/test/test_process_file_output.cpp
@@ -1,21 +1,16 @@
+// mirko.rahn@itwm.fraunhofer.de
+
+#define BOOST_TEST_MODULE process_file_output
+#include <boost/test/unit_test.hpp>
 
 #include <process.hpp>
-#include <iostream>
-#include <cstdlib>
 
-#include <require.hpp>
-
-int
-main (int argc, char ** argv)
+BOOST_AUTO_TEST_CASE (process_file_output)
 {
-  if (argc < 2)
-    {
-      std::cerr << "usage: " << argv[0] << " path_to_deinterleave" << std::endl;
+  BOOST_REQUIRE_GT (boost::unit_test::framework::master_test_suite().argc, 1);
 
-      exit (EXIT_FAILURE);
-    }
-
-  const std::string prog (argv[1]);
+  const std::string prog
+    (boost::unit_test::framework::master_test_suite().argv[1]);
   const std::string command (prog + " %FILE1% %FILE2%");
 
   char buf[11] = "1a2b3c4d5e";
@@ -38,19 +33,15 @@ main (int argc, char ** argv)
                    , files_output
                    );
 
-  REQUIRE (part1[0] == '1');
-  REQUIRE (part1[1] == '2');
-  REQUIRE (part1[2] == '3');
-  REQUIRE (part1[3] == '4');
-  REQUIRE (part1[4] == '5');
+  BOOST_REQUIRE_EQUAL (part1[0], '1');
+  BOOST_REQUIRE_EQUAL (part1[1], '2');
+  BOOST_REQUIRE_EQUAL (part1[2], '3');
+  BOOST_REQUIRE_EQUAL (part1[3], '4');
+  BOOST_REQUIRE_EQUAL (part1[4], '5');
 
-  REQUIRE (part2[0] == 'a');
-  REQUIRE (part2[1] == 'b');
-  REQUIRE (part2[2] == 'c');
-  REQUIRE (part2[3] == 'd');
-  REQUIRE (part2[4] == 'e');
-
-  std::cout << "SUCCESS" << std::endl;
-
-  return EXIT_SUCCESS;
+  BOOST_REQUIRE_EQUAL (part2[0], 'a');
+  BOOST_REQUIRE_EQUAL (part2[1], 'b');
+  BOOST_REQUIRE_EQUAL (part2[2], 'c');
+  BOOST_REQUIRE_EQUAL (part2[3], 'd');
+  BOOST_REQUIRE_EQUAL (part2[4], 'e');
 }
