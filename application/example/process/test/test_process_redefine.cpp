b018161800128992e9b47b161d84ad43d3b41826
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-01-19 23:46:04 +0100

    test_process_redefine: use fhg::util::boost::test::require_exception

--- application/example/process/test/test_process_redefine.cpp
+++ application/example/process/test/test_process_redefine.cpp
@@ -5,29 +5,34 @@
 
 #include <process.hpp>
 
-BOOST_AUTO_TEST_CASE (process_redefine)
+#include <fhg/util/boost/test/require_exception.hpp>
+
+namespace
 {
-  process::file_const_buffer file (NULL, 0, "%redefinition%");
-
-  process::file_const_buffer_list files_input;
-
-  files_input.push_back (file);
-  files_input.push_back (file);
-
-  try
+  //! \todo use boost::bind below
+  struct _
+  {
+    void operator() () const
     {
+      process::file_const_buffer file (NULL, 0, "%redefinition%");
+
+      process::file_const_buffer_list files_input;
+
+      files_input.push_back (file);
+      files_input.push_back (file);
+
       process::execute ( std::string()
-                       , process::const_buffer (NULL,0)
-                       , process::buffer (NULL,0)
+                       , process::const_buffer (NULL, 0)
+                       , process::buffer (NULL, 0)
                        , files_input
-                       , process::file_buffer_list ()
+                       , process::file_buffer_list()
                        );
     }
-  catch (const std::runtime_error & e)
-    {
-      BOOST_REQUIRE_EQUAL
-        ( std::string (e.what())
-        , std::string ("redefinition of key: %redefinition%")
-        );
-    }
+  };
+}
+
+BOOST_AUTO_TEST_CASE (process_redefine)
+{
+  fhg::util::boost::test::require_exception<std::runtime_error>
+    (_(), "redefinition of key: %redefinition%");
 }
