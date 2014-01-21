862b2cb2f2362fc4cf1f7de17bc2fba7d6bb12b1
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-01-21 15:48:47 +0100

    sdpa: plugins: logc: only include one file for std::size_t

--- sdpa/plugins/logc.hpp
+++ sdpa/plugins/logc.hpp
@@ -1,9 +1,7 @@
 #ifndef SDPA_PLUGINS_LOGC_HPP
 #define SDPA_PLUGINS_LOGC_HPP 1
 
-#include <unistd.h>
-#include <stdint.h>
-#include <inttypes.h>
+#include <cstddef>
 
   void fhg_emit_log_message ( const char *filename
                             , const char *function
