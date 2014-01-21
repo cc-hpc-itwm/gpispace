3cccecb85ffcf36c9d62309991a95e7cbebbffaa
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-01-21 16:03:50 +0100

    drts: inline typedef and let scoped locks be unnamed and const

--- src/drts/worker/drts.cpp
+++ src/drts/worker/drts.cpp
@@ -1808,25 +1808,22 @@ private:
 typedef std::pair<DrtsCancelHandler, void*> handler_t;
 typedef std::list<handler_t> handler_list_t;
 
-typedef boost::mutex mutex_type;
-typedef boost::unique_lock<mutex_type> lock_type;
-
 namespace
 {
   handler_list_t s_handler_list;
-  mutex_type s_handler_list_mtx;
+  boost::mutex s_handler_list_mtx;
 }
 
 int drts_on_cancel_add (DrtsCancelHandler h, void *data)
 {
-  lock_type lck (s_handler_list_mtx);
+  boost::mutex::scoped_lock const _ (s_handler_list_mtx);
   s_handler_list.push_back (std::make_pair (h, data));
   return 0;
 }
 
 void drts_on_cancel_clear ()
 {
-  lock_type lck (s_handler_list_mtx);
+  boost::mutex::scoped_lock const _ (s_handler_list_mtx);
   s_handler_list.clear ();
 }
 
@@ -1836,7 +1833,7 @@ int drts_on_cancel ()
 
   handler_list_t to_call;
   {
-    lock_type lck (s_handler_list_mtx);
+    boost::mutex::scoped_lock const _ (s_handler_list_mtx);
     to_call.swap (s_handler_list);
   }
 
