c8babc03034f0fcaf6377ecb8b3e9799a8dfd4a4
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2014-01-22 16:18:29 +0100

    sdpa: added 2 test cases for the serialization of the discovery_info_t structure

--- sdpa/tests/sdpa/test_DiscoverJobStates.cpp
+++ sdpa/tests/sdpa/test_DiscoverJobStates.cpp
@@ -10,3 +10,44 @@ BOOST_GLOBAL_FIXTURE (KVSSetup)
 
 static const boost::posix_time::milliseconds discover_interval (100);
 static const int n_discover_ops(3);
+
+BOOST_AUTO_TEST_CASE (serialization_discovery_info_1)
+{
+  std::stringstream sstr;
+  boost::archive::text_oarchive oar (sstr);
+  sdpa::discovery_info_t disc_info("job_0", boost::none, sdpa::discovery_info_set_t());
+
+  oar << disc_info;
+
+  sdpa::discovery_info_t restored_disc_info;
+  std::istringstream isstr (sstr.str());
+  boost::archive::text_iarchive iar (sstr);
+
+  iar >> restored_disc_info;
+  BOOST_REQUIRE(disc_info == restored_disc_info);
+}
+
+BOOST_AUTO_TEST_CASE (serialization_discovery_info_2)
+{
+  std::stringstream sstr;
+  boost::archive::text_oarchive oar (sstr);
+  sdpa::discovery_info_t disc_info_child_1("job_0_1", sdpa::status::PENDING, sdpa::discovery_info_set_t());
+  sdpa::discovery_info_t disc_info_child_2("job_0_2", sdpa::status::FINISHED, sdpa::discovery_info_set_t());
+  sdpa::discovery_info_t disc_info_child_3("job_0_3", sdpa::status::FAILED, sdpa::discovery_info_set_t());
+
+  sdpa::discovery_info_set_t disc_info_set;
+  disc_info_set.insert(disc_info_child_1);
+  disc_info_set.insert(disc_info_child_2);
+  disc_info_set.insert(disc_info_child_3);
+
+  sdpa::discovery_info_t disc_info("job_0", boost::none, disc_info_set);
+
+  oar << disc_info;
+
+  sdpa::discovery_info_t restored_disc_info;
+  std::istringstream isstr (sstr.str());
+  boost::archive::text_iarchive iar (sstr);
+
+  iar >> restored_disc_info;
+  BOOST_REQUIRE(disc_info == restored_disc_info);
+}
