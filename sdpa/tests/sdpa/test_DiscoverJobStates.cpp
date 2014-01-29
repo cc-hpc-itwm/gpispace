f7a4ed92640b010813a62986439ae87b1e8bf850
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2014-01-29 14:02:51 +0100

    sdpa: added new test cases in test_DiscoverJobStates, updated the existing ones

--- sdpa/tests/sdpa/test_DiscoverJobStates.cpp
+++ sdpa/tests/sdpa/test_DiscoverJobStates.cpp
@@ -11,9 +11,6 @@
 
 BOOST_GLOBAL_FIXTURE (KVSSetup)
 
-static const boost::posix_time::milliseconds discover_interval (100);
-static const int n_discover_ops(3);
-
 boost::mutex generate_id_mutex;
 we::layer::id_type generate_id()
 {
@@ -27,6 +24,7 @@ namespace sdpa {
     class TestAgent : public Agent
     {
     public:
+      typedef std::pair<we::layer::id_type, sdpa::discovery_info_t> pair_discovery_result_t;
       TestAgent( const std::string& name
                 , const std::string& url
                 , const sdpa::master_info_list_t arrMasterNames
@@ -47,54 +45,92 @@ namespace sdpa {
             );
       }
 
-      int n_child_jobs() { return _list_child_jobs.size();}
       std::string gen_id() { return GenericDaemon::gen_id(); }
       void discover (we::layer::id_type discover_id, we::layer::id_type job_id)
       {
-        BOOST_REQUIRE(std::find(_list_child_jobs.begin(), _list_child_jobs.end(), job_id) != _list_child_jobs.end());
+        boost::unique_lock<boost::mutex> const _ (_mtx_disc);
+        _jobs_to_discover.push_back(std::make_pair(discover_id, job_id));
+        _cond_disc.notify_all();
       }
 
       void discovered (we::layer::id_type discover_id, sdpa::discovery_info_t discover_result)
       {
-        BOOST_FOREACH(const sdpa::discovery_info_t& child_info, discover_result.children())
+        boost::unique_lock<boost::mutex> lock_res(_mtx_result);
+        _discovery_result = discover_result;
+        _cond_result.notify_one();
+      }
+
+      void submit( const we::layer::id_type& activityId, const we::type::activity_t& activity                         )
+      {
+        GenericDaemon::submit(activityId, activity);
+        if(jobManager().numberOfJobs()<2)
+          _cond_all_submitted.notify_one();
+      }
+
+      void wait_all_submitted()
+      {
+        boost::unique_lock<boost::mutex> const _(_mtx_all_submitted);
+        _cond_all_submitted.wait(_mtx_all_submitted);
+      }
+
+      void notify_discovered()
+      {
+        while(true)
         {
-           BOOST_REQUIRE_EQUAL(child_info.state(), sdpa::status::PENDING);
+          boost::unique_lock<boost::mutex> lock(_mtx_disc);
+          _cond_disc.timed_wait(lock, boost::posix_time::seconds(1));
+
+          if(!_jobs_to_discover.empty())
+          {
+            std::pair<we::layer::id_type, sdpa::job_id_t> pair = _jobs_to_discover.front();
+            _jobs_to_discover.pop_front();
+
+            Job* pJob = jobManager().findJob(pair.second);
+            sdpa::discovery_info_t discover_result(pair.second, pJob?boost::optional<sdpa::status::code>(pJob->getStatus()):boost::none, sdpa::discovery_info_set_t());
+            workflowEngine()->discovered(pair.first, discover_result);
+          }
         }
       }
 
-      void submit( const we::layer::id_type& activityId, const we::type::activity_t& activity )
+      void wait_for_discovery_result()
       {
-        _list_child_jobs.push_back(activityId);
-
-        if(_list_child_jobs.size()==2)
-          _cond_wait_submit.notify_all();
+        boost::unique_lock<boost::mutex> lock_res(_mtx_result);
+        _cond_result.wait(lock_res);
       }
 
-      void wait()
+      bool test_invariant()
       {
-        boost::mutex::scoped_lock lock(_mtx_submit);
-        _cond_wait_submit.wait(lock);
-      }
+        if(_discovery_result.children().size()<2)
+          return false;
 
-      void notify_discovered_activities(we::layer::id_type discover_id)
-      {
-        BOOST_FOREACH(const sdpa::job_id_t& job_id, _list_child_jobs)
+        bool b_invariant(true);
+        BOOST_FOREACH(const sdpa::discovery_info_t& child_info, _discovery_result.children())
         {
-          sdpa::discovery_info_t discover_result(job_id, sdpa::status::PENDING, sdpa::discovery_info_set_t());
-          workflowEngine()->discovered(discover_id, discover_result);
+          if(child_info.state() != sdpa::status::PENDING)
+          {
+              b_invariant = false;
+              break;
+          }
         }
+
+        return b_invariant;
       }
 
     private:
-      std::list< we::layer::id_type> _list_child_jobs;
-      mutable boost::mutex _mtx_submit;
-      boost::condition_variable_any _cond_wait_submit;
+      boost::mutex _mtx_disc;
+      boost::mutex _mtx_result;
+      boost::mutex _mtx_all_submitted;
+      boost::condition_variable_any _cond_disc;
+      boost::condition_variable_any _cond_result;
+      boost::condition_variable_any _cond_all_submitted;
+      std::deque<std::pair<we::layer::id_type, sdpa::job_id_t> > _jobs_to_discover;
+      sdpa::discovery_info_t _discovery_result;
     };
 }}
 
-BOOST_AUTO_TEST_CASE(test_activity_1)
+BOOST_AUTO_TEST_CASE(test_discover_activities)
 {
-  const we::layer::id_type discovery_id("disc_id_0");
+
   const std::string workflow
      (utils::require_and_read_file ("workflows/coallocation_test2.pnet"));
 
@@ -106,11 +142,24 @@ BOOST_AUTO_TEST_CASE(test_activity_1)
   we::layer::id_type const id (generate_id());
 
   agent.workflowEngine()->submit (id, activity);
+  //! Note it seems to have a race condition somewhere if we call layer submit and
+  // layer discover immediately (deadlock)
+  agent.wait_all_submitted();
 
-  agent.wait();
+  boost::thread thrd_notify(boost::thread(&sdpa::daemon::TestAgent::notify_discovered, &agent));
 
-  agent.workflowEngine()->discover (discovery_id, id);
-  agent. notify_discovered_activities(discovery_id);
+  int i=0;
+  while(!agent.test_invariant())
+  {
+      std::ostringstream oss;
+      oss<<"disc_id_"<<i++;
+      agent.workflowEngine()->discover (oss.str(), id);
+      agent.wait_for_discovery_result();
+  }
+
+  thrd_notify.interrupt();
+  if(thrd_notify.joinable())
+    thrd_notify.join();
 }
 
 BOOST_AUTO_TEST_CASE (serialization_discovery_info_1)
@@ -182,15 +231,16 @@ BOOST_AUTO_TEST_CASE (discover_one_orchestrator_no_agent)
 
   sdpa::discovery_info_t disc_res;
 
-  for(int i=0; i<n_discover_ops; i++)
+  int i=0;
+  bool b_invariant(false);
+  while(!b_invariant)
   {
     std::ostringstream oss;
-    oss<<"disc_id_"<<i;
+    oss<<"disc_id_"<<i++;
     disc_res = (client.discoverJobStates(oss.str(), job_id));
-    boost::this_thread::sleep (discover_interval);
-  }
 
-  BOOST_REQUIRE_EQUAL(disc_res.state(), sdpa::status::PENDING);
+    b_invariant = (disc_res.state() == sdpa::status::PENDING);
+  }
 }
 
 BOOST_AUTO_TEST_CASE (discover_one_orchestrator_one_agent)
@@ -207,20 +257,28 @@ BOOST_AUTO_TEST_CASE (discover_one_orchestrator_one_agent)
   sdpa::client::Client client (orchestrator.name());
   sdpa::job_id_t job_id = client.submitJob (workflow);
 
+  int i=0;
   sdpa::discovery_info_t disc_res;
-  for(int i=0; i<n_discover_ops; i++)
+  bool b_invariant(false);
+  while(!b_invariant)
   {
     std::ostringstream oss;
-    oss<<"disc_id_"<<i;
+    oss<<"disc_id_"<<i++;
     disc_res = (client.discoverJobStates(oss.str(), job_id));
-    boost::this_thread::sleep (discover_interval);
+
+    // invariant: after some time, the leaf jobs are always in pending
+    b_invariant = true;
+    BOOST_FOREACH(const sdpa::discovery_info_t& child_info, disc_res.children())
+    {
+      if(child_info.state() != sdpa::status::PENDING)
+      {
+          b_invariant = false;
+          break;
+      }
+    }
   }
 
-  // invariant: after some time, the leaf jobs are always in pending
-  BOOST_FOREACH(const sdpa::discovery_info_t& child_info, disc_res.children())
-  {
-     BOOST_REQUIRE_EQUAL(child_info.state(), sdpa::status::PENDING);
-  }
+  DMLOG(DEBUG, disc_res);
 }
 
 BOOST_AUTO_TEST_CASE (insufficient_number_of_workers)
@@ -264,30 +322,97 @@ BOOST_AUTO_TEST_CASE (insufficient_number_of_workers)
   sdpa::client::Client client (orchestrator.name());
   sdpa::job_id_t job_id = client.submitJob (workflow);
 
+  int i=0;
   sdpa::discovery_info_t disc_res;
-  for(int i=0; i<n_discover_ops; i++)
+  bool b_invariant(false);
+  while(!b_invariant)
   {
-      std::ostringstream oss;
-      oss<<"disc_id_"<<i;
-      disc_res = (client.discoverJobStates(oss.str(), job_id));
-      boost::this_thread::sleep (discover_interval);
-  }
+    std::ostringstream oss;
+    oss<<"disc_id_"<<i++;
+    disc_res = (client.discoverJobStates(oss.str(), job_id));
 
-  // invariant: after some time, the task B is always in pending
-  BOOST_FOREACH(const sdpa::discovery_info_t& child_info, disc_res.children())
-  {
-    BOOST_REQUIRE( child_info.state() == sdpa::status::PENDING || child_info.state() == sdpa::status::FINISHED );
-  }
-
-  bool b_at_least_one_job_in_pending(false);
-  BOOST_FOREACH(const sdpa::discovery_info_t& child_info, disc_res.children())
-  {
-    if(child_info.state() == sdpa::status::PENDING)
+    // invariant: after some time, the task B is always in pending
+    b_invariant = true;
+    BOOST_FOREACH(const sdpa::discovery_info_t& child_info, disc_res.children())
     {
-      b_at_least_one_job_in_pending = true;
-      break;
+      BOOST_REQUIRE( child_info.state() == sdpa::status::PENDING || child_info.state() == sdpa::status::FINISHED );
+      if(child_info.state() != sdpa::status::PENDING)
+        b_invariant = false;
     }
   }
+}
 
-  BOOST_REQUIRE(b_at_least_one_job_in_pending);
+BOOST_AUTO_TEST_CASE (remove_workers)
+{
+  const std::string workflow
+    (utils::require_and_read_file ("workflows/coallocation_test2.pnet"));
+
+  const utils::orchestrator orchestrator
+    ("orchestrator_0", "127.0.0.1");
+  const utils::agent agent
+    ("agent_0", "127.0.0.1", orchestrator);
+
+  const utils::drts_worker worker_A_0
+    ( "drts_A_0", agent
+    , "A"
+    , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
+    , kvs_host(), kvs_port()
+    );
+
+  boost::scoped_ptr<utils::drts_worker> ptr_worker_A_1
+   (  new utils::drts_worker
+       ( "drts_A_1", agent
+       , "A"
+       , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
+       , kvs_host(), kvs_port()
+       )
+   );
+
+  const utils::drts_worker worker_B_0
+    ( "drts_B_0", agent
+    , "B"
+    , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
+    , kvs_host(), kvs_port()
+    );
+  const utils::drts_worker worker_B_1
+    ( "drts_B_1", agent
+    , "B"
+    , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
+    , kvs_host(), kvs_port()
+    );
+
+  boost::scoped_ptr<utils::drts_worker> ptr_worker_B_2
+    (  new utils::drts_worker
+        ( "drts_B_2", agent
+        , "B"
+        , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
+        , kvs_host(), kvs_port()
+        )
+    );
+
+  // the task A requires 2 workers, task B requires 3 workers
+
+  sdpa::client::Client client (orchestrator.name());
+  sdpa::job_id_t job_id = client.submitJob (workflow);
+
+  ptr_worker_A_1.reset();
+  ptr_worker_B_2.reset();
+
+  int i=0;
+  sdpa::discovery_info_t disc_res;
+  bool b_invariant(false);
+  while(!b_invariant)
+  {
+    std::ostringstream oss;
+    oss<<"disc_id_"<<i++;
+    disc_res = (client.discoverJobStates(oss.str(), job_id));
+
+    // invariant: after some time, the task B is always in pending
+    b_invariant = true;
+    BOOST_FOREACH(const sdpa::discovery_info_t& child_info, disc_res.children())
+    {
+      if(child_info.state() != sdpa::status::PENDING)
+        b_invariant = false;
+    }
+  }
 }
