d35486bb4f57aa86048d703926684c0e697af096
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-02-03 00:42:09 +0100

    gpi-space/pc/container/process: remove unused mutex

--- gpi-space/gpi-space/pc/container/process.hpp
+++ gpi-space/gpi-space/pc/container/process.hpp
@@ -77,9 +77,6 @@ namespace gpi
         gpi::pc::type::size_t wait (const gpi::pc::type::queue_id_t);
         void collect_info (gpi::pc::type::info::descriptor_t &);
       private:
-        typedef boost::recursive_mutex mutex_type;
-        typedef boost::unique_lock<mutex_type> lock_type;
-
         void reader_thread_main (const int fd);
 
         int receive ( const int fd
@@ -91,7 +88,6 @@ namespace gpi
         int close_socket (const int fd);
         int checked_read (const int fd, void *buf, const size_t len);
 
-        mutex_type m_mutex;
         manager_t & m_mgr;
         const gpi::pc::type::process_id_t m_id;
         int m_socket;
