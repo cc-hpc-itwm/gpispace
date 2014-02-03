79d3ed60ec4df461fabca14b9d766c73ab14b86c
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-02-03 16:19:20 +0100

    drts, fhgkernel: inline and remove setup_and_run_fhgkernel into drts-kernel

--- src/drts/worker/drts-kernel.cpp
+++ src/drts/worker/drts-kernel.cpp
@@ -1,10 +1,15 @@
 // bernd.loerwald@itwm.fraunhofer.de
 
-#include <fhg/plugin/core/kernel.hpp> // search_path_t
-#include <fhg/plugin/setup_and_run_fhgkernel.hpp>
+#include <fhg/plugin/core/kernel.hpp>
+#include <fhg/plugin/core/license.hpp>
+#include <fhg/util/daemonize.hpp>
+#include <fhg/util/pidfile_writer.hpp>
+#include <fhg/util/signal_handler_manager.hpp>
+#include <fhg/util/split.hpp>
 
 #include <fhglog/LogMacros.hpp>
 
+#include <boost/foreach.hpp>
 #include <boost/program_options.hpp>
 
 #include <string>
@@ -70,13 +75,71 @@ int main(int ac, char **av)
     mods_to_load.push_back ("gpi_compat");
   }
 
-  return setup_and_run_fhgkernel ( vm.count ("daemonize")
-                                 , vm.count ("keep-going")
-                                 , mods_to_load
-                                 , config_vars
-                                 , pidfile
-                                 , kernel_name
-                                 , search_path
-                                 , logger
-                                 );
+  const bool daemonize (vm.count ("daemonize"));
+  const bool keep_going (vm.count ("keep-going"));
+
+  fhg::plugin::magically_check_license (logger);
+
+  if (not pidfile.empty())
+  {
+    fhg::util::pidfile_writer const pidfile_writer (pidfile);
+
+    if (daemonize)
+    {
+      fhg::util::fork_and_daemonize_child_and_abandon_parent();
+    }
+
+    pidfile_writer.write();
+  }
+  else
+  {
+    if (daemonize)
+    {
+      fhg::util::fork_and_daemonize_child_and_abandon_parent();
+    }
+  }
+
+  fhg::core::kernel_t kernel (kernel_name, search_path);
+
+  BOOST_FOREACH (std::string const & p, config_vars)
+  {
+    typedef std::pair<std::string,std::string> key_val_t;
+    key_val_t kv (fhg::util::split_string(p, "="));
+    if (kv.first.empty())
+    {
+      LLOG (WARN, logger, "invalid config variable: must not be empty");
+    }
+    else
+    {
+      DLLOG (TRACE, logger, "setting " << kv.first << " to " << kv.second);
+      kernel.put(kv.first, kv.second);
+    }
+  }
+
+  BOOST_FOREACH (std::string const & p, mods_to_load)
+  {
+    try
+    {
+      int ec = kernel.load_plugin (p);
+      if (ec != 0)
+        throw std::runtime_error (strerror (ec));
+    }
+    catch (std::exception const &ex)
+    {
+      LLOG (ERROR, logger, "could not load `" << p << "' : " << ex.what());
+      if (! keep_going)
+      {
+        throw;
+      }
+    }
+  }
+
+  fhg::util::signal_handler_manager signal_handlers;
+
+  signal_handlers.add_log_backtrace_and_exit_for_critical_errors (logger);
+
+  signal_handlers.add (SIGTERM, boost::bind (&fhg::core::kernel_t::stop, &kernel));
+  signal_handlers.add (SIGINT, boost::bind (&fhg::core::kernel_t::stop, &kernel));
+
+  return kernel.run_and_unload (false);
 }
