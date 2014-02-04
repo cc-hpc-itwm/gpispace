42e41dc1944e8b3f3128b9a8985b8df3824ed0fc
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-02-04 15:36:38 +0100

    we: split out the expression_shell from test/expr_parser

--- src/we/test/expr_parser.cpp
+++ src/we/test/expr_parser.cpp
@@ -54,120 +54,12 @@ public:
   }
 };
 
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-
-bool read_line (std::string & s)
-{
-  char * line (readline("> "));
-
-  if (!line)
-    {
-      return false;
-    }
-
-  s = std::string (line);
-
-  if (*line)
-    {
-      add_history (line);
-    }
-
-  free (line);
-
-  return true;
-}
-
 using std::cin;
 using std::cout;
 using std::endl;
 
 int main (int ac, char **)
 {
-  if (ac > 1) { // just assume "-i" as first parameter for interactive mode
-    cout << "enter expression, ^D to start measurement" << endl;
-    cout << "clear context: #" << endl;
-    cout << "list state: ?" << endl;
-    cout << "switch constant folding: g" << endl;
-    typedef expr::eval::context context_t;
-    context_t context;
-    std::string input;
-    bool constant_folding (true);
-
-    while (read_line(input))
-      {
-        switch (input[0])
-          {
-          case '?':
-            cout << "constant_folding (switch with f): "
-                 << std::boolalpha << constant_folding << std::noboolalpha
-                 << endl;
-            cout << "context (delete with #): " << endl << context;
-            break;
-          case 'g':
-          case 'G':
-            constant_folding = not constant_folding;
-            cout << "constant_folding: "
-                 << std::boolalpha << constant_folding << std::noboolalpha
-                 << endl;
-            break;
-          case '#':
-            context = context_t(); cout << "context deleted" << endl;
-            break;
-          default:
-            try
-              {
-                typedef expr::parse::parser parser_t;
-
-                parser_t parser (input, constant_folding);
-
-                while (!parser.empty())
-                  {
-                    cout << "expression: " << parser.front() << endl;
-
-                    try
-                      {
-                        cout << "evals to: "
-                             << pnet::type::value::show (parser.eval_front (context))
-                             << endl;
-                      }
-                    catch (const pnet::exception::missing_binding & e)
-                      {
-                        cout << e.what() << endl;
-                      }
-
-                    parser.pop_front();
-                  }
-              }
-            catch (expr::exception::eval::divide_by_zero e)
-              {
-                cout << "EXCEPTION: " << e.what() << endl;
-              }
-            catch (expr::exception::eval::type_error e)
-              {
-                cout << "EXCEPTION: " << e.what() << endl;
-              }
-            catch (expr::exception::parse::exception e)
-              {
-                cout << input << endl;
-                for (unsigned int k (0); k < e.eaten; ++k)
-                  cout << " ";
-                cout << "^" << endl;
-                cout << "EXCEPTION: " << e.what() << endl;
-              }
-            catch (std::runtime_error e)
-              {
-                cout << "EXCEPTION: " << e.what() << endl;
-              }
-          }
-      }
-
-    cout << endl;
-
-    return EXIT_SUCCESS;
-  }
-
   cout << "measure..." << endl;
 
   {
