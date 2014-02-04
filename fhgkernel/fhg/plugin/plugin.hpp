8a5f000d2f6b6c7c547d17ae6e397cdc0adc3167
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-02-04 12:16:58 +0100

    fhgkernel: inline include

--- fhgkernel/fhg/plugin/plugin.hpp
+++ fhgkernel/fhg/plugin/plugin.hpp
@@ -8,7 +8,44 @@
 
 #include <fhg/assert.hpp>
 #include <fhg/plugin/descriptor.hpp>
-#include <fhg/plugin/plugin_base.hpp>
+
+#include <boost/utility.hpp>
+
+namespace fhg
+{
+  namespace plugin
+  {
+    class Kernel;
+
+    class Plugin : boost::noncopyable
+    {
+    public:
+      virtual ~Plugin(){}
+
+      int fhg_plugin_start_entry (Kernel *k)
+      {
+        m_kernel = k;
+        return fhg_plugin_start();
+      }
+
+      virtual int fhg_plugin_start () {return 0;}
+
+      virtual int fhg_plugin_stop  () {return 0;}
+
+      virtual void fhg_on_plugin_loaded (Plugin*) {}
+      virtual void fhg_on_plugin_preunload (Plugin*) {}
+    protected:
+      Plugin ()
+        : m_kernel (0)
+      {}
+
+      Kernel *fhg_kernel() {return m_kernel;}
+    private:
+      Kernel *m_kernel;
+    };
+  }
+}
+
 #include <fhg/plugin/kernel.hpp>
 
 typedef const fhg_plugin_descriptor_t* (*fhg_plugin_query)(void);
