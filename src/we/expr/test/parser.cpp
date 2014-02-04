af9b358ec3e5c9db53e33e732e2e281d1dd2f623
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-02-04 17:45:28 +0100

    we: merged test_expr_rename into test_expr_parser as this tests a method of the parser

--- src/we/expr/test/parser.cpp
+++ src/we/expr/test/parser.cpp
@@ -159,3 +159,46 @@ BOOST_AUTO_TEST_CASE (_floor)
   CHECK ("floor (2.0)", 2.0);
 #undef CHECK
 }
+
+BOOST_AUTO_TEST_CASE (other_variable_not_renamed)
+{
+  expr::parse::parser parser ("${_}");
+  parser.rename ("a", "A");
+
+  BOOST_REQUIRE_EQUAL (parser.string(), expr::parse::parser ("${_}").string());
+}
+
+BOOST_AUTO_TEST_CASE (variable_renamed)
+{
+  expr::parse::parser parser ("${a}");
+  parser.rename ("a", "A");
+
+  BOOST_REQUIRE_EQUAL (parser.string(), expr::parse::parser ("${A}").string());
+}
+
+BOOST_AUTO_TEST_CASE (first_field_renamed)
+{
+  expr::parse::parser parser ("${a._}");
+  parser.rename ("a", "A");
+
+  BOOST_REQUIRE_EQUAL
+    (parser.string(), expr::parse::parser ("${A._}").string());
+}
+
+BOOST_AUTO_TEST_CASE (later_field_not_renamed)
+{
+  expr::parse::parser parser ("${_.a}");
+  parser.rename ("a", "A");
+
+  BOOST_REQUIRE_EQUAL
+    (parser.string(), expr::parse::parser ("${_.a}").string());
+}
+
+BOOST_AUTO_TEST_CASE (renamed_at_depth_greater_zero)
+{
+  expr::parse::parser parser ("sin (${a})");
+  parser.rename ("a", "A");
+
+  BOOST_REQUIRE_EQUAL
+    (parser.string(), expr::parse::parser ("sin (${A})").string());
+}
