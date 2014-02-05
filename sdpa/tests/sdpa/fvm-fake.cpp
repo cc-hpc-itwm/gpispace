7ef44524fc2b3bcc7a7a3ae7d4f512ef43f883ba
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-02-05 13:29:17 +0100

    sdpa: tests: fvm-fake: use <<10 instead of *1024

--- sdpa/tests/sdpa/fvm-fake.cpp
+++ sdpa/tests/sdpa/fvm-fake.cpp
@@ -7,8 +7,8 @@ class FvmFakePluginImpl : FHG_PLUGIN
 public:
   FvmFakePluginImpl (Kernel*, std::list<Plugin*>, std::map<std::string, std::string> config_variables)
   {
-    size_t fvm_size (100*1024*1024);
-    size_t shm_size (50*1024*1024);
+    size_t fvm_size (100 << 20);
+    size_t shm_size (50 << 20);
 
     fvm_size = get<size_t> ("plugin.fvm_fake.fvm_size", config_variables).get_value_or (fvm_size);
     shm_size = get<size_t> ("plugin.fvm_fake.shm_size", config_variables).get_value_or (shm_size);
