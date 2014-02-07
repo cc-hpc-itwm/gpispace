7eb233f3987147d4de53397eec52ee352e0bb4e5
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-02-07 17:26:28 +0100

    gspc: net: actually remove global state of net::initializer by now allocating a new io_service per initializer

--- gspc/gspc/net/io.cpp
+++ gspc/gspc/net/io.cpp
@@ -1,14 +1,8 @@
-#include "io.hpp"
+#include <gspc/net/io.hpp>
 
-#include <fhg/assert.hpp>
 #include <fhg/util/get_cpucount.h>
-#include <fhg/util/threadname.hpp>
 
-#include <vector>
-#include <boost/foreach.hpp>
-#include <boost/format.hpp>
-#include <boost/shared_ptr.hpp>
-#include <boost/thread.hpp>
+#include <boost/bind.hpp>
 
 namespace gspc
 {
@@ -16,118 +10,50 @@ namespace gspc
   {
     namespace
     {
-      struct global_io
+      size_t get_thread_count ()
       {
-        typedef boost::shared_ptr<boost::thread> thread_ptr_t;
-        typedef std::vector<thread_ptr_t>        thread_pool_t;
-      public:
-        global_io ()
-          : m_io_service ()
-          , m_work ()
-          , m_threads ()
-        {}
-
-        ~global_io ()
-        {
-          stop ();
-        }
-
-        void start (size_t nthread)
-        {
-          boost::unique_lock<boost::mutex> _ (m_mutex);
-
-          if (not m_io_service)
-          {
-            m_io_service.reset (new boost::asio::io_service);
-            m_work.reset (new boost::asio::io_service::work (*m_io_service));
-
-            for (size_t i = 0 ; i < nthread ; ++i)
-            {
-              thread_ptr_t thrd
-                (new boost::thread (boost::bind ( &global_io::run
-                                                , this
-                                                , m_io_service
-                                                )
-                                   )
-                );
-
-              fhg::util::set_threadname
-                (*thrd, (boost::format ("io-%1%") % i).str ());
-              m_threads.push_back (thrd);
-            }
-          }
-        }
-
-        void run (boost::shared_ptr<boost::asio::io_service> my_io)
-        {
-          assert (my_io);
-          my_io->run ();
-        }
-
-        void stop ()
-        {
-          boost::unique_lock<boost::mutex> _ (m_mutex);
-
-          if (m_io_service)
-          {
-            m_work.reset ();
-            m_io_service->stop ();
-
-            BOOST_FOREACH (thread_ptr_t thrd, m_threads)
-            {
-              thrd->join ();
-            }
-            m_threads.clear ();
-
-            m_io_service.reset ();
-          }
-        }
-
-        boost::asio::io_service & service ()
-        {
-          return *m_io_service;
-        }
-      private:
-        boost::mutex m_mutex;
-
-        boost::shared_ptr<boost::asio::io_service>       m_io_service;
-        boost::shared_ptr<boost::asio::io_service::work> m_work;
-        thread_pool_t                                    m_threads;
-      };
-
-      global_io & get_io_singleton ()
-      {
-        static global_io gio;
-        return gio;
-      }
-
-      size_t s_get_thread_count ()
-      {
-        int ncpu = fhg_get_cpucount ();
+        int ncpu = fhg_get_cpucount();
         if (ncpu < 0)
           ncpu = 1;
         return ncpu;
       }
     }
 
-    initializer::initializer ()
+    initializer::initializer()
+      : _io_service()
+      , _io_service_work (new boost::asio::io_service::work (_io_service))
     {
-      get_io_singleton ().start (s_get_thread_count ());
+      size_t nthread (get_thread_count());
+
+      while (nthread --> 0)
+      {
+        _threads.create_thread
+          (boost::bind (&boost::asio::io_service::run, &_io_service));
+      }
     }
 
-    initializer::initializer (const size_t nthread)
+    initializer::initializer (size_t nthread)
+      : _io_service()
+      , _io_service_work (new boost::asio::io_service::work (_io_service))
     {
-      get_io_singleton ().start (nthread);
+      while (nthread --> 0)
+      {
+        _threads.create_thread
+          (boost::bind (&boost::asio::io_service::run, &_io_service));
+      }
     }
 
-    initializer::~initializer ()
+    initializer::~initializer()
     {
-      get_io_singleton ().stop ();
+      _io_service_work.reset();
+      _io_service.stop();
+
+      _threads.join_all();
     }
 
     initializer::operator boost::asio::io_service&()
     {
-      return get_io_singleton().service();
+      return _io_service;
     }
   }
 }
--- gspc/gspc/net/io.hpp
+++ gspc/gspc/net/io.hpp
@@ -1,7 +1,10 @@
 #ifndef GSPC_NET_IO_HPP
 #define GSPC_NET_IO_HPP
 
-#include <boost/asio.hpp>
+#include <boost/asio/io_service.hpp>
+#include <boost/thread.hpp>
+
+#include <memory>
 
 namespace gspc
 {
@@ -9,14 +12,19 @@ namespace gspc
   {
     struct initializer
     {
+    public:
+      //! \todo Remove: nthread shall be known by caller!
       initializer ();
-
-      explicit
-      initializer (const size_t nthread);
+      explicit initializer (size_t nthread);
 
       ~initializer ();
 
       operator boost::asio::io_service&();
+
+    private:
+      boost::asio::io_service _io_service;
+      std::auto_ptr<boost::asio::io_service::work> _io_service_work;
+      boost::thread_group _threads;
     };
   }
 }
