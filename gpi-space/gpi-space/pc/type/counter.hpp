21d782d987ca9515403100a0e4237fb332ab3020
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-02-07 21:40:15 +0100

    gpi-space: pc/type/counter: include counter.ipp instead of saying so

--- gpi-space/gpi-space/pc/type/counter.hpp
+++ gpi-space/gpi-space/pc/type/counter.hpp
@@ -32,6 +32,35 @@ namespace gpi
   }
 }
 
-#include "counter.ipp"
+namespace gpi
+{
+  namespace pc
+  {
+    namespace type
+    {
+      counter_t::counter_t(const gpi::pc::type::size_t start)
+        : m_counter (start)
+      {}
+
+      gpi::pc::type::size_t counter_t::value () const
+      {
+        lock_type lock (m_mutex);
+        return m_counter;
+      }
+
+      gpi::pc::type::size_t counter_t::inc ()
+      {
+        lock_type lock (m_mutex);
+        return ++m_counter;
+      }
+
+      void counter_t::reset (const gpi::pc::type::size_t val)
+      {
+        lock_type lock (m_mutex);
+        m_counter = val;
+      }
+    }
+  }
+}
 
 #endif
