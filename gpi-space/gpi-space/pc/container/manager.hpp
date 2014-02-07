f9334979c577000d44bb58c66ccd9c5789254e2f
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-02-07 20:07:47 +0100

    gpi-space: pc/container/manager: remove obvious and noisy comment

--- gpi-space/gpi-space/pc/container/manager.hpp
+++ gpi-space/gpi-space/pc/container/manager.hpp
@@ -109,10 +109,6 @@ namespace gpi
         void detach_process (const gpi::pc::type::process_id_t);
         void detach_memory_from_process (const gpi::pc::type::process_id_t);
 
-        /*                                                    */
-        /*           M E M B E R    V A R I A B L E S         */
-        /*                                                    */
-
         mutable mutex_type m_mutex;
         connector_t m_connector;
         gpi::pc::type::counter_t m_process_counter;
