e5b09bde264026687b8d9ee62f8e5bea687d1e07
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-02-10 19:33:16 +0100

    gpi-space: pc/memory: inline factory into implementation of manager

--- gpi-space/gpi-space/pc/memory/manager.cpp
+++ gpi-space/gpi-space/pc/memory/manager.cpp
@@ -12,7 +12,66 @@
 #include "memory_transfer_t.hpp"
 #include "handle_generator.hpp"
 
-#include "factory.hpp"
+#include <string>
+
+#include <boost/shared_ptr.hpp>
+
+namespace gpi
+{
+  namespace pc
+  {
+    namespace memory
+    {
+      class area_t;
+      typedef boost::shared_ptr<area_t> area_ptr_t;
+
+      class factory_t
+      {
+      public:
+        area_ptr_t create (std::string const & url);
+      };
+
+      factory_t & factory ();
+    }
+  }
+}
+
+#include <gpi-space/pc/memory/gpi_area.hpp>
+#include <gpi-space/pc/memory/sfs_area.hpp>
+#include <gpi-space/pc/memory/shm_area.hpp>
+
+#include <gpi-space/pc/global/topology.hpp>
+
+#include <fhg/util/url.hpp>
+
+namespace gpi
+{
+  namespace pc
+  {
+    namespace memory
+    {
+      area_ptr_t
+      factory_t::create (std::string const &url_s)
+      {
+        fhg::util::url_t url (url_s);
+
+        return
+          ( url.type() == "gpi" ? gpi_area_t::create (url_s, boost::ref (global::topology()))
+          : url.type() == "sfs" ? sfs_area_t::create (url_s, boost::ref (global::topology()))
+          : url.type() == "shm" ? shm_area_t::create (url_s)
+          : throw std::runtime_error
+              ("no memory type registered with: '" + url_s + "'")
+          );
+      }
+
+      factory_t & factory ()
+      {
+        static factory_t instance;
+        return instance;
+      }
+    }
+  }
+}
 
 namespace gpi
 {
