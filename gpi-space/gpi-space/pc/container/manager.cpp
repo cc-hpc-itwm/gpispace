2787d73b12b539cc2f0960bf4d3b80bf8de654f7
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-02-11 16:12:22 +0100

    gpi-space: use fhg::syscall::shutdown instead of lifting error manually

--- gpi-space/gpi-space/pc/container/manager.cpp
+++ gpi-space/gpi-space/pc/container/manager.cpp
@@ -7,6 +7,7 @@
 
 #include <fhg/assert.hpp>
 #include <fhg/util/lift_error_code_to_exception.hpp>
+#include <fhg/syscall.hpp>
 #include <fhglog/LogMacros.hpp>
 
 #include <boost/bind.hpp>
@@ -103,8 +104,7 @@ namespace gpi
 
       void manager_t::close_socket (const int fd)
       {
-        fhg::util::lift_error_code_to_exception
-          (shutdown (fd, SHUT_RDWR), "close_socket: shutdown");
+        fhg::syscall::shutdown (fd, SHUT_RDWR);
         fhg::util::lift_error_code_to_exception
           (close (fd), "close_socket: close");
       }
