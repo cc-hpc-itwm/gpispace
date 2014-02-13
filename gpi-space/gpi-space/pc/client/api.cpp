c433bf26d26c6589c0c2b48fbdc70661731ff7f5
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-02-13 21:54:09 +0100

    gpi-space: pc/client/api: do not name temporary

--- gpi-space/gpi-space/pc/client/api.cpp
+++ gpi-space/gpi-space/pc/client/api.cpp
@@ -75,9 +75,7 @@ namespace gpi
 
           try
           {
-            gpi::pc::proto::control::message_t ctrl_msg
-              (boost::get<gpi::pc::proto::control::message_t>(communicate (gpi::pc::proto::control::message_t (gpi::pc::proto::control::info_t()))));
-            m_info = boost::get<gpi::pc::proto::control::info_reply_t>(ctrl_msg).info;
+            m_info = boost::get<gpi::pc::proto::control::info_reply_t>(boost::get<gpi::pc::proto::control::message_t>(communicate (gpi::pc::proto::control::message_t (gpi::pc::proto::control::info_t())))).info;
           }
           catch (...)
           {
