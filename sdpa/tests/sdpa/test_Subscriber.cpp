54c5cd5fa63aba718fd0d25bec6e568cd9a50306
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2014-02-17 16:51:37 +0100

    sdpa: changed test_Subscriber, no need to instantiate drts workers and load modules for this test

--- sdpa/tests/sdpa/test_Subscriber.cpp
+++ sdpa/tests/sdpa/test_Subscriber.cpp
@@ -8,6 +8,25 @@
 
 BOOST_GLOBAL_FIXTURE (KVSSetup)
 
+class Worker : public sdpa::daemon::Agent
+ {
+  public:
+    Worker (const std::string& name, const std::string& master_name)
+      : Agent (name, "127.0.0.1", kvs_host(), kvs_port(), sdpa::master_info_list_t(1, sdpa::MasterInfo(master_name)), boost::none)
+    {}
+
+    Worker (const std::string& name,const utils::agents_t& masters)
+          : Agent (name, "127.0.0.1", kvs_host(), kvs_port(), utils::assemble_master_info_list (masters), boost::none)
+        {}
+
+    void submit ( const we::layer::id_type& activity_id
+                , const we::type::activity_t& activity )
+    {
+      sdpa::daemon::GenericDaemon::submit(activity_id, activity);
+      workflowEngine()->finished(activity_id, activity);
+    }
+ };
+
 BOOST_AUTO_TEST_CASE (execute_workflow_with_subscribed_client)
 {
   const std::string workflow
@@ -18,12 +37,7 @@ BOOST_AUTO_TEST_CASE (execute_workflow_with_subscribed_client)
   const utils::agent agent
     ("agent_0", "127.0.0.1", kvs_host(), kvs_port(), orchestrator);
 
-  const utils::drts_worker worker_0
-    ( "drts_0", agent
-    , ""
-    , TESTS_TRANSFORM_FILE_MODULES_PATH
-    , kvs_host(), kvs_port()
-    );
+  const Worker worker( "worker_0", agent._.name());
 
   BOOST_REQUIRE_EQUAL
     ( utils::client::submit_job_and_wait_for_termination_as_subscriber
@@ -42,12 +56,7 @@ BOOST_AUTO_TEST_CASE (execute_workflow_and_subscribe_with_second_client)
   const utils::agent agent
     ("agent_0", "127.0.0.1", kvs_host(), kvs_port(), orchestrator);
 
-  const utils::drts_worker worker_0
-    ( "drts_0", agent
-    , ""
-    , TESTS_TRANSFORM_FILE_MODULES_PATH
-    , kvs_host(), kvs_port()
-    );
+  const Worker worker( "worker_0", agent._.name());
 
   BOOST_REQUIRE_EQUAL
     ( utils::client::submit_job_and_wait_for_termination_as_subscriber_with_two_different_clients
