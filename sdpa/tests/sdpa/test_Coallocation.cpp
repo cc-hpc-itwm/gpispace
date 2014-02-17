4728581905ee1dbe6bc1bd0b8798541e397461ea
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2014-02-17 16:53:22 +0100

    sdpa: changed coallocation test, no need to instantiate modules as they do nothing

--- sdpa/tests/sdpa/test_Coallocation.cpp
+++ sdpa/tests/sdpa/test_Coallocation.cpp
@@ -6,7 +6,30 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include <sdpa/events/CapabilitiesGainedEvent.hpp>
+
 BOOST_GLOBAL_FIXTURE (KVSSetup)
+
+class Worker : public sdpa::daemon::Agent
+{
+  public:
+    Worker (const std::string& name, const std::string& master_name, const std::string cpb_name)
+      : Agent (name, "127.0.0.1", kvs_host(), kvs_port(), sdpa::master_info_list_t(1, sdpa::MasterInfo(master_name)), boost::none)
+    {
+        sdpa::capability_t  cpb(cpb_name, name);
+        addCapability(cpb);
+        sdpa::events::CapabilitiesGainedEvent::Ptr
+            ptrCpbGainEvtA( new sdpa::events::CapabilitiesGainedEvent(name, master_name, cpb) );
+        sendEventToOther(ptrCpbGainEvtA);
+    }
+
+    void submit ( const we::layer::id_type& activity_id
+                , const we::type::activity_t& activity )
+    {
+      sdpa::daemon::GenericDaemon::submit(activity_id, activity);
+      workflowEngine()->finished(activity_id, activity);
+    }
+};
 
 BOOST_AUTO_TEST_CASE (testCoallocationWorkflow)
 {
@@ -18,37 +41,11 @@ BOOST_AUTO_TEST_CASE (testCoallocationWorkflow)
   const utils::agent agent
     ("agent_0", "127.0.0.1", kvs_host(), kvs_port(), orchestrator);
 
-  const utils::drts_worker worker_A_0
-    ( "drts_A_0", agent
-    , "A"
-    , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
-    , kvs_host(), kvs_port()
-    );
-  const utils::drts_worker worker_A_1
-    ( "drts_A_1", agent
-    , "A"
-    , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
-    , kvs_host(), kvs_port()
-    );
-
-  const utils::drts_worker worker_B_0
-    ( "drts_B_0", agent
-    , "B"
-    , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
-    , kvs_host(), kvs_port()
-    );
-  const utils::drts_worker worker_B_1
-    ( "drts_B_1", agent
-    , "B"
-    , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
-    , kvs_host(), kvs_port()
-    );
-  const utils::drts_worker worker_B_2
-    ( "drts_B_2", agent
-    , "B"
-    , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
-    , kvs_host(), kvs_port()
-    );
+  Worker worker_0( "worker_0", agent._.name(), "A");
+  Worker worker_1( "worker_1", agent._.name(), "A");
+  Worker worker_2( "worker_2", agent._.name(), "B");
+  Worker worker_3( "worker_3", agent._.name(), "B");
+  Worker worker_4( "worker_4", agent._.name(), "B");
 
   BOOST_REQUIRE_EQUAL
     ( utils::client::submit_job_and_wait_for_termination_as_subscriber
