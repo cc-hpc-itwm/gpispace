6a51a7c110bec22b306c139f15df2603d98f609f
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-02-18 14:00:05 +0100

    Revert "gpi-space: remove pc::memory::gpi_area::init(): is never"
    This reverts commit 4b357c88c486c721c2ccda40b226aae0f966a117.

--- gpi-space/gpi-space/pc/memory/gpi_area.hpp
+++ gpi-space/gpi-space/pc/memory/gpi_area.hpp
@@ -71,6 +71,8 @@ namespace gpi
                            , task_list_t & tasks
                            );
       private:
+        void init ();
+
         bool is_range_local ( const gpi::pc::type::handle::descriptor_t &
                             , const gpi::pc::type::offset_t begin
                             , const gpi::pc::type::size_t   range_size
