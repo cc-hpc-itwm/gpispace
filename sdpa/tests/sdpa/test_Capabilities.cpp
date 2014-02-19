fef5109cc6126d33f97968056d56dff75316b6b5
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2014-02-19 16:36:10 +0100

    sdpa: fixed indentation

--- sdpa/tests/sdpa/test_Capabilities.cpp
+++ sdpa/tests/sdpa/test_Capabilities.cpp
@@ -81,30 +81,30 @@ BOOST_AUTO_TEST_CASE (lose_capabilities)
   const std::string workflow
      (utils::require_and_read_file ("workflows/capabilities.pnet"));
 
-   const utils::orchestrator orchestrator
+  const utils::orchestrator orchestrator
      ("orchestrator_0", "127.0.0.1", kvs_host(), kvs_port());
 
-   utils::agent agent
+  utils::agent agent
      ("agent_0", "127.0.0.1", kvs_host(), kvs_port(), orchestrator);
 
-   Worker worker_0( "worker_0", "agent_0");
-   Worker worker_1( "worker_1", "agent_0");
+  Worker worker_0( "worker_0", "agent_0");
+  Worker worker_1( "worker_1", "agent_0");
 
-   while(!agent._.hasWorker(worker_1.name()));
-   while(!agent._.hasWorker(worker_0.name()));
+  while(!agent._.hasWorker(worker_1.name()));
+  while(!agent._.hasWorker(worker_0.name()));
 
-   sdpa::capability_t cpbA("A", "worker_0");
-   sdpa::events::CapabilitiesGainedEvent::Ptr
-     ptrCpbGainEvtA( new sdpa::events::CapabilitiesGainedEvent(worker_0.name(), "agent_0", cpbA) );
-   agent._.handleCapabilitiesGainedEvent(ptrCpbGainEvtA.get());
+  sdpa::capability_t cpbA("A", "worker_0");
+  sdpa::events::CapabilitiesGainedEvent::Ptr
+    ptrCpbGainEvtA( new sdpa::events::CapabilitiesGainedEvent(worker_0.name(), "agent_0", cpbA) );
+  agent._.handleCapabilitiesGainedEvent(ptrCpbGainEvtA.get());
 
-   sdpa::capability_t cpbB("B", "worker_1");
-   sdpa::events::CapabilitiesGainedEvent::Ptr
-     ptrCpbGainEvtB(new sdpa::events::CapabilitiesGainedEvent(worker_1.name(), "agent_0", cpbB));
-   agent._.handleCapabilitiesGainedEvent(ptrCpbGainEvtB.get());
+  sdpa::capability_t cpbB("B", "worker_1");
+  sdpa::events::CapabilitiesGainedEvent::Ptr
+    ptrCpbGainEvtB(new sdpa::events::CapabilitiesGainedEvent(worker_1.name(), "agent_0", cpbB));
+  agent._.handleCapabilitiesGainedEvent(ptrCpbGainEvtB.get());
 
-   sdpa::capabilities_set_t expected_cpb_set;
-   expected_cpb_set.insert(cpbB);
+  sdpa::capabilities_set_t expected_cpb_set;
+  expected_cpb_set.insert(cpbB);
 
   sdpa::events::CapabilitiesLostEvent::Ptr
     ptrCpbLostEvtB(new sdpa::events::CapabilitiesLostEvent(worker_0.name(), "agent_0", cpbA));
