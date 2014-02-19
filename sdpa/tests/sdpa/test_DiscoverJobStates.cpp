6d95a102f0df98cc7eddb6bbe5e41819cea0f332
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2014-02-19 14:08:58 +0100

    sdpa: removed TestAgent from test_DiscoverJobStates, use mock workers to test the real agent

--- sdpa/tests/sdpa/test_DiscoverJobStates.cpp
+++ sdpa/tests/sdpa/test_DiscoverJobStates.cpp
@@ -11,6 +11,9 @@
 
 #include <deque>
 
+#include <sdpa/events/CapabilitiesGainedEvent.hpp>
+#include <sdpa/events/CapabilitiesLostEvent.hpp>
+
 namespace
 {
   bool has_state_pending (sdpa::discovery_info_t const& disc_res)
@@ -42,133 +45,41 @@ namespace
   {
     return !disc_res.children().empty() && all_childs_are_pending (disc_res);
   }
-
-  sdpa::events::ErrorEvent::Ptr create_disconnect_event (const sdpa::worker_id_t& worker_id,
-                                                         const sdpa::worker_id_t& agent_id)
-  {
-    sdpa::events::ErrorEvent::Ptr pErrEvt(
-          new  sdpa::events::ErrorEvent( worker_id
-                                        , agent_id
-                                        , sdpa::events::ErrorEvent::SDPA_ENETWORKFAILURE
-                                        , std::string("worker ")+worker_id+" went down!" ));
-    return pErrEvt;
-  }
-
 }
 
-BOOST_GLOBAL_FIXTURE (KVSSetup)
-
-namespace sdpa
+class Worker : public sdpa::daemon::Agent
 {
-  namespace daemon
-  {
-    class TestAgent : public Agent
+  public:
+    Worker (const std::string& name, const std::string& master_name, const std::string cpb_name)
+      : Agent (name, "127.0.0.1", kvs_host(), kvs_port(), sdpa::master_info_list_t(1, sdpa::MasterInfo(master_name)), boost::none)
     {
-    public:
-      TestAgent ( const std::string& name
-                , const std::string& url
-                , std::string kvs_host, std::string kvs_port
-                , const sdpa::master_info_list_t arrMasterNames
-                )
-        : Agent (name, url, kvs_host, kvs_port, arrMasterNames, boost::none)
-          , _n_submitted_activities(0)
-      {}
+        sdpa::capability_t  cpb(cpb_name, name);
+        addCapability(cpb);
+        sdpa::events::CapabilitiesGainedEvent::Ptr
+            ptrCpbGainEvtA( new sdpa::events::CapabilitiesGainedEvent(name, master_name, cpb) );
+        sendEventToOther(ptrCpbGainEvtA);
+    }
 
-      std::string gen_id()
+    void submit ( const we::layer::id_type& activity_id
+                , const we::type::activity_t& activity )
+    {
+      sdpa::daemon::GenericDaemon::submit(activity_id, activity);
+      workflowEngine()->finished(activity_id, activity);
+    }
+
+    void remove_capability(const std::string& cpb_name)
+    {
+      sdpa::capability_t cpb(cpb_name, name());
+      BOOST_FOREACH (sdpa::MasterInfo& masterInfo, m_arrMasterInfo)
       {
-        return GenericDaemon::gen_id();
+        sdpa::events::CapabilitiesLostEvent::Ptr
+           ptrCpbLostEvt( new sdpa::events::CapabilitiesLostEvent(name(), masterInfo.name(), cpb) );
+        sendEventToOther(ptrCpbLostEvt);
       }
+    }
+};
 
-      void discover (we::layer::id_type discover_id, we::layer::id_type job_id)
-      {
-        boost::unique_lock<boost::mutex> const _ (_mtx_disc);
-        _jobs_to_discover.push_back (std::make_pair (discover_id, job_id));
-        _cond_disc.notify_all();
-      }
-
-      void discovered
-        (we::layer::id_type disc_id, sdpa::discovery_info_t discover_result)
-      {
-        boost::unique_lock<boost::mutex> const _ (_mtx_result);
-        _discovery_result = discover_result;
-        _discover_id = disc_id;
-        _cond_result.notify_one();
-      }
-
-      void submit ( const we::layer::id_type& activityId
-                  , const we::type::activity_t& activity
-                  )
-      {
-        GenericDaemon::submit (activityId, activity);
-
-        boost::unique_lock<boost::mutex> const _ (_mtx_all_submitted);
-        if (++_n_submitted_activities == 2)
-        {
-          _cond_all_submitted.notify_one();
-        }
-      }
-
-      void wait_all_submitted()
-      {
-        boost::unique_lock<boost::mutex> lock(_mtx_all_submitted);
-        _cond_all_submitted.wait (lock);
-      }
-
-      void notify_discovered()
-      {
-        while (true)
-        {
-          boost::unique_lock<boost::mutex> lock(_mtx_disc);
-          _cond_disc.timed_wait (lock, boost::posix_time::seconds (1));
-
-          if (!_jobs_to_discover.empty())
-          {
-            std::pair<we::layer::id_type, sdpa::job_id_t> const pair
-              (_jobs_to_discover.front());
-            _jobs_to_discover.pop_front();
-
-            Job* const pJob (jobManager().findJob (pair.second));
-
-            sdpa::discovery_info_t const discover_result
-              ( pair.second
-              , pJob
-              ? boost::optional<sdpa::status::code> (pJob->getStatus())
-              : boost::none
-              , sdpa::discovery_info_set_t()
-              );
-
-            workflowEngine()->discovered (pair.first, discover_result);
-          }
-        }
-      }
-
-      we::layer::id_type wait_for_discovery_result()
-      {
-        boost::unique_lock<boost::mutex> lock_res (_mtx_result);
-        _cond_result.wait (lock_res);
-        return _discover_id;
-      }
-
-      bool has_two_pending_children()
-      {
-        return _discovery_result.children().size() >= 2
-          && all_childs_are_pending (_discovery_result);
-      }
-
-    private:
-      boost::mutex _mtx_disc;
-      boost::mutex _mtx_result;
-      boost::mutex _mtx_all_submitted;
-      boost::condition_variable_any _cond_disc;
-      boost::condition_variable_any _cond_result;
-      boost::condition_variable_any _cond_all_submitted;
-      std::deque<std::pair<we::layer::id_type, sdpa::job_id_t> > _jobs_to_discover;
-      sdpa::discovery_info_t _discovery_result;
-      we::layer::id_type _discover_id;
-      unsigned int _n_submitted_activities;
-    };
-  }
- }
+BOOST_GLOBAL_FIXTURE (KVSSetup)
 
 namespace
 {
@@ -177,41 +88,6 @@ namespace
     static int i (0);
 
     return (boost::format ("discover_%1%") % i++).str();
-  }
-}
-
-BOOST_AUTO_TEST_CASE (test_discover_activities)
-{
-  const std::string workflow
-    (utils::require_and_read_file ("coallocation_test2.pnet"));
-
-  const we::type::activity_t activity (workflow);
-
-  sdpa::master_info_list_t listMasterInfo;
-  sdpa::daemon::TestAgent agent
-    ("agent_0", "127.0.0.1", kvs_host(), kvs_port(), listMasterInfo);
-
-  we::layer::id_type const id ("test_job");
-
-  agent.workflowEngine()->submit (id, activity);
-  //! Note it seems to have a race condition somewhere if we call layer submit and
-  // layer discover immediately (deadlock)
-  agent.wait_all_submitted();
-
-  boost::thread thrd_notify
-    (boost::thread (&sdpa::daemon::TestAgent::notify_discovered, &agent));
-
-  while (!agent.has_two_pending_children())
-  {
-    we::layer::id_type const sent_disc_id(get_next_disc_id());
-    agent.workflowEngine()->discover (sent_disc_id, id);
-    BOOST_REQUIRE_EQUAL (agent.wait_for_discovery_result(), sent_disc_id);
-  }
-
-  thrd_notify.interrupt();
-  if (thrd_notify.joinable())
-  {
-    thrd_notify.join();
   }
 }
 
@@ -263,6 +139,8 @@ BOOST_AUTO_TEST_CASE (discover_one_orchestrator_one_agent)
 
 BOOST_AUTO_TEST_CASE (insufficient_number_of_workers)
 {
+  // this workflow produces 2 activities
+  // one requires 2 workers, the other 3 workers
   const std::string workflow
     (utils::require_and_read_file ("coallocation_test2.pnet"));
 
@@ -271,26 +149,9 @@ BOOST_AUTO_TEST_CASE (insufficient_number_of_workers)
   const utils::agent agent
     ("agent_0", "127.0.0.1", kvs_host(), kvs_port(), orchestrator);
 
-  const utils::drts_worker worker_A_0
-    ( "drts_A_0", agent
-    , "A"
-    , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
-    , kvs_host(), kvs_port()
-    );
-  const utils::drts_worker worker_B_0
-    ( "drts_B_0", agent
-    , "B"
-    , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
-    , kvs_host(), kvs_port()
-    );
-  const utils::drts_worker worker_B_1
-    ( "drts_B_1", agent
-    , "B"
-    , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
-    , kvs_host(), kvs_port()
-    );
-
-  // the task A requires 2 workers, task B requires 3 workers
+  const Worker worker_0( "worker_0", agent._.name(), "A");
+  const Worker worker_1( "worker_1", agent._.name(), "B");
+  const Worker worker_2( "worker_2", agent._.name(), "B");
 
   sdpa::client::Client client (orchestrator.name(), kvs_host(), kvs_port());
   sdpa::job_id_t const job_id (client.submitJob (workflow));
@@ -301,7 +162,7 @@ BOOST_AUTO_TEST_CASE (insufficient_number_of_workers)
   {} // do nothing, discover again
 }
 
-BOOST_AUTO_TEST_CASE (discover_after_removing_workers)
+BOOST_AUTO_TEST_CASE (discover_after_losing_capabilities)
 {
   const std::string workflow
     (utils::require_and_read_file ("coallocation_test.pnet"));
@@ -309,50 +170,14 @@ BOOST_AUTO_TEST_CASE (discover_after_removing_workers)
   const utils::orchestrator orchestrator
     ("orchestrator_0", "127.0.0.1", kvs_host(), kvs_port());
 
-  sdpa::daemon::Agent agent ( "agent_0",
-                              "127.0.0.1",  kvs_host(), kvs_port(),
-                              sdpa::master_info_list_t (1, sdpa::MasterInfo ("orchestrator_0")),
-                              boost::none);
+  const utils::agent agent
+     ("agent_0", "127.0.0.1", kvs_host(), kvs_port(), orchestrator);
 
-  const utils::drts_worker worker_A_0
-    ( "drts_A_0"
-      , "agent_0"
-      , "A"
-      , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
-      , kvs_host(), kvs_port()
-    );
-
-  const utils::drts_worker worker_B_0
-     ( "drts_B_0"
-       , "agent_0"
-       , "B"
-       , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
-       , kvs_host(), kvs_port()
-     );
-
-  const utils::drts_worker worker_A_1
-      ( "drts_A_1"
-        , "agent_0"
-        , "A"
-        , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
-        , kvs_host(), kvs_port()
-      );
-
-  const utils::drts_worker worker_B_1
-    ( "drts_B_1"
-       , "agent_0"
-       , "B"
-       , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
-       , kvs_host(), kvs_port()
-    );
-
-  const utils::drts_worker worker_B_2
-      ( "drts_B_2"
-        , "agent_0"
-        , "B"
-        , TESTS_EXAMPLE_COALLOCATION_TEST_MODULES_PATH
-        , kvs_host(), kvs_port()
-      );
+  Worker worker_0( "worker_0", agent._.name(), "A");
+  Worker worker_1( "worker_1", agent._.name(), "B");
+  const Worker worker_2( "worker_2", agent._.name(), "A");
+  const Worker worker_3( "worker_3", agent._.name(), "B");
+  const Worker worker_4( "worker_4", agent._.name(), "B");
 
   // the task A requires 2 workers, task B requires 3 workers
   // initially, there are sufficient resources and the workflow computation should finish
@@ -363,17 +188,89 @@ BOOST_AUTO_TEST_CASE (discover_after_removing_workers)
       );
 
   sdpa::client::Client client (orchestrator.name(), kvs_host(), kvs_port());
-  // be sure that the drts worker to disconnect are registered
-  while(!agent.hasWorker("drts_A_0"));
-  while(!agent.hasWorker("drts_B_0"));
-
   sdpa::job_id_t const job_id (client.submitJob (workflow));
 
-  agent.handleErrorEvent(create_disconnect_event("drts_A_0", "agent_0").get());
-  agent.handleErrorEvent(create_disconnect_event("drts_B_0", "agent_0").get());
+  worker_0.remove_capability("A");
+  worker_1.remove_capability("B");
 
   while (!has_children_and_all_children_are_pending
           (client.discoverJobStates (get_next_disc_id(), job_id))
         )
   {} // do nothing, discover again
+}
+
+BOOST_AUTO_TEST_CASE (discover_after_removing_workers)
+{
+  const std::string workflow
+    (utils::require_and_read_file ("workflows/coallocation_test2.pnet"));
+
+  const utils::orchestrator orchestrator
+    ("orchestrator_0", "127.0.0.1", kvs_host(), kvs_port());
+
+  const utils::agent agent
+     ("agent_0", "127.0.0.1", kvs_host(), kvs_port(), orchestrator);
+
+  Worker*  pWorker_0 = new Worker( "worker_0", agent._.name(), "A");
+  Worker*  pWorker_1 = new Worker( "worker_1", agent._.name(), "B");
+  const Worker worker_2( "worker_2", agent._.name(), "A");
+  const Worker worker_3( "worker_3", agent._.name(), "B");
+  const Worker worker_4( "worker_4", agent._.name(), "B");
+
+  // the task A requires 2 workers, task B requires 3 workers
+  // initially, there are sufficient resources and the workflow computation should finish
+  BOOST_REQUIRE_EQUAL
+      ( utils::client::submit_job_and_wait_for_termination_as_subscriber
+        (workflow, orchestrator)
+      , sdpa::status::FINISHED
+      );
+
+  sdpa::client::Client client (orchestrator.name(), kvs_host(), kvs_port());
+  sdpa::job_id_t const job_id (client.submitJob (workflow));
+
+  delete pWorker_0;
+  delete pWorker_1;
+
+  while (!has_children_and_all_children_are_pending
+          (client.discoverJobStates (get_next_disc_id(), job_id))
+        )
+  {} // do nothing, discover again
+}
+
+
+BOOST_AUTO_TEST_CASE (discover_after_restarting_workers)
+{
+  const std::string workflow
+    (utils::require_and_read_file ("workflows/coallocation_test2.pnet"));
+
+  const utils::orchestrator orchestrator
+    ("orchestrator_0", "127.0.0.1", kvs_host(), kvs_port());
+
+  const utils::agent agent
+     ("agent_0", "127.0.0.1", kvs_host(), kvs_port(), orchestrator);
+
+  Worker*  pWorker_0 = new Worker( "worker_0", agent._.name(), "A");
+  Worker*  pWorker_1 = new Worker( "worker_1", agent._.name(), "B");
+  const Worker worker_2( "worker_2", agent._.name(), "A");
+  const Worker worker_3( "worker_3", agent._.name(), "B");
+  const Worker worker_4( "worker_4", agent._.name(), "B");
+
+  sdpa::client::Client client (orchestrator.name(), kvs_host(), kvs_port());
+  sdpa::job_id_t const job_id (client.submitJob (workflow));
+
+  delete pWorker_0;
+  delete pWorker_1;
+
+  while (!has_children_and_all_children_are_pending
+          (client.discoverJobStates (get_next_disc_id(), job_id))
+        )
+  {} // do nothing, discover again
+
+  Worker worker_0( "worker_0", agent._.name(), "A");
+  Worker worker_1( "worker_1", agent._.name(), "B");
+
+  BOOST_REQUIRE_EQUAL
+       ( utils::client::submit_job_and_wait_for_termination_as_subscriber
+         (workflow, orchestrator)
+       , sdpa::status::FINISHED
+       );
 }
