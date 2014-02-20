8974bb428286f778a4b25d84314c6ec37a0d3622
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2014-02-20 10:15:49 +0100

    sdpa: simplified test_Capabilities by not explicitly handling capability gain/loss events

--- sdpa/tests/sdpa/test_Capabilities.cpp
+++ sdpa/tests/sdpa/test_Capabilities.cpp
@@ -16,17 +16,18 @@ BOOST_GLOBAL_FIXTURE (KVSSetup)
 class Worker : public sdpa::daemon::Agent
 {
   public:
-    Worker (const std::string& name, const std::string& master_name)
+    Worker (const std::string& name, const std::string& master_name, const std::string cpb_name)
       : Agent (name, "127.0.0.1", kvs_host(), kvs_port(), sdpa::master_info_list_t(1, sdpa::MasterInfo(master_name)), boost::none)
-    {}
-
-    Worker (const std::string& name,const utils::agents_t& masters)
-          : Agent (name, "127.0.0.1", kvs_host(), kvs_port(), utils::assemble_master_info_list (masters), boost::none)
-        {}
+    {
+        sdpa::capability_t  cpb(cpb_name, name);
+        addCapability(cpb);
+        sdpa::events::CapabilitiesGainedEvent::Ptr
+            ptrCpbGainEvt( new sdpa::events::CapabilitiesGainedEvent(name, master_name, cpb) );
+        sendEventToOther(ptrCpbGainEvt);
+    }
 
     void submit ( const we::layer::id_type& activity_id
-                , const we::type::activity_t& activity
-                )
+                , const we::type::activity_t& activity )
     {
       sdpa::daemon::GenericDaemon::submit(activity_id, activity);
       workflowEngine()->finished(activity_id, activity);
@@ -44,73 +45,60 @@ BOOST_AUTO_TEST_CASE (acquire_capabilities_from_workers)
   utils::agent agent
     ("agent_0", "127.0.0.1", kvs_host(), kvs_port(), orchestrator);
 
-  Worker worker_0( "worker_0", "agent_0");
-  Worker worker_1( "worker_1", "agent_0");
+  Worker worker_0( "worker_0", "agent_0", "A");
+  Worker worker_1( "worker_1", "agent_0", "B");
 
-  while(!agent._.hasWorker(worker_1.name()));
-  while(!agent._.hasWorker(worker_0.name()));
-
-  sdpa::capability_t cpbA("A", "worker_0");
-  sdpa::events::CapabilitiesGainedEvent::Ptr
-    ptrCpbGainEvtA( new sdpa::events::CapabilitiesGainedEvent(worker_0.name(), "agent_0", cpbA) );
-  agent._.handleCapabilitiesGainedEvent(ptrCpbGainEvtA.get());
-
-  sdpa::capability_t cpbB("B", "worker_1");
-  sdpa::events::CapabilitiesGainedEvent::Ptr
-    ptrCpbGainEvtB(new sdpa::events::CapabilitiesGainedEvent(worker_1.name(), "agent_0", cpbB));
-  agent._.handleCapabilitiesGainedEvent(ptrCpbGainEvtB.get());
-
-  sdpa::capabilities_set_t expected_cpb_set;
-  expected_cpb_set.insert(cpbA);
-  expected_cpb_set.insert(cpbB);
-
-  sdpa::capabilities_set_t cpbset_worker_0;
-  agent._.getWorkerCapabilities(worker_0.name(), cpbset_worker_0);
-
-  sdpa::capabilities_set_t cpbset_worker_1;
-  agent._.getWorkerCapabilities(worker_1.name(), cpbset_worker_1);
+  sdpa::capabilities_set_t set_cpbs_0;
+  worker_0.getCapabilities(set_cpbs_0);
+  sdpa::capabilities_set_t set_cpbs_1;
+  worker_1.getCapabilities(set_cpbs_1);
+  sdpa::capabilities_set_t expected_cpbs(set_cpbs_0);
+  BOOST_FOREACH(const sdpa::capability_t& cpb, set_cpbs_1)
+  {
+    expected_cpbs.insert(cpb);
+  }
 
   sdpa::capabilities_set_t acquired_cpbs;
-  agent._.getCapabilities(acquired_cpbs);
+  while(acquired_cpbs.size()!=2)
+  {
+     acquired_cpbs.clear();
+     agent._.getCapabilities(acquired_cpbs);
+  }
 
-  BOOST_REQUIRE(acquired_cpbs==expected_cpb_set);
+  BOOST_REQUIRE(acquired_cpbs == expected_cpbs);
 }
 
 BOOST_AUTO_TEST_CASE (lose_capabilities)
 {
   const std::string workflow
-     (utils::require_and_read_file ("workflows/capabilities.pnet"));
+    (utils::require_and_read_file ("workflows/capabilities.pnet"));
 
   const utils::orchestrator orchestrator
-     ("orchestrator_0", "127.0.0.1", kvs_host(), kvs_port());
+    ("orchestrator_0", "127.0.0.1", kvs_host(), kvs_port());
 
   utils::agent agent
-     ("agent_0", "127.0.0.1", kvs_host(), kvs_port(), orchestrator);
+    ("agent_0", "127.0.0.1", kvs_host(), kvs_port(), orchestrator);
 
-  Worker worker_0( "worker_0", "agent_0");
-  Worker worker_1( "worker_1", "agent_0");
+  Worker worker_0( "worker_0", "agent_0", "A");
+  Worker* pWorker_1(new Worker( "worker_1", "agent_0", "B"));
 
-  while(!agent._.hasWorker(worker_1.name()));
-  while(!agent._.hasWorker(worker_0.name()));
-
-  sdpa::capability_t cpbA("A", "worker_0");
-  sdpa::events::CapabilitiesGainedEvent::Ptr
-    ptrCpbGainEvtA( new sdpa::events::CapabilitiesGainedEvent(worker_0.name(), "agent_0", cpbA) );
-  agent._.handleCapabilitiesGainedEvent(ptrCpbGainEvtA.get());
-
-  sdpa::capability_t cpbB("B", "worker_1");
-  sdpa::events::CapabilitiesGainedEvent::Ptr
-    ptrCpbGainEvtB(new sdpa::events::CapabilitiesGainedEvent(worker_1.name(), "agent_0", cpbB));
-  agent._.handleCapabilitiesGainedEvent(ptrCpbGainEvtB.get());
-
-  sdpa::capabilities_set_t expected_cpb_set;
-  expected_cpb_set.insert(cpbB);
-
-  sdpa::events::CapabilitiesLostEvent::Ptr
-    ptrCpbLostEvtB(new sdpa::events::CapabilitiesLostEvent(worker_0.name(), "agent_0", cpbA));
-  agent._.handleCapabilitiesLostEvent(ptrCpbLostEvtB.get());
+  sdpa::capabilities_set_t set_cpbs_0;
+  worker_0.getCapabilities(set_cpbs_0);
 
   sdpa::capabilities_set_t acquired_cpbs;
-  agent._.getCapabilities(acquired_cpbs);
-  BOOST_REQUIRE(acquired_cpbs==expected_cpb_set);
+  while (acquired_cpbs.size()!=2)
+  {
+     acquired_cpbs.clear();
+     agent._.getCapabilities(acquired_cpbs);
+  }
+
+  delete pWorker_1;
+
+  while (acquired_cpbs.size()!=1)
+  {
+     acquired_cpbs.clear();
+     agent._.getCapabilities(acquired_cpbs);
+  }
+
+  BOOST_REQUIRE(acquired_cpbs == set_cpbs_0);
 }
