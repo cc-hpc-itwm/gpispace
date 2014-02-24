fd7a7878d191f4262af5d59168446636cdc05cd8
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2014-02-24 17:29:28 +0100

    sdpa: introduce mock worker in test_CancelJob, wait the worker get jobs before cancelation instead of calling hasJobs

--- sdpa/tests/sdpa/test_CancelJob.cpp
+++ sdpa/tests/sdpa/test_CancelJob.cpp
@@ -10,6 +10,38 @@
 
 BOOST_GLOBAL_FIXTURE (KVSSetup)
 
+class Worker : public sdpa::daemon::Agent
+ {
+  public:
+    Worker (const std::string& name, const std::string& master_name)
+    : Agent ( name
+            , "127.0.0.1"
+            , kvs_host()
+            , kvs_port()
+            , sdpa::master_info_list_t(1, sdpa::MasterInfo(master_name))
+            , boost::none)
+    {}
+
+    void submit ( const we::layer::id_type& activity_id
+                , const we::type::activity_t& activity
+                )
+    {
+      sdpa::daemon::GenericDaemon::submit(activity_id, activity);
+      _cond_got_job.notify_one();
+    }
+
+    void wait_for_jobs()
+    {
+      boost::unique_lock<boost::mutex> lock(_mtx_got_job);
+      _cond_got_job.wait(lock);
+    }
+
+  private:
+
+    boost::mutex _mtx_got_job;
+    boost::condition_variable_any _cond_got_job;
+ };
+
 BOOST_AUTO_TEST_CASE (cancel_no_agent)
 {
   const std::string workflow
@@ -39,10 +71,13 @@ BOOST_AUTO_TEST_CASE (cancel_with_agent)
   utils::agent agent
     ("agent_0", "127.0.0.1", kvs_host(), kvs_port(), orchestrator);
 
+  Worker worker("worker_0", agent._.name());
+
   sdpa::client::Client client (orchestrator.name(), kvs_host(), kvs_port());
 
   sdpa::job_id_t job_id(client.submitJob (workflow));
-  while(!agent._.hasJobs());
+
+  worker.wait_for_jobs();
 
   client.cancelJob(job_id);
 
@@ -85,10 +120,13 @@ BOOST_AUTO_TEST_CASE (call_cancel_twice_agent)
   utils::agent agent
       ("agent_2", "127.0.0.1", kvs_host(), kvs_port(), orchestrator);
 
+  Worker worker("worker_0", agent._.name());
+
   sdpa::client::Client client (orchestrator.name(),  kvs_host(), kvs_port());
   sdpa::job_id_t job_id(client.submitJob (workflow));
 
-  while(!agent._.hasJobs());
+  worker.wait_for_jobs();
+
   client.cancelJob(job_id);
 
   BOOST_REQUIRE_EQUAL
