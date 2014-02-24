1cceca3e1d07cb3beacf14427d6ac156156dbaf0
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-02-24 15:09:18 +0100

    sdpa: daemon: remove unused typedef

--- sdpa/sdpa/daemon/Job.hpp
+++ sdpa/sdpa/daemon/Job.hpp
@@ -119,7 +119,6 @@ namespace sdpa {
     class Job : public boost::msm::back::state_machine<JobFSM_>
     {
     public:
-      typedef boost::unordered_map<job_id_t, Job*> job_list_t;
       typedef boost::recursive_mutex mutex_type;
       typedef boost::unique_lock<mutex_type> lock_type;
 
