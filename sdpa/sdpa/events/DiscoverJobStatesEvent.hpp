21c249eee6815b007974dd68d2b4f93117141aa0
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-03-03 17:46:19 +0100

    sdpa: events: formatting

--- sdpa/sdpa/events/DiscoverJobStatesEvent.hpp
+++ sdpa/sdpa/events/DiscoverJobStatesEvent.hpp
@@ -13,12 +13,12 @@ namespace sdpa
       typedef boost::shared_ptr<DiscoverJobStatesEvent> Ptr;
 
       DiscoverJobStatesEvent ( const address_t& a_from
-                              , const address_t& a_to
-                              , const sdpa::job_id_t& a_job_id
-                              , const sdpa::job_id_t& discover_id
-                              )
+                             , const address_t& a_to
+                             , const sdpa::job_id_t& a_job_id
+                             , const sdpa::job_id_t& discover_id
+                             )
         : sdpa::events::JobEvent (a_from, a_to, a_job_id)
-        , discover_id_(discover_id)
+        , discover_id_ (discover_id)
       {}
 
       std::string str() const
@@ -26,7 +26,10 @@ namespace sdpa
         return "DiscoverJobStatestEvent(" + job_id () + ")";
       }
 
-      const sdpa::job_id_t& discover_id() const { return discover_id_; }
+      const sdpa::job_id_t& discover_id() const
+      {
+        return discover_id_;
+      }
 
       virtual void handleBy (EventHandler* handler)
       {
@@ -46,7 +49,7 @@ namespace sdpa
     LOAD_CONSTRUCT_DATA_DEF (DiscoverJobStatesEvent, e)
     {
       LOAD_JOBEVENT_CONSTRUCT_DATA (from, to, job_id);
-      LOAD_FROM_ARCHIVE ( sdpa::job_id_t, disc_id);
+      LOAD_FROM_ARCHIVE (sdpa::job_id_t, disc_id);
       ::new (e) DiscoverJobStatesEvent (from, to, job_id, disc_id);
     }
   }
