18d30f26d322c827b37b949ced7aa8c07fb24c68
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2014-03-04 10:59:06 +0100

    sdpa: tests: use mock worker with minimal network support in test_Topologies instead of inheriting from the Agent class; removed irrelevant test case

--- sdpa/tests/sdpa/test_Topologies.cpp
+++ sdpa/tests/sdpa/test_Topologies.cpp
@@ -8,25 +8,33 @@
 
 BOOST_GLOBAL_FIXTURE (KVSSetup)
 
-class Worker : public sdpa::daemon::Agent
- {
+class Worker : public utils::BasicWorker
+{
   public:
-    Worker (const std::string& name, const std::string& master_name)
-      : Agent (name, "127.0.0.1", kvs_host(), kvs_port(), sdpa::master_info_list_t(1, sdpa::MasterInfo(master_name)), boost::none)
+    Worker (const std::string& name, const std::string& master_name, const std::string cpb_name = "")
+      :  utils::BasicWorker (name, master_name, cpb_name)
     {}
 
-    Worker (const std::string& name,const utils::agents_t& masters)
-          : Agent (name, "127.0.0.1", kvs_host(), kvs_port(), utils::assemble_master_info_list (masters), boost::none)
-        {}
-
-    void submit ( const we::layer::id_type& activity_id
-                , const we::type::activity_t& activity
-                )
+    void handleSubmitJobEvent (const sdpa::events::SubmitJobEvent* pEvt)
     {
-      sdpa::daemon::GenericDaemon::submit(activity_id, activity);
-      workflowEngine()->finished(activity_id, activity);
+      sdpa::events::SubmitJobAckEvent::Ptr
+      pSubmitJobAckEvt(new sdpa::events::SubmitJobAckEvent( _name
+                                                          , pEvt->from()
+                                                          , *pEvt->job_id()));
+      _network_strategy->perform (pSubmitJobAckEvt);
+
+      sdpa::events::JobFinishedEvent::Ptr
+      pJobFinishedEvt(new sdpa::events::JobFinishedEvent( _name
+                                                        , pEvt->from()
+                                                        , *pEvt->job_id()
+                                                        , pEvt->description() ));
+
+      _network_strategy->perform (pJobFinishedEvt);
     }
- };
+
+    void handleJobFinishedAckEvent(const sdpa::events::JobFinishedAckEvent* ){}
+};
+
 
 BOOST_AUTO_TEST_CASE (orchestrator_agent_worker)
 {
@@ -108,40 +116,7 @@ BOOST_AUTO_TEST_CASE (two_workers_with_seperate_master_agent)
     ("agent_2", "127.0.0.1", kvs_host(), kvs_port(), agent_0);
 
   const Worker worker_0( "worker_0", "agent_1");
-  const Worker worker_1( "worker_0", "agent_2");
-
-  BOOST_REQUIRE_EQUAL ( utils::client::submit_job_and_wait_for_termination
-                        (workflow, orchestrator)
-                      , sdpa::status::FINISHED
-                      );
-}
-
-BOOST_AUTO_TEST_CASE (one_worker_with_multiple_master_agents)
-{
-  // O-+-+
-  // | | |
-  // A ? A -> variable agents #
-  // | | |
-  // W-+-+
-
-  const std::string workflow
-    (utils::require_and_read_file ("transform_file.pnet"));
-
-  const utils::orchestrator orchestrator
-    ("orchestrator_0", "127.0.0.1", kvs_host(), kvs_port());
-
-  //! \note "variable agents #" was hardcoded to 1 when this test got
-  //! rewritten. Probably should be more, so got bumped to 2.
-  const utils::agent agent_0
-    ("agent_0", "127.0.0.1", kvs_host(), kvs_port(), orchestrator);
-  const utils::agent agent_1
-    ("agent_1", "127.0.0.1", kvs_host(), kvs_port(), orchestrator);
-
-  utils::agents_t agents;
-  agents.push_back (boost::cref (agent_0));
-  agents.push_back (boost::cref (agent_1));
-
-  const Worker worker ("worker_0", agents);
+  const Worker worker_1( "worker_1", "agent_2");
 
   BOOST_REQUIRE_EQUAL ( utils::client::submit_job_and_wait_for_termination
                         (workflow, orchestrator)
