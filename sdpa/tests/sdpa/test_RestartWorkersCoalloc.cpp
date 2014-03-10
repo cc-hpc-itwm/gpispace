173480df53edf9882eb380e4f21c3cf2072365ac
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2014-03-10 16:33:29 +0100

    sdpa: added test for re-atsrting workers wit co-allocation workflow

---
+++ sdpa/tests/sdpa/test_RestartWorkersCoalloc.cpp
@@ -0,0 +1,79 @@
+#define BOOST_TEST_MODULE restart_worker_with_coallocation_workflow
+
+#include "kvs_setup_fixture.hpp"
+#include "tests_config.hpp"
+#include <utils.hpp>
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_GLOBAL_FIXTURE (KVSSetup)
+
+#include <fhg/util/random_string.hpp>
+
+class Worker : public utils::BasicWorker
+{
+  public:
+    Worker (const std::string& name, const utils::agent& master_agent, const std::string cpb_name = "", bool notify_finished = false)
+      :  utils::BasicWorker (name, master_agent, cpb_name)
+      , _notify_finished(notify_finished)
+    {}
+
+    void handleSubmitJobEvent (const sdpa::events::SubmitJobEvent* pEvt)
+    {
+      sdpa::events::SubmitJobAckEvent::Ptr
+      pSubmitJobAckEvt(new sdpa::events::SubmitJobAckEvent( _name
+                                                          , pEvt->from()
+                                                          , *pEvt->job_id()));
+      _network_strategy->perform (pSubmitJobAckEvt);
+
+      if(_notify_finished)
+      {
+          sdpa::events::JobFinishedEvent::Ptr
+          pJobFinishedEvt(new sdpa::events::JobFinishedEvent( _name
+                                                            , pEvt->from()
+                                                            , *pEvt->job_id()
+                                                            , pEvt->description() ));
+
+          _network_strategy->perform (pJobFinishedEvt);
+      }
+    }
+
+    void handleJobFinishedAckEvent(const sdpa::events::JobFinishedAckEvent* ){}
+
+  private:
+    bool _notify_finished;
+};
+
+namespace {
+  std::string gen_name() { return fhg::util::random_string_without (". "); }
+}
+
+BOOST_AUTO_TEST_CASE (restart_workers_while_job_requiring_coallocation_is_runnig)
+{
+  const std::string workflow
+    (utils::require_and_read_file ("coallocation.pnet"));
+
+  const utils::orchestrator orchestrator
+    (gen_name(), "127.0.0.1", kvs_host(), kvs_port());
+
+  const utils::agent agent
+    (gen_name(), "127.0.0.1", kvs_host(), kvs_port(), orchestrator);
+
+
+  const Worker worker_0(gen_name(), agent, "A", true);
+
+  sdpa::worker_id_t worker_id_1(gen_name());
+  Worker* pWorker_1(new Worker(worker_id_1, agent, "A"));
+
+  sdpa::client::Client client (orchestrator.name(), kvs_host(), kvs_port());
+  sdpa::job_id_t const job_id (client.submitJob (workflow));
+
+  delete pWorker_1;
+
+  const Worker worker_1(worker_id_1, agent, "A", true);
+
+  sdpa::client::job_info_t job_info;
+  BOOST_REQUIRE_EQUAL
+        ( client.wait_for_terminal_state (job_id, job_info)
+        , sdpa::status::FINISHED );
+}
