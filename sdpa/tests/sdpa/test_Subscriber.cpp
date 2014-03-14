1d30724def8df6147ddb50d24b776d12f28288fb
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-03-14 17:54:02 +0100

    sdpa: tests: use fake worker from utils

--- sdpa/tests/sdpa/test_Subscriber.cpp
+++ sdpa/tests/sdpa/test_Subscriber.cpp
@@ -39,8 +39,7 @@ BOOST_AUTO_TEST_CASE (execute_workflow_with_subscribed_client)
 {
   const utils::orchestrator orchestrator (kvs_host(), kvs_port());
   const utils::agent agent (orchestrator);
-
-  const Worker worker( "worker_0", agent);
+  const utils::fake_drts_worker_directly_finishing_jobs worker (agent);
 
   BOOST_REQUIRE_EQUAL
     ( utils::client::submit_job_and_wait_for_termination_as_subscriber
@@ -53,8 +52,7 @@ BOOST_AUTO_TEST_CASE (execute_workflow_and_subscribe_with_second_client)
 {
   const utils::orchestrator orchestrator (kvs_host(), kvs_port());
   const utils::agent agent (orchestrator);
-
-  const Worker worker( "worker_0", agent);
+  const utils::fake_drts_worker_directly_finishing_jobs worker (agent);
 
   BOOST_REQUIRE_EQUAL
     ( utils::client::submit_job_and_wait_for_termination_as_subscriber_with_two_different_clients
