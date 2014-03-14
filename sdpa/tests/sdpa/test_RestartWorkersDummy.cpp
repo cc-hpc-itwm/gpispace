33ab5c62db2a4ce0d1704808aa0cf3b3a56b5907
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-03-14 18:17:15 +0100

    sdpa: tests: remove another copy of the fake worker

--- sdpa/tests/sdpa/test_RestartWorkersDummy.cpp
+++ sdpa/tests/sdpa/test_RestartWorkersDummy.cpp
@@ -8,23 +8,6 @@
 
 BOOST_GLOBAL_FIXTURE (KVSSetup)
 
-class Worker : public utils::BasicAgent
-{
-  public:
-    Worker (const std::string& name, const utils::agent& master_agent, const std::string cpb_name = "")
-      :  utils::BasicAgent (name, master_agent, cpb_name)
-    {}
-
-    void handleSubmitJobEvent (const sdpa::events::SubmitJobEvent* pEvt)
-    {
-      sdpa::events::SubmitJobAckEvent::Ptr
-      pSubmitJobAckEvt(new sdpa::events::SubmitJobAckEvent( _name
-                                                          , pEvt->from()
-                                                          , *pEvt->job_id()));
-      _network_strategy->perform (pSubmitJobAckEvt);
-    }
-};
-
 BOOST_AUTO_TEST_CASE (restart_worker_with_dumm_workflow)
 {
   const utils::orchestrator orchestrator (kvs_host(), kvs_port());
