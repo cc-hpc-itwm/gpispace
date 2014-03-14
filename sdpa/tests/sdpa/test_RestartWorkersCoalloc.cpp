78de249889ff17c89e17d2f5f8d8917f8472aaa8
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-03-14 18:28:42 +0100

    sdpa: tests: rewrite restart_workers_while_job_requiring_coallocation_is_running to get rid of race, use client wrapper, use utils fake workers and add todo about huge bug

--- sdpa/tests/sdpa/test_RestartWorkersCoalloc.cpp
+++ sdpa/tests/sdpa/test_RestartWorkersCoalloc.cpp
@@ -47,22 +47,38 @@ BOOST_AUTO_TEST_CASE (restart_workers_while_job_requiring_coallocation_is_runnin
   const utils::orchestrator orchestrator (kvs_host(), kvs_port());
   const utils::agent agent (orchestrator);
 
-
-  const Worker worker_0 (utils::random_peer_name(), agent, "A", true);
-
-  sdpa::worker_id_t worker_id_1 (utils::random_peer_name());
-  Worker* pWorker_1(new Worker(worker_id_1, agent, "A"));
-
-  sdpa::client::Client client (orchestrator.name(), kvs_host(), kvs_port());
+  utils::client::client_t client (orchestrator);
   sdpa::job_id_t const job_id
-    (client.submitJob (utils::net_with_one_child_requiring_workers (2).to_string()));
+    (client.submit_job (utils::net_with_one_child_requiring_workers (2).to_string()));
 
-  delete pWorker_1;
+  sdpa::worker_id_t const worker_id (utils::random_peer_name());
 
-  const Worker worker_1(worker_id_1, agent, "A", true);
+  fhg::util::thread::event<std::string> job_submitted_0;
+  utils::fake_drts_worker_notifying_module_call_submission worker_0
+    (boost::bind (&fhg::util::thread::event<std::string>::notify, &job_submitted_0, _1), agent);
 
-  sdpa::client::job_info_t job_info;
+  {
+    fhg::util::thread::event<> job_submitted_1;
+
+    const utils::fake_drts_worker_notifying_module_call_submission worker_1
+      ( worker_id
+      , boost::bind (&fhg::util::thread::event<>::notify, &job_submitted_1)
+      , agent
+      );
+
+    std::string ignore;
+    job_submitted_0.wait (ignore);
+    job_submitted_1.wait();
+
+    //! \todo At this point, worker_0 should get a cancelJob event for `ignore`!
+  }
+
+  const utils::fake_drts_worker_directly_finishing_jobs restarted_worker
+    (worker_id, agent);
+  std::string job_id_0;
+  job_submitted_0.wait (job_id_0);
+  worker_0.finish (job_id_0);
+
   BOOST_REQUIRE_EQUAL
-        ( client.wait_for_terminal_state (job_id, job_info)
-        , sdpa::status::FINISHED );
+    (client.wait_for_terminal_state (job_id), sdpa::status::FINISHED);
 }
