3677786bc59d61c951f70c83e885da38a12092d3
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2014-04-02 17:27:52 +0200

    macros: added INVALID_ENUM_VALUE macro

--- util/fhg/util/macros.hpp
+++ util/fhg/util/macros.hpp
@@ -19,4 +19,15 @@
 #define IFNDEF_NDEBUG(x...)
 #endif
 
+#include <stdexcept>
+#include <boost/format.hpp>
+#include <boost/current_function.hpp>
+#define INVALID_ENUM_VALUE(type, value) \
+  throw std::out_of_range \
+     ((boost::format \
+      ("%1%:%2%: %3%: value of enum '%4%' expected: got non-enummed-value %5%") \
+      % __FILE__ % __LINE__ % BOOST_CURRENT_FUNCTION % #type % value \
+      ).str() \
+     );
+
 #endif
