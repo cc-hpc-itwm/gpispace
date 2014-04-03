d3e997625ecbb60817977bec43b44548666d9c4b
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-04-03 18:26:29 +0200

    xml: fixup 5ce6744d570a45fb885589bae5cdea9878f77f68, only test for bundled boost when SDPA_HOME is set

--- src/xml/parse/type/function.cpp
+++ src/xml/parse/type/function.cpp
@@ -1168,13 +1168,15 @@ namespace xml
         stream << "    BOOST_ROOT := $(SDPA_HOME)/external/boost"  << std::endl;
         stream << "  endif"                                        << std::endl;
         stream << "else"                                           << std::endl;
-        stream << "  ifneq \"$(BOOST_ROOT)\" \"$(SDPA_HOME)/external/boost\""
+        stream << "  ifdef SDPA_HOME"                              << std::endl;
+        stream << "    ifneq \"$(BOOST_ROOT)\" \"$(SDPA_HOME)/external/boost\""
                                                                    << std::endl;
-        stream << "    $(warning \"BOOST_ROOT is set and different from GSPC bundled version\")"
+        stream << "      $(warning \"BOOST_ROOT is set and different from GSPC bundled version\")"
                                                                    << std::endl;
-        stream << "    $(warning \"BOOST_ROOT = $(BOOST_ROOT)\")"  << std::endl;
-        stream << "    $(warning \"GSPC_BUNDLED = $(SDPA_HOME)/external/boost\")"
+        stream << "      $(warning \"BOOST_ROOT = $(BOOST_ROOT)\")"<< std::endl;
+        stream << "      $(warning \"GSPC_BUNDLED = $(SDPA_HOME)/external/boost\")"
                                                                    << std::endl;
+        stream << "    endif"                                      << std::endl;
         stream << "  endif"                                        << std::endl;
         stream << "endif"                                          << std::endl;
         stream                                                     << std::endl;
