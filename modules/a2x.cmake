57d826fc0404a444d4430b73713f8dfcd1edfc5d
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2014-04-16 15:07:30 +0000

    new: a2x.cmake script to call 'a2x'

---
+++ modules/a2x.cmake
@@ -0,0 +1,8 @@
+file (MAKE_DIRECTORY "${OUTPUT}")
+execute_process (
+     COMMAND "${A2X}" -f "${FORMAT}" ${OPTION} --no-xmllint "--destination-dir=${OUTPUT}" "${INPUT}"
+     RESULT_VARIABLE __res
+)
+if (NOT ${__res} EQUAL 0)
+  file (REMOVE_RECURSE "${OUTPUT}")
+endif ()
