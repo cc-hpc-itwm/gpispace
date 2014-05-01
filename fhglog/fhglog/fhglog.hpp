2ce1cfd1a8e70a8bba5ffe1732c36a35c0666b56
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-05-01 20:11:09 +0200

    fhglog: remove include container fhglog/fhglog.hpp

--- fhglog/fhglog/fhglog.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef  FHGLOG_INC
-#define  FHGLOG_INC
-
-#include	<fhglog/LogMacros.hpp>
-#include	<fhglog/Logger.hpp>
-
-#include    <fhglog/Configuration.hpp>
-
-#endif   /* ----- #ifndef FHGLOG_INC  ----- */
