9e3a30e8cae0f1b99c62669c444462ce09488bdd
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-05-06 14:52:12 +0200

    util: assert: throw std::logic_error directly
    - do have a special exception that stores additional information is not
      necessary as it shall not be catched
    - improve testing by check the message too

--- util/fhg/assert.hpp
+++ util/fhg/assert.hpp
@@ -20,7 +20,7 @@
    the message is optional but if given, must be a C-string
  */
 #define FHG_ASSERT_DISABLED  0 //! \note ignore asserts
-#define FHG_ASSERT_EXCEPTION 1 //! \note throw fhg::assertion_failed
+#define FHG_ASSERT_EXCEPTION 1 //! \note throw std::logic_error
 
 #ifndef FHG_ASSERT_MODE
 #  error missing definition for FHG_ASSERT_MODE
@@ -32,18 +32,26 @@
 #  define fhg_assert(cond, ...)
 
 #elif FHG_ASSERT_EXCEPTION == FHG_ASSERT_MODE
-#  include <fhg/assertion_failed.hpp>
+#  include <stdexcept>
+#  include <boost/format.hpp>
 #  define FHG_ASSERT_STR_(x) #x
 #  define FHG_ASSERT_STR(x) FHG_ASSERT_STR_(x)
 
 #  define IF_FHG_ASSERT(x...) x
-#  define fhg_assert(cond, ...)                                      \
+#  define fhg_assert(cond, ...)                                         \
   do                                                                    \
   {                                                                     \
     if (! (cond))                                                       \
     {                                                                   \
-      throw fhg::assertion_failed                                       \
-            (FHG_ASSERT_STR(cond), "" __VA_ARGS__, __FILE__, __LINE__);         \
+      throw std::logic_error                                            \
+        ( ( boost::format ("[%1%:%2%] assertion '%3%' failed%4%%5%.")   \
+          % __FILE__                                                    \
+          % __LINE__                                                    \
+          % FHG_ASSERT_STR (cond)                                       \
+          % (std::string ("" __VA_ARGS__).empty() ? "" : ": ")          \
+          % std::string ("" __VA_ARGS__)                                \
+          ).str()                                                       \
+        );                                                              \
     }                                                                   \
   } while (0)
 
