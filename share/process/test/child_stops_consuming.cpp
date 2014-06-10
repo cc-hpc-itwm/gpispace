72b85b53f358d19b072d5db43ba2cf2757ccaa90
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-06-10 12:14:40 +0200

    process: test: add tests for parent having too little input or too little output buffer, factor out duplicate code

--- share/process/test/child_stops_consuming.cpp
+++ share/process/test/child_stops_consuming.cpp
@@ -1,72 +1,74 @@
 // bernd.loerwald@itwm.fraunhofer.de
 
-#define BOOST_TEST_MODULE "child stops consuming input while there is more"
+#define BOOST_TEST_MODULE "child or parent stops consuming while there is more"
 #include <boost/test/unit_test.hpp>
 
 #include <process.hpp>
 
+#include <boost/optional.hpp>
+
+#include <signal.h>
+
+namespace
+{
+  void consume ( int exit_code
+               , std::size_t input_buffer_size
+               , std::size_t output_buffer_size
+               , std::size_t to_read
+               , boost::optional<std::size_t> written
+               , std::size_t read
+               )
+  {
+    char m_input [input_buffer_size];
+    char m_output [output_buffer_size];
+
+    process::execute_return_type result
+      ( process::execute ( "/usr/bin/head -c " + std::to_string (to_read)
+                         , process::const_buffer (m_input, sizeof (m_input))
+                         , process::buffer (m_output, sizeof (m_output))
+                         , {}
+                         , {}
+                         )
+      );
+
+    BOOST_REQUIRE_EQUAL (result.exit_code, exit_code);
+    if (written)
+    {
+      BOOST_REQUIRE_EQUAL (result.bytes_written_stdin, *written);
+    }
+    BOOST_REQUIRE_EQUAL (result.bytes_read_stdout, read);
+    BOOST_REQUIRE_EQUAL (result.bytes_read_stderr, 0);
+  }
+}
+
 BOOST_AUTO_TEST_CASE (consume_everything)
 {
-  char m_input [2 << 20];
-  char m_output [sizeof (m_input)];
-
-  const std::size_t to_read (sizeof (m_input));
-
-  process::execute_return_type result
-    ( process::execute ( "/usr/bin/head -c " + std::to_string (to_read)
-                       , process::const_buffer (m_input, sizeof (m_input))
-                       , process::buffer (m_output, sizeof (m_output))
-                       , {}
-                       , {}
-                       )
-    );
-
-  BOOST_REQUIRE_EQUAL (result.exit_code, 0);
-  BOOST_REQUIRE_EQUAL (result.bytes_written_stdin, to_read);
-  BOOST_REQUIRE_EQUAL (result.bytes_read_stdout, to_read);
-  BOOST_REQUIRE_EQUAL (result.bytes_read_stderr, 0);
+  std::size_t base (1 << 20);
+  consume (0, base, base, base, base, base);
 }
 
 BOOST_AUTO_TEST_CASE (consume_half)
 {
-  char m_input [2 << 20];
-  char m_output [sizeof (m_input)];
-
-  const std::size_t to_read (sizeof (m_input) / 2);
-
-  process::execute_return_type result
-    ( process::execute ( "/usr/bin/head -c " + std::to_string (to_read)
-                       , process::const_buffer (m_input, sizeof (m_input))
-                       , process::buffer (m_output, sizeof (m_output))
-                       , {}
-                       , {}
-                       )
-    );
-
-  BOOST_REQUIRE_EQUAL (result.exit_code, 0);
-  BOOST_REQUIRE_EQUAL (result.bytes_written_stdin, to_read);
-  BOOST_REQUIRE_EQUAL (result.bytes_read_stdout, to_read);
-  BOOST_REQUIRE_EQUAL (result.bytes_read_stderr, 0);
+  std::size_t base (1 << 20);
+  consume (0, base, base / 2,  base / 2, base / 2, base / 2);
 }
 
 BOOST_AUTO_TEST_CASE (consume_nothing)
 {
-  char m_input [2 << 20];
-  char m_output [sizeof (m_input)];
+  std::size_t base (1 << 20);
+  consume (0, base, base, 0UL, 0UL, 0UL);
+}
 
-  const std::size_t to_read (0);
+BOOST_AUTO_TEST_CASE (try_consume_more_than_available)
+{
+  std::size_t base (1 << 20);
+  consume (0, base, base * 2, base * 2, base, base);
+}
 
-  process::execute_return_type result
-    ( process::execute ( "/usr/bin/head -c " + std::to_string (to_read)
-                       , process::const_buffer (m_input, sizeof (m_input))
-                       , process::buffer (m_output, sizeof (m_output))
-                       , {}
-                       , {}
-                       )
-    );
-
-  BOOST_REQUIRE_EQUAL (result.exit_code, 0);
-  BOOST_REQUIRE_EQUAL (result.bytes_written_stdin, to_read);
-  BOOST_REQUIRE_EQUAL (result.bytes_read_stdout, to_read);
-  BOOST_REQUIRE_EQUAL (result.bytes_read_stderr, 0);
+BOOST_AUTO_TEST_CASE (try_produce_more_than_available)
+{
+  std::size_t base (1 << 20);
+  //! \note Not possible to assert a specific amount written, as child
+  //! may consume more from stdin before getting SIGPIPE for stdout
+  consume (128 + SIGPIPE, base * 2, base, base * 2, boost::none, base);
 }
