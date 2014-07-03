5eab304c2060b6e81830789cdcda3961fd9e1479
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2014-07-03 09:23:23 +0200

    process: removed additional character in output buffer

--- share/process/test/environment.cpp
+++ share/process/test/environment.cpp
@@ -15,7 +15,7 @@ BOOST_AUTO_TEST_CASE (process_empty_environment)
   std::map<std::string, std::string> env;
 
   process::circular_buffer buf_stderr;
-  char out_stdout [1]; // one additional character to verify that no more than the 0 bytes were written
+  char out_stdout [0];
 
   process::execute_return_type ret
     ( process::execute ( env_program
@@ -45,7 +45,7 @@ BOOST_AUTO_TEST_CASE (process_single_variable_environment)
   env ["FOO"] = "BAR";
 
   process::circular_buffer buf_stderr;
-  char out_stdout [8+1]; // one additional character to verify that no more than the 8 bytes were written
+  char out_stdout [8];
 
   process::execute_return_type ret
     ( process::execute ( env_program
