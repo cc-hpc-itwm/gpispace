3758ad1b7546ce76d5da11523618ca5145c59900
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2014-07-14 15:42:44 +0200

    process: whitespace fix in 'env.c'

--- share/process/test/env.c
+++ share/process/test/env.c
@@ -4,7 +4,7 @@ int main (int argc, char *argv[], char *envp[])
 {
   char **env;
 
-  for (env = envp ; env && *env != 0 ; ++env)
+  for (env = envp; env && *env != 0; ++env)
   {
     printf ("%s\n", *env);
   }
