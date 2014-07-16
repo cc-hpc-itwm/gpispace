19c44742aed549388a7fccccc2a13b2c8ab3e25a
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-07-16 18:54:12 +0200

    drts: define and use options for virtual_memory

--- src/drts/drts.cpp
+++ src/drts/drts.cpp
@@ -3,15 +3,20 @@
 #include <drts/drts.hpp>
 #include <drts/system.hpp>
 
+#include <fhg/util/boost/program_options/validators/executable.hpp>
 #include <fhg/util/boost/program_options/validators/existing_directory.hpp>
 #include <fhg/util/boost/program_options/validators/existing_path.hpp>
 #include <fhg/util/boost/program_options/validators/is_directory_if_exists.hpp>
 #include <fhg/util/boost/program_options/validators/nonempty_string.hpp>
+#include <fhg/util/boost/program_options/validators/nonexisting_path.hpp>
 #include <fhg/util/boost/program_options/validators/positive_integral.hpp>
 
 #include <boost/format.hpp>
 
+#include <chrono>
+#include <iostream>
 #include <sstream>
+#include <thread>
 
 namespace gspc
 {
@@ -29,6 +34,13 @@ namespace gspc
       constexpr char const* const state_directory {"state-directory"};
       constexpr char const* const gspc_home {"gspc-home"};
       constexpr char const* const nodefile {"nodefile"};
+
+      constexpr char const* const virtual_memory_manager
+        {"virtual-memory-manager"};
+      constexpr char const* const virtual_memory_per_node
+        {"virtual-memory-per-node"};
+      constexpr char const* const virtual_memory_socket
+        {"virtual-memory-socket"};
     }
 
     boost::program_options::options_description logging()
@@ -84,6 +96,30 @@ namespace gspc
 
       return drts;
     }
+
+    boost::program_options::options_description virtual_memory()
+    {
+      boost::program_options::options_description vmem ("Runtime system");
+
+      vmem.add_options()
+        ( name::virtual_memory_manager
+        , boost::program_options::value<validators::executable>()
+        , "memory manager, typically installed in the privilegded folder"
+        )
+        ( name::virtual_memory_per_node
+        , boost::program_options::value
+          <validators::positive_integral<unsigned long>>()->required()
+        , "virtual memory per node in bytes"
+        )
+        ( name::virtual_memory_socket
+        , boost::program_options::value<validators::nonexisting_path>()
+        ->required()
+        , "virtual memory per node in bytes"
+        )
+        ;
+
+      return vmem;
+    }
   }
 
   scoped_runtime_system::scoped_runtime_system
@@ -101,6 +137,22 @@ namespace gspc
       , _nodefile
         ( boost::filesystem::canonical
           (vm[options::name::nodefile].as<validators::existing_path>())
+        )
+      , _virtual_memory_per_node
+        ( vm.count (options::name::virtual_memory_per_node)
+        ? boost::make_optional
+          ( vm[options::name::virtual_memory_per_node]
+          . as<validators::positive_integral<unsigned long>>()
+          )
+        : boost::none
+        )
+      , _virtual_memory_socket
+        ( vm.count (options::name::virtual_memory_socket)
+        ? boost::make_optional
+          ( vm[options::name::virtual_memory_socket]
+          . as<validators::nonexisting_path>()
+          )
+        : boost::none
         )
   {
     std::string const log_host
@@ -125,9 +177,41 @@ namespace gspc
       << " -f " << _nodefile
       << " -l " << log_host << ":" << log_port
       << " -g " << gui_host << ":" << gui_port
-      << boot_options;
+      ;
+
+    if (_virtual_memory_per_node)
+    {
+      boost::filesystem::path const virtual_memory_manager
+        ( boost::filesystem::canonical
+          ( vm[options::name::virtual_memory_manager]
+          . as<validators::executable>()
+          )
+        );
+
+      command_boot
+        << " -x " << virtual_memory_manager
+        << " -y " << *_virtual_memory_socket
+        << " -m " << *_virtual_memory_per_node
+        ;
+    }
+
+    command_boot << boot_options;
 
     system (command_boot.str(), "start runtime system");
+
+    if (_virtual_memory_per_node)
+    {
+      std::cerr << "waiting for virtual memory manager" << std::flush;
+
+      while (!boost::filesystem::exists (*_virtual_memory_socket))
+      {
+        std::cerr << "." << std::flush;
+
+        std::this_thread::sleep_for (std::chrono::milliseconds (200));
+      }
+
+      std::cerr << " OKAY" << std::endl;
+    }
   }
 
   scoped_runtime_system::~scoped_runtime_system()
--- src/drts/drts.hpp
+++ src/drts/drts.hpp
@@ -4,6 +4,7 @@
 #define DRTS_DRTS_HPP
 
 #include <boost/filesystem.hpp>
+#include <boost/optional.hpp>
 #include <boost/program_options.hpp>
 
 #include <string>
@@ -14,6 +15,7 @@ namespace gspc
   {
     boost::program_options::options_description logging();
     boost::program_options::options_description drts();
+    boost::program_options::options_description virtual_memory();
   }
 
   struct scoped_runtime_system
@@ -36,11 +38,21 @@ namespace gspc
     {
       return _nodefile;
     }
+    boost::optional<unsigned long> const& virtual_memory_per_node() const
+    {
+      return _virtual_memory_per_node;
+    }
+    boost::optional<boost::filesystem::path> const& virtual_memory_socket() const
+    {
+      return _virtual_memory_socket;
+    }
 
   private:
     boost::filesystem::path const _gspc_home;
     boost::filesystem::path const _state_directory;
     boost::filesystem::path const _nodefile;
+    boost::optional<unsigned long> _virtual_memory_per_node;
+    boost::optional<boost::filesystem::path> _virtual_memory_socket;
   };
 }
 
