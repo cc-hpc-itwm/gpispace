b554368ddee7d326ed215480bb7b31d8dff9dec5
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-07-16 18:18:21 +0200

    drts: define drts options and extract information from variables_map, add observers

--- src/drts/drts.cpp
+++ src/drts/drts.cpp
@@ -3,6 +3,9 @@
 #include <drts/drts.hpp>
 #include <drts/system.hpp>
 
+#include <fhg/util/boost/program_options/validators/existing_directory.hpp>
+#include <fhg/util/boost/program_options/validators/existing_path.hpp>
+#include <fhg/util/boost/program_options/validators/is_directory_if_exists.hpp>
 #include <fhg/util/boost/program_options/validators/nonempty_string.hpp>
 #include <fhg/util/boost/program_options/validators/positive_integral.hpp>
 
@@ -22,6 +25,10 @@ namespace gspc
       constexpr char const* const log_port {"log-port"};
       constexpr char const* const gui_host {"gui-host"};
       constexpr char const* const gui_port {"gui-port"};
+
+      constexpr char const* const state_directory {"state-directory"};
+      constexpr char const* const gspc_home {"gspc-home"};
+      constexpr char const* const nodefile {"nodefile"};
     }
 
     boost::program_options::options_description logging()
@@ -53,16 +60,48 @@ namespace gspc
 
       return logging;
     }
+
+    boost::program_options::options_description drts()
+    {
+      boost::program_options::options_description drts ("Runtime system");
+
+      drts.add_options()
+        ( name::gspc_home
+        , boost::program_options::value<validators::existing_directory>()
+        ->required()
+        , "gspc installation directory"
+        )
+        ( name::nodefile
+        , boost::program_options::value<validators::existing_path>()->required()
+        , "nodefile"
+        )
+        ( name::state_directory
+        , boost::program_options::value<validators::is_directory_if_exists>()
+        ->required()
+        , "directory where to store drts runtime state information"
+        )
+        ;
+
+      return drts;
+    }
   }
 
   scoped_runtime_system::scoped_runtime_system
     ( std::string const& boot_options
     , boost::program_options::variables_map const& vm
-    , boost::filesystem::path const& gspc_home
-    , boost::filesystem::path const& state_directory
     )
-      : _gspc_home (gspc_home)
-      , _state_directory (state_directory)
+      : _gspc_home
+        ( boost::filesystem::canonical
+          (vm[options::name::gspc_home].as<validators::existing_directory>())
+        )
+      , _state_directory
+        ( vm[options::name::state_directory]
+        . as<validators::is_directory_if_exists>()
+        )
+      , _nodefile
+        ( boost::filesystem::canonical
+          (vm[options::name::nodefile].as<validators::existing_path>())
+        )
   {
     std::string const log_host
       (vm[options::name::log_host].as<validators::nonempty_string>());
@@ -82,6 +121,8 @@ namespace gspc
     command_boot
       << (_gspc_home / "bin" / "sdpa")
       << " boot"
+      << " -S " << _state_directory
+      << " -f " << _nodefile
       << " -l " << log_host << ":" << log_port
       << " -g " << gui_host << ":" << gui_port
       << boot_options;
--- src/drts/drts.hpp
+++ src/drts/drts.hpp
@@ -13,21 +13,34 @@ namespace gspc
   namespace options
   {
     boost::program_options::options_description logging();
+    boost::program_options::options_description drts();
   }
 
   struct scoped_runtime_system
   {
     scoped_runtime_system ( std::string const& command_boot
                           , boost::program_options::variables_map const& vm
-                          , boost::filesystem::path const& gspc_home
-                          , boost::filesystem::path const& state_directory
                           );
 
     ~scoped_runtime_system();
 
+    boost::filesystem::path const& gspc_home() const
+    {
+      return _gspc_home;
+    }
+    boost::filesystem::path const& state_directory() const
+    {
+      return _state_directory;
+    }
+    boost::filesystem::path const& nodefile() const
+    {
+      return _nodefile;
+    }
+
   private:
-    boost::filesystem::path const& _gspc_home;
-    boost::filesystem::path const& _state_directory;
+    boost::filesystem::path const _gspc_home;
+    boost::filesystem::path const _state_directory;
+    boost::filesystem::path const _nodefile;
   };
 }
 
