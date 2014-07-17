10e583dec14fd2917630256860cfbcd41f4db357
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-07-17 10:38:53 +0200

    drts: remove unnecessary observers

--- src/drts/drts.hpp
+++ src/drts/drts.hpp
@@ -58,18 +58,6 @@ namespace gspc
     {
       return _gspc_home;
     }
-    boost::filesystem::path const& state_directory() const
-    {
-      return _state_directory;
-    }
-    boost::filesystem::path const& nodefile() const
-    {
-      return _nodefile;
-    }
-    boost::optional<unsigned long> const& virtual_memory_per_node() const
-    {
-      return _virtual_memory_per_node;
-    }
     unsigned long virtual_memory_total() const
     {
       return _nodes.size() * (*_virtual_memory_per_node - 32UL * (1UL << 20UL));
