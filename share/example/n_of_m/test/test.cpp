97aa74cbaf210eed92a62f07b5df792d650d0f18
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-07-19 13:57:10 +0200

    share: example: n_of_m: replace test.sumit with binary test.cpp

---
+++ share/example/n_of_m/test/test.cpp
@@ -0,0 +1,137 @@
+// mirko.rahn@itwm.fraunhofer.de
+
+#define BOOST_TEST_MODULE share_example_n_of_m
+#include <boost/test/unit_test.hpp>
+
+#include <drts/drts.hpp>
+
+#include <test/scoped_nodefile_with_localhost.hpp>
+#include <test/scoped_state_directory.hpp>
+#include <test/source_directory.hpp>
+
+#include <we/type/literal/control.hpp>
+#include <we/type/value.hpp>
+#include <we/type/value/boost/test/printer.hpp>
+
+#include <fhg/util/boost/program_options/validators/executable.hpp>
+
+#include <fhg/util/hostname.hpp>
+#include <fhg/util/temporary_path.hpp>
+
+#include <boost/filesystem.hpp>
+#include <boost/format.hpp>
+#include <boost/program_options.hpp>
+
+#include <map>
+#include <sstream>
+#include <stdexcept>
+
+BOOST_AUTO_TEST_CASE (share_example_n_of_m)
+{
+  namespace validators = fhg::util::boost::program_options;
+
+  boost::program_options::options_description options_description;
+
+  constexpr char const* const option_command ("command");
+
+  options_description.add_options()
+    ( option_command
+    , boost::program_options::value<validators::executable>()->required()
+    , "command to execute"
+    );
+
+  options_description.add (test::options::source_directory());
+  options_description.add (gspc::options::installation());
+  options_description.add (gspc::options::drts());
+  options_description.add (gspc::options::logging());
+
+  boost::program_options::variables_map vm;
+  boost::program_options::store
+    ( boost::program_options::command_line_parser
+      ( boost::unit_test::framework::master_test_suite().argc
+      , boost::unit_test::framework::master_test_suite().argv
+      )
+    . options (options_description).run()
+    , vm
+    );
+
+  test::scoped_state_directory const state_directory (vm);
+  test::scoped_nodefile_with_localhost const nodefile_with_localhost (vm);
+
+  //! \todo start logger with specific port
+  gspc::set_log_host (vm, fhg::util::hostname());
+  gspc::set_log_port (vm, 47095);
+
+  //! \todo either disable gui logging or get the port from a gui manager
+  gspc::set_gui_host (vm, fhg::util::hostname());
+  gspc::set_gui_port (vm, 47096);
+
+  fhg::util::temporary_path const _installation_dir
+    ( boost::filesystem::temp_directory_path()
+    / boost::filesystem::unique_path()
+    );
+  boost::filesystem::path const installation_dir (_installation_dir);
+
+  gspc::set_application_search_path (vm, installation_dir);
+
+  vm.notify();
+
+  gspc::installation const installation (vm);
+
+  fhg::util::temporary_path const _build_dir
+    ( boost::filesystem::temp_directory_path()
+    / boost::filesystem::unique_path()
+    );
+  boost::filesystem::path const build_dir (_build_dir);
+
+  std::ostringstream command_build;
+
+  command_build
+    << " make -f "
+    << (installation.gspc_home() / "share" / "sdpa" / "make" / "common.mk")
+    << " SDPA_HOME=" << installation.gspc_home()
+    << " BOOST_ROOT=" << (installation.gspc_home() / "external" / "boost")
+    << " BUILDDIR=" << build_dir
+    << " MAIN=n_of_m"
+    << " LIB_DESTDIR=" << installation_dir
+    << " CXXLIBRARYPATHS=" << (installation.gspc_home() / "libexec" / "sdpa")
+    << " -C " << test::source_directory(vm)
+    << " net lib install"
+    ;
+
+  if (int ec = std::system (command_build.str().c_str()) != 0)
+  {
+    throw std::runtime_error
+      (( boost::format ("Could not run '%1%': error code '%1%'")
+       % command_build.str()
+       % ec
+       ).str()
+      );
+  }
+
+  gspc::scoped_runtime_system const drts (vm, installation, " worker:12");
+
+  std::multimap<std::string, pnet::type::value::value_type> const result
+    ( drts.put_and_run
+      ( build_dir / "n_of_m.pnet"
+      , { {"m", 25L}
+        , {"parallel", 2L}
+        , {"cmd"
+          , boost::filesystem::path
+              (vm[option_command].as<validators::executable>()).string()
+          }
+        }
+      )
+    );
+
+  BOOST_REQUIRE_EQUAL (result.size(), 1);
+
+  std::string const port_done ("done");
+
+  BOOST_REQUIRE_EQUAL (result.count (port_done), 1);
+
+  BOOST_CHECK_EQUAL
+    ( result.find (port_done)->second
+    , pnet::type::value::value_type (we::type::literal::control())
+    );
+}
