3cc25d093b7d0f8f98d3f67b92aae4d48c26ce50
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-07-19 14:03:32 +0200

    test: added setup_logging

---
+++ test/setup_logging.hpp
@@ -0,0 +1,13 @@
+// mirko.rahn@itwm.fraunhofer.de
+
+#ifndef TEST_SETUP_LOGGING_HPP
+#define TEST_SETUP_LOGGING_HPP
+
+#include <boost/program_options.hpp>
+
+namespace test
+{
+  void setup_logging (boost::program_options::variables_map&);
+}
+
+#endif
