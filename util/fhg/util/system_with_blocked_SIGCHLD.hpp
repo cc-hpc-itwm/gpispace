3fbf224241f0ef40f2268ff6f5dfeb6a3dcb83c7
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-07-21 14:10:04 +0200

    util: added system_with_blocked_SIGCHLD

---
+++ util/fhg/util/system_with_blocked_SIGCHLD.hpp
@@ -0,0 +1,14 @@
+// {bernd.loerwald,mirko.rahn}@itwm.fraunhofer.de
+
+#ifndef FHG_UTIL_SYSTEM_WITH_BLOCKED_SIGCHLD_HPP
+#define FHG_UTIL_SYSTEM_WITH_BLOCKED_SIGCHLD_HPP
+
+namespace fhg
+{
+  namespace util
+  {
+    int system_with_blocked_SIGCHLD (const char*);
+  }
+}
+
+#endif
