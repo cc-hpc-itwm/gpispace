2ec5ad307c15949f41347119fa3730225f8755da
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2014-07-23 13:47:42 +0200

    sdpa: worker: removed comments, the class methods have sufficiently descriptive names

--- sdpa/sdpa/daemon/Worker.hpp
+++ sdpa/sdpa/daemon/Worker.hpp
@@ -13,13 +13,6 @@
 
 namespace sdpa { namespace daemon {
 
-  /**
-    This class holds all information about an attached worker.
-
-    On the orchestrator this represents an aggregator and on the aggregator
-    all information about attached NREs is held in this class.
-  */
-
   class Worker {
   public:
 
@@ -27,11 +20,6 @@ namespace sdpa { namespace daemon {
     typedef boost::recursive_mutex mutex_type;
     typedef boost::unique_lock<mutex_type> lock_type;
 
-    /**
-      A worker has a globally unique name.
-
-      @param name a unique name for the worker
-     */
     explicit Worker ( const worker_id_t&
                     , const boost::optional<unsigned int>&
                     , const capabilities_set_t&
@@ -39,29 +27,14 @@ namespace sdpa { namespace daemon {
                     );
 
     void submit (const job_id_t&);
-
-    /**
-	 Acknowledge a given job id and move it to the acknowledged_ queue.
-
-	 @param job_id the job_id to acknowledge
-	 */
     void acknowledge (const job_id_t&);
 
     // update last service time
     double lastScheduleTime() {lock_type lock(mtx_); return last_schedule_time_; }
 
-    /**
-      Return the name of the worker.
-    */
     const worker_id_t &name() const { lock_type lock(mtx_); return name_; }
-
-    /**
-       Return the host name (the worker name already contains the name of the host!)
-    */
     const std::string hostname() const;
-    /**
-         Return the rank of the worker.
-     */
+
     boost::optional<unsigned int> capacity() const { lock_type lock(mtx_); return capacity_; }
 
     // capabilities
@@ -71,18 +44,8 @@ namespace sdpa { namespace daemon {
     void removeCapabilities (const capabilities_set_t& cpbset);
     bool hasCapability (const std::string& cpbName);
 
-    /**
-         Checks if the worker has job
-    */
     bool has_job (const job_id_t& job_id);
 
-
-    /**
-      Remove a job that was finished or failed from the acknowledged_ queue
-
-      a second flag is needed in the case the job is canceled (in order to look into the other queues, as well)
-      @param last_job_id the id of the last sucessfully submitted job
-    */
     void deleteJob (const job_id_t &job_id );
 
     // methods related to reservation
