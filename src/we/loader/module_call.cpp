a0907014f5fcbd75376a40c97a7217ef101485c1
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-07-29 13:47:33 +0200

    we: move ranges into type/range.hpp

--- src/we/loader/module_call.cpp
+++ src/we/loader/module_call.cpp
@@ -3,7 +3,7 @@
 #include <we/type/id.hpp>
 #include <we/type/port.hpp>
 #include <we/expr/parse/parser.hpp>
-#include <we/field.hpp>
+#include <we/type/range.hpp>
 
 #include <fvm-pc/pc.hpp>
 // used
@@ -15,8 +15,6 @@
 
 //! \todo remove, needed to make a complete type
 #include <we/type/net.hpp>
-
-#include <fhg/assert.hpp>
 
 #include <boost/format.hpp>
 
@@ -79,108 +77,6 @@ namespace we
       unsigned long _size;
       unsigned long _position;
     };
-
-    struct interval
-    {
-    public:
-      interval (pnet::type::value::value_type const& value)
-        : _offset ( pnet::field_as<unsigned long>
-                    ("offset", value, std::string ("unsigned long"))
-                  )
-        , _size ( pnet::field_as<unsigned long>
-                  ("size", value, std::string ("unsigned long"))
-                )
-      {}
-      interval (interval const& r, unsigned long size)
-        : _offset (r.offset())
-        , _size (size)
-      {
-        fhg_assert (size <= r.size());
-      }
-      void shrink (unsigned long delta)
-      {
-        fhg_assert (delta <= _size);
-
-        _offset += delta;
-        _size -= delta;
-      }
-      unsigned long offset() const
-      {
-        return _offset;
-      }
-      unsigned long size() const
-      {
-        return _size;
-      }
-
-    private:
-      unsigned long _offset;
-      unsigned long _size;
-    };
-
-    namespace global
-    {
-      struct handle
-      {
-      public:
-        handle (pnet::type::value::value_type const& value)
-          : _name ( pnet::field_as<std::string>
-                    ("name", value, std::string ("string"))
-                  )
-        {}
-        std::string name() const
-        {
-          return _name;
-        }
-      private:
-        std::string _name;
-      };
-
-      struct range : public interval
-      {
-      public:
-        range (pnet::type::value::value_type const& value)
-          : interval (value)
-          , _handle (pnet::field ("handle", value, std::string ("handle")))
-        {}
-        range (range const& r, unsigned long size)
-          : interval (r, size)
-          , _handle (r.handle())
-        {}
-        struct handle const& handle() const
-        {
-          return _handle;
-        }
-
-      private:
-        struct handle _handle;
-      };
-    }
-
-    namespace local
-    {
-      struct range : interval
-      {
-      public:
-        range (pnet::type::value::value_type const& value)
-          : interval (value)
-          , _buffer ( pnet::field_as<std::string>
-                      ("buffer", value, std::string ("string"))
-                    )
-        {}
-        range (range const& r, unsigned long size)
-          : interval (r, size)
-          , _buffer (r.buffer())
-        {}
-        std::string const& buffer() const
-        {
-          return _buffer;
-        }
-
-      private:
-        std::string _buffer;
-      };
-    }
 
     template<typename Range>
       std::list<Range> evaluate ( expr::eval::context& context
