c141df256c537d97d82a087fc1d57d6fe69df996
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-08-01 13:06:54 +0200

    tutorial: sum: test: factor out run_and_check

--- doc/tutorial/sum/test/test.cpp
+++ doc/tutorial/sum/test/test.cpp
@@ -26,6 +26,54 @@
 #include <map>
 #include <vector>
 
+namespace
+{
+  void run_and_check
+    ( boost::program_options::variables_map const& vm
+    , gspc::installation const& installation
+    , boost::filesystem::path const& pnet
+    )
+  {
+    gspc::scoped_runtime_system const drts (vm, installation, "work:4");
+
+    auto pair
+      ( [] (long x, long y) -> pnet::type::value::value_type
+      {
+        pnet::type::value::value_type v;
+        pnet::type::value::poke ("x", v, x);
+        pnet::type::value::poke ("y", v, y);
+        return v;
+      }
+      );
+
+    std::multimap<std::string, pnet::type::value::value_type> const result
+      (drts.put_and_run (pnet, {{"p", pair (3, 4)}, {"p", pair (-2, 3)}}));
+
+    BOOST_REQUIRE_EQUAL (result.size(), 2);
+
+    std::string const port_s ("s");
+
+    BOOST_REQUIRE_EQUAL (result.count (port_s), 2);
+
+    std::vector<long> const expected_output {1L, 7L};
+    std::vector<long> output;
+
+    for ( pnet::type::value::value_type i
+        : result.equal_range (port_s) | boost::adaptors::map_values
+        )
+    {
+      output.push_back (boost::get<long> (i));
+    }
+
+    std::sort (output.begin(), output.end());
+
+    BOOST_REQUIRE_EQUAL_COLLECTIONS
+      ( expected_output.begin(), expected_output.end()
+      , output.begin(), output.end()
+      );
+  }
+}
+
 BOOST_AUTO_TEST_CASE (tutorial_sum_expr)
 {
   boost::program_options::options_description options_description;
@@ -64,49 +112,8 @@ BOOST_AUTO_TEST_CASE (tutorial_sum_expr)
     , "net"
     );
 
-  gspc::scoped_runtime_system const drts (vm, installation, " work:4");
-
-  auto pair
-    ( [] (long x, long y) -> pnet::type::value::value_type
-      {
-        pnet::type::value::value_type v;
-        pnet::type::value::poke ("x", v, x);
-        pnet::type::value::poke ("y", v, y);
-        return v;
-      }
-    );
-
-  std::multimap<std::string, pnet::type::value::value_type> const result
-    ( drts.put_and_run
-      ( make.build_directory() / "sum_expr_many.pnet"
-      , { {"p", pair (3, 4)}
-        , {"p", pair (-2, 3)}
-        }
-      )
-    );
-
-  BOOST_REQUIRE_EQUAL (result.size(), 2);
-
-  std::string const port_s ("s");
-
-  BOOST_REQUIRE_EQUAL (result.count (port_s), 2);
-
-  std::vector<long> const expected_output {1L, 7L};
-  std::vector<long> output;
-
-  for ( pnet::type::value::value_type i
-      : result.equal_range (port_s) | boost::adaptors::map_values
-      )
-  {
-    output.push_back (boost::get<long> (i));
-  }
-
-  std::sort (output.begin(), output.end());
-
-  BOOST_REQUIRE_EQUAL_COLLECTIONS
-    ( expected_output.begin(), expected_output.end()
-    , output.begin(), output.end()
-    );
+  run_and_check
+    (vm, installation, make.build_directory () / "sum_expr_many.pnet");
 }
 
 BOOST_AUTO_TEST_CASE (tutorial_sum_mod)
@@ -185,47 +192,5 @@ BOOST_AUTO_TEST_CASE (tutorial_sum_mod)
     , "net lib install"
     );
 
-  gspc::scoped_runtime_system const drts (vm, installation, " work:4");
-
-  auto pair
-    ( [] (long x, long y) -> pnet::type::value::value_type
-      {
-        pnet::type::value::value_type v;
-        pnet::type::value::poke ("x", v, x);
-        pnet::type::value::poke ("y", v, y);
-        return v;
-      }
-    );
-
-  std::multimap<std::string, pnet::type::value::value_type> const result
-    ( drts.put_and_run
-      ( make.build_directory() / "sum_many.pnet"
-      , { {"p", pair (3, 4)}
-        , {"p", pair (-2, 3)}
-        }
-      )
-    );
-
-  BOOST_REQUIRE_EQUAL (result.size(), 2);
-
-  std::string const port_s ("s");
-
-  BOOST_REQUIRE_EQUAL (result.count (port_s), 2);
-
-  std::vector<long> const expected_output {1L, 7L};
-  std::vector<long> output;
-
-  for ( pnet::type::value::value_type i
-      : result.equal_range (port_s) | boost::adaptors::map_values
-      )
-  {
-    output.push_back (boost::get<long> (i));
-  }
-
-  std::sort (output.begin(), output.end());
-
-  BOOST_REQUIRE_EQUAL_COLLECTIONS
-    ( expected_output.begin(), expected_output.end()
-    , output.begin(), output.end()
-    );
+  run_and_check (vm, installation, make.build_directory () / "sum_many.pnet");
 }
