e6d8cce2674f47e392a32276c1e112653b0dd7a3
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-08-05 18:02:20 +0200

    application: asian: inline asian.h

--- application/asian/asianopt.h
+++ application/asian/asianopt.h
@@ -1,5 +1,18 @@
 #include<vector>
-#include <asian.h>
+
+typedef struct param_s {
+  double   m_dS;
+  double   m_dK;
+  double   m_dT;
+
+  double   m_dSigma;
+  double   m_dr;
+  double   m_dd;
+  int      m_nFirstFixing;
+  double   m_dFixingsProJahr;
+  long int m_nn;
+
+} param_t;
 
 #ifndef __ASIAN_H__
 #define __ASIAN_H__
