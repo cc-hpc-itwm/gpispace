e92188e85c364fdd89cb667dbde5eb3027aa724a
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2014-09-04 14:16:41 +0200

    drts: expose 'gspc::options::name' to the outside

--- src/drts/drts.cpp
+++ src/drts/drts.cpp
@@ -38,35 +38,6 @@ namespace gspc
 
   namespace options
   {
-    namespace name
-    {
-      namespace
-      {
-        constexpr char const* const log_host {"log-host"};
-        constexpr char const* const log_port {"log-port"};
-        constexpr char const* const log_level {"log-level"};
-        constexpr char const* const gui_host {"gui-host"};
-        constexpr char const* const gui_port {"gui-port"};
-
-        constexpr char const* const state_directory {"state-directory"};
-        constexpr char const* const gspc_home {"gspc-home"};
-        constexpr char const* const nodefile {"nodefile"};
-        constexpr char const* const application_search_path
-          {"application-search-path"};
-
-        constexpr char const* const virtual_memory_manager
-          {"virtual-memory-manager"};
-        constexpr char const* const virtual_memory_per_node
-          {"virtual-memory-per-node"};
-        constexpr char const* const virtual_memory_socket
-          {"virtual-memory-socket"};
-        constexpr char const* const virtual_memory_port
-          {"virtual-memory-port"};
-        constexpr char const* const virtual_memory_timeout
-          {"virtual-memory-timeout"};
-      }
-    }
-
     boost::program_options::options_description logging()
     {
       boost::program_options::options_description logging ("Logging");
