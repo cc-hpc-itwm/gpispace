9619883440f7c4ab73e93dd0d6fc5eb3af08cac4
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2014-09-05 13:35:41 +0200

    sdpa: worker: fixed formatting & indentation

--- sdpa/sdpa/daemon/Worker.cpp
+++ sdpa/sdpa/daemon/Worker.cpp
@@ -9,131 +9,131 @@ namespace sdpa
 {
   namespace daemon
   {
-Worker::Worker(	const worker_id_t& name,
-				const boost::optional<unsigned int>& cap
-              , const capabilities_set_t& capabilities
-              )
-  : name_(name),
-    capacity_(cap)
-  , capabilities_ (capabilities)
-  , last_schedule_time_(0),
-    reserved_(false)
-{
-
-}
-
-bool Worker::has_job( const job_id_t& job_id )
-{
-  lock_type const _ (mtx_);
-  return submitted_.count (job_id) || acknowledged_.count (job_id);
-}
-
-void Worker::submit(const job_id_t& jobId)
-{
-  lock_type const _ (mtx_);
-  submitted_.insert (jobId);
-  reserve();
-}
-
-void Worker::acknowledge(const job_id_t &job_id)
-{
-  lock_type const _ (mtx_);
-  if (submitted_.erase (job_id) == 0)
-  {
-    throw std::runtime_error ("acknowledge: job not in submitted queue");
-  }
-  acknowledged_.insert (job_id);
-}
-
-void Worker::deleteJob(const job_id_t &job_id)
-{
-  lock_type const _ (mtx_);
-  submitted_.erase (job_id);
-  acknowledged_.erase (job_id);
-  free();
-}
-
-const capabilities_set_t& Worker::capabilities() const
-{
-  lock_type const _ (mtx_);
-  return capabilities_;
-}
-
-bool Worker::addCapabilities( const capabilities_set_t& recvCpbSet )
-{
-  lock_type const _ (mtx_);
-
-  bool bModified = false;
-  for (Capability const& capability : recvCpbSet)
-  {
-    capabilities_set_t::iterator itwcpb (capabilities_.find (capability));
-    if (itwcpb == capabilities_.end())
+    Worker::Worker( const worker_id_t& name
+		  , const boost::optional<unsigned int>& cap
+		  , const capabilities_set_t& capabilities
+		  )
+      : name_(name)
+      , capacity_(cap)
+      , capabilities_ (capabilities)
+      , last_schedule_time_(0)
+      ,	reserved_(false)
     {
-      capabilities_.insert (capability);
-      bModified = true;
+
     }
-    else if (itwcpb->depth() > capability.depth())
+
+    bool Worker::has_job( const job_id_t& job_id )
     {
-      capabilities_.erase (itwcpb);
-      capabilities_.insert (capability);
-      bModified = true;
+      lock_type const _ (mtx_);
+      return submitted_.count (job_id) || acknowledged_.count (job_id);
     }
-  }
 
-  return bModified;
-}
+    void Worker::submit(const job_id_t& jobId)
+    {
+      lock_type const _ (mtx_);
+      submitted_.insert (jobId);
+      reserve();
+    }
 
-void Worker::removeCapabilities( const capabilities_set_t& cpbset )
-{
-  lock_type const _ (mtx_);
-  for (Capability const& capability : cpbset)
-  {
-    capabilities_.erase (capability);
-  }
-}
+    void Worker::acknowledge(const job_id_t &job_id)
+    {
+      lock_type const _ (mtx_);
+      if (submitted_.erase (job_id) == 0)
+      {
+	throw std::runtime_error ("acknowledge: job not in submitted queue");
+      }
+      acknowledged_.insert (job_id);
+    }
 
-bool Worker::hasCapability(const std::string& cpbName)
-{
-  lock_type const _ (mtx_);
+    void Worker::deleteJob(const job_id_t &job_id)
+    {
+      lock_type const _ (mtx_);
+      submitted_.erase (job_id);
+      acknowledged_.erase (job_id);
+      free();
+    }
 
-  return std::find_if ( capabilities_.begin(), capabilities_.end()
-                      , [&cpbName] (capability_t const& cap)
-                      {
-                        return cap.name() == cpbName;
-                      }
-                      ) != capabilities_.end();
-}
+    const capabilities_set_t& Worker::capabilities() const
+    {
+      lock_type const _ (mtx_);
+      return capabilities_;
+    }
 
-void Worker::reserve()
-{
-  lock_type const _ (mtx_);
-  reserved_ = true;
-  last_schedule_time_ = fhg::util::now();
-}
+    bool Worker::addCapabilities( const capabilities_set_t& recvCpbSet )
+    {
+      lock_type const _ (mtx_);
 
-bool Worker::isReserved()
-{
-  lock_type const _ (mtx_);
-  return reserved_;
-}
+      bool bModified = false;
+      for (Capability const& capability : recvCpbSet)
+      {
+	capabilities_set_t::iterator itwcpb (capabilities_.find (capability));
+	if (itwcpb == capabilities_.end())
+	{
+	  capabilities_.insert (capability);
+	  bModified = true;
+	}
+	else if (itwcpb->depth() > capability.depth())
+	{
+	  capabilities_.erase (itwcpb);
+	  capabilities_.insert (capability);
+	  bModified = true;
+	}
+      }
 
-void Worker::free()
-{
-  lock_type const _ (mtx_);
-  reserved_ = false;
-}
+      return bModified;
+    }
 
-std::set<job_id_t> Worker::getJobListAndCleanQueues()
-{
-  lock_type const _ (mtx_);
-  std::set<job_id_t> listAssignedJobs;
+    void Worker::removeCapabilities( const capabilities_set_t& cpbset )
+    {
+      lock_type const _ (mtx_);
+      for (Capability const& capability : cpbset)
+      {
+	capabilities_.erase (capability);
+      }
+    }
 
-  listAssignedJobs.insert (submitted_.begin(), submitted_.end());
-  listAssignedJobs.insert (acknowledged_.begin(), acknowledged_.end());
-  submitted_.clear();
-  acknowledged_.clear();
+    bool Worker::hasCapability(const std::string& cpbName)
+    {
+      lock_type const _ (mtx_);
 
-  return listAssignedJobs;
-}
+      return std::find_if ( capabilities_.begin(), capabilities_.end()
+			  , [&cpbName] (capability_t const& cap)
+			  {
+			    return cap.name() == cpbName;
+			  }
+			  ) != capabilities_.end();
+    }
+
+    void Worker::reserve()
+    {
+      lock_type const _ (mtx_);
+      reserved_ = true;
+      last_schedule_time_ = fhg::util::now();
+    }
+
+    bool Worker::isReserved()
+    {
+      lock_type const _ (mtx_);
+      return reserved_;
+    }
+
+    void Worker::free()
+    {
+      lock_type const _ (mtx_);
+      reserved_ = false;
+    }
+
+    std::set<job_id_t> Worker::getJobListAndCleanQueues()
+    {
+      lock_type const _ (mtx_);
+      std::set<job_id_t> listAssignedJobs;
+
+      listAssignedJobs.insert (submitted_.begin(), submitted_.end());
+      listAssignedJobs.insert (acknowledged_.begin(), acknowledged_.end());
+      submitted_.clear();
+      acknowledged_.clear();
+
+      return listAssignedJobs;
+    }
   }
 }
