ba890840bc2cf6df031dea9b9572a269b22be40a
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2014-09-22 12:55:44 +0200

    cmake: removed commented lines in FindGASPI.cmake

--- modules/FindGASPI.cmake
+++ modules/FindGASPI.cmake
@@ -51,15 +51,3 @@ find_package_handle_standard_args (GASPI
   REQUIRED_VARS GASPI_INCLUDE_DIR GASPI_STATIC_LIBRARY GASPI_LIBRARY GASPI_VERSION_STRING
   VERSION_VAR GASPI_VERSION_STRING
 )
-
-#if (GASPI_INCLUDE_DIR AND GASPI_LIBRARY)
-#  set (GASPI_FOUND TRUE)
-#  get_filename_component (GASPI_LD_PATH ${GASPI_LIBRARY} PATH)
-#  if (NOT GASPI_FIND_QUIETLY)
-#    message (STATUS "Found GASPI headers in ${GASPI_INCLUDE_DIR} and libraries ${GASPI_LIBRARY} ${GASPI_SHARED_LIBRARY}")
-#  endif ()
-#else ()
-#  if (GASPI_FIND_REQUIRED)
-#    message (FATAL_ERROR "GASPI could not be found!")
-#  endif ()
-#endif ()
