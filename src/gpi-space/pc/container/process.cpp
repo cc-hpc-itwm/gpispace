306ef4ae4d36d017e71e91aadd3d4fc26f9e4792
Alexander Petry <alexander.petry@itwm.fraunhofer.de>
2014-10-07 15:53:56 +0200

    vmem: implemented 'get_transfer_costs'
    - manager delegates costs to actual memory area, collecting the combined costs
      for each rank

--- src/gpi-space/pc/container/process.cpp
+++ src/gpi-space/pc/container/process.cpp
@@ -141,6 +141,14 @@ namespace gpi
             return gpi::pc::proto::memory::message_t (rpl);
           }
 
+          gpi::pc::proto::message_t
+            operator() (const gpi::pc::proto::memory::get_transfer_costs_t& request) const
+          {
+            gpi::pc::proto::memory::transfer_costs_t rpl;
+            rpl.costs = _memory_manager.get_transfer_costs (request.transfers);
+            return gpi::pc::proto::memory::message_t (rpl);
+          }
+
           /**********************************************/
           /***     S E G M E N T   R E L A T E D      ***/
           /**********************************************/
