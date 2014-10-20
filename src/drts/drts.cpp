f5773593114f6eefde282b70427db3060b54deb2
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2014-10-20 18:01:42 +0200

    drts: introduced option.hpp, not installed in the public api but can be used by internal implementations to get the option names

--- src/drts/drts.cpp
+++ src/drts/drts.cpp
@@ -1,6 +1,7 @@
 // mirko.rahn@itwm.fraunhofer.de
 
 #include <drts/drts.hpp>
+#include <drts/option.hpp>
 
 #include <drts/virtual_memory.hpp>
 
@@ -38,137 +39,6 @@
 namespace gspc
 {
   namespace validators = fhg::util::boost::program_options;
-
-  namespace options
-  {
-    namespace name
-    {
-      constexpr char const* const log_host {"log-host"};
-      constexpr char const* const log_port {"log-port"};
-      constexpr char const* const log_level {"log-level"};
-      constexpr char const* const gui_host {"gui-host"};
-      constexpr char const* const gui_port {"gui-port"};
-
-      constexpr char const* const state_directory {"state-directory"};
-      constexpr char const* const gspc_home {"gspc-home"};
-      constexpr char const* const nodefile {"nodefile"};
-      constexpr char const* const application_search_path
-      {"application-search-path"};
-
-      constexpr char const* const virtual_memory_per_node
-      {"virtual-memory-per-node"};
-      constexpr char const* const virtual_memory_socket
-      {"virtual-memory-socket"};
-      constexpr char const* const virtual_memory_port
-      {"virtual-memory-port"};
-      constexpr char const* const virtual_memory_startup_timeout
-      {"virtual-memory-startup-timeout"};
-    }
-
-    boost::program_options::options_description logging()
-    {
-      boost::program_options::options_description logging ("Logging");
-
-      logging.add_options()
-        ( name::log_host
-        , boost::program_options::value<validators::nonempty_string>()
-        ->required()
-        , "name of log host"
-        )
-        ( name::log_port
-        , boost::program_options::value
-          <validators::positive_integral<unsigned short>>()->required()
-        , "port on log-host to log to"
-        )
-        ( name::log_level
-        , boost::program_options::value<std::string>()->default_value ("INFO")
-        , "log level to use"
-        )
-        ( name::gui_host
-        , boost::program_options::value<validators::nonempty_string>()
-          ->required()
-        , "name of gui host"
-        )
-        ( name::gui_port
-        , boost::program_options::value
-          <validators::positive_integral<unsigned short>>()->required()
-        , "port on gui-host to send to"
-        )
-        ;
-
-      return logging;
-    }
-
-    boost::program_options::options_description installation()
-    {
-      boost::program_options::options_description
-        installation ("GSPC Installation");
-
-      installation.add_options()
-        ( name::gspc_home
-        , boost::program_options::value<validators::existing_directory>()
-        ->required()
-        , "gspc installation directory"
-        )
-        ;
-
-      return installation;
-    }
-
-    boost::program_options::options_description drts()
-    {
-      boost::program_options::options_description drts ("Runtime system");
-
-      drts.add_options()
-        ( name::nodefile
-        , boost::program_options::value<validators::existing_path>()->required()
-        , "nodefile"
-        )
-        ( name::state_directory
-        , boost::program_options::value<validators::is_directory_if_exists>()
-        ->required()
-        , "directory where to store drts runtime state information"
-        )
-        //! \todo let it be a list of existing_directories
-        ( name::application_search_path
-        , boost::program_options::value<validators::existing_directory>()
-        , "adds a path to the list of application search paths"
-        )
-        ;
-
-      return drts;
-    }
-
-    boost::program_options::options_description virtual_memory()
-    {
-      boost::program_options::options_description vmem ("Virtual memory");
-
-      vmem.add_options()
-        ( name::virtual_memory_per_node
-        , boost::program_options::value
-          <validators::positive_integral<unsigned long>>()->required()
-        , "virtual memory per node in bytes"
-        )
-        ( name::virtual_memory_socket
-        , boost::program_options::value<validators::nonexisting_path>()
-        ->required()
-        , "socket file to communicate with the virtual memory manager"
-        )
-        ( name::virtual_memory_port
-        , boost::program_options::value<validators::positive_integral<unsigned short>>()
-        ->required()
-        , "internal communication port that shall be used by the virtual memory manager"
-        )
-        ( name::virtual_memory_startup_timeout
-        , boost::program_options::value<validators::positive_integral<unsigned long>>()
-        ->required()
-        , "timeout in seconds for the virtual memory manager to connect and start up."
-        )
-        ;
-
-      return vmem;
-    }
-  }
 
   namespace
   {
---
+++ src/drts/option.cpp
@@ -0,0 +1,125 @@
+// mirko.rahn@itwm.fraunhofer.de
+
+#include <drts/drts.hpp>
+
+#include <drts/option.hpp>
+
+#include <fhg/util/boost/program_options/validators/executable.hpp>
+#include <fhg/util/boost/program_options/validators/existing_directory.hpp>
+#include <fhg/util/boost/program_options/validators/existing_path.hpp>
+#include <fhg/util/boost/program_options/validators/is_directory_if_exists.hpp>
+#include <fhg/util/boost/program_options/validators/nonempty_string.hpp>
+#include <fhg/util/boost/program_options/validators/nonexisting_path.hpp>
+#include <fhg/util/boost/program_options/validators/positive_integral.hpp>
+
+namespace gspc
+{
+  namespace validators = fhg::util::boost::program_options;
+
+  namespace options
+  {
+    boost::program_options::options_description logging()
+    {
+      boost::program_options::options_description logging ("Logging");
+
+      logging.add_options()
+        ( name::log_host
+        , boost::program_options::value<validators::nonempty_string>()
+        ->required()
+        , "name of log host"
+        )
+        ( name::log_port
+        , boost::program_options::value
+          <validators::positive_integral<unsigned short>>()->required()
+        , "port on log-host to log to"
+        )
+        ( name::log_level
+        , boost::program_options::value<std::string>()->default_value ("INFO")
+        , "log level to use"
+        )
+        ( name::gui_host
+        , boost::program_options::value<validators::nonempty_string>()
+          ->required()
+        , "name of gui host"
+        )
+        ( name::gui_port
+        , boost::program_options::value
+          <validators::positive_integral<unsigned short>>()->required()
+        , "port on gui-host to send to"
+        )
+        ;
+
+      return logging;
+    }
+
+    boost::program_options::options_description installation()
+    {
+      boost::program_options::options_description
+        installation ("GSPC Installation");
+
+      installation.add_options()
+        ( name::gspc_home
+        , boost::program_options::value<validators::existing_directory>()
+        ->required()
+        , "gspc installation directory"
+        )
+        ;
+
+      return installation;
+    }
+
+    boost::program_options::options_description drts()
+    {
+      boost::program_options::options_description drts ("Runtime system");
+
+      drts.add_options()
+        ( name::nodefile
+        , boost::program_options::value<validators::existing_path>()->required()
+        , "nodefile"
+        )
+        ( name::state_directory
+        , boost::program_options::value<validators::is_directory_if_exists>()
+        ->required()
+        , "directory where to store drts runtime state information"
+        )
+        //! \todo let it be a list of existing_directories
+        ( name::application_search_path
+        , boost::program_options::value<validators::existing_directory>()
+        , "adds a path to the list of application search paths"
+        )
+        ;
+
+      return drts;
+    }
+
+    boost::program_options::options_description virtual_memory()
+    {
+      boost::program_options::options_description vmem ("Virtual memory");
+
+      vmem.add_options()
+        ( name::virtual_memory_per_node
+        , boost::program_options::value
+          <validators::positive_integral<unsigned long>>()->required()
+        , "virtual memory per node in bytes"
+        )
+        ( name::virtual_memory_socket
+        , boost::program_options::value<validators::nonexisting_path>()
+        ->required()
+        , "socket file to communicate with the virtual memory manager"
+        )
+        ( name::virtual_memory_port
+        , boost::program_options::value<validators::positive_integral<unsigned short>>()
+        ->required()
+        , "internal communication port that shall be used by the virtual memory manager"
+        )
+        ( name::virtual_memory_startup_timeout
+        , boost::program_options::value<validators::positive_integral<unsigned long>>()
+        ->required()
+        , "timeout in seconds for the virtual memory manager to connect and start up."
+        )
+        ;
+
+      return vmem;
+    }
+  }
+}
