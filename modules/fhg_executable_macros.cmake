977e817a11d3fddf5379011edad7d8e136547681
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-12-01 16:50:00 +0100

    cmake: use fhg_add_runtime_executable() for all bundled applications

--- modules/fhg_executable_macros.cmake
+++ modules/fhg_executable_macros.cmake
@@ -2,11 +2,15 @@ include(cmake_parse_arguments)
 include(car_cdr_macros)
 
 macro (FHG_ADD_RUNTIME_EXECUTABLE)
-  PARSE_ARGUMENTS(EXECUTABLE "LINK_LIBRARIES" "" ${ARGN})
+  PARSE_ARGUMENTS(EXECUTABLE "LINK_LIBRARIES;NAME" "" ${ARGN})
   CAR (EXECUTABLE_SOURCE ${EXECUTABLE_DEFAULT_ARGS})
   CDR (EXECUTABLE_ADDITIONAL_SOURCES ${EXECUTABLE_DEFAULT_ARGS})
 
-  string (REGEX REPLACE "(.*)\\.c.*" "\\1" exe_name ${EXECUTABLE_SOURCE})
+  if (EXECUTABLE_NAME)
+    set (exe_name ${EXECUTABLE_NAME})
+  else()
+    string (REGEX REPLACE "(.*)\\.c.*" "\\1" exe_name ${EXECUTABLE_SOURCE})
+  endif()
 
   add_executable (
     ${exe_name}
