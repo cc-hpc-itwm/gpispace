b9c2989e626fd2293c3f646d8ee6307d95607578
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-12-08 11:18:06 +0100

    util: use non-breaking space to separate number and unit to match SI style

--- util/fhg/util/boost/test/printer/chrono.hpp
+++ util/fhg/util/boost/test/printer/chrono.hpp
@@ -9,27 +9,27 @@
 
 FHG_BOOST_TEST_LOG_VALUE_PRINTER (std::chrono::nanoseconds, os, duration)
 {
-  os << FHG_BOOST_TEST_PRINT_LOG_VALUE_HELPER (duration.count()) << "ns";
+  os << FHG_BOOST_TEST_PRINT_LOG_VALUE_HELPER (duration.count()) << " ns";
 }
 FHG_BOOST_TEST_LOG_VALUE_PRINTER (std::chrono::microseconds, os, duration)
 {
-  os << FHG_BOOST_TEST_PRINT_LOG_VALUE_HELPER (duration.count()) << "µs";
+  os << FHG_BOOST_TEST_PRINT_LOG_VALUE_HELPER (duration.count()) << " µs";
 }
 FHG_BOOST_TEST_LOG_VALUE_PRINTER (std::chrono::milliseconds, os, duration)
 {
-  os << FHG_BOOST_TEST_PRINT_LOG_VALUE_HELPER (duration.count()) << "ms";
+  os << FHG_BOOST_TEST_PRINT_LOG_VALUE_HELPER (duration.count()) << " ms";
 }
 FHG_BOOST_TEST_LOG_VALUE_PRINTER (std::chrono::seconds, os, duration)
 {
-  os << FHG_BOOST_TEST_PRINT_LOG_VALUE_HELPER (duration.count()) << "s";
+  os << FHG_BOOST_TEST_PRINT_LOG_VALUE_HELPER (duration.count()) << " s";
 }
 FHG_BOOST_TEST_LOG_VALUE_PRINTER (std::chrono::minutes, os, duration)
 {
-  os << FHG_BOOST_TEST_PRINT_LOG_VALUE_HELPER (duration.count()) << "min";
+  os << FHG_BOOST_TEST_PRINT_LOG_VALUE_HELPER (duration.count()) << " min";
 }
 FHG_BOOST_TEST_LOG_VALUE_PRINTER (std::chrono::hours, os, duration)
 {
-  os << FHG_BOOST_TEST_PRINT_LOG_VALUE_HELPER (duration.count()) << "hr";
+  os << FHG_BOOST_TEST_PRINT_LOG_VALUE_HELPER (duration.count()) << " hr";
 }
 
 #endif
