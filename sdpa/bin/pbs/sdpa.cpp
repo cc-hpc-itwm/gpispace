0d2f3700269f08e402e8e5599f6e9150bf636e3d
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-12-23 15:36:57 +0100

    pbs: remove "sdpa submit": superceeded by drts::client

--- sdpa/bin/pbs/sdpa.cpp
+++ sdpa/bin/pbs/sdpa.cpp
@@ -3,101 +3,8 @@
 #include <drts/private/startup_and_shutdown.hpp>
 
 #include <fhg/revision.hpp>
-#include <fhg/util/read_file.hpp>
-#include <fhg/util/system_with_blocked_SIGCHLD.hpp>
 
-#include <fhgcom/peer_info.hpp>
-
-#include <sdpa/client.hpp>
-#include <sdpa/job_states.hpp>
-
-#include <boost/asio/io_service.hpp>
-#include <boost/format.hpp>
-
-#include <chrono>
-#include <fstream>
-
-namespace
-{
-  std::ostream &operator<< ( std::ostream &stream
-                           , std::chrono::system_clock::time_point const& tp
-                           )
-  {
-    std::time_t const tp_c (std::chrono::system_clock::to_time_t (tp));
-    char mbstr[100];
-    if (!std::strftime (mbstr, sizeof (mbstr), "%c", std::localtime (&tp_c)))
-    {
-      throw std::length_error ("strftime: out of space");
-    }
-    return stream << mbstr;
-  }
-}
-
-//! \todo unify with gspc::client
-void submit ( boost::filesystem::path const& state_dir
-            , boost::filesystem::path const& net
-            , boost::filesystem::path const& output
-            )
-{
-  fhg::com::host_t const orchestrator_host
-    (fhg::util::read_file (state_dir / "orchestrator.host"));
-  fhg::com::port_t const orchestrator_port
-    (fhg::util::read_file (state_dir / "orchestrator.port"));
-
-  std::cout << "I: submitting file: " << net << "\n";
-
-  boost::asio::io_service io_service;
-  sdpa::client::Client sdpac (orchestrator_host, orchestrator_port, io_service);
-
-  sdpa::job_id_t jobid (sdpac.submitJob (fhg::util::read_file (net)));
-  std::cout << "  + job (" << jobid << ")\n";
-
-  std::chrono::time_point<std::chrono::system_clock> const start
-    (std::chrono::system_clock::now());
-
-  std::cout << "starting at: " << start << "\n"
-            << "waiting for job to return..." << std::endl;
-
-  sdpa::client::job_info_t job_info;
-  sdpa::status::code const status
-    (sdpac.wait_for_terminal_state (jobid, job_info));
-
-  std::chrono::time_point<std::chrono::system_clock> const end
-    (std::chrono::system_clock::now());
-
-  std::cout << "stopped at: " << end << "\n"
-            << "execution time: "
-            << std::chrono::duration<float, std::chrono::seconds::period>
-                 (end - start).count()
-            << " s" << std::endl;
-
-  if (sdpa::status::FAILED == status)
-  {
-    std::cout << "failed: "
-              << "error-message := " << job_info.error_message
-              << std::endl;
-  }
-
-  std::cout << "    " << jobid << " -> " << sdpa::status::show (status) << "\n";
-
-  {
-    std::ofstream ofs (output.string());
-    ofs << sdpac.retrieveResults (jobid);
-  }
-  std::cout << "stored results in: " << output << "\n";
-
-  sdpac.deleteJob (jobid);
-  std::cout << "  - job (" << jobid << ")\n";
-
-  if (sdpa::status::FAILED == status)
-  {
-    throw std::runtime_error ("job failed");
-  }
-  else if (sdpa::status::CANCELED == status)
-  {
-    throw std::runtime_error ("job canceled");
-  }
-}
+#include <boost/filesystem.hpp>
 
 namespace
 {
@@ -132,7 +39,6 @@ int main (int argc, char** argv)
       ( "help\t\n"
         "version\t\n"
         "stop [vmem|orchestrator|agent|drts [host...]]\tstop component(s) on hosts\n"
-        "submit <file>\tsubmit a job"
       );
 
     if (argc < 2)
@@ -196,24 +102,6 @@ int main (int argc, char** argv)
       {
         fhg::drts::shutdown (state_dir_and_extra_args.first, boost::none, {});
       }
-    }
-    else if (command == "submit")
-    {
-      std::pair<boost::filesystem::path, std::vector<std::string>> const
-        state_dir_and_extra_args (get_state_dir_and_extra_args (argc - 2, argv + 2));
-      if (state_dir_and_extra_args.second.size() < 1)
-      {
-        throw std::invalid_argument ("missing argument: net");
-      }
-
-      submit ( state_dir_and_extra_args.first
-             , state_dir_and_extra_args.second[0] == "-"
-             ? "/dev/stdin"
-             : state_dir_and_extra_args.second[0]
-             , state_dir_and_extra_args.second.size() >= 2
-             ? state_dir_and_extra_args.second[1]
-             : "/dev/null"
-             );
     }
     else
     {
