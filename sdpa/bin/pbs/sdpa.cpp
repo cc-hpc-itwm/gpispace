ed43e979f1a767d2cdf2c42bbad55bd617dc20d9
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2014-12-23 15:40:14 +0100

    pbs: remove "sdpa stop": ~scoped_runtime_system does that, partial stop needs c api, just as partial startup does

--- sdpa/bin/pbs/sdpa.cpp
+++ sdpa/bin/pbs/sdpa.cpp
@@ -1,35 +1,10 @@
 // bernd.loerwald@itwm.fraunhofer.de
 
-#include <drts/private/startup_and_shutdown.hpp>
-
 #include <fhg/revision.hpp>
 
-#include <boost/filesystem.hpp>
-
-namespace
-{
-  std::vector<std::string> chop_first (std::vector<std::string> vec)
-  {
-    if (!vec.empty())
-    {
-      vec.erase (vec.begin());
-    }
-    return vec;
-  }
-
-  std::pair<boost::filesystem::path, std::vector<std::string>>
-    get_state_dir_and_extra_args (int argc, char** argv)
-  {
-    if (argc < 2 || argv[0] != std::string ("-s"))
-    {
-      throw std::invalid_argument ("missing state dir: -s <path>");
-    }
-    return std::pair<boost::filesystem::path, std::vector<std::string>>
-      ( boost::filesystem::canonical (argv[1])
-      , std::vector<std::string> (argv + 2, argv + argc)
-      );
-  }
-}
+#include <iostream>
+#include <stdexcept>
+#include <string>
 
 int main (int argc, char** argv)
 {
@@ -38,7 +13,6 @@ int main (int argc, char** argv)
     std::string const command_description
       ( "help\t\n"
         "version\t\n"
-        "stop [vmem|orchestrator|agent|drts [host...]]\tstop component(s) on hosts\n"
       );
 
     if (argc < 2)
@@ -57,51 +31,6 @@ int main (int argc, char** argv)
     else if (command == "version")
     {
       std::cout << fhg::project_info ("GPI-Space");
-    }
-    else if (command == "stop")
-    {
-      std::pair<boost::filesystem::path, std::vector<std::string>> const
-        state_dir_and_extra_args (get_state_dir_and_extra_args (argc - 2, argv + 2));
-      if (!state_dir_and_extra_args.second.empty())
-      {
-        if (state_dir_and_extra_args.second.front() == "vmem")
-        {
-          fhg::drts::shutdown ( state_dir_and_extra_args.first
-                              , fhg::drts::components_type::vmem
-                              , chop_first (state_dir_and_extra_args.second)
-                              );
-        }
-        else if (state_dir_and_extra_args.second.front() == "orchestrator")
-        {
-          fhg::drts::shutdown ( state_dir_and_extra_args.first
-                              , fhg::drts::components_type::orchestrator
-                              , chop_first (state_dir_and_extra_args.second)
-                              );
-        }
-        else if (state_dir_and_extra_args.second.front() == "agent")
-        {
-          fhg::drts::shutdown ( state_dir_and_extra_args.first
-                              , fhg::drts::components_type::agent
-                              , chop_first (state_dir_and_extra_args.second)
-                              );
-        }
-        else if (state_dir_and_extra_args.second.front() == "drts")
-        {
-          fhg::drts::shutdown ( state_dir_and_extra_args.first
-                              , fhg::drts::components_type::worker
-                              , chop_first (state_dir_and_extra_args.second)
-                              );
-        }
-        else
-        {
-          throw std::invalid_argument
-            ("bad component: " + state_dir_and_extra_args.second.front());
-        }
-      }
-      else
-      {
-        fhg::drts::shutdown (state_dir_and_extra_args.first, boost::none, {});
-      }
     }
     else
     {
