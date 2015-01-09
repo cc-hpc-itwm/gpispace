95fa57f346911da12339acf478de8dd58e1c0bf7
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2015-01-09 16:40:06 +0100

    we: loader: do not define never used macro DLL_LOCAL

--- src/we/loader/macros.hpp
+++ src/we/loader/macros.hpp
@@ -20,14 +20,11 @@
       #define DLL_PUBLIC __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
     #endif
   #endif
-  #define DLL_LOCAL
 #else
   #if __GNUC__ >= 4
     #define DLL_PUBLIC __attribute__ ((visibility ("default")))
-    #define DLL_LOCAL  __attribute__ ((visibility ("hidden")))
   #else
     #define DLL_PUBLIC
-    #define DLL_LOCAL
   #endif
 #endif
 
