e2912d345c734006802527d3472f4fd9be75ab37
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2015-01-12 21:26:39 +0100

    drts: scoped_allocation: store the size and let clients observe it

--- src/drts/private/scoped_allocation.hpp
+++ src/drts/private/scoped_allocation.hpp
@@ -58,6 +58,7 @@ namespace gspc
                     , gpi::pc::F_EXCLUSIVE
                     )
                   )
+        , _size (size)
     {}
 
     ~scoped_allocation()
@@ -70,10 +71,16 @@ namespace gspc
       return _handle;
     }
 
+    unsigned long size() const
+    {
+      return _size;
+    }
+
   private:
     std::unique_ptr<gpi::pc::client::api_t> const& _virtual_memory;
     scoped_segment const _scoped_segment;
     gpi::pc::type::handle_t const _handle;
+    unsigned long const _size;
   };
 }
 
