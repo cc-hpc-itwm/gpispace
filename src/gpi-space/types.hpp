5c678f40a12fb9dfba80933794409158af03fe5f
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2015-01-21 01:41:06 +0100

    gpi-space: remove unused typedef int network_type_t
    - followup to 1c6c093e00e004adf4576e8b0723de440a3be69c

--- src/gpi-space/types.hpp
+++ src/gpi-space/types.hpp
@@ -13,7 +13,6 @@ namespace gpi
   typedef uint64_t size_t;
   typedef float    version_t;
   typedef unsigned short port_t;
-  typedef int      network_type_t;
   typedef boost::dynamic_bitset<> error_vector_t;
 }
 
