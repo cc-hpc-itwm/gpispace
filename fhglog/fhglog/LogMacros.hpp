8eebc00b7c37b1296dd696f466ce2126ba2c9838
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2015-01-22 23:54:33 +0100

    fhglog: FHGLOG_SETUP: remove suppress_unused_variable_warning, there is no such warning and never will be as the object has a non trivial dtor and thus is used

--- fhglog/fhglog/LogMacros.hpp
+++ fhglog/fhglog/LogMacros.hpp
@@ -30,12 +30,8 @@ namespace fhg
       }
     };
 
-    template <typename T>
-    void suppress_unused_variable_warning (T) {}
-
 #define FHGLOG_SETUP(remote_log_io_service)                             \
     ::fhg::log::flush_at_end_of_scope_t _1e872d1a_6dcf_11e3_9e3b_13f239ecd3ca; \
-    ::fhg::log::suppress_unused_variable_warning (_1e872d1a_6dcf_11e3_9e3b_13f239ecd3ca); \
     ::fhg::log::configure (remote_log_io_service)
 
 #define __LOG(logger, level, msg)                               \
