4519b247be94551470b14c65d4810c75da94c55b
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2015-02-16 17:26:47 +0100

    vmem: pc: inline process_t into manager_t to resolve the thread-end-race
    * now explicitly detaches the thread and removes ownership and
      _immediately_ terminates, with a note, but only if manager_t is not
      m_stopping. then, ~manager_t will join.
    * process_t boiled down to a thread

--- src/gpi-space/pc/container/manager.hpp
+++ src/gpi-space/pc/container/manager.hpp
@@ -1,22 +1,19 @@
 #ifndef GPI_SPACE_PC_CONTAINER_MANAGER_HPP
 #define GPI_SPACE_PC_CONTAINER_MANAGER_HPP 1
 
-#include <boost/thread.hpp>
-
-#include <gpi-space/pc/type/counter.hpp>
-#include <gpi-space/pc/container/process.hpp>
-#include <gpi-space/pc/memory/manager.hpp>
 #include <gpi-space/pc/global/topology.hpp>
+#include <gpi-space/pc/memory/manager.hpp>
+#include <gpi-space/pc/type/counter.hpp>
+#include <gpi-space/pc/type/typedefs.hpp>
+
+#include <boost/noncopyable.hpp>
 
 #include <map>
 #include <memory>
+#include <mutex>
 #include <string>
-
-#include <boost/shared_ptr.hpp>
-#include <boost/thread.hpp>
-#include <boost/noncopyable.hpp>
-
-#include <gpi-space/pc/type/typedefs.hpp>
+#include <thread>
+#include <vector>
 
 namespace gpi
 {
@@ -36,33 +33,25 @@ namespace gpi
         ~manager_t ();
 
       private:
-        typedef boost::shared_ptr<boost::thread> thread_t;
-
         void listener_thread_main();
+        void process_communication_thread (gpi::pc::type::process_id_t, int socket);
 
         void close_socket (const int fd);
         int safe_unlink(std::string const & path);
 
         std::string m_path;
-        thread_t m_listener;
         int m_socket;
         bool m_stopping;
 
         gpi::pc::type::counter_t m_process_counter;
-        mutable boost::mutex _mutex_processes;
-        std::map<gpi::pc::type::process_id_t, std::unique_ptr<process_t>>
-          m_processes;
-
-        void handle_process_error ( const gpi::pc::type::process_id_t proc_id
-                                  , int error
-                                  );
-        void detach_process ( const gpi::pc::type::process_id_t
-                            , bool called_from_dtor = false
-                            );
+        mutable std::mutex _mutex_processes;
+        std::map<gpi::pc::type::process_id_t, std::thread> m_processes;
 
         gpi::api::gpi_api_t& _gpi_api;
         memory::manager_t _memory_manager;
         global::topology_t _topology;
+
+        std::thread _listener_thread;
       };
     }
   }
