97ec48921b50133a8220e7213f133824f9564cc4
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2015-02-17 16:05:11 +0100

    util: remove shared library (was installed, static library still installed)

--- modules/fhg_test_macros.cmake
+++ modules/fhg_test_macros.cmake
@@ -69,7 +69,6 @@ set (FILES_REQUIRED_IN_INSTALLATION
   "${CMAKE_INSTALL_PREFIX}/include/we/type/value/unwrap.hpp"
   "${CMAKE_INSTALL_PREFIX}/include/we/type/value/wrap.hpp"
   "${CMAKE_INSTALL_PREFIX}/lib/libfhg-util.a"
-  "${CMAKE_INSTALL_PREFIX}/lib/libfhg-util.so.1"
   "${CMAKE_INSTALL_PREFIX}/lib/libfhglog.a"
   "${CMAKE_INSTALL_PREFIX}/lib/libfhglog.so.2"
   "${CMAKE_INSTALL_PREFIX}/lib/libgspc.so"
