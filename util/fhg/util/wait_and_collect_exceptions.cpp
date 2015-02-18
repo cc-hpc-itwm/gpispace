09f947a24279f4d2aef80fb08c79fcb43281b4f9
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2015-02-18 12:45:50 +0100

    util: express wait_and_collect_exceptions with the more general apply_for_each_and_collect_exceptions

--- util/fhg/util/wait_and_collect_exceptions.cpp
+++ util/fhg/util/wait_and_collect_exceptions.cpp
@@ -1,38 +1,35 @@
 #include <fhg/util/wait_and_collect_exceptions.hpp>
 
-#include <fhg/util/join.hpp>
 #include <fhg/util/print_exception.hpp>
 
+#include <functional>
 #include <sstream>
-#include <stdexcept>
 #include <string>
 
 namespace fhg
 {
   namespace util
   {
+    void throw_collected_exceptions
+      (std::vector<std::exception_ptr> const& exceptions)
+    {
+      if (exceptions.empty())
+      {
+        return;
+      }
+
+      std::ostringstream oss;
+      for (std::exception_ptr const& exception : exceptions)
+      {
+        fhg::util::print_exception (oss, "", exception);
+      }
+      throw std::runtime_error (oss.str());
+    }
+
     void wait_and_collect_exceptions (std::vector<std::future<void>>& futures)
     {
-      std::vector<std::string> accumulated_whats;
-
-      for (std::future<void>& future : futures)
-      {
-        try
-        {
-          future.get();
-        }
-        catch (...)
-        {
-          std::ostringstream oss;
-          fhg::util::print_current_exception (oss, "");
-          accumulated_whats.emplace_back (oss.str());
-        }
-      }
-
-      if (!accumulated_whats.empty())
-      {
-        throw std::runtime_error (fhg::util::join (accumulated_whats, ", "));
-      }
+      apply_for_each_and_collect_exceptions
+        (futures, std::bind (&std::future<void>::get, std::placeholders::_1));
     }
   }
 }
--- util/fhg/util/wait_and_collect_exceptions.hpp
+++ util/fhg/util/wait_and_collect_exceptions.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <exception>
 #include <future>
 #include <vector>
 
@@ -7,6 +8,29 @@ namespace fhg
 {
   namespace util
   {
+    void throw_collected_exceptions (std::vector<std::exception_ptr> const&);
+
+    template<typename Collection, typename Operation>
+      void apply_for_each_and_collect_exceptions
+        (Collection&& collection, Operation&& operation)
+    {
+      std::vector<std::exception_ptr> exceptions;
+
+      for (auto& element : collection)
+      {
+        try
+        {
+          operation (element);
+        }
+        catch (...)
+        {
+          exceptions.push_back (std::current_exception());
+        }
+      }
+
+      throw_collected_exceptions (exceptions);
+    }
+
     void wait_and_collect_exceptions (std::vector<std::future<void>>&);
   }
 }
