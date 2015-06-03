86e8d15ee9a2875465871dad6e7ef5de99f408c1
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2015-06-03 11:34:16 +0200

    drts: workflow_response: use simple_client of rpc
    src/rpc:
      a6a6d07 server_with_multiple_clients: fix: don't delete connections while inside
              callback of connection
      117f7fb cmake: let library be position independent
      9807ae0 test: remove unused include
      cc1c02d test: explicitly default copy-ctors: defaulting deprecated with explicit
              dtor
      fe3e07c test: mark throw_as_nested implementations as noreturn
      f81844d common: avoid shadowing
      f6296b1 use reinterpret_cast to be more verbose about hackery
      3266081 test: only use function_description overloads
      624294b simple_client: remove sync() and async() functions but expose
              remote_endpoint
      a180e99 add simple_client and use simple_client in test/basic
      846184e test: remove helper/server_for_dispatcher and use
              server_with_multiple_clients instead

--- src/drts/workflow_response.cpp
+++ src/drts/workflow_response.cpp
@@ -2,7 +2,7 @@
 
 #include <drts/workflow_response.hpp>
 
-#include <rpc/client.hpp>
+#include <rpc/simple_client.hpp>
 
 #include <we/type/value/serialize.hpp>
 
@@ -15,33 +15,8 @@ namespace gspc
                          , pnet::type::value::value_type const& value
                          )
   {
-    struct io_service_with_work_thread_and_stop_on_scope_exit
-    {
-      io_service_with_work_thread_and_stop_on_scope_exit()
-        : service()
-        , _work (service)
-        , _thread ([this] { service.run(); })
-      {}
-      ~io_service_with_work_thread_and_stop_on_scope_exit()
-      {
-        service.stop();
-      }
-      boost::asio::io_service service;
-
-    private:
-      boost::asio::io_service::work const _work;
-      boost::strict_scoped_thread<boost::interrupt_and_join_if_joinable> const
-        _thread;
-    } io_service_client;
-
-    fhg::rpc::remote_endpoint endpoint
-      ( io_service_client.service
-      , trigger_address
-      , trigger_port
-      , fhg::util::serialization::exception::serialization_functions()
-      );
-
+    fhg::rpc::simple_client remote_client (trigger_address, trigger_port);
     fhg::rpc::sync_remote_function<void (pnet::type::value::value_type)>
-      (endpoint, "set_result") (value);
+      (remote_client, "set_result") (value);
   }
 }
