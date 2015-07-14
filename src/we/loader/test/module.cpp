dfd56649125f3ca50312449507c394003511d773
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2015-07-14 14:53:22 +0200

    fixup adopt to shared/util-generic!15: new-style require_exception
    missing test cases
    
    src/util-generic
     6fe2b9d cxx17: void_t: use a gcc compatible implementation
     a58fff0 asynchronous: fix: use enable_if and is_callable instead of
             specific std::function<>
    
    src/network
     dc80270 adopt to shared/util-generic!15: new-style require_exception
     2ef571c fix issue 2: connect before running the io_service
    
    src/rpc
     91196ae test: adopt locked_with_info_file to shared/util-generic!15 aswell

--- src/we/loader/test/module.cpp
+++ src/we/loader/test/module.cpp
@@ -45,14 +45,16 @@ BOOST_AUTO_TEST_CASE (ctor_failed_bad_boost_version)
 {
 #define XSTR(x) STR(x)
 #define STR(x) #x
-  fhg::util::testing::require_exception<std::runtime_error>
+  fhg::util::testing::require_exception
     ( [] { we::loader::Module ("./libempty_not_linked_with_pnet.so"); }
-    , ( boost::format
-        ( "could not load module './libempty_not_linked_with_pnet.so':"
-          " ./libempty_not_linked_with_pnet.so: undefined symbol: %1%"
+    , std::runtime_error
+        ( ( boost::format
+              ( "could not load module './libempty_not_linked_with_pnet.so':"
+                " ./libempty_not_linked_with_pnet.so: undefined symbol: %1%"
+              )
+          % XSTR (WE_GUARD_SYMBOL)
+          ).str()
         )
-      % XSTR (WE_GUARD_SYMBOL)
-      ).str()
     );
 #undef STR
 #undef XSTR
