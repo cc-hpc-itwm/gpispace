1bc955f794e81adb2e2170ca43db4eee7b5e82dd
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2015-07-24 06:15:24 +0200

    remove doc/user
    - is outdated
    - was never complete
    - was never installed
    - requires asciidoc and source-highlight

--- modules/asciidoc.cmake
+++
@@ -1,8 +0,0 @@
-set(ENV{PATH} "${PATH}")
-execute_process (
-     COMMAND ${ASCIIDOC} -b ${BACKEND} -o ${OUTPUT} ${INPUT}
-     RESULT_VARIABLE __res
-)
-if (NOT ${__res} EQUAL 0)
-  file (REMOVE_RECURSE "${OUTPUT}")
-endif ()
