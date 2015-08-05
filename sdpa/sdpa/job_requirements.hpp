b44802fedee6e1f4e0423d01d08423044fa1244c
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2015-08-05 16:04:14 +0200

    job_requirements: remove type information from variable name

--- sdpa/sdpa/job_requirements.hpp
+++ sdpa/sdpa/job_requirements.hpp
@@ -13,13 +13,13 @@ class job_requirements_t
 public:
   job_requirements_t() = delete;
 
-  job_requirements_t ( const std::list<we::type::requirement_t>& r_list
+  job_requirements_t ( const std::list<we::type::requirement_t>& requirements
                      , const we::type::schedule_data& schedule_data
                      , std::function<double (std::string const&)> transfer_cost
                      , double estimated_computational_cost
                      , unsigned long shared_memory_amount_required
                      )
-    : _requirementList (r_list)
+    : _requirements (requirements)
     , _scheduleData (schedule_data)
     , _transfer_cost (transfer_cost)
     , _estimated_computational_cost (estimated_computational_cost)
@@ -27,12 +27,12 @@ public:
   {}
 
   unsigned long numWorkers() const {return _scheduleData.num_worker().get_value_or(1);}
-  const std::list<we::type::requirement_t>& getReqList() const {return _requirementList;}
+  const std::list<we::type::requirement_t>& getReqList() const {return _requirements;}
   const std::function<double (std::string const&)> transfer_cost() const {return _transfer_cost;}
   double computational_cost() const {return _estimated_computational_cost;}
   unsigned long shared_memory_amount_required() const {return _shared_memory_amount_required;}
 private:
-  std::list<we::type::requirement_t> _requirementList;
+  std::list<we::type::requirement_t> _requirements;
   we::type::schedule_data _scheduleData;
   std::function<double (std::string const&)> _transfer_cost;
   double _estimated_computational_cost;
