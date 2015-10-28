efa51174dea54192aad83110e9f05cd8f5a5c8a3
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2015-10-28 15:28:41 +0100

    vmem: simplify transfer_queue

--- src/gpi-space/pc/memory/transfer_queue.cpp
+++ src/gpi-space/pc/memory/transfer_queue.cpp
@@ -1,7 +1,5 @@
 #include <gpi-space/pc/memory/transfer_queue.hpp>
 
-#include <boost/make_shared.hpp>
-
 namespace gpi
 {
   namespace pc
@@ -9,8 +7,7 @@ namespace gpi
     namespace memory
     {
       transfer_queue_t::transfer_queue_t()
-        : m_enabled (true)
-        , m_thread (&transfer_queue_t::worker, this)
+        : m_thread (&transfer_queue_t::worker, this)
       {}
 
       void
@@ -25,73 +22,7 @@ namespace gpi
       void
       transfer_queue_t::enqueue (const task_ptr &task)
       {
-        task_list_t tasks;
-        tasks.push_back (task);
-        enqueue (tasks);
-      }
-
-      void
-      transfer_queue_t::enqueue (const task_list_t & tasks)
-      {
-        if (is_disabled ())
-        {
-          throw std::runtime_error
-            ("queue permanently disabled due to previous errors");
-        }
-
-        for (task_ptr const& task : tasks)
-        {
-            m_task_queue.put (task);
-        }
-
-        // this  needs to  be atomic,  otherwise (enqueue();  wait();)  would be
-        // broken
-        {
-          boost::mutex::scoped_lock const _ (_mutex_dispatched);
-          m_dispatched.insert (tasks.begin(), tasks.end());
-        }
-      }
-
-      void
-      transfer_queue_t::disable ()
-      {
-        boost::mutex::scoped_lock const _ (m_mutex);
-        m_enabled = false;
-      }
-
-      bool
-      transfer_queue_t::is_disabled () const
-      {
-        return not m_enabled;
-      }
-
-      std::size_t
-      transfer_queue_t::wait ()
-      {
-        task_set_t wait_on_tasks;
-        {
-          boost::mutex::scoped_lock const _ (_mutex_dispatched);
-          m_dispatched.swap (wait_on_tasks);
-        }
-
-        std::size_t const res (wait_on_tasks.size());
-        while (! wait_on_tasks.empty())
-        {
-          task_ptr task(*wait_on_tasks.begin());
-          wait_on_tasks.erase (wait_on_tasks.begin());
-          task->wait();
-
-          // TODO: WORK HERE:  this failure should be propagated  to the correct
-          // process container
-          if (task->has_failed())
-          {
-            throw std::runtime_error
-              ( "task failed: " + task->get_name ()
-              + ": " + task->get_error_message ()
-              );
-          }
-        }
-        return res;
+        m_task_queue.put (task);
       }
     }
   }
