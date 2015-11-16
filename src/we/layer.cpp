8739e5f115a6724161168fc077f08f220118b4b6
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2015-11-16 16:51:22 +0100

    layer: hackfix: regard incoming finished/failed after a cancel/failed as canceled
    * this forgets about the actual termination reason per child after the
      first, which is just as before, but at least no longer hangs, somewhat
      related to #492
    * fixes #609

--- src/we/layer.cpp
+++ src/we/layer.cpp
@@ -180,6 +180,14 @@ namespace we
       boost::optional<id_type> const parent (_running_jobs.parent (id));
       fhg_assert (parent);
 
+      //! \todo Don't forget that this child actually finished and
+      //! inject result.
+      if (_finalize_job_cancellation.count (*parent))
+      {
+        canceled (id);
+        return;
+      }
+
       _nets_to_extract_from.apply
         ( *parent
         , [this, parent, result, id] (activity_data_type& activity_data)
@@ -230,6 +238,14 @@ namespace we
     {
       boost::optional<id_type> const parent (_running_jobs.parent (id));
       fhg_assert (parent);
+
+      //! \todo Don't forget that this child actually failed and
+      //! store reason.
+      if (_finalize_job_cancellation.count (*parent))
+      {
+        canceled (id);
+        return;
+      }
 
       _nets_to_extract_from.remove_and_apply
         ( *parent
