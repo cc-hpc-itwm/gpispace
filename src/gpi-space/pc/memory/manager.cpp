cc-hpc-itwm/gpispace

commit 477e5e8837943e63391e8a2fa87d2a4e9deb3a14

vmem: move gaspi_t ownership into gpi_area
* no longer has meaningful timeout for segment alloc specified by user
* horribly breaks when there are multiple gpi_areas (did before, but
  probably just implicitly overwriting everything rather than hopefully
  now failing segment creation)

src/gpi-space/pc/memory/manager.cpp
@@ -6,7 +6,6 @@
 
 #include <util-generic/finally.hpp>
 
-#include <gpi-space/gpi/gaspi.hpp>
 #include <gpi-space/pc/global/topology.hpp>
 #include <gpi-space/pc/memory/gpi_area.hpp>
 #include <gpi-space/pc/memory/handle_generator.hpp>
@@ -29,14 +28,13 @@ namespace gpi
                                , std::string const &url_s
                                , global::topology_t& topology
                                , handle_generator_t& handle_generator
-                               , api::gaspi_t& gaspi
                                , fhg::vmem::gaspi_context& gaspi_context
                                )
       {
         url_t url (url_s);
 
         return
-          ( url.type() == "gpi" ? gpi_area_t::create (logger, url_s, topology, handle_generator, gaspi, gaspi_context)
+          ( url.type() == "gpi" ? gpi_area_t::create (logger, url_s, topology, handle_generator, gaspi_context)
           : url.type() == "sfs" ? sfs_area_t::create (logger, url_s, topology, handle_generator)
           : throw std::runtime_error
               ("no memory type registered with: '" + url_s + "'")
@@ -45,11 +43,9 @@ namespace gpi
       }
 
       manager_t::manager_t ( fhg::log::Logger& logger
-                           , api::gaspi_t& gaspi
                            , fhg::vmem::gaspi_context& gaspi_context
                            )
         : _logger (logger)
-        , _gaspi (gaspi)
         , _gaspi_context (gaspi_context)
         , _next_memcpy_id (0)
         , _handle_generator (gaspi_context.rank())
@@ -483,7 +479,7 @@ namespace gpi
                                    , global::topology_t& topology
                                    )
       {
-        area_ptr_t area (create_area (_logger, url, topology, _handle_generator, _gaspi, _gaspi_context));
+        area_ptr_t area (create_area (_logger, url, topology, _handle_generator, _gaspi_context));
         area->set_owner (0);
         area->set_id (seg_id);
         add_area (area);
@@ -497,7 +493,7 @@ namespace gpi
                             , global::topology_t& topology
                             )
       {
-        area_ptr_t area (create_area (_logger, url_s, topology, _handle_generator, _gaspi, _gaspi_context));
+        area_ptr_t area (create_area (_logger, url_s, topology, _handle_generator, _gaspi_context));
         area->set_owner (proc_id);
         if (seg_id > 0)
           area->set_id (seg_id);

src/gpi-space/pc/container/manager.cpp
@@ -11,7 +11,6 @@
 
 #include <fhglog/LogMacros.hpp>
 
-#include <gpi-space/gpi/gaspi.hpp>
 #include <gpi-space/pc/memory/shm_area.hpp>
 #include <gpi-space/pc/proto/message.hpp>
 #include <gpi-space/pc/url.hpp>
@@ -428,7 +427,6 @@ namespace gpi
       manager_t::manager_t ( fhg::log::Logger& logger
                            , std::string const & p
                            , std::vector<std::string> const& default_memory_urls
-                           , api::gaspi_t& gaspi
                            , fhg::vmem::gaspi_context& gaspi_context
                            , std::unique_ptr<fhg::rpc::server_with_multiple_clients_and_deferred_dispatcher> topology_rpc_server
                            )
@@ -437,7 +435,7 @@ namespace gpi
         , m_socket (-1)
         , m_stopping (false)
         , m_process_counter (0)
-        , _memory_manager (_logger, gaspi, gaspi_context)
+        , _memory_manager (_logger, gaspi_context)
         , _topology ( _memory_manager
                     , gaspi_context
                     , std::move (topology_rpc_server)

src/bin/gpi-space.cpp
@@ -191,11 +191,6 @@ int main (int argc, char** argv)
       , port
       , topology_rpc_server->local_endpoint().port()
       );
-    gpi::api::gaspi_t gaspi ( gaspi_context
-                            , logger
-                            , gpi_mem
-                            , initialization_timeout
-                            );
 
     // other url examples are:
     //      gpi://?buffers=8&buffer_size=4194304 GPI memory
@@ -203,8 +198,7 @@ int main (int argc, char** argv)
     const gpi::pc::container::manager_t container_manager
       ( logger
       , socket_path.string()
-      , {"gpi://?buffer_size=4194304&buffers=8"}
-      , gaspi
+      , {"gpi://?memory_size=" + std::to_string (gpi_mem) + "&buffer_size=4194304&buffers=8"}
       , gaspi_context
       , std::move (topology_rpc_server)
       );

src/gpi-space/pc/container/manager.hpp
@@ -28,7 +28,6 @@ namespace gpi
         manager_t ( fhg::log::Logger&
                   , std::string const & p
                   , std::vector<std::string> const& default_memory_urls
-                  , api::gaspi_t&
                   , fhg::vmem::gaspi_context&
                   , std::unique_ptr<fhg::rpc::server_with_multiple_clients_and_deferred_dispatcher> topology_rpc_server
                   );

src/gpi-space/pc/memory/CMakeLists.txt
@@ -15,4 +15,5 @@ target_link_libraries (gpi-space-pc-memory
   mmgr
   gpi-space_url
   Util::Generic
+  gpi-space-gpiapi
   )

src/gpi-space/pc/memory/gpi_area.cpp
@@ -29,23 +29,24 @@ namespace gpi
                              , const gpi::pc::type::flags_t flags
                              , gpi::pc::global::itopology_t & topology
                              , handle_generator_t& handle_generator
-                             , api::gaspi_t& gaspi
                              , fhg::vmem::gaspi_context& gaspi_context
+                             , fhg::vmem::gaspi_timeout& time_left
+                             , type::size_t memory_size
                              )
         : area_t ( logger
                  , gpi_area_t::area_type
                  , creator
                  , name
-                 , gaspi.memory_size()
+                 , memory_size
                  , flags
                  , handle_generator
                  )
-        , m_ptr (gaspi.dma_ptr())
+        , _gaspi_context (gaspi_context)
+        , _gaspi (_gaspi_context, logger, memory_size, time_left)
+        , m_ptr (_gaspi.dma_ptr())
         , m_num_com_buffers (8)
         , m_com_buffer_size (4* (1<<20))
         , _topology (topology)
-        , _gaspi (gaspi)
-        , _gaspi_context (gaspi_context)
       {}
 
       void gpi_area_t::init ()
@@ -531,7 +532,6 @@ namespace gpi
         , std::string const &url_s
         , gpi::pc::global::itopology_t & topology
         , handle_generator_t& handle_generator
-        , api::gaspi_t& gaspi
         , fhg::vmem::gaspi_context& gaspi_context
         )
       {
@@ -541,7 +541,11 @@ namespace gpi
           boost::lexical_cast<type::size_t>(url.get ("buffer_size").get_value_or ("4194304"));
         type::size_t numbuf =
           boost::lexical_cast<type::size_t>(url.get ("buffers").get_value_or ("8"));
+        type::size_t const memory_size
+          (boost::lexical_cast<type::size_t> (url.get ("memory_size").get()));
 
+        //! \todo get from user? use for other areas as well? remove?
+        fhg::vmem::gaspi_timeout time_left (std::chrono::seconds (30));
         gpi_area_t * area = new gpi_area_t ( logger
                                            , GPI_PC_INVAL
                                            , "GPI"
@@ -549,8 +553,9 @@ namespace gpi
                                            + gpi::pc::F_GLOBAL
                                            , topology
                                            , handle_generator
-                                           , gaspi
                                            , gaspi_context
+                                           , time_left
+                                           , memory_size
                                            );
         area->m_num_com_buffers = numbuf;
         area->m_com_buffer_size = comsize;

src/gpi-space/pc/memory/gpi_area.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <gpi-space/gpi/gaspi.hpp>
+
 #include <gpi-space/pc/type/segment_type.hpp>
 #include <gpi-space/pc/memory/memory_area.hpp>
 #include <gpi-space/pc/memory/handle_buffer.hpp>
@@ -31,7 +33,6 @@ namespace gpi
                                  , std::string const &url
                                  , gpi::pc::global::itopology_t & topology
                                  , handle_generator_t&
-                                 , api::gaspi_t& gaspi
                                  , fhg::vmem::gaspi_context&
                                  );
 
@@ -42,8 +43,9 @@ namespace gpi
                    , const gpi::pc::type::flags_t flags
                    , gpi::pc::global::itopology_t & topology
                    , handle_generator_t&
-                   , api::gaspi_t&
                    , fhg::vmem::gaspi_context&
+                   , fhg::vmem::gaspi_timeout&
+                   , type::size_t memory_size
                    );
 
         virtual bool is_allowed_to_attach (const gpi::pc::type::process_id_t) const override;
@@ -96,6 +98,9 @@ namespace gpi
                                   , const gpi::rank_t
                                   ) const override;
 
+        fhg::vmem::gaspi_context& _gaspi_context;
+        api::gaspi_t _gaspi;
+
         void * m_ptr;
 
         handle_pool_t m_com_handles; // local allocations that can be used to transfer data
@@ -104,9 +109,6 @@ namespace gpi
         gpi::pc::type::size_t m_com_buffer_size;
 
         gpi::pc::global::itopology_t & _topology;
-
-        api::gaspi_t& _gaspi;
-        fhg::vmem::gaspi_context& _gaspi_context;
       };
     }
   }

src/gpi-space/pc/memory/manager.hpp
@@ -7,12 +7,13 @@
 
 #include <fhglog/Logger.hpp>
 
-#include <gpi-space/gpi/gaspi.hpp>
 #include <gpi-space/pc/type/typedefs.hpp>
 #include <gpi-space/pc/memory/memory_area.hpp>
 #include <gpi-space/pc/memory/memory_buffer.hpp>
 
 #include <fhg/util/thread/queue.hpp>
+
+#include <vmem/gaspi_context.hpp>
 
 #include <boost/thread/scoped_thread.hpp>
 
@@ -38,7 +39,7 @@ namespace gpi
 
         static const gpi::pc::type::segment_id_t MAX_PREALLOCATED_SEGMENT_ID=16;
 
-        manager_t (fhg::log::Logger&, api::gaspi_t&, fhg::vmem::gaspi_context&);
+        manager_t (fhg::log::Logger&, fhg::vmem::gaspi_context&);
         ~manager_t ();
 
         void clear ();
@@ -140,7 +141,6 @@ namespace gpi
         mutable mutex_type m_mutex;
         area_map_t m_areas;
         handle_to_segment_t m_handle_to_segment;
-        api::gaspi_t& _gaspi;
         fhg::vmem::gaspi_context& _gaspi_context;
 
         std::mutex _memcpy_task_guard;

commit d4386a0492519b5d2cdce2d59b94a5b3f71f9b80

vmem: handle initialization of segments different based on type
also mark the current state as BROKEN. this has been broken since
477e5e88379 where gaspi segment initialization no longer happened
simultaneously at boot-time but only when adding memory.

for sfs, master creates file that slaves need to open determining
filesize from the opened file. thus, to avoid a race, the master is
doing this before all others. gaspi on the other side needs simultaneous
initialization for gaspi_segment_create etc.

src/gpi-space/pc/memory/manager.cpp
@@ -485,35 +485,51 @@ namespace gpi
                             , global::topology_t& topology
                             )
       {
-        area_ptr_t area (create_area (_logger, url_s, topology, _handle_generator, _gaspi_context));
-        area->set_owner (proc_id);
-        area->set_id
-          (_handle_generator.next (gpi::pc::type::segment::SEG_INVAL));
+        //! \note for sfs, master creates file that slaves need to
+        //! open determining filesize from the opened file. thus, to
+        //! avoid a race, the master is doing this before all
+        //! others. gaspi on the other side needs simultaneous
+        //! initialization for gaspi_segment_create etc.
+        bool const require_earlier_master_initialization
+          (url_t (url_s).type() == "sfs");
 
-        add_area (area);
+        area_ptr_t area (nullptr);
 
-        if (area->flags () & F_GLOBAL)
+        //! \todo BROKEN: simultaneous initialization not implemented,
+        //! thus falling back to earlier one as a hack, which is wrong
+        //! and lets gaspi segment creation time out!
+        if (true || require_earlier_master_initialization)
         {
-          try
-          {
-            url_t old_url (url_s);
-            url_t new_url (old_url.type(), old_url.path());
-            new_url.set ("persistent", "true");
-            topology.add_memory
-              (area->get_id (), boost::lexical_cast<std::string>(new_url));
-          }
-          catch (std::exception const & up)
+          area = create_area (_logger, url_s, topology, _handle_generator, _gaspi_context);
+          area->set_owner (proc_id);
+          area->set_id
+            (_handle_generator.next (gpi::pc::type::segment::SEG_INVAL));
+
+          add_area (area);
+
+          if (area->flags () & F_GLOBAL)
           {
             try
             {
-              del_memory (proc_id, area->get_id (), topology);
+              url_t old_url (url_s);
+              url_t new_url (old_url.type(), old_url.path());
+              new_url.set ("persistent", "true");
+              topology.add_memory
+                (area->get_id (), boost::lexical_cast<std::string>(new_url));
             }
-            catch (...)
+            catch (std::exception const & up)
             {
-              // ignore follow up exception
-            }
+              try
+              {
+                del_memory (proc_id, area->get_id (), topology);
+              }
+              catch (...)
+              {
+                // ignore follow up exception
+              }
 
-            throw;
+              throw;
+            }
           }
         }
 