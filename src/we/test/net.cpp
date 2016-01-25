42743ba8c76ac15c420713ee1ac125b1d60113d8
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2016-01-25 17:29:53 +0100

    we: test: net: add xml/example/capacity.xpnet
    - the quintessence is that a transition can fire even if it depends on
      its own output

--- src/we/test/net.cpp
+++ src/we/test/net.cpp
@@ -81,3 +81,84 @@ BOOST_AUTO_TEST_CASE (deserialized_transition_without_input_port_can_not_fire)
         (random_engine(), unexpected_workflow_response)
     );
 }
+
+BOOST_AUTO_TEST_CASE (transition_that_depends_on_own_output_can_fire)
+{
+  pnet::type::signature::signature_type const signature
+    (std::string ("control"));
+  we::type::property::type const no_properties;
+
+  we::type::net_type net;
+
+  auto&& add_place
+    ([&]() -> we::place_id_type
+     {
+       static int place {0};
+
+       return net.add_place
+         (place::type ( std::to_string (++place)
+                      , signature
+                      , boost::none
+                      , no_properties
+                      )
+         );
+     }
+    );
+
+  we::place_id_type const place_in (add_place());
+  we::place_id_type const place_out (add_place());
+  we::place_id_type const place_credit (add_place());
+
+  net.put_value (place_in, we::type::literal::control());
+  net.put_value (place_credit, we::type::literal::control());
+
+  we::type::transition_t transition
+    ( fhg::util::testing::random_identifier()
+    , we::type::expression_t ("${out} := ${in}")
+    , boost::none
+    , no_properties
+    , we::priority_type()
+    );
+
+  auto&& add_port
+    ([&] ( std::string const& name
+         , we::type::PortDirection direction
+         ) -> we::port_id_type
+     {
+       return transition.add_port
+         (we::type::port_t (name, direction, signature, no_properties));
+     }
+    );
+
+  we::port_id_type const port_in (add_port ("in", we::type::PORT_IN));
+  we::port_id_type const port_out (add_port ("out", we::type::PORT_OUT));
+  we::port_id_type const port_credit_in (add_port ("c", we::type::PORT_IN));
+  we::port_id_type const port_credit_out (add_port ("c", we::type::PORT_OUT));
+
+  we::transition_id_type const transition_id (net.add_transition (transition));
+
+  auto&& connect
+    ([&] (we::edge::type edge, we::place_id_type place, we::port_id_type port)
+     {
+       net.add_connection (edge, transition_id, place, port, no_properties);
+     }
+    );
+
+  connect (we::edge::PT, place_in, port_in);
+  connect (we::edge::TP, place_out, port_out);
+  connect (we::edge::PT, place_credit, port_credit_in);
+  connect (we::edge::TP, place_credit, port_credit_out);
+
+  BOOST_REQUIRE_EQUAL (net.get_token (place_in).size(), 1);
+  BOOST_REQUIRE (net.get_token (place_out).empty());
+  BOOST_REQUIRE_EQUAL (net.get_token (place_credit).size(), 1);
+
+  BOOST_REQUIRE
+    ( !net.fire_expressions_and_extract_activity_random
+        (random_engine(), unexpected_workflow_response)
+    );
+
+  BOOST_REQUIRE (net.get_token (place_in).empty());
+  BOOST_REQUIRE_EQUAL (net.get_token (place_out).size(), 1);
+  BOOST_REQUIRE_EQUAL (net.get_token (place_credit).size(), 1);
+}
