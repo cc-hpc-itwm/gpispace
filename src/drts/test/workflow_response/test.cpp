7995e6dc955f0f1d281001714acedebb05c545aa
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2016-03-24 16:21:46 +0100

    drts: test: add workflow response fails if workflow fails after requesting
    closes issue #541

--- src/drts/test/workflow_response/test.cpp
+++ src/drts/test/workflow_response/test.cpp
@@ -453,3 +453,114 @@ BOOST_AUTO_TEST_CASE (one_response_waits_while_others_are_made)
                           (initial_state + status_updates)
                       );
 }
+
+BOOST_AUTO_TEST_CASE (response_fails_if_workflow_fails_after_requesting)
+{
+  boost::program_options::options_description options_description;
+
+  options_description.add (test::options::source_directory());
+  options_description.add (test::options::shared_directory());
+  options_description.add (gspc::options::installation());
+  options_description.add (gspc::options::drts());
+  options_description.add (gspc::options::scoped_rifd());
+  options_description.add_options()
+    ( "rpc-lib"
+    , boost::program_options::value<boost::filesystem::path>()->required()
+    , "lib for workflow to link to"
+    );
+
+  boost::program_options::variables_map vm
+    ( test::parse_command_line
+        ( boost::unit_test::framework::master_test_suite().argc
+        , boost::unit_test::framework::master_test_suite().argv
+        , options_description
+        )
+    );
+
+  fhg::util::temporary_path const shared_directory
+    ( test::shared_directory (vm)
+    / "workflow_response_fails_if_workflow_fails_after_requesting"
+    );
+
+  test::scoped_nodefile_from_environment const nodefile_from_environment
+    (shared_directory, vm);
+
+  fhg::util::temporary_path const _installation_dir
+    (shared_directory / boost::filesystem::unique_path());
+  boost::filesystem::path const installation_dir (_installation_dir);
+
+  gspc::set_application_search_path (vm, installation_dir);
+
+  vm.notify();
+
+  gspc::installation const installation (vm);
+
+  test::make_net_lib_install const make
+    ( installation
+    , "workflow_response_fails_if_workflow_fails_after_requesting"
+    , test::source_directory (vm)
+    , installation_dir
+    , test::option::options()
+    . add<test::option::gen::include> (test::source_directory (vm))
+    . add<test::option::gen::include>
+        (test::source_directory (vm).parent_path().parent_path().parent_path())
+    . add<test::option::gen::cxx11>()
+    . add<test::option::gen::link>
+        (vm.at ("rpc-lib").as<boost::filesystem::path>())
+    . add<test::option::gen::ld_flag> ("-lboost_coroutine")
+    );
+
+  gspc::scoped_rifds const rifds { gspc::rifd::strategy (vm)
+                                 , gspc::rifd::hostnames (vm)
+                                 , gspc::rifd::port (vm)
+                                 , installation
+                                 };
+
+  gspc::scoped_runtime_system const drts
+    (vm, installation, "work:1", rifds.entry_points());
+
+  gspc::client client (drts);
+
+  gspc::workflow workflow (make.pnet());
+
+  workflow.set_wait_for_output();
+
+  unsigned long const initial_state (0);
+
+  fhg::util::latch workflow_actually_running (1);
+  fhg::util::latch workflow_shall_fail (1);
+
+  fhg::util::scoped_boost_asio_io_service_with_threads io_service (1);
+  fhg::rpc::service_dispatcher service_dispatcher;
+  fhg::rpc::service_handler<protocol::callback> register_service
+    ( service_dispatcher
+    , [&workflow_actually_running]
+      {
+        workflow_actually_running.count_down();
+      }
+    );
+  fhg::rpc::service_tcp_provider const registry (io_service, service_dispatcher);
+
+  gspc::job_id_t const job_id
+    ( client.submit
+        ( workflow
+        , { {"state", initial_state}
+          , {"register_host", fhg::util::connectable_to_address_string
+                                (registry.local_endpoint().address())}
+          , {"register_port", static_cast<unsigned int>
+                                (registry.local_endpoint().port())}
+          }
+        )
+    );
+
+  workflow_actually_running.wait();
+
+  fhg::util::testing::require_exception
+    ([&client, &job_id]()
+     {
+       client.synchronous_workflow_response
+         (job_id, "get_and_update_state", 0UL);
+     }
+    , std::runtime_error ("workflow failed")
+    );
+}
