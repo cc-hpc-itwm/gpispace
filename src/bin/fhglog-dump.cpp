5ca51c72c0da586c459dcf1e1dabeb72c1f1bc36
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2016-06-16 16:45:28 +0200

    fhglog-dump: fix: skip spaces between events

--- src/bin/fhglog-dump.cpp
+++ src/bin/fhglog-dump.cpp
@@ -3,6 +3,7 @@
 #include <fhglog/format.hpp>
 
 #include <fhg/util/parse/position.hpp>
+#include <fhg/util/parse/require.hpp>
 #include <util-generic/print_exception.hpp>
 
 #include <boost/program_options.hpp>
@@ -79,6 +80,8 @@ try
   while (std::cin.good())
   {
     logger.log (fhg::log::LogEvent (pos));
+
+    fhg::util::parse::require::skip_spaces (pos);
   }
 
   return 0;
