2f6d321094dd2f8a34302fe574e326500be38532
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2017-02-08 17:08:38 +0100

    vmem: let remote_segment not be local since it is used publicly

--- src/drts/private/virtual_memory_impl.hpp
+++ src/drts/private/virtual_memory_impl.hpp
@@ -16,76 +16,73 @@ namespace fhg
 {
   namespace vmem
   {
-    namespace
+    struct remote_segment
     {
-      struct remote_segment
+      remote_segment ( intertwine::vmem::ipc_client& client
+                     , intertwine::vmem::size_t size
+                     , intertwine::vmem::serialized_segment_description const& info
+                     )
+        : _client (client)
+        , _segment_id (_client.segment_create (size, info))
+      {}
+
+      ~remote_segment()
       {
-        remote_segment ( intertwine::vmem::ipc_client& client
-                       , intertwine::vmem::size_t size
-                       , intertwine::vmem::serialized_segment_description const& info
-                       )
-          : _client (client)
-          , _segment_id (_client.segment_create (size, info))
-        {}
-
-        ~remote_segment()
-        {
-          _client.segment_delete (_segment_id);
-        }
-
-        remote_segment (remote_segment const&) = delete;
-        remote_segment (remote_segment&&) = delete;
-        remote_segment& operator= (remote_segment const&) = delete;
-        remote_segment& operator= (remote_segment&&) = delete;
-
-        operator intertwine::vmem::segment_id_t() const
-        {
-          return _segment_id;
-        }
-
-      private:
-        intertwine::vmem::ipc_client& _client;
-        intertwine::vmem::segment_id_t _segment_id;
-      };
-
-      std::unique_ptr<remote_segment>
-        make_remote_segment ( intertwine::vmem::ipc_client& client
-                            , intertwine::vmem::size_t size
-                            , gspc::vmem::segment_description description
-                            )
-      {
-        struct visitor_t
-          : public boost::static_visitor<std::unique_ptr<remote_segment>>
-        {
-          std::unique_ptr<remote_segment> operator()
-            (gspc::vmem::gaspi_segment_description const& desc) const
-          {
-            return fhg::util::cxx14::make_unique<remote_segment>
-              ( _client
-              , _size
-              , intertwine::vmem::gaspi::equally_distributed_segment_description
-                  ( intertwine::vmem::gaspi::communication_buffers
-                      ( intertwine::vmem::size_t (desc._communication_buffer_size)
-                      , desc._communication_buffer_count
-                      )
-                  )
-              );
-          }
-          std::unique_ptr<remote_segment> operator()
-            (gspc::vmem::beegfs_segment_description const&) const
-          {
-            throw std::logic_error ("NYI: beegfs segment (desc._path)");
-          }
-
-          visitor_t ( intertwine::vmem::ipc_client& client
-                    , intertwine::vmem::size_t size
-                    )
-            : _client (client), _size (size) {}
-          intertwine::vmem::ipc_client& _client;
-          intertwine::vmem::size_t _size;
-        } visitor = {client, size};
-        return boost::apply_visitor (visitor, description);
+        _client.segment_delete (_segment_id);
       }
+
+      remote_segment (remote_segment const&) = delete;
+      remote_segment (remote_segment&&) = delete;
+      remote_segment& operator= (remote_segment const&) = delete;
+      remote_segment& operator= (remote_segment&&) = delete;
+
+      operator intertwine::vmem::segment_id_t() const
+      {
+        return _segment_id;
+      }
+
+    private:
+      intertwine::vmem::ipc_client& _client;
+      intertwine::vmem::segment_id_t _segment_id;
+    };
+
+    inline std::unique_ptr<remote_segment>
+      make_remote_segment ( intertwine::vmem::ipc_client& client
+                          , intertwine::vmem::size_t size
+                          , gspc::vmem::segment_description description
+                          )
+    {
+      struct visitor_t
+        : public boost::static_visitor<std::unique_ptr<remote_segment>>
+      {
+        std::unique_ptr<remote_segment> operator()
+          (gspc::vmem::gaspi_segment_description const& desc) const
+        {
+          return fhg::util::cxx14::make_unique<remote_segment>
+            ( _client
+            , _size
+            , intertwine::vmem::gaspi::equally_distributed_segment_description
+                ( intertwine::vmem::gaspi::communication_buffers
+                    ( intertwine::vmem::size_t (desc._communication_buffer_size)
+                    , desc._communication_buffer_count
+                    )
+                )
+            );
+        }
+        std::unique_ptr<remote_segment> operator()
+          (gspc::vmem::beegfs_segment_description const&) const
+        {
+          throw std::logic_error ("NYI: beegfs segment (desc._path)");
+        }
+
+        visitor_t ( intertwine::vmem::ipc_client& client
+                  , intertwine::vmem::size_t size
+                  )
+          : _client (client), _size (size) {}
+        intertwine::vmem::ipc_client& _client;
+        intertwine::vmem::size_t _size;
+      } visitor = {client, size};
+      return boost::apply_visitor (visitor, description);
     }
   }
 }
