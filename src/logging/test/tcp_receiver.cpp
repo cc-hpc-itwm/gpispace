881d0c6118f9654e32ec4a146f50050864f20099
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2018-06-28 17:10:20 +0200

    logging: test: also handle non-authoritive host-not-found
    'x.invalid' will always be not found as per RFC 6761 6.4, yet the
    response isn't always authorative, thus may be both `host_not_found`
    or `host_not_found_try_again`. Thus, handle them both with a custom
    comparator. We ignore lhs and only use that to get the right type
    catched.

--- src/logging/test/tcp_receiver.cpp
+++ src/logging/test/tcp_receiver.cpp
@@ -11,19 +11,100 @@
 
 #include <util-generic/connectable_to_address_string.hpp>
 #include <util-generic/testing/printer/future.hpp>
+#include <util-generic/testing/printer/generic.hpp>
+#include <util-generic/testing/printer/list.hpp>
 #include <util-generic/testing/random.hpp>
 #include <util-generic/testing/require_exception.hpp>
 
 #include <boost/test/unit_test.hpp>
 
+//! \todo src/util-generic/testing/printer/boost/system_error.hpp?
+FHG_BOOST_TEST_LOG_VALUE_PRINTER (boost::system::error_code, os, ec)
+{
+  os << ec << " (\""<< ec.message() << "\")";
+}
+
 namespace fhg
 {
+  namespace util
+  {
+    namespace testing
+    {
+      namespace detail
+      {
+        template<typename Value>
+          bool require_equal_one_of_condition
+            (Value const& value, std::list<Value> const& choices)
+        {
+          return std::any_of ( choices.begin(), choices.end()
+                             , [&] (Value const& choice)
+                               {
+                                 return value == choice;
+                               }
+                             );
+        }
+
+        template<typename Value>
+          std::string require_equal_one_of_message
+            ( Value const& value
+            , char const* const variable
+            , std::list<Value> const& choices
+            )
+        {
+          std::ostringstream oss;
+          oss << variable
+              << " ["
+              << FHG_BOOST_TEST_PRINT_LOG_VALUE_HELPER (value)
+              << "] not in "
+              << FHG_BOOST_TEST_PRINT_LOG_VALUE_HELPER (choices);
+          return oss.str();
+        }
+      }
+
+      //! \todo src/util-generic/testing/require_choice.hpp?
+#define FHG_UTIL_TESTING_REQUIRE_EQUAL_ONE_OF(value_, choices_...)      \
+      BOOST_REQUIRE_MESSAGE                                             \
+        ( ::fhg::util::testing::detail::require_equal_one_of_condition  \
+            (value_, {choices_})                                        \
+        , ::fhg::util::testing::detail::require_equal_one_of_message    \
+            (value_, #value_, {choices_})                               \
+        )
+
+      //! \todo src/util-generic/testing/require_strings.hpp?
+#define FHG_UTIL_TESTING_REQUIRE_STARTS_WITH(value_, prefix_)           \
+      BOOST_REQUIRE_EQUAL                                               \
+        (std::string (value_).substr (0, strlen (prefix_)), prefix_)
+
+    }
+  }
+
   namespace logging
   {
     BOOST_AUTO_TEST_CASE (receiver_shall_throw_if_connecting_fails)
     {
       std::promise<tcp_endpoint> received;
       auto received_future (received.get_future());
+
+      //! \note 'x.invalid' will always be not found as per RFC 6761
+      //! 6.4, yet the response isn't always authorative, thus may be
+      //! both `host_not_found` or `host_not_found_try_again`. Thus,
+      //! handle them both with a custom comparator. We ignore lhs and
+      //! only use that to get the right type catched.
+      struct
+      {
+        void operator() ( boost::system::system_error const&
+                        , boost::system::system_error const& catched
+                        )
+        {
+          FHG_UTIL_TESTING_REQUIRE_STARTS_WITH (catched.what(), "resolve:");
+          FHG_UTIL_TESTING_REQUIRE_EQUAL_ONE_OF
+            ( catched.code()
+            , boost::asio::error::host_not_found
+            , boost::asio::error::host_not_found_try_again
+            );
+        }
+
+      } comparator;
 
       util::testing::require_exception
         ( []
@@ -33,6 +114,7 @@ namespace fhg
           }
         , boost::system::system_error
             (boost::asio::error::host_not_found, "resolve")
+        , comparator
         );
     }
 
