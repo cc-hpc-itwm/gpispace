4c2eeb64fb0122ab0d79decc9c32e7444c81d9d5
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2018-08-28 21:31:54 +0200

    util: support enum classes in INVALID_ENUM_VALUE

--- src/fhg/util/macros.hpp
+++ src/fhg/util/macros.hpp
@@ -1,14 +1,21 @@
-// bernd.loerwald@itwm.fraunhofer.de
-
 #pragma once
 
-#include <stdexcept>
-#include <boost/format.hpp>
 #include <boost/current_function.hpp>
-#define INVALID_ENUM_VALUE(type, value) \
-  throw std::out_of_range \
-     ((boost::format \
-      ("%1%:%2%: %3%: value of enum '%4%' expected: got non-enummed-value %5%") \
-      % __FILE__ % __LINE__ % BOOST_CURRENT_FUNCTION % #type % value \
-      ).str() \
-     )
+#include <boost/format.hpp>
+
+#include <stdexcept>
+#include <type_traits>
+
+#define INVALID_ENUM_VALUE(type_, value_)                               \
+  throw std::out_of_range                                               \
+    ( ( boost::format                                                   \
+          ( "%1%:%2%: %3%: value of enum '%4%' expected: "              \
+            "got non-enummed-value %5%"                                 \
+          )                                                             \
+      % __FILE__                                                        \
+      % __LINE__                                                        \
+      % BOOST_CURRENT_FUNCTION                                          \
+      % #type_                                                          \
+      % static_cast<std::underlying_type<type_>::type> (value_)         \
+      ).str()                                                           \
+    )
