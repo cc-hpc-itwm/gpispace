501482bcc2a5b1f38818c097d6b9164d545339c7
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2019-02-14 20:31:48 +0100

    stencil_cache: flatten library used by only one other target

--- test/gspc/stencil_cache/workflow/Neighborhood.cpp
+++ test/gspc/stencil_cache/workflow/Neighborhood.cpp
@@ -78,3 +78,20 @@ namespace test
     }
   }
 }
+
+using N = test::gspc::stencil_cache::workflow::Neighborhood;
+
+GSPC_STENCIL_CACHE_CALLBACK (std::shared_ptr<void>, init)
+  (std::vector<char> const& data)
+{
+  return std::make_shared<N> (data);
+}
+
+GSPC_STENCIL_CACHE_CALLBACK
+  (std::list<gspc::stencil_cache::Coordinate>, neighbors)
+  ( void* neighborhood
+  , gspc::stencil_cache::Stencil coordinate
+  )
+{
+  return static_cast<N*> (neighborhood)->operator() (coordinate);
+}
