4c3e81bd41a3fff5ab15f9a1597931fca9abaf8a
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2019-09-16 16:42:18 +0200

    logging: add tool to log TCP emitters to stdout
    updates src/util-generic:
    
    * 23047bf Merge branch 'syscall-sigwaitinfo_sigtimedwait' into
      'master'
    * 9d824fd syscall: add sigwaitinfo, sigtimedwait - synchronously wait
      for queued signals
    * d1f59ce Merge branch 'workaround_issue_45' into 'master'
    * 8cfc5d3 Apply suggestion to CMakeLists.txt
    * 03414e6 workaround issue #45
    * c4f0330 add missing include <boost/noncopyable.hpp>
    * 3547fea Merge branch 'cxx1x-forward_and_revert_test_hacks' into
      'master'
    * 1a620e8 ci: simplify gitlab-ci.yml and add cxx14/17 test jobs
    * b859e10 cxx17: forward apply to std:: if available
    * 95f1d96 cxx17: forward void_t to std:: if available
    * 92c812b cxx17: forward logical operator typetraits to std:: if
      available
    * 5691daf cxx14: forward get-by-type to std:: if available
    * 405c0c7 cxx14: forward integer_sequence family to std:: if available
    * 469226d cxx14: forward make_unique to std:: if available
    * 7c8b681 cxx17: remove test for apply (void(int), int)
    * 1396fe0 cxx14: reduce requirements on get-by-type implementation
    * 234dd6a cxx14: make integer_sequence more compliant
    * 4ed3bd5 cmake: let std::hash<Enum> detection use inline code
    * 3acdffa Merge branch 'Wc++2a-compat' into 'master'
    * fd3cbad -Wc++2a-compat
    * 407b1ac serialization: add missing include <utility>
    * 097715d Merge branch 'make_gcc7_with_higher_cpp_levels_happy' into
      'master'
    * 6a8327a test: cxx17: make gcc7 --std=c++17 happy
    * cb96581 test: cxx14: make gcc7 --std=c++14 happy
    * c9d0bda Merge branch 'fix_some_pvs_findings' into 'master'
    * 20c6e4f test: fix: remove unreachable code
    * a0ed06e test: fix: initialize all members

---
+++ src/logging/gspc-logging-to-stdout.cpp
@@ -0,0 +1,51 @@
+#include <logging/tcp_endpoint.hpp>
+#include <logging/tcp_receiver.hpp>
+
+#include <fhg/util/boost/program_options/generic.hpp>
+#include <util-generic/ostream/put_time.hpp>
+#include <util-generic/print_exception.hpp>
+#include <util-generic/syscall.hpp>
+#include <util-generic/syscall/process_signal_block.hpp>
+#include <util-generic/syscall/signal_set.hpp>
+
+#include <iostream>
+#include <vector>
+
+int main (int argc, char** argv)
+try
+{
+  fhg::util::syscall::signal_set const signals {SIGINT, SIGTERM};
+  fhg::util::syscall::process_signal_block const signal_block (signals);
+
+  namespace po = fhg::util::boost::program_options;
+  po::option<std::vector<fhg::logging::tcp_endpoint>> const emitters
+    {"emitters", "list of tcp emitters"};
+
+  boost::program_options::variables_map const vm
+    ( po::options ("GPI-Space Logging to stdout")
+    . require (emitters)
+    . store_and_notify (argc, argv)
+    );
+
+  fhg::logging::tcp_receiver receiver
+    ( emitters.get_from (vm)
+    , [] (fhg::logging::message const& message)
+      {
+        std::cout << message._process_id << "." << message._thread_id
+                  << "@" << message._hostname << " "
+                  << fhg::util::ostream::put_time<std::chrono::system_clock>
+                       (message._timestamp)
+                  << " " << message._category
+                  << " " << message._content << "\n";
+      }
+    );
+
+  fhg::util::syscall::sigwaitinfo (&signals._, nullptr);
+
+  return EXIT_SUCCESS;
+}
+catch (...)
+{
+  std::cerr << fhg::util::current_exception_printer() << "\n";
+  return EXIT_FAILURE;
+}
