5ed91964e668e65a54876f289f6aeb05457e7fa3
shankar <shankar@lts002hpc.itwm.fhg.de>
2019-10-28 13:01:50 +0100

    we: test: put_many: consolidating the two TP/TP_MANY duplicate connection test-cases into one data test-case

--- src/we/test/tp_many_duplicate_connections.cpp
+++ src/we/test/tp_many_duplicate_connections.cpp
@@ -1,12 +1,15 @@
 #include <boost/test/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
 
 #include <we/type/transition.hpp>
 #include <we/type/net.hpp>
 
-#include <util-generic/testing/flatten_nested_exceptions.hpp>
 #include <util-generic/testing/require_exception.hpp>
 
-BOOST_AUTO_TEST_CASE (tp_many_check_no_duplicate_tp_many_for_tp)
+BOOST_DATA_TEST_CASE ( add_connection_only_allows_a_tp_or_tp_many
+                     , std::vector<bool> ({true, false})
+                     , many_first
+                     )
 {
   we::type::net_type net;
   we::type::property::type empty;
@@ -22,51 +25,6 @@ BOOST_AUTO_TEST_CASE (tp_many_check_no_duplicate_tp_many_for_tp)
 
   we::place_id_type const pid_out_list
     (net.add_place (place::type ("out_list", "list", boost::none)));
-
-  we::port_id_type const port_out_list
-    ( trans_io.add_port
-      (we::type::port_t ("out", we::type::PORT_OUT, "list"))
-    );
-
-  net.add_connection ( we::edge::TP
-                     , tid
-                     , pid_out_list
-                     , port_out_list
-                     , empty
-                     );
-
-  we::place_id_type const pid_out_t
-    (net.add_place (place::type ("out", "long", boost::none)));
-
-  BOOST_TEST_INFO ("duplicate TP_MANY for existing TP edge");
-  fhg::util::testing::require_exception
-    ( [&]
-      {
-        net.add_connection ( we::edge::TP_MANY
-                           , tid
-                           , pid_out_t
-                           , port_out_list
-                           , {}
-                           );
-      }
-    , std::logic_error ("duplicate connection: out and out-many")
-    );
-}
-
-BOOST_AUTO_TEST_CASE (tp_many_check_no_duplicate_tp_for_tp_many)
-{
-  we::type::net_type net;
-  we::type::property::type empty;
-
-  we::type::transition_t trans_io
-    ( "put_many_dup_test"
-      , we::type::expression_t ("")
-      , boost::none
-      , {}
-      , we::priority_type()
-    );
-  we::transition_id_type const tid (net.add_transition (trans_io));
-
   we::place_id_type const pid_out_t
     (net.add_place (place::type ("out", "long", boost::none)));
 
@@ -75,28 +33,28 @@ BOOST_AUTO_TEST_CASE (tp_many_check_no_duplicate_tp_for_tp_many)
       (we::type::port_t ("out", we::type::PORT_OUT, "list"))
     );
 
-  net.add_connection ( we::edge::TP_MANY
+  auto const edge_first (many_first ? we::edge::TP_MANY : we::edge::TP);
+  auto const edge_second (many_first ? we::edge::TP :  we::edge::TP_MANY);
+  auto const pid_first (many_first ? pid_out_t : pid_out_list);
+  auto const pid_second (many_first ? pid_out_list : pid_out_t);
+
+  net.add_connection ( edge_first
                      , tid
-                     , pid_out_t
+                     , pid_first
                      , port_out_list
                      , empty
                      );
 
-  we::place_id_type const pid_out_list
-    (net.add_place (place::type ("out_list", "list", boost::none)));
-
-  BOOST_TEST_INFO ("duplicate TP for existing TP_MANY edge");
   fhg::util::testing::require_exception
     ( [&]
       {
-        net.add_connection ( we::edge::TP
+        net.add_connection ( edge_second
                            , tid
-                           , pid_out_list
+                           , pid_second
                            , port_out_list
-                           , {}
+                           , empty
                            );
       }
     , std::logic_error ("duplicate connection: out and out-many")
     );
 }
-
