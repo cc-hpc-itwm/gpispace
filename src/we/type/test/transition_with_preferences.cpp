f9742bde2064b12f22c0fa92ff760b2cc25a081f
shankar <shankar@lts002hpc.itwm.fhg.de>
2019-11-26 13:53:29 +0100

    we: test: clean up we_transition_with_preferences_test (+ comment for muti-modules update)
    - using enum vs vector-of-strings
    
    - consolidating create_transition_with_preferences functionality

--- src/we/type/test/transition_with_preferences.cpp
+++ src/we/type/test/transition_with_preferences.cpp
@@ -6,58 +6,48 @@
 
 #include <util-generic/testing/require_exception.hpp>
 #include <util-generic/testing/printer/list.hpp>
+#include <util-generic/testing/require_serialized_to_id.hpp>
 
 #include <we/test/operator_equal.hpp>
 
 namespace {
-  std::vector<std::string> const non_preference_types {"net", "expression"};
+  //! \todo update this list to contain single module type
+  enum transition_type { net, module, expression };
+
+  std::vector<transition_type> const non_preference_types
+    { transition_type::net
+    , transition_type::expression
+    };
+
+  using data_type = typename std::remove_reference
+                      <decltype
+                        ( std::declval <we::type::transition_t&>()
+                          .data()
+                        )
+                      >::type;
 
   we::type::transition_t create_transition_with_preferences
-    (const std::string type)
+    ( const std::string transition_name
+    , const transition_type type
+    )
   {
-    std::list<we::type::preference_t> preferences { "target1"
-                                                  , "target2"
-                                                  };
-    if (type == "net")
-    {
-      return we::type::transition_t
-        ( "preferences_test"
-        , we::type::net_type()
-        , boost::none
-        , {}
-        , we::priority_type()
-        , preferences
-        );
-    }
-    else if (type == "expression")
-    {
-      return we::type::transition_t
-        ( "preferences_test"
-        , we::type::expression_t("")
-        , boost::none
-        , {}
-        , we::priority_type()
-        , preferences
-        );
-    }
-    else if (type == "module")
-    {
-      return we::type::transition_t
-        ( "preferences_test"
-        , we::type::module_call_t()
-        , boost::none
-        , {}
-        , we::priority_type()
-        , preferences
-        );
-    }
-    else
-    {
-      throw std::runtime_error ( "Transiton data_type invalid"
-                                 " for non-modules test: "
-                                 + type
-                               );
-    }
+    return we::type::transition_t
+      ( transition_name
+      , type == transition_type::net ?
+          data_type (we::type::net_type())
+      : type == transition_type::expression ?
+          data_type (we::type::expression_t (""))
+      : type == transition_type::module ?
+          data_type (we::type::module_call_t())
+      : throw std::logic_error
+          ("invalid transition data_type specified")
+      , boost::none
+      , {}
+      , we::priority_type()
+      , std::list<we::type::preference_t> { "target1"
+                                          , "target2"
+                                          }
+      );
   }
 }
 
@@ -66,17 +56,18 @@ BOOST_DATA_TEST_CASE ( we_transition_check_preferences_without_modules
                      , type_name
                      )
 {
-  std::string const test_name ("preferences_test");
+  const std::string transition_name ("transition_with_prefs");
 
   fhg::util::testing::require_exception
     ( [&]
       {
-        create_transition_with_preferences (type_name);
+        create_transition_with_preferences ( transition_name
+                                           , type_name);
       }
       , std::runtime_error
         ( ( boost::format ("Error when creating transition '%1%'"
                            " preferences defined without modules"
-                          ) % test_name
+                          ) % transition_name
           ).str()
         )
     );
@@ -96,7 +87,9 @@ namespace we
 BOOST_AUTO_TEST_CASE (we_transition_check_for_serialized_preferences)
 {
   auto const transition_with_preferences_and_modules =
-    create_transition_with_preferences ("module");
+    create_transition_with_preferences ( "transition_for_serialize"
+                                       , transition_type::module
+                                       );
 
   FHG_UTIL_TESTING_REQUIRE_SERIALIZED_TO_ID
     ( {transition_with_preferences_and_modules}
