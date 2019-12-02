b7335c110bf47f18c6fdbc15b9191306b48f8f0f
shankar <shankar@lts002hpc.itwm.fhg.de>
2019-12-02 18:25:59 +0100

    xml: test: re-structing and updating valid preferences test
    - adding three test cases:
      - check no parser exception for valid preference list and
        modules-with-target
      - check if parser propagates parsed list function type created
      - check if parser propagates parser list to WE transition
    
    - encapsulating redudant pnet XML generation per transition content type +
      valid preference list generation in struct valid_pnet_with_multi_modules

--- src/xml/tests/parse_valid_preference_list.cpp
+++ src/xml/tests/parse_valid_preference_list.cpp
@@ -45,4 +45,92 @@ namespace
     return _targets;
   }
 
+  struct valid_pnet_with_multi_modules
+  {
+    std::list<std::string> const test_targets;
+    std::string test_pnet;
+
+    valid_pnet_with_multi_modules ()
+      : test_targets (gen_valid_targets())
+    {
+      std::string const pnet_target_list
+        ( "<target>"
+          + fhg::util::join_reference<std::list<std::string>, std::string>
+            (test_targets, "</target><target>").string()
+          + "</target>"
+        );
+
+      std::string const pnet_module_prefix
+        ( "<module name=\""
+          + fhg::util::testing::random_identifier() + "\""
+          + " function=\"func()\""
+          + " target=\""
+        );
+      std::string const pnet_module_suffix ("\"></module>");
+      std::string const pnet_module_list
+        ( pnet_module_prefix
+          + fhg::util::join_reference<std::list<std::string>, std::string>
+            (test_targets, pnet_module_suffix + pnet_module_prefix)
+            .string()
+          + pnet_module_suffix
+        );
+
+      test_pnet =
+        ( boost::format (R"EOS(
+          <defun name="n_preferences">
+            <preferences>
+            %1%
+            </preferences>
+            %2%
+          </defun>)EOS")
+          % pnet_target_list
+          % pnet_module_list
+        ).str();
+    }
+  };
+}
+
+BOOST_AUTO_TEST_CASE (xml_parse_valid_preferences_and_match_to_target_in_modules)
+{
+  valid_pnet_with_multi_modules const pnet_with_multi_modules;
+
+  xml::parse::state::type state;
+  std::istringstream input_stream (pnet_with_multi_modules.test_pnet);
+
+  BOOST_REQUIRE_NO_THROW (xml::parse::just_parse (state, input_stream));
+}
+
+BOOST_AUTO_TEST_CASE (xml_parse_valid_preferences_and_match_targets_in_function)
+{
+  valid_pnet_with_multi_modules const pnet_with_multi_modules;
+
+  xml::parse::state::type state;
+
+  std::istringstream input_stream (pnet_with_multi_modules.test_pnet);
+  xml::parse::type::function_type fun
+    = xml::parse::just_parse (state, input_stream);
+
+  const std::list<xml::parse::type::preference_type>& out_targets
+    = fun.preferences().targets();
+
+  BOOST_REQUIRE_EQUAL (pnet_with_multi_modules.test_targets, out_targets);
+}
+
+BOOST_AUTO_TEST_CASE (xml_parse_valid_preferences_and_propagate_to_we)
+{
+  valid_pnet_with_multi_modules const pnet_with_multi_modules;
+
+  xml::parse::state::type state;
+
+  std::istringstream input_stream (pnet_with_multi_modules.test_pnet);
+  xml::parse::type::function_type fun
+    = xml::parse::just_parse (state, input_stream);
+
+  we::type::activity_t activity
+    (xml::parse::xml_to_we (fun, state));
+
+  const std::list<xml::parse::type::preference_type>& out_targets
+    = activity.preferences();
+
+  BOOST_REQUIRE_EQUAL (pnet_with_multi_modules.test_targets, out_targets);
 }
