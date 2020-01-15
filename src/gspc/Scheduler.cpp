202f5868578bccef3296411fb661ef6360f3e0d1
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2020-01-15 11:40:20 +0100

    remove old proto-proto

--- src/gspc/Scheduler.cpp
+++
@@ -1,9 +0,0 @@
-#include <gspc/Scheduler.hpp>
-
-namespace gspc
-{
-  Scheduler::Scheduler ( resource_manager::Connection
-                       , workflow::Connection
-                       )
-  {}
-}
--- src/gspc/Workflow.cpp
+++
@@ -1,7 +0,0 @@
-#include <gspc/Workflow.hpp>
-
-namespace gspc
-{
-  Workflow::Workflow (State)
-  {}
-}
