6dca69a20969668517eb58003a544cf0ed2e3e75
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2020-01-17 10:43:38 +0100

    resource/Class

--- src/gspc/Proto.hpp
+++ src/gspc/Proto.hpp
@@ -6,6 +6,7 @@
 #include <gspc/remote_interface/Hostname.hpp>
 #include <gspc/remote_interface/ID.hpp>
 
+#include <gspc/resource/Class.hpp>
 #include <gspc/resource/ID.hpp>
 
 #include <gspc/rpc/TODO.hpp>
@@ -38,20 +39,6 @@
 #include <unordered_map>
 #include <unordered_set>
 
-namespace gspc
-{
-  namespace resource
-  {
-    //! \note *not* a set, if users want a single worker to have more
-    //! than one resource, they need to explicitly model that in the
-    //! forest. (e.g. "CPU+Compute" -> ["CPU", "Compute"], which
-    //! allows for aquisition of "CPU", "Compute" or "CPU+Compute",
-    //! and allows the user to specify exactly which subset is a valid
-    //! aquisition, e.g. "GPU+Compute" -> ["Compute"], would not allow
-    //! to aquire a solo GPU worker, but still a compute-only task).
-    using Class = std::string;
-  }
-}
 
 namespace gspc
 {
