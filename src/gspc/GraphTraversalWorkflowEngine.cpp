6e9ed400f138b21731585bc3a1a735da3fa1601a
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2020-01-19 19:23:01 +0100

    GraphTraversalEngine
    - arbitrary graphs (inluding cycles and diamonds)
    - Note: tests in the next commits
    
    TODO
    - factor workflow_engine
    - factor drivers

---
+++ src/gspc/GraphTraversalWorkflowEngine.cpp
@@ -0,0 +1,176 @@
+#include <gspc/GraphTraversalWorkflowEngine.hpp>
+
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/iostreams/device/array.hpp>
+#include <boost/iostreams/device/back_inserter.hpp>
+#include <boost/iostreams/filtering_stream.hpp>
+
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+namespace gspc
+{
+  GraphTraversalWorkflowEngine::GraphTraversalWorkflowEngine
+    ( std::unordered_set<value_type> open
+    , Task::Symbol symbol
+    , Task::Inputs inputs
+    )
+  {
+    _workflow_state._open = open;
+    _workflow_state._symbol = symbol;
+    _workflow_state._inputs = inputs;
+  }
+
+  Forest<value_type> const& GraphTraversalWorkflowEngine::structure() const
+  {
+    return _workflow_state._structure;
+  }
+
+  std::unordered_map<value_type, std::size_t> const&
+    GraphTraversalWorkflowEngine::seen() const
+  {
+    return _workflow_state._seen;
+  }
+
+  std::unordered_set<value_type> const&
+    GraphTraversalWorkflowEngine::open() const
+  {
+    return _workflow_state._open;
+  }
+
+  Task::Symbol const& GraphTraversalWorkflowEngine::symbol() const
+  {
+    return _workflow_state._symbol;
+  }
+
+  Task::Inputs const& GraphTraversalWorkflowEngine::inputs() const
+  {
+    return _workflow_state._inputs;
+  }
+
+  bool GraphTraversalWorkflowEngine::workflow_finished() const
+  {
+    return _workflow_state._open.empty();
+  }
+
+  template<typename Archive>
+    void GraphTraversalWorkflowEngine::WorkflowState::serialize
+      (Archive& ar, unsigned int /* version */)
+  {
+    ar & _structure;
+    ar & _seen;
+    ar & _open;
+    ar & _symbol;
+    ar & _inputs;
+  }
+
+  workflow_engine::State GraphTraversalWorkflowEngine::state() const
+  {
+    std::vector<char> data;
+    {
+      boost::iostreams::filtering_ostream zos
+        (boost::iostreams::back_inserter (data));
+      boost::archive::binary_oarchive oa (zos);
+
+      oa & _workflow_state;
+    }
+
+    return {std::move (data), workflow_finished(), _processing_state};
+  }
+
+  GraphTraversalWorkflowEngine::GraphTraversalWorkflowEngine
+      (workflow_engine::State state)
+    : _processing_state (state.processing_state)
+  {
+    //! \todo see aloma::core::data::serialization
+    auto const& data (state.engine_specific);
+
+    boost::iostreams::filtering_istream zis
+      (boost::iostreams::array_source (data.data(), data.size()));
+    boost::archive::binary_iarchive ia (zis);
+
+    ia & _workflow_state;
+
+    if (state.workflow_finished != workflow_finished())
+    {
+      throw std::logic_error ("INCONSISTENCY: finished or not!?");
+    }
+  }
+
+  namespace
+  {
+    value_type pop_any (std::unordered_set<value_type>& values)
+    {
+      auto selected (values.begin());
+      auto value (*selected);
+      values.erase (selected);
+      return value;
+    }
+  }
+
+  boost::variant<Task, bool> GraphTraversalWorkflowEngine::extract()
+  {
+    if (workflow_finished())
+    {
+      return !_processing_state.has_extracted_tasks();
+    }
+
+    auto const parent (pop_any (_workflow_state._open));
+    if (!_workflow_state._seen[parent]++)
+    {
+      _workflow_state._structure.insert (parent, {}, {});
+    }
+
+    Task::Inputs inputs (_workflow_state._inputs);
+    inputs.emplace ("parent", parent);
+
+    return _processing_state.extract
+      ("core", inputs, "graph_so", _workflow_state._symbol);
+  }
+
+  void GraphTraversalWorkflowEngine::inject
+    (task::ID id, ErrorOr<task::Result> result)
+  {
+    return _processing_state.inject
+      ( std::move (id)
+      , std::move (result)
+      , [&] (Task const& input_task, task::Result const& result)
+        {
+          auto const& outputs (result.outputs);
+          auto const& inputs (input_task.inputs);
+
+          auto value_at
+            ( [&] (auto const& xs, auto x)
+              {
+                return xs.equal_range (x).first->second;
+              }
+            );
+
+          if (  outputs.count ("parent") != 1
+             || inputs.count ("parent") != 1
+             || value_at (outputs, "parent") != value_at (inputs, "parent")
+             )
+          {
+            throw std::logic_error
+              ("GraphTraversalWorkflowEngine::inject: Unexpected result");
+          }
+
+          auto const& parent (value_at (outputs, "parent"));
+          auto children (outputs.equal_range ("children"));
+
+          for (auto child (children.first); child != children.second; ++child)
+          {
+            if (!_workflow_state._seen[child->second]++)
+            {
+              _workflow_state._open.emplace (child->second);
+              _workflow_state._structure.insert (child->second, {}, {parent});
+            }
+          }
+        }
+      );
+  }
+}
---
+++ src/gspc/GraphTraversalWorkflowEngine.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <gspc/interface/WorkflowEngine.hpp>
+
+#include <gspc/ErrorOr.hpp>
+#include <gspc/Forest.hpp>
+#include <gspc/Task.hpp>
+#include <gspc/task/ID.hpp>
+#include <gspc/task/Result.hpp>
+#include <gspc/value_type.hpp>
+#include <gspc/workflow_engine/ProcessingState.hpp>
+#include <gspc/workflow_engine/State.hpp>
+
+#include <boost/variant.hpp>
+
+#include <cstdint>
+#include <list>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+
+namespace gspc
+{
+  //! graph might contain cycles and diamonds
+
+  //! nodes are expanded only once. duplicate parent connection is
+  //! inserted by the child that returns first
+
+  class GraphTraversalWorkflowEngine : public interface::WorkflowEngine
+  {
+  public:
+    GraphTraversalWorkflowEngine
+      ( std::unordered_set<value_type>
+      , Task::Symbol
+      , Task::Inputs // default parameter for each task
+      );
+
+    Forest<value_type> const& structure() const;
+    std::unordered_map<value_type, std::size_t> const& seen() const;
+    std::unordered_set<value_type> const& open() const;
+    Task::Symbol const& symbol() const;
+    Task::Inputs const& inputs() const;
+
+    virtual boost::variant<Task, bool> extract() override;
+    virtual void inject (task::ID, ErrorOr<task::Result>) override;
+
+    virtual workflow_engine::State state() const override;
+    GraphTraversalWorkflowEngine (workflow_engine::State);
+
+  private:
+    struct WorkflowState
+    {
+      Forest<value_type> _structure;
+      std::unordered_map<value_type, std::size_t> _seen;
+      std::unordered_set<value_type> _open;
+
+      Task::Symbol _symbol;
+      Task::Inputs _inputs;
+
+      template<typename Archive>
+        void serialize (Archive& ar, unsigned int /* version */);
+    } _workflow_state;
+
+    bool workflow_finished() const;
+
+    workflow_engine::ProcessingState _processing_state;
+  };
+}
