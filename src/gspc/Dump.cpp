abaa57e2c8fb1be907a7f82545aaf7c29728a760
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2020-01-23 14:34:29 +0100

    util: dump workflow state

---
+++ src/gspc/Dump.cpp
@@ -0,0 +1,25 @@
+#include <gspc/serialization.hpp>
+#include <gspc/workflow_engine/State.hpp>
+
+#include <util-generic/print_exception.hpp>
+#include <util-generic/read_file.hpp>
+
+#include <iostream>
+
+int main()
+try
+{
+  auto state ( gspc::bytes_load<gspc::workflow_engine::State>
+                 (fhg::util::read_file<std::vector<char>> ("/dev/stdin"))
+             );
+
+  std::cout << state << std::endl;
+
+  return EXIT_SUCCESS;
+}
+catch (...)
+{
+  std::cerr << "EXCEPTION: " << fhg::util::current_exception_printer() << '\n';
+
+  return EXIT_FAILURE;
+}
