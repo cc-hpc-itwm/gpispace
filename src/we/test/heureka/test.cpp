ee9b5a471c27d4d57109289910bdc76c6e012495
Dipti Shankar <dipti.shankar@itwm.fraunhofer.de>
2020-02-17 13:51:56 +0100

    we: test: adding initial example/test for heureka
    - example contains pnets for both module and expression-triggered heurekas
    
    - starts four workers and launches 1-10 tasks that may contain a heureka event
      (task: generate random 1<=value<=30 + sleep 5 seconds; heureka: if value < 20)
    
    - to-do: improve on this base example;  consider other variations of petri net

---
+++ src/we/test/heureka/test.cpp
@@ -0,0 +1,102 @@
+#include <boost/test/unit_test.hpp>
+
+#include <drts/client.hpp>
+#include <drts/drts.hpp>
+#include <drts/scoped_rifd.hpp>
+
+#include <test/make.hpp>
+#include <test/parse_command_line.hpp>
+#include <test/scoped_nodefile_from_environment.hpp>
+#include <test/source_directory.hpp>
+#include <test/shared_directory.hpp>
+
+#include <we/type/value.hpp>
+#include <we/type/value/wrap.hpp>
+#include <we/type/value/unwrap.hpp>
+#include <we/type/value/boost/test/printer.hpp>
+
+#include <util-generic/testing/flatten_nested_exceptions.hpp>
+#include <util-generic/temporary_path.hpp>
+
+#include <boost/program_options.hpp>
+
+BOOST_AUTO_TEST_CASE (we_heureka_kill_all)
+{
+  boost::program_options::options_description options_description;
+
+  options_description.add (test::options::source_directory());
+  options_description.add (test::options::shared_directory());
+  options_description.add (gspc::options::installation());
+  options_description.add (gspc::options::drts());
+  options_description.add (gspc::options::scoped_rifd());
+
+  boost::program_options::variables_map vm
+    ( test::parse_command_line
+        ( boost::unit_test::framework::master_test_suite().argc
+        , boost::unit_test::framework::master_test_suite().argv
+        , options_description
+        )
+    );
+
+  fhg::util::temporary_path const shared_directory
+    (test::shared_directory (vm) / "we_heureka_kill_all");
+
+  test::scoped_nodefile_from_environment const nodefile_from_environment
+    (shared_directory, vm);
+
+  fhg::util::temporary_path const _installation_dir
+    (shared_directory / boost::filesystem::unique_path());
+  boost::filesystem::path const installation_dir (_installation_dir);
+
+  gspc::set_application_search_path (vm, installation_dir);
+
+  vm.notify();
+
+  gspc::installation const installation (vm);
+
+  gspc::scoped_rifds const rifds ( gspc::rifd::strategy {vm}
+                                 , gspc::rifd::hostnames {vm}
+                                 , gspc::rifd::port {vm}
+                                 , installation
+                                 );
+  gspc::scoped_runtime_system const drts
+    (vm, installation, "work:4", rifds.entry_points());
+
+  test::make_net_lib_install const with_mod
+    ( installation
+    , "find_heureka_with_mod"
+    , test::source_directory (vm)
+    , installation_dir
+    );
+  test::make_net_lib_install const with_exp
+    ( installation
+    , "find_heureka_with_exp"
+    , test::source_directory (vm)
+    , installation_dir
+    );
+
+  for (auto workflow : {with_exp.pnet(), with_mod.pnet()})
+  {
+    BOOST_TEST_CONTEXT ("With heureka transition: " << workflow.filename())
+    {
+      for (unsigned long N (1UL); N <= 10UL; ++N)
+      {
+        std::list<unsigned long> token_list;
+        token_list.resize (N);
+        std::iota (token_list.begin(), token_list.end(), 1);
+
+        BOOST_TEST_CONTEXT ("#workers: " << N)
+        {
+          std::multimap<std::string, pnet::type::value::value_type> const result
+            ( gspc::client (drts).put_and_run
+              ( gspc::workflow (workflow)
+              , { {"ntokens", pnet::type::value::wrap (token_list)}
+                , {"heureka_gid", std::string("heureka_gid")}
+                }
+              )
+            );
+        }
+      }
+    }
+  }
+}
