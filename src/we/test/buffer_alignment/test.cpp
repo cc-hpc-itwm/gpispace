fd3cd2a12d3f970864e4003bebc7ef6e52b0ebe9
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2020-03-10 16:35:55 +0100

    added negative test with arbitrary sizes and alignments for memory buffers, failing due not taking into consideration the alignments when calculating the total amount of local memory needed

--- src/we/test/buffer_alignment/test.cpp
+++ src/we/test/buffer_alignment/test.cpp
@@ -1,6 +1,7 @@
 #include <boost/test/unit_test.hpp>
 
 #include <net_with_arbitrary_buffer_sizes_and_default_alignments.hpp>
+#include <net_with_arbitrary_buffer_sizes_and_alignments_insufficient_memory.hpp>
 
 #include <drts/client.hpp>
 #include <drts/drts.hpp>
@@ -26,50 +27,52 @@
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
 
+#define INSTALLATION_AND_RFIDS()                                         \
+  boost::program_options::options_description options_description;       \
+  options_description.add (test::options::source_directory());           \
+  options_description.add (test::options::shared_directory());           \
+  options_description.add (gspc::options::installation());               \
+  options_description.add (gspc::options::drts());                       \
+  options_description.add (gspc::options::scoped_rifd());                \
+  options_description.add (gspc::options::virtual_memory());             \
+                                                                         \
+  boost::program_options::variables_map vm                               \
+    ( test::parse_command_line                                           \
+        ( boost::unit_test::framework::master_test_suite().argc          \
+        , boost::unit_test::framework::master_test_suite().argv          \
+        , options_description                                            \
+        )                                                                \
+    );                                                                   \
+                                                                         \
+  fhg::util::temporary_path const shared_directory                       \
+    ( test::shared_directory (vm)                                        \
+    / "buffer_alignment"                                                 \
+    );                                                                   \
+                                                                         \
+  test::scoped_nodefile_from_environment const nodefile_from_environment \
+    (shared_directory, vm);                                              \
+                                                                         \
+  fhg::util::temporary_path const _installation_dir                      \
+    (shared_directory / boost::filesystem::unique_path());               \
+  boost::filesystem::path const installation_dir (_installation_dir);    \
+                                                                         \
+  gspc::set_application_search_path (vm, installation_dir);              \
+  test::set_virtual_memory_socket_name_for_localhost (vm);               \
+                                                                         \
+  vm.notify();                                                           \
+                                                                         \
+  gspc::installation const installation (vm);                            \
+                                                                         \
+  gspc::scoped_rifds const rifds ( gspc::rifd::strategy {vm}             \
+                                 , gspc::rifd::hostnames {vm}            \
+                                 , gspc::rifd::port {vm}                 \
+                                 , installation                          \
+                                 )
+
+
 BOOST_AUTO_TEST_CASE (arbitrary_buffer_sizes_and_default_alignments)
 {
-  boost::program_options::options_description options_description;
-
-  options_description.add (test::options::source_directory());
-  options_description.add (test::options::shared_directory());
-  options_description.add (gspc::options::installation());
-  options_description.add (gspc::options::drts());
-  options_description.add (gspc::options::logging());
-  options_description.add (gspc::options::scoped_rifd());
-  options_description.add (gspc::options::virtual_memory());
-
-  boost::program_options::variables_map vm
-    ( test::parse_command_line
-        ( boost::unit_test::framework::master_test_suite().argc
-        , boost::unit_test::framework::master_test_suite().argv
-        , options_description
-        )
-    );
-
-  fhg::util::temporary_path const shared_directory
-    ( test::shared_directory (vm)
-    / "buffer_alignment"
-    );
-
-  test::scoped_nodefile_from_environment const nodefile_from_environment
-    (shared_directory, vm);
-
-  fhg::util::temporary_path const _installation_dir
-    (shared_directory / boost::filesystem::unique_path());
-  boost::filesystem::path const installation_dir (_installation_dir);
-
-  gspc::set_application_search_path (vm, installation_dir);
-  test::set_virtual_memory_socket_name_for_localhost (vm);
-
-  vm.notify();
-
-  gspc::installation const installation (vm);
-
-  gspc::scoped_rifds const rifds ( gspc::rifd::strategy {vm}
-                                 , gspc::rifd::hostnames {vm}
-                                 , gspc::rifd::port {vm}
-                                 , installation
-                                 );
+  INSTALLATION_AND_RFIDS();
 
   unsigned long total_buffer_size (0);
 
@@ -116,5 +119,55 @@ BOOST_AUTO_TEST_CASE (arbitrary_buffer_sizes_and_default_alignments)
   BOOST_CHECK_EQUAL
     ( result.find ("done")->second
     , pnet::type::value::value_type (we::type::literal::control())
+    );
+}
+
+BOOST_AUTO_TEST_CASE
+  (arbitrary_buffer_sizes_and_alignments_insufficient_memory)
+{
+  INSTALLATION_AND_RFIDS();
+
+  unsigned long total_buffer_size (0);
+
+  fhg::util::temporary_path const _workflow_dir
+   (shared_directory / boost::filesystem::unique_path());
+  boost::filesystem::path const workflow_dir (_workflow_dir);
+
+  boost::filesystem::ofstream ofs
+    ( workflow_dir
+    /"net_with_arbitrary_buffer_sizes_and_alignments_insufficient_memory.xpnet"
+    );
+  std::string net_description
+    (net_with_arbitrary_buffer_sizes_and_alignments_insufficient_memory
+       (total_buffer_size)
+    );
+  ofs << net_description;
+  ofs.close();
+
+  test::make_net_lib_install const make
+     ( installation
+     , "net_with_arbitrary_buffer_sizes_and_alignments_insufficient_memory"
+     , workflow_dir
+     , installation_dir
+     );
+
+  gspc::scoped_runtime_system const drts
+     ( vm
+     , installation
+     , "worker:1," + std::to_string (total_buffer_size)
+     , rifds.entry_points()
+     );
+
+  BOOST_REQUIRE_EXCEPTION
+    ( gspc::client (drts).put_and_run
+        (gspc::workflow (make.pnet()), {{"start", we::type::literal::control()}})
+    , std::runtime_error
+    , [](std::runtime_error const& exc)
+      {
+        return std::string (exc.what()).find
+          ("Please take into account also the buffer alignments "
+           "when allocating local shared memory!"
+          ) != std::string::npos;
+      }
     );
 }
