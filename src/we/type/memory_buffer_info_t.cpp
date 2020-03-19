4056fe3f6181c140d29f61ebb812c7d18f9866b7
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2020-03-19 15:41:02 +0100

    enforce the alignments be powers of 2

--- src/we/type/memory_buffer_info_t.cpp
+++ src/we/type/memory_buffer_info_t.cpp
@@ -5,6 +5,12 @@ namespace we
 {
   namespace type
   {
+    //note: equivalent to std::has_single_bit (c++ 20)
+    bool is_power_of_2 (unsigned long n)
+    {
+      return n != 0 && (n & (n - 1)) == 0;
+    }
+
     //! For deserialization only.
     memory_buffer_info_t::memory_buffer_info_t() = default;
 
@@ -26,8 +32,18 @@ namespace we
        (expr::eval::context const& input) const
      {
        expr::eval::context context (input);
-       return boost::get<unsigned long>
-         (expr::parse::parser (_alignment).eval_all (context));
+       auto const alignment 
+         ( boost::get<unsigned long>
+             (expr::parse::parser (_alignment).eval_all (context))
+         );
+       
+       if (!is_power_of_2 (alignment))
+       {
+         throw std::runtime_error 
+           ("Invalid alignment expression. The alignment should be a power of 2!");
+       }
+      
+       return alignment;
      }
   }
 }
