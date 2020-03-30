4d79bcc62d771f34223522cc1a4dbde43e08e47e
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2020-03-30 17:00:46 +0200

    we: activity.cpp: white space

--- src/we/type/activity.cpp
+++ src/we/type/activity.cpp
@@ -1,329 +1,318 @@
-// {petry,rahn}@itwm.fhg.de
-
-#include <we/type/net.hpp>
-
-#include <we/type/transition.hpp>
-
 #include <we/type/activity.hpp>
 
-#include <util-generic/nest_exceptions.hpp>
+#include <we/type/net.hpp>
+#include <we/type/transition.hpp>
 
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/format.hpp>
-#include <boost/lexical_cast.hpp>
 #include <boost/range/adaptor/map.hpp>
 
 #include <algorithm>
 #include <cassert>
-#include <sstream>
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <sstream>
 #include <utility>
 
 namespace we
 {
-    namespace type
-    {
-      activity_t::activity_t
-        ( const we::type::transition_t& transition
-        , boost::optional<we::transition_id_type> const& transition_id
-        )
+  namespace type
+  {
+    activity_t::activity_t
+      ( const we::type::transition_t& transition
+      , boost::optional<we::transition_id_type> const& transition_id
+      )
         : _transition (transition)
         , _transition_id (transition_id)
-      {}
+    {}
 
-      namespace
+    namespace
+    {
+      void decode (std::istream& s, activity_t& t)
       {
-        void decode (std::istream& s, activity_t& t)
+        try
         {
-          try
-          {
-            boost::archive::text_iarchive ar (s);
+          boost::archive::text_iarchive ar (s);
 
-            ar >> BOOST_SERIALIZATION_NVP (t);
-          }
-          catch (std::exception const &ex)
-          {
-            throw std::runtime_error
-              ( ( boost::format ("deserialization error: '%1%'") % ex.what()
-                ).str()
-              );
-          }
+          ar >> BOOST_SERIALIZATION_NVP (t);
         }
-      }
-
-      activity_t::activity_t (const boost::filesystem::path& path)
-      {
-        std::ifstream stream (path.string().c_str());
-
-        if (!stream)
+        catch (std::exception const &ex)
         {
           throw std::runtime_error
-            ((boost::format ("could not open '%1%' for reading") % path).str());
-        }
-
-        decode (stream, *this);
-      }
-
-      activity_t::activity_t (std::istream& stream)
-      {
-        decode (stream, *this);
-      }
-
-      activity_t::activity_t (const std::string& s)
-      {
-        std::istringstream stream (s);
-
-        decode (stream, *this);
-      }
-
-      std::string activity_t::to_string() const
-      {
-        std::ostringstream oss;
-        boost::archive::text_oarchive ar (oss);
-        ar << BOOST_SERIALIZATION_NVP (*this);
-        return oss.str();
-      }
-
-      void activity_t::add_input
-        ( we::port_id_type const& port_id
-        , pnet::type::value::value_type const& value
-        )
-      {
-        if (_transition.net())
-        {
-          //! \todo is the conditional neccessary? isn't is ensured already?
-          if (_transition.ports_input().at (port_id).associated_place())
-          {
-            _transition.mutable_net().put_value
-              ( *_transition.ports_input().at (port_id).associated_place()
-              , value
-              );
-          }
-        }
-        else
-        {
-          _input.emplace_back (value, port_id);
-        }
-      }
-
-      const we::type::transition_t& activity_t::transition() const
-      {
-        return _transition;
-      }
-
-      void activity_t::set_wait_for_output()
-      {
-        return _transition.set_property ({"drts", "wait_for_output"}, true);;
-      }
-      void activity_t::put_token
-        (std::string place_name, pnet::type::value::value_type const& token)
-      {
-        return _transition.mutable_net()
-          . put_token (std::move (place_name), token)
-          ;
-      }
-      void activity_t::inject ( activity_t const& result
-                              , workflow_response_callback workflow_response
-                              , eureka_response_callback eureka_response
-                              )
-      {
-        return _transition.mutable_net()
-          . inject ( result
-                   , std::move (workflow_response)
-                   , std::move (eureka_response)
-                   );
-      }
-      boost::optional<activity_t>
-        activity_t::extract
-          ( std::mt19937& random_engine
-          , workflow_response_callback const& workflow_response
-          , eureka_response_callback const& eureka_response
-          , gspc::we::plugin::Plugins& plugins
-          , gspc::we::plugin::PutToken put_token
-          )
-      {
-        return _transition.mutable_net()
-          . fire_expressions_and_extract_activity_random
-            ( random_engine
-            , workflow_response
-            , eureka_response
-            , plugins
-            , std::move (put_token)
-            )
-          ;
-      }
-
-      const activity_t::input_t& activity_t::input() const
-      {
-        return _input;
-      }
-
-      activity_t::output_t activity_t::output() const
-      {
-        if (_transition.net())
-        {
-          output_t output;
-
-          for ( we::type::transition_t::port_map_t::value_type const& p
-              : _transition.ports_output()
-              )
-          {
-                const we::port_id_type& port_id (p.first);
-                const we::place_id_type& pid
-                  (*p.second.associated_place());
-
-                for ( const pnet::type::value::value_type& token
-                    : _transition.net()->get_token (pid)
-                    | boost::adaptors::map_values
-                    )
-                {
-                  output.emplace_back (token, port_id);
-                }
-          }
-
-          return output;
-        }
-        else
-        {
-          return _output;
-        }
-      }
-
-      void activity_t::add_output
-        ( we::port_id_type const& port_id
-        , pnet::type::value::value_type const& value
-        )
-      {
-        _output.emplace_back (value, port_id);
-      }
-      void activity_t::add_output (expr::eval::context const& output)
-      {
-        for (auto const& port_by_id : _transition.ports_output())
-        {
-          add_output
-            (port_by_id.first, output.value ({port_by_id.second.name()}));
-        }
-      }
-
-      bool activity_t::wait_for_output() const
-      {
-        if (!_transition.prop().is_true ({"drts", "wait_for_output"}))
-        {
-          return false;
-        }
-
-        output_t const out (output());
-
-        if (out.size() < _transition.ports_output().size())
-        {
-          return true;
-        }
-
-        std::unordered_set<port_id_type> port_ids_with_output;
-
-        for (port_id_type port_id : out | boost::adaptors::map_values)
-        {
-          port_ids_with_output.emplace (port_id);
-        }
-
-        return port_ids_with_output.size() != _transition.ports_output().size();
-      }
-
-      boost::optional<we::transition_id_type> const&
-        activity_t::transition_id() const
-      {
-        return _transition_id;
-      }
-
-      expr::eval::context activity_t::evaluation_context() const
-      {
-        expr::eval::context context;
-
-        for ( std::pair< pnet::type::value::value_type
-                       , we::port_id_type
-                       > const& token_on_port
-            : _input
-            )
-        {
-          context.bind_ref
-            ( transition().ports_input().at (token_on_port.second).name()
-            , token_on_port.first
+            ( ( boost::format ("deserialization error: '%1%'") % ex.what()
+              ).str()
             );
         }
-
-        return context;
-      }
-
-      namespace
-      {
-        template<typename T>
-          boost::optional<T> eval_schedule_data
-            ( transition_t const& transition
-            , expr::eval::context context
-            , const std::string& key
-            )
-        {
-          boost::optional<const property::value_type&> expression_value
-            (transition.prop().get ({"fhg", "drts", "schedule", key}));
-
-          if (!expression_value)
-          {
-            return boost::none;
-          }
-
-          expression_t const expression
-            (boost::get<std::string> (expression_value.get()));
-
-          return boost::get<T> (expression.ast().eval_all (context));
-        }
-
-        boost::optional<std::string> eval_dynamic_requirement
-          ( transition_t const& transition
-          , expr::eval::context context
-          )
-        {
-          boost::optional<const property::value_type&> expression_value
-            (transition.prop().get ({"fhg", "drts", "require", "dynamic_requirement"}));
-
-          if (!expression_value)
-          {
-            return boost::none;
-          }
-
-          expression_t const expression
-            (boost::get<std::string> (expression_value.get()));
-
-          return boost::get<std::string> (expression.ast().eval_all (context));
-        }
-      }
-
-      schedule_data activity_t::get_schedule_data() const
-      {
-        return { eval_schedule_data<unsigned long>
-                   (_transition, evaluation_context(), "num_worker")
-               };
-      }
-
-      std::list<we::type::requirement_t> const activity_t::requirements() const
-      {
-        std::list<we::type::requirement_t> requirements (_transition.requirements());
-
-        boost::optional<std::string> dynamic_requirement
-          (eval_dynamic_requirement (_transition, evaluation_context()));
-
-        if (dynamic_requirement)
-        {
-          requirements.emplace_back (dynamic_requirement.get(), true);
-        }
-
-        return requirements;
-      }
-
-      std::list<we::type::preference_t> const activity_t::preferences() const
-      {
-        return _transition.preferences();
       }
     }
+
+    activity_t::activity_t (const boost::filesystem::path& path)
+    {
+      std::ifstream stream (path.string().c_str());
+
+      if (!stream)
+      {
+        throw std::runtime_error
+          ((boost::format ("could not open '%1%' for reading") % path).str());
+      }
+
+      decode (stream, *this);
+    }
+
+    activity_t::activity_t (std::istream& stream)
+    {
+      decode (stream, *this);
+    }
+
+    activity_t::activity_t (const std::string& s)
+    {
+      std::istringstream stream (s);
+
+      decode (stream, *this);
+    }
+
+    std::string activity_t::to_string() const
+    {
+      std::ostringstream oss;
+      boost::archive::text_oarchive ar (oss);
+      ar << BOOST_SERIALIZATION_NVP (*this);
+      return oss.str();
+    }
+
+    void activity_t::add_input
+      ( we::port_id_type const& port_id
+      , pnet::type::value::value_type const& value
+      )
+    {
+      if (_transition.net())
+      {
+        //! \todo is the conditional neccessary? isn't is ensured already?
+        if (_transition.ports_input().at (port_id).associated_place())
+        {
+          _transition.mutable_net().put_value
+            ( *_transition.ports_input().at (port_id).associated_place()
+            , value
+            );
+        }
+      }
+      else
+      {
+        _input.emplace_back (value, port_id);
+      }
+    }
+
+    const we::type::transition_t& activity_t::transition() const
+    {
+      return _transition;
+    }
+
+    void activity_t::set_wait_for_output()
+    {
+      return _transition.set_property ({"drts", "wait_for_output"}, true);;
+    }
+    void activity_t::put_token
+      (std::string place_name, pnet::type::value::value_type const& token)
+    {
+      return _transition.mutable_net()
+        . put_token (std::move (place_name), token)
+        ;
+    }
+    void activity_t::inject ( activity_t const& result
+                            , workflow_response_callback workflow_response
+                            , eureka_response_callback eureka_response
+                            )
+    {
+      return _transition.mutable_net()
+        . inject ( result
+                 , std::move (workflow_response)
+                 , std::move (eureka_response)
+                 );
+    }
+    boost::optional<activity_t>
+      activity_t::extract
+      ( std::mt19937& random_engine
+      , workflow_response_callback const& workflow_response
+      , eureka_response_callback const& eureka_response
+      , gspc::we::plugin::Plugins& plugins
+      , gspc::we::plugin::PutToken put_token
+      )
+    {
+      return _transition.mutable_net()
+        . fire_expressions_and_extract_activity_random
+          ( random_engine
+          , workflow_response
+          , eureka_response
+          , plugins
+          , std::move (put_token)
+          )
+        ;
+    }
+
+    const activity_t::input_t& activity_t::input() const
+    {
+      return _input;
+    }
+
+    activity_t::output_t activity_t::output() const
+    {
+      if (_transition.net())
+      {
+        output_t output;
+
+        for ( we::type::transition_t::port_map_t::value_type const& p
+            : _transition.ports_output()
+            )
+        {
+          const we::port_id_type& port_id (p.first);
+          const we::place_id_type& pid
+            (*p.second.associated_place());
+
+          for ( const pnet::type::value::value_type& token
+              : _transition.net()->get_token (pid) | boost::adaptors::map_values
+              )
+          {
+            output.emplace_back (token, port_id);
+          }
+        }
+
+        return output;
+      }
+      else
+      {
+        return _output;
+      }
+    }
+
+    void activity_t::add_output
+      ( we::port_id_type const& port_id
+      , pnet::type::value::value_type const& value
+      )
+    {
+      _output.emplace_back (value, port_id);
+    }
+    void activity_t::add_output (expr::eval::context const& output)
+    {
+      for (auto const& port_by_id : _transition.ports_output())
+      {
+        add_output
+          (port_by_id.first, output.value ({port_by_id.second.name()}));
+      }
+    }
+
+    bool activity_t::wait_for_output() const
+    {
+      if (!_transition.prop().is_true ({"drts", "wait_for_output"}))
+      {
+        return false;
+      }
+
+      output_t const out (output());
+
+      if (out.size() < _transition.ports_output().size())
+      {
+        return true;
+      }
+
+      std::unordered_set<port_id_type> port_ids_with_output;
+
+      for (port_id_type port_id : out | boost::adaptors::map_values)
+      {
+        port_ids_with_output.emplace (port_id);
+      }
+
+      return port_ids_with_output.size() != _transition.ports_output().size();
+    }
+
+    boost::optional<we::transition_id_type> const&
+      activity_t::transition_id() const
+    {
+      return _transition_id;
+    }
+
+    expr::eval::context activity_t::evaluation_context() const
+    {
+      expr::eval::context context;
+
+      for (auto const& token_on_port : _input)
+      {
+        context.bind_ref
+          ( transition().ports_input().at (token_on_port.second).name()
+          , token_on_port.first
+          );
+      }
+
+      return context;
+    }
+
+    namespace
+    {
+      template<typename T>
+        boost::optional<T> eval_schedule_data
+        ( transition_t const& transition
+        , expr::eval::context context
+        , const std::string& key
+        )
+      {
+        boost::optional<const property::value_type&> expression_value
+          (transition.prop().get ({"fhg", "drts", "schedule", key}));
+
+        if (!expression_value)
+        {
+          return boost::none;
+        }
+
+        expression_t const expression
+          (boost::get<std::string> (expression_value.get()));
+
+        return boost::get<T> (expression.ast().eval_all (context));
+      }
+
+      boost::optional<std::string> eval_dynamic_requirement
+        ( transition_t const& transition
+        , expr::eval::context context
+        )
+      {
+        boost::optional<const property::value_type&> expression_value
+          (transition.prop().get ({"fhg", "drts", "require", "dynamic_requirement"}));
+
+        if (!expression_value)
+        {
+          return boost::none;
+        }
+
+        expression_t const expression
+          (boost::get<std::string> (expression_value.get()));
+
+        return boost::get<std::string> (expression.ast().eval_all (context));
+      }
+    }
+
+    schedule_data activity_t::get_schedule_data() const
+    {
+      return { eval_schedule_data<unsigned long>
+        (_transition, evaluation_context(), "num_worker")
+      };
+    }
+
+    std::list<we::type::requirement_t> const activity_t::requirements() const
+    {
+      std::list<we::type::requirement_t> requirements (_transition.requirements());
+
+      boost::optional<std::string> dynamic_requirement
+        (eval_dynamic_requirement (_transition, evaluation_context()));
+
+      if (dynamic_requirement)
+      {
+        requirements.emplace_back (dynamic_requirement.get(), true);
+      }
+
+      return requirements;
+    }
+
+    std::list<we::type::preference_t> const activity_t::preferences() const
+    {
+      return _transition.preferences();
+    }
+  }
 }
