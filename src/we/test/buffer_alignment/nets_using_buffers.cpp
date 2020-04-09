d89bdce4e979267470a49d7c973be58abb76c359
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2020-04-09 10:10:55 +0200

    we: test: reduce difference between net generators
    diff a b
    
    ```
    17,19c17
    <     auto const exp
    <       (fhg::util::testing::random<unsigned long>{}(10, 0));
    <     unsigned long const buffer_alignment (1ul << exp);
    ---
    >     unsigned long const buffer_alignment (1);
    24c22
    <                              (buffer_name, buffer_size, buffer_alignment);
    ---
    >                              (buffer_name, buffer_size);
    ```
    
    diff a c
    
    ```
    21c21
    <     total_buffer_size += buffer_size + buffer_alignment - 1;
    ---
    >     total_buffer_size += buffer_size;
    ```

--- src/we/test/buffer_alignment/nets_using_buffers.cpp
+++ src/we/test/buffer_alignment/nets_using_buffers.cpp
@@ -40,7 +40,7 @@ std::string net_with_arbitrary_buffer_sizes_and_alignments
   std::list<std::string> buffer_names;
   std::string alignment_tests;
 
-  unsigned long num_buffers
+  unsigned long const num_buffers
     (fhg::util::testing::random<unsigned long>{} (15, 5));
 
   for (unsigned int i (0); i < num_buffers; ++i)
@@ -50,7 +50,7 @@ std::string net_with_arbitrary_buffer_sizes_and_alignments
       (fhg::util::testing::random<unsigned long>{} (200, 100));
 
     auto const exp
-      (fhg::util::testing::random<unsigned long>{}(10, 0));
+      (fhg::util::testing::random<unsigned long>{} (10, 0));
     unsigned long const buffer_alignment (1ul << exp);
 
     total_buffer_size += buffer_size + buffer_alignment - 1;
@@ -77,22 +77,22 @@ std::string net_with_arbitrary_buffer_sizes_and_default_alignments
   unsigned long const num_buffers
     (fhg::util::testing::random<unsigned long>{} (15, 5));
 
-  unsigned int const default_alignment (1);
-
   for (unsigned int i (0); i < num_buffers; ++i)
   {
     auto const buffer_name (get_new_buffer_name (buffer_names));
     auto const buffer_size
       (fhg::util::testing::random<unsigned long>{} (200, 100));
 
-    total_buffer_size += buffer_size;
+    unsigned long const buffer_alignment (1);
+
+    total_buffer_size += buffer_size + buffer_alignment - 1;
 
     buffer_descriptions += create_buffer_description
                              (buffer_name, buffer_size);
 
     buffer_names.emplace_back (buffer_name);
 
-    alignment_tests += create_alignment_test (default_alignment, buffer_name);
+    alignment_tests += create_alignment_test (buffer_alignment, buffer_name);
   }
 
   return create_net_description
@@ -106,7 +106,7 @@ std::string net_with_arbitrary_buffer_sizes_and_alignments_insufficient_memory
   std::list<std::string> buffer_names;
   std::string alignment_tests;
 
-  unsigned long num_buffers
+  unsigned long const num_buffers
     (fhg::util::testing::random<unsigned long>{} (15, 5));
 
   for (unsigned int i (0); i < num_buffers; ++i)
