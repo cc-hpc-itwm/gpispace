f4a2eac9ac29c40f35803f35953421905c925082
Mirko Rahn <mirko.rahn@itwm.fraunhofer.de>
2020-04-09 15:34:07 +0200

    we: plugins: Tunnel out of a subnetwork
    - generate a plugin at top level
    - use it inside a subnetwork to put a token into the top level
    
    Observations:
    
     N          time     Memory
    ---------- -------- ---------
       10.000    2 sec   nothing
      100.000   14 sec   3.5 GiB
    1.000.000    ?       >12 GiB

---
+++ src/we/plugins/Tunnel.cpp
@@ -0,0 +1,45 @@
+#include <we/plugin/Base.hpp>
+
+#include <stdexcept>
+#include <string>
+#include <utility>
+
+namespace gspc
+{
+  namespace we
+  {
+    namespace plugin
+    {
+      struct Tunnel : public Base
+      {
+        Tunnel (Context const& context, PutToken put_token)
+          : _place_name
+            ( boost::get<std::string>
+                (context.value ({"plugin", "Tunnel", "place_name"}))
+            )
+          , _context_key
+            ( boost::get<std::string>
+                (context.value ({"plugin", "Tunnel", "context_key"}))
+            )
+          , _put_token (std::move (put_token))
+        {}
+
+        virtual void before_eval (Context const&) override
+        {
+          throw std::logic_error ("Tunnel::before_eval");
+        }
+        virtual void after_eval (Context const& context) override
+        {
+          return _put_token (_place_name, context.value ({_context_key}));
+        }
+
+      private:
+        std::string _place_name;
+        std::string _context_key;
+        PutToken _put_token;
+      };
+    }
+  }
+}
+
+GSPC_WE_PLUGIN_CREATE (gspc::we::plugin::Tunnel)
