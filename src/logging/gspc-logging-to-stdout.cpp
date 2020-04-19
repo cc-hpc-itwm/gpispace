24a86d6e8f38cbaaa9777dc2d83fcef032136a2d
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2020-04-19 13:49:25 +0200

    logging: allow gspc-logging-to-stdout to get runtime added emitters as well
    Allows users to start a runtime system with a console based log view,
    instead of the monitor GUI, e.g. for batch jobs or easy redirection to
    files.

--- src/logging/gspc-logging-to-stdout.cpp
+++ src/logging/gspc-logging-to-stdout.cpp
@@ -1,11 +1,14 @@
 #include <logging/endpoint.hpp>
 #include <logging/stdout_sink.hpp>
+#include <logging/tcp_server_providing_add_emitters.hpp>
 
 #include <fhg/util/boost/program_options/generic.hpp>
 #include <util-generic/print_exception.hpp>
 #include <util-generic/syscall.hpp>
 #include <util-generic/syscall/process_signal_block.hpp>
 #include <util-generic/syscall/signal_set.hpp>
+
+#include <boost/utility/in_place_factory.hpp>
 
 #include <iostream>
 #include <vector>
@@ -17,6 +20,8 @@ namespace option
     namespace po = fhg::util::boost::program_options;
     po::option<std::vector<fhg::logging::endpoint>> const emitters
       {"emitters", "list of emitters"};
+    po::option<unsigned short> const port
+      {"port", "a port to listen on for new emitters"};
   }
 }
 
@@ -28,11 +33,20 @@ try
 
   boost::program_options::variables_map const vm
     ( fhg::util::boost::program_options::options ("GPI-Space Logging to stdout")
-    . require (option::emitters)
+    . add (option::emitters)
+    . add (option::port)
     . store_and_notify (argc, argv)
     );
 
-  fhg::logging::stdout_sink impl (option::emitters.get_from (vm));
+  fhg::logging::stdout_sink impl (option::emitters.get_from_or_value (vm, {}));
+
+  boost::optional<fhg::logging::tcp_server_providing_add_emitters>
+    tcp_server_providing_add_emitters;
+  auto const port (option::port.get<unsigned short> (vm));
+  if (port)
+  {
+    tcp_server_providing_add_emitters = boost::in_place (&impl, *port);
+  }
 
   fhg::util::syscall::sigwaitinfo (&signals._, nullptr);
 
