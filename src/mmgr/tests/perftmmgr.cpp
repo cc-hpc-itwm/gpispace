3ab7dfbe1e64bd6800b2cd067757ce8c4fa54e30
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2020-04-21 16:20:04 +0200

    mmgr: tests: use data test case

--- src/mmgr/tests/perftmmgr.cpp
+++ src/mmgr/tests/perftmmgr.cpp
@@ -1,10 +1,12 @@
-#include <boost/test/unit_test.hpp>
-
 #include <mmgr/tmmgr.hpp>
 
 #include <util-generic/testing/flatten_nested_exceptions.hpp>
+#include <util-generic/testing/printer/set.hpp>
 #include <util-generic/testing/random.hpp>
 #include <util-generic/testing/require_maximum_running_time.hpp>
+
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/unit_test.hpp>
 
 #include <chrono>
 #include <set>
@@ -37,41 +39,39 @@ namespace
     return handles;
   }
 
-  void test_fill_clear (std::set<gspc::vmem::Handle_t> const& handles)
+  std::vector<std::set<gspc::vmem::Handle_t>> handless()
   {
-    gspc::vmem::tmmgr tmmgr (num_handles, 1);
-
-    std::chrono::steady_clock::time_point const start
-      (std::chrono::steady_clock::now());
-
-    {
-      fhg::util::testing::require_maximum_running_time<std::chrono::seconds>
-        const max_time_for_alloc (1);
-
-      for (gspc::vmem::Handle_t const& handle : handles)
-      {
-        tmmgr.alloc (handle, 1);
-      }
-    }
-
-    {
-      fhg::util::testing::require_maximum_running_time
-        <std::chrono::milliseconds>
-          const max_time_for_free
-            (std::chrono::duration_cast<std::chrono::milliseconds>
-              (std::chrono::steady_clock::now() - start)
-            );
-
-      for (gspc::vmem::Handle_t const& handle : handles)
-      {
-        tmmgr.free (handle);
-      }
-    }
+    return {handles_random(), handles_sequence()};
   }
 }
 
-BOOST_AUTO_TEST_CASE (fill_clear)
+BOOST_DATA_TEST_CASE (fill_clear, handless(), handles)
 {
-  test_fill_clear (handles_random());
-  test_fill_clear (handles_sequence());
+  gspc::vmem::tmmgr tmmgr (num_handles, 1);
+
+  std::chrono::steady_clock::time_point const start
+    (std::chrono::steady_clock::now());
+
+  {
+    fhg::util::testing::require_maximum_running_time<std::chrono::seconds>
+      const max_time_for_alloc (1);
+
+    for (gspc::vmem::Handle_t const& handle : handles)
+    {
+      tmmgr.alloc (handle, 1);
+    }
+  }
+
+  {
+    fhg::util::testing::require_maximum_running_time
+      <std::chrono::milliseconds> const max_time_for_free
+        (std::chrono::duration_cast<std::chrono::milliseconds>
+          (std::chrono::steady_clock::now() - start)
+        );
+
+    for (gspc::vmem::Handle_t const& handle : handles)
+    {
+      tmmgr.free (handle);
+    }
+  }
 }
