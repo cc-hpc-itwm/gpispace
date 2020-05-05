9a4e53a55d075b579cb8b2da55a02bee00b4d145
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2020-05-05 16:18:37 +0200

    replace workers directly finishing jobs in the RestartWorkersCoalloc test

--- src/sdpa/test/sdpa/RestartWorkersCoalloc.cpp
+++ src/sdpa/test/sdpa/RestartWorkersCoalloc.cpp
@@ -57,9 +57,18 @@ BOOST_DATA_TEST_CASE
 
   worker_0.canceled (cancel_requested_0.wait());
 
-  const utils::fake_drts_worker_directly_finishing_jobs restarted_worker
-    (utils::reused_component_name (worker_id), agent, certificates);
+  fhg::util::thread::event<std::string> job_submitted_to_restarted_worker;
+  utils::fake_drts_worker_notifying_module_call_submission restarted_worker
+    ( utils::reused_component_name (worker_id)
+    , [&job_submitted_to_restarted_worker] (std::string s)
+      { job_submitted_to_restarted_worker.notify (s); }
+    , agent
+    , certificates
+    );
+
   worker_0.finish (job_submitted_0.wait());
+  restarted_worker.finish
+    (job_submitted_to_restarted_worker.wait());
 
   BOOST_REQUIRE_EQUAL
     (client.wait_for_terminal_state (job_id), sdpa::status::FINISHED);
@@ -100,9 +109,18 @@ BOOST_DATA_TEST_CASE
     job_submitted_1.wait();
   }
 
-  const utils::fake_drts_worker_directly_finishing_jobs restarted_worker
-    (utils::reused_component_name (worker_id), agent, certificates);
+  fhg::util::thread::event<std::string> job_submitted_to_restarted_worker;
+  utils::fake_drts_worker_notifying_module_call_submission restarted_worker
+    ( utils::reused_component_name (worker_id)
+    , [&job_submitted_to_restarted_worker] (std::string s)
+      { job_submitted_to_restarted_worker.notify (s); }
+    , agent
+    , certificates
+    );
+
   worker_0.finish (job_submitted_0.wait());
+  restarted_worker.finish
+    (job_submitted_to_restarted_worker.wait());
 
   BOOST_REQUIRE_EQUAL
     (client.wait_for_terminal_state (job_id), sdpa::status::FINISHED);
