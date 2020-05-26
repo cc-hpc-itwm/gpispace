54bfae3df26c0a43fc26f654b00c2552fa793564
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2020-05-26 17:10:10 +0200

    Removed outdated comments related to put token: we know that the job was submitted either to the workers or to the workflow engine, being in running state. In case no worker assigned is found, the job was submitted to the workflow engine (provided it exists one)i, otherwise an exception is thrown.

--- src/sdpa/daemon/GenericDaemon.cpp
+++ src/sdpa/daemon/GenericDaemon.cpp
@@ -1442,18 +1442,10 @@ void GenericDaemon::handleJobFailedAckEvent
                        );
         }
       }
-      //! \todo Other criteria to know it was submitted to the
-      //! wfe. All jobs are regarded as going to the wfe and the only
-      //! way to prevent a loop is to check whether the put_token
-      //! comes out of the wfe. Special "worker" id?
       else if (workflowEngine())
       {
         _put_token_source.emplace (event->put_token_id(), source);
 
-        //! \todo There is a race here: between SubmitJobAck and
-        //! we->submit(), there's still a lot of stuff. We can't
-        //! guarantee, that the job is already submitted to the wfe!
-        //! We need to handle the "pending" state.
         workflowEngine()->put_token ( event->job_id()
                                     , event->put_token_id()
                                     , event->place_name()
