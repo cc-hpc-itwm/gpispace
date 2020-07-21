41a9d926bdba29dc098fdc3b691ff7313a0f0dc3
Tiberiu Rotaru <tiberiu.rotaru@itwm.fraunhofer.de>
2020-07-21 14:18:59 +0200

    added remote_interface class

---
+++ src/drts/remote_interface/ID.cpp
@@ -0,0 +1,33 @@
+#include <drts/remote_interface/ID.hpp>
+
+#include <tuple>
+
+namespace gspc
+{
+  namespace remote_interface
+  {
+    ID& ID::operator++()
+    {
+      ++id;
+      return *this;
+    }
+
+    bool operator== (ID const& lhs, ID const& rhs)
+    {
+      return std::tie (lhs.id) == std::tie (rhs.id);
+    }
+    std::ostream& operator<< (std::ostream& os, ID const& x)
+    {
+      return os << "remote_interface " << x.id;
+    }
+
+    bool operator== (ID const& lhs, ID const& rhs);
+    std::ostream& operator<< (std::ostream&, ID const&);
+  }
+}
+
+UTIL_MAKE_COMBINED_STD_HASH_DEFINE
+  ( gspc::remote_interface::ID
+  , x
+  , x.id
+  );
