e5d54497e1652403718662298b35c35b3e82199f
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2020-08-04 16:07:49 +0200

    split installation_path into hpp+cpp

---
+++ src/gspc/installation_path.cpp
@@ -0,0 +1,48 @@
+#include <gspc/installation_path.hpp>
+
+#include <util-generic/executable_path.hpp>
+
+namespace gspc
+{
+  installation_path::installation_path()
+    : boost::filesystem::path
+      (fhg::util::executable_path().parent_path().parent_path())
+  {}
+  installation_path::installation_path (boost::filesystem::path const& gspc_home)
+    : boost::filesystem::path (gspc_home)
+  {}
+
+  //! \todo configure
+  boost::filesystem::path installation_path::include() const
+  {
+    return *this / "include";
+  }
+  boost::filesystem::path installation_path::lib() const
+  {
+    return *this / "lib";
+  }
+  boost::filesystem::path installation_path::boost_root() const
+  {
+    return *this / "external" / "boost";
+  }
+  boost::filesystem::path installation_path::libexec_gspc() const
+  {
+    return *this / "libexec" / "gspc";
+  }
+  boost::filesystem::path installation_path::agent() const
+  {
+    return libexec_gspc() / "agent";
+  }
+  boost::filesystem::path installation_path::drts_kernel() const
+  {
+    return libexec_gspc() / "drts-kernel";
+  }
+  boost::filesystem::path installation_path::vmem() const
+  {
+    return libexec_gspc() / "gpi-space";
+  }
+  boost::filesystem::path installation_path::logging_demultiplexer() const
+  {
+    return libexec_gspc() / "gspc-logging-demultiplexer.exe";
+  }
+}
--- src/gspc/installation_path.hpp
+++ src/gspc/installation_path.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <util-generic/executable_path.hpp>
-
 #include <boost/filesystem/path.hpp>
 
 namespace gspc
@@ -9,46 +7,16 @@ namespace gspc
   class installation_path : public boost::filesystem::path
   {
   public:
-    installation_path()
-      : boost::filesystem::path
-        (fhg::util::executable_path().parent_path().parent_path())
-    {}
-    installation_path (boost::filesystem::path const& gspc_home)
-      : boost::filesystem::path (gspc_home)
-    {}
+    installation_path();
+    installation_path (boost::filesystem::path const& gspc_home);
 
-    //! \todo configure
-    boost::filesystem::path include() const
-    {
-      return *this / "include";
-    }
-    boost::filesystem::path lib() const
-    {
-      return *this / "lib";
-    }
-    boost::filesystem::path boost_root() const
-    {
-      return *this / "external" / "boost";
-    }
-    boost::filesystem::path libexec_gspc() const
-    {
-      return *this / "libexec" / "gspc";
-    }
-    boost::filesystem::path agent() const
-    {
-      return libexec_gspc() / "agent";
-    }
-    boost::filesystem::path drts_kernel() const
-    {
-      return libexec_gspc() / "drts-kernel";
-    }
-    boost::filesystem::path vmem() const
-    {
-      return libexec_gspc() / "gpi-space";
-    }
-    boost::filesystem::path logging_demultiplexer() const
-    {
-      return libexec_gspc() / "gspc-logging-demultiplexer.exe";
-    }
+    boost::filesystem::path include() const;
+    boost::filesystem::path lib() const;
+    boost::filesystem::path boost_root() const;
+    boost::filesystem::path libexec_gspc() const;
+    boost::filesystem::path agent() const;
+    boost::filesystem::path drts_kernel() const;
+    boost::filesystem::path vmem() const;
+    boost::filesystem::path logging_demultiplexer() const;
   };
 }
