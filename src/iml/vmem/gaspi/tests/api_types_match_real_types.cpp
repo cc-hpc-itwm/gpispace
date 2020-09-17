9318767a1e1a178b9b796443c6ce91f23344064e
Bernd Lörwald <bernd.loerwald@itwm.fraunhofer.de>
2020-09-17 13:30:32 +0200

    iml: move test into actual test rather than "does library compile" side effect

---
+++ src/iml/vmem/gaspi/tests/api_types_match_real_types.cpp
@@ -0,0 +1,27 @@
+#include <iml/vmem/gaspi/types.hpp>
+
+#include <util-generic/testing/require_type.hpp>
+
+#include <boost/test/unit_test.hpp>
+
+#include <GASPI.h>
+
+namespace gpi
+{
+  // We don't want to expose GASPI.h to users, but we still want the
+  // correct types, so these checks assert that the public definition
+  // and GASPI's definition are equal.
+  BOOST_AUTO_TEST_CASE (api_types_match_real_types)
+  {
+    FHG_UTIL_TESTING_CHECK_TYPE_EQUAL (offset_t, gaspi_offset_t);
+    FHG_UTIL_TESTING_CHECK_TYPE_EQUAL (rank_t, gaspi_rank_t);
+    FHG_UTIL_TESTING_CHECK_TYPE_EQUAL (queue_desc_t, gaspi_queue_id_t);
+    FHG_UTIL_TESTING_CHECK_TYPE_EQUAL (size_t, gaspi_size_t);
+    FHG_UTIL_TESTING_CHECK_TYPE_EQUAL (notification_t, gaspi_notification_t);
+    FHG_UTIL_TESTING_CHECK_TYPE_EQUAL
+      (notification_id_t, gaspi_notification_id_t);
+    FHG_UTIL_TESTING_CHECK_TYPE_EQUAL (timeout_t, gaspi_timeout_t);
+    FHG_UTIL_TESTING_CHECK_TYPE_EQUAL (segment_id_t, gaspi_segment_id_t);
+    FHG_UTIL_TESTING_CHECK_TYPE_EQUAL (netdev_id_t, gaspi_int);
+  }
+}
